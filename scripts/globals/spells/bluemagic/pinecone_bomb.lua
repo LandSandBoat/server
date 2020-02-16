a9a18a0ec5ac02f5aa21bc642475fe592711d7a4
ibm2431 <ibm2431@dekumi.com>
2020-02-16 02:12:48 -0800

    Update references from previous branding

--- scripts/globals/spells/bluemagic/pinecone_bomb.lua
+++ scripts/globals/spells/bluemagic/pinecone_bomb.lua
@@ -24,22 +24,22 @@ end
 function onSpellCast(caster,target,spell)
     local params = {}
     -- This data should match information on http://wiki.ffxiclopedia.org/wiki/Calculating_Blue_Magic_Damage
-        params.tpmod = TPMOD_CRITICAL
-        params.dmgtype = DMGTYPE_PIERCE
-        params.scattr = SC_LIQUEFACTION
-        params.numhits = 1
-        params.multiplier = 1.25
-        params.tp150 = 1.25
-        params.tp300 = 1.25
-        params.azuretp = 1.25
-        params.duppercap = 37
-        params.str_wsc = 0.20
-        params.dex_wsc = 0.0
-        params.vit_wsc = 0.0
-        params.agi_wsc = 0.20
-        params.int_wsc = 0.0
-        params.mnd_wsc = 0.0
-        params.chr_wsc = 0.0
+    params.tpmod = TPMOD_CRITICAL
+    params.dmgtype = DMGTYPE_PIERCE
+    params.scattr = SC_LIQUEFACTION
+    params.numhits = 1
+    params.multiplier = 1.25
+    params.tp150 = 1.25
+    params.tp300 = 1.25
+    params.azuretp = 1.25
+    params.duppercap = 37
+    params.str_wsc = 0.20
+    params.dex_wsc = 0.0
+    params.vit_wsc = 0.0
+    params.agi_wsc = 0.20
+    params.int_wsc = 0.0
+    params.mnd_wsc = 0.0
+    params.chr_wsc = 0.0
 
     local damage = BluePhysicalSpell(caster, target, spell, params)
     damage = BlueFinalAdjustments(caster, target, spell, damage, params)
@@ -48,10 +48,10 @@ function onSpellCast(caster,target,spell)
     -- preexisting sleep, if necesesary), apply the sleep effect for this spell.
     if (damage > 0) then
         local sleepParams = {}
-            sleepParams.attribute = dsp.mod.INT
-            sleepParams.bonus = 0
-            sleepParams.effect = dsp.effect.SLEEP_II
-            sleepParams.skillType = dsp.skill.BLUE_MAGIC
+        sleepParams.attribute = tpz.mod.INT
+        sleepParams.bonus = 0
+        sleepParams.effect = tpz.effect.SLEEP_II
+        sleepParams.skillType = tpz.skill.BLUE_MAGIC
         local resist = applyResistanceEffect(caster, target, spell, sleepParams)
         if (resist > 0.5) then -- Apply the sleep
             local power = 2
