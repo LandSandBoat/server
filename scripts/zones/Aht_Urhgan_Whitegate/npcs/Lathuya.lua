e908b5f8297831e6d70fbde489ad439a50545e0c
kaincenteno <kain.centeno@catsangel.com>
2020-02-19 15:41:39 -0800

    Added Omens Quest (BLU AF2) Changed BEGINNINGS to check correct AF1 global Position of flans not accurate to retail as there's no captures for a full alliance fighting BCNM Fixed commas missing spaces after them. Immortal Flans do not have any special AI or move sets that differences them from other flans. Thank you Wiggo for capturing this quest.

--- scripts/zones/Aht_Urhgan_Whitegate/npcs/Lathuya.lua
+++ scripts/zones/Aht_Urhgan_Whitegate/npcs/Lathuya.lua
@@ -2,17 +2,53 @@
 -- Area: Aht Urhgan Whitegate
 --  NPC: Lathuya
 -- Standard Info NPC
+-- Involved in quests: Omens
+-- !pos -95.081 -6.000 31.638 50
+-----------------------------------
+require("scripts/globals/quests")
+require("scripts/globals/npc_util")
 -----------------------------------
 
-function onTrade(player,npc,trade)
+function onTrade(player, npc, trade)
 end
 
-function onTrigger(player,npc)
-    player:startEvent(770)
+function onTrigger(player, npc)
+    local omensProgress = player:getCharVar("OmensProgress")
+    local omens = player:getQuestStatus(AHT_URHGAN, tpz.quest.id.ahtUrhgan.OMENS)
+
+    -- OMENS
+    if omens >= QUEST_ACCEPTED then
+        if omensProgress == 3 then
+            player:startEvent(714) -- Tells Master location
+        elseif omensProgress == 4 then
+            player:startEvent(715) -- Reminder of master location
+        elseif omensProgress== 5 then
+            player:startEvent(716) -- Quest Completed
+        elseif omens == QUEST_COMPLETED then
+            player:startEvent(771) -- Default dialog
+        end
+
+    -- DEFAULT DIALOG
+    else
+        player:startEvent(770)
+    end
 end
 
-function onEventUpdate(player,csid,option)
+function onEventUpdate(player, csid, option)
 end
 
-function onEventFinish(player,csid,option)
+function onEventFinish(player, csid, option)
+    local omensProgress = player:getCharVar("OmensProgress")
+
+    -- OMENS
+    if csid == 714 and omensProgress == 3 then
+        player:setCharVar("OmensProgress", 4)
+    elseif csid == 716 and omensProgress == 5 then
+        npcUtil.completeQuest(player, AHT_URHGAN, tpz.quest.id.ahtUrhgan.OMENS, {
+            item = 15684,
+            title = tpz.title.IMMORTAL_LION,
+            var = { OmensProgress }
+        })
+        player:delKeyItem(tpz.ki.SEALED_IMMORTAL_ENVELOPE)
+    end
 end
---
+++ scripts/zones/Navukgo_Execution_Chamber/bcnms/omens.lua
@@ -0,0 +1,57 @@
+-----------------------------------
+-- Area: Navukgo Execution Chamber
+-- BCNM: Omens
+-----------------------------------
+require("scripts/globals/battlefield")
+local ID = require("scripts/zones/Navukgo_Execution_Chamber/IDs")
+----------------------------------------
+
+function onBattlefieldInitialise(battlefield)
+end
+
+function onBattlefieldTick(battlefield, tick)
+    tpz.battlefield.onBattlefieldTick(battlefield, tick)
+end
+
+function onBattlefieldRegister(player, battlefield)
+end
+
+function onBattlefieldEnter(player, battlefield)
+    local mobOffset = (battlefield:getArea() - 1) * 7  -- Offset to spawn correct mob depending on battlefieldNumber
+    local players = battlefield:getPlayers()
+    local flan2 = GetMobByID(ID.mob.IMMORTAL_FLAN2 + mobOffset)
+    local flan3 = GetMobByID(ID.mob.IMMORTAL_FLAN3 + mobOffset)
+    local flan4 = GetMobByID(ID.mob.IMMORTAL_FLAN4 + mobOffset)
+    local flan5 = GetMobByID(ID.mob.IMMORTAL_FLAN5 + mobOffset)
+    local flan6 = GetMobByID(ID.mob.IMMORTAL_FLAN6 + mobOffset)
+    -- Spawns flans depending on party size. Flan1 always spawns by default
+    if not flan6:isSpawned() and #players >= 16 then
+        flan6:spawn()
+    elseif not flan5:isSpawned() and #players >= 14 then
+        flan5:spawn()
+    elseif not flan4:isSpawned() and #players >= 12 then
+        flan4:spawn()
+    elseif not flan3:isSpawned() and #players >=  8 then
+        flan3:spawn()
+    elseif not flan2:isSpawned() and #players >=  4 then
+        flan2:spawn()
+    end
+end
+
+function onBattlefieldLeave(player, battlefield, leavecode)
+    if leavecode == tpz.battlefield.leaveCode.WON then
+        local name, clearTime, partySize = battlefield:getRecord()
+        player:startEvent(32001, battlefield:getArea(), clearTime, partySize, battlefield:getTimeInside(), 1, battlefield:getLocalVar("[cs]bit"), 0)
+    elseif leavecode == tpz.battlefield.leaveCode.LOST then
+        player:startEvent(32002)
+    end
+end
+
+function onEventUpdate(player, csid, option)
+end
+
+function onEventFinish(player, csid, option)
+    if csid == 32001 and player:getCharVar("OmensProgress") == 1 then
+        player:setCharVar("OmensProgress",2)
+    end
+end
