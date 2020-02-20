d428375011d0f67b753232f9b3992bcbfb2b20fd
Mark Klara <mrhappyasthma@gmail.com>
2020-02-20 08:07:03 -0800

    Rename `dmgtype` to `damageType`.
    This is needed after https://github.com/project-topaz/topaz/commit/b4ed738e8901e0127ffe19b855d6a886f82c476e

--- scripts/globals/spells/bluemagic/pinecone_bomb.lua
+++ scripts/globals/spells/bluemagic/pinecone_bomb.lua
@@ -25,7 +25,7 @@ function onSpellCast(caster,target,spell)
     local params = {}
     -- This data should match information on http://wiki.ffxiclopedia.org/wiki/Calculating_Blue_Magic_Damage
     params.tpmod = TPMOD_DURATION
-    params.dmgtype = DMGTYPE_PIERCE
+    params.damageType = DMGTYPE_PIERCE
     params.scattr = SC_LIQUEFACTION
     params.numhits = 1
     params.multiplier = 2.25
