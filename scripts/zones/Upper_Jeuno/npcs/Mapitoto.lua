74ae73962241d92d3688a1169a68f1cde5ed1802
Zach Toogood <zrtoogood@gmail.com>
2020-02-29 18:03:37 +0200

    Add Mapitoto text

--- scripts/zones/Upper_Jeuno/npcs/Mapitoto.lua
+++ scripts/zones/Upper_Jeuno/npcs/Mapitoto.lua
@@ -19,6 +19,10 @@ function onTrigger(player,npc)
       player:getMainLvl() >= 20 and
       player:hasKeyItem(tpz.ki.MAP_OF_THE_JEUNO_AREA)
     then
+        -- Raptor riding minigame
+        -- player:startEvent(10223)
+    else
+        player:startEvent(10222)
     end
 end
 
