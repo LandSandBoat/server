1e5e8530f54267c51bc06324813f7a05742a25c4
Zach Toogood <zrtoogood@gmail.com>
2020-02-29 13:38:25 +0200

    Mapitoto and Raptor

---
+++ scripts/zones/Upper_Jeuno/npcs/Mapitoto.lua
@@ -0,0 +1,29 @@
+-----------------------------------
+-- Area: Upper Jeuno
+--  NPC: Mapitoto
+-- Type: Full Speed Ahead Mount NPC
+-- !pos -54.310 8.200 85.940 244
+-----------------------------------
+local ID = require("scripts/zones/Upper_Jeuno/IDs")
+require("scripts/globals/chocobo")
+require("scripts/globals/keyitems")
+require("scripts/globals/status")
+-----------------------------------
+
+function onTrade(player,npc,trade)
+end
+
+function onTrigger(player,npc)
+    if
+      player:hasKeyItem(tpz.ki.CHOCOBO_LICENSE) and
+      player:getMainLvl() >= 20 and
+      player:hasKeyItem(tpz.ki.MAP_OF_THE_JEUNO_AREA)
+    then
+    end
+end
+
+function onEventUpdate(player,csid,option)
+end
+
+function onEventFinish(player,csid,option)
+end
