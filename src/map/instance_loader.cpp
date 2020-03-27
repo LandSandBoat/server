33918dee64ffd787a1ee9ebfc4d87941b2b9d65e
ibm2431 <ibm2431@dekumi.com>
2020-03-27 13:55:29 -0700

    Move charmable check out of CalculateStats, and into zone and instance loaders

--- src/map/instance_loader.cpp
+++ src/map/instance_loader.cpp
@@ -230,6 +230,14 @@ CInstance* CInstanceLoader::LoadInstance(CInstance* instance)
 
             PMob->setMobMod(MOBMOD_CHARMABLE, Sql_GetUIntData(SqlInstanceHandle, 65));
 
+            // Overwrite base family charmables depending on mob type. Disallowed mobs which should be charmable
+            // can be set in mob_spawn_mods or in their onInitialize
+            if (PMob->m_Type & MOBTYPE_EVENT || PMob->m_Type & MOBTYPE_FISHED || PMob->m_Type & MOBTYPE_BATTLEFIELD ||
+                PMob->m_Type & MOBTYPE_NOTORIOUS)
+            {
+                PMob->setMobMod(MOBMOD_CHARMABLE, 0);
+            }
+
             // must be here first to define mobmods
             mobutils::InitializeMob(PMob, zone);
             PMob->PInstance = instance;
