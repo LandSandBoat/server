3957316edffc89e9268bd4a959e4d5bef7433df2
ibm2431 <ibm2431@dekumi.com>
2020-04-02 23:38:42 -0700

    Make caskets stop trying to disappear players

--- scripts/globals/caskets.lua
+++ scripts/globals/caskets.lua
@@ -723,7 +723,7 @@ local function giveItem(player, npc, itemNum)
                     if player:addItem(itemID, 33) then
                         messageChest(player, "PLAYER_OBTAINS_ITEM", itemID, 0, 0, 0)
                         npc:setLocalVar(itemQuery, 0)
-                        checkItemChestIsEmpty(player, npc)
+                        checkItemChestIsEmpty(npc)
                     end
                 else
                     if player:addItem(itemID) then
