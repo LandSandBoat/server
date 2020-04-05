3f9d243104c93b3b134df3c6dd249d698f441949
wrenffxi <oliviapekoe@gmail.com>
2020-04-04 20:18:10 -0400

    [version update] textID shifts

--- scripts/zones/Bastok_Mines/IDs.lua
+++ scripts/zones/Bastok_Mines/IDs.lua
@@ -72,8 +72,8 @@ zones[tpz.zone.BASTOK_MINES] =
         EMALIVEULAUX_COP_NOT_COMPLETED = 12241, -- I'd like to start my own business someday, but I just haven't found anything that truly interests me.
         EMALIVEULAUX_OPEN_DIALOG       = 12242, -- Rare Tavnazian imports! Get them before they're gone!
         EMALIVEULAUX_CLOSED_DIALOG     = 12243, -- I'd love to sell you goods imported from the island of Tavnazia, but with the area under foreign control, I can't secure my trade routes...
-        COMMON_SENSE_SURVIVAL          = 15991, -- It appears that you have arrived at a new survival guide provided by the Adventurers' Mutual Aid Network. Common sense dictates that you should now be able to teleport here from similar tomes throughout the world.
-        TEAR_IN_FABRIC_OF_SPACE        = 16588, -- There appears to be a tear in the fabric of space...
+        COMMON_SENSE_SURVIVAL          = 15996, -- It appears that you have arrived at a new survival guide provided by the Adventurers' Mutual Aid Network. Common sense dictates that you should now be able to teleport here from similar tomes throughout the world.
+        TEAR_IN_FABRIC_OF_SPACE        = 16593, -- There appears to be a tear in the fabric of space...
     },
     mob =
     {
