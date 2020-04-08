1160e433c5ab085aa8097b37fa0be4b87801f924
Solarsurge <solarsurge@comcast.net>
2020-04-08 04:26:44 -0400

    Updated
    *Added blinding light event on trade for map acquisition per request
    *Fixed crash in Mea for missing text

--- scripts/zones/Promyvion-Mea/IDs.lua
+++ scripts/zones/Promyvion-Mea/IDs.lua
@@ -10,6 +10,7 @@ zones[tpz.zone.PROMYVION_MEA] =
 {
     text =
     {
+        NOTHING_HAPPENS         = 119, -- Nothing happens...
         ITEM_CANNOT_BE_OBTAINED = 6382, -- You cannot obtain the <item>. Come back after sorting your inventory.
         ITEM_OBTAINED           = 6388, -- Obtained: <item>.
         GIL_OBTAINED            = 6389, -- Obtained <number> gil.
--- scripts/zones/Promyvion-Mea/npcs/qm1.lua
+++ scripts/zones/Promyvion-Mea/npcs/qm1.lua
@@ -1,6 +1,7 @@
 -----------------------------------
 -- Area: Promyvion Mea
 -- ??? map acquisition
+-- NPC ID 16859468
 -----------------------------------
 local ID = require("scripts/zones/Promyvion-Mea/IDs")
 require("scripts/globals/keyitems")
@@ -13,8 +14,8 @@ end
 
 function onTrade(player, npc, trade)
     if npcUtil.tradeHas(trade, 1722) and not player:hasKeyItem(tpz.ki.MAP_OF_PROMYVION_MEA) then
-        npcUtil.giveKeyItem(player, tpz.ki.MAP_OF_PROMYVION_MEA)
         player:confirmTrade()
+        player:startEvent(49)
     else
         player:messageSpecial(ID.text.NOTHING_HAPPENS)
     end
@@ -24,4 +25,7 @@ function onEventUpdate(player, csid, option)
 end
 
 function onEventFinish(player, csid, option)
+    if csid == 49 then
+        npcUtil.giveKeyItem(player, tpz.ki.MAP_OF_PROMYVION_MEA)
+    end
 end
