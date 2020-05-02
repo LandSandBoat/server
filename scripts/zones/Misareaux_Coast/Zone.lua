d749e4f011029eb0fd55959e273c3cf1f2e21906
hookstar <carl.hooker@gmail.com>
2020-05-02 16:06:08 -0400

    Impliment Ziphus spawn
    Updated to Topaz from @Wiggo DSP contribution a few years ago.

--- scripts/zones/Misareaux_Coast/Zone.lua
+++ scripts/zones/Misareaux_Coast/Zone.lua
@@ -3,32 +3,52 @@
 -- Zone: Misareaux_Coast (25)
 --
 -----------------------------------
-local ID = require("scripts/zones/Misareaux_Coast/IDs")
 require("scripts/globals/conquest")
 require("scripts/globals/helm")
+require("scripts/zones/Misareaux_Coast/IDs")
 -----------------------------------
 
 function onInitialize(zone)
     tpz.helm.initZone(zone, tpz.helm.type.LOGGING)
-end;
+end
 
 function onConquestUpdate(zone, updatetype)
     tpz.conq.onConquestUpdate(zone, updatetype)
-end;
+end
 
 function onZoneIn(player,prevZone)
-    local cs = -1;
+    local cs = -1
     if (player:getXPos() == 0 and player:getYPos() == 0 and player:getZPos() == 0) then
-        player:setPos(567.624,-20,280.775,120);
+        player:setPos(567.624,-20,280.775,120)
     end
-    return cs;
-end;
+    return cs
+end
 
 function onRegionEnter(player,region)
-end;
+end
+
+function onGameHour(zone)
+    local ZIPHIUS_QM_BASE  = 16879919
+    if VanadielHour() == 22 then  -- Spawn traps for Ziphius
+        for i = ZIPHIUS_QM_BASE, ZIPHIUS_QM_BASE+5, 1 do
+            GetNPCByID(i):setStatus(tpz.status.NORMAL)
+        end
+    elseif VanadielHour() == 7 then  -- Despawn traps for Ziphius
+        for i = ZIPHIUS_QM_BASE, ZIPHIUS_QM_BASE+5, 1 do
+            GetNPCByID(i):setStatus(tpz.status.DISAPPEAR)
+            GetNPCByID(i):setLocalVar("[Ziphius]Bait Trap", 0)
+        end
+    elseif VanadielHour() == 4 then  -- Despawn non-baited traps
+        for i = ZIPHIUS_QM_BASE, ZIPHIUS_QM_BASE+5, 1 do
+            if (GetNPCByID(i):getLocalVar("[Ziphius]Bait Trap") == 0) then
+                GetNPCByID(i):setStatus(tpz.status.DISAPPEAR)
+            end
+        end
+    end
+end
 
 function onEventUpdate(player,csid,option)
-end;
+end
 
 function onEventFinish(player,csid,option)
-end;
+end
---
+++ scripts/zones/Misareaux_Coast/npcs/qm6.lua
@@ -0,0 +1,53 @@
+-----------------------------------
+-- Area: Misareaux_Coast
+-- NPC:  ??? (Spawn Ziphius)
+-- !pos 207 -16 479 25
+-----------------------------------
+
+require("scripts/globals/npc_util")
+local ID = require("scripts/zones/Misareaux_Coast/IDs")
+
+function onTrade(player,npc,trade)
+    local baited = npc:getLocalVar("[Ziphius]Bait Trap")
+
+    -- Trade Slice of Carp
+    if (npcUtil.tradeHas(trade, 16994) and baited == 0) then
+        npc:setLocalVar("[Ziphius]Bait Trap", 1)
+        player:confirmTrade()
+        player:messageSpecial(ID.text.PUT_IN_TRAP, 16994)
+    else
+        player:messageSpecial(ID.text.NOTHING_HERE_YET)
+    end
+end
+function onTrigger(player,npc)
+    local baited = npc:getLocalVar("[Ziphius]Bait Trap")
+
+    if VanadielHour() >= 22 or VanadielHour() < 4 then
+        if (baited == 0) then
+            player:messageSpecial(ID.text.APPEARS_TO_BE_TRAP)
+        else
+            player:messageSpecial(ID.text.NOTHING_HERE_YET)
+        end
+    elseif VanadielHour() >= 4 and VanadielHour() < 7 then
+        if (baited == 1) then
+            if (math.random(1,1000) <= 176) then
+                SpawnMob(ID.mob.ZIPHIUS):updateClaim(player)
+                GetMobByID(ID.mob.ZIPHIUS):setPos(npc:getXPos(),npc:getYPos(),npc:getZPos()-1)
+                npc:setStatus(tpz.status.DISAPPEAR)
+            else
+                player:messageSpecial(ID.text.DID_NOT_CATCH_ANYTHING)
+            end
+            npc:setLocalVar("[Ziphius]Bait Trap", 0)
+        else
+            player:messageSpecial(ID.text.APPEARS_TO_BE_TRAP)
+        end
+    end
+end
+
+function onEventUpdate(player,csid,option)
+
+end
+
+function onEventFinish(player,csid,option)
+
+end
