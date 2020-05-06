fbfaf095426f059b3f6a3f650776cdfbf08671e3
Zach Toogood <zrtoogood@gmail.com>
2020-05-06 14:18:36 +0300

    Move Soul Voice check from Lunatic Voice to Entice (where it should be)

--- scripts/globals/mobskills/lunatic_voice.lua
+++ scripts/globals/mobskills/lunatic_voice.lua
@@ -8,10 +8,7 @@ require("scripts/globals/status")
 ---------------------------------------------
 
 function onMobSkillCheck(target,mob,skill)
-    if mob:hasStatusEffect(tpz.effect.SOUL_VOICE) then
-        return 0
-    end
-    return 1
+    return 0
 end
 
 function onMobWeaponSkill(target, mob, skill)
