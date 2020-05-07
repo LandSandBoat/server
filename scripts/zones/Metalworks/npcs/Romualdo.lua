e9d5a094d5dbc749921b24a5d34f45c94c46e730
hooksta4 <carl.hooker@gmail.com>
2020-05-07 18:36:19 -0400

    Faded Promises Bastok Quest (#559)
    * Faded Promises Bastok Quest

--- scripts/zones/Metalworks/npcs/Romualdo.lua
+++ scripts/zones/Metalworks/npcs/Romualdo.lua
@@ -10,14 +10,17 @@ function onTrade(player,npc,trade)
 end;
 
 function onTrigger(player,npc)
-    local StampHunt = player:getQuestStatus(BASTOK,tpz.quest.id.bastok.STAMP_HUNT);
+    local StampHunt = player:getQuestStatus(BASTOK,tpz.quest.id.bastok.STAMP_HUNT)
+    local FadedPromises = player:getQuestStatus(BASTOK,tpz.quest.id.bastok.FADED_PROMISES)
 
     if (StampHunt == QUEST_ACCEPTED and player:getMaskBit(player:getCharVar("StampHunt_Mask"),4) == false) then
-        player:startEvent(726);
+        player:startEvent(726)
+    elseif (FadedPromises == QUEST_AVAILABLE and player:getMainJob() == tpz.job.NIN and player:getMainLvl() >= 20 and player:getFameLevel(NORG) >= 4) then
+        player:startEvent(802)
     else
-        player:startEvent(705);
+        player:startEvent(705)
     end
-end;
+end
 
 function onEventUpdate(player,csid,option)
 end;
@@ -25,7 +28,10 @@ end;
 function onEventFinish(player,csid,option)
 
     if (csid == 726) then
-        player:setMaskBit(player:getCharVar("StampHunt_Mask"),"StampHunt_Mask",4,true);
+        player:setMaskBit(player:getCharVar("StampHunt_Mask"),"StampHunt_Mask",4,true)
+    elseif csid == 802 then
+        player:addQuest(BASTOK,tpz.quest.id.bastok.FADED_PROMISES)
+        player:setCharVar("FadedPromises",1)
     end
 
-end;
+end
