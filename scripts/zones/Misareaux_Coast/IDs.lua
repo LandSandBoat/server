b3f8fff10b9b178efb925b9de58f73b2e82d2cd3
Corey Sotiropoulos <cocosolos@pm.me>
2020-05-14 16:05:04 -0400

    Globalize functions
    Style cleanup and logic changes after retail testing.

--- scripts/zones/Misareaux_Coast/IDs.lua
+++ scripts/zones/Misareaux_Coast/IDs.lua
@@ -21,6 +21,7 @@ zones[tpz.zone.MISAREAUX_COAST] =
         DOOR_CLOSED              = 7347, -- The door is locked tight.
         LOGGING_IS_POSSIBLE_HERE = 7601, -- Logging is possible here if you have <item>.
         NOTHING_HERE_YET         = 7660; -- There is nothing here yet. Check again in the morning.
+        ALREADY_BAITED           = 7661; -- The trap already contains <item>.
         APPEARS_TO_BE_TRAP       = 7662; -- There appears to be some kind of trap here. Bits of fish are lying around the area.
         DID_NOT_CATCH_ANYTHING   = 7663; -- You did not catch anything.
         PUT_IN_TRAP              = 7664; -- You put <item> in the trap.
--- scripts/zones/Misareaux_Coast/Zone.lua
+++ scripts/zones/Misareaux_Coast/Zone.lua
@@ -5,11 +5,13 @@
 -----------------------------------
 require("scripts/globals/conquest")
 require("scripts/globals/helm")
-require("scripts/zones/Misareaux_Coast/IDs")
+local ID = require("scripts/zones/Misareaux_Coast/IDs")
+local MISAREAUX_COAST = require("scripts/zones/Misareaux_Coast/globals")
 -----------------------------------
 
 function onInitialize(zone)
     tpz.helm.initZone(zone, tpz.helm.type.LOGGING)
+    MISAREAUX_COAST.ziphiusHandleQM()
 end
 
 function onConquestUpdate(zone, updatetype)
@@ -18,8 +20,8 @@ end
 
 function onZoneIn(player,prevZone)
     local cs = -1
-    if (player:getXPos() == 0 and player:getYPos() == 0 and player:getZPos() == 0) then
-        player:setPos(567.624,-20,280.775,120)
+    if player:getXPos() == 0 and player:getYPos() == 0 and player:getZPos() == 0 then
+        player:setPos(567.624, -20, 280.775, 120)
     end
     return cs
 end
@@ -28,22 +30,9 @@ function onRegionEnter(player,region)
 end
 
 function onGameHour(zone)
-    local ZIPHIUS_QM_BASE  = 16879919
-    if VanadielHour() == 22 then  -- Spawn traps for Ziphius
-        for i = ZIPHIUS_QM_BASE, ZIPHIUS_QM_BASE+5, 1 do
-            GetNPCByID(i):setStatus(tpz.status.NORMAL)
-        end
-    elseif VanadielHour() == 7 then  -- Despawn traps for Ziphius
-        for i = ZIPHIUS_QM_BASE, ZIPHIUS_QM_BASE+5, 1 do
-            GetNPCByID(i):setStatus(tpz.status.DISAPPEAR)
-            GetNPCByID(i):setLocalVar("[Ziphius]Bait Trap", 0)
-        end
-    elseif VanadielHour() == 4 then  -- Despawn non-baited traps
-        for i = ZIPHIUS_QM_BASE, ZIPHIUS_QM_BASE+5, 1 do
-            if (GetNPCByID(i):getLocalVar("[Ziphius]Bait Trap") == 0) then
-                GetNPCByID(i):setStatus(tpz.status.DISAPPEAR)
-            end
-        end
+    local vHour = VanadielHour()
+    if vHour >= 22 or vHour <= 7 then
+        MISAREAUX_COAST.ziphiusHandleQM()
     end
 end
 
