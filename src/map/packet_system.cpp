08c37a0fc52ee9631ef137f2cd153840d7a1381a
wrenffxi <oliviapekoe@gmail.com>
2020-05-17 13:29:02 -0400

    Tier-0 players can only use delivery box in zones that contain either an AH or a MH

--- src/map/packet_system.cpp
+++ src/map/packet_system.cpp
@@ -1588,6 +1588,13 @@ void SmallPacket0x04D(map_session_data_t* session, CCharEntity* PChar, CBasicPac
         return;
     }
 
+    if (PChar->m_GMlevel == 0 && !PChar->loc.zone->CanUseMisc(MISC_AH) && !PChar->loc.zone->CanUseMisc(MISC_MOGMENU))
+    {
+        ShowDebug(CL_CYAN"%s is trying to use the delivery box in a disallowed zone [%s]\n" CL_RESET, PChar->GetName(), PChar->loc.zone->GetName());
+        return;
+    }
+
+
     // 0x01 - Send old items..
     // 0x02 - Add items to be sent..
     // 0x03 - Send confirmation..
