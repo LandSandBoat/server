70b831299d2f09f7a264dd59daf7a69acc8e5a33
59blargedy <59blargedy@gmail.com>
2020-07-02 11:40:56 -0400

    removing spell script

--- scripts/zones/RuAun_Gardens/mobs/Sprinkler.lua
+++ scripts/zones/RuAun_Gardens/mobs/Sprinkler.lua
@@ -8,16 +8,6 @@ require("scripts/globals/regimes")
 function onMobDeath(mob, player, isKiller)
     tpz.regime.checkRegime(player, mob, 142, 2, tpz.regime.type.FIELDS)
     tpz.regime.checkRegime(player, mob, 143, 1, tpz.regime.type.FIELDS)
-end;
+end
 
--- Return the selected spell ID.
-function onMonsterMagicPrepare(mob, target)
-    return 212; -- burst
 
-    -- -- Example of how you can select logical spells
-    -- if (target:getMod(tpz.mod.UTSUSEMI) > 0) then
-    --    return 189; -- stonega
-    -- else
-    --     return 212; -- burst
-    -- end
-end;
