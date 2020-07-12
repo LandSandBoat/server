32883deafb7b247086f8eb9a35467dee3070fe46
Kreidos <kreidos@users.noreply.github.com>
2020-07-11 17:35:52 -0700

    Fix 15-length names on character screen appending world name

--- src/login/lobby.cpp
+++ src/login/lobby.cpp
@@ -191,7 +191,7 @@ int32 lobbydata_parse(int32 fd)
                         ////////////////////////////////////////////////////
                         ref<uint32>(CharList, 4 + 32 + i * 140) = CharID;
 
-                        memcpy(CharList + 12 + 32 + i * 140, strCharName, 15);
+                        memcpy(CharList + 12 + 32 + i * 140, strCharName, 16);
 
                         ref<uint8>(CharList, 46 + 32 + i * 140) = MainJob;
                         ref<uint8>(CharList, 73 + 32 + i * 140) = lvlMainJob;
