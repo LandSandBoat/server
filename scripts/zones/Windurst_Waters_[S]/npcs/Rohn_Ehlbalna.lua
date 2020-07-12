b7d361bb21b0da06becea4685093be1fe4a595b8
hookstar <carl.hooker@gmail.com>
2020-07-12 16:25:14 -0400

    Watch Naughty Mithra Teen get SPANKED for JEWELRY! WOW!
    Orginal Credit goes to @Lusiphur. Converted from DSP to TPZ.

--- scripts/zones/Windurst_Waters_[S]/npcs/Rohn_Ehlbalna.lua
+++ scripts/zones/Windurst_Waters_[S]/npcs/Rohn_Ehlbalna.lua
@@ -9,11 +9,21 @@ function onTrade(player,npc,trade)
 end
 
 function onTrigger(player,npc)
+    local RedeemingRocks = player:getQuestStatus(CRYSTAL_WAR, tpz.quest.id.crystalWar.REDEEMING_ROCKS)
+    local RocksProg = player:getCharVar("RedeemingRocksProg")
+
+    if (RedeemingRocks == QUEST_ACCEPTED and RocksProg == 1) then
+        player:startEvent(114) -- 2nd CS quest "Redeeming Rocks"
+    else
     player:startEvent(440)
+    end
 end
 
 function onEventUpdate(player,csid,option)
 end
 
 function onEventFinish(player,csid,option)
+    if csid == 114 then -- Finish "Redeeming Rocks" second CS
+        player:setCharVar("RedeemingRocksProg", 2)
+    end
 end
