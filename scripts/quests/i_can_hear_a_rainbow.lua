3812b5f97ebdc47e9d058a2d8daf7a5ef496af32
wrenffxi <oliviapekoe@gmail.com>
2020-07-19 19:57:20 -0400

    add binding zone:getWeather() add optional isZoning param to player:getZone() use player:getZone(isZoning):getWeather() in ichar script use set constructor in ichar script swap order of quest check and hasItem

--- scripts/quests/i_can_hear_a_rainbow.lua
+++ scripts/quests/i_can_hear_a_rainbow.lua
@@ -2,6 +2,7 @@
 -- I Can Hear a Rainbow
 -----------------------------------
 require("scripts/globals/weather")
+require("scripts/globals/common")
 require("scripts/globals/quests")
 require("scripts/globals/zone")
 -----------------------------------
@@ -18,95 +19,88 @@ local rubyData =
     [tpz.weather.HOT_SPELL] =
     {
         bit = 0,
-        zones =
-        {
-            [tpz.zone.VALKURM_DUNES] = true,
-            [tpz.zone.ROLANBERRY_FIELDS] = true,
-            [tpz.zone.CAPE_TERIGGAN] = true,
-            [tpz.zone.EASTERN_ALTEPA_DESERT] = true,
-            [tpz.zone.MERIPHATAUD_MOUNTAINS] = true,
-            [tpz.zone.YUHTUNGA_JUNGLE] = true,
-            [tpz.zone.YHOATOR_JUNGLE] = true,
-            [tpz.zone.WESTERN_ALTEPA_DESERT] = true,
+        zones = set{
+            tpz.zone.VALKURM_DUNES,
+            tpz.zone.ROLANBERRY_FIELDS,
+            tpz.zone.CAPE_TERIGGAN,
+            tpz.zone.EASTERN_ALTEPA_DESERT,
+            tpz.zone.MERIPHATAUD_MOUNTAINS,
+            tpz.zone.YUHTUNGA_JUNGLE,
+            tpz.zone.YHOATOR_JUNGLE,
+            tpz.zone.WESTERN_ALTEPA_DESERT,
         },
     },
     [tpz.weather.NONE] =
     {
         bit = 1,
-        zones =
-        {
-            [tpz.zone.WEST_RONFAURE] = true,
-            [tpz.zone.EAST_RONFAURE] = true,
-            [tpz.zone.NORTH_GUSTABERG] = true,
-            [tpz.zone.SOUTH_GUSTABERG] = true,
-            [tpz.zone.EASTERN_ALTEPA_DESERT] = true,
-            [tpz.zone.WEST_SARUTABARUTA] = true,
-            [tpz.zone.EAST_SARUTABARUTA] = true,
-            [tpz.zone.BUBURIMU_PENINSULA] = true,
-            [tpz.zone.YHOATOR_JUNGLE] = true,
+        zones = set{
+            tpz.zone.WEST_RONFAURE,
+            tpz.zone.EAST_RONFAURE,
+            tpz.zone.NORTH_GUSTABERG,
+            tpz.zone.SOUTH_GUSTABERG,
+            tpz.zone.EASTERN_ALTEPA_DESERT,
+            tpz.zone.WEST_SARUTABARUTA,
+            tpz.zone.EAST_SARUTABARUTA,
+            tpz.zone.BUBURIMU_PENINSULA,
+            tpz.zone.YHOATOR_JUNGLE,
         },
     },
     [tpz.weather.DUST_STORM] =
     {
         bit = 2,
-        zones =
-        {
-            [tpz.zone.VALKURM_DUNES] = true,
-            [tpz.zone.BATALLIA_DOWNS] = true,
-            [tpz.zone.KONSCHTAT_HIGHLANDS] = true,
-            [tpz.zone.EASTERN_ALTEPA_DESERT] = true,
-            [tpz.zone.TAHRONGI_CANYON] = true,
-            [tpz.zone.MERIPHATAUD_MOUNTAINS] = true,
-            [tpz.zone.SAUROMUGUE_CHAMPAIGN] = true,
-            [tpz.zone.WESTERN_ALTEPA_DESERT] = true,
+        zones = set{
+            tpz.zone.VALKURM_DUNES,
+            tpz.zone.BATALLIA_DOWNS,
+            tpz.zone.KONSCHTAT_HIGHLANDS,
+            tpz.zone.EASTERN_ALTEPA_DESERT,
+            tpz.zone.TAHRONGI_CANYON,
+            tpz.zone.MERIPHATAUD_MOUNTAINS,
+            tpz.zone.SAUROMUGUE_CHAMPAIGN,
+            tpz.zone.WESTERN_ALTEPA_DESERT,
         },
     },
     [tpz.weather.WIND] =
     {
         bit = 3,
-        zones =
-        {
-            [tpz.zone.LA_THEINE_PLATEAU] = true,
-            [tpz.zone.CAPE_TERIGGAN] = true,
-            [tpz.zone.TAHRONGI_CANYON] = true,
-            [tpz.zone.BUBURIMU_PENINSULA] = true,
+        zones = set{
+            tpz.zone.LA_THEINE_PLATEAU,
+            tpz.zone.CAPE_TERIGGAN,
+            tpz.zone.TAHRONGI_CANYON,
+            tpz.zone.BUBURIMU_PENINSULA,
         },
     },
     [tpz.weather.RAIN] =
     {
         bit = 4,
-        zones =
-        {
-            [tpz.zone.LA_THEINE_PLATEAU] = true,
-            [tpz.zone.JUGNER_FOREST] = true,
-            [tpz.zone.PASHHOW_MARSHLANDS] = true,
-            [tpz.zone.ROLANBERRY_FIELDS] = true,
-            [tpz.zone.BUBURIMU_PENINSULA] = true,
-            [tpz.zone.THE_SANCTUARY_OF_ZITAH] = true,
-            [tpz.zone.YUHTUNGA_JUNGLE] = true,
-            [tpz.zone.YHOATOR_JUNGLE] = true,
+        zones = set{
+            tpz.zone.LA_THEINE_PLATEAU,
+            tpz.zone.JUGNER_FOREST,
+            tpz.zone.PASHHOW_MARSHLANDS,
+            tpz.zone.ROLANBERRY_FIELDS,
+            tpz.zone.BUBURIMU_PENINSULA,
+            tpz.zone.THE_SANCTUARY_OF_ZITAH,
+            tpz.zone.YUHTUNGA_JUNGLE,
+            tpz.zone.YHOATOR_JUNGLE,
         },
     },
     [tpz.weather.SNOW] =
     {
         bit = 5,
-        zones =
-        {
-            [tpz.zone.BATALLIA_DOWNS] = true,
-            [tpz.zone.BEAUCEDINE_GLACIER] = true,
-            [tpz.zone.XARCABARD] = true,
+        zones = set{
+            tpz.zone.BATALLIA_DOWNS,
+            tpz.zone.BEAUCEDINE_GLACIER,
+            tpz.zone.XARCABARD,
         },
     },
     [tpz.weather.THUNDER] =
     {
         bit = 6,
-        zones =
-        {
-            [tpz.zone.JUGNER_FOREST] = true,
-            [tpz.zone.KONSCHTAT_HIGHLANDS] = true,
-            [tpz.zone.PASHHOW_MARSHLANDS] = true,
-            [tpz.zone.SAUROMUGUE_CHAMPAIGN] = true,
-            [tpz.zone.THE_SANCTUARY_OF_ZITAH] = true,
+        zones = set{
+            tpz.zone.JUGNER_FOREST,
+            tpz.zone.KONSCHTAT_HIGHLANDS,
+            tpz.zone.PASHHOW_MARSHLANDS,
+            tpz.zone.SAUROMUGUE_CHAMPAIGN,
+            tpz.zone.THE_SANCTUARY_OF_ZITAH,
         },
     },
 }
@@ -116,15 +110,20 @@ local rubyData =
 -------------------------------------------------
 
 quests.i_can_hear_a_rainbow.onZoneIn = function(player)
-    if player:hasItem(1125, 0) and player:getQuestStatus(WINDURST, tpz.quest.id.windurst.I_CAN_HEAR_A_RAINBOW) == QUEST_ACCEPTED then
+    if player:getQuestStatus(WINDURST, tpz.quest.id.windurst.I_CAN_HEAR_A_RAINBOW) == QUEST_ACCEPTED and player:hasItem(1125, 0) then
         local trigger = false
 
         -- get data for basic weather (e.g. downgrade GALES to WIND)
-        local weather = player:getWeather()
+        local zone = player:getZone(true)
+        if not zone then return false end
+
+        local weather = zone:getWeather()
+        if not weather then return false end
+
         local baseWeather = (weather % 2 == 0) and weather or weather - 1
         local data = rubyData[baseWeather]
 
-        if data and data.zones[player:getZoneID()] then
+        if data and data.zones[zone:getID()] then
             local mask = player:getCharVar('I_CAN_HEAR_A_RAINBOW')
             local needsColor = bit.band(mask, 2^data.bit) == 0
 
