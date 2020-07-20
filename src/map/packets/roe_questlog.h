773da4212221f0fa3cf588ab66a8315b44cf3570
Kreidos <kreidos@users.noreply.github.com>
2020-07-20 14:13:05 -0700

    Correct copyright notices on new source files
    Per GPL Recommended practice and with advice from
    Topaz staff.

--- src/map/packets/roe_questlog.h
+++ src/map/packets/roe_questlog.h
@@ -1,7 +1,7 @@
 ﻿/*
 ===========================================================================
 
-  Copyright (c) 2010-2015 Darkstar Dev Teams
+  Copyright (c) 2020 - Kreidos | github.com/kreidos
 
   This program is free software: you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
