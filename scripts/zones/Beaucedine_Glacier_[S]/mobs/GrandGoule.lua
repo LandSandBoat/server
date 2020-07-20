5fc387b866d97b1a53529d4594c854696280861c
wrenffxi <oliviapekoe@gmail.com>
2020-07-19 23:35:36 -0400

    merge duplicate mob scripts (hunts) remove apostrophes from mob, npc, and instance names npc_list cleanup, rename some scripts to match

--- scripts/zones/Beaucedine_Glacier_[S]/mobs/GrandGoule.lua
+++ scripts/zones/Beaucedine_Glacier_[S]/mobs/GrandGoule.lua
@@ -2,6 +2,7 @@
 -- Area: Beaucedine Glacier [S]
 --   NM: Grand'Goule
 -----------------------------------
+require("scripts/globals/hunts")
 require("scripts/globals/mobs")
 -----------------------------------
 
@@ -14,4 +15,5 @@ function onAdditionalEffect(mob, target, damage)
 end
 
 function onMobDeath(mob, player, isKiller)
+    tpz.hunts.checkHunt(mob, player, 538)
 end
