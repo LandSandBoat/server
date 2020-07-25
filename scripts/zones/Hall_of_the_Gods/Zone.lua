d6df063add368829c8e461dcdffc9685659f0275
wrenffxi <oliviapekoe@gmail.com>
2020-07-25 02:12:18 -0400

    LUA cleanup: Hall of Transference, Hall of the Gods

--- scripts/zones/Hall_of_the_Gods/Zone.lua
+++ scripts/zones/Hall_of_the_Gods/Zone.lua
@@ -12,13 +12,15 @@ require("scripts/globals/zone")
 function onInitialize(zone)
 end
 
-function onZoneIn(player,prevZone)
+function onZoneIn(player, prevZone)
     local cs = -1
-    if (player:getXPos() == 0 and player:getYPos() == 0 and player:getZPos() == 0) then
-        player:setPos(-0.011,-1.848,-176.133,192)
-    elseif (player:getCurrentMission(ACP) == tpz.mission.id.acp.REMEMBER_ME_IN_YOUR_DREAMS and prevZone == tpz.zone.ROMAEVE) then
+
+    if player:getXPos() == 0 and player:getYPos() == 0 and player:getZPos() == 0 then
+        player:setPos(-0.011, -1.848, -176.133, 192)
+    elseif player:getCurrentMission(ACP) == tpz.mission.id.acp.REMEMBER_ME_IN_YOUR_DREAMS and prevZone == tpz.zone.ROMAEVE then
         cs = 5
     end
+
     return cs
 end
 
@@ -26,15 +28,15 @@ function onConquestUpdate(zone, updatetype)
     tpz.conq.onConquestUpdate(zone, updatetype)
 end
 
-function onRegionEnter(player,region)
+function onRegionEnter(player, region)
 end
 
-function onEventUpdate(player,csid,option)
+function onEventUpdate(player, csid, option)
 end
 
-function onEventFinish(player,csid,option)
-    if (csid == 5) then
-        player:completeMission(ACP,tpz.mission.id.acp.REMEMBER_ME_IN_YOUR_DREAMS)
-        player:addMission(ACP,tpz.mission.id.acp.BORN_OF_HER_NIGHTMARES)
+function onEventFinish(player, csid, option)
+    if csid == 5 then
+        player:completeMission(ACP, tpz.mission.id.acp.REMEMBER_ME_IN_YOUR_DREAMS)
+        player:addMission(ACP, tpz.mission.id.acp.BORN_OF_HER_NIGHTMARES)
     end
-end;
+end
--- scripts/zones/Hall_of_the_Gods/npcs/Shimmering_Circle.lua
+++ scripts/zones/Hall_of_the_Gods/npcs/Shimmering_Circle.lua
@@ -4,23 +4,30 @@
 -- Lifts players up to the sky!
 -- !pos 0 -20 147 251
 -----------------------------------
-require("scripts/globals/keyitems")
-require("scripts/globals/missions")
 local ID = require("scripts/zones/Hall_of_the_Gods/IDs")
+require("scripts/globals/missions")
 -----------------------------------
 
-function onTrade(player,npc,trade)
+function onTrade(player, npc, trade)
 end
 
-function onTrigger(player,npc)
-    local ZilartProgress = player:getCurrentMission(ZILART)
-    local ZVar = player:getCharVar("ZilartStatus")
+function onTrigger(player, npc)
+    local roz = player:getCurrentMission(ZILART)
+    local rozStat = player:getCharVar("ZilartStatus")
 
-    if (player:getZPos() < 200) then
-        if (ZilartProgress == tpz.mission.id.zilart.THE_GATE_OF_THE_GODS and ZVar == 0) then
+    if player:getZPos() < 200 then
+        if roz == tpz.mission.id.zilart.THE_GATE_OF_THE_GODS and rozStat == 0 then
             player:startEvent(3) -- First time.
-        elseif (ZilartProgress ~= tpz.mission.id.zilart.NONE and ZilartProgress > tpz.mission.id.zilart.THE_GATE_OF_THE_GODS or -- If player has not done any ZM, Progress == 65535
-         (ZilartProgress == tpz.mission.id.zilart.THE_GATE_OF_THE_GODS and ZVar > 0)) then
+        elseif
+            roz ~= tpz.mission.id.zilart.NONE and
+            (
+                roz > tpz.mission.id.zilart.THE_GATE_OF_THE_GODS or
+                (
+                    roz == tpz.mission.id.zilart.THE_GATE_OF_THE_GODS and
+                    rozStat > 0
+                )
+             )
+        then
             player:startEvent(10)
         else
             player:messageSpecial(ID.text.NOTHING_OUT_OF_ORDINARY)
@@ -28,16 +35,13 @@ function onTrigger(player,npc)
     else
         player:startEvent(11)
     end
-
 end
 
-function onEventUpdate(player,csid,option)
+function onEventUpdate(player, csid, option)
 end
 
-function onEventFinish(player,csid,option)
-
-    if (csid == 3) then
-        player:setCharVar("ZilartStatus",1)
+function onEventFinish(player, csid, option)
+    if csid == 3 then
+        player:setCharVar("ZilartStatus", 1)
     end
-
-end;
+end
