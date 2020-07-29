9d4fe91e3af79950878ecf792b77e14a819a98bf
Corey Sotiropoulos <cocosolos@pm.me>
2020-07-29 03:08:16 -0400

    Fix issues with Lion II cipher and some cutscenes

--- scripts/zones/Windurst_Walls/Zone.lua
+++ scripts/zones/Windurst_Walls/Zone.lua
@@ -21,7 +21,7 @@ function onZoneIn(player, prevZone)
 
     if ENABLE_ROV == 1 and player:getCurrentMission(ROV) == tpz.mission.id.rov.RHAPSODIES_OF_VANADIEL and player:getMainLvl()>=3 then
         cs = 30035
-    elseif player:getCurrentMission(ROV) == tpz.mission.id.rov.FATES_CALL and player:getFameLevel(player:getNation()) >= 5 then
+    elseif player:getCurrentMission(ROV) == tpz.mission.id.rov.FATES_CALL and player:getRank(player:getNation()) >= 5 then
         cs = 30036
     elseif
         ENABLE_ASA == 1 and player:getCurrentMission(ASA) == tpz.mission.id.asa.A_SHANTOTTO_ASCENSION
