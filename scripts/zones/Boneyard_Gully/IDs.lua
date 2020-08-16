ccb27f4ed5a64802c1189cb5336f09b2b405cf52
Daniel R. Koris <korisd@gmail.com>
2020-08-16 09:05:08 -0600

    PR: lua: Added supporting IDs for Shell We Dance?

--- scripts/zones/Boneyard_Gully/IDs.lua
+++ scripts/zones/Boneyard_Gully/IDs.lua
@@ -17,10 +17,31 @@ zones[tpz.zone.BONEYARD_GULLY] =
         CONQUEST_BASE           = 7421, -- Tallying conquest results...
     },
     mob =
-    {
+	{
     },
     npc =
     {
+    },
+    shellWeDance = {
+        [1] = {
+            PARATA_ID        = 16810024,
+            BLADMALL_ID      = 16810025,
+            PARATA_PET_IDS   = { 16810026, 16810027, 16810028 },
+            BLADMALL_PET_IDS = { 16810029, 16810030, 16810031 },
+        },
+        [2] = {
+            PARATA_ID        = 16810033,
+            BLADMALL_ID      = 16810034,
+            PARATA_PET_IDS   = { 16810035, 16810036, 16810037 },
+            BLADMALL_PET_IDS = { 16810038, 16810039, 16810040 },
+        },
+        [3] = {
+            PARATA_ID        = 16810042,
+            BLADMALL_ID      = 16810043,
+            PARATA_PET_IDS   = { 16810044, 16810045, 16810046 },
+            BLADMALL_PET_IDS = { 16810047, 16810048, 16810049 },
+
+        },
     },
 }
 
