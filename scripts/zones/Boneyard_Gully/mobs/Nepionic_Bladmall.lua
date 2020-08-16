704fbe0590ffe81cb1e9f725be7288bf6853e69e
Daniel R. Koris <korisd@gmail.com>
2020-08-16 09:08:56 -0600

    PR: lua: Added mob scripts for Shell We Dance?

---
+++ scripts/zones/Boneyard_Gully/mobs/Nepionic_Bladmall.lua
@@ -0,0 +1,14 @@
+-----------------------------------
+-- Area: Boneyard Gully
+--  Mob: Nepionic Bladmall
+--  ENM: Shell We Dance?
+-----------------------------------
+require("scripts/globals/status")
+require("scripts/globals/titles")
+-----------------------------------
+
+function onMobSpawn(mob)
+end
+
+function onMobDeath(mob, player, isKiller)
+end
