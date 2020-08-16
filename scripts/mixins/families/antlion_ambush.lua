c8927245680306aa894858e35caccc700aea3234
Daniel R. Koris <korisd@gmail.com>
2020-08-16 08:48:43 -0600

    PR: lua: Updated antlion mixin to make sure no_move isn't on after use

--- scripts/mixins/families/antlion_ambush.lua
+++ scripts/mixins/families/antlion_ambush.lua
@@ -14,6 +14,7 @@ g_mixins.families.antlion_ambush = function(mob)
     end)
     mob:addListener("ENGAGE", "ANTLION_AMBUSH_ENGAGE", function(mob, target)
         mob:useMobAbility(278) -- Pit Ambush
+        mob:setMobMod(tpz.mobMod.NO_MOVE, 0)
     end)
     mob:addListener("DISENGAGE", "ANTLION_AMBUSH_DISENGAGE", function(mob)
         mob:hideName(true)
