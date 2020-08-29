776f897b9f158bc978d3269a656991b3f073be93
CloudyDesarroi <30502556+CloudyDesarroi@users.noreply.github.com>
2020-08-29 10:53:00 -0700

    Update vulture_1.lua

--- scripts/globals/mobskills/vulture_1.lua
+++ scripts/globals/mobskills/vulture_1.lua
@@ -4,7 +4,6 @@
 ---------------------------------------------
 require("scripts/globals/monstertpmoves")
 require("scripts/globals/settings")
-require("scripts/globals/status")
 require("scripts/globals/msg")
 ---------------------------------------------
 
