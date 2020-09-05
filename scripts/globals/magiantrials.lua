96b4b1eff5d7a89726a39e841362e957e4f7320b
Zach Toogood <zrtoogood@gmail.com>
2020-09-05 14:06:39 +0300

    Finish merging

--- scripts/globals/magiantrials.lua
+++ scripts/globals/magiantrials.lua
@@ -367,20 +367,12 @@ end
 -- Magian Green  --
 -------------------
 
-<<<<<<< HEAD
-function magianGreenEventUpdate(player,itemId,csid,option)
-=======
 function magianGreenEventUpdate(player, ItemID, csid, option)
->>>>>>> release
 end
 
 -------------------
 -- Magian Blue   --
 -------------------
 
-<<<<<<< HEAD
-function magianBlueEventUpdate(player,itemId,csid,option)
-=======
 function magianBlueEventUpdate(player, ItemID, csid, option)
->>>>>>> release
 end
