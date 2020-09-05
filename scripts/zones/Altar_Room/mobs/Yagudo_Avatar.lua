4171920f129cca49caeb83c3daf9b5a880522cea
Zach Toogood <zrtoogood@gmail.com>
2020-09-05 09:04:51 +0300

    Styling pass: Quest: A Moral Manifest

--- scripts/zones/Altar_Room/mobs/Yagudo_Avatar.lua
+++ scripts/zones/Altar_Room/mobs/Yagudo_Avatar.lua
@@ -4,12 +4,14 @@
 local ID = require("scripts/zones/Altar_Room/IDs")
 mixins = {require("scripts/mixins/job_special")}
 require("scripts/globals/mobs")
+-----------------------------------
 
 function onMobSpawn(mob)
 end
 
 function onMobDeath(mob, player, isKiller)
-    if  player:getQuestStatus(OTHER_AREAS_LOG,tpz.quest.id.otherAreas.A_MORAL_MANIFEST) == QUEST_ACCEPTED and player:getCharVar("moral") == 5 then
+    if player:getQuestStatus(OTHER_AREAS_LOG, tpz.quest.id.otherAreas.A_MORAL_MANIFEST) == QUEST_ACCEPTED and
+        player:getCharVar("moral") == 5 then
         player:setCharVar("moral", 6)
         player:delKeyItem(tpz.ki.VAULT_QUIPUS);
     end
--- scripts/zones/Altar_Room/npcs/Hooknox.lua
+++ scripts/zones/Altar_Room/npcs/Hooknox.lua
@@ -4,16 +4,15 @@
 -- Type: Standard NPC
 -- !pos -265.248 11.693 -102.547 152
 -----------------------------------
+function onTrade(player, npc, trade)
+end
 
-function onTrade(player,npc,trade)
-end;
-
-function onTrigger(player,npc)
+function onTrigger(player, npc)
     player:startEvent(46);
-end;
+end
 
-function onEventUpdate(player,csid,option)
-end;
+function onEventUpdate(player, csid, option)
+end
 
-function onEventFinish(player,csid,option)
-end;
+function onEventFinish(player, csid, option)
+end
