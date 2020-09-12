6fb2e06e73160281411eac532787cee8381a7734
Illzbee <65986311+illzbee@users.noreply.github.com>
2020-09-12 10:16:34 -0400

    removed extra ( )

--- scripts/globals/abilities/presto.lua
+++ scripts/globals/abilities/presto.lua
@@ -16,18 +16,18 @@ end
 function onUseAbility(player,target,ability)
     target:addStatusEffect(tpz.effect.PRESTO,19,1,30)
 
-    if (player:addStatusEffect(tpz.effect.FINISHING_MOVE_1)) then
+    if player:addStatusEffect(tpz.effect.FINISHING_MOVE_1) then
         player:addStatusEffect(tpz.effect.FINISHING_MOVE_1)
-	elseif (player:hasStatusEffect(tpz.effect.FINISHING_MOVE_1)) then
+	elseif player:hasStatusEffect(tpz.effect.FINISHING_MOVE_1) then
         player:delStatusEffect(tpz.effect.FINISHING_MOVE_1)
         player:addStatusEffect(tpz.effect.FINISHING_MOVE_3)
-	elseif (player:hasStatusEffect(tpz.effect.FINISHING_MOVE_2)) then
+	elseif player:hasStatusEffect(tpz.effect.FINISHING_MOVE_2) then
         player:delStatusEffect(tpz.effect.FINISHING_MOVE_2)
         player:addStatusEffect(tpz.effect.FINISHING_MOVE_3)
-    elseif (player:hasStatusEffect(tpz.effect.FINISHING_MOVE_3)) then
+    elseif player:hasStatusEffect(tpz.effect.FINISHING_MOVE_3) then
         player:delStatusEffect(tpz.effect.FINISHING_MOVE_3)
         player:addStatusEffect(tpz.effect.FINISHING_MOVE_4)
-    elseif (player:hasStatusEffect(tpz.effect.FINISHING_MOVE_4)) then
+    elseif player:hasStatusEffect(tpz.effect.FINISHING_MOVE_4) then
         player:delStatusEffect(tpz.effect.FINISHING_MOVE_4)
         player:addStatusEffect(tpz.effect.FINISHING_MOVE_5)
     end
