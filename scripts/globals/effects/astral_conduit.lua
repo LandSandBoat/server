58808c9ab96bcfb8f2af423f5eee72d6ec860d70
Illzbee <65986311+illzbee@users.noreply.github.com>
2020-09-12 10:21:07 -0400

    reformatted indentation

--- scripts/globals/effects/astral_conduit.lua
+++ scripts/globals/effects/astral_conduit.lua
@@ -5,14 +5,14 @@
 -----------------------------------
 
 function onEffectGain(target, effect)
-target:addMod(tpz.mod.BP_DELAY, 99)
-target:addMod(tpz.mod.MPP, 100)
+    target:addMod(tpz.mod.BP_DELAY, 99)
+    target:addMod(tpz.mod.MPP, 100)
 end
 
 function onEffectTick(target, effect)
 end
 
 function onEffectLose(target, effect)
-target:delMod(tpz.mod.BP_DELAY, 99)
-target:delMod(tpz.mod.MPP, 100)
+    target:delMod(tpz.mod.BP_DELAY, 99)
+    target:delMod(tpz.mod.MPP, 100)
 end
