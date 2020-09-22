c05c3d2d761b4a07508a909d8380ca118c9e7b44
Marian Arlt <marianarlt@icloud.com>
2020-09-21 20:49:10 -0500

    Update timeout to current retail
    Use minute as reference instead of day like currently described by bg-wiki and ffxiclopedia.

--- scripts/zones/Crawlers_Nest/npcs/qm8.lua
+++ scripts/zones/Crawlers_Nest/npcs/qm8.lua
@@ -1,7 +1,7 @@
 -----------------------------------
 -- Area: Crawlers Nest
 --  NPC: ???
--- Involved in Quest: The Crimson Trial
+-- Involved in Quest: Enveloped in Darkness
 -- !pos 59 0.1 66 197
 -----------------------------------
 require("scripts/globals/settings")
@@ -15,15 +15,15 @@ end
 
 function onTrigger(player, npc)
 
-    local cprog = player:getCharVar("theCrimsonTrial_prog")
-    local cdate = player:getCharVar("theCrimsonTrial_date")
-    local realday = tonumber(os.date("%j")) -- %M for next minute, %j for next day
+    local cprog = player:getCharVar("envelopedInDarkness_prog")
+    local cdate = player:getCharVar("envelopedInDarkness_date")
+    local realdate = tonumber(os.date("%M"))
 
     if (player:hasKeyItem(tpz.ki.CRAWLER_BLOOD) == true and player:hasKeyItem(tpz.ki.OLD_BOOTS) == true) then
         player:startEvent(4)
-    elseif (cprog == 1 and cdate == realday) then
-        player:messageSpecial(ID.text.EQUIPMENT_COMPLETELY_PURIFIED)
-    elseif (cprog == 1 and cdate ~= realday) then
+    elseif (cprog == 1 and cdate == realdate) then
+        player:messageSpecial(ID.text.EQUIPMENT_COMPLETELY_PURIFIED) -- Stagger retrieval
+    elseif (cprog == 1 and cdate ~= realdate) then
         player:startEvent(5)
     else
         player:messageSpecial(ID.text.SOMEONE_HAS_BEEN_DIGGING_HERE)
@@ -39,17 +39,17 @@ function onEventFinish(player, csid, option)
     if (csid == 4 and option == 1) then
         player:delKeyItem(tpz.ki.CRAWLER_BLOOD)
         player:delKeyItem(tpz.ki.OLD_BOOTS)
-        player:setCharVar("theCrimsonTrial_date", os.date("%j")) -- %M for next minute, %j for next day
-        player:setCharVar("theCrimsonTrial_prog", 1)
+        player:setCharVar("envelopedInDarkness_date", os.date("%M")) -- Store minute at time of event
+        player:setCharVar("envelopedInDarkness_prog", 1) -- Flag the turn in
         player:messageSpecial(ID.text.YOU_BURY_THE, tpz.ki.OLD_BOOTS, tpz.ki.CRAWLER_BLOOD)
     elseif (csid == 5) then
         if (player:getFreeSlotsCount() == 0) then
-            player:messageSpecial(ID.text.ITEM_CANNOT_BE_OBTAINED, 14093) -- Warlock's Boots
+            player:messageSpecial(ID.text.ITEM_CANNOT_BE_OBTAINED, 14093) -- Inventory full
         else
             player:addItem(14093)
             player:messageSpecial(ID.text.ITEM_OBTAINED, 14093) -- Warlock's Boots
-            player:setCharVar("theCrimsonTrial_date", 0)
-            player:setCharVar("theCrimsonTrial_prog", 0)
+            player:setCharVar("envelopedInDarkness_date", 0)
+            player:setCharVar("envelopedInDarkness_prog", 0)
             player:setCharVar("needs_crawler_blood", 2) -- Fixed being unable start next quest
             player:addFame(SANDORIA, 40)
             player:completeQuest(SANDORIA, tpz.quest.id.sandoria.ENVELOPED_IN_DARKNESS)
