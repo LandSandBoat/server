e645458fdc0d7aadb8408cdd01380c124d89ed9e
NeuromancerXI <neuromancerxi@users.noreply.github.com>
2020-10-02 09:48:19 -0700

    Maths II and Account for Subtle Blow/II Minus.

--- src/map/utils/battleutils.cpp
+++ src/map/utils/battleutils.cpp
@@ -2000,9 +2000,9 @@ namespace battleutils
             if (giveTPtoVictim)
             {
                 //account for attacker's subtle blow which reduces the baseTP gain for the defender
-                float sBlow1 = (100.0f - std::clamp((float)PAttacker->getMod(Mod::SUBTLE_BLOW), 0.0f, 50.0f));
-                float sBlow2 = (100.0f - std::clamp((float)PAttacker->getMod(Mod::SUBTLE_BLOW_II), 0.0f, 50.0f));
-                float sBlowMult = ((100.0f - std::clamp((float)(sBlow1 + sBlow2), 0.0f, 75.0f)) / 100.0f);
+                float sBlow1 = std::clamp((float)PAttacker->getMod(Mod::SUBTLE_BLOW), -50.0f, 50.0f);
+                float sBlow2 = std::clamp((float)PAttacker->getMod(Mod::SUBTLE_BLOW_II), -50.0f, 50.0f);
+                float sBlowMult = ((100.0f - std::clamp((float)(sBlow1 + sBlow2), -75.0f, 75.0f)) / 100.0f);
 
                 //mobs hit get basetp+30 whereas pcs hit get basetp/3
                 if (PDefender->objtype == TYPE_PC || (PDefender->objtype == TYPE_PET && PDefender->PMaster && PDefender->PMaster->objtype == TYPE_PC))
@@ -2128,9 +2128,9 @@ namespace battleutils
             }
 
             //account for attacker's subtle blow which reduces the baseTP gain for the defender
-            float sBlow1 = (100.0f - std::clamp((float)PAttacker->getMod(Mod::SUBTLE_BLOW), 0.0f, 50.0f));
-            float sBlow2 = (100.0f - std::clamp((float)PAttacker->getMod(Mod::SUBTLE_BLOW_II), 0.0f, 50.0f));
-            float sBlowMult = ((100.0f - std::clamp((float)(sBlow1 + sBlow2), 0.0f, 75.0f)) / 100.0f);
+            float sBlow1 = std::clamp((float)PAttacker->getMod(Mod::SUBTLE_BLOW), -50.0f, 50.0f);
+            float sBlow2 = std::clamp((float)PAttacker->getMod(Mod::SUBTLE_BLOW_II), -50.0f, 50.0f);
+            float sBlowMult = ((100.0f - std::clamp((float)(sBlow1 + sBlow2), -75.0f, 75.0f)) / 100.0f);
 
             //mobs hit get basetp+30 whereas pcs hit get basetp/3
             if (PDefender->objtype == TYPE_PC)
