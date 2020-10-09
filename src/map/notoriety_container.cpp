5fe14ba2f9a20891e250ae074384e61b04fc6852
Zach Toogood <zrtoogood@gmail.com>
2020-10-09 20:20:10 +0300

    Add NotorietyList to CBattleEntity

---
+++ src/map/notoriety_container.cpp
@@ -0,0 +1,74 @@
+﻿/*
+ * notoriety_container.cpp
+ *      Author: zach2good | github.com/zach2good
+ *
+===========================================================================
+  Copyright (c) 2020 Topaz Dev Teams
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see http://www.gnu.org/licenses/
+===========================================================================
+*/
+#include "notoriety_container.h"
+
+#include "enmity_container.h"
+#include "entities/battleentity.h"
+#include "entities/mobentity.h"
+
+CNotorietyContainer::CNotorietyContainer(CBattleEntity* owner)
+    : m_POwner(owner)
+{
+}
+
+std::set<CBattleEntity*>::iterator CNotorietyContainer::begin()
+{
+    return m_Lookup.begin();
+}
+
+std::set<CBattleEntity*>::iterator CNotorietyContainer::end()
+{
+    return m_Lookup.end();
+}
+
+void CNotorietyContainer::add(CBattleEntity* entity)
+{
+    m_Lookup.insert(entity);
+}
+
+void CNotorietyContainer::remove(CBattleEntity* entity)
+{
+    BattleEntitySet::iterator entity_itr = m_Lookup.find(entity);
+    if (entity_itr != m_Lookup.end())
+    {
+        m_Lookup.erase(*entity_itr);
+    }
+}
+
+bool CNotorietyContainer::hasEnmity()
+{
+    return !m_Lookup.empty();
+}
+
+void CNotorietyContainer::clear()
+{
+    for (CBattleEntity* entity : m_Lookup)
+    {
+        if (CMobEntity* mob = dynamic_cast<CMobEntity*>(entity))
+        {
+            mob->PEnmityContainer->Clear(m_POwner->id);
+        }
+    }
+    m_Lookup.clear();
+}
+
+std::size_t CNotorietyContainer::size()
+{
+    return m_Lookup.size();
+}
---
+++ src/map/notoriety_container.h
@@ -0,0 +1,52 @@
+﻿/*
+ * notoriety_container.h
+ *      Author: zach2good | github.com/zach2good
+ *
+===========================================================================
+  Copyright (c) 2020 Topaz Dev Teams
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see http://www.gnu.org/licenses/
+===========================================================================
+*/
+
+#ifndef _NOTORIETYCONTAINER_H
+#define _NOTORIETYCONTAINER_H
+
+#include <set>
+
+class CBattleEntity;
+class CMobEntity;
+
+// If enmity is a one-to-many list held by monsters,
+// notoriety is the many-to-one opposite list held by players (or other targets of enmity)
+class CNotorietyContainer
+{
+public:
+    CNotorietyContainer(CBattleEntity* owner);
+    ~CNotorietyContainer() = default;
+
+    std::set<CBattleEntity*>::iterator begin();
+    std::set<CBattleEntity*>::iterator end();
+
+    void add(CBattleEntity* entity);
+    void remove(CBattleEntity* entity);
+    void clear();
+    std::size_t size();
+
+    bool hasEnmity();
+
+private:
+    using BattleEntitySet = std::set<CBattleEntity*>;
+    CBattleEntity* m_POwner;
+    BattleEntitySet m_Lookup;
+};
+
+#endif // _NOTORIETYCONTAINER_H
