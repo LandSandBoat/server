04be4fb89a40817f9089f10ef4a7ee6450dd181f
wrenffxi <oliviapekoe@gmail.com>
2020-10-09 23:19:23 -0400

    * remove en-skills from Kam'lanaut's skill list, because he doesn't use these as normal TP moves (both fights) * instead, he en-skills on a timer (~20sec RtDT, ~30sec Apoc Nigh), which does not consume TP * when the Nigh version uses TP, he can chain up to three attacks from his two actual TP moves

--- scripts/zones/Empyreal_Paradox/mobs/Kamlanaut.lua
+++ scripts/zones/Empyreal_Paradox/mobs/Kamlanaut.lua
@@ -16,6 +16,19 @@ local skillToAbsorb =
     [828] = tpz.mod.WATER_ABSORB, -- water_blade
 }
 
+function onMobEngaged(mob, target)
+    mob:setLocalVar("nextEnSkill", os.time() + 10)
+end
+
+function onMobFight(mob, target)
+    if os.time() > mob:getLocalVar("nextEnSkill") then
+        local skill = math.random(823, 828)
+        mob:setLocalVar("currentTP", mob:getTP())
+        mob:useMobAbility(skill)
+        mob:setLocalVar("nextEnSkill", os.time() + 30)
+    end
+end
+
 function onMobWeaponSkill(target, mob, skill)
     local skillId = skill:getID()
     local absorbId = skillToAbsorb[skillId]
@@ -36,6 +49,9 @@ function onMobWeaponSkill(target, mob, skill)
         mob:setLocalVar("currentAbsorb", absorbId)
         mob:setMod(absorbId, 100)
 
+        -- return TP
+        mob:setTP(mob:getLocalVar("currentTP"))
+
     else
         -- ----------------------------------------------------------------------
         -- when using Light Blade or Great Wheel, can do up to three WS in a row
@@ -51,7 +67,7 @@ function onMobWeaponSkill(target, mob, skill)
 
         if wsCount < wsMax then
             mob:setLocalVar("wsCount", wsCount + 1)
-            mob:useMobAbility(skillId)
+            mob:setTP(1000)
         else
             mob:setLocalVar("wsCount", 0)
         end
