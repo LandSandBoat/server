ce4efc04260c6e325e044cc7e30226e705621d74
Zach Toogood <zrtoogood@gmail.com>
2020-10-11 22:58:18 +0300

    Add missing break

--- src/map/packets/action.cpp
+++ src/map/packets/action.cpp
@@ -227,6 +227,7 @@ CActionPacket::CActionPacket(action_t& action)
             ref<uint8>(0x0D) = 0x59;
             ref<uint8>(0x0E) = 0x19;
         }
+        break;
         case SPELLGROUP_TRUST:
         {
             ref<uint8>(0x0C) = 0x98;
