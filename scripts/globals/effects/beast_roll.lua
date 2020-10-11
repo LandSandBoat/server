1db4d522394415ec66f471780e3aa86215f062db
Across <55287958+Acrossy@users.noreply.github.com>
2020-10-11 16:58:47 +0100

    Update beast_roll.lua
    I tried the first time to request an update but I had a screw up on that attempt and since i don't know how to edit the changes I made before I'm trying a new request.
    This is my trial to send a proposal for BST roll to actually add ranged atk to pets including PUP bots.
    Please check if it's in accordance.
    Thank you.

--- scripts/globals/effects/beast_roll.lua
+++ scripts/globals/effects/beast_roll.lua
@@ -8,6 +8,7 @@ require("scripts/globals/status")
 
 function onEffectGain(target, effect)
     target:addPetMod(tpz.mod.ATTP, effect:getPower())
+    target:addPetMod(tpz.mod.RATTP, effect:getPower())
 end
 
 function onEffectTick(target, effect)
@@ -15,4 +16,5 @@ end
 
 function onEffectLose(target, effect)
     target:delPetMod(tpz.mod.ATTP, effect:getPower())
+    target:delPetMod(tpz.mod.RATTP, effect:getPower())
 end
