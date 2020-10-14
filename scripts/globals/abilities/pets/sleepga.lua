1300ce166c88ff0c1162582aada0102ff2d43d7b
Matthew James Davis <davismj@foursails.co>
2020-10-13 20:22:47 -0400

    fix(ability): Use tabled element value in Sleepga

--- scripts/globals/abilities/pets/sleepga.lua
+++ scripts/globals/abilities/pets/sleepga.lua
@@ -5,6 +5,7 @@ require("scripts/globals/monstertpmoves")
 require("scripts/globals/status")
 require("scripts/globals/magic")
 require("scripts/globals/msg")
+require("scripts/globals/spell_data")
 require("scripts/globals/summon")
 -----------------------------------------
 
@@ -16,7 +17,7 @@ function onPetAbility(target, pet, skill)
     local duration = 90
     local dINT = pet:getStat(tpz.mod.INT) - target:getStat(tpz.mod.INT)
     local bonus = getSummoningSkillOverCap(pet)
-    local resm = applyPlayerResistance(pet, -1, target, dINT, bonus, 5)
+    local resm = applyPlayerResistance(pet, -1, target, dINT, bonus, tpz.magic.element.ICE)
     if (resm < 0.5) then
         skill:setMsg(tpz.msg.basic.JA_MISS_2) -- resist message
         return tpz.effect.SLEEP_I
