fac64b07c859257b2374a7078a58eb5edd81cd79
rude-jerk <9592857+rude-jerk@users.noreply.github.com>
2020-10-14 18:39:36 -0400

    Comment the relics table a bit

--- scripts/zones/Castle_Zvahl_Baileys/npcs/Switchstix.lua
+++ scripts/zones/Castle_Zvahl_Baileys/npcs/Switchstix.lua
@@ -28,99 +28,99 @@ local currency =
 local relics =
 {
     -- Spharai
-    [18260] = { { 1460, 1459, 665 }, currency.BYNE_100, 4, 1, 1 },
-    [18261] = { { 16390, 16392, 16396 }, currency.SILVER_100, 14, 2, 2 },
-    [18262] = { { 1556 }, currency.SHELL_100, 61, 3, 3 },
+    [18260] = { { 1460, 1459, 665 }, currency.BYNE_100, 4, 1, 1 }, -- Relic Knuckles
+    [18261] = { { 16390, 16392, 16396 }, currency.SILVER_100, 14, 2, 2 }, -- Militant Knuckles
+    [18262] = { { 1556 }, currency.SHELL_100, 61, 3, 3 }, -- Dynamis Knuckles
     [18263] = { { 1571, 1589 }, currency.BYNE_10000, 1, 4, 3 }, -- Caestus
 
     -- Mandau
-    [18266] = { { 4246, 747, 4166 }, currency.BYNE_100, 4, 1, 1 },
-    [18267] = { { 16449, 16496, 16452 }, currency.SILVER_100, 14, 2, 2 },
-    [18268] = { { 1557 }, currency.SHELL_100, 61, 3, 3 },
+    [18266] = { { 4246, 747, 4166 }, currency.BYNE_100, 4, 1, 1 }, -- Relic Dagger
+    [18267] = { { 16449, 16496, 16452 }, currency.SILVER_100, 14, 2, 2 }, -- Malefic Dagger
+    [18268] = { { 1557 }, currency.SHELL_100, 61, 3, 3 }, -- Dynamis Dagger
     [18269] = { { 1572, 1589 }, currency.BYNE_10000, 1, 4, 3 }, -- Batardeau
 
     -- Excalibur
-    [18272] = { { 1460, 763, 931 }, currency.SILVER_100, 4, 1, 1 },
-    [18273] = { { 16535, 16537, 16542 }, currency.BYNE_100, 14, 2, 2 },
-    [18274] = { { 1558 }, currency.SHELL_100, 61, 3, 3 },
+    [18272] = { { 1460, 763, 931 }, currency.SILVER_100, 4, 1, 1 }, -- Relic Sword
+    [18273] = { { 16535, 16537, 16542 }, currency.BYNE_100, 14, 2, 2 }, -- Glyptic Sword
+    [18274] = { { 1558 }, currency.SHELL_100, 61, 3, 3 }, -- Dynamis Sword
     [18275] = { { 1573, 1589 }, currency.SILVER_10000, 1, 4, 3 }, -- Caliburn
 
     -- Ragnarok
-    [18278] = { { 1459, 655, 746 }, currency.SILVER_100, 4, 1, 1 },
-    [18279] = { { 16583, 16584, 16585 }, currency.SHELL_100, 14, 2, 2 },
-    [18280] = { { 1559 }, currency.BYNE_100, 61, 3, 3 },
+    [18278] = { { 1459, 655, 746 }, currency.SILVER_100, 4, 1, 1 }, -- Relic Blade
+    [18279] = { { 16583, 16584, 16585 }, currency.SHELL_100, 14, 2, 2 }, -- Gilded Blade
+    [18280] = { { 1559 }, currency.BYNE_100, 61, 3, 3 }, -- Dynamis Blade
     [18281] = { { 1574, 1589 }, currency.SILVER_10000, 1, 4, 3 }, -- Valhalla
 
     -- Guttler
-    [18284] = { { 1312, 1463, 13060 }, currency.SHELL_100, 3, 1, 1 },
-    [18285] = { { 16657, 16658, 16659 }, currency.SILVER_100, 14, 2, 2 },
-    [18286] = { { 1560 }, currency.BYNE_100, 60, 3, 3 },
+    [18284] = { { 1312, 1463, 13060 }, currency.SHELL_100, 3, 1, 1 }, -- Relic Axe
+    [18285] = { { 16657, 16658, 16659 }, currency.SILVER_100, 14, 2, 2 }, -- Leonine Axe
+    [18286] = { { 1560 }, currency.BYNE_100, 60, 3, 3 }, -- Dynamis Axe
     [18287] = { { 1575, 1589 }, currency.SHELL_10000, 1, 4, 3 }, -- Ogre Killer
 
     -- Bravura
-    [18290] = { { 1461, 658, 720 }, currency.BYNE_100, 3, 1, 1 },
-    [18291] = { { 16704, 16705, 16724 }, currency.SHELL_100, 16, 2, 2 },
-    [18292] = { { 1561 }, currency.SILVER_100, 60, 3, 3 },
+    [18290] = { { 1461, 658, 720 }, currency.BYNE_100, 3, 1, 1 }, -- Relic Bhuj
+    [18291] = { { 16704, 16705, 16724 }, currency.SHELL_100, 16, 2, 2 }, -- Agonal Bhuj
+    [18292] = { { 1561 }, currency.SILVER_100, 60, 3, 3 }, -- Dynamis Bhuj
     [18293] = { { 1576, 1589 }, currency.BYNE_10000, 1, 4, 3 }, -- Abaddon Killer
 
     -- Gungnir
-    [18296] = { { 1462, 747, 1294 }, currency.SHELL_100, 4, 1, 1 },
-    [18297] = { { 16834, 16836, 16841 }, currency.BYNE_100, 16, 2, 2 },
-    [18298] = { { 1563 }, currency.SILVER_100, 61, 3, 3 },
+    [18296] = { { 1462, 747, 1294 }, currency.SHELL_100, 4, 1, 1 }, -- Relic Lance
+    [18297] = { { 16834, 16836, 16841 }, currency.BYNE_100, 16, 2, 2 }, -- Hotspur Lance
+    [18298] = { { 1563 }, currency.SILVER_100, 61, 3, 3 }, -- Dynamis Lance
     [18299] = { { 1578, 1589 }, currency.SHELL_10000, 1, 4, 3 }, -- Gae Assail
 
     -- Apocalypse
-    [18302] = { { 1458, 1117, 13208 }, currency.SHELL_100, 5, 1, 1 },
-    [18303] = { { 16774, 16794, 16777 }, currency.SILVER_100, 16, 2, 2 },
-    [18304] = { { 1562 }, currency.BYNE_100, 62, 3, 3 },
+    [18302] = { { 1458, 1117, 13208 }, currency.SHELL_100, 5, 1, 1 }, -- Relic Scythe
+    [18303] = { { 16774, 16794, 16777 }, currency.SILVER_100, 16, 2, 2 }, -- Memento Scythe
+    [18304] = { { 1562 }, currency.BYNE_100, 62, 3, 3 }, -- Dynamis Scythe
     [18305] = { { 1577, 1589 }, currency.SHELL_10000, 1, 4, 3 }, -- Bec De Faucon
 
     -- Kikoku
-    [18308] = { { 1467, 1276, 1278 }, currency.BYNE_100, 4, 1, 1 },
-    [18309] = { { 16900, 16903, 16902 }, currency.SHELL_100, 16, 2, 2 },
-    [18310] = { { 1564 }, currency.SILVER_100, 61, 3, 3 },
+    [18308] = { { 1467, 1276, 1278 }, currency.BYNE_100, 4, 1, 1 }, -- Ihintanto
+    [18309] = { { 16900, 16903, 16902 }, currency.SHELL_100, 16, 2, 2 }, -- Mimizuku
+    [18310] = { { 1564 }, currency.SILVER_100, 61, 3, 3 }, -- Rogetsu
     [18311] = { { 1579, 1589 }, currency.BYNE_10000, 1, 4, 3 }, -- Yoshimitsu
 
     -- Amanomurakumo
-    [18314] = { { 1467, 1409, 657 }, currency.SILVER_100, 3, 1, 1 },
-    [18315] = { { 16966, 16967, 16972 }, currency.SHELL_100, 15, 2, 2 },
-    [18316] = { { 1565 }, currency.BYNE_100, 60, 3, 3 },
+    [18314] = { { 1467, 1409, 657 }, currency.SILVER_100, 3, 1, 1 }, -- Ito
+    [18315] = { { 16966, 16967, 16972 }, currency.SHELL_100, 15, 2, 2 }, -- Hayatemaru
+    [18316] = { { 1565 }, currency.BYNE_100, 60, 3, 3 }, -- Oboromaru
     [18317] = { { 1580, 1589 }, currency.SILVER_10000, 1, 4, 3 }, -- Totsukanotsurugi
 
     -- Mjollnir
-    [18320] = { { 1461, 746, 830 }, currency.SILVER_100, 5, 1, 1 },
-    [18321] = { { 17044, 17080, 17043 }, currency.BYNE_100, 16, 2, 2 },
-    [18322] = { { 1566 }, currency.SHELL_100, 62, 3, 3 },
+    [18320] = { { 1461, 746, 830 }, currency.SILVER_100, 5, 1, 1 }, -- Relic Maul
+    [18321] = { { 17044, 17080, 17043 }, currency.BYNE_100, 16, 2, 2 }, -- Battering Maul
+    [18322] = { { 1566 }, currency.SHELL_100, 62, 3, 3 }, -- Dynamis Maul
     [18323] = { { 1581, 1589 }, currency.SILVER_10000, 1, 4, 3 }, -- Gullintani
 
     -- Claustrum
-    [18326] = { { 1462, 1271, 1415 }, currency.SHELL_100, 5, 1, 1 },
-    [18327] = { { 17088, 17090, 17092 }, currency.BYNE_100, 16, 2, 2 },
-    [18328] = { { 1567 }, currency.SILVER_100, 62, 3, 3 },
+    [18326] = { { 1462, 1271, 1415 }, currency.SHELL_100, 5, 1, 1 }, -- Relic Staff
+    [18327] = { { 17088, 17090, 17092 }, currency.BYNE_100, 16, 2, 2 }, -- Sage's Staff
+    [18328] = { { 1567 }, currency.SILVER_100, 62, 3, 3 }, -- Dynamis Staff
     [18329] = { { 1582, 1589 }, currency.SHELL_10000, 1, 4, 3 }, -- Thyrus
 
     -- Annihilator
-    [18332] = { { 1468, 830, 654 }, currency.BYNE_100, 5, 1, 1 },
-    [18333] = { { 17248, 17251, 17259 }, currency.SHELL_100, 15, 2, 2 },
-    [18334] = { { 1570 }, currency.SILVER_100, 62, 3, 3 },
+    [18332] = { { 1468, 830, 654 }, currency.BYNE_100, 5, 1, 1 }, -- Relic Gun
+    [18333] = { { 17248, 17251, 17259 }, currency.SHELL_100, 15, 2, 2 }, -- Marksman Gun
+    [18334] = { { 1570 }, currency.SILVER_100, 62, 3, 3 }, -- Dynamis Gun
     [18335] = { { 1585, 1589 }, currency.BYNE_10000, 1, 4, 3 }, -- Ferdinand
 
     -- Gjallarhorn
-    [18338] = { { 1458, 1463, 13232 }, currency.SHELL_100, 3, 1, 1 },
-    [18339] = { { 17352, 17351, 17362 }, currency.BYNE_100, 14, 2, 2 },
-    [18340] = { { 1569 }, currency.SILVER_100, 60, 3, 3 },
+    [18338] = { { 1458, 1463, 13232 }, currency.SHELL_100, 3, 1, 1 }, -- Relic Horn
+    [18339] = { { 17352, 17351, 17362 }, currency.BYNE_100, 14, 2, 2 }, -- Pyrrhic Horn
+    [18340] = { { 1569 }, currency.SILVER_100, 60, 3, 3 }, -- Dynamis Horn
     [18341] = { { 1584, 1589 }, currency.SHELL_10000, 1, 4, 3 }, -- Milennium Horn
 
     -- Yoichinoyumi
-    [18344] = { { 883, 1462, 932 }, currency.SILVER_100, 4, 1, 1 },
-    [18345] = { { 17161, 17164, 18142 }, currency.SILVER_100, 15, 2, 2 },
-    [18346] = { { 1568 }, currency.SHELL_100, 61, 3, 3 },
+    [18344] = { { 883, 1462, 932 }, currency.SILVER_100, 4, 1, 1 }, -- Relic Bow
+    [18345] = { { 17161, 17164, 18142 }, currency.SILVER_100, 15, 2, 2 }, -- Wolver Bow
+    [18346] = { { 1568 }, currency.SHELL_100, 61, 3, 3 }, -- Dynamis Bow
     [18347] = { { 1583, 1589 }, currency.SILVER_10000, 1, 4, 3 }, -- Futatokoroto
 
     -- Aegis
-    [15066] = { { 875, 668, 720 }, 0, 1, 1, 4 },
-    [15067] = { { 12301, 12295, 12387 }, 0, 4, 2, 5 },
-    [15068] = { { 1821 }, 0, 20, 3, 6 },
+    [15066] = { { 875, 668, 720 }, 0, 1, 1, 4 }, -- Relic Shield
+    [15067] = { { 12301, 12295, 12387 }, 0, 4, 2, 5 }, -- Bulwark Shield
+    [15068] = { { 1821 }, 0, 20, 3, 6 }, -- Dynamis Shield
     [15069] = { { 1822, 1589 }, currency.SILVER_10000, 1, 4, 6 }, -- Ancile
 }
 
