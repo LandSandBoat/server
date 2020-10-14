3e6f85487683819017ab8ed3b52cbdb94fcb9e0d
Marian Arlt <marianarlt@icloud.com>
2020-10-14 05:43:19 -0500

    csid added and timeout on spawn

--- scripts/zones/Garlaige_Citadel/npcs/qm15.lua
+++ scripts/zones/Garlaige_Citadel/npcs/qm15.lua
@@ -4,6 +4,8 @@
 -- Involved in Quest: Hitting the Marquisate (THF AF3)
 -- !pos 19.893 -5.500 325.767 200
 -----------------------------------
+require("scripts/globals/settings")
+require("scripts/globals/keyitems")
 local ID = require("scripts/zones/Garlaige_Citadel/IDs")
 -----------------------------------
 
@@ -11,14 +13,14 @@ function onTrade(player, npc, trade)
 end
 
 function onTrigger(player, npc)
-    local nm = GetMobByID(ID.mob.CHANDELIER)
-    if (
+    player:messageSpecial(ID.text.HEAT_FROM_CEILING)
+    if
+        not GetMobByID(ID.mob.CHANDELIER):isSpawned() and
+        player:hasKeyItem(tpz.ki.BOMB_INCENSE) and
         player:getCharVar("hittingTheMarquisateHagainCS") == 8 and
-        os.time() > GetNPCByID(ID.npc.CHANDELIER_QM):getLocalVar("pop") and
-        not nm:isSpawned()
-    ) then
-        player:messageSpecial(ID.text.HEAT_FROM_CEILING)
-        nm:spawn()
+        os.time() > GetNPCByID(ID.npc.CHANDELIER_QM):getLocalVar("chandelierCooldown")
+    then
+        player:startEvent(56, tpz.keyItem.BOMB_INCENSE)
     end
 end
 
@@ -26,4 +28,7 @@ function onEventUpdate(player, csid, option)
 end
 
 function onEventFinish(player, csid, option)
+    if csid == 56 and option == 1 then
+        GetMobByID(ID.mob.CHANDELIER):setRespawnTime(10)
+    end
 end
