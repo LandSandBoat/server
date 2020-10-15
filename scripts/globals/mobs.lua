bbec01d43e00b28516db550976ae93e8b5d7a0a1
Kreidos <kreidos@users.noreply.github.com>
2020-10-14 21:11:05 -0700

    Make NM Lottery configurable

--- scripts/globals/mobs.lua
+++ scripts/globals/mobs.lua
@@ -61,6 +61,13 @@ end
 -- potential lottery placeholder was killed
 tpz.mob.phOnDespawn = function(ph, phList, chance, cooldown, immediate)
     if type(immediate) ~= "boolean" then immediate = false end
+    
+    if NM_LotteryChance then
+        chance = NM_LotteryChance >= 0 and (chance * NM_LotteryChance) or 100
+    end
+    if NM_LotteryCooldown then
+        cooldown = NM_LotteryCooldown >= 0 and (cooldown * NM_LotteryCooldown) or cooldown
+    end
 
     local phId = ph:getID()
     local nmId = phList[phId]
