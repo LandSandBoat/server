fbf1859b32a1136821f3291d6f481c10cf0bb846
nassiralawar <nassir.alawar@gmail.com>
2020-10-21 21:08:01 -0700

    In 2014 pet base damage was updated, I do not have references to data for other pets but celestial avatars weapon damage is approximately equal to (1+level) now. I have not tested carbuncle so the 0.9 is currently a guess based on the fact that the weapon damage was 10% less previously and carbuncle is still weaker than the celestials. Happy to get numbers for carbuncle if needed. Cait is the same as carbuncle, they suck equally.

--- src/map/utils/petutils.cpp
+++ src/map/utils/petutils.cpp
@@ -1608,12 +1608,16 @@ namespace petutils
             {
                 ((CItemWeapon*)PPet->m_Weapons[SLOT_MAIN])->setDelay((uint16)(floor(1000.0 * (280.0f / 60.0f))));
             }
-            ((CItemWeapon*)PPet->m_Weapons[SLOT_MAIN])->setDamage((uint16)(floor(PPet->GetMLevel() * 0.74f)));
 
-            if (PetID == PETID_CARBUNCLE)
+            //In a 2014 update SE updated Avatar base damage
+            //Based on testing this value appears to be Level now instead of Level * 0.74f
+            uint16 weaponDamage = 1 + PPet->GetMLevel();
+            if (PetID == PETID_CARBUNCLE || PETID_CAIT_SITH)
             {
-                ((CItemWeapon*)PPet->m_Weapons[SLOT_MAIN])->setDamage((uint16)(floor(PPet->GetMLevel() * 0.67f)));
+                weaponDamage = (uint16)(floor(PPet->GetMLevel() * 0.9f));
             }
+
+            ((CItemWeapon*)PPet->m_Weapons[SLOT_MAIN])->setDamage(weaponDamage);
 
             //Set B+ weapon skill (assumed capped for level derp)
             //attack is madly high for avatars (roughly x2)
