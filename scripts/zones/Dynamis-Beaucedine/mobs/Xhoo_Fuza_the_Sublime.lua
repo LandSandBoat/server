4d7b1d322b6d34435e60d920d776a01eea470213
rude-jerk <9592857+rude-jerk@users.noreply.github.com>
2020-10-24 02:18:11 -0400

    Generalize doom removal mixin, allow local var param to set removal chance, add message for escaping doom

--- scripts/zones/Dynamis-Beaucedine/mobs/Xhoo_Fuza_the_Sublime.lua
+++ scripts/zones/Dynamis-Beaucedine/mobs/Xhoo_Fuza_the_Sublime.lua
@@ -6,7 +6,7 @@ mixins =
 {
     require("scripts/mixins/dynamis_beastmen"),
     require("scripts/mixins/job_special"),
-    require("scripts/mixins/dynamis_yagudo")
+    require("scripts/mixins/remove_doom")
 }
 -----------------------------------
 
