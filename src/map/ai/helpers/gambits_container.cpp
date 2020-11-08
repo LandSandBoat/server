b40d15087b68ec1281ec443025e099fd6a8635ba
Zach Toogood <zrtoogood@gmail.com>
2020-11-08 11:53:34 +0200

    Trust: replace GetRandomNumber with GetRandomElement

--- src/map/ai/helpers/gambits_container.cpp
+++ src/map/ai/helpers/gambits_container.cpp
@@ -558,7 +558,7 @@ bool CGambitsContainer::TryTrustSkill()
         {
             case G_SELECT::RANDOM:
             {
-                chosen_skill = tp_skills.at(tpzrand::GetRandomNumber<std::size_t>(tp_skills.size()));
+                chosen_skill = tpzrand::GetRandomElement(tp_skills);                
                 break;
             }
             case G_SELECT::HIGHEST: // Form the best possible skillchain
