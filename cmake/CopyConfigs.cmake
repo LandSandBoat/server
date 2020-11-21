1df9a5c4d09189625f16ab92423a966b82355678
Zach Toogood <zrtoogood@gmail.com>
2020-11-21 18:02:24 +0200

    CI: General CMake and CI cleanup

---
+++ cmake/CopyConfigs.cmake
@@ -0,0 +1,21 @@
+set(CONFIG_FILES
+    login.conf
+    maint.conf
+    map.conf
+    packet_tcp.conf
+    packet_udp.conf
+    search_server.conf
+    version.conf
+)
+
+foreach(file ${CONFIG_FILES})
+    if (NOT EXISTS "${CMAKE_SOURCE_DIR}/conf/${file}")
+        set(copied_configs TRUE)
+        message(STATUS "Copying config file: ${CMAKE_SOURCE_DIR}/conf/default/${file} -> ${CMAKE_SOURCE_DIR}/conf/${file}")
+        file(COPY ${CMAKE_SOURCE_DIR}/conf/default/${file} DESTINATION ${CMAKE_SOURCE_DIR}/conf)
+    endif()
+endforeach()
+
+if (${copied_configs})
+    message(STATUS "Populated config files, don't forget to fill them in!")
+endif()
