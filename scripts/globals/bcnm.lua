5fc59a94ff30029e67751098d23b6b8429b13b14
ibm2431 <ibm2431@dekumi.com>
2020-12-01 19:36:07 -0800

    Disable Early Bird Catches the Wyrm until fixed
    - Problem with Draw In drawing players out of bounds
    - Reports of inability to hit Wyrm while on the ground
    
    (cherry picked from commit 959e512ec14dfa2d715508f3bdebc6099263ba55)

--- scripts/globals/bcnm.lua
+++ scripts/globals/bcnm.lua
@@ -317,7 +317,7 @@ local battlefields = {
         { 8,  104, 1552},   -- Creeping Doom (BS30)
         { 9,  105, 1551},   -- Charming Trio (BS20)
         {10,  106, 1552},   -- Harem Scarem (BS30)
-        {11,  107, 1553},   -- Early Bird Catches the Wyrm (KS99)
+     -- {11,  107, 1553},   -- Early Bird Catches the Wyrm (KS99)
         {12,  108, 1131},   -- Royal Succession (BS40)
         {13,  109, 1177},   -- Rapid Raptors (BS50)
         {14,  110, 1130},   -- Wild Wild Whiskers (BS60) -- TODO: should use petrifactive breath more often than other mobskill. Message before spellcasting.
