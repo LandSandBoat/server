42163bcae1da3d3a0fc69e2ac5c35c7331d83093
claywar <steinmann00@gmail.com>
2021-01-06 08:56:24 -0500

    Performance: Crawlers Nest [S], Dangruf, Davoi mob conversion

--- scripts/zones/Dangruf_Wadi/mobs/Hoarder_Hare.lua
+++ scripts/zones/Dangruf_Wadi/mobs/Hoarder_Hare.lua
@@ -5,6 +5,8 @@
 -----------------------------------
 local ID = require("scripts/zones/Dangruf_Wadi/IDs")
 require("scripts/globals/mobs")
+-----------------------------------
+local entity = {}
 
 function onMobDeath(mob, player, isKiller)
 end
@@ -12,3 +14,5 @@ end
 function onMobDespawn(mob)
     tpz.mob.phOnDespawn(mob, ID.mob.TEPORINGO_PH, 20, 3600) -- 1 hour
 end
+
+return entity
