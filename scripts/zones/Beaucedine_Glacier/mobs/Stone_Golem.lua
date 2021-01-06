330c648e57fab20212bf59ea8f4af252778f1bb0
claywar <steinmann00@gmail.com>
2021-01-05 19:49:58 -0500

    Performance: Beaucedine, Beaucedine [S] mob conversion

--- scripts/zones/Beaucedine_Glacier/mobs/Stone_Golem.lua
+++ scripts/zones/Beaucedine_Glacier/mobs/Stone_Golem.lua
@@ -5,6 +5,8 @@
 -----------------------------------
 local ID = require("scripts/zones/Beaucedine_Glacier/IDs")
 require("scripts/globals/mobs")
+-----------------------------------
+local entity = {}
 
 function onMobDeath(mob, player, isKiller)
 end
@@ -12,3 +14,5 @@ end
 function onMobDespawn(mob)
     tpz.mob.phOnDespawn(mob, ID.mob.GARGANTUA_PH, 5, math.random(3600, 25200)) -- 1 to 7 hours
 end
+
+return entity
--- scripts/zones/Beaucedine_Glacier_[S]/mobs/Gargouille.lua
+++ scripts/zones/Beaucedine_Glacier_[S]/mobs/Gargouille.lua
@@ -6,6 +6,7 @@
 local ID = require("scripts/zones/Beaucedine_Glacier_[S]/IDs")
 require("scripts/globals/mobs")
 -----------------------------------
+local entity = {}
 
 function onMobDeath(mob, player, isKiller)
 end
@@ -13,3 +14,5 @@ end
 function onMobDespawn(mob)
     tpz.mob.phOnDespawn(mob, ID.mob.GRANDGOULE_PH, 10, 3600) -- 1 hour
 end
+
+return entity
