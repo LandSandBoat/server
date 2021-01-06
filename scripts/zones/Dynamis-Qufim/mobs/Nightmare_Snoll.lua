ff089c462177aa1d05a4f7c86b7d174300496b17
claywar <steinmann00@gmail.com>
2021-01-06 15:32:29 -0500

    Performance: Dynamis-Qufim mob conversion

--- scripts/zones/Dynamis-Qufim/mobs/Nightmare_Snoll.lua
+++ scripts/zones/Dynamis-Qufim/mobs/Nightmare_Snoll.lua
@@ -4,6 +4,7 @@
 -----------------------------------
 mixins = {require("scripts/mixins/dynamis_dreamland")}
 -----------------------------------
+local entity = {}
 
 function onMobSpawn(mob)
     mob:setLocalVar("dynamis_currency", 1452)
@@ -11,3 +12,5 @@ end
 
 function onMobDeath(mob, player, isKiller)
 end
+
+return entity
