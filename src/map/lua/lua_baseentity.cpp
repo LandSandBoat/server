55bc8fb1913792971a770d8178c8ad7a27a9014a
Zach Toogood <zrtoogood@gmail.com>
2021-01-09 13:54:32 +0200

    Fix crash with badly formed table in getEnmityList

--- src/map/lua/lua_baseentity.cpp
+++ src/map/lua/lua_baseentity.cpp
@@ -11777,21 +11777,20 @@ sol::table CLuaBaseEntity::getEnmityList()
 
     if (enmityList)
     {
-        sol::table table = luautils::lua.create_table();
-        int        i     = 1;
+        auto table = luautils::lua.create_table();
         for (auto member : *enmityList)
         {
             if (member.second.PEnmityOwner)
             {
-                sol::table subTable;
+                auto subTable = luautils::lua.create_table();
 
-                subTable.add("entity", CLuaBaseEntity(member.second.PEnmityOwner));
-                subTable.add("ce", member.second.CE);
-                subTable.add("ve", member.second.VE);
-                subTable.add("active", member.second.active);
-                subTable.add("tameable", ((CMobEntity*)m_PBaseEntity)->PEnmityContainer->IsTameable());
+                subTable["entity"] = CLuaBaseEntity(member.second.PEnmityOwner);
+                subTable["ce"] = member.second.CE;
+                subTable["ve"] = member.second.VE;
+                subTable["active"] = member.second.active;
+                subTable["tameable"] = ((CMobEntity*)m_PBaseEntity)->PEnmityContainer->IsTameable();
 
-                table.add(i++, subTable);
+                table.add(subTable);
             }
         }
 
