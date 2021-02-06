e75f9fa1eac56cd9a704d9782184658159aef178
Zach Toogood <zrtoogood@gmail.com>
2021-02-06 14:16:59 +0200

    Minor Utsu formatting fixes

--- scripts/globals/abilities/third_eye.lua
+++ scripts/globals/abilities/third_eye.lua
@@ -10,16 +10,17 @@ require("scripts/globals/status")
 -----------------------------------
 
 function onAbilityCheck(player, target, ability)
-    if (player:hasStatusEffect(tpz.effect.SEIGAN)) then
-        ability:setRecast(ability:getRecast()/2)
+    if player:hasStatusEffect(tpz.effect.SEIGAN) then
+        ability:setRecast(ability:getRecast() / 2)
     end
     return 0, 0
 end
 
 function onUseAbility(player, target, ability)
-    if (player:hasStatusEffect(tpz.effect.COPY_IMAGE)) then
-        effect:setMsg(tpz.msg.basic.JA_NO_EFFECT) --Returns "no effect" message when Copy Image is active when Third Eye is used.
+    if player:hasStatusEffect(tpz.effect.COPY_IMAGE)
+        -- Returns "no effect" message when Copy Image is active when Third Eye is used.
+        effect:setMsg(tpz.msg.basic.JA_NO_EFFECT)
     else
-        player:addStatusEffect(tpz.effect.THIRD_EYE, 0, 0, 30) --power keeps track of procs
+        player:addStatusEffect(tpz.effect.THIRD_EYE, 0, 0, 30) -- Power keeps track of procs
     end
 end
--- scripts/globals/spells/utsusemi_san.lua
+++ scripts/globals/spells/utsusemi_san.lua
@@ -10,8 +10,9 @@ function onMagicCastingCheck(caster, target, spell)
 end
 
 function onSpellCast(caster, target, spell)
-    if (target:hasStatusEffect(tpz.effect.THIRD_EYE)) then
-        target:delStatusEffect(tpz.effect.THIRD_EYE) -- Third Eye and Utsusemi don't stack.  Utsusemi removes Third Eye.
+    if target:hasStatusEffect(tpz.effect.THIRD_EYE) then
+        -- Third Eye and Utsusemi don't stack. Utsusemi removes Third Eye.
+        target:delStatusEffect(tpz.effect.THIRD_EYE)
     end
 
     local effect = target:getStatusEffect(tpz.effect.COPY_IMAGE)
@@ -19,7 +20,7 @@ function onSpellCast(caster, target, spell)
     -- Get extras shadows
     local numShadows = 5 + target:getMod(tpz.mod.UTSUSEMI_BONUS)
 
-    if (effect == nil or effect:getPower() <= 3) then
+    if effect == nil or effect:getPower() <= 3 then
         target:addStatusEffectEx(tpz.effect.COPY_IMAGE, tpz.effect.COPY_IMAGE_4, numShadows, 0, 900, 0, numShadows)
         spell:setMsg(tpz.msg.basic.MAGIC_GAIN_EFFECT)
     else
