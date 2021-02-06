c4d7ccf1489a98ca5191f42523d1e4c992e6947f
Zach Toogood <zrtoogood@gmail.com>
2021-02-06 08:51:20 +0200

    Adjust dbtool import_file routine to handle InnoDB bulk imports better

--- tools/dbtool.py
+++ tools/dbtool.py
@@ -1,4 +1,5 @@
 import os
+import subprocess
 import sys
 import re
 import time
@@ -255,10 +256,21 @@ def write_version(silent=False):
         print(Fore.RED + 'Error writing version.')
 
 def import_file(file):
-    updatecmd = '"' + mysql_bin + 'mysql' + exe + '" -h ' + host + ' -P ' + str(port) + ' -u ' + login + ' -p' + password + ' ' + database
     print('Importing ' + file + '...')
-    os.system(updatecmd + ' < ../sql/' + file + log_errors)
-    fetch_errors()
+    result = subprocess.run([
+        '{}mysql{}'.format(mysql_bin, exe),
+        '-h', host,
+        '-P', str(port),
+        '-u', login,
+        '-p{}'.format(password),
+        database,
+        '-e', 'SET autocommit=0; SET unique_checks=0; SET foreign_key_checks=0; source ../sql/{}; SET unique_checks=1; SET foreign_key_checks=1; COMMIT;'.format(file)],
+        capture_output=True, text=True)
+
+    for line in result.stderr.splitlines():
+        # Safe to ignore this warning
+        if 'Using a password on the command line interface can be insecure' not in line:
+            print(Fore.RED + line)
 
 def connect():
     global db, cur
