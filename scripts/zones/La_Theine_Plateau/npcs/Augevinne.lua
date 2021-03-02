a8a90d50bed85f387a8fad93e4f2b4edef0aace8
claywar <steinmann00@gmail.com>
2021-03-02 17:50:00 -0500

    Fix "The Rescue Drill" NPC messages

--- scripts/zones/La_Theine_Plateau/npcs/Augevinne.lua
+++ scripts/zones/La_Theine_Plateau/npcs/Augevinne.lua
@@ -13,23 +13,21 @@ entity.onTrade = function(player, npc, trade)
 end
 
 entity.onTrigger = function(player, npc)
-
-    if (player:getCurrentMission(SANDORIA) == tpz.mission.id.sandoria.THE_RESCUE_DRILL) then
+    if player:getCurrentMission(SANDORIA) == tpz.mission.id.sandoria.THE_RESCUE_DRILL then
         local MissionStatus = player:getCharVar("MissionStatus")
 
-        if (MissionStatus >= 5 and MissionStatus <= 7) then
+        if MissionStatus >= 5 and MissionStatus <= 7 then
             player:startEvent(103)
-        elseif (MissionStatus == 8) then
+        elseif MissionStatus == 8 then
             player:showText(npc, ID.text.RESCUE_DRILL + 21)
-        elseif (MissionStatus >= 9) then
+        elseif MissionStatus >= 9 then
             player:showText(npc, ID.text.RESCUE_DRILL + 26)
         else
-            player:showText(npc, ID.text.RESCUE_DRILL)
+            player:showText(npc, ID.text.RESCUE_DRILL + 6)
         end
     else
-        player:showText(npc, ID.text.RESCUE_DRILL)
+        player:showText(npc, ID.text.RESCUE_DRILL + 33)
     end
-
 end
 
 entity.onEventUpdate = function(player, csid, option)
--- scripts/zones/La_Theine_Plateau/npcs/Equesobillot.lua
+++ scripts/zones/La_Theine_Plateau/npcs/Equesobillot.lua
@@ -13,46 +13,43 @@ entity.onTrade = function(player, npc, trade)
 end
 
 entity.onTrigger = function(player, npc)
-
-    if (player:getCurrentMission(SANDORIA) == tpz.mission.id.sandoria.THE_RESCUE_DRILL) then
+    if player:getCurrentMission(SANDORIA) == tpz.mission.id.sandoria.THE_RESCUE_DRILL then
         local MissionStatus = player:getCharVar("MissionStatus")
 
-        if (MissionStatus == 2) then
+        if MissionStatus == 2 then
             player:startEvent(101)
-        elseif (MissionStatus == 3) then
+        elseif MissionStatus == 3 then
             player:showText(npc, ID.text.RESCUE_DRILL + 3)
-        elseif (MissionStatus == 8) then
-            if (player:getCharVar("theRescueDrillRandomNPC") == 2) then
+        elseif MissionStatus == 8 then
+            if player:getCharVar("theRescueDrillRandomNPC") == 2 then
                 player:startEvent(112)
             else
                 player:showText(npc, ID.text.RESCUE_DRILL + 21)
             end
-        elseif (MissionStatus == 9) then
-            if (player:getCharVar("theRescueDrillRandomNPC") == 2) then
+        elseif MissionStatus == 9 then
+            if player:getCharVar("theRescueDrillRandomNPC") == 2 then
                 player:showText(npc, ID.text.RESCUE_DRILL + 25)
             else
                 player:showText(npc, ID.text.RESCUE_DRILL + 26)
             end
-        elseif (MissionStatus >= 10) then
+        elseif MissionStatus >= 10 then
             player:showText(npc, ID.text.RESCUE_DRILL + 30)
         else
             player:showText(npc, ID.text.RESCUE_DRILL)
         end
     else
-        player:showText(npc, ID.text.RESCUE_DRILL)
+        player:showText(npc, ID.text.RESCUE_DRILL + 31)
     end
-
 end
 
 entity.onEventUpdate = function(player, csid, option)
 end
 
 entity.onEventFinish = function(player, csid, option)
-
-    if (csid == 101) then
+    if csid == 101 then
         player:setCharVar("MissionStatus", 3)
-    elseif (csid == 112) then
-        if (player:getFreeSlotsCount() == 0) then
+    elseif csid == 112 then
+        if player:getFreeSlotsCount() == 0 then
             player:messageSpecial(ID.text.ITEM_CANNOT_BE_OBTAINED, 16535) -- Bronze Sword
         else
             player:addItem(16535)
@@ -60,7 +57,6 @@ entity.onEventFinish = function(player, csid, option)
             player:setCharVar("MissionStatus", 9)
         end
     end
-
 end
 
 return entity
