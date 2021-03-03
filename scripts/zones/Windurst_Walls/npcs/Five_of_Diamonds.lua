70a00433f3b68e4b835149ce4e1f02ff7aa1ebe7
claywar <steinmann00@gmail.com>
2021-03-03 15:38:46 -0500

    Fix "A Greeting Cardian" additional dialogue

--- scripts/zones/Windurst_Walls/npcs/Five_of_Diamonds.lua
+++ scripts/zones/Windurst_Walls/npcs/Five_of_Diamonds.lua
@@ -4,7 +4,7 @@
 -- Type: Standard NPC
 -- !pos -220.954 -0.001 -122.708 239
 -----------------------------------
--- Auto-Script: Requires Verification (Verfied by Brawndo)
+require("scripts/globals/quests")
 -----------------------------------
 local entity = {}
 
@@ -12,7 +12,11 @@ entity.onTrade = function(player, npc, trade)
 end
 
 entity.onTrigger = function(player, npc)
-    player:startEvent(266)
+    if player:getQuestStatus(tpz.quest.log_id.WINDURST, tpz.quest.id.windurst.A_GREETING_CARDIAN) == QUEST_ACCEPTED then
+        player:startEvent(339)
+    else
+        player:startEvent(266)
+    end
 end
 
 entity.onEventUpdate = function(player, csid, option)
