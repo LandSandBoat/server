8de25fe1cca4e7f970a76efad156f82dd864b6e5
TeoTwawki <arawn.annwn@yandex.com>
2021-04-10 23:11:47 -0400

    tpzdb -> xidb in migrations folder

--- tools/migrations/convert_all_tables_to_innodb.py
+++ tools/migrations/convert_all_tables_to_innodb.py
@@ -8,14 +8,14 @@ def check_preconditions(cur):
 	return
 
 def needs_to_run(cur):
-	cur.execute("SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = 'tpzdb' AND ENGINE = 'MyISAM' LIMIT 1")
+	cur.execute("SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = 'xidb' AND ENGINE = 'MyISAM' LIMIT 1")
 	if cur.fetchone():
 		return True
 	return False
 
 def migrate(cur, db):
 	try:
-		cur.execute("SELECT TABLE_NAME FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = 'tpzdb' AND ENGINE = 'MyISAM'") 
+		cur.execute("SELECT TABLE_NAME FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = 'xidb' AND ENGINE = 'MyISAM'") 
 		for r in cur.fetchall():
 			cur.execute('ALTER TABLE `{}` ROW_FORMAT=DYNAMIC;'.format(r[0]))
 			cur.execute('ALTER TABLE `{}` ENGINE=InnoDB;'.format(r[0]))
