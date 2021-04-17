cocosolos/topaz-1

Convert scripts to use mission log

scripts/zones/Chamber_of_Oracles/npcs/Pedestal_of_Water.lua
@@ -15,12 +15,12 @@ entity.onTrade = function(player, npc, trade)
 end
 
 entity.onTrigger = function(player, npc)
-    local ZilartStatus = player:getCharVar("ZilartStatus")
+    local ZilartStatus = player:getMissionStatus(tpz.mission.log_id.ZILART)
 
     if (player:getCurrentMission(ZILART) == tpz.mission.id.zilart.THE_CHAMBER_OF_ORACLES) then
         if (player:hasKeyItem(tpz.ki.WATER_FRAGMENT)) then
             player:delKeyItem(tpz.ki.WATER_FRAGMENT)
-            player:setCharVar("ZilartStatus", ZilartStatus + 64)
+            player:setMissionStatus(tpz.mission.log_id.ZILART, ZilartStatus + 64)
             player:messageSpecial(ID.text.YOU_PLACE_THE, tpz.ki.WATER_FRAGMENT)
 
             if (ZilartStatus == 255) then
@@ -50,7 +50,7 @@ entity.onEventFinish = function(player, csid, option)
 
     if (csid == 1) then
         player:addTitle(tpz.title.LIGHTWEAVER)
-        player:setCharVar("ZilartStatus", 0)
+        player:setMissionStatus(tpz.mission.log_id.ZILART, 0)
         player:addKeyItem(tpz.ki.PRISMATIC_FRAGMENT)
         player:messageSpecial(ID.text.KEYITEM_OBTAINED, tpz.ki.PRISMATIC_FRAGMENT)
         player:completeMission(tpz.mission.log_id.ZILART, tpz.mission.id.zilart.THE_CHAMBER_OF_ORACLES)

scripts/zones/Port_Bastok/npcs/Hilda.lua
@@ -38,7 +38,7 @@ entity.onTrigger = function(player, npc)
 
     local WildcatBastok = player:getCharVar("WildcatBastok")
 
-    if (player:getCurrentMission(BASTOK) == tpz.mission.id.bastok.ON_MY_WAY) and (player:getCharVar("MissionStatus") == 1) then
+    if (player:getCurrentMission(BASTOK) == tpz.mission.id.bastok.ON_MY_WAY) and (player:getMissionStatus(player:getNation()) == 1) then
         player:startEvent(255)
     elseif (player:getQuestStatus(tpz.quest.log_id.BASTOK, tpz.quest.id.bastok.LURE_OF_THE_WILDCAT) == QUEST_ACCEPTED and not utils.mask.getBit(WildcatBastok, 3)) then
         player:startEvent(356)
@@ -97,7 +97,7 @@ entity.onEventFinish = function(player, csid, option)
             player:completeQuest(tpz.quest.log_id.BASTOK, tpz.quest.id.bastok.THE_USUAL)
         end
     elseif (csid == 255) then
-        player:setCharVar("MissionStatus", 2)
+        player:setMissionStatus(player:getNation(), 2)
     elseif (csid == 356) then
         player:setCharVar("WildcatBastok", utils.mask.setBit(player:getCharVar("WildcatBastok"), 3, true))
     end

scripts/zones/Windurst_Walls/npcs/_6n2.lua
@@ -24,7 +24,7 @@ entity.onTrigger = function(player, npc)
     local iCanHearARainbow = player:getQuestStatus(tpz.quest.log_id.WINDURST, tpz.quest.id.windurst.I_CAN_HEAR_A_RAINBOW)
 
     -- LOST FOR WORDS
-    if player:getCurrentMission(WINDURST) == tpz.mission.id.windurst.LOST_FOR_WORDS and player:getCharVar("MissionStatus") == 5 then
+    if player:getCurrentMission(WINDURST) == tpz.mission.id.windurst.LOST_FOR_WORDS and player:getMissionStatus(player:getNation()) == 5 then
         player:startEvent(337)
 
     -- KNOW ONE'S ONIONS
@@ -95,7 +95,7 @@ end
 entity.onEventFinish = function(player, csid, option)
     -- LOST FOR WORDS
     if csid == 337 then
-        player:setCharVar("MissionStatus", 6)
+        player:setMissionStatus(player:getNation(), 6)
 
     -- KNOW ONE'S ONIONS
     elseif csid == 288 then

scripts/zones/Xarcabard/Zone.lua
@@ -41,7 +41,7 @@ zone_object.onZoneIn = function(player, prevZone)
         cs = 9
     elseif UnbridledPassionCS == 3 then
         cs = 4
-    elseif player:getCurrentMission(WINDURST) == tpz.mission.id.windurst.VAIN and player:getCharVar("MissionStatus") == 1 then
+    elseif player:getCurrentMission(WINDURST) == tpz.mission.id.windurst.VAIN and player:getMissionStatus(player:getNation()) == 1 then
         cs = 11
     end
 

LandSandBoat/server

scripts/zones/Chamber_of_Oracles/npcs/Pedestal_of_Water.lua
@@ -15,12 +15,12 @@ entity.onTrade = function(player, npc, trade)
 end
 
 entity.onTrigger = function(player, npc)
-    local ZilartStatus = player:getCharVar("ZilartStatus")
+    local ZilartStatus = player:getMissionStatus(xi.mission.log_id.ZILART)
 
     if (player:getCurrentMission(ZILART) == xi.mission.id.zilart.THE_CHAMBER_OF_ORACLES) then
         if (player:hasKeyItem(xi.ki.WATER_FRAGMENT)) then
             player:delKeyItem(xi.ki.WATER_FRAGMENT)
-            player:setCharVar("ZilartStatus", ZilartStatus + 64)
+            player:setMissionStatus(xi.mission.log_id.ZILART, ZilartStatus + 64)
             player:messageSpecial(ID.text.YOU_PLACE_THE, xi.ki.WATER_FRAGMENT)
 
             if (ZilartStatus == 255) then
@@ -50,7 +50,7 @@ entity.onEventFinish = function(player, csid, option)
 
     if (csid == 1) then
         player:addTitle(xi.title.LIGHTWEAVER)
-        player:setCharVar("ZilartStatus", 0)
+        player:setMissionStatus(xi.mission.log_id.ZILART, 0)
         player:addKeyItem(xi.ki.PRISMATIC_FRAGMENT)
         player:messageSpecial(ID.text.KEYITEM_OBTAINED, xi.ki.PRISMATIC_FRAGMENT)
         player:completeMission(xi.mission.log_id.ZILART, xi.mission.id.zilart.THE_CHAMBER_OF_ORACLES)

scripts/zones/Port_Bastok/npcs/Hilda.lua
@@ -38,7 +38,7 @@ entity.onTrigger = function(player, npc)
 
     local WildcatBastok = player:getCharVar("WildcatBastok")
 
-    if (player:getCurrentMission(BASTOK) == xi.mission.id.bastok.ON_MY_WAY) and (player:getCharVar("MissionStatus") == 1) then
+    if (player:getCurrentMission(BASTOK) == xi.mission.id.bastok.ON_MY_WAY) and (player:getMissionStatus(player:getNation()) == 1) then
         player:startEvent(255)
     elseif (player:getQuestStatus(xi.quest.log_id.BASTOK, xi.quest.id.bastok.LURE_OF_THE_WILDCAT) == QUEST_ACCEPTED and not utils.mask.getBit(WildcatBastok, 3)) then
         player:startEvent(356)
@@ -97,7 +97,7 @@ entity.onEventFinish = function(player, csid, option)
             player:completeQuest(xi.quest.log_id.BASTOK, xi.quest.id.bastok.THE_USUAL)
         end
     elseif (csid == 255) then
-        player:setCharVar("MissionStatus", 2)
+        player:setMissionStatus(player:getNation(), 2)
     elseif (csid == 356) then
         player:setCharVar("WildcatBastok", utils.mask.setBit(player:getCharVar("WildcatBastok"), 3, true))
     end

scripts/zones/Windurst_Walls/npcs/_6n2.lua
@@ -24,7 +24,7 @@ entity.onTrigger = function(player, npc)
     local iCanHearARainbow = player:getQuestStatus(xi.quest.log_id.WINDURST, xi.quest.id.windurst.I_CAN_HEAR_A_RAINBOW)
 
     -- LOST FOR WORDS
-    if player:getCurrentMission(WINDURST) == xi.mission.id.windurst.LOST_FOR_WORDS and player:getCharVar("MissionStatus") == 5 then
+    if player:getCurrentMission(WINDURST) == xi.mission.id.windurst.LOST_FOR_WORDS and player:getMissionStatus(player:getNation()) == 5 then
         player:startEvent(337)
 
     -- KNOW ONE'S ONIONS
@@ -95,7 +95,7 @@ end
 entity.onEventFinish = function(player, csid, option)
     -- LOST FOR WORDS
     if csid == 337 then
-        player:setCharVar("MissionStatus", 6)
+        player:setMissionStatus(player:getNation(), 6)
 
     -- KNOW ONE'S ONIONS
     elseif csid == 288 then

scripts/zones/Xarcabard/Zone.lua
@@ -41,7 +41,7 @@ zone_object.onZoneIn = function(player, prevZone)
         cs = 9
     elseif UnbridledPassionCS == 3 then
         cs = 4
-    elseif player:getCurrentMission(WINDURST) == xi.mission.id.windurst.VAIN and player:getCharVar("MissionStatus") == 1 then
+    elseif player:getCurrentMission(WINDURST) == xi.mission.id.windurst.VAIN and player:getMissionStatus(player:getNation()) == 1 then
         cs = 11
     end
 