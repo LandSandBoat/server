2c1b920cc7fcc8b8ffa86259c0ad42e9d74d1ea2
TeoTwawki <arawn.annwn@yandex.com>
2021-05-02 14:33:02 -0400

    Fix commands/setmobflags.lua

--- scripts/commands/setmobflags.lua
+++ scripts/commands/setmobflags.lua
@@ -17,14 +17,16 @@ end
 
 function onTrigger(player, flags, target)
     -- validate flags
-    if (flags == nil) then
+    if flags ~= nil and tonumber(flags) ~= nil then
+        flags = tonumber(flags)
+    else
         error(player, "You must supply a flags value.")
         return
     end
 
     -- validate target
     local targ
-    if (target == nil) then
+    if target == nil then
         targ = player:getCursorTarget()
         if (targ == nil or not targ:isMob()) then
             error(player, "You must either supply a mob ID or target a mob.")
@@ -32,7 +34,7 @@ function onTrigger(player, flags, target)
         end
     else
         targ = GetMobByID(target)
-        if (targ == nil) then
+        if targ == nil then
             error(player, "Invalid mob ID.")
             return
         end
