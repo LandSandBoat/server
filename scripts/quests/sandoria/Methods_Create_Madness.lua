c59ea05dfbdbae3b5858f64bbaf8e0aa4c883465
wrenffxi <oliviapekoe@gmail.com>
2021-05-10 08:00:35 -0400

    QC and cleanup for quest "Methods Create Madness" * make start quest event once per zone, so it does not block other quests * check option before giving item * fix delQuest arguments * replace final quest section with DefaultAction * remove old quest code from NPCs

--- scripts/quests/sandoria/Methods_Create_Madness.lua
+++ scripts/quests/sandoria/Methods_Create_Madness.lua
@@ -3,11 +3,16 @@
 -- Balasiel !pos -136 -11 64 230
 -- qm1 !pos 107 0.7 -125.25 176
 -----------------------------------
-require("scripts/globals/items")
-require("scripts/globals/quests")
-require("scripts/globals/npc_util")
 require('scripts/globals/interaction/quest')
 require("scripts/globals/weaponskillids")
+require("scripts/globals/keyitems")
+require("scripts/globals/npc_util")
+require("scripts/globals/quests")
+require("scripts/globals/status")
+require("scripts/globals/items")
+-----------------------------------
+local southernSandOriaID = require("scripts/zones/Southern_San_dOria/IDs")
+local seaSerpentGrottoID = require("scripts/zones/Sea_Serpent_Grotto/IDs")
 -----------------------------------
 
 local quest = Quest:new(xi.quest.log_id.SANDORIA, xi.quest.id.sandoria.METHODS_CREATE_MADNESS)
@@ -17,7 +22,7 @@ quest.reward = {
 }
 
 quest.sections = {
-
+    -- Talk to Balasiel in Southern San d'Oria who will give you the Spear of Trials and the Weapon Training Guide.
     {
         check = function(player, status, vars)
             return status == QUEST_AVAILABLE and
@@ -29,13 +34,13 @@ quest.sections = {
         [xi.zone.SOUTHERN_SAN_DORIA] = {
             ['Balasiel'] = {
                 onTrigger = function(player, npc)
-                    return quest:progressEvent(8) -- start
+                    return quest:event(8):oncePerZone() -- start
                 end,
             },
 
             onEventFinish = {
                 [8] = function(player, csid, option, npc)
-                    if npcUtil.giveItem(player, xi.items.SPEAR_OF_TRIALS)and option == 1 then
+                    if option == 1 and npcUtil.giveItem(player, xi.items.SPEAR_OF_TRIALS) then
                         npcUtil.giveKeyItem(player, xi.keyItem.WEAPON_TRAINING_GUIDE)
                         quest:begin(player)
                     end
@@ -44,6 +49,12 @@ quest.sections = {
         },
     },
 
+    -- Perform Weapon Skills with the Spear of Trials on monsters that grant XP until the latent effect disappears.
+    -- Once you no longer receive the latent effect bonus, trade the Spear of Trials back to Balasiel, who will
+    -- take the weapon from you and give you a Map to the Annals of Truth and tell you to head to Sea Serpent Grotto.
+    -- Click the ??? to spawn the NM Water Leaper.
+    -- Once the NM is dead, reexamine the ??? to obtain the Annals of Truth.
+    -- Bring this back to Balasiel for your reward.
     {
         check = function(player, status, vars)
             return status == QUEST_ACCEPTED
@@ -62,8 +73,9 @@ quest.sections = {
                 end,
 
                 onTrade = function(player, npc, trade)
-                    local wsPoints = (trade:getItem(0):getWeaponskillPoints())
                     if npcUtil.tradeHasExactly(trade, xi.items.SPEAR_OF_TRIALS) then
+                        local wsPoints = trade:getItem(0):getWeaponskillPoints()
+
                         if wsPoints < 300 then
                             return quest:event(10) -- unfinished weapon
                         else
@@ -78,21 +90,23 @@ quest.sections = {
                     if option == 1 then
                         npcUtil.giveItem(player, xi.items.SPEAR_OF_TRIALS)
                     elseif option == 2 then
-                        player:delQuest(SANDORIA, METHODS_CREATE_MADNESS)
+                        player:delQuest(xi.quest.log_id.SANDORIA, xi.quest.id.sandoria.METHODS_CREATE_MADNESS)
                         player:delKeyItem(xi.ki.WEAPON_TRAINING_GUIDE)
                         player:delKeyItem(xi.ki.MAP_TO_THE_ANNALS_OF_TRUTH)
                     end
                 end,
+
                 [9] = function(player, csid, option, npc)
                     player:confirmTrade()
                     npcUtil.giveKeyItem(player, xi.ki.MAP_TO_THE_ANNALS_OF_TRUTH)
                 end,
+
                 [13] = function(player, csid, option, npc)
                     player:delKeyItem(xi.ki.MAP_TO_THE_ANNALS_OF_TRUTH)
                     player:delKeyItem(xi.ki.ANNALS_OF_TRUTH)
                     player:delKeyItem(xi.ki.WEAPON_TRAINING_GUIDE)
                     player:addLearnedWeaponskill(xi.ws_unlock.IMPULSE_DRIVE)
-                    player:messageSpecial(zones[player:getZoneID()].text.IMPULSE_DRIVE_LEARNED)
+                    player:messageSpecial(southernSandOriaID.text.IMPULSE_DRIVE_LEARNED)
                     quest:complete(player)
                 end,
             },
@@ -104,12 +118,17 @@ quest.sections = {
                     if player:getLocalVar('killed_wsnm') == 1 then
                         player:setLocalVar('killed_wsnm', 0)
                         player:addKeyItem(xi.ki.ANNALS_OF_TRUTH)
-                        return quest:messageSpecial(zones[player:getZoneID()].text.KEYITEM_OBTAINED, xi.ki.ANNALS_OF_TRUTH)
-                    elseif player:hasKeyItem(xi.ki.MAP_TO_THE_ANNALS_OF_TRUTH) and not player:hasKeyItem(xi.keyItem.ANNALS_OF_TRUTH) and npcUtil.popFromQM(player, npc, zones[player:getZoneID()].mob.WATER_LEAPER, {hide = 0}) then
-                        return quest:messageSpecial(zones[player:getZoneID()].text.SENSE_OMINOUS_PRESENCE)
+                        return quest:messageSpecial(seaSerpentGrottoID.text.KEYITEM_OBTAINED, xi.ki.ANNALS_OF_TRUTH)
+                    elseif
+                        player:hasKeyItem(xi.ki.MAP_TO_THE_ANNALS_OF_TRUTH) and
+                        not player:hasKeyItem(xi.keyItem.ANNALS_OF_TRUTH) and
+                        npcUtil.popFromQM(player, npc, seaSerpentGrottoID.mob.WATER_LEAPER, {hide = 0})
+                    then
+                        return quest:messageSpecial(seaSerpentGrottoID.text.SENSE_OMINOUS_PRESENCE)
                     end
                 end,
             },
+
             ['Water_Leaper'] = {
                 onMobDeath = function(mob, player, isKiller, firstCall)
                     player:setLocalVar('killed_wsnm', 1)
@@ -117,21 +136,6 @@ quest.sections = {
             },
         },
     },
-
-    {
-        check = function(player, status, vars)
-            return status >= QUEST_AVAILABLE
-        end,
-
-        [xi.zone.SEA_SERPENT_GROTTO] = {
-            ['qm1'] = {
-                onTrigger = function(player, npc)
-                    return quest:messageSpecial(zones[player:getZoneID()].text.NOTHING_OUT_OF_ORDINARY)
-                end,
-            },
-        },
-    },
 }
-
 
 return quest
---
+++ scripts/zones/Sea_Serpent_Grotto/DefaultActions.lua
@@ -0,0 +1,5 @@
+local ID = require("scripts/zones/Sea_Serpent_Grotto/IDs")
+
+return {
+    ['qm1'] = { messageSpecial = ID.text.NOTHING_OUT_OF_ORDINARY },
+}
