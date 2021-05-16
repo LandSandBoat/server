e5f45c0b3dfbac5090f4603227d6a88639563ab7
wrenffxi <oliviapekoe@gmail.com>
2021-05-16 11:48:23 -0400

    quest qc: hook line and sinker

--- scripts/zones/Lower_Jeuno/npcs/Omer.lua
+++ scripts/zones/Lower_Jeuno/npcs/Omer.lua
@@ -9,7 +9,6 @@ entity.onTrade = function(player, npc, trade)
 end
 
 entity.onTrigger = function(player, npc)
-    player:startEvent(206)
 end
 
 entity.onEventUpdate = function(player, csid, option)
