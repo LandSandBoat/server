62eb7989fb2417ec16123a15dda619100da98600
Kain <kain.centeno@catsangel.com>
2021-05-17 17:34:10 -0700

    Added missing Dhahah's npc file

---
+++ scripts/zones/Windurst_Woods/npcs/Dhahah.lua
@@ -0,0 +1,34 @@
+-----------------------------------
+-- Area: Windurst Woods
+--  NPC: Dhahah
+-----------------------------------
+local entity = {}
+
+local path =
+{
+12.309, 0.000, 69.636,
+19.116, 0.000, 87.066
+}
+
+entity.onSpawn = function(npc)
+    npc:initNpcAi()
+    npc:setPos(xi.path.first(path))
+end
+
+entity.onPath = function(npc)
+    xi.path.patrol(npc, path)
+end
+
+entity.onTrade = function(player, npc, trade)
+end
+
+entity.onTrigger = function(player, npc)
+end
+
+entity.onEventUpdate = function(player, csid, option)
+end
+
+entity.onEventFinish = function(player, csid, option)
+end
+
+return entity
