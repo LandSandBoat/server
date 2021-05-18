a94b0d4b6e85a011542dc441de944c44b3570148
claywar <claywar@beerlab.dev>
2021-05-18 09:57:27 -0400

    Interaction - Quest: Not Meant to Be

--- scripts/zones/Caedarva_Mire/mobs/Lamia_No27.lua
+++ scripts/zones/Caedarva_Mire/mobs/Lamia_No27.lua
@@ -3,14 +3,9 @@
 --   NM: Lamia No.27
 -- Note: Spawned during quest: "Not Meant to Be"
 -----------------------------------
-require("scripts/globals/quests")
------------------------------------
 local entity = {}
 
 entity.onMobDeath = function(mob, player, isKiller)
-    if player:getQuestStatus(xi.quest.log_id.AHT_URHGAN, xi.quest.id.ahtUrhgan.NOT_MEANT_TO_BE) == QUEST_ACCEPTED and player:getCharVar("notmeanttobeCS") == 3 and player:getCharVar("notmeanttobeLamia27Killed") < 1 then
-        player:setCharVar("notmeanttobeLamia27Killed", 1)
-    end
 end
 
 return entity
--- scripts/zones/Caedarva_Mire/mobs/Moshdahn.lua
+++ scripts/zones/Caedarva_Mire/mobs/Moshdahn.lua
@@ -4,14 +4,10 @@
 -- Note: Spawned during quest: "Not Meant to Be"
 -----------------------------------
 mixins = {require("scripts/mixins/families/qutrub")}
-require("scripts/globals/quests")
 -----------------------------------
 local entity = {}
 
 entity.onMobDeath = function(mob, player, isKiller)
-    if player:getQuestStatus(xi.quest.log_id.AHT_URHGAN, xi.quest.id.ahtUrhgan.NOT_MEANT_TO_BE) == QUEST_ACCEPTED and player:getCharVar("notmeanttobeCS") == 3 and player:getCharVar("notmeanttobeMoshdahnKilled") < 1 then
-        player:setCharVar("notmeanttobeMoshdahnKilled", 1)
-    end
 end
 
 return entity
