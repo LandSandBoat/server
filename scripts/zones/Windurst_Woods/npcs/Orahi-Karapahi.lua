d2c505d328ed0b45bd225a931fde46c10cd06ec5
Kain <kain.centeno@catsangel.com>
2021-05-17 17:15:06 -0700

    Added Orahi-Karapahi's pathing coordinates

--- scripts/zones/Windurst_Woods/npcs/Orahi-Karapahi.lua
+++ scripts/zones/Windurst_Woods/npcs/Orahi-Karapahi.lua
@@ -5,6 +5,21 @@
 -----------------------------------
 local entity = {}
 
+local path =
+{
+63.612, -4.250, 102.695,    -- TODO: wait at location for 1 seconds
+58.772, -4.250, 103.357     -- TODO: wait at location for 1 seconds
+}
+
+entity.onSpawn = function(npc)
+    npc:initNpcAi()
+    npc:setPos(xi.path.first(path))
+end
+
+entity.onPath = function(npc)
+    xi.path.patrol(npc, path)
+end
+
 entity.onTrade = function(player, npc, trade)
 end
 
