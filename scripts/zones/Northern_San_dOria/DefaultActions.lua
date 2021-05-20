4c2209ada1c7a72190e433f072c601498ab33c1f
claywar <claywar@beerlab.dev>
2021-05-20 12:43:09 -0400

    Interaction - Quest: Rosel the Armorer

--- scripts/zones/Northern_San_dOria/DefaultActions.lua
+++ scripts/zones/Northern_San_dOria/DefaultActions.lua
@@ -3,6 +3,7 @@ local ID = require("scripts/zones/Northern_San_dOria/IDs")
 return {
     ['Ailbeche']   = { event = 868 },
     ['Gilipese']   = { text = ID.text.GILIPESE_DIALOG },
+    ['Guilerme']   = { text = ID.text.GUILERME_DIALOG },
     ['Maurinne']   = { text = ID.text.MAURINNE_DIALOG },
     ['Miageau']    = { event = 517 },
     ['Nouveil']    = { event = 574 },
