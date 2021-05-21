083801999f171ed994fa31db90d2276d24dcf0d3
claywar <claywar@beerlab.dev>
2021-05-21 14:13:50 -0400

    Interaction - Quest: Black Tiger Skins

---
+++ scripts/quests/sandoria/Black_Tiger_Skins.lua
@@ -0,0 +1,105 @@
+-----------------------------------
+-- Black Tiger Skins
+-----------------------------------
+-- Log ID: 0, Quest ID: 31
+-- Hanaa Punaa : !pos -179.726 -8.8 27.574 230
+-----------------------------------
+require("scripts/globals/npc_util")
+require("scripts/globals/quests")
+require("scripts/globals/status")
+require("scripts/globals/titles")
+require("scripts/globals/zone")
+require("scripts/globals/interaction/quest")
+-----------------------------------
+
+local quest = Quest:new(xi.quest.log_id.SANDORIA, xi.quest.id.sandoria.BLACK_TIGER_SKINS)
+
+quest.reward =
+{
+    fame = 30,
+    fameArea = SANDORIA,
+    item = xi.items.TIGER_STOLE,
+    title = xi.title.CAT_SKINNER,
+}
+
+quest.sections =
+{
+    {
+        check = function(player, status, vars)
+            return status == QUEST_AVAILABLE and
+                player:getFameLevel(SANDORIA) >= 3 and
+                player:hasCompletedQuest(xi.quest.log_id.SANDORIA, xi.quest.id.sandoria.LIZARD_SKINS)
+        end,
+
+        [xi.zone.SOUTHERN_SAN_DORIA] = {
+            ['Hanaa_Punaa'] =
+            {
+                onTrigger = function(player, npc)
+                    local questProgress = quest:getVar(player, 'Prog')
+
+                    if questProgress == 0 then
+                        return quest:progressEvent(576)
+                    elseif questProgress == 1 then
+                        return quest:progressEvent(579)
+                    end
+                end,
+            },
+
+            onEventFinish =
+            {
+                [576] = function(player, csid, option, npc)
+                    if option == 0 then
+                        quest:begin(player)
+                    else
+                        quest:setVar(player, 'Prog', 1)
+                    end
+                end,
+
+                [579] = function(player, csid, option, npc)
+                    if option == 0 then
+                        quest:begin(player)
+                    end
+                end,                
+            },
+        },
+    },
+
+    {
+        check = function(player, status, vars)
+            return status == QUEST_ACCEPTED
+        end,
+
+        [xi.zone.SOUTHERN_SAN_DORIA] = {
+            ['Hanaa_Punaa'] =
+            {
+                onTrade = function(player, npc, trade)
+                    if npcUtil.tradeHasExactly(trade, {{ xi.items.TIGER_HIDE, 3 }}) then
+                        return quest:progressEvent(577)
+                    end
+                end,
+
+                onTrigger = quest:progressEvent(578),
+            },
+
+            onEventFinish =
+            {
+                [577] = function(player, csid, option, npc)
+                    player:confirmTrade()
+                    quest:complete(player)
+                end,
+            },
+        },
+    },
+
+    {
+        check = function(player, status, vars)
+            return status == QUEST_COMPLETED and player:getFameLevel(SANDORIA) >= 3
+        end,
+
+        [xi.zone.SOUTHERN_SAN_DORIA] = {
+            ['Hanaa_Punaa'] = quest:event(592):replaceDefault()
+        },
+    },
+}
+
+return quest
--- scripts/zones/Southern_San_dOria/npcs/Hanaa_Punaa.lua
+++ scripts/zones/Southern_San_dOria/npcs/Hanaa_Punaa.lua
@@ -1,63 +1,21 @@
 -----------------------------------
 -- Area: Southern San d'Oria
 --  NPC: Hanaa Punaa
--- Starts and Finishes: A Squire's Test, A Squire's Test II, A Knight's Test
--- !zone 230
------------------------------------
-local ID = require("scripts/zones/Southern_San_dOria/IDs")
-require("scripts/globals/quests")
-require("scripts/globals/titles")
+-- Starts and Finishes: The Seamstress, Lizard Skins, and Black Tiger Skins
+-- !pos -179.726 -8.8 27.574 230
 -----------------------------------
 local entity = {}
 
 entity.onTrade = function(player, npc, trade)
-    -- "Black Tiger Skins", Tiger Hide trade
-    if (player:getQuestStatus(xi.quest.log_id.SANDORIA, xi.quest.id.sandoria.BLACK_TIGER_SKINS) == QUEST_ACCEPTED) then
-        if (trade:hasItemQty(861, 3) and trade:getItemCount() == 3) then
-            player:startEvent(577)
-        end
-    end
 end
 
 entity.onTrigger = function(player, npc)
-    -- Checking Fame Level & Quest
-    local sanFame = player:getFameLevel(SANDORIA)
-    local blackTigerSkins = player:getQuestStatus(xi.quest.log_id.SANDORIA, xi.quest.id.sandoria.BLACK_TIGER_SKINS)
-
-    -- "Black Tiger Skins" Quest Dialogs
-    if (blackTigerSkins == QUEST_AVAILABLE and player:getCharVar("blkTigerSkin") == 1 and sanFame >= 3 and theSteamStress == QUEST_COMPLETED and lizardSkins == QUEST_COMPLETED) then
-        player:startEvent(579)
-    elseif (blackTigerSkins == QUEST_AVAILABLE and sanFame >= 3 and theSteamStress == QUEST_COMPLETED and lizardSkins == QUEST_COMPLETED) then
-        player:startEvent(576)
-        player:setCharVar("blkTigerSkin", 1)
-    elseif (blackTigerSkins == QUEST_ACCEPTED) then
-        player:startEvent(578)
-    elseif (blackTigerSkins == QUEST_COMPLETED) then
-        player:startEvent(592)
-    end
 end
 
 entity.onEventUpdate = function(player, csid, option)
 end
 
 entity.onEventFinish = function(player, csid, option)
-    -- "Black Tiger Skins" Quest
-    if ((csid == 576 or csid == 579) and option == 0) then
-        player:addQuest(xi.quest.log_id.SANDORIA, xi.quest.id.sandoria.BLACK_TIGER_SKINS)
-        player:setCharVar("blkTigerSkin", 0)
-    elseif (csid == 577) then
-        if (player:getFreeSlotsCount() == 0) then
-            player:messageSpecial(ID.text.ITEM_CANNOT_BE_OBTAINED, 13119) -- Tyger Stole
-        else
-            player:tradeComplete()
-            player:addTitle(xi.title.CAT_SKINNER)
-            player:addItem(13119)
-            player:messageSpecial(ID.text.ITEM_OBTAINED, 13119) -- Tyger Stole
-            player:addFame(SANDORIA, 30)
-            player:completeQuest(xi.quest.log_id.SANDORIA, xi.quest.id.sandoria.BLACK_TIGER_SKINS)
-        end
-    end
-
 end
 
 return entity
