b69c3dec76101b49c39c123eba3dd9fafe96224a
claywar <claywar@beerlab.dev>
2021-05-21 13:23:46 -0400

    Interaction - Quest: The Seamstress

---
+++ scripts/quests/sandoria/The_Seamstress.lua
@@ -0,0 +1,121 @@
+-----------------------------------
+-- The Seamstress
+-----------------------------------
+-- Log ID: 0, Quest ID: 5
+-- Hanaa Punaa : !pos -179.726 -8.8 27.574 230
+-----------------------------------
+require("scripts/globals/npc_util")
+require("scripts/globals/quests")
+require("scripts/globals/status")
+require("scripts/globals/titles")
+require("scripts/globals/zone")
+require("scripts/globals/interaction/quest")
+-----------------------------------
+
+local quest = Quest:new(xi.quest.log_id.SANDORIA, xi.quest.id.sandoria.THE_SEAMSTRESS)
+
+quest.reward =
+{
+    fame = 30,
+    fameArea = SANDORIA,
+    -- Repeatable Items handled within the Trigger:
+    -- item = xi.items.LEATHER_GLOVES,
+    -- title = xi.title.SILENCER_OF_THE_LAMBS,
+}
+
+quest.sections =
+{
+    {
+        check = function(player, status, vars)
+            return status == QUEST_AVAILABLE
+        end,
+
+        [xi.zone.SOUTHERN_SAN_DORIA] = {
+            ['Hanaa_Punaa'] =
+            {
+                onTrigger = function(player, npc)
+                    local questProgress = quest:getVar(player, 'Prog')
+
+                    if questProgress == 0 then
+                        return quest:progressEvent(528)
+                    elseif questProgress == 1 then
+                        return quest:progressEvent(531)
+                    end
+                end,
+            },
+
+            onEventFinish =
+            {
+                [528] = function(player, csid, option, npc)
+                    if option == 0 then
+                        quest:begin(player)
+                    else
+                        quest:setVar(player, 'Prog', 1)
+                    end
+                end,
+
+                [531] = function(player, csid, option, npc)
+                    if option == 0 then
+                        quest:begin(player)
+                    end
+                end,                
+            },
+        },
+    },
+
+    {
+        check = function(player, status, vars)
+            return status == QUEST_ACCEPTED
+        end,
+
+        [xi.zone.SOUTHERN_SAN_DORIA] = {
+            ['Hanaa_Punaa'] = quest:progressEvent(529),
+        },
+    },
+
+    -- These functions check the status of ~= QUEST_AVAILABLE to support repeating
+    -- the quest.  Does not have to be flagged again to complete an additional time.
+    {
+        check = function(player, status, vars)
+            return status ~= QUEST_AVAILABLE
+        end,
+
+        [xi.zone.SOUTHERN_SAN_DORIA] = {
+            ['Hanaa_Punaa'] =
+            {
+                onTrade = function(player, npc, trade)
+                    if npcUtil.tradeHasExactly(trade, {{ xi.items.SHEEPSKIN, 3 }}) then
+                        return quest:progressEvent(530)
+                    end
+                end,
+            },
+
+            onEventFinish =
+            {
+                [530] = function(player, csid, option, npc)
+                    if npcUtil.giveItem(player, xi.items.LEATHER_GLOVES) then
+                        player:confirmTrade()
+                        player:addTitle(xi.title.SILENCER_OF_THE_LAMBS)
+                        if not player:hasCompletedQuest(quest.areaId, quest.questId) then
+                            quest:complete(player)
+                        else
+                            player:addFame(SANDORIA, 5)
+                        end
+                    end
+                end,
+            },
+        },
+    },
+
+    {
+        check = function(player, status, vars)
+            return status == QUEST_COMPLETED and player:getFameLevel(SANDORIA) < 2
+        end,
+
+        [xi.zone.SOUTHERN_SAN_DORIA] = {
+            ['Hanaa_Punaa'] = quest:event(590):replaceDefault()
+        },
+    },
+}
+
+return quest
--- scripts/zones/Southern_San_dOria/npcs/Hanaa_Punaa.lua
+++ scripts/zones/Southern_San_dOria/npcs/Hanaa_Punaa.lua
@@ -11,14 +11,6 @@ require("scripts/globals/titles")
 local entity = {}
 
 entity.onTrade = function(player, npc, trade)
-
-    -- "The Seamstress" , x3 sheepskin trade
-    if (player:getQuestStatus(xi.quest.log_id.SANDORIA, xi.quest.id.sandoria.THE_SEAMSTRESS) ~= QUEST_AVAILABLE) then
-        if (trade:hasItemQty(505, 3) and trade:getItemCount() == 3) then
-            player:startEvent(530)
-        end
-    end
-
     -- "Black Tiger Skins", Tiger Hide trade
     if (player:getQuestStatus(xi.quest.log_id.SANDORIA, xi.quest.id.sandoria.BLACK_TIGER_SKINS) == QUEST_ACCEPTED) then
         if (trade:hasItemQty(861, 3) and trade:getItemCount() == 3) then
@@ -38,23 +30,11 @@ entity.onTrigger = function(player, npc)
 
     -- Checking Fame Level & Quest
     local sanFame = player:getFameLevel(SANDORIA)
-    local theSteamStress = player:getQuestStatus(xi.quest.log_id.SANDORIA, xi.quest.id.sandoria.THE_SEAMSTRESS)
     local lizardSkins = player:getQuestStatus(xi.quest.log_id.SANDORIA, xi.quest.id.sandoria.LIZARD_SKINS)
     local blackTigerSkins = player:getQuestStatus(xi.quest.log_id.SANDORIA, xi.quest.id.sandoria.BLACK_TIGER_SKINS)
 
-    -- "The Seamstress" Quest Status
-    if (theSteamStress == QUEST_AVAILABLE and player:getCharVar("theSeamStress") == 1) then
-        player:startEvent(531)
-    elseif (theSteamStress == QUEST_AVAILABLE) then
-        player:startEvent(528)
-        player:setCharVar("theSeamStress", 1)
-    elseif (theSteamStress == QUEST_ACCEPTED) then
-        player:startEvent(529)
-    elseif (theSteamStress == QUEST_COMPLETED and sanFame < 2) then
-        player:startEvent(590)
-
     -- "Lizard Skins" Quest Dialogs
-    elseif (lizardSkins == QUEST_AVAILABLE and player:getCharVar("lzdSkins") == 1 and sanFame >= 2 and theSteamStress == QUEST_COMPLETED) then
+    if (lizardSkins == QUEST_AVAILABLE and player:getCharVar("lzdSkins") == 1 and sanFame >= 2 and theSteamStress == QUEST_COMPLETED) then
         player:startEvent(562)
     elseif (lizardSkins == QUEST_AVAILABLE and sanFame >= 2 and theSteamStress == QUEST_COMPLETED) then
         player:startEvent(559)
@@ -66,7 +46,7 @@ entity.onTrigger = function(player, npc)
 
     -- "Black Tiger Skins" Quest Dialogs
     elseif (blackTigerSkins == QUEST_AVAILABLE and player:getCharVar("blkTigerSkin") == 1 and sanFame >= 3 and theSteamStress == QUEST_COMPLETED and lizardSkins == QUEST_COMPLETED) then
-        player:startEvent(579 )
+        player:startEvent(579)
     elseif (blackTigerSkins == QUEST_AVAILABLE and sanFame >= 3 and theSteamStress == QUEST_COMPLETED and lizardSkins == QUEST_COMPLETED) then
         player:startEvent(576)
         player:setCharVar("blkTigerSkin", 1)
@@ -82,29 +62,8 @@ entity.onEventUpdate = function(player, csid, option)
 end
 
 entity.onEventFinish = function(player, csid, option)
-
-    -- "The Seamstress" Quest
-    if ((csid == 528 or csid == 531) and option == 0) then
-        player:addQuest(xi.quest.log_id.SANDORIA, xi.quest.id.sandoria.THE_SEAMSTRESS)
-        player:setCharVar("theSeamStress", 0)
-    elseif (csid == 530) then
-        if (player:getFreeSlotsCount() == 0) then
-            player:messageSpecial(ID.text.ITEM_CANNOT_BE_OBTAINED, 12696) -- Leather Gloves
-        else
-            player:tradeComplete()
-            player:addTitle(xi.title.SILENCER_OF_THE_LAMBS)
-            player:addItem(12696)
-            player:messageSpecial(ID.text.ITEM_OBTAINED, 12696) -- Leather Gloves
-            if (player:getQuestStatus(xi.quest.log_id.SANDORIA, xi.quest.id.sandoria.THE_SEAMSTRESS) == QUEST_ACCEPTED) then
-                player:addFame(SANDORIA, 30)
-                player:completeQuest(xi.quest.log_id.SANDORIA, xi.quest.id.sandoria.THE_SEAMSTRESS)
-            else
-                player:addFame(SANDORIA, 5)
-            end
-        end
-
     -- "Liard Skins" Quest
-    elseif ((csid == 559 or csid == 562) and option == 0) then
+    if ((csid == 559 or csid == 562) and option == 0) then
         player:addQuest(xi.quest.log_id.SANDORIA, xi.quest.id.sandoria.LIZARD_SKINS)
         player:setCharVar("lzdSkins", 0)
     elseif (csid == 561) then
