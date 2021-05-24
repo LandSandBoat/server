225467c92845e31f934dddd883efeb7ae473e138
claywar <claywar@beerlab.dev>
2021-05-24 16:26:19 -0400

    Interaction - Quest: Drachenfall

---
+++ scripts/zones/Bastok_Mines/DefaultActions.lua
@@ -0,0 +1,6 @@
+local ID = require("scripts/zones/Port_San_dOria/IDs")
+
+return {
+    ['Black_Mud']     = { event = 100 },
+    ['Tall_Mountain'] = { event = 55 },
+}
--- scripts/zones/North_Gustaberg/npcs/Waterfall_Base.lua
+++ scripts/zones/North_Gustaberg/npcs/Waterfall_Base.lua
@@ -4,20 +4,12 @@
 -- Involved In Quest: Drachenfall
 -- !pos -217.594 98.644 464.722 106
 -----------------------------------
-local ID = require("scripts/zones/North_Gustaberg/IDs")
-require("scripts/globals/items")
-require("scripts/globals/npc_util")
------------------------------------
 local entity = {}
 
 entity.onTrade = function(player, npc, trade)
-    if npcUtil.tradeHas(trade, xi.items.BRASS_CANTEEN) and npcUtil.giveItem(player, xi.items.CANTEEN_OF_DRACHENFALL_WATER) then
-        player:confirmTrade()
-    end
 end
 
 entity.onTrigger = function(player, npc)
-    player:messageSpecial(ID.text.REACH_WATER_FROM_HERE)
 end
 
 entity.onEventUpdate = function(player, csid, option)
