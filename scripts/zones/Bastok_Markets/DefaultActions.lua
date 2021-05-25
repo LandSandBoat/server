7a2efc70963d051cc06692bc51e1b692c917aff0
claywar <claywar@beerlab.dev>
2021-05-25 12:43:59 -0400

    Interaction - Quest: Stamp Hunt

---
+++ scripts/zones/Bastok_Markets/DefaultActions.lua
@@ -0,0 +1,7 @@
+local ID = require("scripts/zones/Bastok_Markets/IDs")
+
+return {
+    ['Arawn'] = { event = 114 },
+    ['Emrys'] = { event = 115 },
+    ['Pavel'] = { event = 128 },
+}
---
+++ scripts/zones/Port_Bastok/DefaultActions.lua
@@ -0,0 +1,6 @@
+local ID = require("scripts/zones/Port_Bastok/IDs")
+
+return {
+    ['Ehrhard'] = { event = 47 },
+    ['Latifah'] = { event = 13 },
+}
