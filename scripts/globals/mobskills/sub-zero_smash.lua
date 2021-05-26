a35bc45f2caa25c5907dab0daae523de6557594e
MrHappyAsthma <mrhappyasthma@gmail.com>
2021-05-25 18:03:05 -0700

    Implement `Sub-Zero Smash` mob TP skill.
    The implementation is pretty straightforward since it's a single-hit physical attack.
    
    The potency of the paralysis is taken from: https://www.bg-wiki.com/ffxi/Sub-zero_Smash
    
    Also updated the range and shape on the .sql to match the documentation. It should be a cone shape of 5 yalms.

---
+++ scripts/globals/mobskills/sub-zero_smash.lua
@@ -0,0 +1,38 @@
+-----------------------------------
+--  Sub-Zero Smash
+--
+--  Description: Additional Effect: Paralysis. Damage varies with TP.
+--  Type: Physical (blunt)
+--  Range: Cone (5' yalms)
+--  Notes: This spell should be used anytime the target is behind the mob.
+--         However the online documentation suggests that this spell can
+--         still be used anytime. As a result, any other Ruszor spells
+--         should not trigger if the target is behind the mob.
+-----------------------------------
+require("scripts/globals/settings")
+require("scripts/globals/status")
+require("scripts/globals/monstertpmoves")
+-----------------------------------
+local mobskill_object = {}
+
+mobskill_object.onMobSkillCheck = function(target, mob, skill)
+    return 0
+end
+
+mobskill_object.onMobWeaponSkill = function(target, mob, skill)
+    local numhits = 1  -- Hits once, despite the animation looking like it hits twice.
+    local dmgmod = 1
+    local accmod = 1
+    local info = MobPhysicalMove(mob, target, skill, numhits, accmod, dmgmod, TP_DMG_VARIES, 1, 1.5, 2)
+    local dmg = MobFinalAdjustments(info.dmg, mob, skill, target, xi.attackType.PHYSICAL, xi.damageType.BLUNT, info.hitslanded)
+
+    local typeEffect = xi.effect.PARALYSIS
+    local duration = 180
+    local potency = 10
+    MobPhysicalStatusEffectMove(mob, target, skill, typeEffect, potency, 0, duration)
+
+    target:takeDamage(dmg, mob, xi.attackType.PHYSICAL, xi.damageType.BLUNT)
+    return dmg
+end
+
+return mobskill_object
