23484fb5c27e4f685805fa0f584a6f68bd9c6245
claywar <claywar@beerlab.dev>
2021-05-27 19:09:01 -0400

    Add getVE and getCE ability bindings to support existing scripts

--- src/map/lua/lua_ability.cpp
+++ src/map/lua/lua_ability.cpp
@@ -83,9 +83,19 @@ void CLuaAbility::setRecast(uint16 recastTime)
     m_PLuaAbility->setRecastTime(recastTime);
 }
 
+uint16 CLuaAbility::getCE()
+{
+    return m_PLuaAbility->getCE();
+}
+
 void CLuaAbility::setCE(uint16 ce)
 {
     m_PLuaAbility->setCE(ce);
+}
+
+uint16 CLuaAbility::getVE()
+{
+    return m_PLuaAbility->getVE();
 }
 
 void CLuaAbility::setVE(uint16 ve)
@@ -112,7 +122,9 @@ void CLuaAbility::Register()
     SOL_REGISTER("setMsg", CLuaAbility::setMsg);
     SOL_REGISTER("setAnimation", CLuaAbility::setAnimation);
     SOL_REGISTER("setRecast", CLuaAbility::setRecast);
+    SOL_REGISTER("getCE", CLuaAbility::getCE);
     SOL_REGISTER("setCE", CLuaAbility::setCE);
+    SOL_REGISTER("getVE", CLuaAbility::getVE);
     SOL_REGISTER("setVE", CLuaAbility::setVE);
     SOL_REGISTER("setRange", CLuaAbility::setRange);
 }
