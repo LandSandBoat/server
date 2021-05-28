948cd695fba99a6a74b440788fd832b5fa755f3b
Zach Toogood <zrtoogood@gmail.com>
2021-05-28 17:47:50 +0300

    [ci] Improve CI linting scripts

--- tools/ci/sql.sh
+++ tools/ci/sql.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
-cd sql
-for f in *.sql
-  do
-     echo -n "Importing $f into the database..."
-     mysql xidb_test -u root < $f && echo "Success"
-     CODE=$?
-     if [ $CODE -ne 0 ]; then exit $CODE; fi
-  done
-cd ..
+
+target=${1:-sql}
+printf "Target: %s\n" ${target}
+
+file_list=()
+
+if [[ -d ${target} ]]
+then
+    for f in ${target}/*.sql; do
+        file_list+=("${f}")
+    done
+else
+    file_list+=(${target})
+fi
+
+#for f in "${file_list[@]}"
+#do
+    # TODO: Use a mysql executable to validate the sql scripts
+#done
