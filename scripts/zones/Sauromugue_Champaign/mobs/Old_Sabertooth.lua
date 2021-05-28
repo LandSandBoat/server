c7afd9e5a115a30a916b20516bcac9f08ab3f596
Mark Klara <mrhappyasthma@gmail.com>
2021-05-28 15:26:45 -0700

    Remove `nil` check that was skipping death logic.
    I'm not sure the history of why this nil check was being used, but now the `player` is non-nil when you kill it so that the entire `onMobDeath` is essentially skipped.
    
    Tested the mission flow with the change on a new character.

--- scripts/zones/Sauromugue_Champaign/mobs/Old_Sabertooth.lua
+++ scripts/zones/Sauromugue_Champaign/mobs/Old_Sabertooth.lua
@@ -12,13 +12,10 @@ entity.onMobSpawn = function(mob)
 end
 
 entity.onMobDeath = function(mob, player, isKiller)
-    if player == nil then
-        local players = mob:getZone():getPlayers()
-
-        for i, person in pairs(players) do -- can't use the variable name "player" because it's already being used
-            if person:getQuestStatus(xi.quest.log_id.WINDURST, xi.quest.id.windurst.THE_FANGED_ONE) == QUEST_ACCEPTED and person:checkDistance(mob) < 32 then
-                person:setCharVar("TheFangedOneCS", 2)
-            end
+    local players = mob:getZone():getPlayers()
+    for i, person in pairs(players) do -- can't use the variable name "player" because it's already being used
+        if person:getQuestStatus(xi.quest.log_id.WINDURST, xi.quest.id.windurst.THE_FANGED_ONE) == QUEST_ACCEPTED and person:checkDistance(mob) < 32 then
+            person:setCharVar("TheFangedOneCS", 2)
         end
     end
 end
