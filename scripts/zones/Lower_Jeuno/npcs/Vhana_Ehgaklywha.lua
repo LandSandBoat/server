82e6e05aae73a51bac517f3539e52e800b61ef83
claywar <claywar@beerlab.dev>
2021-05-29 13:44:42 -0400

    Luacheck: Lower Jeuno non-complexity warnings

--- scripts/zones/Lower_Jeuno/npcs/Vhana_Ehgaklywha.lua
+++ scripts/zones/Lower_Jeuno/npcs/Vhana_Ehgaklywha.lua
@@ -4,7 +4,7 @@
 -- Lights lamps in Lower Jeuno if nobody accepts Community Service by 1AM.
 -- !pos -122.853 0.000 -195.605 245
 -----------------------------------
-require("scripts/zones/Lower_Jeuno/globals")
+local lowerJeunoGlobal = require("scripts/zones/Lower_Jeuno/globals")
 local ID = require("scripts/zones/Lower_Jeuno/IDs")
 require("scripts/globals/pathfind")
 require("scripts/globals/status")
@@ -22,7 +22,7 @@ entity.onTrigger = function(player, npc)
 end
 
 entity.onPath = function(npc)
-    if (npc:isFollowingPath()) then
+    if npc:isFollowingPath() then
 
         -- if vasha reaches the end node, halt and disappear her.
         -- do this at node 48 instead of 49 because isFollowingPath will be false by 49.
@@ -30,7 +30,7 @@ entity.onPath = function(npc)
         -- once a pathThrough begins, there doesn't seem to be a clean way to stop onPath
         -- from being called forever.
 
-        if (npc:atPoint(xi.path.get(LOWER_JEUNO.lampPath, 48))) then
+        if npc:atPoint(xi.path.get(lowerJeunoGlobal.lampPath, 48)) then
             npc:clearPath()
             npc:setStatus(2)
 
@@ -38,9 +38,9 @@ entity.onPath = function(npc)
         -- she reaches the lamps in reverse order of their npcIds, hence (12 - i).
 
         else
-            for i, v in ipairs(LOWER_JEUNO.lampPoints) do
-                local lampPos = xi.path.get(LOWER_JEUNO.lampPath, v)
-                if (npc:atPoint(lampPos)) then
+            for i, v in ipairs(lowerJeunoGlobal.lampPoints) do
+                local lampPos = xi.path.get(lowerJeunoGlobal.lampPath, v)
+                if npc:atPoint(lampPos) then
                     -- Vhana is at a lamp (she reaches them in reverse order)
                     local lampId = ID.npc.STREETLAMP_OFFSET + (12 - i)
                     GetNPCByID(lampId):setAnimation(xi.anim.OPEN_DOOR)
