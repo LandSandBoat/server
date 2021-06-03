5b7c12c077c22949998f9c9a09f10545151413c6
Era-Lusiphur <sf.chakan@gmail.com>
2021-06-03 05:09:12 -0400

    Make Fisherman's Belt usable
    Item script for Fisherman's Belt, which is usable once in place. Adds Fishing Imagery support with +2 skill for 2 hours, per wiki entry below.
    
    https://ffxiclopedia.fandom.com/wiki/Fisherman's_Belt

---
+++ scripts/globals/items/fishermans_belt.lua
@@ -0,0 +1,35 @@
+-----------------------------------
+-- ID: 15452
+-- Item: Fisherman's belt
+-- Enchantment: Fishing image support
+-- 2 hours, All Races
+-----------------------------------
+-- Enchantment: Fishing image support
+-- Duration: 2 hours
+-- Fishing Skill +2
+-----------------------------------
+require("scripts/globals/status")
+-----------------------------------
+local item_object = {}
+
+item_object.onItemCheck = function(target)
+    local result = 0
+    if (target:hasStatusEffect(xi.effect.FISHING_IMAGERY) == true) then
+        result = 242
+    end
+    return result
+end
+
+item_object.onItemUse = function(target)
+    target:addStatusEffect(xi.effect.FISHING_IMAGERY, 2, 0, 7200)
+end
+
+item_object.onEffectGain = function(target, effect)
+    target:addMod(xi.mod.FISH, 1)
+end
+
+item_object.onEffectLose = function(target, effect)
+    target:delMod(xi.mod.FISH, 1)
+end
+
+return item_object
