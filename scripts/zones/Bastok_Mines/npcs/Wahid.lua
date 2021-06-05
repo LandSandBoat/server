7f0cbff0bb9eed0d7b379df5d5f9e24ec97a9301
claywar <claywar@beerlab.dev>
2021-06-05 12:34:04 -0400

    Interaction - Quest: The Siren's Tear

--- scripts/zones/Bastok_Mines/npcs/Wahid.lua
+++ scripts/zones/Bastok_Mines/npcs/Wahid.lua
@@ -4,51 +4,18 @@
 -- Start & Finishes Quest: The Siren's Tear
 -- !pos 26.305 -1 -66.403 234
 -----------------------------------
-require("scripts/globals/quests")
-require("scripts/globals/titles")
-require("scripts/globals/settings")
-local ID = require("scripts/zones/Bastok_Mines/IDs")
------------------------------------
 local entity = {}
 
 entity.onTrade = function(player, npc, trade)
-    local SirensTear = player:getQuestStatus(xi.quest.log_id.BASTOK, xi.quest.id.bastok.THE_SIREN_S_TEAR)
-
-    if (SirensTear ~= QUEST_AVAILABLE) then
-        if (trade:hasItemQty(576, 1) and trade:getItemCount() == 1) then
-            player:startEvent(82)
-        end
-    end
 end
 
 entity.onTrigger = function(player, npc)
-    local SirensTear = player:getQuestStatus(xi.quest.log_id.BASTOK, xi.quest.id.bastok.THE_SIREN_S_TEAR)
-
-    if (SirensTear == QUEST_AVAILABLE) then
-        player:startEvent(81)
-    else
-        player:startEvent(28)
-    end
 end
 
 entity.onEventUpdate = function(player, csid, option)
-    -- printf("CSID2: %u", csid)
-    -- printf("RESULT2: %u", option)
 end
 
 entity.onEventFinish = function(player, csid, option)
-
-    if (csid == 81) then
-        player:addQuest(xi.quest.log_id.BASTOK, xi.quest.id.bastok.THE_SIREN_S_TEAR)
-    elseif (csid == 82) then
-        player:tradeComplete()
-        player:completeQuest(xi.quest.log_id.BASTOK, xi.quest.id.bastok.THE_SIREN_S_TEAR)
-        player:addFame(BASTOK, 120)
-        player:addGil(150*GIL_RATE)
-        player:messageSpecial(ID.text.GIL_OBTAINED, 150*GIL_RATE)
-        player:addTitle(xi.title.TEARJERKER)
-        player:setCharVar("SirensTear", 0)
-    end
 end
 
 return entity
