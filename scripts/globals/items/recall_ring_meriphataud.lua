1b356b68ae96ef4ce6cc08a3f90c3489c80440bb
Snuggles <19786824+Snugglesffxi@users.noreply.github.com>
2021-06-08 23:17:19 -0700

    add script files
    created scripts for 3x recall ring items.

---
+++ scripts/globals/items/recall_ring_meriphataud.lua
@@ -0,0 +1,24 @@
+-----------------------------------
+-- ID: 15843
+-- Recall ring: Meriphataud
+-- Enchantment: "Recall-Meriphataud"
+-----------------------------------
+require("scripts/globals/status")
+require("scripts/globals/keyitems")
+require("scripts/globals/teleports")
+-----------------------------------
+local item_object = {}
+
+item_object.onItemCheck = function(target)
+    local result = 0
+    if (target:hasKeyItem(xi.ki.MERIPHATAUD_GATE_CRYSTAL) == false) then
+        result = 445
+    end
+    return result
+end
+
+item_object.onItemUse = function(target)
+    target:addStatusEffectEx(xi.effect.TELEPORT, 0, xi.teleport.id.MERIPH, 0, 1)
+end
+
+return item_object
