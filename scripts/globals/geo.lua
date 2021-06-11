af20b61befd913f26fa560186eaf5e958b08aea8
Zach Toogood <zrtoogood@gmail.com>
2021-06-11 11:02:59 +0300

    [lua] Add duration handling to xi.geo.addAura

--- scripts/globals/geo.lua
+++ scripts/globals/geo.lua
@@ -9,6 +9,8 @@ xi = xi or {}
 xi.geo = xi.geo or {}
 
 -----------------------------------
+-- duration:   Length of the aura effect, not ticks of the aura's effect
+--             0 = does not wear off
 -- tickEffect: The effect being granted/imposed by the aura
 --             Use xi.effect table: xi.effect.GEO_POISON for example,
 --               but doesn't _need_ to be a GEO_ effect)
@@ -16,8 +18,8 @@ xi.geo = xi.geo or {}
 -- targetType: Target allies or enemies with:
 --             xi.auraTarget.ALLIES or xi.auraTarget.ENEMIES
 -----------------------------------
-xi.geo.addAura = function(target, tickEffect, tickPower, targetType)
-    target:addStatusEffectEx(xi.effect.COLURE_ACTIVE, xi.effect.COLURE_ACTIVE, 0, 3, 0, tickEffect, tickPower, targetType, xi.effectFlag.AURA)
+xi.geo.addAura = function(target, duration, tickEffect, tickPower, targetType)
+    target:addStatusEffectEx(xi.effect.COLURE_ACTIVE, xi.effect.COLURE_ACTIVE, 0, 3, duration, tickEffect, tickPower, targetType, xi.effectFlag.AURA)
 end
 
 -- TODO: After elements are aligned in the codebase, this should become:
@@ -28,7 +30,7 @@ xi.geo.spawnLuopan = function(player, target, modelID, tickEffect, tickPower, ta
     local luopan = player:getPet()
 
     -- Attach effect
-    xi.geo.addAura(luopan, tickEffect, tickPower, targetType)
+    xi.geo.addAura(luopan, 0, tickEffect, tickPower, targetType)
 
     -- Save the mp cost for use with Full Circle
     luopan:setLocalVar("MP_COST", spell:getMPCost())
