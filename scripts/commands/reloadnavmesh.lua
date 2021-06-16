95dba002478c1d07f7a7b835b9882b35328a70bf
Zach Toogood <zrtoogood@gmail.com>
2021-06-16 21:08:49 +0300

    Add better logging to !reloadnavmesh

--- scripts/commands/reloadnavmesh.lua
+++ scripts/commands/reloadnavmesh.lua
@@ -5,6 +5,14 @@
 --       to do anything to runtime pathing
 -----------------------------------
 
+cmdprops =
+{
+    permission = 5,
+    parameters = ""
+}
+
 function onTrigger(player)
-    player:getZone():reloadNavmesh()
+    local zone = player:getZone()
+    player:PrintToPlayer("Reloading Navmesh for " .. zone:getName())
+    zone:reloadNavmesh()
 end
