85648a98f9e11d52e8813b43a08fd5f99e627064
Zach Toogood <zrtoogood@gmail.com>
2021-06-18 15:39:29 +0300

    [RUN] Start job_utils/runefencer.lua

---
+++ scripts/globals/job_utils/runefencer.lua
@@ -0,0 +1,13 @@
+-----------------------------------
+-- Runefencer Job Utilities
+-----------------------------------
+require("scripts/globals/settings")
+require("scripts/globals/ability")
+require("scripts/globals/jobpoints")
+require("scripts/globals/status")
+require("scripts/globals/msg")
+-----------------------------------
+xi = xi or {}
+xi.job_utils = xi.job_utils or {}
+xi.job_utils.runefencer = xi.job_utils.runefencer or {}
+-----------------------------------
