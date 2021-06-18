1805645b4cdc7ae9fb41c6fdfba683dfb8e5d5a9
Zach Toogood <zrtoogood@gmail.com>
2021-06-18 16:27:22 +0300

    [lua] Cleanup GEO logic into geomancer job_util

--- scripts/globals/job_utils/geomancer.lua
+++ scripts/globals/job_utils/geomancer.lua
@@ -1,12 +1,27 @@
 -----------------------------------
--- Geomancer helpers
+-- Geomancer Job Utilities
 -----------------------------------
-require("scripts/globals/pets")
+require("scripts/globals/settings")
+require("scripts/globals/ability")
 require("scripts/globals/status")
+require("scripts/globals/msg")
+require("scripts/globals/weaponskills")
+require("scripts/globals/jobpoints")
+-----------------------------------
+xi = xi or {}
+xi.job_utils = xi.job_utils or {}
+xi.job_utils.geomancer = xi.job_utils.geomancer or {}
 -----------------------------------
 
-xi = xi or {}
-xi.geo = xi.geo or {}
+xi.job_utils.geomancer.geoOnMagicCastingCheck = function(caster, target, spell)
+    if caster:getPet() ~= nil then
+        return xi.msg.basic.LUOPAN_ALREADY_PLACED
+    elseif not caster:canUseMisc(xi.zoneMisc.PET) then
+        return xi.msg.basic.CANT_BE_USED_IN_AREA
+    else
+        return 0
+    end
+end
 
 -----------------------------------
 -- duration:   Length of the aura effect, not ticks of the aura's effect
@@ -18,19 +33,19 @@ xi.geo = xi.geo or {}
 -- targetType: Target allies or enemies with:
 --             xi.auraTarget.ALLIES or xi.auraTarget.ENEMIES
 -----------------------------------
-xi.geo.addAura = function(target, duration, tickEffect, tickPower, targetType)
+xi.job_utils.geomancer.addAura = function(target, duration, tickEffect, tickPower, targetType)
     target:addStatusEffectEx(xi.effect.COLURE_ACTIVE, xi.effect.COLURE_ACTIVE, 0, 3, duration, tickEffect, tickPower, targetType, xi.effectFlag.AURA)
 end
 
 -- TODO: After elements are aligned in the codebase, this should become:
--- xi.geo.spawnLuopan = function(player, target, spell, tickEffect, tickPower, targetType)
-xi.geo.spawnLuopan = function(player, target, modelID, tickEffect, tickPower, targetType, spell)
+-- xi.job_utils.geomancer.spawnLuopan = function(player, target, spell, tickEffect, tickPower, targetType)
+xi.job_utils.geomancer.spawnLuopan = function(player, target, modelID, tickEffect, tickPower, targetType, spell)
 
     xi.pet.spawnPet(player, xi.pet.id.LUOPAN)
     local luopan = player:getPet()
 
     -- Attach effect
-    xi.geo.addAura(luopan, 0, tickEffect, tickPower, targetType)
+    xi.job_utils.geomancer.addAura(luopan, 0, tickEffect, tickPower, targetType)
 
     -- Save the mp cost for use with Full Circle
     luopan:setLocalVar("MP_COST", spell:getMPCost())
