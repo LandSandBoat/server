0c61c22504221da78ad5992edafdf4cacf9e6c03
Zach Toogood <zrtoogood@gmail.com>
2021-06-20 10:40:24 +0300

    [Pankration] Add prohibited families list

--- scripts/globals/pankration.lua
+++ scripts/globals/pankration.lua
@@ -14,6 +14,79 @@ require("scripts/globals/utils")
 xi = xi or {}
 xi.pankration = xi.pankration or {}
 
+-- https://www.bg-wiki.com/ffxi/Category:Pankration#Prohibited_Monsters
+-- IDs from mob_family_system.sql
+-- We only store the family, not the ecosystem, so we have to lookup
+-- based on that
+xi.pankration.prohibitedFamilies = {
+    -- All Beastmen
+    25, -- Antica
+    59, -- Bugbear
+    126, 127, 128, 129, 130, -- Gigas
+    133, -- Goblin
+    171, -- Lamiae
+    176, 177, -- Mamool Ja
+    189, -- Orc
+    190, -- Orcish Warmachine
+    196, -- Poroggo
+    199, -- Qiqirn
+    200, 201, 202, -- Quadav
+    213, -- Sahagin
+    243, 244, -- Tonbery
+    246, -- Troll
+    270, -- Yagudo
+
+    -- All Empty
+    78, -- Craver
+    137, 138, -- Gorger
+    181, -- Receptacle
+    220, -- Seether
+    241, -- Thinker
+    255, -- Wanderer
+    256, -- Weeper
+
+    -- All Luminians
+    3, -- Aern
+    109, -- Euvhi
+    144, -- Hpemde
+    194, -- Phuabo
+    269, -- Xzomit
+    271, -- Yorva
+
+    -- All Luminions
+    122, 123, 124, -- Ghrah
+    272, -- Zdei
+
+    -- Avatar
+    34, -- Carbuncle
+    35, -- Diabolos
+    37, -- Garuda
+    38, -- Ifrit
+    40, -- Leviathan
+    43, -- Ramuh
+    44, -- Shiva
+    45, -- Titan
+
+    -- Biotechnological Weapons
+    54, -- Omega & Ultima
+
+    2, -- Adamantoise
+    28, 29, 30, 31, -- Automation
+    51, -- Behemoth
+    61, -- Cardian
+    62, -- Cerberus
+    -- Demon
+    -- Devrgr
+    115, 359, -- Fomor
+    140, 141, -- Hippogryph
+    163, 164, -- Hydra
+    -- Khamaira
+    193, -- Pet Wyvern
+    251, -- Uragnite
+    252, -- Vampyr
+    259, 260, 261, 262, 263, 264, 391, 392, 393, -- Wyrm
+}
+
 -- https://github.com/Windower/Lua/blob/dev/addons/libs/extdata.lua
 -- https://www.bg-wiki.com/ffxi/Feral_Skills_Guide_by_Shoya
 -- https://ffxiclopedia.fandom.com/wiki/Category:Feral_Skills
