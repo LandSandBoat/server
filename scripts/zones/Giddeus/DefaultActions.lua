d47c7013b72ea73a3e83ef95ac994f76005b170c
claywar <81713309+claywar@users.noreply.github.com>
2021-06-22 08:07:26 -0400

    Interaction - Mission: San d'Oria M2-3 (#310)
    * Interaction - Mission: San d'Oria M2-3
    
    
    Fix missing variable for Windurst2 Kupipi onTrigger
    
    
    Fix windurst player:startEvent to mission:event conventions
    
    
    Minor cleanup
    
    * QC: Bugfixes and Cleanup
    
    
    Delete previous active mission for 2-3 base before adding another
    
    * Move M2-3 guard text from global to mission script
    
    
    Missing requires
    
    * Additional cleanup and bugfixes
    
    
    Second leg cleanup
    
    * Fix mission area references to use xi global

---
+++ scripts/zones/Giddeus/DefaultActions.lua
@@ -0,0 +1,5 @@
+local ID = require("scripts/zones/Giddeus/IDs")
+
+return {
+    ['Uu_Zhoumo']     = { event = 44 },
+}
--- scripts/zones/Giddeus/npcs/Uu_Zhoumo.lua
+++ scripts/zones/Giddeus/npcs/Uu_Zhoumo.lua
@@ -10,27 +10,20 @@ require("scripts/globals/missions")
 local entity = {}
 
 entity.onTrade = function(player, npc, trade)
-
-    if (player:getCurrentMission(BASTOK) == xi.mission.id.bastok.THE_EMISSARY_WINDURST and trade:hasItemQty(16509, 1) and trade:getItemCount() == 1) then -- Trade Aspir Knife
+    if player:getCurrentMission(BASTOK) == xi.mission.id.bastok.THE_EMISSARY_WINDURST and trade:hasItemQty(16509, 1) and trade:getItemCount() == 1 then -- Trade Aspir Knife
         player:startEvent(41)
     end
-
 end
 
 entity.onTrigger = function(player, npc)
-
-    if (player:hasKeyItem(xi.ki.SHIELD_OFFERING)) then
-        player:startEvent(42)
-    elseif (player:getCurrentMission(BASTOK) == xi.mission.id.bastok.THE_EMISSARY_WINDURST) then
-        if (player:hasKeyItem(xi.ki.DULL_SWORD)) then
+    if player:getCurrentMission(BASTOK) == xi.mission.id.bastok.THE_EMISSARY_WINDURST then
+        if player:hasKeyItem(xi.ki.DULL_SWORD) then
             player:startEvent(40)
-        elseif (player:getMissionStatus(player:getNation()) == 5) then
+        elseif player:getMissionStatus(player:getNation()) == 5 then
             player:startEvent(43)
         else
             player:startEvent(44)
         end
-    else
-        player:startEvent(44)
     end
 end
 
@@ -38,16 +31,12 @@ entity.onEventUpdate = function(player, csid, option)
 end
 
 entity.onEventFinish = function(player, csid, option)
-
-    if (csid == 40) then
+    if csid == 40 then
         player:setMissionStatus(player:getNation(), 5)
         player:delKeyItem(xi.ki.DULL_SWORD)
-    elseif (csid == 41) then
+    elseif csid == 41 then
         player:tradeComplete()
         player:setMissionStatus(player:getNation(), 6)
-    elseif (csid == 42) then
-        player:setMissionStatus(player:getNation(), 6)
-        player:delKeyItem(xi.ki.SHIELD_OFFERING)
     end
 end
 
