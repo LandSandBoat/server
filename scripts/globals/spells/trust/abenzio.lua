d00a8bfca4c2950b897f98d3b5b0281c78a1ceae
Zach Toogood <zrtoogood@gmail.com>
2021-06-23 14:24:40 +0300

    [trust] Add missing information for Abenzio

--- scripts/globals/spells/trust/abenzio.lua
+++ scripts/globals/spells/trust/abenzio.lua
@@ -13,16 +13,31 @@ spell_object.onSpellCast = function(caster, target, spell)
     return xi.trust.spawn(caster, spell)
 end
 
+local isWearingMandragoraGear = function(player)
+    local wearingHead = player:getEquipID(xi.slot.HEAD) == 26705 or player:getEquipID(xi.slot.HEAD) == 26706 -- Mandragora Masque or Masque + 1
+    local wearingBody = player:getEquipID(xi.slot.BODY) == 27854 or player:getEquipID(xi.slot.BODY) == 27855 -- Mandragora Suit or Suit + 1
+    return wearingHead and wearingBody
+end
+
 spell_object.onMobSpawn = function(mob)
-    xi.trust.message(mob, xi.trust.message_offset.SPAWN)
+    local master = mob:getMaster()
+    if isWearingMandragoraGear(master) then
+        xi.trust.message(mob, xi.trust.message_offset.SPAWN)
+    end
 end
 
 spell_object.onMobDespawn = function(mob)
-    xi.trust.message(mob, xi.trust.message_offset.DESPAWN)
+    local master = mob:getMaster()
+    if isWearingMandragoraGear(master) then
+        xi.trust.message(mob, xi.trust.message_offset.DESPAWN)
+    end
 end
 
 spell_object.onMobDeath = function(mob)
-    xi.trust.message(mob, xi.trust.message_offset.DEATH)
+    local master = mob:getMaster()
+    if isWearingMandragoraGear(master) then
+        xi.trust.message(mob, xi.trust.message_offset.DEATH)
+    end
 end
 
 return spell_object
