a2eec78e3d181c4b3421b60ec455fa37e51c9fd8
Zach Toogood <zrtoogood@gmail.com>
2021-07-01 15:32:51 +0300

    [instance] Beef up Test Meeble

---
+++ scripts/globals/mobskills/thrashing_assault.lua
@@ -0,0 +1,28 @@
+-----------------------------------
+-- Thrashing Assault
+-- Physical damage + Defense Down. Absorbed by 4 shadows.
+-----------------------------------
+require("scripts/globals/monstertpmoves")
+require("scripts/globals/status")
+-----------------------------------
+local mobskill_object = {}
+
+mobskill_object.onMobSkillCheck = function(target, mob, skill)
+    return 0
+end
+
+mobskill_object.onMobWeaponSkill = function(target, mob, skill)
+    local numhits = 4
+    local accmod = 1
+    local dmgmod = 2.0
+    local info = MobPhysicalMove(mob, target, skill, numhits, accmod, dmgmod, TP_NO_EFFECT)
+    local dmg = MobFinalAdjustments(info.dmg, mob, skill, target, xi.attackType.PHYSICAL, xi.damageType.SLASHING, info.hitslanded)
+
+    MobPhysicalStatusEffectMove(mob, target, skill, xi.effect.STUN, 1, 0, 15)
+    MobPhysicalStatusEffectMove(mob, target, skill, xi.effect.DEFENSE_BOOST, 50, 0, 60)
+
+    target:takeDamage(dmg, mob, xi.attackType.PHYSICAL, xi.damageType.SLASHING)
+    return dmg
+end
+
+return mobskill_object
--- scripts/zones/Maquette_Abdhaljs-Legion_B/mobs/Bozzetto_Breadwinner.lua
+++ scripts/zones/Maquette_Abdhaljs-Legion_B/mobs/Bozzetto_Breadwinner.lua
@@ -1,15 +1,62 @@
 -----------------------------------
 -- Bozzetto Breadwinner (Meeble)
+-- https://ffxiclopedia.fandom.com/wiki/Ambuscade/Battlefield_Archive/Bozzetto_Breadwinner
+--
+-- Will Warble approximately once every 30 seconds.
+-- Every Warble move has roughly 2 seconds of readying time on Very Easy,
+-- shortening as the difficulty setting rises.
+-- Warbles have a chance to summon Urchins and activate the Housemaker,
+-- and will wake any nearby Urchins. Inflicting Silence on the Breadwinner
+-- may (but not reliably so) prevent Warbles from summoning Urchins or
+-- activating the Housemaker.[3].
+-----------------------------------
+mixins = {require("scripts/mixins/job_special")}
 -----------------------------------
 local entity = {}
 
+local doWarble
+doWarble = function(mob)
+    -- 3968 -- fire_meeble_warble
+    -- 3969 -- blizzard_meeble_warble
+    -- 3970 -- thunder_meeble_warble
+    -- 3971 -- stone_meeble_warble
+    -- 3972 -- water_meeble_warble
+    -- 3973 -- aero_meeble_warble
+    local randomOffset = math.random(0, 5)
+    print(randomOffset)
+    mob:useMobAbility(3968 + randomOffset)
+
+    -- Again in 30s
+    mob:timer(30000, function(mobArg)
+        doWarble(mobArg)
+    end)
+end
+
 entity.onMobInitialize = function(mob)
+    -- Clod Spikes: Become active during Hundred Fists. Deals earth damage and inflicts a potent
+    -- Slow effect on any player who physically strikes the Breadwinner.
+    -- The Slow effect is strong enough to overwite Haste II.
+    mob:addListener("TAKE_DAMAGE", "BREADWINNDER_TAKE_DAMAGE", function(mobArg, amount, attacker, attackType, damageType)
+        if mobArg:hasStatusEffect(xi.effect.HUNDRED_FISTS) then
+            attacker:addStatusEffect(xi.effect.SLOW, 30 * 100, 0, 60)
+        end
+    end)
 end
 
 entity.onMobSpawn = function(mob)
+    xi.mix.jobSpecial.config(mob, {
+        specials =
+        {
+            { id = xi.jsa.HUNDRED_FISTS, hpp = 50, duration = 45 },
+        },
+    })
 end
 
 entity.onMobEngaged = function(mob, target)
+    -- After 30s, start warbling
+    mob:timer(30000, function(mobArg)
+        doWarble(mobArg)
+    end)
 end
 
 entity.onMobFight = function(mob, target)
