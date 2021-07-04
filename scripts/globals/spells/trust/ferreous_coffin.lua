8efe4bbbe5c8073f62de9595a71cae09a31972db
Era-Lusiphur <sf.chakan@gmail.com>
2021-07-04 11:17:09 -0400

    add Ferreous Coffin initial pass

--- scripts/globals/spells/trust/ferreous_coffin.lua
+++ scripts/globals/spells/trust/ferreous_coffin.lua
@@ -15,6 +15,32 @@ end
 
 spell_object.onMobSpawn = function(mob)
     xi.trust.message(mob, xi.trust.message_offset.SPAWN)
+
+    -- TODO: Research and apply correct amounts of Double Attack and Auto-Refresh. Given some lower base
+    -- values for now.
+    -- TODO: Add Randgrith to skill list.
+    -- TODO: Raise family gambit not being respected. Resolve before merge.
+
+    mob:addSimpleGambit(ai.t.TOP_ENMITY, ai.c.STATUS, xi.effect.PARALYSIS, ai.r.MA, ai.s.SPECIFIC, xi.magic.spell.PARALYNA)
+    mob:addSimpleGambit(ai.t.TOP_ENMITY, ai.c.STATUS, xi.effect.CURSE_I, ai.r.MA, ai.s.SPECIFIC, xi.magic.spell.CURSNA)
+
+    mob:addSimpleGambit(ai.t.TOP_ENMITY, ai.c.STATUS_FLAG, xi.effectFlag.ERASABLE, ai.r.MA, ai.s.SPECIFIC, xi.magic.spell.ERASE)
+
+    mob:addSimpleGambit(ai.t.PARTY, ai.c.HPP_LT, 25, ai.r.MA, ai.s.HIGHEST, xi.magic.spellFamily.CURE)
+
+    mob:addSimpleGambit(ai.t.MELEE, ai.c.NOT_STATUS, xi.effect.HASTE, ai.r.MA, ai.s.HIGHEST, xi.magic.spellFamily.HASTE)
+
+    mob:addSimpleGambit(ai.t.PARTY, ai.c.STATUS, xi.effect.KO, ai.r.MA, ai.s.HIGHEST, xi.magic.spellFamily.RAISE)
+
+    mob:addListener("WEAPONSKILL_USE", "FERREOUS_WEAPONSKILL_USE", function(mobArg, target, wsid, tp, action)
+        if wsid == 1198 then -- Randgrith
+            --  Return to the dust whence you came! Randgrith!!!
+            xi.trust.message(mobArg, xi.trust.message_offset.SPECIAL_MOVE_1)
+        end
+    end)
+
+    mob:addMod(xi.mod.DOUBLE_ATTACK, 5)
+    mob:addMod(xi.mod.REFRESH, 1)
 end
 
 spell_object.onMobDespawn = function(mob)
