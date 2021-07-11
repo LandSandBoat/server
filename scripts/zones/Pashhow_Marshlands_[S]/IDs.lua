a608d9b6c2cebfc550eeb097d13dbc641374ce75
Zach Toogood <zrtoogood@gmail.com>
2021-07-11 08:38:16 +0300

    [client update] text ids O-Z

--- scripts/zones/Pashhow_Marshlands_[S]/IDs.lua
+++ scripts/zones/Pashhow_Marshlands_[S]/IDs.lua
@@ -20,16 +20,16 @@ zones[xi.zone.PASHHOW_MARSHLANDS_S] =
         LOGIN_NUMBER            = 7002, -- In celebration of your most recent login (login no. <number>), we have provided you with <number> points! You currently have a total of <number> points.
         FISHING_MESSAGE_OFFSET  = 7147, -- You can't fish here.
         ALREADY_OBTAINED_TELE   = 7692, -- You already possess the gate crystal for this telepoint.
-        COMMON_SENSE_SURVIVAL   = 9072, -- It appears that you have arrived at a new survival guide provided by the Servicemen's Mutual Aid Network. Common sense dictates that you should now be able to teleport here from similar tomes throughout the world.
+        VOIDWALKER_DESPAWN      = 7988, -- The monster fades before your eyes, a look of disappointment on its face.
         VOIDWALKER_NO_MOB       = 8035, -- The <keyitem> quivers ever so slightly, but emits no light. There seem to be no monsters in the area.
         VOIDWALKER_MOB_TOO_FAR  = 8036, -- The <keyitem> quivers ever so slightly and emits a faint light. There seem to be no monsters in the immediate vicinity.
         VOIDWALKER_MOB_HINT     = 8037, -- The <keyitem> resonates [feebly/softly/solidly/strongly/very strongly/furiously], sending a radiant beam of light lancing towards a spot roughly <number> [yalm/yalms] [east/southeast/south/southwest/west/northwest/north/northeast] of here.
         VOIDWALKER_SPAWN_MOB    = 8038, -- A monster materializes out of nowhere!
-        VOIDWALKER_DESPAWN      = 8039, -- The monster fades before your eyes, a look of disappointment on its face.
         VOIDWALKER_UPGRADE_KI_1 = 8040, -- The <keyitem> takes on a slightly deeper hue and becomes <keyitem>!
         VOIDWALKER_UPGRADE_KI_2 = 8041, -- The <keyitem> takes on a deeper, richer hue and becomes <keyitem>!
         VOIDWALKER_BREAK_KI     = 8042, -- The <keyitem> shatters into tiny fragments.
         VOIDWALKER_OBTAIN_KI    = 8043, -- Obtained key item: <keyitem>!
+        COMMON_SENSE_SURVIVAL   = 9072, -- It appears that you have arrived at a new survival guide provided by the Servicemen's Mutual Aid Network. Common sense dictates that you should now be able to teleport here from similar tomes throughout the world.
     },
     mob =
     {
--- scripts/zones/Qulun_Dome/IDs.lua
+++ scripts/zones/Qulun_Dome/IDs.lua
@@ -18,16 +18,16 @@ zones[xi.zone.QULUN_DOME] =
         CARRIED_OVER_POINTS                     = 7000, -- You have carried over <number> login point[/s].
         LOGIN_CAMPAIGN_UNDERWAY                 = 7001, -- The [/January/February/March/April/May/June/July/August/September/October/November/December] <number> Login Campaign is currently underway!<space>
         LOGIN_NUMBER                            = 7002, -- In celebration of your most recent login (login no. <number>), we have provided you with <number> points! You currently have a total of <number> points.
-        CONQUEST_BASE                           = 7050, -- Tallying conquest results...
-        IT_SEEMS_TO_BE_LOCKED_BY_POWERFUL_MAGIC = 7209, -- A door... It seems to be locked by powerful magic.
-        THE_3_ITEMS_GLOW_FAINTLY                = 7210, -- The <item>, <item>, and <item> glow faintly.
-        CANNOT_BE_OPENED_FROM_THIS_SIDE         = 7214, -- It cannot be opened from this side!
-        THE_MAGICITE_GLOWS_OMINOUSLY            = 7252, -- The magicite glows ominously.
-        YOU_FIND_NOTHING                        = 7253, -- You find nothing.
-        DIAMOND_QUADAV_ENGAGE                   = 7254, -- Gwa-ha-ha, puny peoples! Ou-ur king never forge-ets a gru-udge. He'll gri-ind you into pa-aste!
-        DIAMOND_QUADAV_DEATH                    = 7255, -- Glo-ory to the Adamantking!
-        QUADAV_KING_ENGAGE                      = 7256, -- Childre-en of Altana? I will ba-athe in your blood as I did at the Ba-attle of Jeuno!
-        QUADAV_KING_DEATH                       = 7257, -- I a-am fini-ished. Hear me, wa-arriors of the Quadav! The throne of the Adamantking and the line of Za'Dha pa-asses to my bro-other...
+        CONQUEST_BASE                           = 7053, -- Tallying conquest results...
+        IT_SEEMS_TO_BE_LOCKED_BY_POWERFUL_MAGIC = 7212, -- A door... It seems to be locked by powerful magic.
+        THE_3_ITEMS_GLOW_FAINTLY                = 7213, -- The <item>, <item>, and <item> glow faintly.
+        CANNOT_BE_OPENED_FROM_THIS_SIDE         = 7217, -- It cannot be opened from this side!
+        THE_MAGICITE_GLOWS_OMINOUSLY            = 7255, -- The magicite glows ominously.
+        YOU_FIND_NOTHING                        = 7256, -- You find nothing.
+        DIAMOND_QUADAV_ENGAGE                   = 7257, -- Gwa-ha-ha, puny peoples! Ou-ur king never forge-ets a gru-udge. He'll gri-ind you into pa-aste!
+        DIAMOND_QUADAV_DEATH                    = 7258, -- Glo-ory to the Adamantking!
+        QUADAV_KING_ENGAGE                      = 7259, -- Childre-en of Altana? I will ba-athe in your blood as I did at the Ba-attle of Jeuno!
+        QUADAV_KING_DEATH                       = 7260, -- I a-am fini-ished. Hear me, wa-arriors of the Quadav! The throne of the Adamantking and the line of Za'Dha pa-asses to my bro-other...
     },
     mob =
     {
