f42be64c684d398a8591cf8ce3be983e7711f56b
Tiberon <TiberonKalkaz@gmail.com>
2021-07-16 14:57:37 -0700

    Copies set/getMods commands to create set/getMobMods

---
+++ scripts/commands/getmobmod.lua
@@ -0,0 +1,51 @@
+---------------------------------------------------------------------------------------------------
+-- func: getmobmod <modID>
+-- desc: gets a mod by ID on the player or cursor target
+---------------------------------------------------------------------------------------------------
+require("scripts/globals/status")
+
+cmdprops =
+{
+    permission = 1,
+    parameters = "s"
+}
+
+function error(player, msg)
+    player:PrintToPlayer(msg)
+    player:PrintToPlayer("!getmod <modID>")
+end
+
+function onTrigger(player, id)
+    -- invert tpz.mobMod table
+    local modNameByNum = {}
+    for k, v in pairs(tpz.mobMod) do
+        modNameByNum[v]=k
+    end
+
+    -- validate modID
+    id = string.upper(id)
+    local modId = tonumber(id)
+    local modName = nil
+
+    if modId ~= nil then
+        if modNameByNum[modId] ~= nil then
+            modName = modNameByNum[modId]
+        end
+    elseif tpz.mobMod[id] ~= nil then
+        modId = tpz.mobMod[id]
+        modName = id
+    end
+    if modName == nil or modId == nil then
+        error(player, "Invalid modMobID.")
+        return
+    end
+
+    -- validate target
+    local effectTarget = player:getCursorTarget()
+    if effectTarget == nil or effectTarget:isMob() == false then
+        error(player, "Current target is not a MOB, which can not have mob mods.")
+        return
+    end
+
+    player:PrintToPlayer(string.format("%s's Mod %i (%s) is %i", effectTarget:getName(), modId, modName, effectTarget:getMobMod(modId)))
+end
