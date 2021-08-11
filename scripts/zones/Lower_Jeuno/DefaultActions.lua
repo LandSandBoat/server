fc40a9da1f2df0dba7fc1beff06245f741b2ff12
Kietsu3756 <Kietsu3756@gmail.com>
2021-08-11 04:31:49 -0400

    Add Bluffnix default action

--- scripts/zones/Lower_Jeuno/DefaultActions.lua
+++ scripts/zones/Lower_Jeuno/DefaultActions.lua
@@ -1,6 +1,7 @@
 local ID = require("scripts/zones/Lower_Jeuno/IDs")
 
 return {
+    ['Bluffnix']      = { event = 43, options = 81 },
     ['Faursel']       = { event = 10065 },
     ['Muckvix']       = { event = 15 },
     ['Omer']          = { event = 206 },
