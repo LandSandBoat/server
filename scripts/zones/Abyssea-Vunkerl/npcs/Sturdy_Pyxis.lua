4801af093cef93754f8c72c64aed4d3780204821
claywar <claywar@beerlab.dev>
2021-08-16 10:47:19 -0400

    [lua] Add Sturdy Pyxis NPC scripts to Abyssea zones

---
+++ scripts/zones/Abyssea-Vunkerl/npcs/Sturdy_Pyxis.lua
@@ -0,0 +1,27 @@
+-----------------------------------
+-- Area: Abyssea
+-- NPC: Sturdy Pyxis
+-----------------------------------
+require("scripts/globals/sturdypyxis")
+require("scripts/globals/status")
+-----------------------------------
+
+local entity = {}
+
+entity.onTrade = function(player,npc,trade)
+    xi.pyxis.onPyxisTrade(player,npc,trade)
+end
+
+entity.onTrigger = function(player,npc)
+    xi.pyxis.onPyxisTrigger(player,npc)
+end
+
+entity.onEventUpdate = function(player,csid,option,input)
+    xi.pyxis.onPyxisEventUpdate(player,csid,option,input)
+end
+
+entity.onEventFinish = function(player,csid,option,npc)
+    xi.pyxis.onPyxisEventFinish(player,csid,option,npc)
+end
+
+return entity
