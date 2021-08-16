508b6deb8f67a885e51e14133137013c297c8909
claywar <claywar@beerlab.dev>
2021-08-16 09:31:42 -0400

    [lua] Add Veridical Conflux NPC scripts to Abyssea zones

---
+++ scripts/zones/Abyssea-Konschtat/npcs/Veridical_Conflux_#02.lua
@@ -0,0 +1,25 @@
+-----------------------------------
+-- Area: Abyssea-Konschtat
+--  NPC: Veridical Conflux #02
+-- Aybssea Teleport NPC
+-----------------------------------
+require("scripts/globals/conflux")
+-----------------------------------
+local entity = {}
+
+entity.onTrade = function(player, npc, trade)
+end
+
+entity.onTrigger = function(player, npc)
+	xi.conflux.confluxOnTrigger(player, npc)
+end
+
+entity.onEventUpdate = function(player, csid, option)
+    xi.conflux.confluxEventUpdate(player, csid, option)
+end
+
+entity.onEventFinish = function(player, csid, option, npc)
+    xi.conflux.confluxEventFinish(player, csid, option, npc)
+end
+
+return entity
