afd7ddf7c7e4597daf16aacc3537293be440c37f
Zach Toogood <zrtoogood@gmail.com>
2021-08-29 16:06:25 +0300

    Add resistance calc

--- scripts/globals/job_utils/rune_fencer.lua
+++ scripts/globals/job_utils/rune_fencer.lua
@@ -12,14 +12,21 @@ xi.job_utils = xi.job_utils or {}
 xi.job_utils.rune_fencer = xi.job_utils.rune_fencer or {}
 -----------------------------------
 
-local getMaxRunesForLevel = function(target)
+local getRUNLevel = function(target)
     local level = 0
-    local maxRuneCount = 1
+
     if target:getMainJob() == xi.job.RUN then
         level = target:getMainLvl()
     else
         level = target:getSubLvl()
     end
+
+    return level
+end
+
+local getMaxRunesForLevel = function(target)
+    local level = getRUNLevel(target)
+    local maxRuneCount = 1
 
     if level > 35 and level < 65 then
         maxRuneCount = 2
@@ -32,6 +39,7 @@ end
 
 -- https://ffxiclopedia.fandom.com/wiki/Rune_Enchantment
 xi.job_utils.rune_fencer.runeEnchantment = function(player, target, ability, effect)
+    local level = getRUNLevel(target)
     local runeList = player:getMaxRune()
     local runeCount = runeList.count
     local maxRunesForLevel = getMaxRunesForLevel(player)
@@ -40,7 +48,12 @@ xi.job_utils.rune_fencer.runeEnchantment = function(player, target, ability, eff
         player:removeOldestRune()
     end
 
-    target:addStatusEffect(effect, 10, 0, 300)
+    -- Each rune gives this much resistance. These stack:
+    local resistance = math.floor((48 * level / 99) + 5.5)
+        -- TODO: + (2~10 Merits)
+        -- TODO: + (1~20 JP)
+
+    target:addStatusEffect(effect, resistance, 0, 300)
 
     return effect
 end
