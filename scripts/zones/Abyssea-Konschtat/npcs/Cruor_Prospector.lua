9e6ec4fcaf3f076961b1ffa2e47541ace1540915
claywar <claywar@beerlab.dev>
2021-08-29 10:11:02 -0400

    [lua] Add remaining Cruor Prospectors, fix Forbidden Keys for visions areas

--- scripts/zones/Abyssea-Konschtat/npcs/Cruor_Prospector.lua
+++ scripts/zones/Abyssea-Konschtat/npcs/Cruor_Prospector.lua
@@ -22,48 +22,44 @@ local prospectorItems =
 {
     [itemType.ITEM] =
     {-- Sel      Item                         Cost,  Qty
-        [ 1] = { xi.items.PERLE_SALADE,       4000,  1 },
-        [ 2] = { xi.items.PERLE_HAUBERK,      5000,  1 },
-        [ 3] = { xi.items.PERLE_MOUFLES,      3000,  1 },
-        [ 4] = { xi.items.PERLE_BRAYETTES,    3000,  1 },
-        [ 5] = { xi.items.PERLE_SOLLERETS,    3000,  1 },
-        [ 6] = { xi.items.AURORE_BERET,       4000,  1 },
-        [ 7] = { xi.items.AURORE_DOUBLET,     5000,  1 },
-        [ 8] = { xi.items.AURORE_GLOVES,      3000,  1 },
-        [ 9] = { xi.items.AURORE_BRAIS,       3000,  1 },
-        [10] = { xi.items.AURORE_GAITERS,     3000,  1 },
-        [11] = { xi.items.TEAL_CHAPEAU,       4000,  1 },
-        [12] = { xi.items.TEAL_SAIO,          5000,  1 },
-        [13] = { xi.items.TEAL_CUFFS,         3000,  1 },
-        [14] = { xi.items.TEAL_SLOPS,         3000,  1 },
-        [15] = { xi.items.TEAL_PIGACHES,      3000,  1 },
-        [16] = { xi.items.FORBIDDEN_KEY,       500,  1 },
-        [17] = { xi.items.FORBIDDEN_KEY,       500,  5 },
-        [18] = { xi.items.FORBIDDEN_KEY,       500, 10 },
-        [19] = { xi.items.FORBIDDEN_KEY,       500, 30 },
-        [20] = { xi.items.FORBIDDEN_KEY,       500, 50 },
-        [21] = { xi.items.SHADOW_THRONE,   2000000,  1 },
+        [ 1] = { xi.items.PERLE_SALADE,       4000 },
+        [ 2] = { xi.items.PERLE_HAUBERK,      5000 },
+        [ 3] = { xi.items.PERLE_MOUFLES,      3000 },
+        [ 4] = { xi.items.PERLE_BRAYETTES,    3000 },
+        [ 5] = { xi.items.PERLE_SOLLERETS,    3000 },
+        [ 6] = { xi.items.AURORE_BERET,       4000 },
+        [ 7] = { xi.items.AURORE_DOUBLET,     5000 },
+        [ 8] = { xi.items.AURORE_GLOVES,      3000 },
+        [ 9] = { xi.items.AURORE_BRAIS,       3000 },
+        [10] = { xi.items.AURORE_GAITERS,     3000 },
+        [11] = { xi.items.TEAL_CHAPEAU,       4000 },
+        [12] = { xi.items.TEAL_SAIO,          5000 },
+        [13] = { xi.items.TEAL_CUFFS,         3000 },
+        [14] = { xi.items.TEAL_SLOPS,         3000 },
+        [15] = { xi.items.TEAL_PIGACHES,      3000 },
+        [16] = { xi.items.FORBIDDEN_KEY,       500 },
+        [17] = { xi.items.SHADOW_THRONE,   2000000 },
     },
 
     [itemType.TEMP] =
     {-- Sel      Item                         Cost, Qty
-        [ 1] = { xi.items.LUCID_POTION_I,       80, 1 },
-        [ 2] = { xi.items.LUCID_ETHER_I,        80, 1 },
-        [ 3] = { xi.items.CATHOLICON,           80, 1 },
-        [ 4] = { xi.items.DUSTY_ELIXIR,        120, 1 },
-        [ 5] = { xi.items.CLEAR_SALVE_I,       120, 1 },
-        [ 6] = { xi.items.STALWARTS_TONIC,     150, 1 },
-        [ 7] = { xi.items.ASCETICS_TONIC,      150, 1 },
-        [ 8] = { xi.items.CHAMPIONS_TONIC,     150, 1 },
-        [ 9] = { xi.items.LUCID_POTION_II,     200, 1 },
-        [10] = { xi.items.LUCID_ETHER_II,      200, 1 },
-        [11] = { xi.items.LUCID_ELIXIR_I,      300, 1 },
-        [12] = { xi.items.HEALING_POWDER,      300, 1 },
-        [13] = { xi.items.MANA_POWDER,         300, 1 },
-        [14] = { xi.items.HEALING_SALVE_I,     300, 1 },
-        [15] = { xi.items.VICARS_DRINK,        300, 1 },
-        [16] = { xi.items.CLEAR_SALVE_II,      300, 1 },
-        [17] = { xi.items.PRIMEVAL_BREW,   2000000, 1 },
+        [ 1] = { xi.items.LUCID_POTION_I,       80 },
+        [ 2] = { xi.items.LUCID_ETHER_I,        80 },
+        [ 3] = { xi.items.CATHOLICON,           80 },
+        [ 4] = { xi.items.DUSTY_ELIXIR,        120 },
+        [ 5] = { xi.items.CLEAR_SALVE_I,       120 },
+        [ 6] = { xi.items.STALWARTS_TONIC,     150 },
+        [ 7] = { xi.items.ASCETICS_TONIC,      150 },
+        [ 8] = { xi.items.CHAMPIONS_TONIC,     150 },
+        [ 9] = { xi.items.LUCID_POTION_II,     200 },
+        [10] = { xi.items.LUCID_ETHER_II,      200 },
+        [11] = { xi.items.LUCID_ELIXIR_I,      300 },
+        [12] = { xi.items.HEALING_POWDER,      300 },
+        [13] = { xi.items.MANA_POWDER,         300 },
+        [14] = { xi.items.HEALING_SALVE_I,     300 },
+        [15] = { xi.items.VICARS_DRINK,        300 },
+        [16] = { xi.items.CLEAR_SALVE_II,      300 },
+        [17] = { xi.items.PRIMEVAL_BREW,   2000000 },
     },
 
     [itemType.KEYITEM] =
@@ -113,26 +109,27 @@ end
 
 entity.onEventFinish = function(player, csid, option)
     local itemCategory = bit.band(option, 0x07)
-    local itemSelected = bit.rshift(option, 16)
+    local itemSelected = bit.band(bit.rshift(option, 16), 0x1F)
     local cruorTotal = player:getCurrency("cruor")
 
     if itemCategory == itemType.ITEM then
         local itemData = prospectorItems[itemCategory][itemSelected]
-        local itemCost = itemData[2] * itemData[3]
+        local itemQty = itemData[1] ~= xi.items.FORBIDDEN_KEY and 1 or bit.rshift(option, 24)
+        local itemCost = itemData[2] * itemQty
 
         if
             itemCost <= cruorTotal and
-            npcUtil.giveItem(player, {{ itemData[1], itemData[3] }})
+            npcUtil.giveItem(player, {{ itemData[1], itemQty }})
         then
             player:delCurrency("cruor", itemCost)
         end
     elseif itemCategory == itemType.TEMP then
         local itemData = prospectorItems[itemCategory][itemSelected]
-        local itemCost = itemData[2] * itemData[3]
+        local itemCost = itemData[2]
 
         if
             itemCost <= cruorTotal and
-            npcUtil.giveTempItem(player, {{ itemData[1], itemData[3] }})
+            npcUtil.giveTempItem(player, {{ itemData[1], 1 }})
         then
             player:delCurrency("cruor", itemCost)
         end
