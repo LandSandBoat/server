653071794f96e5168a68c160e8a3fd0bd5259afb
59blargedy <59blargedy@gmail.com>
2021-09-10 10:40:51 -0400

    adding effects

--- scripts/globals/effects/round.lua
+++ scripts/globals/effects/round.lua
@@ -4,12 +4,14 @@
 local effect_object = {}
 
 effect_object.onEffectGain = function(target, effect)
+    target:addMod(xi.mod.CURSERES, effect:getPower())
 end
 
 effect_object.onEffectTick = function(target, effect)
 end
 
 effect_object.onEffectLose = function(target, effect)
+    target:delMod(xi.mod.CURSERES, effect:getPower())
 end
 
 return effect_object
