d65d8c5aa1fc376903be3c581b4d0b96193adfba
Zach Toogood <zrtoogood@gmail.com>
2021-09-21 09:28:24 +0300

    [trust] Basic plumbing for Monberaux

--- scripts/globals/mobskills.lua
+++ scripts/globals/mobskills.lua
@@ -889,18 +889,18 @@ xi.mobskills.mobBuffMove = function(mob, typeEffect, power, tick, duration)
     return xi.msg.basic.SKILL_NO_EFFECT
 end
 
-xi.mobskills.mobHealMove = function(target, heal)
+xi.mobskills.mobHealMove = function(target, healAmount)
     local mobHP = target:getHP()
     local mobMaxHP = target:getMaxHP()
 
-    if mobHP + heal > mobMaxHP then
-        heal = mobMaxHP - mobHP
+    if (mobHP + healAmount) > mobMaxHP then
+        healAmount = mobMaxHP - mobHP
     end
 
     target:wakeUp()
-    target:addHP(heal)
+    target:addHP(healAmount)
 
-    return heal
+    return healAmount
 end
 
 xi.mobskills.fSTR = function(atk_str, def_vit)
---
+++ scripts/globals/mobskills/mix_panacea-1.lua
@@ -0,0 +1,54 @@
+-----------------------------------
+-- Mix: Panacea-1 - Removes anything a Panacea can remove.
+-----------------------------------
+require("scripts/globals/status")
+require("scripts/globals/mobskills")
+-----------------------------------
+local mobskillObject = {}
+
+mobskillObject.onMobSkillCheck = function(target, mob, skill)
+    return 0
+end
+
+local statii =
+{
+    xi.effect.PARALYSIS,
+    xi.effect.BIND,
+    xi.effect.WEIGHT,
+    xi.effect.ADDLE,
+    xi.effect.BURN,
+    xi.effect.FROST,
+    xi.effect.CHOKE,
+    xi.effect.RASP,
+    xi.effect.SHOCK,
+    xi.effect.DROWN,
+    xi.effect.DIA,
+    xi.effect.BIO,
+    xi.effect.STR_DOWN,
+    xi.effect.DEX_DOWN,
+    xi.effect.VIT_DOWN,
+    xi.effect.AGI_DOWN,
+    xi.effect.INT_DOWN,
+    xi.effect.MND_DOWN,
+    xi.effect.CHR_DOWN,
+    xi.effect.MAX_HP_DOWN,
+    xi.effect.MAX_MP_DOWN,
+    xi.effect.ATTACK_DOWN,
+    xi.effect.EVASION_DOWN,
+    xi.effect.DEFENSE_DOWN,
+    xi.effect.MAGIC_DEF_DOWN,
+    xi.effect.INHIBIT_TP,
+    xi.effect.MAGIC_ACC_DOWN,
+    xi.effect.MAGIC_ATK_DOWN,
+}
+
+mobskillObject.onMobWeaponSkill = function(target, mob, skill)
+    for _, effect in pairs(statii) do
+        target:delStatusEffect(effect)
+    end
+
+    skill:setMsg(xi.msg.basic.SKILL_ERASE)
+    return 0
+end
+
+return mobskillObject
