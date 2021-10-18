45155a0b9b0f0457593af9ad22db28e11b521485
David Norris <Knowone134@gmail.com>
2021-10-18 13:29:59 -0500

    be able to spawn mobs in instances

--- scripts/commands/spawnmob.lua
+++ scripts/commands/spawnmob.lua
@@ -2,6 +2,8 @@
 -- func: spawnmob
 -- desc: Spawns a mob.
 -----------------------------------
+require("scripts/globals/zone")
+-----------------------------------
 
 cmdprops =
 {
@@ -15,30 +17,55 @@ function error(player, msg)
 end
 
 function onTrigger(player, mobId, despawntime, respawntime)
+    local zone = player:getZone()
+    if zone:getType() == dsp.zoneType.INSTANCED then
+        local instance = player:getInstance()
 
-    -- validate mobId
-    if (mobId == nil) then
-        error(player, "You must provide a mob ID.")
-        return
-    end
-    local targ = GetMobByID(mobId)
-    if (targ == nil) then
-        error(player, "Invalid mob ID.")
-        return
-    end
+        -- validate mobId
+        if mobId == nil then
+            error(player, "You must provide a mob ID.");
+            return;
+        end
+        if GetMobByID(mobId, instance) == nil then
+            error(player, "Invalid mobID")
+            return
+        end
+        if GetMobByID(mobId, instance):isSpawned() then
+            error(player, "Mob is already spawned")
+            return
+        end
+        SpawnMob(mobId, instance)
+        player:PrintToPlayer(string.format("Spawned %s %s in %s.", GetMobByID(mobId, instance):getName(), mobId, instance));
+    else
+        -- validate mobId
+        if mobId == nil then
+            error(player, "You must provide a mob ID.")
+            return
+        end
+        local targ = GetMobByID(mobId)
+        if targ == nil then
+            error(player, "Invalid mob ID.")
+            return
+        end
 
-    -- validate despawntime
-    if (despawntime ~= nil and despawntime < 0) then
-        error(player, "Invalid despawn time.")
-        return
-    end
+        if targ:isSpawned() then
+            error(player, "Mob is already spawned")
+            return
+        end
 
-    -- validate respawntime
-    if (respawntime ~= nil and respawntime < 0) then
-        error(player, "Invalid respawn time.")
-        return
-    end
+        -- validate despawntime
+        if despawntime ~= nil and despawntime < 0 then
+            error(player, "Invalid despawn time.")
+            return
+        end
 
-    SpawnMob( targ:getID(), despawntime, respawntime )
-    player:PrintToPlayer( string.format("Spawned %s %s.", targ:getName(), targ:getID()) )
+        -- validate respawntime
+        if respawntime ~= nil and respawntime < 0 then
+            error(player, "Invalid respawn time.")
+            return
+        end
+
+        SpawnMob(targ:getID(), despawntime, respawntime)
+        player:PrintToPlayer(string.format("Spawned %s %s.", targ:getName(), mobId))
+    end
 end
