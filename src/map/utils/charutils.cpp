ab54543ba3bd6be577936ddbda3ae693cb501168
Zach Toogood <zrtoogood@gmail.com>
2021-10-19 20:17:42 +0300

    [core] Add details to charutils::AvatarPerpetuationReduction

--- src/map/utils/charutils.cpp
+++ src/map/utils/charutils.cpp
@@ -5136,13 +5136,21 @@ namespace charutils
 
     uint16 AvatarPerpetuationReduction(CCharEntity* PChar)
     {
-        auto*   PPet           = static_cast<CPetEntity*>(PChar->PPet);
-        ELEMENT petElement     = static_cast<ELEMENT>(PPet->m_Element) - 1;
-        ELEMENT dayElement     = battleutils::GetDayElement();
-        WEATHER weather        = battleutils::GetWeather(PChar, false);
-        int16 perpReduction    = PChar->getMod(Mod::PERPETUATION_REDUCTION);
-        int16 dayReduction     = PChar->getMod(Mod::DAY_REDUCTION);
-        int16 weatherReduction = PChar->getMod(Mod::WEATHER_REDUCTION);
+        // REMEMBER:
+        // Elements start at 0 and the 0th element is ELEMENT_NONE
+        // Weather starts at 0 and the 0th weather is WEATHER_NONE
+        // Days start at 0 and the 0th day is Firesday
+        // Affinity starts at 0 and the 0th affinity is FIRE_AFFINITY
+        // petElement and petElementIdx exist to bridge these gaps here!
+
+        auto*   PPet             = static_cast<CPetEntity*>(PChar->PPet);
+        ELEMENT petElement       = static_cast<ELEMENT>(PPet->m_Element);
+        uint8   petElementIdx    = static_cast<uint8>(petElement) - 1;
+        ELEMENT dayElement       = battleutils::GetDayElement();
+        WEATHER weather          = battleutils::GetWeather(PChar, false);
+        int16   perpReduction    = PChar->getMod(Mod::PERPETUATION_REDUCTION);
+        int16   dayReduction     = PChar->getMod(Mod::DAY_REDUCTION);
+        int16   weatherReduction = PChar->getMod(Mod::WEATHER_REDUCTION);
 
         static const Mod strong[8] = { Mod::FIRE_AFFINITY_PERP, Mod::ICE_AFFINITY_PERP, Mod::WIND_AFFINITY_PERP, Mod::EARTH_AFFINITY_PERP,
                                        Mod::THUNDER_AFFINITY_PERP, Mod::WATER_AFFINITY_PERP, Mod::LIGHT_AFFINITY_PERP, Mod::DARK_AFFINITY_PERP };
@@ -5151,16 +5159,15 @@ namespace charutils
                                                   WEATHER_THUNDER, WEATHER_RAIN, WEATHER_AURORAS, WEATHER_GLOOM };
 
         // If you wear a fire staff, you have +2 perp affinity reduction for fire, but -2 for ice as mods.
-        perpReduction += PChar->getMod(strong[petElement]);
+        perpReduction += PChar->getMod(strong[petElementIdx]);
 
-        // +1 to petElement to account for ELEMENT_NONE not being able to index into dayElement
-        if (dayElement == petElement + 1)
+        // Compare day element and pet element (both ELEMENT, both 0-based)
+        if (dayElement == petElement)
         {
             perpReduction += dayReduction;
         }
 
-        // TODO: What is this?
-        if (weather == weatherStrong[petElement] || weather == weatherStrong[petElement] + 1)
+        if (weather == weatherStrong[petElementIdx] || weather == weatherStrong[petElementIdx] + 1)
         {
             perpReduction += weatherReduction;
         }
