dd4a711b72cef09365a4e0437c5b9f4185ea1ebd
claywar <claywar@beerlab.dev>
2021-10-23 17:48:13 -0400

    First pass for reducing Leepe-Hoppe complexity, removed M7-2 conditions

--- scripts/zones/Windurst_Waters/npcs/Leepe-Hoppe.lua
+++ scripts/zones/Windurst_Waters/npcs/Leepe-Hoppe.lua
@@ -14,6 +14,46 @@ local ID = require("scripts/zones/Windurst_Waters/IDs")
 -----------------------------------
 local entity = {}
 
+local avatarKeyItems =
+{
+    xi.ki.WHISPER_OF_FLAMES,
+    xi.ki.WHISPER_OF_TREMORS,
+    xi.ki.WHISPER_OF_TIDES,
+    xi.ki.WHISPER_OF_GALES,
+    xi.ki.WHISPER_OF_FROST,
+    xi.ki.WHISPER_OF_STORMS,
+}
+
+local function hasAvatarWhispers(player)
+    for _, v in ipairs(avatarKeyItems) do
+        if not player:hasKeyItem(v) then
+            return false
+        end
+    end
+
+    return true
+end
+
+local function getFenrirRewardMask(player)
+    local rewardMask = 0
+
+    if player:findItem(18165) then rewardMask = rewardMask + 1;  end -- Fenrir's Stone
+    if player:findItem(13572) then rewardMask = rewardMask + 2;  end -- Fenrir's Cape
+    if player:findItem(13138) then rewardMask = rewardMask + 4;  end -- Fenrir's Torque
+    if player:findItem(13399) then rewardMask = rewardMask + 8;  end -- Fenrir's Earring
+    if player:findItem(1208)  then rewardMask = rewardMask + 16; end -- Ancient's Key
+    if player:hasSpell(297)   then rewardMask = rewardMask + 64; end  -- Pact
+
+    if
+        not player:hasKeyItem(xi.ki.TRAINERS_WHISTLE) or
+        player:hasKeyItem(xi.ki.FENRIR_WHISTLE)
+    then
+        rewardMask = rewardMask + 128
+    end -- Mount Pact
+
+    return rewardMask
+end
+
 entity.onTrade = function(player, npc, trade)
     if npcUtil.tradeHasExactly(trade, { 1696, 1697, 1698 }) -- Magicked Steel Ingot, Spruce Lumber, Extra-fine File
         and player:getQuestStatus(xi.quest.log_id.WINDURST, xi.quest.id.windurst.TUNING_IN) == QUEST_ACCEPTED
@@ -22,24 +62,13 @@ entity.onTrade = function(player, npc, trade)
     end
 end
 
-
 entity.onTrigger = function(player, npc)
     local moonlitPath = player:getQuestStatus(xi.quest.log_id.WINDURST, xi.quest.id.windurst.THE_MOONLIT_PATH)
     local tuningIn = player:getQuestStatus(xi.quest.log_id.WINDURST, xi.quest.id.windurst.TUNING_IN)
     local tuningOut = player:getQuestStatus(xi.quest.log_id.WINDURST, xi.quest.id.windurst.TUNING_OUT)
     local turmoil = player:getQuestStatus(xi.quest.log_id.WINDURST, xi.quest.id.windurst.TORAIMARAI_TURMOIL)
 
-    -- Check if we are on Windurst Mission 7-2
-    if (player:getCurrentMission(WINDURST) == xi.mission.id.windurst.AWAKENING_OF_THE_GODS and player:getMissionStatus(player:getNation()) == 0) then
-        player:startEvent(734)
-    elseif (player:getCurrentMission(WINDURST) == xi.mission.id.windurst.AWAKENING_OF_THE_GODS and player:getMissionStatus(player:getNation()) == 1) then
-        player:startEvent(735)
-    elseif (player:getCurrentMission(WINDURST) == xi.mission.id.windurst.AWAKENING_OF_THE_GODS and player:getMissionStatus(player:getNation()) == 2) then
-        player:startEvent(739)
-    elseif (player:getCurrentMission(WINDURST) == xi.mission.id.windurst.AWAKENING_OF_THE_GODS and player:getMissionStatus(player:getNation()) == 5 and player:hasKeyItem(xi.ki.BOOK_OF_THE_GODS)) then
-        player:startEvent(742)
-    -----------------------------------
-    elseif (player:getQuestStatus(xi.quest.log_id.WINDURST, xi.quest.id.windurst.FOOD_FOR_THOUGHT) == QUEST_ACCEPTED) then
+    if (player:getQuestStatus(xi.quest.log_id.WINDURST, xi.quest.id.windurst.FOOD_FOR_THOUGHT) == QUEST_ACCEPTED) then
         player:startEvent(311)
 
     -- Tuning In
@@ -48,7 +77,6 @@ entity.onTrigger = function(player, npc)
         and (player:getCurrentMission(COP) >= xi.mission.id.cop.DISTANT_BELIEFS or player:hasCompletedMission(xi.mission.log_id.COP, xi.mission.id.cop.THE_LAST_VERSE))
     then
         player:startEvent(884, 0, 1696, 1697, 1698) -- Magicked Steel Ingot, Spruce Lumber, Extra-fine File
-
 
     -- Tuning Out
     elseif tuningIn == QUEST_COMPLETED and tuningOut == QUEST_AVAILABLE then
@@ -74,13 +102,7 @@ entity.onTrigger = function(player, npc)
                 player:hasKeyItem(xi.ki.FENRIR_WHISTLE) then availRewards = availRewards + 128; end -- Mount Pact
 
             player:startEvent(846, 0, 13399, 1208, 1125, availRewards, 18165, 13572)
-        elseif (player:hasKeyItem(xi.ki.WHISPER_OF_FLAMES) and
-            player:hasKeyItem(xi.ki.WHISPER_OF_TREMORS) and
-            player:hasKeyItem(xi.ki.WHISPER_OF_TIDES) and
-            player:hasKeyItem(xi.ki.WHISPER_OF_GALES) and
-            player:hasKeyItem(xi.ki.WHISPER_OF_FROST) and
-            player:hasKeyItem(xi.ki.WHISPER_OF_STORMS)) then
-
+        elseif hasAvatarWhispers(player) then
             -- Collected the whispers
             player:startEvent(844, 0, 1125, 334)
         else -- Talked to after flag without the whispers
@@ -90,15 +112,7 @@ entity.onTrigger = function(player, npc)
         if (player:hasKeyItem(xi.ki.MOON_BAUBLE)) then -- Default text after acquiring moon bauble and before fighting Fenrir
             player:startEvent(845, 0, 1125, 334)
         elseif (player:hasKeyItem(xi.ki.WHISPER_OF_THE_MOON)) then -- Repeat turn-in
-            local availRewards = 0
-            if (player:hasItem(18165)) then availRewards = availRewards + 1; end -- Fenrir's Stone
-            if (player:hasItem(13572)) then availRewards = availRewards + 2; end -- Fenrir's Cape
-            if (player:hasItem(13138)) then availRewards = availRewards + 4; end -- Fenrir's Torque
-            if (player:hasItem(13399)) then availRewards = availRewards + 8; end -- Fenrir's Earring
-            if (player:hasItem(1208)) then availRewards = availRewards + 16; end -- Ancient's Key
-            if (player:hasSpell(297)) then availRewards = availRewards + 64; end -- Pact
-            if not player:hasKeyItem(xi.ki.TRAINERS_WHISTLE) or
-                player:hasKeyItem(xi.ki.FENRIR_WHISTLE) then availRewards = availRewards + 128; end -- Mount Pact
+            local availRewards = getFenrirRewardMask(player)
 
             player:startEvent(850, 0, 13399, 1208, 1125, availRewards, 18165, 13572)
         elseif (os.time() > player:getCharVar("MoonlitPath_date")) then --24 hours have passed, flag a new fight
@@ -119,7 +133,6 @@ entity.onEventUpdate = function(player, csid, option)
 end
 
 entity.onEventFinish = function(player, csid, option)
-
     -- Moonlit Path and Other Fenrir Stuff
     if (csid == 842 and option == 2) then
         player:addQuest(xi.quest.log_id.WINDURST, xi.quest.id.windurst.THE_MOONLIT_PATH)
@@ -138,7 +151,7 @@ entity.onEventFinish = function(player, csid, option)
         player:delQuest(xi.quest.log_id.OUTLANDS, xi.quest.id.outlands.TRIAL_BY_WIND)
         player:delQuest(xi.quest.log_id.SANDORIA, xi.quest.id.sandoria.TRIAL_BY_ICE)
         player:delQuest(xi.quest.log_id.OTHER_AREAS, xi.quest.id.otherAreas.TRIAL_BY_LIGHTNING)
-    elseif (csid == 846) then -- Turn-in event
+    elseif csid == 846 or csid == 850 then -- Turn-in event
         local reward = 0
         if (option == 1) then reward = 18165 -- Fenrir's Stone
         elseif (option == 2) then reward = 13572 -- Fenrir's Cape
@@ -156,68 +169,26 @@ entity.onEventFinish = function(player, csid, option)
             -- Pact as Mount
         end
 
-        if (reward ~= nil) then
-            player:addTitle(xi.title.HEIR_OF_THE_NEW_MOON)
-            player:delKeyItem(xi.ki.WHISPER_OF_THE_MOON)
-            player:setCharVar("MoonlitPath_date", getMidnight())
-            player:addFame(WINDURST, 30)
+        -- TODO: Reward is never nil since it is initialized; however, we should't run this block
+        -- if the player chooses an item, and they don't have space in their inventory.
+        player:addTitle(xi.title.HEIR_OF_THE_NEW_MOON)
+        player:delKeyItem(xi.ki.WHISPER_OF_THE_MOON)
+        player:setCharVar("MoonlitPath_date", getMidnight())
+        player:addFame(WINDURST, 30)
+
+        if player:getQuestStatus(xi.quest.log_id.WINDURST, xi.quest.id.windurst.THE_MOONLIT_PATH) == QUEST_ACCEPTED then
             player:completeQuest(xi.quest.log_id.WINDURST, xi.quest.id.windurst.THE_MOONLIT_PATH)
         end
 
-        if (player:getFreeSlotsCount() == 0 and reward ~= 0) then
-            player:messageSpecial(ID.text.ITEM_CANNOT_BE_OBTAINED, reward)
-        elseif (reward ~= 0) then
-            player:addItem(reward)
-            player:messageSpecial(ID.text.ITEM_OBTAINED, reward)
+        if reward ~= 0 then
+            npcUtil.giveItem(player, reward)
         end
 
-        if (player:getNation() == xi.nation.WINDURST and player:getRank(player:getNation()) == 10 and player:getQuestStatus(xi.quest.log_id.WINDURST, xi.quest.id.windurst.THE_PROMISE) == QUEST_COMPLETED) then
-            player:addKeyItem(xi.ki.DARK_MANA_ORB)
-            player:messageSpecial(ID.text.KEYITEM_OBTAINED, xi.ki.DARK_MANA_ORB)
+        if player:getNation() == xi.nation.WINDURST and player:getRank(player:getNation()) == 10 and player:getQuestStatus(xi.quest.log_id.WINDURST, xi.quest.id.windurst.THE_PROMISE) == QUEST_COMPLETED then
+            npcUtil.giveKeyItem(player, xi.ki.DARK_MANA_ORB)
         end
-    elseif (csid == 850) then -- Repeat turn-in event
-        local reward = 0
-        if (option == 1) then reward = 18165 -- Fenrir's Stone
-        elseif (option == 2) then reward = 13572 -- Fenrir's Cape
-        elseif (option == 3) then reward = 13138 -- Fenrir's Torque
-        elseif (option == 4) then reward = 13399 -- Fenrir's Earring
-        elseif (option == 5) then reward = 1208 -- Ancient's Key
-        elseif (option == 6) then
-            player:addGil(xi.settings.GIL_RATE*15000)
-            player:messageSpecial(ID.text.GIL_OBTAINED, xi.settings.GIL_RATE*15000) -- Gil
-        elseif (option == 7) then
-            player:addSpell(297) -- Pact
-        elseif (option == 8) then
-            player:addKeyItem(xi.ki.FENRIR_WHISTLE)
-            player:messageSpecial(ID.text.KEYITEM_OBTAINED, xi.ki.FENRIR_WHISTLE)
-            -- Pact as Mount
-        end
-
-        if (reward ~= nil) then
-            player:addTitle(xi.title.HEIR_OF_THE_NEW_MOON)
-            player:delKeyItem(xi.ki.WHISPER_OF_THE_MOON)
-            player:setCharVar("MoonlitPath_date", getMidnight())
-            player:addFame(WINDURST, 30)
-        end
-
-        if (player:getFreeSlotsCount() == 0 and reward ~= 0) then
-            player:messageSpecial(ID.text.ITEM_CANNOT_BE_OBTAINED, reward)
-        elseif (reward ~= 0) then
-            player:addItem(reward)
-            player:messageSpecial(ID.text.ITEM_OBTAINED, reward)
-        end
-
-        if (player:getNation() == xi.nation.WINDURST and player:getRank(player:getNation()) == 10 and player:getQuestStatus(xi.quest.log_id.WINDURST, xi.quest.id.windurst.THE_PROMISE) == QUEST_COMPLETED) then
-            player:addKeyItem(xi.ki.DARK_MANA_ORB)
-            player:messageSpecial(ID.text.KEYITEM_OBTAINED, xi.ki.DARK_MANA_ORB)
-        end
-    elseif (csid == 848) then
-        player:addKeyItem(xi.ki.MOON_BAUBLE)
-        player:messageSpecial(ID.text.KEYITEM_OBTAINED, xi.ki.MOON_BAUBLE)
-    elseif (csid == 734) then
-        player:setMissionStatus(player:getNation(), 1)
-    elseif (csid == 742) then
-        finishMissionTimeline(player, 3, csid, option)
+    elseif csid == 848 then
+        npcUtil.giveKeyItem(player, xi.ki.MOON_BAUBLE)
 
     -- Tuning In
     elseif csid == 884 then
