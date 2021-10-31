d853d3e7ff93f06da9fbb67ea7732b4ee383b3ab
David Norris <Knowone134@gmail.com>
2021-10-31 15:50:17 -0500

    missing entity

--- sql/instance_entities.sql
+++ sql/instance_entities.sql
@@ -432,6 +432,7 @@ INSERT INTO `instance_entities` VALUES (6900,17059855);
 INSERT INTO `instance_entities` VALUES (6900,17060014);
 INSERT INTO `instance_entities` VALUES (6900,17060015);
 INSERT INTO `instance_entities` VALUES (6900,17060138);
+INSERT INTO `instance_entities` VALUES (6900,17060142);
 INSERT INTO `instance_entities` VALUES (6900,17060146);
 INSERT INTO `instance_entities` VALUES (6900,17060147);
 
