4cf2cf94df539a9cf6ca740005d3bb02b0c9ddbc
59blargedy <59blargedy@gmail.com>
2021-11-12 13:59:14 -0500

    needed to avoid mp/tp drain causing damage

--- scripts/globals/mobs.lua
+++ scripts/globals/mobs.lua
@@ -477,7 +477,9 @@ xi.mob.onAddEffect = function(mob, target, damage, effect, params)
                 power = addBonusesAbility(mob, ae.ele, target, power, ae.bonusAbilityParams)
                 power = power * applyResistanceAddEffect(mob, target, ae.ele, 0)
                 power = adjustForTarget(target, power, ae.ele)
-                power = finalMagicNonSpellAdjustments(mob, target, ae.ele, power)
+                if ae.sub ~= dsp.subEffect.TP_DRAIN or ae.sub ~= dsp.subEffect.MP_DRAIN then
+                    power = finalMagicNonSpellAdjustments(mob, target, ae.ele, power)
+                end
 
                 -- target:PrintToPlayer(string.format("Adjusted Power: %f", power)) -- DEBUG
 
