31de0912bf635989f29f63e17eb111bb9799e512
RAIST5150 <73744368+RAIST5150@users.noreply.github.com>
2021-11-17 23:00:10 -0500

    Update upheaval.lua
    Fix SoA rule to use 0.03.  Had the math right in my head, didn't type it correctly when editing the files.

--- scripts/globals/weaponskills/upheaval.lua
+++ scripts/globals/weaponskills/upheaval.lua
@@ -29,7 +29,7 @@ weaponskill_object.onUseWeaponSkill = function(player, target, wsID, tp, primary
     params.atk100 = 1; params.atk200 = 1; params.atk300 = 1
 
     if (xi.settings.USE_ADOULIN_WEAPON_SKILL_CHANGES == true) then
-        params.vit_wsc = 0.7 + (player:getMerit(xi.merit.UPHEAVAL) * 0.3)
+        params.vit_wsc = 0.7 + (player:getMerit(xi.merit.UPHEAVAL) * 0.03)
     end
 
     local damage, criticalHit, tpHits, extraHits = doPhysicalWeaponskill(player, target, wsID, params, tp, action, primary, taChar)
