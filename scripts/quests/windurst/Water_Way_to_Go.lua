2f90e4070761dc53ea3a89e6f268b56082105813
claywar <claywar@beerlab.dev>
2021-11-18 11:55:21 -0500

    Interaction - Quest: Water Way to Go!

---
+++ scripts/quests/windurst/Water_Way_to_Go.lua
@@ -0,0 +1,129 @@
+-----------------------------------
+-- Water Way to Go
+-----------------------------------
+-- !addquest 2 16
+-- Ohbiru-Dohbiru : !pos 23 -5 -193 238
+-- Giddeus Spring : !pos -258 -2 -249 145
+-----------------------------------
+require('scripts/globals/interaction/quest')
+require('scripts/globals/items')
+require('scripts/globals/keyitems')
+require('scripts/globals/npc_util')
+require('scripts/globals/quests')
+require('scripts/globals/status')
+require('scripts/globals/zone')
+-----------------------------------
+
+local quest = Quest:new(xi.quest.log_id.WINDURST, xi.quest.id.windurst.WATER_WAY_TO_GO)
+
+quest.reward =
+{
+    fame = 40,
+    gil  = 900,
+}
+
+quest.sections =
+{
+    {
+        check = function(player, status, vars)
+            return status == QUEST_AVAILABLE and
+                player:hasCompletedQuest(xi.quest.log_id.WINDURST, xi.quest.id.windurst.OVERNIGHT_DELIVERY) and
+                player:getFameLevel(WINDURST) >= 3 and
+                not quest:getMustZone(player)
+        end,
+
+        [xi.zone.WINDURST_WATERS] =
+        {
+            ['Ohbiru-Dohbiru'] = quest:progressEvent(352, 0, xi.items.CANTEEN_OF_GIDDEUS_WATER),
+
+            onEventFinish =
+            {
+                [352] = function(player, csid, option, npc)
+                    if
+                        option == 0 and
+                        npcUtil.giveItem(player, xi.items.RHINOSTERY_CANTEEN)
+                    then
+                        quest:begin(player)
+                    end
+                end,
+            },
+        },
+    },
+
+    {
+        check = function(player, status, vars)
+            return status == QUEST_ACCEPTED
+        end,
+
+        [xi.zone.GIDDEUS] =
+        {
+            ['Giddeus_Spring'] =
+            {
+                onTrade = function(player, npc, trade)
+                    if npcUtil.tradeHasExactly(trade, xi.items.RHINOSTERY_CANTEEN) then
+                        return quest:progressEvent(55)
+                    end
+                end,
+            },
+
+            onEventFinish =
+            {
+                [55] = function(player, csid, option, npc)
+                    if npcUtil.giveItem(player, xi.items.CANTEEN_OF_GIDDEUS_WATER) then
+                        player:confirmTrade()
+                    end
+                end,
+            },
+        },
+
+        [xi.zone.WINDURST_WATERS] =
+        {
+            ['Ohbiru-Dohbiru'] =
+            {
+                onTrade = function(player, npc, trade)
+                    if npcUtil.tradeHasExactly(trade, xi.items.CANTEEN_OF_GIDDEUS_WATER) then
+                        return quest:progressEvent(355, 900)
+                    end
+                end,
+
+                onTrigger = function(player, npc)
+                    if
+                        not player:findItem(xi.items.RHINOSTERY_CANTEEN) and
+                        not player:findItem(xi.items.CANTEEN_OF_GIDDEUS_WATER)
+                    then
+                        return quest:progressEvent(354)
+                    else
+                        return quest:progressEvent(353)
+                    end
+                end,
+            },
+
+            onEventFinish =
+            {
+                [354] = function(player, csid, option, npc)
+                    npcUtil.giveItem(player, xi.items.RHINOSTERY_CANTEEN)
+                end,
+
+                [355] = function(player, csid, option, npc)
+                    quest:complete(player)
+                    quest:setMustZone(player)
+                    player:setLocalVar('Quest[2][17]mustZone', 1)
+                end,
+            },
+        },
+    },
+
+    {
+        check = function(player, status, vars)
+            return status == QUEST_COMPLETED and
+                quest:getMustZone(player)
+        end,
+
+        [xi.zone.WINDURST_WATERS] =
+        {
+            ['Ohbiru-Dohbiru'] = quest:progressEvent(356, 0, xi.items.CANTEEN_OF_GIDDEUS_WATER)
+        },
+    },
+}
+
+return quest
--- scripts/zones/Windurst_Waters/npcs/Ohbiru-Dohbiru.lua
+++ scripts/zones/Windurst_Waters/npcs/Ohbiru-Dohbiru.lua
@@ -17,11 +17,7 @@ entity.onTrade = function(player, npc, trade)
     local turmoil = player:getQuestStatus(xi.quest.log_id.WINDURST, xi.quest.id.windurst.TORAIMARAI_TURMOIL)
     local count = trade:getItemCount()
 
-    if (player:getQuestStatus(xi.quest.log_id.WINDURST, xi.quest.id.windurst.WATER_WAY_TO_GO) == QUEST_ACCEPTED) then
-        if (trade:hasItemQty(4351, 1) and count == 1) then
-            player:startEvent(355, 900)
-        end
-    elseif (turmoil == QUEST_ACCEPTED) then
+    if (turmoil == QUEST_ACCEPTED) then
         if (count == 3 and trade:getGil() == 0 and trade:hasItemQty(906, 3) == true) then --Check that all 3 items have been traded
             player:startEvent(791)
         else
@@ -37,13 +33,9 @@ entity.onTrade = function(player, npc, trade)
 end
 
 entity.onTrigger = function(player, npc)
-    -- Check for Missions first (priority?)
-    -- If the player has started the mission or not
     local pfame = player:getFameLevel(WINDURST)
     local turmoil = player:getQuestStatus(xi.quest.log_id.WINDURST, xi.quest.id.windurst.TORAIMARAI_TURMOIL)
     local needToZone = player:needToZone()
-    local waterWayToGo = player:getQuestStatus(xi.quest.log_id.WINDURST, xi.quest.id.windurst.WATER_WAY_TO_GO)
-    local overnightDelivery = player:getQuestStatus(xi.quest.log_id.WINDURST, xi.quest.id.windurst.OVERNIGHT_DELIVERY)
     local SayFlowers = player:getQuestStatus(xi.quest.log_id.WINDURST, xi.quest.id.windurst.SAY_IT_WITH_FLOWERS)
     local FlowerProgress = player:getCharVar("FLOWER_PROGRESS")
     local blueRibbonBlues = player:getQuestStatus(xi.quest.log_id.WINDURST, xi.quest.id.windurst.BLUE_RIBBON_BLUES)
@@ -58,20 +50,8 @@ entity.onTrigger = function(player, npc)
         else
             player:startEvent(516, 0, 0, 0, 0, 950)
         end
-    elseif (waterWayToGo == QUEST_COMPLETED and needToZone) then
-        player:startEvent(356, 0, 4351)
-    elseif (waterWayToGo == QUEST_ACCEPTED) then
-        if (player:hasItem(504) == false and player:hasItem(4351) == false) then
-            player:startEvent(354)
-        else
-            player:startEvent(353)
-        end
-    elseif (waterWayToGo == QUEST_AVAILABLE and overnightDelivery == QUEST_COMPLETED and pfame >= 3) then
-        player:startEvent(352, 0, 4351)
 
-    --
     -- Begin Toraimarai Turmoil Section
-    --
     elseif blueRibbonBlues == QUEST_COMPLETED and turmoil == QUEST_AVAILABLE and pfame >= 6 and needToZone == false then
         player:startEvent(785, 4500, 267, 906)
     elseif (turmoil == QUEST_ACCEPTED) then
@@ -86,7 +66,7 @@ end
 
 entity.onEventFinish = function(player, csid, option)
 
-    local tabre =
+    local flowerList =
     {
         [0] = { itemid = 948, gil = 300 }, -- Carnation
         [1] = { itemid = 941, gil = 200 }, -- Red Rose
@@ -115,27 +95,11 @@ entity.onEventFinish = function(player, csid, option)
         player:messageSpecial(ID.text.GIL_OBTAINED, xi.settings.GIL_RATE*4500)
         player:addFame(WINDURST, 50)
         player:tradeComplete()
-    elseif (csid == 352 and option == 0 or csid == 354) then
-        if (player:getFreeSlotsCount() >= 1) then
-            if (player:getQuestStatus(xi.quest.log_id.WINDURST, xi.quest.id.windurst.WATER_WAY_TO_GO) == QUEST_AVAILABLE) then
-                player:addQuest(xi.quest.log_id.WINDURST, xi.quest.id.windurst.WATER_WAY_TO_GO)
-            end
-            player:addItem(504)
-            player:messageSpecial(ID.text.ITEM_OBTAINED, 504)
-        else
-            player:messageSpecial(ID.text.ITEM_CANNOT_BE_OBTAINED, 504)
-        end
-    elseif (csid == 355) then
-        player:addGil(xi.settings.GIL_RATE*900)
-        player:completeQuest(xi.quest.log_id.WINDURST, xi.quest.id.windurst.WATER_WAY_TO_GO)
-        player:addFame(WINDURST, 40)
-        player:tradeComplete()
-        player:needToZone(true)
     elseif (csid == 872) then
         player:setCharVar("MEMORIES_OF_A_MAIDEN_Status", 3)
     elseif (csid == 516) then
         if (option < 7) then
-            local choice = tabre[option]
+            local choice = flowerList[option]
             if (choice and player:getGil() >= choice.gil) then
                 if (player:getFreeSlotsCount() > 0) then
                     player:addItem(choice.itemid)
