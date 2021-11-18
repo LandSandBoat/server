b8dcc51057e6e751eb1f844d3478830bcef7409a
RAIST5150 <73744368+RAIST5150@users.noreply.github.com>
2021-11-17 22:59:57 -0500

    Update stardiver.lua
    Fix SoA rule to use 0.03.  Had the math right in my head, didn't type it correctly when editing the files.

--- scripts/globals/weaponskills/stardiver.lua
+++ scripts/globals/weaponskills/stardiver.lua
@@ -27,7 +27,7 @@ weaponskill_object.onUseWeaponSkill = function(player, target, wsID, tp, primary
     params.atk100 = 1; params.atk200 = 1; params.atk300 = 1
 
     if (xi.settings.USE_ADOULIN_WEAPON_SKILL_CHANGES == true) then
-        params.str_wsc = 0.7 + (player:getMerit(xi.merit.STARDIVER) * 0.3)
+        params.str_wsc = 0.7 + (player:getMerit(xi.merit.STARDIVER) * 0.03)
     end
 
     local damage, criticalHit, tpHits, extraHits = doPhysicalWeaponskill(player, target, wsID, params, tp, action, primary, taChar)
