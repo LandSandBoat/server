77343ad116aa5e64339e5f688171128f1097ecba
relliko <apolywka@gmail.com>
2021-11-19 00:25:13 -0500

    implemented fix

--- scripts/mixins/families/lycopodium.lua
+++ scripts/mixins/families/lycopodium.lua
@@ -2,47 +2,71 @@ require("scripts/globals/mixins")
 require("scripts/globals/status")
 -----------------------------------
 
-tpz = tpz or {}
-tpz.mix = tpz.mix or {}
-tpz.mix.lycopodium = tpz.mix.lycopodium or {}
+xi = xi or {}
+xi.mix = xi.mix or {}
+xi.mix.lycopodium = xi.mix.lycopodium or {}
 
 g_mixins = g_mixins or {}
 g_mixins.families = g_mixins.families or {}
 
 g_mixins.families.lycopodium = function(mob)
-    mob:addListener("SPAWN", "LYCOPODIUM_SPAWN", function(mob)
-        mob:SetAutoAttackEnabled(false)
-        mob:SetMobAbilityEnabled(false)
-        mob:setMobMod(tpz.mobMod.ALWAYS_AGGRO, 1)
+    mob:addListener("SPAWN", "LYCOPODIUM_SPAWN", function(lycopodium)
+        lycopodium:SetAutoAttackEnabled(false)
+        lycopodium:SetMobAbilityEnabled(false)
+        lycopodium:setMobMod(xi.mobMod.ALWAYS_AGGRO, 1)
     end)
 
-    mob:addListener("ROAM_TICK", "LYCOPODIUM_RTICK", function(mob)
-        if mob:getHPP() == 100 then
-            mob:setLocalVar("[lycopodium]damaged", 0)
+    mob:addListener("ROAM_TICK", "LYCOPODIUM_RTICK", function(lycopodium)
+        -- Goes back to a passive state if roaming and reset completely.
+        if lycopodium:getHPP() == 100 then
+            if not lycopodium:isAlly() then
+                lycopodium:setLocalVar("[lycopodium]damaged", 0)
+            end
+            lycopodium:SetAutoAttackEnabled(false)
+            lycopodium:SetMobAbilityEnabled(false)
         end
     end)
 
-    mob:addListener("DISENGAGE", "LYCOPODIUM_DISENGAGE", function(mob, target)
-        mob:SetAutoAttackEnabled(false)
-        mob:SetMobAbilityEnabled(false)
+    mob:addListener("DISENGAGE", "LYCOPODIUM_DISENGAGE", function(lycopodium)
+        -- Ensure it goes back into passive state on deaggro
+        if not lycopodium:isAlly() then
+            lycopodium:SetAutoAttackEnabled(false)
+            lycopodium:SetMobAbilityEnabled(false)
+        end
     end)
 
-    mob:addListener("ENGAGE", "LYCOPODIUM_ENGAGE", function(mob, target)
-        mob:setLocalVar("[lycopodium]disengageTime",  mob:getBattleTime() + 45)
+    mob:addListener("ENGAGE", "LYCOPODIUM_ENGAGE", function(lycopodium)
+        lycopodium:setLocalVar("[lycopodium]disengageTime",  lycopodium:getBattleTime() + 45)
+        if lycopodium:isAlly() then
+            lycopodium:SetAutoAttackEnabled(true)
+            lycopodium:SetMobAbilityEnabled(true)
+        end
     end)
 
-    mob:addListener("COMBAT_TICK", "LYCOPODIUM_CTICK", function(mob)
-        if mob:getLocalVar("[lycopodium]damaged") == 0 then
-            local disengageTime = mob:getLocalVar("[lycopodium]disengageTime")
+    mob:addListener("COMBAT_TICK", "LYCOPODIUM_CTICK", function(lycopodium)
+        if lycopodium:isAlly() then
+            lycopodium:SetAutoAttackEnabled(true)
+            lycopodium:SetMobAbilityEnabled(true)
+        else
+            local disengageTime = lycopodium:getLocalVar("[lycopodium]disengageTime")
+            local damaged = lycopodium:getLocalVar("[lycopodium]damaged")
 
-            if mob:getHP() < mob:getMaxHP() then
-                mob:SetAutoAttackEnabled(true)
-                mob:SetMobAbilityEnabled(true)
-                mob:setLocalVar("[lycopodium]damaged", 1)
-            elseif disengageTime > 0 and mob:getBattleTime() > disengageTime then
-                mob:setLocalVar("[lycopodium]disengageTime",  0)
-                mob:disengage()
+            if lycopodium:getHP() < lycopodium:getMaxHP() then
+                lycopodium:SetAutoAttackEnabled(true)
+                lycopodium:SetMobAbilityEnabled(true)
+            elseif disengageTime > 0 and lycopodium:getBattleTime() > disengageTime and damaged == 0 then
+                lycopodium:setLocalVar("[lycopodium]disengageTime",  0)
+                lycopodium:disengage()
             end
+        end
+    end)
+
+    mob:addListener("TAKE_DAMAGE", "LYCOPODIUM_DAMAGE", function(lycopodium)
+        lycopodium:SetAutoAttackEnabled(true)
+        lycopodium:SetMobAbilityEnabled(true)
+
+        if not lycopodium:isAlly() then
+            lycopodium:setLocalVar("[lycopodium]damaged", 1)
         end
     end)
 end
