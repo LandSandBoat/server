5cea04b11ef819aca2edad5c68123c5f54043a68
claywar <claywar@beerlab.dev>
2021-11-30 10:20:47 -0500

    Interaction - Mission: Lost Kingdom

--- scripts/zones/Caedarva_Mire/DefaultActions.lua
+++ scripts/zones/Caedarva_Mire/DefaultActions.lua
@@ -1,5 +1,6 @@
 local ID = require("scripts/zones/Caedarva_Mire/IDs")
 
 return {
-    ['qm12'] = { messageSpecial = ID.text.NOTHING_HAPPENS },
+    ['Jazaraats_Headstone'] = { messageSpecial = ID.text.JAZARAATS_HEADSTONE },
+    ['qm12']                = { messageSpecial = ID.text.NOTHING_HAPPENS },
 }
--- scripts/zones/Nashmau/npcs/Pyopyoroon.lua
+++ scripts/zones/Nashmau/npcs/Pyopyoroon.lua
@@ -3,17 +3,12 @@
 --  NPC: Pyopyoroon
 -- Standard Info NPC
 -----------------------------------
-require("scripts/globals/missions")
------------------------------------
 local entity = {}
 
 entity.onTrade = function(player, npc, trade)
 end
 
 entity.onTrigger = function(player, npc)
-    if (player:getCurrentMission(TOAU) == xi.mission.id.toau.LOST_KINGDOM) then
-        player:startEvent(280)
-    end
 end
 
 entity.onEventUpdate = function(player, csid, option)
