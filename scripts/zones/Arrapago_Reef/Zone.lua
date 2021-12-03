c3eeea1b90b0d5aa12d44f82788d2965bc3867f8
claywar <claywar@beerlab.dev>
2021-12-03 14:38:26 -0500

    Interaction - Mission: Prevalence of Pirates
    Comment formatting

--- scripts/zones/Arrapago_Reef/Zone.lua
+++ scripts/zones/Arrapago_Reef/Zone.lua
@@ -17,6 +17,7 @@ end
 
 zone_object.onZoneIn = function(player, prevZone)
     local cs = -1
+
     if player:getXPos() == 0 and player:getYPos() == 0 and player:getZPos() == 0 then
         player:setPos(-456, -3, -405, 64)
     end
@@ -25,13 +26,10 @@ zone_object.onZoneIn = function(player, prevZone)
         if player:getCharVar("AgainstAllOdds") == 3 then
             cs = 238
         end
-    elseif prevZone == xi.zone.CAEDARVA_MIRE then
-        if player:getCurrentMission(TOAU) == xi.mission.id.toau.PREVALENCE_OF_PIRATES and player:getCharVar("AhtUrganStatus") == 0 then
-            cs = 13
-        end
     elseif prevZone == xi.zone.ILRUSI_ATOLL then
         player:setPos(26, -7, 606, 222)
     end
+
     return cs
 end
 
@@ -41,9 +39,7 @@ zone_object.afterZoneIn = function(player)
 end
 
 zone_object.onRegionEnter = function(player, region)
-    if player:getCurrentMission(TOAU) == xi.mission.id.toau.PREVALENCE_OF_PIRATES and player:getCharVar("AhtUrganStatus") == 1 then
-        player:startEvent(14)
-    elseif player:getCurrentMission(TOAU) == xi.mission.id.toau.TESTING_THE_WATERS and player:hasKeyItem(xi.ki.EPHRAMADIAN_GOLD_COIN) then
+    if player:getCurrentMission(TOAU) == xi.mission.id.toau.TESTING_THE_WATERS and player:hasKeyItem(xi.ki.EPHRAMADIAN_GOLD_COIN) then
         player:startEvent(15)
     elseif player:getQuestStatus(xi.quest.log_id.AHT_URHGAN, xi.quest.id.ahtUrhgan.AGAINST_ALL_ODDS) == QUEST_ACCEPTED and player:getCharVar("AgainstAllOdds") == 1 then
         player:startEvent(237)
@@ -54,15 +50,7 @@ zone_object.onEventUpdate = function(player, csid, option)
 end
 
 zone_object.onEventFinish = function(player, csid, option)
-    if csid == 13 then
-        player:setCharVar("AhtUrganStatus", 1)
-    elseif csid == 14 then
-        player:completeMission(xi.mission.log_id.TOAU, xi.mission.id.toau.PREVALENCE_OF_PIRATES)
-        player:setCharVar("AhtUrganStatus", 0)
-        player:addKeyItem(xi.ki.PERIQIA_ASSAULT_AREA_ENTRY_PERMIT)
-        player:messageSpecial(ID.text.KEYITEM_OBTAINED, xi.ki.PERIQIA_ASSAULT_AREA_ENTRY_PERMIT)
-        player:addMission(xi.mission.log_id.TOAU, xi.mission.id.toau.SHADES_OF_VENGEANCE)
-    elseif csid == 15 then
+    if csid == 15 then
         player:setCharVar("AhtUrganStatus", 1)
         player:setPos(0, 0, 0, 0, 57)
     elseif csid == 108 then -- enter instance: illrusi atoll
