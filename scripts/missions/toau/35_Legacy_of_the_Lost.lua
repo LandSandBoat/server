de7adc998b25312cdbd6884c341824b3f97e2115
claywar <claywar@beerlab.dev>
2021-12-03 19:28:03 -0500

    Interaction - Mission: Legacy of the Lost

---
+++ scripts/missions/toau/35_Legacy_of_the_Lost.lua
@@ -0,0 +1,45 @@
+-----------------------------------
+-- Legacy of the Lost
+-- Aht Uhrgan Mission 35
+-----------------------------------
+-- !addmission 4 34
+-----------------------------------
+require("scripts/globals/besieged")
+require('scripts/globals/items')
+require('scripts/globals/missions')
+require('scripts/globals/npc_util')
+require('scripts/globals/keyitems')
+require('scripts/globals/interaction/mission')
+require('scripts/globals/zone')
+-----------------------------------
+
+local mission = Mission:new(xi.mission.log_id.TOAU, xi.mission.id.toau.LEGACY_OF_THE_LOST)
+
+mission.reward =
+{
+    title       = xi.title.GESSHOS_MERCY,
+    nextMission = { xi.mission.log_id.TOAU, xi.mission.id.toau.GAZE_OF_THE_SABOTEUR },
+}
+
+mission.sections =
+{
+    {
+        check = function(player, currentMission, missionStatus, vars)
+            return currentMission == mission.missionId
+        end,
+
+        [xi.zone.TALACCA_COVE] =
+        {
+            onEventFinish =
+            {
+                [32001] = function(player, csid, option, npc)
+                    if player:getLocalVar('battlefieldWin') == 1092 then
+                        mission:complete(player)
+                    end
+                end,
+            },
+        },
+    },
+}
+
+return mission
