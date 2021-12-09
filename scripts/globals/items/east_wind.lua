bc6e37d034b3ab18095d8985029d41d5320e583a
Zelita <siknoz@yahoo.com>
2021-12-09 13:40:19 -0800

    God Wind and PVP crash fix

---
+++ scripts/globals/items/east_wind.lua
@@ -0,0 +1,21 @@
+-----------------------------------------
+-- ID: 18162
+-- Item: East Wind
+-- Additional Effect: Removes Byakko's Light Damage
+-----------------------------------------
+require("scripts/globals/status")
+-----------------------------------------
+local item_object = {}
+
+item_object.onAdditionalEffect = function(player, target, damage)
+    if not target:isMob() then
+        return 1
+    end
+
+    if (target:getFamily() == 279) then
+        target:setMobMod(xi.mobMod.ADD_EFFECT, 0)
+    end
+    return 0, 0, 0
+end
+
+return item_object
