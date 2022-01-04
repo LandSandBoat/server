31ac2f074d098ebc68255594a11d2067c57a7093
Xaver-DaRed <60053999+Xaver-DaRed@users.noreply.github.com>
2022-01-04 18:05:13 +0100

    Implement "AUGMENT_SONG_STAT" modifier
    Co-Authored-By: neuromancerxi <neuromancerxi@users.noreply.github.com>

--- scripts/globals/effects/minne.lua
+++ scripts/globals/effects/minne.lua
@@ -7,6 +7,7 @@ local effect_object = {}
 
 effect_object.onEffectGain = function(target, effect)
     target:addMod(xi.mod.DEF, effect:getPower())
+    target:addMod(xi.mod.VIT, effect:getSubPower()) -- Apply Stat Buff from AUGMENT_SONG_STAT
 end
 
 effect_object.onEffectTick = function(target, effect)
@@ -14,6 +15,7 @@ end
 
 effect_object.onEffectLose = function(target, effect)
     target:delMod(xi.mod.DEF, effect:getPower())
+    target:delMod(xi.mod.VIT, effect:getSubPower()) -- Remove Stat Buff from AUGMENT_SONG_STAT
 end
 
 return effect_object
