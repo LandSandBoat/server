12cbd804b3376dd5c17b5a3a88d7135f247d8a83
Zach Toogood <zrtoogood@gmail.com>
2022-01-06 13:36:03 +0200

    [debug] Add more information to crash dumps on Windows

--- src/common/version.h
+++ src/common/version.h
@@ -22,14 +22,24 @@
 #ifndef _VERSION_H_
 #define _VERSION_H_
 
+#include <string>
+
 #define XI_MAJOR_VERSION 2021 // Major Version
 #define XI_MINOR_VERSION 2    // Minor Version
 #define XI_REVISION      1    // Revision
 
-#define XI_RELEASE_FLAG 1 // 1=Stable,0=Unstable
+#define XI_RELEASE_FLAG  1 // 1=Stable, 0=Unstable
 
-#define XI_SERVER_NONE     0 // not defined
+#define XI_SERVER_NONE   0 // not defined
 #define XI_SERVER_LOGIN  1 // login server
-#define XI_SERVER_MAP      2 // map server
+#define XI_SERVER_MAP    2 // map server
+
+namespace version
+{
+    std::string GetGitSha();
+    std::string GetGitBranch();
+    std::string GetGitDate();
+    std::string GetGitCommitSubject();
+}
 
 #endif /* _VERSION_H_ */
