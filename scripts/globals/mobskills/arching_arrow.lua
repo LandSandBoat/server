e6eda6aaac0e6f94b22e910457661946ef8f97c4
Zach Toogood <zrtoogood@gmail.com>
2022-01-16 21:19:50 +0200

    [trust] WS's for Semih Lafihna

---
+++ scripts/globals/mobskills/arching_arrow.lua
@@ -0,0 +1,26 @@
+-----------------------------------
+-- Stellar Arrow (AOE)
+-- Trust: Semih Lafihna
+-- Darkness/Gravitation skillchain properties, AoE damage
+-----------------------------------
+require("scripts/settings/main")
+require("scripts/globals/status")
+require("scripts/globals/mobskills")
+-----------------------------------
+local mobskill_object = {}
+
+mobskill_object.onMobSkillCheck = function(target, mob, skill)
+    return 0
+end
+
+mobskill_object.onMobWeaponSkill = function(target, mob, skill)
+    local numhits = 1
+    local accmod = 1
+    local dmgmod = 2
+    local info = xi.mobskills.mobPhysicalMove(mob, target, skill, numhits, accmod, dmgmod, xi.mobskills.magicalTpBonus.NO_EFFECT)
+    local dmg = xi.mobskills.mobFinalAdjustments(info.dmg, mob, skill, target, xi.attackType.RANGED, xi.damageType.PIERCING, xi.mobskills.shadowBehavior.NUMSHADOWS_3)
+    target:takeDamage(dmg, mob, xi.attackType.RANGED, xi.damageType.PIERCING)
+    return dmg
+end
+
+return mobskill_object
