cbd473f3af81c262057ba823f936ac319d015912
beneliezer <beneliezer@gmail.com>
2022-01-16 19:37:05 -0500

    working on mythic quest

--- scripts/zones/Nashmau/npcs/Paparoon.lua
+++ scripts/zones/Nashmau/npcs/Paparoon.lua
@@ -4,6 +4,7 @@
 -- Standard Info NPC
 -----------------------------------
 require("scripts/globals/npc_util")
+require("scripts/globals/besieged")
 -----------------------------------
 local entity = {}
 
@@ -11,12 +12,24 @@ entity.onTrade = function(player, npc, trade)
     local alexandrite = 2488
 	local alexCount = trade:getItemCount()
 	local storedAlex = player:getCharVar("Paparoon_AlexStored")
+	local hasCaptain = (player:hasKeyItem(xi.keyItem.CAPTAIN_WILDCAT_BADGE) == TRUE)
+	local hasCompletedTOAU = player:hasCompletedMission(xi.mission.log_id.TOAU, xi.mission.id.toau.PREVALENCE_OF_PIRATES)
 	
 	-- check to see if player already has alex stored, if he does we'll add to his total --
 	if (storedAlex and npcUtil.tradeHasOnly(trade, alexandrite)) then
 	    local newStoredAlex = storedAlex + alexCount
 	    player:setCharVar("Paparoon_AlexStored", newStoredAlex)
-		player:PrintToPlayer(string.format("Paparoon: That makes a total of %i alexandrite stored!", newStoredAlex), 0xD)
+	-- exchange alexandrite for alex if we're at 30000 --
+		if newStoredAlex >= 3000 then
+		    player:setCharVar("Paparoon_AlexStored", "CAPPED")
+		    player:PrintToPlayer("Paparoon: 1...2...3...30,0000! That's unreal yo!", 0xD)
+			if hasCompletedTOAU == TRUE 
+    	        --player:addItem(3443)
+			else
+			    player:PrintToPlayer("Paparoon: You still not a captain?! Come back when you are!", 0xD)
+		    return
+		end
+	    player:PrintToPlayer(string.format("Paparoon: That makes a total of %i alexandrite stored!", newStoredAlex), 0xD)
 		player:tradeComplete()
 	elseif not storedAlex then
         player:setCharVar("Paparoon_AlexStored", alexCount)
@@ -27,6 +40,7 @@ end
 
 entity.onTrigger = function(player, npc)
     local storedAlex = player:getCharVar("Paparoon_AlexStored")
+	local hasCaptain = (player:hasKeyItem(xi.keyItem.CAPTAIN_WILDCAT_BADGE) == TRUE)
 	
 	-- tell the player how many alex they have stored, if any --
 	if (storedAlex) then 
