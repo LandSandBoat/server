14b25942416ed2018ade096e0f64ef162f0107d2
Tiberon <TiberonKalkaz@gmail.com>
2022-01-18 16:17:45 -0800

    Snarl no longer requires the jug pet to be engaged

--- scripts/globals/abilities/snarl.lua
+++ scripts/globals/abilities/snarl.lua
@@ -16,7 +16,7 @@ ability_object.onAbilityCheck = function(player, target, ability)
     if (player:getPet() == nil) then
         return xi.msg.basic.REQUIRES_A_PET, 0
     else
-        if ((player:getPet():getTarget() ~= nil) and (player:isJugPet())) then
+        if ((player:getPet() ~= nil) and (player:isJugPet())) then
             return 0, 0
         else
             return xi.msg.basic.PET_CANNOT_DO_ACTION, 0
