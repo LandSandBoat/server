0e793d471eb5b3f590ae585a544fe21aaa0c5fde
Legiongreed <97728069+Legiongreed@users.noreply.github.com>
2022-01-26 01:47:25 -0500

    Open and Spin Command
    Open - Allows open any locked door
    
    Spin - Help to catch fishbots,
    
    Credit goes to Wings Server

---
+++ scripts/commands/open.lua
@@ -0,0 +1,28 @@
+---------------------------------------------------------------------------------------------------
+-- func: open <time>
+-- desc: Forces a door to open
+---------------------------------------------------------------------------------------------------
+
+cmdprops =
+{
+    permission = 2,
+    parameters = "i"
+}
+
+function error(player, msg)
+    player:PrintToPlayer(msg)
+    player:PrintToPlayer("!open <time>")
+end
+
+function onTrigger(player, open_time, target)
+    -- validate target
+    local targ = player:getCursorTarget()
+    if targ == nil then
+        error(player, "A door NPC must be selected.")
+        return
+    end
+
+    targ:openDoor(open_time)
+
+
+end
---
+++ scripts/commands/spin.lua
@@ -0,0 +1,46 @@
+---------------------------------------------------------------------------------------------------
+-- func: spin <player>
+-- desc: Spins a character in place. Used for fishbot detection.
+---------------------------------------------------------------------------------------------------
+
+cmdprops =
+{
+    permission = 2,
+    parameters = "si"
+}
+
+function error(player, msg)
+    player:PrintToPlayer(msg)
+    player:PrintToPlayer("!spin <player> {angle}")
+end
+
+function onTrigger(player, target, angle)
+    -- validate target
+    if (target == nil) then
+        error(player, "You must enter a target player name.")
+        return
+    end
+    local targ = GetPlayerByName( target )
+    if (targ == nil) then
+        if not player:bringPlayer( target ) then
+            error(player, string.format( "Player named '%s' not found!", target ) )
+        end
+        return
+    end
+
+	if (angle == nil) then
+		angle = 128
+    end
+	if ((angle < 0) or (angle > 255)) then
+		error(player, "Angle must be between 0 and 255")
+		return
+	end
+
+    -- spin target
+	local oldRot = targ:getRotPos()
+	local newRot = oldRot + angle
+	if (newRot > 255) then
+		newRot = newRot - 255
+	end
+    targ:setPos( targ:getXPos(), targ:getYPos(), targ:getZPos(), newRot )
+end
