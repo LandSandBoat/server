bbc2910f497ea6300555b1704a170eeb710f0928
RuthlessMortal <98451272+RuthlessMortal@users.noreply.github.com>
2022-01-27 18:09:17 -0500

    Update Zone.lua
    Fix correct zone

--- scripts/zones/Ifrits_Cauldron/Zone.lua
+++ scripts/zones/Ifrits_Cauldron/Zone.lua
@@ -1,30 +1,47 @@
 -----------------------------------
--- Zone: Chateau_dOraguille (233)
+--
+-- Zone: Ifrits_Cauldron (205)
+--
 -----------------------------------
+local ID = require("scripts/zones/Ifrits_Cauldron/IDs")
 require("scripts/globals/conquest")
-require("scripts/globals/zone")
+require("scripts/globals/treasure")
+require("scripts/globals/helm")
 -----------------------------------
 local zone_object = {}
 
 zone_object.onInitialize = function(zone)
-    zone:registerRegion(1, -95, 0, 75, -85, 5, 85)
-end
+    UpdateNMSpawnPoint(ID.mob.ASH_DRAGON)
+    GetMobByID(ID.mob.ASH_DRAGON):setRespawnTime(math.random(900, 10800))
 
-zone_object.onZoneIn = function(player, prevZone)
-    local cs = -1
-
-    if player:getXPos() == 0 and player:getYPos() == 0 and player:getZPos() == 0 then
-        player:setPos(0, 0, -11, 194)
-    end
-
-    return cs
+    xi.treasure.initZone(zone)
+    xi.helm.initZone(zone, xi.helm.type.MINING)
 end
 
 zone_object.onConquestUpdate = function(zone, updatetype)
     xi.conq.onConquestUpdate(zone, updatetype)
 end
 
+zone_object.onZoneIn = function(player, prevZone)
+    local cs = -1
+
+    if player:getXPos() == 0 and player:getYPos() == 0 and player:getZPos() == 0 then
+        player:setPos(100, 0, -320, 188)
+    end
+
+    return cs
+end
+
 zone_object.onRegionEnter = function(player, region)
+end
+
+zone_object.onGameHour = function(zone)
+    -- Opens flame spouts every 3 hours Vana'diel time. Doors are offset from spouts by 5.
+    if VanadielHour() % 3 == 0 then
+        for i = 5, 8 do
+            GetNPCByID(ID.npc.FLAME_SPOUT_OFFSET + i):openDoor(90)
+        end
+    end
 end
 
 zone_object.onEventUpdate = function(player, csid, option)
