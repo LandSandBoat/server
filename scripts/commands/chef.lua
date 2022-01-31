9b118a53ff7bf3a61bc988a8b171327d66893907
jeffnavy14 <68021436+jeffnavy14@users.noreply.github.com>
2022-01-31 14:52:26 -0500

    Edited the chef lua. This will be easier for players to use in the field and purchase multiples if needed.

--- scripts/commands/chef.lua
+++ scripts/commands/chef.lua
@@ -1,49 +1,25 @@
------------------------------------
--- func: chef <itemname>
--- desc: Purchases a food item from the server.
------------------------------------
+--------------------------------------------------------------
+-- func: !Chef
+-- desc: opens a Chef Shop.
+--------------------------------------------------------------
 
 cmdprops =
 {
     permission = 0,
-    parameters = "s"
-}
+    parameters = "iiii"
+};
 
-function purchaseItem(player,itemId, price)
-    local ID = zones[player:getZoneID()]
-    local playerGil = player:getGil()
-    if (playerGil > price) then
-        player:delGil(price)
-        player:PrintToPlayer("Thanks for your purchase.")
-        player:addItem( itemId, 1)
-        player:messageSpecial( ID.text.ITEM_OBTAINED, itemId )
-    else
-        player:PrintToPlayer("Not enough gil to complete purchase.")
-    end
-end
-
-function error(player, msg)
-    player:PrintToPlayer(msg)
-    player:PrintToPlayer("!chef melonpie       (Melon Pie  2400g)")
-    player:PrintToPlayer("!chef jacko    (Jack-o'-Lantern  3000g)")
-    player:PrintToPlayer("!chef yagudo      (Yagudo drink  4000g)")
-    player:PrintToPlayer("!chef solesushi     (Sole Sushi  6000g)")
-    player:PrintToPlayer("!chef meatkabob (Meat Mithkabob  6000g)")
-    player:PrintToPlayer("!chef tavtaco   (Tavnazian Taco 60000g)")
-    player:PrintToPlayer("!chef behesteak   (Behemoth steak 80000g)")
-end
-
-function onTrigger(player, itemName)
-    if (player:getFreeSlotsCount() == 0) then
-        player:PrintToPlayer("ITEM CANNOT BE OBTAINED")
-        return
-    end
-    if(itemName == "melonpie") then purchaseItem(player, 4421, 2400)
-    elseif(itemName == "jacko") then purchaseItem(player, 4488, 3000)
-    elseif(itemName == "yagudo") then purchaseItem(player, 4558,4000)
-    elseif(itemName == "solesushi") then purchaseItem(player, 5149, 6000)
-    elseif(itemName == "meatkabob") then purchaseItem(player, 4381, 6000)
-    elseif(itemName == "tavtaco") then purchaseItem(player, 5174, 5000)
-    elseif(itemName == "behesteak") then purchaseItem(player, 6464, 5000)
-    else error(player,"Invalid item name") end
+function onTrigger(player,npc)
+player:PrintToPlayer("Welcome to the one-stop Chef Shop!")
+stock =
+{
+ 4421, 2400, -- Melon Pie
+ 4488, 3000,-- Jaco
+ 4558, 4000, -- Yagudo
+ 5149, 6000, -- SoleSushi
+ 4381, 6000, -- MeatKabob
+ 5174, 5000, -- Tav-Taco
+ 6464, 5000, -- BeheSteak
+} 
+xi.shop.general(player, stock)
 end
