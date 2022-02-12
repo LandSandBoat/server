d0f9e3d0ec627f4a94c0d8ac3a3cac78a26d8b8a
claywar <claywar@beerlab.dev>
2022-02-12 14:22:14 -0500

    Interaction - Mission: Ring My Bell

--- scripts/zones/Norg/DefaultActions.lua
+++ scripts/zones/Norg/DefaultActions.lua
@@ -1,6 +1,7 @@
 local ID = require("scripts/zones/Norg/IDs")
 
 return {
+    ['_700']       = { event = 5 },
     ['Aeka']       = { event = 120 },
     ['Comitiolus'] = { event = 72 },
     ['Heillal']    = { event = 64 },
