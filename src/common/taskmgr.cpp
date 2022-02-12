4f594b77f87c898e3055017a1b7fb775a96afe77
Zach Toogood <zrtoogood@gmail.com>
2022-02-12 12:36:15 +0200

    [core] Add missing CTaskMgr::RemoveTask impl

--- src/common/taskmgr.cpp
+++ src/common/taskmgr.cpp
@@ -62,7 +62,36 @@ CTaskMgr::CTask* CTaskMgr::AddTask(CTask* PTask)
 
 void CTaskMgr::RemoveTask(const std::string& TaskName)
 {
-    // empty method
+    // m_TaskList is a priority_queue, so we can't directly pull members out of it.
+    //
+    // Tasks are compared using their m_tick values, so we can safely remove all the tasks
+    // and re-insert them, sans the one we're trying to remove.
+
+    std::size_t tasksRemoved = 0;
+    TaskList_t newPq;
+    while (!m_TaskList.empty())
+    {
+        CTask* PTask = m_TaskList.top();
+        m_TaskList.pop();
+
+        // Don't add tasks we're trying to remove to the new pq
+        if (PTask->m_name != TaskName)
+        {
+            newPq.push(PTask);
+        }
+        else
+        {
+            ++tasksRemoved;
+        }
+    }
+
+    if (tasksRemoved == 0)
+    {
+        ShowWarning("Tried to remove task: %s, but didn't find it!", TaskName);
+    }
+
+    // Replace the old queue with the new queue
+    m_TaskList = newPq;
 }
 
 duration CTaskMgr::DoTimer(time_point tick)
