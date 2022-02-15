64de52af903dd6d1b0cba6e030de08bd5395ce9c
Zach Toogood <zrtoogood@gmail.com>
2022-02-15 10:54:33 +0200

    [deps] build_mariadb_dep.bat now extracts the built output to the correct place

--- tools/build_mariadb_dep.bat
+++ tools/build_mariadb_dep.bat
@@ -5,7 +5,7 @@ REM Change TYPE between Debug/Release, if you like
 
 SET TYPE=Release
 
-git clone --depth 1 --branch 3.1.10 https://github.com/mariadb-corporation/mariadb-connector-c.git
+git clone --branch 3.1.10 --depth 1 https://github.com/mariadb-corporation/mariadb-connector-c.git
 
 mkdir mariadb-connector-c\build32
 mkdir mariadb-connector-c\build64
@@ -16,6 +16,10 @@ cmake -S mariadb-connector-c -B "mariadb-connector-c\build64"
 cmake --build mariadb-connector-c\build32 --config %TYPE% --target package
 cmake --build mariadb-connector-c\build64 --config %TYPE% --target package
 
-REM You can now extract the packaged outputs of these two builds to:
-REM ext/mariadb32
-REM ext/mariadb64
+for /R "mariadb-connector-c\build32" %%I in ("*.zip") do (
+    tar -xf %%I -C ../ext/mariadb32/ --strip-components=1
+)
+
+for /R "mariadb-connector-c\build64" %%I in ("*.zip") do (
+    tar -xf %%I -C ../ext/mariadb64/ --strip-components=1
+)
