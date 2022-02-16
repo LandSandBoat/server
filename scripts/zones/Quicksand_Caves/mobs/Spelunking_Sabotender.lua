c7e1307735e04b78f96d264ca1217c937baa193d
hooksta4 <carl.hooker@gmail.com>
2022-02-16 11:10:02 -0500

    Implemented Bailarin/Bailarina
    added bailarin/a to lottery spawn NM and associated loot tables IAW the table that has been passed around.

--- scripts/zones/Quicksand_Caves/mobs/Spelunking_Sabotender.lua
+++ scripts/zones/Quicksand_Caves/mobs/Spelunking_Sabotender.lua
@@ -10,4 +10,8 @@ entity.onMobDeath = function(mob, player, isKiller)
     xi.regime.checkRegime(player, mob, 816, 1, xi.regime.type.GROUNDS)
 end
 
+entity.onMobDespawn = function(mob)
+    xi.mob.phOnDespawn(mob, ID.mob.SABOTENDER_BAILARINA_PH, 10, 9000) -- 2.5 hours
+end
+
 return entity
