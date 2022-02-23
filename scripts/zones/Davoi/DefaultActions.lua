89e4022a32898b7b193d2a57183b2f4675c7cd02
claywar <claywar@beerlab.dev>
2022-02-23 10:59:29 -0500

    Interaction - Hidden Quest: Crimson Orb

--- scripts/zones/Davoi/DefaultActions.lua
+++ scripts/zones/Davoi/DefaultActions.lua
@@ -3,7 +3,9 @@ local ID = require("scripts/zones/Davoi/IDs")
 return {
     ['!']            = { messageSpecial = ID.text.YOU_SEE_NOTHING },
     ['_459']         = { event = 55 },
+    ['_45d']         = { messageSpecial = ID.text.CAVE_HAS_BEEN_SEALED_OFF },
+    ['Bernal']       = { event = 31 },
     ['qm2']          = { messageSpecial = ID.text.NOTHING_OUT_OF_ORDINARY },
-    ['Sedal-Godjal'] = { event = 24 },
+    ['Sedal-Godjal'] = { event = 27 },
     ['Zantaviat']    = { event = 101 },
 }
--- scripts/zones/Davoi/npcs/Sedal-Godjal.lua
+++ scripts/zones/Davoi/npcs/Sedal-Godjal.lua
@@ -16,36 +16,12 @@ entity.onTrade = function(player, npc, trade)
 end
 
 entity.onTrigger = function(player, npc)
-    -- Whence Blows the Wind
-    if player:hasKeyItem(xi.ki.CRIMSON_ORB) == false then
-        local miniQuestForORB_CS = player:getCharVar("miniQuestForORB_CS")
-
-        if miniQuestForORB_CS == 0 then
-            player:startEvent(24)
-        elseif miniQuestForORB_CS == 99 then
-            player:startEvent(22) -- Start mini quest
-        elseif miniQuestForORB_CS == 1 and player:getCharVar("countRedPoolForORB") ~= 15 then
-            player:startEvent(21) -- During mini quest
-        elseif miniQuestForORB_CS == 1 then
-            player:startEvent(25, 0, 0, 0, xi.ki.CRIMSON_ORB) -- Finish mini quest
-        end
-    end
 end
 
 entity.onEventUpdate = function(player, csid, option)
 end
 
 entity.onEventFinish = function(player, csid, option)
-    -- Whence Blows the Wind
-    if csid == 22 and option == 1 then
-        player:setCharVar("miniQuestForORB_CS", 1)
-        npcUtil.giveKeyItem(player, xi.ki.WHITE_ORB)
-    elseif csid == 25 then
-        player:setCharVar("miniQuestForORB_CS", 0)
-        player:setCharVar("countRedPoolForORB", 0)
-        player:delKeyItem(xi.ki.CURSED_ORB)
-        npcUtil.giveKeyItem(player, xi.ki.CRIMSON_ORB)
-    end
 end
 
 return entity
