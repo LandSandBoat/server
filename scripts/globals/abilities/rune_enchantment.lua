796e34f05c2c5993e21b29b000ed2a9def09de3c
WinterSolstice8 <60417494+WinterSolstice8@users.noreply.github.com>
2022-02-24 06:49:45 +0000

    Add Rune Fencer job helper * Move Rune Enchantment effects into RUN job helper

--- scripts/globals/abilities/rune_enchantment.lua
+++ scripts/globals/abilities/rune_enchantment.lua
@@ -14,43 +14,9 @@ ability_object.onAbilityCheck = function(player, target, ability)
     return 0, 0
 end
 
+--this is a dummied ability that is not actually usable by a player due to Rune Enchantment only being the main category in a menu, as well as the name of the ability for recast.
 ability_object.onUseAbility = function(user, target, ability)
     -- Leave blank.
-end
-
--- helpers for runes
-function getRUNLevel(player)
-    return player:getMainJob() == xi.job.RUN and player:getMainLvl() or player:getSubLvl()
-end
-
-function applyRuneEnhancement(effectType, player)
-    local RUNLevel = getRUNLevel(player)
-    local meritBonus = (player:getMerit(xi.merit.MERIT_RUNE_ENHANCE) * 2) -- 2 more elemental resistance per merit for a maximum total of (2*5) = 10
-    local jobPointBonus = player:getJobPointLevel(xi.jp.RUNE_ENCHANTMENT_EFFECT) -- 1 more elemental resistance per level for a maximum total of 20
-	
-    -- see https://www.bg-wiki.com/ffxi/Category:Rune
-    local power = math.floor((49 * RUNLevel / 99) + 5.5) + meritBonus  + jobPointBonus
-    player:addStatusEffect(effectType, power, 0, 300)
-end
-
-function enforceRuneCounts(target)
-    local RUNLevel = getRUNLevel(target)
-    local maxRunes = RUNLevel >= 65 and 3 or RUNLevel >= 35 and 2 or 1
-    local effects = target:getStatusEffects()
-    local runes = { }
-    local i = 0
-
-    for _, effect in ipairs(effects) do
-        type = effect:getType()
-        if type >= xi.effect.IGNIS and type <= xi.effect.TENEBRAE then
-            runes[i+1] = effect
-            i = i + 1
-        end
-    end
-
-    if i >= maxRunes then -- delete the first rune in the list with the least duration
-        target:delStatusEffect(runes[1]:getType())
-    end
 end
 
 return ability_object
