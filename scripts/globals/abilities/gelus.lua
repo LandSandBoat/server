2da3e293e57a33d0bb9993ebf5d464b1a2c9024a
WinterSolstice8 <60417494+WinterSolstice8@users.noreply.github.com>
2022-02-24 02:58:17 +0000

    Stage 1 implementation of Rune Enhancement Elemental resistances are implemented, but the enspell damage formula is not yet known so it is not implemented yet.

---
+++ scripts/globals/abilities/gelus.lua
@@ -0,0 +1,24 @@
+-----------------------------------
+-- Ability: Gelus
+-- Increases resistance against wind and deals ice damage.
+-- Obtained: Rune Fencer Level 5
+-- Recast Time: 0:10
+-- Duration: 5:00
+-----------------------------------
+require("scripts/settings/main")
+require("scripts/globals/status")
+require("scripts/globals/abilities/rune_enchantment")
+-----------------------------------
+local ability_object = {}
+
+ability_object.onAbilityCheck = function(player, target, ability)
+	return 0, 0
+end
+
+ability_object.onUseAbility = function(caster, target, ability, action)
+    local effect = xi.effect.GELUS
+    enforceRuneCounts(target)
+    applyRuneEnhancement(effect, target)
+end
+
+return ability_object
---
+++ scripts/globals/effects/ignis.lua
@@ -0,0 +1,21 @@
+-----------------------------------
+--
+-- xi.effect.IGNIS
+--
+-----------------------------------
+require("scripts/globals/status")
+-----------------------------------
+local effect_object = {}
+
+effect_object.onEffectGain = function(target, effect)
+    target:addMod(xi.mod.ICE_RES, effect:getPower())
+end
+
+effect_object.onEffectTick = function(target, effect)
+end
+
+effect_object.onEffectLose = function(target, effect)
+    target:delMod(xi.mod.ICE_RES, effect:getPower())
+end
+
+return effect_object
