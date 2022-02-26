77e62799080610b61c4c22d0312b3cdd3dbdfa08
Tal Ben-Eliezer <beneliezer@gmail.com>
2022-02-26 12:16:05 -0500

    Buff HNM Levels

---
+++ catseyexi-mods/CatsEyeXI_Buff_HNM_Levels.sql
@@ -0,0 +1,14 @@
+UPDATE `mob_groups` SET `minLevel` = "97" AND `maxLevel` = "97" WHERE `name` = "Jormungand" AND `zoneid` = "5";
+UPDATE `mob_groups` SET `minLevel` = "97" AND `maxLevel` = "97" WHERE `name` = "Tiamat" AND `zoneid` = "7";
+UPDATE `mob_groups` SET `minLevel` = "97" AND `maxLevel` = "97" WHERE `name` = "Vrtra" AND `zoneid` = "190";
+UPDATE `mob_groups` SET `minLevel` = "92" AND `maxLevel` = "92" WHERE `name` = "Behemoth" AND `zoneid` = "127";
+UPDATE `mob_groups` SET `minLevel` = "92" AND `maxLevel` = "92" WHERE `name` = "Adamantoise" AND `zoneid` = "128";
+UPDATE `mob_groups` SET `minLevel` = "92" AND `maxLevel` = "92" WHERE `name` = "Fafnir" AND `zoneid` = "154";
+UPDATE `mob_groups` SET `minLevel` = "95" AND `maxLevel` = "95" WHERE `name` = "King_Behemoth" AND `zoneid` = "127";
+UPDATE `mob_groups` SET `minLevel` = "95" AND `maxLevel` = "95" WHERE `name` = "Aspidochelone" AND `zoneid` = "128";
+UPDATE `mob_groups` SET `minLevel` = "95" AND `maxLevel` = "95" WHERE `name` = "Nidhogg" AND `zoneid` = "154";
+UPDATE `mob_groups` SET `minLevel` = "95" AND `maxLevel` = "95" WHERE `name` = "Kirin" AND `zoneid` = "178";
+UPDATE `mob_groups` SET `minLevel` = "90" AND `maxLevel` = "90" WHERE `name` = "Genbu" AND `zoneid` = "130";
+UPDATE `mob_groups` SET `minLevel` = "90" AND `maxLevel` = "90" WHERE `name` = "Seiryu" AND `zoneid` = "130";
+UPDATE `mob_groups` SET `minLevel` = "90" AND `maxLevel` = "90" WHERE `name` = "Byakko" AND `zoneid` = "130";
+UPDATE `mob_groups` SET `minLevel` = "90" AND `maxLevel` = "90" WHERE `name` = "Suzaku" AND `zoneid` = "130";
