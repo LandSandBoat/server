9be15486bf0ca80b35602153975162bbcb4f8986
Kayanesa <100643440+Kayanesa@users.noreply.github.com>
2022-03-04 03:12:45 -0500

    Reverted tornado kick to original hit

--- scripts/globals/weaponskills/tornado_kick.lua
+++ scripts/globals/weaponskills/tornado_kick.lua
@@ -17,8 +17,8 @@ weaponskill_object.onUseWeaponSkill = function(player, target, wsID, tp, primary
 
     local params = {}
     -- number of normal hits for ws
-    params.numHits = 3
-    -- This is a 3 hit ws but H2H ws are done in a different way, the off hand hit is been taking into account in another place, it is possible that we are adding one hit too many but lacking data
+    params.numHits = 2
+    -- This is a 3 hit ws but H2H ws are done in a different way, the off hand hit is been taking into account in another place
     -- stat-modifiers (0.0 = 0%, 0.2 = 20%, 0.5 = 50%..etc)
     params.str_wsc = 0.32        params.dex_wsc = 0.0
     params.vit_wsc = 0.32        params.agi_wsc = 0.0
