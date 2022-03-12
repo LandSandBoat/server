b9ff19c0b877e7c841caf4f9d32e111f3c9f9b23
Zach Toogood <zrtoogood@gmail.com>
2022-03-12 15:07:08 +0200

    [debug] Fix new warnings in Wheaty

--- src/common/message_server.cpp
+++ src/common/message_server.cpp
@@ -24,7 +24,6 @@ along with this program.  If not, see http://www.gnu.org/licenses/
 #include <queue>
 
 #include "common/logging.h"
-#include "login.h"
 #include "message_server.h"
 
 zmq::context_t                 zContext;
