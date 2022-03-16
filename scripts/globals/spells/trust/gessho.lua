f52fd8c1d5d843a09b5fbc5b3e30f9fbe49ae8c1
Zach Toogood <zrtoogood@gmail.com>
2022-03-16 16:34:06 +0200

    [trust] QC Gessho

--- scripts/globals/spells/trust/gessho.lua
+++ scripts/globals/spells/trust/gessho.lua
@@ -29,17 +29,19 @@ spell_object.onMobSpawn = function(mob)
         end
     end)
 
+    -- Shadows are represented by xi.effect.COPY_IMAGE, but with different icons depending on the tier
+    mob:addSimpleGambit(ai.t.SELF, ai.c.NOT_STATUS, xi.effect.COPY_IMAGE, ai.r.MA, ai.s.HIGHEST, xi.magic.spellFamily.UTSUSEMI)
+
+    mob:addSimpleGambit(ai.t.TARGET, ai.c.NOT_STATUS, xi.effect.BLINDNESS, ai.r.MA, ai.s.HIGHEST, xi.magic.spellFamily.KURAYAMI, 60)
+    mob:addSimpleGambit(ai.t.TARGET, ai.c.NOT_STATUS, xi.effect.SLOW, ai.r.MA, ai.s.HIGHEST, xi.magic.spellFamily.HOJO, 60)
+
     mob:addSimpleGambit(ai.t.SELF, ai.c.NOT_STATUS, xi.effect.YONIN,
         ai.r.JA, ai.s.SPECIFIC, xi.ja.YONIN)
 
     mob:addSimpleGambit(ai.t.SELF, ai.c.NOT_HAS_TOP_ENMITY, 0,
                         ai.r.JA, ai.s.SPECIFIC, xi.ja.PROVOKE)
 
-    -- Shadows are represented by xi.effect.COPY_IMAGE, but with different icons depending on the tier
-    mob:addSimpleGambit(ai.t.SELF, ai.c.NOT_STATUS, xi.effect.COPY_IMAGE, ai.r.MA, ai.s.HIGHEST, xi.magic.spellFamily.UTSUSEMI)
-
-    mob:addSimpleGambit(ai.t.TARGET, ai.c.NOT_STATUS, xi.effect.BLINDNESS, ai.r.MA, ai.s.HIGHEST, xi.magic.spellFamily.KURAYAMI, 60)
-    mob:addSimpleGambit(ai.t.TARGET, ai.c.NOT_STATUS, xi.effect.SLOW, ai.r.MA, ai.s.HIGHEST, xi.magic.spellFamily.HOJO, 60)
+    mob:setTrustTPSkillSettings(ai.tp.ASAP, ai.s.RANDOM)
 end
 
 spell_object.onMobDespawn = function(mob)
