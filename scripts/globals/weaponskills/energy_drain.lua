9b7910004644c5c604f28d6a8b5982432aee708a
Kayanesa <100643440+Kayanesa@users.noreply.github.com>
2022-03-17 00:45:26 -0400

    Style check

--- scripts/globals/weaponskills/energy_drain.lua
+++ scripts/globals/weaponskills/energy_drain.lua
@@ -8,25 +8,30 @@ require("scripts/globals/weaponskills")
 local weaponskill_object = {}
 
 weaponskill_object.onUseWeaponSkill = function(player, target, wsID, tp, primary, action, taChar)
-    -- TODO: Should not wake the mob involved. Not tested on sleeping mob. Need to add Darkness element and resistance. More testing needed to understand retail behavior. 
+    -- TODO: Should not wake the mob involved. Not tested on sleeping mob. Need to add Darkness element and resistance. More testing needed to understand retail behavior.
 
-    -- local damage = 60 -- Base mp absorbtion from http://wiki.ffo.jp/html/687.html saying it was between 60 and 120 and always higher than Energy Steal this is before adoulin era
+    --[[
+    local damage = 60 -- Base mp absorbtion from http://wiki.ffo.jp/html/687.html saying it was between 60 and 120 and always higher than Energy Steal this is before adoulin era
 
     -- params.ftp100 = 1 params.ftp200 = 1.5 params.ftp300 = 2 -- to follow earlier statement of been always higher than Energy Steal
     -- params.mnd_wsc = 1
 
-    -- local damagemod = damage * ((tp - 1000) / 2000 + 1) -- Simplified formula for getting difference in tp return from 1000 tp to 3000 tp
+    local damagemod = damage * ((tp - 1000) / 2000 + 1) -- Simplified formula for getting difference in tp return from 1000 tp to 3000 tp
 
-    -- if xi.settings.USE_ADOULIN_WEAPON_SKILL_CHANGES then
-    --     damagemod = damagemod + player:getStat(xi.mod.MND)
-    -- end
+    if xi.settings.USE_ADOULIN_WEAPON_SKILL_CHANGES then
+        damagemod = damagemod + player:getStat(xi.mod.MND)
+    end
 
-    -- damagemod = damagemod * xi.settings.WEAPON_SKILL_POWER
+    damagemod = damagemod * xi.settings.WEAPON_SKILL_POWER
 
-    -- if not target:isUndead() then
-    --     player:addMP(target:addMP(-damagemod))
-    -- end
+    if not target:isUndead() then
+        player:addMP(target:addMP(-damagemod))
+    end
 
+    if not target:isUndead() then
+        player:addMP(target:addMP(-damagemod))
+    end
+    --]]
     return 1, 0, false, 0
 end
 
--- scripts/globals/weaponskills/energy_steal.lua
+++ scripts/globals/weaponskills/energy_steal.lua
@@ -9,25 +9,30 @@ require("scripts/globals/msg")
 local weaponskill_object = {}
 
 weaponskill_object.onUseWeaponSkill = function(player, target, wsID, tp, primary, action, taChar)
-    -- TODO: Should not wake the mob involved. Not tested on sleeping mob. Need to add Darkness element and resistance. More testing needed to understand retail behavior. 
+    -- TODO: Should not wake the mob involved. Not tested on sleeping mob. Need to add Darkness element and resistance. More testing needed to understand retail behavior.
 
-    -- local damage = 40 -- Base mp absorbtion from http://wiki.ffo.jp/html/686.html saying it would do between 40 and 80 this is before adoulin era
+    --[[
+    local damage = 40 -- Base mp absorbtion from http://wiki.ffo.jp/html/686.html saying it would do between 40 and 80 this is before adoulin era
 
     -- params.ftp100 = 1 params.ftp200 = 1.5 params.ftp300 = 2  from https://ffxiclopedia.fandom.com/wiki/Energy_Steal
     -- params.mnd_wsc = 1
 
-    -- local damagemod = damage * ((tp - 1000) / 2000 + 1) -- Simplified formula for getting difference in tp return from 1000 tp to 3000 tp
+    local damagemod = damage * ((tp - 1000) / 2000 + 1) -- Simplified formula for getting difference in tp return from 1000 tp to 3000 tp
 
-    -- if xi.settings.USE_ADOULIN_WEAPON_SKILL_CHANGES then
-    --     damagemod = damagemod + player:getStat(xi.mod.MND)
-    -- end
+    if xi.settings.USE_ADOULIN_WEAPON_SKILL_CHANGES then
+        damagemod = damagemod + player:getStat(xi.mod.MND)
+    end
 
-    -- damagemod = damagemod * xi.settings.WEAPON_SKILL_POWER
+    damagemod = damagemod * xi.settings.WEAPON_SKILL_POWER
 
-    -- if not target:isUndead() then
-    --     player:addMP(target:addMP(-damagemod))
-    -- end
+    if not target:isUndead() then
+        player:addMP(target:addMP(-damagemod))
+    end
 
+    if not target:isUndead() then
+        player:addMP(target:addMP(-damagemod))
+    end
+    --]]
     return 1, 0, false, 0
 end
 
