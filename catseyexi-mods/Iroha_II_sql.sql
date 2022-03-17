7a29f2689b75aae850196d4cc4b032d9d1e81039
Tal Ben-Eliezer <beneliezer@gmail.com>
2022-03-17 15:57:54 -0400

    housekeeping?

--- catseyexi-mods/Iroha_II_sql.sql
+++
@@ -1,6 +0,0 @@
--- Mob Skill List
-INSERT INTO `mob_skill_lists` VALUES ('TRUST_Iroha_II',1133,3733); -- Amatsu Kyori
-INSERT INTO `mob_skill_lists` VALUES ('TRUST_Iroha_II',1133,3734); -- Amatsu Hanadoki
-INSERT INTO `mob_skill_lists` VALUES ('TRUST_Iroha_II',1133,3736); -- Amatsu Gachirin
-INSERT INTO `mob_skill_lists` VALUES ('TRUST_Iroha_II',1133,3737); -- Amatsu Suien
-INSERT INTO `mob_skill_lists` VALUES ('TRUST_Iroha_II',1133,3738); -- Rise from Ashes
