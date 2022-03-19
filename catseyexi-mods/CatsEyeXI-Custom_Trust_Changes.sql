fa14655d970f96d1ae0c26a85988507d5c5763cf
Tal Ben-Eliezer <beneliezer@gmail.com>
2022-03-19 01:21:44 -0400

    dsfcggsdf

--- catseyexi-mods/CatsEyeXI-Custom_Trust_Changes.sql
+++ catseyexi-mods/CatsEyeXI-Custom_Trust_Changes.sql
@@ -11,6 +11,11 @@ INSERT INTO `mob_skill_lists` VALUES ('TRUST_Halver',1087,116); -- Penta Thrust
 INSERT INTO `mob_skill_lists` VALUES ('TRUST_Halver',1087,120); -- Impulse Drive
 
 -- Add WS to ArkEv
+DELETE FROM mob_skill_lists WHERE skill_list_id = "1108"
+INSERT INTO `mob_skill_lists` VALUES ('TRUST_AAEV',1108,3711);      -- Vorpal Blade
+INSERT INTO `mob_skill_lists` VALUES ('TRUST_AAEV',1108,3713);      -- Chant du Cygne
+INSERT INTO `mob_skill_lists` VALUES ('TRUST_AAEV',1108,3712);      -- Dominion Slash
+INSERT INTO `mob_skill_lists` VALUES ('TRUST_AAEV',1108,3710);      -- Arrogance Incarnate
 UPDATE mob_skills SET mob_skill_aoe = '1', mob_anim_id = '642' WHERE mob_skill_id = '3710'; -- ArkEv Arrogance_Incarnate
 UPDATE mob_skills SET mob_anim_id = '236' WHERE mob_skill_id = '3711'; -- ArkEv Vorpal_Blade
 INSERT INTO `mob_skills` VALUES (3712,635,'dominion_slash',0,7.0,2000,1500,4,0,0,0,0,0,0); -- ArkEV Dominion_Slash
