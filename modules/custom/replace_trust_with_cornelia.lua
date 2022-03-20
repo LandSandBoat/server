7fadc7e82f9462c93ffcdf3dd80d9118246f6c60
Zach Toogood <zrtoogood@gmail.com>
2022-03-20 22:10:13 +0200

    [module] Make Cornelia replacement more configurable

--- modules/custom/replace_trust_with_cornelia.lua
+++ modules/custom/replace_trust_with_cornelia.lua
@@ -1,5 +1,5 @@
 -----------------------------------
--- Replace Trust: Kuyin Hathdenna with Trust: Cornelia
+-- Replace Trust with Trust: Cornelia (configurable)
 --
 -- The name of the spell itself can't be changed (would require client changes),
 -- but everything else can be changed!
@@ -7,10 +7,13 @@
 require("modules/module_utils")
 require("scripts/globals/trust")
 -----------------------------------
-local m = Module:new("replace_trust_kuyin_with_cornelia")
+local m = Module:new("replace_trust_with_cornelia")
 m:setEnabled(false)
 
-m:addOverride("xi.globals.spells.trust.kuyin_hathdenna.onSpellCast", function(caster, target, spell)
+-- This is the filename of the trust you're trying to replace, without the ".lua" extension
+local trustToReplaceName = "kuyin_hathdenna"
+
+m:addOverride(string.format("xi.globals.spells.trust.%s.onSpellCast", trustToReplaceName), function(caster, target, spell)
     -----------------------------------
     -- NOTE: This is the logic from xi.trust.spawn()
     -----------------------------------
@@ -40,7 +43,7 @@ m:addOverride("xi.globals.spells.trust.kuyin_hathdenna.onSpellCast", function(ca
     trust:setLocalVar("MASTER_ID", trust:getMaster():getID())
 end)
 
-m:addOverride("xi.globals.spells.trust.kuyin_hathdenna.onMobSpawn", function(mob)
+m:addOverride(string.format("xi.globals.spells.trust.%s.onMobSpawn", trustToReplaceName), function(mob)
     for _, member in ipairs(mob:getMaster():getParty()) do
         if member:isPC() then
             member:PrintToPlayer("Cornelia, at your service.", 4, "Cornelia") -- 4: MESSAGE_PARTY
@@ -48,7 +51,7 @@ m:addOverride("xi.globals.spells.trust.kuyin_hathdenna.onMobSpawn", function(mob
     end
 end)
 
-m:addOverride("xi.globals.spells.trust.kuyin_hathdenna.onMobDespawn", function(mob)
+m:addOverride(string.format("xi.globals.spells.trust.%s.onMobDespawn", trustToReplaceName), function(mob)
     -- NOTE: Apparently getMaster() returns nil by now, so we're going to get the master's ID that
     --     : we cached earlier
     local masterId = mob:getLocalVar("MASTER_ID")
@@ -60,7 +63,7 @@ m:addOverride("xi.globals.spells.trust.kuyin_hathdenna.onMobDespawn", function(m
     end
 end)
 
-m:addOverride("xi.globals.spells.trust.kuyin_hathdenna.onMobDeath", function(mob)
+m:addOverride(string.format("xi.globals.spells.trust.%s.onMobDeath", trustToReplaceName), function(mob)
     -- Intentionally blank
 end)
 
