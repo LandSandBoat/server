efea986f4ebb9e889469c177d4bf9c12aa44f226
WinterSolstice8 <60417494+WinterSolstice8@users.noreply.github.com>
2022-03-20 18:58:14 -0700

    Fix trailing whitespace

--- scripts/globals/spells/white/foil.lua
+++ scripts/globals/spells/white/foil.lua
@@ -11,7 +11,7 @@ spell_object.onMagicCastingCheck = function(caster, target, spell)
     return 0
 end
 
--- TODO: determine mechanics of how Foil's "Special Attack" evasion works. 
+-- TODO: determine mechanics of how Foil's "Special Attack" evasion works.
 -- Martel has a post about it here: https://www.bluegartr.com/threads/115399-Rune-Fencer-Findings?p=5665305&viewfull=1#post5665305
 -- More testing is required (such as determining accuracy of the target used for testing)
 spell_object.onSpellCast = function(caster, target, spell)
