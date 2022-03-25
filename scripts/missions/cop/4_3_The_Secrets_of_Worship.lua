11cdaac87a5f3fc50e93c09383b33d60eaf7a09d
claywar <claywar@beerlab.dev>
2022-03-25 07:17:36 -0400

    Interaction - Mission: The Secrets of Worship

---
+++ scripts/missions/cop/4_3_The_Secrets_of_Worship.lua
@@ -0,0 +1,185 @@
+-----------------------------------
+-- The Secrets of Worship
+-- Promathia 4-3
+-----------------------------------
+-- !addmission 6 428
+-- Justinius :
+-----------------------------------
+require('scripts/globals/interaction/mission')
+require('scripts/globals/missions')
+require('scripts/globals/titles')
+require('scripts/globals/zone')
+-----------------------------------
+local sacrariumID = require("scripts/zones/Sacrarium/IDs")
+-----------------------------------
+
+local mission = Mission:new(xi.mission.log_id.COP, xi.mission.id.cop.THE_SECRETS_OF_WORSHIP)
+
+mission.reward =
+{
+    nextMission = { xi.mission.log_id.COP, xi.mission.id.cop.SLANDEROUS_UTTERINGS },
+}
+
+mission.sections =
+{
+    {
+        check = function(player, currentMission, missionStatus, vars)
+            return currentMission == mission.missionId
+        end,
+
+        [xi.zone.TAVNAZIAN_SAFEHOLD] =
+        {
+            ['_0qa'] =
+            {
+                onTrigger = function(player, npc)
+                    if mission:getVar(player, 'Status') == 0 then
+                        return mission:progressEvent(111)
+                    end
+                end,
+            },
+
+            ['Justinius'] =
+            {
+                onTrigger = function(player, npc)
+                    if mission:getVar(player, 'Status') == 0 then
+                        return mission:event(570):importantEvent()
+                    end
+                end,
+            },
+
+            onEventFinish =
+            {
+                [111] = function(player, csid, option, npc)
+                    mission:setVar(player, 'Status', 2)
+                end,
+
+                [570] = function(player, csid, option, npc)
+                    if option == 1 then
+                        mission:setVar(player, 'Status', 1)
+                    end
+                end,
+            },
+        },
+
+        [xi.zone.MISAREAUX_COAST] =
+        {
+            ['_0p8'] =
+            {
+                onTrigger = function(player, npc)
+                    local missionStatus = mission:getVar(player, 'Status')
+
+                    if missionStatus == 2 then
+                        return mission:progressEvent(9)
+                    elseif missionStatus > 2 then
+                        return mission:event(502)
+                    end
+                end,
+            },
+
+            onEventFinish =
+            {
+                [9] = function(player, csid, option, npc)
+                    if option == 1 then
+                        mission:setVar(player, 'Status', 3)
+                        player:setPos(-220.075, -15.999, 79.634, 62, 28)
+                    end
+                end,
+
+                [502] = function(player, csid, option, npc)
+                    if option == 1 then
+                        player:setPos(-220.075, -15.999, 79.634, 62, 28)
+                    end
+                end,
+            },
+        },
+
+        [xi.zone.SACRARIUM] =
+        {
+            ['_0s8'] =
+            {
+                onTrigger = function(player, npc)
+                    local missionStatus = mission:getVar(player, 'Status')
+
+                    if player:getXPos() > 45 then
+                        if missionStatus == 3 then
+                            return mission:progressEvent(6, 0, xi.ki.RELIQUIARIUM_KEY)
+                        elseif missionStatus == 5 and player:hasKeyItem(xi.ki.RELIQUIARIUM_KEY) then
+                            return mission:progressEvent(5)
+                        end
+                    else
+                        return mission:messageSpecial(sacrariumID.text.CANNOT_OPEN_SIDE)
+                    end
+                end,
+            },
+
+            ['qm_professor_mariselle'] =
+            {
+                onTrigger = function(player, npc)
+                    local missionStatus = mission:getVar(player, 'Status')
+                    local isSpawnPoint = npc:getLocalVar('hasProfessorMariselle') == 1
+
+                    if
+                        missionStatus == 4 and
+                        not player:hasKeyItem(xi.ki.RELIQUIARIUM_KEY) and
+                        isSpawnPoint and
+                        npcUtil.popFromQM(player, npc, sacrariumID.mob.OLD_PROFESSOR_MARISELLE, { radius = 2, hide = 0 })
+                    then
+                        return mission:messageSpecial(sacrariumID.text.EVIL_PRESENCE)
+                    elseif
+                        mission:getLocalVar(player, 'hasKilled') == 1 and
+                        not player:hasKeyItem(xi.ki.RELIQUIARIUM_KEY)
+                    then
+                        npcUtil.giveKeyItem(player, xi.ki.RELIQUIARIUM_KEY)
+                        return mission:noAction()
+                    elseif isSpawnPoint then
+                        return mission:messageSpecial(sacrariumID.text.DRAWER_SHUT) -- TODO: Need to see if these messages are replaced after completing mission or if there's a default
+                    else
+                        player:messageSpecial(sacrariumID.text.DRAWER_OPEN)
+                        return mission:messageSpecial(sacrariumID.text.DRAWER_EMPTY)
+                    end
+                end,
+            },
+
+            ['Old_Professor_Mariselle'] =
+            {
+                onMobDeath = function(mob, player, isKiller, noKiller)
+                    if mission:getVar(player, 'Status') == 4 then
+                        mission:setLocalVar(player, 'hasKilled', 1)
+                    end
+                end,
+            },
+
+            onEventFinish =
+            {
+                [5] = function(player, csid, option, npc)
+                    mission:setVar(player, 'Status', 4)
+                end,
+
+                [6] = function(player, csid, option, npc)
+                    mission:complete(player)
+                end,
+            },
+        },
+    },
+
+    {
+        check = function(player, currentMission, missionStatus, vars)
+            return player:hasCompletedMission(mission.areaId, mission.missionId)
+        end,
+
+        ['_0s8'] =
+        {
+            onTrigger = function(player, npc)
+                if player:getXPos() > 45 then
+                    if player:hasKeyItem(xi.ki.RELIQUIARIUM_KEY) then
+                        player:startEvent(110)
+                    end
+                else
+                    return mission:messageSpecial(sacrariumID.text.CANNOT_OPEN_SIDE)
+                end
+            end,
+        },
+    },
+}
+
+return mission
