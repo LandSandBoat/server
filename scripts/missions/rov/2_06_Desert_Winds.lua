52fd7bb3bcad11c2dd97656c46d7db0663ffc9b8
Kayanesa <100643440+Kayanesa@users.noreply.github.com>
2022-03-24 20:01:24 -0400

    changed check on toauProgress

--- scripts/missions/rov/2_06_Desert_Winds.lua
+++ scripts/missions/rov/2_06_Desert_Winds.lua
@@ -41,7 +41,7 @@ mission.sections =
                     local toauProgress = player:getCurrentMission(xi.mission.log_id.TOAU) == xi.mission.id.toau.SEAL_OF_THE_SERPENT and 0 or 1
 
                     if toauProgress ~= 1 then
-                        toauProgress = player:getCurrentMission(xi.mission.log_id.TOAU) <= xi.mission.id.toau.ROYAL_PUPPETEER and 1 or 2
+                        toauProgress = player:getCurrentMission(xi.mission.log_id.TOAU) < xi.mission.id.toau.ROYAL_PUPPETEER and 1 or 2
                     end
 
                     -- I dont have enough knowledge of story to infer Ghatsad and upset tenzen trigger
