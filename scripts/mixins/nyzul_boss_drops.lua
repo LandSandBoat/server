cee93199f48752fb5399068a23a3515e3213e660
Xaver-DaRed <xaver.entropia@gmail.com>
2022-03-29 02:40:12 +0200

    Add missing Mixin

---
+++ scripts/mixins/nyzul_boss_drops.lua
@@ -0,0 +1,32 @@
+-----------------------------------
+-- Nyzul Boss Drop ID Mixin
+-----------------------------------
+require("scripts/globals/mixins")
+require("scripts/globals/status")
+-----------------------------------
+
+xi = xi or {}
+xi.mix = xi.mix or {}
+g_mixins = g_mixins or {}
+
+g_mixins.nyzul_boss_drop = function(mob)
+    mob:addListener("SPAWN", "NYZUL_BOSS_SPAWN", function(mob)
+        local instance = mob:getInstance()
+        local result   = instance:getLocalVar("Nyzul_Current_Floor")
+
+        if result == 20 then
+            mob:setDropID(7350)
+        elseif result == 40 then
+            mob:setDropID(7351)
+        elseif result == 60 then
+            mob:setDropID(7352)
+        elseif result == 80 then
+            mob:setDropID(7353)
+        elseif result == 100 then
+            mob:setDropID(7354)
+        end
+    end)
+end
+
+return g_mixins.nyzul_boss_drop
+
