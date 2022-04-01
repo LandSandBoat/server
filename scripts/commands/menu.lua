15ee96e167d767f49996cb80cb6c61b6fa8e4f7b
Zach Toogood <zrtoogood@gmail.com>
2022-04-02 01:03:07 +0300

    Format custom menu example

--- scripts/commands/menu.lua
+++ scripts/commands/menu.lua
@@ -10,18 +10,30 @@ cmdprops =
 }
 
 function onTrigger(player)
-    player:customMenu({ "Test Menu (Play Effect)",
-        { "Option 1: Hearts", function(playerArg)
-            playerArg:PrintToPlayer("Option 1 Selected", xi.msg.channel.NS_SAY)
-            playerArg:independentAnimation(playerArg, 251, 4) -- Hearts
-        end },
-        { "Option 2: Music Notes", function(playerArg)
-            playerArg:PrintToPlayer("Option 2 Selected", xi.msg.channel.NS_SAY)
-            playerArg:independentAnimation(playerArg, 252, 4) -- Music Notes
-        end },
-        { "Option 3: Lightbulb", function(playerArg)
-            playerArg:PrintToPlayer("Option 3 Selected", xi.msg.channel.NS_SAY)
-            playerArg:independentAnimation(playerArg, 250, 4) -- Lightbulb
-        end },
-    })
+    local menu =
+    {
+        "Test Menu (Play Effect)",
+        {
+            "Option 1: Hearts",
+            function(playerArg)
+                playerArg:PrintToPlayer("Option 1 Selected", xi.msg.channel.NS_SAY)
+                playerArg:independentAnimation(playerArg, 251, 4) -- Hearts
+            end,
+        },
+        {
+            "Option 2: Music Notes",
+            function(playerArg)
+                playerArg:PrintToPlayer("Option 2 Selected", xi.msg.channel.NS_SAY)
+                playerArg:independentAnimation(playerArg, 252, 4) -- Music Notes
+            end,
+        },
+        {
+            "Option 3: Lightbulb",
+            function(playerArg)
+                playerArg:PrintToPlayer("Option 3 Selected", xi.msg.channel.NS_SAY)
+                playerArg:independentAnimation(playerArg, 250, 4) -- Lightbulb
+            end,
+        },
+    }
+    player:customMenu(menu)
 end
