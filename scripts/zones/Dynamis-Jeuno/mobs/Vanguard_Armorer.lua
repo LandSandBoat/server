c4ad3ce84c7f36851dfd42d4bc76b61d0234ecd1
Kayanesa <100643440+Kayanesa@users.noreply.github.com>
2022-04-05 12:49:07 -0400

    [Dynamis Jeuno] Group Spawning
    Added a REFILL_MOB_GROUP to spawn monster in different grouping instead of all individual spawn. Created the table of possible spawn.
    All following documentation
    https://www.dropbox.com/s/p3zpuq93bs2vw90/Dynamis%20Jeuno%20IDs%20Groups%20Mechanics.zip?dl=0
    It also line up with retail video of the zone.
    Also added NM spawn condition for both group NM and Place holder NM type.
    Will solve #1108

--- scripts/zones/Dynamis-Jeuno/mobs/Vanguard_Armorer.lua
+++ scripts/zones/Dynamis-Jeuno/mobs/Vanguard_Armorer.lua
@@ -7,10 +7,20 @@ mixins =
     require("scripts/mixins/dynamis_beastmen"),
     require("scripts/mixins/job_special")
 }
+local ID = require("scripts/zones/Dynamis-Jeuno/IDs")
+require("scripts/globals/mobs")
+require("scripts/globals/dynamis")
 -----------------------------------
 local entity = {}
 
 entity.onMobDeath = function(mob, player, isKiller)
+    dynamis.refillMobGroupOnDeath(mob, player, isKiller, 600) -- 10 min
 end
 
+entity.onMobDespawn = function(mob)
+    xi.mob.phOnDespawn(mob, ID.mob.GABBLOX_MAGPIETONGUE_PH, 10, 1200) -- 20 minutes
+    xi.mob.phOnDespawn(mob, ID.mob.TUFFIX_LOGLIMBS_PH, 10, 1200) -- 20 minutes
+    xi.mob.phOnDespawn(mob, ID.mob.CLOKTIX_LONGNAIL_PH, 10, 1200) -- 20 minutes
+    xi.mob.phOnDespawn(mob, ID.mob.SCRUFFIX_SHAGGYCHEST_PH, 10, 1200) -- 20 minutes
+end
 return entity
