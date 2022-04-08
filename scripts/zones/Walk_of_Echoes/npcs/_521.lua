065aa1d2bbb0cd5406548938442431ba0d7c6f42
Kayanesa <100643440+Kayanesa@users.noreply.github.com>
2022-04-07 22:39:46 -0400

    [NPC] Walk of Echo Missin info change wrong value
    Adding Ornate door (_521)  to Walk of Echo change value of npc_list according to
    https://drive.google.com/file/d/166mZ1RlfvjfXplGfX6FwQL8jj-_2wraC/view
    
    tested !cs 27 was wrong before the change to npc_list and is now showing correctly

---
+++ scripts/zones/Walk_of_Echoes/npcs/_521.lua
@@ -0,0 +1,20 @@
+-----------------------------------
+-- Area: Walk Of Echoes
+--  NPC: Ornate Door
+-- !pos -700 -20.250 -303.399 0
+-----------------------------------
+local entity = {}
+
+entity.onTrade = function(player, npc, trade)
+end
+
+entity.onTrigger = function(player, npc)
+end
+
+entity.onEventUpdate = function(player, csid, option)
+end
+
+entity.onEventFinish = function(player, csid, option)
+end
+
+return entity
