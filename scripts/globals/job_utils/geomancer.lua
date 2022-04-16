8a5352da6f394068efb82a1769e964788cae280a
MrSent <mrsentientone@gmail.com>
2022-04-16 23:06:55 +0100

    indi calcs and potency calcs ect

--- scripts/globals/job_utils/geomancer.lua
+++ scripts/globals/job_utils/geomancer.lua
@@ -27,6 +27,18 @@ local loupanModels =
     dark    = 2865,
 }
 
+local indiVisualEffect =
+{
+    fire    = {allies = 0, enemies = 8 },
+    ice     = {allies = 1, enemies = 9 },
+    wind    = {allies = 2, enemies = 10},
+    earth   = {allies = 3, enemies = 11},
+    thunder = {allies = 4, enemies = 12},
+    water   = {allies = 5, enemies = 13},
+    light   = {allies = 6, enemies = 14},
+    dark    = {allies = 7, enemies = 15},
+}
+
 local geoData =
 {
     [xi.magic.spell.GEO_REGEN]      = {luopanModel = loupanModels.light,   effect = xi.effect.GEO_REGEN,               targetType = xi.auraTarget.ALLIES },
@@ -63,43 +75,71 @@ local geoData =
 
 local indiData =
 {
-    [xi.magic.spell.INDI_REGEN]  = {effect = xi.effect.GEO_REGEN,               targetType = xi.auraTarget.ALLIES },
-    [xi.magic.spell.INDI_POISON] = {effect = xi.effect.GEO_POISON,              targetType = xi.auraTarget.ENEMIES},
+    [xi.magic.spell.INDI_REGEN]      = {visualEffect = indiVisualEffect.light.allies,    effect = xi.effect.GEO_REGEN,               targetType = xi.auraTarget.ALLIES },
+    [xi.magic.spell.INDI_POISON]     = {visualEffect = indiVisualEffect.water.enemies,   effect = xi.effect.GEO_POISON,              targetType = xi.auraTarget.ENEMIES},
+    [xi.magic.spell.INDI_REFRESH]    = {visualEffect = indiVisualEffect.light.allies,    effect = xi.effect.GEO_REFRESH,             targetType = xi.auraTarget.ALLIES },
+    [xi.magic.spell.INDI_HASTE]      = {visualEffect = indiVisualEffect.wind.allies,     effect = xi.effect.GEO_HASTE,               targetType = xi.auraTarget.ALLIES },
+    [xi.magic.spell.INDI_STR]        = {visualEffect = indiVisualEffect.fire.allies,     effect = xi.effect.GEO_STR_BOOST,           targetType = xi.auraTarget.ALLIES },
+    [xi.magic.spell.INDI_DEX]        = {visualEffect = indiVisualEffect.thunder.allies,  effect = xi.effect.GEO_DEX_BOOST,           targetType = xi.auraTarget.ALLIES },
+    [xi.magic.spell.INDI_VIT]        = {visualEffect = indiVisualEffect.earth.allies,    effect = xi.effect.GEO_VIT_BOOST,           targetType = xi.auraTarget.ALLIES },
+    [xi.magic.spell.INDI_AGI]        = {visualEffect = indiVisualEffect.wind.allies,     effect = xi.effect.GEO_AGI_BOOST,           targetType = xi.auraTarget.ALLIES },
+    [xi.magic.spell.INDI_INT]        = {visualEffect = indiVisualEffect.ice.allies,      effect = xi.effect.GEO_INT_BOOST,           targetType = xi.auraTarget.ALLIES },
+    [xi.magic.spell.INDI_MND]        = {visualEffect = indiVisualEffect.water.allies,    effect = xi.effect.GEO_MND_BOOST,           targetType = xi.auraTarget.ALLIES },
+    [xi.magic.spell.INDI_CHR]        = {visualEffect = indiVisualEffect.light.allies,    effect = xi.effect.GEO_CHR_BOOST,           targetType = xi.auraTarget.ALLIES },
+    [xi.magic.spell.INDI_FURY]       = {visualEffect = indiVisualEffect.fire.allies,     effect = xi.effect.GEO_ATTACK_BOOST,        targetType = xi.auraTarget.ALLIES },
+    [xi.magic.spell.INDI_BARRIER]    = {visualEffect = indiVisualEffect.earth.allies,    effect = xi.effect.GEO_DEFENSE_BOOST,       targetType = xi.auraTarget.ALLIES },
+    [xi.magic.spell.INDI_ACUMEN]     = {visualEffect = indiVisualEffect.ice.allies,      effect = xi.effect.GEO_MAGIC_ATK_BOOST,     targetType = xi.auraTarget.ALLIES },
+    [xi.magic.spell.INDI_FEND]       = {visualEffect = indiVisualEffect.water.allies,    effect = xi.effect.GEO_MAGIC_DEF_BOOST,     targetType = xi.auraTarget.ALLIES },
+    [xi.magic.spell.INDI_PRECISION]  = {visualEffect = indiVisualEffect.thunder.allies,  effect = xi.effect.GEO_ACCURACY_BOOST,      targetType = xi.auraTarget.ALLIES },
+    [xi.magic.spell.INDI_VOIDANCE]   = {visualEffect = indiVisualEffect.wind.allies,     effect = xi.effect.GEO_EVASION_BOOST,       targetType = xi.auraTarget.ALLIES },
+    [xi.magic.spell.INDI_FOCUS]      = {visualEffect = indiVisualEffect.dark.allies,     effect = xi.effect.GEO_MAGIC_ACC_BOOST,     targetType = xi.auraTarget.ALLIES },
+    [xi.magic.spell.INDI_ATTUNEMENT] = {visualEffect = indiVisualEffect.light.allies,    effect = xi.effect.GEO_MAGIC_EVASION_BOOST, targetType = xi.auraTarget.ALLIES },
+    [xi.magic.spell.INDI_WILT]       = {visualEffect = indiVisualEffect.water.enemies,   effect = xi.effect.GEO_ATTACK_DOWN,         targetType = xi.auraTarget.ENEMIES},
+    [xi.magic.spell.INDI_FRAILTY]    = {visualEffect = indiVisualEffect.wind.enemies,    effect = xi.effect.GEO_DEFENSE_DOWN,        targetType = xi.auraTarget.ENEMIES},
+    [xi.magic.spell.INDI_FADE]       = {visualEffect = indiVisualEffect.fire.enemies,    effect = xi.effect.GEO_MAGIC_ATK_DOWN,      targetType = xi.auraTarget.ENEMIES},
+    [xi.magic.spell.INDI_MALAISE]    = {visualEffect = indiVisualEffect.thunder.enemies, effect = xi.effect.GEO_MAGIC_DEF_DOWN,      targetType = xi.auraTarget.ENEMIES},
+    [xi.magic.spell.INDI_SLIP]       = {visualEffect = indiVisualEffect.earth.enemies,   effect = xi.effect.GEO_ACCURACY_DOWN,       targetType = xi.auraTarget.ENEMIES},
+    [xi.magic.spell.INDI_TORPOR]     = {visualEffect = indiVisualEffect.ice.enemies,     effect = xi.effect.GEO_EVASION_DOWN,        targetType = xi.auraTarget.ENEMIES},
+    [xi.magic.spell.INDI_VEX]        = {visualEffect = indiVisualEffect.light.enemies,   effect = xi.effect.GEO_MAGIC_ACC_DOWN,      targetType = xi.auraTarget.ENEMIES},
+    [xi.magic.spell.INDI_LANGUOR]    = {visualEffect = indiVisualEffect.dark.enemies,    effect = xi.effect.GEO_MAGIC_EVASION_DOWN,  targetType = xi.auraTarget.ENEMIES},
+    [xi.magic.spell.INDI_SLOW]       = {visualEffect = indiVisualEffect.earth.enemies,   effect = xi.effect.GEO_SLOW,                targetType = xi.auraTarget.ENEMIES},
+    [xi.magic.spell.INDI_PARALYSIS]  = {visualEffect = indiVisualEffect.ice.enemies,     effect = xi.effect.GEO_PARALYSIS,           targetType = xi.auraTarget.ENEMIES},
+    [xi.magic.spell.INDI_GRAVITY]    = {visualEffect = indiVisualEffect.wind.enemies,    effect = xi.effect.GEO_WEIGHT,              targetType = xi.auraTarget.ENEMIES},
 }
 
 
 local potencyData =
 {
-    [xi.effect.GEO_REGEN]               = {divisor =  20, minPotency = 1.0, maxPotency = 30.0, geoModMultiplier = 2.0},
-    [xi.effect.GEO_POISON]              = {divisor =  20, minPotency = 1.0, maxPotency = 30.0, geoModMultiplier = 3.0},
-    [xi.effect.GEO_REFRESH]             = {divisor = 120, minPotency = 1.0, maxPotency =  6.0, geoModMultiplier = 1.0},
-    [xi.effect.GEO_STR_BOOST]           = {divisor =  37, minPotency = 1.0, maxPotency = 25.0, geoModMultiplier = 2.0},
-    [xi.effect.GEO_DEX_BOOST]           = {divisor =  37, minPotency = 1.0, maxPotency = 25.0, geoModMultiplier = 2.0},
-    [xi.effect.GEO_VIT_BOOST]           = {divisor =  37, minPotency = 1.0, maxPotency = 25.0, geoModMultiplier = 2.0},
-    [xi.effect.GEO_AGI_BOOST]           = {divisor =  37, minPotency = 1.0, maxPotency = 25.0, geoModMultiplier = 2.0},
-    [xi.effect.GEO_INT_BOOST]           = {divisor =  37, minPotency = 1.0, maxPotency = 25.0, geoModMultiplier = 2.0},
-    [xi.effect.GEO_MND_BOOST]           = {divisor =  37, minPotency = 1.0, maxPotency = 25.0, geoModMultiplier = 2.0},
-    [xi.effect.GEO_CHR_BOOST]           = {divisor =  37, minPotency = 1.0, maxPotency = 25.0, geoModMultiplier = 2.0},
-    [xi.effect.GEO_ATTACK_BOOST]        = {divisor =  30, minPotency = 4.6, maxPotency = 34.7, geoModMultiplier = 2.7},
-    [xi.effect.GEO_DEFENSE_BOOST]       = {divisor =  30, minPotency = 9.7, maxPotency = 39.8, geoModMultiplier = 4.6},
-    [xi.effect.GEO_MAGIC_ATK_BOOST]     = {divisor =  75, minPotency = 3.0, maxPotency = 15.0, geoModMultiplier = 3.0},
-    [xi.effect.GEO_MAGIC_DEF_BOOST]     = {divisor =  60, minPotency = 5.0, maxPotency = 20.0, geoModMultiplier = 4.0},
-    [xi.effect.GEO_ACCURACY_BOOST]      = {divisor =  18, minPotency = 1.0, maxPotency = 50.0, geoModMultiplier = 5.0},
-    [xi.effect.GEO_EVASION_BOOST]       = {divisor =  14, minPotency = 1.0, maxPotency = 65.0, geoModMultiplier = 5.0},
-    [xi.effect.GEO_MAGIC_ACC_BOOST]     = {divisor =  18, minPotency = 1.0, maxPotency = 50.0, geoModMultiplier = 5.0},
-    [xi.effect.GEO_MAGIC_EVASION_BOOST] = {divisor =  14, minPotency = 1.0, maxPotency = 65.0, geoModMultiplier = 6.0},
-    [xi.effect.GEO_ATTACK_DOWN]         = {divisor =  44, minPotency = 4.6, maxPotency = 25.0, geoModMultiplier = 4.6},
-    [xi.effect.GEO_DEFENSE_DOWN]        = {divisor =  74, minPotency = 2.7, maxPotency = 14.8, geoModMultiplier = 2.7},
-    [xi.effect.GEO_MAGIC_ATK_DOWN]      = {divisor =  60, minPotency = 5.0, maxPotency = 20.0, geoModMultiplier = 4.0},
-    [xi.effect.GEO_MAGIC_DEF_DOWN]      = {divisor =  75, minPotency = 3.0, maxPotency = 15.0, geoModMultiplier = 3.0},
-    [xi.effect.GEO_ACCURACY_DOWN]       = {divisor =  14, minPotency = 1.0, maxPotency = 65.0, geoModMultiplier = 6.0},
-    [xi.effect.GEO_EVASION_DOWN]        = {divisor =  18, minPotency = 1.0, maxPotency = 50.0, geoModMultiplier = 5.0},
-    [xi.effect.GEO_MAGIC_ACC_DOWN]      = {divisor =  14, minPotency = 1.0, maxPotency = 65.0, geoModMultiplier = 6.0},
-    [xi.effect.GEO_MAGIC_EVASION_DOWN]  = {divisor =  18, minPotency = 1.0, maxPotency = 50.0, geoModMultiplier = 5.0},
-    [xi.effect.GEO_SLOW]                = {divisor =  64, minPotency = 0.9, maxPotency = 14.9, geoModMultiplier = 0.5},
-    [xi.effect.GEO_PARALYSIS]           = {divisor =  64, minPotency = 1.0, maxPotency = 15.0, geoModMultiplier = 1.0},
-    [xi.effect.GEO_WEIGHT]              = {divisor =  56, minPotency = 3.9, maxPotency = 19.9, geoModMultiplier = 1.1},
-    [xi.effect.GEO_HASTE]               = {divisor =  30, minPotency = 2.4, maxPotency = 29.9, geoModMultiplier = 1.1},
+    [xi.effect.GEO_REGEN]               = {divisor =  20.00, minPotency = 1.0, maxPotency = 30.0, geoModMultiplier = 2.0},
+    [xi.effect.GEO_POISON]              = {divisor =  20.00, minPotency = 1.0, maxPotency = 30.0, geoModMultiplier = 3.0},
+    [xi.effect.GEO_REFRESH]             = {divisor = 150.00, minPotency = 1.0, maxPotency =  6.0, geoModMultiplier = 1.0},
+    [xi.effect.GEO_STR_BOOST]           = {divisor =  36.00, minPotency = 1.0, maxPotency = 25.0, geoModMultiplier = 2.0},
+    [xi.effect.GEO_DEX_BOOST]           = {divisor =  36.00, minPotency = 1.0, maxPotency = 25.0, geoModMultiplier = 2.0},
+    [xi.effect.GEO_VIT_BOOST]           = {divisor =  36.00, minPotency = 1.0, maxPotency = 25.0, geoModMultiplier = 2.0},
+    [xi.effect.GEO_AGI_BOOST]           = {divisor =  36.00, minPotency = 1.0, maxPotency = 25.0, geoModMultiplier = 2.0},
+    [xi.effect.GEO_INT_BOOST]           = {divisor =  36.00, minPotency = 1.0, maxPotency = 25.0, geoModMultiplier = 2.0},
+    [xi.effect.GEO_MND_BOOST]           = {divisor =  36.00, minPotency = 1.0, maxPotency = 25.0, geoModMultiplier = 2.0},
+    [xi.effect.GEO_CHR_BOOST]           = {divisor =  36.00, minPotency = 1.0, maxPotency = 25.0, geoModMultiplier = 2.0},
+    [xi.effect.GEO_ATTACK_BOOST]        = {divisor =  25.93, minPotency = 4.6, maxPotency = 34.7, geoModMultiplier = 2.7},
+    [xi.effect.GEO_DEFENSE_BOOST]       = {divisor =  22.61, minPotency = 9.7, maxPotency = 39.8, geoModMultiplier = 4.6},
+    [xi.effect.GEO_MAGIC_ATK_BOOST]     = {divisor =  60.00, minPotency = 3.0, maxPotency = 15.0, geoModMultiplier = 3.0},
+    [xi.effect.GEO_MAGIC_DEF_BOOST]     = {divisor =  45.00, minPotency = 5.0, maxPotency = 20.0, geoModMultiplier = 4.0},
+    [xi.effect.GEO_ACCURACY_BOOST]      = {divisor =  18.00, minPotency = 1.0, maxPotency = 50.0, geoModMultiplier = 5.0},
+    [xi.effect.GEO_EVASION_BOOST]       = {divisor =  13.84, minPotency = 1.0, maxPotency = 65.0, geoModMultiplier = 5.0},
+    [xi.effect.GEO_MAGIC_ACC_BOOST]     = {divisor =  18.00, minPotency = 1.0, maxPotency = 50.0, geoModMultiplier = 5.0},
+    [xi.effect.GEO_MAGIC_EVASION_BOOST] = {divisor =  13.84, minPotency = 1.0, maxPotency = 65.0, geoModMultiplier = 6.0},
+    [xi.effect.GEO_ATTACK_DOWN]         = {divisor =  36.00, minPotency = 4.6, maxPotency = 25.0, geoModMultiplier = 4.6},
+    [xi.effect.GEO_DEFENSE_DOWN]        = {divisor =  60.81, minPotency = 2.7, maxPotency = 14.8, geoModMultiplier = 2.7},
+    [xi.effect.GEO_MAGIC_ATK_DOWN]      = {divisor =  45.00, minPotency = 5.0, maxPotency = 20.0, geoModMultiplier = 4.0},
+    [xi.effect.GEO_MAGIC_DEF_DOWN]      = {divisor =  60.00, minPotency = 3.0, maxPotency = 15.0, geoModMultiplier = 3.0},
+    [xi.effect.GEO_ACCURACY_DOWN]       = {divisor =  13.84, minPotency = 1.0, maxPotency = 65.0, geoModMultiplier = 6.0},
+    [xi.effect.GEO_EVASION_DOWN]        = {divisor =  18.00, minPotency = 1.0, maxPotency = 50.0, geoModMultiplier = 5.0},
+    [xi.effect.GEO_MAGIC_ACC_DOWN]      = {divisor =  13.84, minPotency = 1.0, maxPotency = 65.0, geoModMultiplier = 6.0},
+    [xi.effect.GEO_MAGIC_EVASION_DOWN]  = {divisor =  18.00, minPotency = 1.0, maxPotency = 50.0, geoModMultiplier = 5.0},
+    [xi.effect.GEO_SLOW]                = {divisor =  60.4,  minPotency = 0.9, maxPotency = 14.9, geoModMultiplier = 0.5},
+    [xi.effect.GEO_PARALYSIS]           = {divisor =  60.00, minPotency = 1.0, maxPotency = 15.0, geoModMultiplier = 1.0},
+    [xi.effect.GEO_WEIGHT]              = {divisor =  45.22, minPotency = 3.9, maxPotency = 19.9, geoModMultiplier = 1.1},
+    [xi.effect.GEO_HASTE]               = {divisor =  30.1,  minPotency = 2.4, maxPotency = 29.9, geoModMultiplier = 1.1},
 }
 
 xi.job_utils.geomancer.geoOnAbilityCheck = function(player, target, ability)
@@ -142,7 +182,7 @@ local function getEffectPotency(player, effect)
         handbellSkill = 0
     end
 
-    player:PrintToPlayer(string.format("Geo Skill: (%i), Handbell Skill: (%i), Geomancy Mod: (%i)",geoSkill, handbellSkill, geomancyMod), 15)
+    player:PrintToPlayer(string.format("Skill: (%i), Geomancy Mod: (%i)",geoSkill + handbellSkill, geomancyMod), 15)
 
     local combinedSkillLevel = utils.clamp(handbellSkill + geoSkill, 0, 900)
     local divisor            = potencyData[effect].divisor
@@ -155,7 +195,7 @@ local function getEffectPotency(player, effect)
         potency = potency + (geomancyMod * potencyData[effect].geoModMultiplier)
     end
 
-    return math.floor(potency)
+    return potency --math.floor(potency)
 end
 
 -----------------------------------
@@ -177,11 +217,12 @@ xi.job_utils.geomancer.doIndiSpell = function(caster, target, spell)
     local effect       = indiData[spellID].effect
     local potency      = getEffectPotency(caster, effect)
     local targetType   = indiData[spellID].targetType
+    local visualEffect = indiData[spellID].visualEffect
     local duration = 180 -- TODO: add mod for duration
 
-    xi.job_utils.geomancer.addAura(target, duration, effect, potency, targetType)
-    caster:PrintToPlayer(string.format("Potency: (%i)", potency), 15)
-    return xi.effect.COLURE_ACTIVE
+    target:addStatusEffectEx(xi.effect.COLURE_ACTIVE, xi.effect.COLURE_ACTIVE, visualEffect, 3, duration, effect, potency, targetType, xi.effectFlag.AURA)
+    caster:PrintToPlayer(string.format("Potency: (%f)", potency), 15)
+    return effect
 end
 
 
