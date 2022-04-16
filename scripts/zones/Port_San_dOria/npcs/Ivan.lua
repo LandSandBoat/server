3397ead35b6b2dd4be658e851e8c116bceb9d625
claywar <claywar@beerlab.dev>
2022-04-16 07:56:20 -0400

    [abyssea] Implement City NPCs, add Lights settings

--- scripts/zones/Port_San_dOria/npcs/Ivan.lua
+++ scripts/zones/Port_San_dOria/npcs/Ivan.lua
@@ -1,25 +1,26 @@
 -----------------------------------
--- Area: Port Windurst
---  NPC: Fabricius
--- Type: Abyssea Service NPC
+-- Area: Port San d'Oria
+--  NPC: Ivan
+-- Type: Abyssea Warp NPC
+-- !pos 8.27 -4 -65 232
 -----------------------------------
 require("scripts/globals/abyssea")
-----------------------------------
+-----------------------------------
 local entity = {}
 
 entity.onTrade = function(player, npc, trade)
 end
 
 entity.onTrigger = function(player, npc)
-    xi.abyssea.traverserNPCOnTrigger(player, npc)
+    xi.abyssea.warpNPCOnTrigger(player, npc)
 end
 
 entity.onEventUpdate = function(player, csid, option)
-    xi.abyssea.traverserNPCOnUpdate(player, csid, option)
+    xi.abyssea.warpNPCOnEventUpdate(player, csid, option)
 end
 
 entity.onEventFinish = function(player, csid, option, npc)
-    xi.abyssea.traverserNPCOnEventFinish(player, csid, option, npc)
+    xi.abyssea.warpNPCOnEventFinish(player, csid, option, npc)
 end
 
 return entity
