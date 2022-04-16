8bf743c2665121c61709fae8ba3c6e2246899bcb
claywar <claywar@beerlab.dev>
2022-04-16 11:23:14 -0400

    [abyssea] Konschtat NPCs

--- scripts/zones/Abyssea-Konschtat/npcs/qm_alkonost.lua
+++ scripts/zones/Abyssea-Konschtat/npcs/qm_alkonost.lua
@@ -1,19 +1,21 @@
 -----------------------------------
 -- Zone: Abyssea-Konschtat
---  NPC: ???
+--  NPC: qm_alkonost (???)
 -- Spawns Alkonost
 -- !pos 54.000 30.654 414.000 15
 -----------------------------------
-require("scripts/globals/abyssea")
+require('scripts/globals/abyssea')
+require('scripts/globals/items')
 -----------------------------------
 local entity = {}
 
 entity.onTrade = function(player, npc, trade)
-    xi.abyssea.qmOnTrade(player, npc, trade)
+    local ID = zones[player:getZoneID()]
+    xi.abyssea.qmOnTrade(player, npc, trade, ID.mob.ALKONOST, { xi.items.GIANT_BUGARD_TUSK })
 end
 
 entity.onTrigger = function(player, npc)
-    xi.abyssea.qmOnTrigger(player, npc)
+    xi.abyssea.qmOnTrigger(player, npc, 0, 0, { xi.items.GIANT_BUGARD_TUSK })
 end
 
 entity.onEventUpdate = function(player, csid, option)
--- scripts/zones/Abyssea-Konschtat/npcs/qm_fear_gorta.lua
+++ scripts/zones/Abyssea-Konschtat/npcs/qm_fear_gorta.lua
@@ -1,19 +1,21 @@
 -----------------------------------
 -- Zone: Abyssea-Konschtat
---  NPC: ???
+--  NPC: qm_fear_gorta (???)
 -- Spawns Fear Gorta
 -- !pos 630.000 33.608 410.000 15
 -----------------------------------
-require("scripts/globals/abyssea")
+require('scripts/globals/abyssea')
+require('scripts/globals/items')
 -----------------------------------
 local entity = {}
 
 entity.onTrade = function(player, npc, trade)
-    xi.abyssea.qmOnTrade(player, npc, trade)
+    local ID = zones[player:getZoneID()]
+    xi.abyssea.qmOnTrade(player, npc, trade, ID.mob.FEAR_GORTA, { xi.items.SQUARE_OF_MOONGLOW_CLOTH })
 end
 
 entity.onTrigger = function(player, npc)
-    xi.abyssea.qmOnTrigger(player, npc)
+    xi.abyssea.qmOnTrigger(player, npc, 0, 0, { xi.items.SQUARE_OF_MOONGLOW_CLOTH })
 end
 
 entity.onEventUpdate = function(player, csid, option)
