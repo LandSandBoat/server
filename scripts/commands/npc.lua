f70391f34f7273a028ee28e8731f09801958cc50
beneliezer <beneliezer@gmail.com>
2022-04-21 09:40:36 -0400

    fix more styling issues and resolve shadowing of arguments

--- scripts/commands/npc.lua
+++ scripts/commands/npc.lua
@@ -1,6 +1,6 @@
 -----------------------------------
 -- func: !npc
--- desc: Summon a NPC
+-- desc: Test dynamic entity before its placed into a module for testing.
 -- note: Will spawn after you move from your current position
 -----------------------------------
 
@@ -10,7 +10,7 @@ cmdprops =
     parameters = ""
 }
 
-function onTrigger(player, npc)
+function onTrigger(player)
     local zone = player:getZone()
 
     local npc = zone:insertDynamicEntity({
@@ -23,13 +23,13 @@ function onTrigger(player, npc)
         --     : You can then hide the name with entity:hideName(true)
         -- NOTE: This name CAN include spaces and underscores.
         name = "Test",
-       
+
         -- You can use regular model ids (See documentation/model_ids.txt, or play around with !costume)
         look = 2430,
 
         -- You can also use the raw packet look information (as a string), as seen in npc_list and mob_pools
         -- look = "0x0100020500101120003000400050006000700000",
-       
+
         -- Set the NPC at your current position, can set the position using in-game x, y and z if desired.
         x = player:getXPos(),
         y = player:getYPos(),
@@ -38,18 +38,19 @@ function onTrigger(player, npc)
 
         -- onTrade and onTrigger can be hooked up to your dynamic entity,
         -- just like with regular entities. You can also omit these.
-        onTrade = function(player, npc, trade)
+        onTrade = function(playerArg, npcArg, trade)
             -- NOTE: We have to use getPacketName, because the regular name is modified and being used
             --     : for internal lookups
-            player:PrintToPlayer("No, thanks!", 0, npc:getPacketName())
+            player:PrintToPlayer("No, thanks!", 0, npcArg:getPacketName())
         end,
 
         -- The entity will not be "triggerable" unless you populate onTrigger
-        onTrigger = function(player, npc)
+        onTrigger = function(playerArg, npcArg)
             -- NOTE: We have to use getPacketName, because the regular name is modified and being used
             --     : for internal lookups
-            player:PrintToPlayer("Thank you I am free now!", 0, npc:getPacketName())
+            player:PrintToPlayer("Thank you I am free now!", 0, npcArg:getPacketName())
         end,
     })
     player:PrintToPlayer(string.format("Please move to spawn (%s)", npc:getPacketName()))
 end
+
