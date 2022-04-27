1c765fd01aa9ec6c03226f482711a82bd6e192a0
claywar <claywar@beerlab.dev>
2022-04-27 17:39:51 -0400

    Interaction - Missions: RoV Reunited through From the Ruins

---
+++ scripts/missions/rov/2_11_Take_Wing.lua
@@ -0,0 +1,49 @@
+-----------------------------------
+-- Take Wing
+-- Rhapsodies of Vana'diel Mission 2-7
+-----------------------------------
+-- !addmission 13 66
+-----------------------------------
+require('scripts/globals/items')
+require('scripts/globals/missions')
+require('scripts/globals/rhapsodies')
+require('scripts/globals/zone')
+require('scripts/globals/interaction/mission')
+-----------------------------------
+
+local mission = Mission:new(xi.mission.log_id.ROV, xi.mission.id.rov.TAKE_WING)
+
+mission.reward =
+{
+    nextMission = { xi.mission.log_id.ROV, xi.mission.id.rov.PRIME_NUMBER },
+}
+
+mission.sections =
+{
+    {
+        check = function(player, currentMission, missionStatus, vars)
+            return currentMission == mission.missionId
+        end,
+
+        [xi.zone.AHT_URHGAN_WHITEGATE] =
+        {
+            onRegionEnter =
+            {
+                [5] = function(player, region)
+                    local hasCompletedPath = player:hasCompletedMission(xi.mission.log_id.COP, xi.mission.id.cop.THE_WARRIOR_S_PATH) and 1 or 0
+
+                    return mission:progressEvent(168 { [0] = hasCompletedPath, text_table = 0)
+                end,
+            },
+
+            onEventFinish =
+            {
+                [168] = function(player, csid, option, npc)
+                    mission:complete(player)
+                end,
+            },
+        },
+    },
+}
+
+return mission
