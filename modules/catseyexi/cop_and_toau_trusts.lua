105b5cac0290a242538010da10a2c0cd0cbf0204
Tal Ben-Eliezer <beneliezer@gmail.com>
2022-05-12 21:09:32 -0400

    add gessho to trusts and rename module

--- modules/catseyexi/cop_and_toau_trusts.lua
+++ modules/catseyexi/cop_and_toau_trusts.lua
@@ -26,4 +26,14 @@ m:addOverride("xi.zones.Misareaux_Coast.npcs._0p0.onTrigger", function(player, n
 	player:messageSpecial(ID.text.SNOWMINT_POINT_LOCKED)
 end)
 
+m:addOverride("xi.zones.Tavnazian_Safehold.npcs.Cushion.onTrigger", function(player, npc)
+    if player:getCurrentMission(TOAU) >= xi.mission.id.toau.PASSING_GLORY then
+        player:addSpell(918) -- Gessho
+        player:PrintToPlayer("Unlocked Trust: Gessho")
+    end
+
+    player:PrintToPlayer("Unlocked Trust: Gessho")
+end)
+
+
 return m
