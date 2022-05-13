9d17da6d734edf3b97c1b77d6b5e9f64443f0483
claywar <claywar@beerlab.dev>
2022-05-13 10:41:18 -0400

    Interaction - Quest: A Certain Substitute Patrolman
    Fix title comment

--- scripts/zones/Western_Adoulin/npcs/Nylene.lua
+++ scripts/zones/Western_Adoulin/npcs/Nylene.lua
@@ -6,7 +6,6 @@
 -- !pos 12 0 -82 256
 -----------------------------------
 require("scripts/globals/missions")
-require("scripts/globals/quests")
 -----------------------------------
 local entity = {}
 
@@ -14,16 +13,9 @@ entity.onTrade = function(player, npc, trade)
 end
 
 entity.onTrigger = function(player, npc)
-    local ACSP = player:getQuestStatus(xi.quest.log_id.ADOULIN, xi.quest.id.adoulin.A_CERTAIN_SUBSTITUTE_PATROLMAN)
-    local SOA_Mission = player:getCurrentMission(xi.mission.log_id.SOA)
-    if (SOA_Mission >= xi.mission.id.soa.LIFE_ON_THE_FRONTIER) then
-        if ((ACSP == QUEST_ACCEPTED) and (player:getCharVar("ACSP_NPCs_Visited") == 7)) then
-            -- Progresses Quest: 'A Certain Substitute Patrolman'
-            player:startEvent(2559)
-        else
-            -- Standard dialogue
-            player:startEvent(562)
-        end
+    if player:getCurrentMission(xi.mission.log_id.SOA) >= xi.mission.id.soa.LIFE_ON_THE_FRONTIER then
+        -- Standard dialogue
+        player:startEvent(562)
     else
         -- Dialogue prior to joining colonization effort
         player:startEvent(533)
@@ -34,10 +26,6 @@ entity.onEventUpdate = function(player, csid, option)
 end
 
 entity.onEventFinish = function(player, csid, option)
-    if (csid == 2559) then
-        -- Progresses Quest: 'A Certain Substitute Patrolman'
-        player:setCharVar("ACSP_NPCs_Visited", 8)
-    end
 end
 
 return entity
