4e5095b61e94145759b2f0a394f3c181925f7627
Kain <26943220+kaincenteno@users.noreply.github.com>
2022-05-19 11:58:13 -0700

    Purakoko pathing
    adds 3 points that Purakoko uses for pathing

--- scripts/zones/Windurst_Walls/npcs/Purakoko.lua
+++ scripts/zones/Windurst_Walls/npcs/Purakoko.lua
@@ -3,7 +3,29 @@
 --  NPC: Purakoko
 -- Working 100%
 -----------------------------------
+require("scripts/globals/pathfind")
+-----------------------------------
 local entity = {}
+
+-- TODO: Purakoko randomely chooses the following when pathing:
+-- Sometimes waits at a point 1-5 seconds.
+-- Sometimes before/after waiting, rotates to look at a point.
+-- Sometimes she skips going to 3rd point.
+local path =
+{
+-72.580, -10.000, 115.887,
+-65.567, -10.786, 120.000,
+-59.242, -12.416, 123.703,
+}
+
+entity.onSpawn = function(npc)
+    npc:initNpcAi()
+    npc:setPos(xi.path.first(path))
+end
+
+entity.onPath = function(npc)
+    xi.path.patrol(npc, path)
+end
 
 entity.onTrade = function(player, npc, trade)
 end
