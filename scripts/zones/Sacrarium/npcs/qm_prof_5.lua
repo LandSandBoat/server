bc56e1dc727d4787e3eb8828693e6cbd044d1012
DESKTOP-J453B6L\EZSCH <ezschneider1@gmail.com>
2022-05-19 22:37:06 -0400

    Initial CoP Mechanics - The Secrets of Worship - Old Professor Mariselles and Pupils

---
+++ scripts/zones/Sacrarium/npcs/qm_prof_5.lua
@@ -0,0 +1,35 @@
+-----------------------------------
+-- Area: Sacrarium
+--  NPC: qm_professor_mariselle (???)
+-- Notes: Used to spawn Old Prof. Mariselle
+-- !pos 22.669 -3.111 -127.318 28
+-----------------------------------
+local ID = require("scripts/zones/Sacrarium/IDs")
+-----------------------------------
+local entity = {}
+
+entity.onTrade = function(player, npc, trade)
+end
+
+entity.onTrigger = function(player, npc)
+    local isSpawnPoint = npc:getLocalVar('hasProfessorMariselle') == 1
+
+    if isSpawnPoint == true then
+        print('check')
+    end
+
+    if isSpawnPoint then
+        player:messageSpecial(ID.text.DRAWER_SHUT)
+    else
+        player:messageSpecial(ID.text.DRAWER_OPEN)
+        player:messageSpecial(ID.text.DRAWER_EMPTY)
+    end
+end
+
+entity.onEventUpdate = function(player, csid, option)
+end
+
+entity.onEventFinish = function(player, csid, option)
+end
+
+return entity
