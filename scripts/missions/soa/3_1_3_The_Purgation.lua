5709dcda4aef08bf2940f966bc5a47d75198aff2
claywar <claywar@beerlab.dev>
2022-05-21 15:20:46 -0400

    Interaction - Mission: The Purgation

---
+++ scripts/missions/soa/3_1_3_The_Purgation.lua
@@ -0,0 +1,56 @@
+-----------------------------------
+-- The Purgation
+-- Seekers of Adoulin M3-1-3
+-----------------------------------
+-- !addmission 12 40
+-- Erminold : !pos 50.949 -40 -90.942 257
+-----------------------------------
+require('scripts/globals/missions')
+require('scripts/globals/interaction/mission')
+require('scripts/globals/zone')
+-----------------------------------
+
+local mission = Mission:new(xi.mission.log_id.SOA, xi.mission.id.soa.THE_PURGATION)
+
+mission.reward =
+{
+    nextMission = { xi.mission.log_id.SOA, xi.mission.id.soa.THE_KEY },
+}
+
+mission.sections =
+{
+    {
+        check = function(player, currentMission, missionStatus, vars)
+            return currentMission == mission.missionId
+        end,
+
+        [xi.zone.WESTERN_ADOULIN] =
+        {
+            -- TODO: Continue to check if Levil's dialogue changes.  This event has been
+            -- repeated for several missions.
+
+            ['Levil'] = mission:event(138),
+        },
+
+        [xi.zone.EASTERN_ADOULIN] =
+        {
+            ['Erminold'] = mission:event(1526):replaceDefault(),
+
+            onRegionEnter =
+            {
+                [1] = function(player, region)
+                    return mission:progressEvent(1510)
+                end,
+            },
+
+            onEventFinish =
+            {
+                [1510] = function(player, csid, option, npc)
+                    mission:complete(player)
+                end,
+            },
+        },
+    },
+}
+
+return mission
