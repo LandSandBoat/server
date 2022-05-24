958586d48bcb67f2e138aa18aa070db0a3a6d3f5
claywar <claywar@beerlab.dev>
2022-05-24 06:57:01 -0400

    [waypoints] Prototype initial implementation for teleports
    Additional data entry and test point
    
    
    Additional data entry, cleanup, and comments

---
+++ scripts/globals/waypoint.lua
@@ -0,0 +1,225 @@
+-----------------------------------
+-- Waypoint Teleporters
+-- https://www.bg-wiki.com/ffxi/Waypoint
+-----------------------------------
+require('scripts/globals/items')
+require('scripts/globals/keyitems')
+require('scripts/globals/utils')
+require('scripts/globals/zone')
+-----------------------------------
+xi = xi or {}
+xi.waypoint = xi.waypoint or {}
+
+local waypointStartIndex =
+{
+    [xi.zone.WESTERN_ADOULIN      ] = 0,
+    [xi.zone.EASTERN_ADOULIN      ] = 20,
+    [xi.zone.YAHSE_HUNTING_GROUNDS] = 30,
+    [xi.zone.CEIZAK_BATTLEGROUNDS ] = 40,
+    [xi.zone.FORET_DE_HENNETIEL   ] = 50,
+    [xi.zone.MORIMAR_BASALT_FIELDS] = 60,
+    [xi.zone.YORCIA_WEALD         ] = 70,
+    [xi.zone.MARJAMI_RAVINE       ] = 80,
+    [xi.zone.KAMIHR_DRIFTS        ] = 90,
+    [xi.zone.LOWER_JEUNO          ] = 100,
+}
+
+-- Table Format: Index = { Offset, GroupID, EventID, { Teleport Position } }
+-- Note: Group ID is unused at this time, but can be observed in the eventUpdate option.  This remains
+-- in case it is needed for future implementations
+local waypointInfo =
+{
+    -- Western Adoulin
+    [1] = { 0, 1, 5000, {   4.896,     0,   -4.789,  33, xi.zone.WESTERN_ADOULIN } }, -- Platea Triumphus
+    [2] = { 1, 1, 5001, {  -110.5,  3.85,  -13.482, 191, xi.zone.WESTERN_ADOULIN } }, -- Pioneer's Coalition
+    [3] = { 2, 1, 5002, { -20.982, -0.15,  -79.891, 127, xi.zone.WESTERN_ADOULIN } }, -- Mummer's Coalition
+    [4] = { 3, 1, 5003, {  91.451, -0.15,  -49.013,   0, xi.zone.WESTERN_ADOULIN } }, -- Inventor's Coalition
+    [5] = { 4, 1, 5004, { -68.099,     4,  -73.672,  28, xi.zone.WESTERN_ADOULIN } }, -- Auction House
+    [6] = { 5, 1, 5005, {   5.731,     0, -123.043, 127, xi.zone.WESTERN_ADOULIN } }, -- Rent-a-Room
+    [7] = { 6, 1, 5006, { 174.783,  3.85,  -35.788,  63, xi.zone.WESTERN_ADOULIN } }, -- Big Bridge
+    [8] = { 7, 1, 5007, {  14.586,     0,  162.608, 191, xi.zone.WESTERN_ADOULIN } }, -- Airship Docks
+    [9] = { 8, 1, 5008, {  51.094,    32,  126.299, 191, xi.zone.WESTERN_ADOULIN } }, -- Adoulin Waterfront
+
+    -- Eastern Adoulin
+    [21] = { 15, 2, 5000, { -101.274,  -0.15, -10.726, 191, xi.zone.EASTERN_ADOULIN } }, -- Peacekeeper's Coalition
+    [22] = { 16, 2, 5001, {  -77,944,  -0.15, -63.926,   0, xi.zone.EASTERN_ADOULIN } }, -- Scout's Coalition
+    [23] = { 17, 2, 5002, {  -46.838, -0.075, -12.767,  63, xi.zone.EASTERN_ADOULIN } }, -- Statue of the Goddess
+    [24] = { 18, 2, 5003, {  -57.773,  -0.15,  85.237, 127, xi.zone.EASTERN_ADOULIN } }, -- Yahse Wharf
+    [25] = { 19, 2, 5004, {  -61.865,  -0.15, -120.81, 127, xi.zone.EASTERN_ADOULIN } }, -- Rent-a-Room
+    [26] = { 20, 2, 5005, {  -42.065,  -0.15, -89.979, 191, xi.zone.EASTERN_ADOULIN } }, -- Auction House
+    [27] = { 21, 2, 5006, {   11.681, -22.15,  29.976, 127, xi.zone.EASTERN_ADOULIN } }, -- Sverdhried Hillock
+    [28] = { 22, 2, 5007, {   27.124, -40.15, -60.844, 127, xi.zone.EASTERN_ADOULIN } }, -- Coronal Esplanade
+    [29] = { 23, 2, 5008, {   95.994, -40.15, -74.541,   0, xi.zone.EASTERN_ADOULIN } }, -- Castle Gates
+
+    -- Yahse Hunting Grounds
+    [31] = { 70, 4, 5000, {    321, 0, -199.8, 127, xi.zone.YAHSE_HUNTING_GROUNDS } }, -- Frontier Station
+    [32] = { 71, 4, 5001, {   86.5, 0,    1.5,   0, xi.zone.YAHSE_HUNTING_GROUNDS } }, -- Bivouac #1
+    [33] = { 72, 4, 5002, { -286.5, 0,   43.5, 127, xi.zone.YAHSE_HUNTING_GROUNDS } }, -- Bivouac #2
+    [34] = { 73, 4, 5003, { -162.4, 0, -272.8, 191, xi.zone.YAHSE_HUNTING_GROUNDS } }, -- Bivouac #3
+
+    -- Ceizak Battlegrounds
+    [41] = { 64, 3, 5000, {    365, 0.448,      190, 128, xi.zone.CEIZAK_BATTLEGROUNDS } }, -- Frontier Station
+    [42] = { 65, 3, 5001, { -6.879,     0, -117.511,  63, xi.zone.CEIZAK_BATTLEGROUNDS } }, -- Bivouac #1
+    [43] = { 66, 3, 5002, {    -42,     0,      155, 191, xi.zone.CEIZAK_BATTLEGROUNDS } }, -- Bivouac #2
+    [44] = { 67, 3, 5003, {   -442,     0,     -247, 191, xi.zone.CEIZAK_BATTLEGROUNDS } }, -- Bivouac #3
+
+    -- Foret de Hennetiel
+    [51] = {  96, 5, 5000, { 398.11,    -2, 279.11,   0, xi.zone.FORET_DE_HENNETIEL } }, -- Frontier Station
+    [52] = {  97, 5, 5001, {   12.6,  -2.4,    342,   0, xi.zone.FORET_DE_HENNETIEL } }, -- Bivouac #1
+    [53] = {  98, 5, 5002, {    505, -2.25, -303.5, 127, xi.zone.FORET_DE_HENNETIEL } }, -- Bivouac #2
+    [54] = {  99, 5, 5003, {    103,  -2.2,  -92.3,  63, xi.zone.FORET_DE_HENNETIEL } }, -- Bivouac #3
+    [55] = { 100, 5, 5004, { -251.8, -2.37, -39.25,  63, xi.zone.FORET_DE_HENNETIEL } }, -- Bivouac #4
+
+    -- Morimar Basalt Fields
+    [61] = { 102, 6, 5000, { 443.728,     -16, -325.428, 191, xi.zone.MORIMAR_BASALT_FIELDS } }, -- Frontier Station
+    [62] = { 103, 6, 5001, {     368,     -16,     37.5, 127, xi.zone.MORIMAR_BASALT_FIELDS } }, -- Bivouac #1
+    [63] = { 104, 6, 5002, {   112.8,  -0.483,    324.4,  63, xi.zone.MORIMAR_BASALT_FIELDS } }, -- Bivouac #2
+    [64] = { 105, 6, 5003, {   175.5, -15.581,   -318.2, 127, xi.zone.MORIMAR_BASALT_FIELDS } }, -- Bivouac #3
+    [65] = { 106, 6, 5004, {    -323,     -32,        2,  63, xi.zone.MORIMAR_BASALT_FIELDS } }, -- Bivouac #4
+    [66] = { 107, 6, 5005, {   -78.2, -47.284,      303, 191, xi.zone.MORIMAR_BASALT_FIELDS } }, -- Bivouac #5
+
+    -- Yorcia Weald
+    [71] = { 128, 7, 5000, {    353.3,   0.2,    153.3, 223, xi.zone.YORCIA_WEALD } }, -- Frontier Station
+    [72] = { 129, 7, 5001, {    -40.5, 0.367,  296.367,   0, xi.zone.YORCIA_WEALD } }, -- Bivouac #1
+    [73] = { 130, 7, 5002, {  122.132, 0.146, -287.731, 127, xi.zone.YORCIA_WEALD } }, -- Bivouac #2
+    [74] = { 131, 7, 5003, { -274.776, 0.357,  85.376,  127, xi.zone.YORCIA_WEALD } }, -- Bivouac #3
+
+    -- Marjami Ravine
+    [81] = { 134, 8, 5000, {      358,     -60,      165,  63, xi.zone.MARJAMI_RAVINE } }, -- Frontier Station
+    [82] = { 135, 8, 5001, {      323,     -20,      -79,   0, xi.zone.MARJAMI_RAVINE } }, -- Bivouac #1
+    [83] = { 136, 8, 5002, {    6.808,       0,   78.437, 191, xi.zone.MARJAMI_RAVINE } }, -- Bivouac #2
+    [84] = { 137, 8, 5003, { -318.708,     -20, -127.275,  63, xi.zone.MARJAMI_RAVINE } }, -- Bivouac #3
+    [85] = { 138, 8, 5004, { -326.022, -40.023,  201.096, 191, xi.zone.MARJAMI_RAVINE } }, -- Bivouac #4
+
+    -- Kamihr Drifts
+    [91] = { 166, 9, 5000, {  439.403,    63, -272.554,  63, xi.zone.KAMIHR_DRIFTS } }, -- Frontier Station
+    [92] = { 167, 9, 5001, {  -42.574,    43,  -71.319,   0, xi.zone.KAMIHR_DRIFTS } }, -- Bivouac #1
+    [93] = { 168, 9, 5002, {     8.24,    43, -283.017, 191, xi.zone.KAMIHR_DRIFTS } }, -- Bivouac #2
+    [94] = { 169, 9, 5003, {     9.24,    23,  162.803,  63, xi.zone.KAMIHR_DRIFTS } }, -- Bivouac #3
+    [95] = { 170, 9, 5004, { -228.942, 3.567,  364.512, 127, xi.zone.KAMIHR_DRIFTS } }, -- Bivouac #4
+
+    -- Jeuno
+
+    -- Enigmatic Devices
+
+    -- Runes
+}
+
+local runeKeyItems =
+{
+    xi.ki.SAN_DORIA_WARP_RUNE,
+    xi.ki.BASTOK_WARP_RUNE,
+    xi.ki.WINDURST_WARP_RUNE,
+    xi.ki.SELBINA_WARP_RUNE,
+    xi.ki.MHAURA_WARP_RUNE,
+    xi.ki.KAZHAM_WARP_RUNE,
+    xi.ki.RABAO_WARP_RUNE,
+    xi.ki.NORG_WARP_RUNE,
+    xi.ki.TAVNAZIA_WARP_RUNE,
+    xi.ki.WHITEGATE_WARP_RUNE,
+    xi.ki.NASHMAU_WARP_RUNE,
+}
+
+-- Number of Kinetic Units granted for each item trade.
+-- TODO: Add support for HQ Crystals
+local crystalTradeValues =
+{
+    [xi.items.EARTH_CRYSTAL    ] = 15,
+    [xi.items.FIRE_CRYSTAL     ] = 15,
+    [xi.items.WATER_CRYSTAL    ] = 15,
+    [xi.items.WIND_CRYSTAL     ] = 15,
+    [xi.items.ICE_CRYSTAL      ] = 30,
+    [xi.items.LIGHTNING_CRYSTAL] = 30,
+    [xi.items.DARK_CRYSTAL     ] = 80,
+    [xi.items.LIGHT_CRYSTAL    ] = 80,
+    [xi.items.EARTH_CLUSTER    ] = 200,
+    [xi.items.FIRE_CLUSTER     ] = 200,
+    [xi.items.WATER_CLUSTER    ] = 200,
+    [xi.items.WIND_CLUSTER     ] = 200,
+    [xi.items.ICE_CLUSTER      ] = 400,
+    [xi.items.LIGHTNING_CLUSTER] = 400,
+    [xi.items.DARK_CLUSTER     ] = 1000,
+    [xi.items.LIGHT_CLUSTER    ] = 1000,
+}
+
+local function getWaypointData(npcObj)
+    local waypointNpcId = npcObj:getID()
+    local zoneObject    = npcObj:getZone()
+    local waypointList  = zoneObject:queryEntitiesByName('Waypoint') -- TODO: Fallback to Enigmatic devices if not found
+    local waypointData  = {}
+
+    for indexVal, npcData in ipairs(waypointList) do
+        if npcData:getID() == waypointNpcId then
+            waypointData[1] = waypointStartIndex[zoneObject:getID()] + indexVal
+            waypointData[2] = waypointInfo[waypointData[1]][1]
+            waypointData[3] = waypointInfo[waypointData[1]][2]
+            waypointData[4] = waypointInfo[waypointData[1]][3]
+
+            return waypointData
+        end
+    end
+end
+
+local function getRuneMask(player)
+    local resultMask = utils.MAX_UINT32
+
+    for bitIndex, keyItem in ipairs(runeKeyItems) do
+        if player:hasKeyItem(keyItem) then
+            resultMask = utils.mask.setBit(resultMask, bitIndex, false)
+        end
+    end
+
+    -- TODO: This seems to remain 0
+    resultMask = utils.mask.setBit(resultMask, 0, false)
+
+    return resultMask
+end
+
+-- The below functions are used by all Waypoint NPCs
+xi.waypoint.onTrade = function(player, npc, trade)
+
+end
+
+xi.waypoint.onTrigger = function(player, npc)
+    local resultTable    = { 0, 0, 0, 0, 0, 0, 0, 0 }
+    local waypointData   = getWaypointData(npc)
+    local discountParams = 0b0100 -- TODO: Bit 2 here is discount, Bit 3 is accept/decline for simple/normal transport
+
+    -- Waypoint Event ID
+    resultTable[1] = waypointData[4]
+
+    -- First event parameters packs the player's kinetic units, two bits that determine teleportation cost,
+    -- and the Index value of the waypoint (See: waypointInfo table)
+    resultTable[2] = bit.lshift(player:getCurrency('kinetic_unit'), 16) + bit.lshift(discountParams, 12) + waypointData[1]
+
+    -- Second event parameter packs an initial bit which could be related to having the charter permit,
+    -- along with the unlocked geomagnetrons in Eastern and Western Adoulin
+    resultTable[3] = 16744959
+
+    -- Third event parameter is an inverted bitfield for all but bit 0, the remainder of the field is set
+    -- by the key items for warp runes.
+    resultTable[4] = getRuneMask(player)
+
+    -- Unlock all non-city Waypoints
+    resultTable[5] = 975
+    resultTable[6] = 4063
+    resultTable[7] = 1999
+    resultTable[8] = 1984
+
+    player:startEvent(unpack(resultTable))
+end
+
+xi.waypoint.onEventUpdate = function(player, csid, option, npc)
+    -- local currentWaypointIndex = bit.band(option, 0x7F)
+    -- local destinationGroup     = bit.band(bit.rshift(option, 7), 0xF)
+    -- local destinationOffset    = bit.band(bit.rshift(option, 11), 0xF)
+    -- local travelCost           = bit.rshift(option, 21)
+
+    player:updateEvent(0, 0, 0, 0, 0, 0, 0, 1)
+end
+
+xi.waypoint.onEventFinish = function(player, csid, option, npc)
+    if option > 0 and option <= 303 then
+        player:setPos(unpack(waypointInfo[option][4]))
+    end
+end
