88ba94496fd4b9d4f13b25ec6ee9f294125c45c6
RunAwayDSP <sschwender@gmail.com>
2022-05-27 11:14:08 -0400

    Update Dropid
    merge conflict
    
    more conflict updates
    
    garrison update

--- sql/mob_droplist.sql
+++ sql/mob_droplist.sql
@@ -17438,9 +17438,9 @@ INSERT INTO `mob_droplist` VALUES (3222,0,0,1000,15123,10);
 INSERT INTO `mob_droplist` VALUES (3222,0,0,1000,15140,10);
 INSERT INTO `mob_droplist` VALUES (3222,0,0,1000,15142,10);
 INSERT INTO `mob_droplist` VALUES (3222,0,0,1000,16360,10);
-INSERT INTO `mob_droplist` VALUES (3223,0,0,1000,1604,100); -- Mannequin Legs
-INSERT INTO `mob_droplist` VALUES (3223,0,0,1000,1605,100); -- Mannequin Feet
-
+INSERT INTO `mob_droplist` VALUES (3223,0,0,1000,16017,150);
+INSERT INTO `mob_droplist` VALUES (3224,0,0,1000,1604,100); -- Mannequin Legs
+INSERT INTO `mob_droplist` VALUES (3224,0,0,1000,1605,100); -- Mannequin Feet
 
 
 /*!40000 ALTER TABLE `mob_droplist` ENABLE KEYS */;
