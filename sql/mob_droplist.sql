6920b99bed54aa0530c89ed5c9674a25fe2d1f76
RunAwayDSP <sschwender@gmail.com>
2022-05-27 11:37:45 -0400

    garrison update

--- sql/mob_droplist.sql
+++ sql/mob_droplist.sql
@@ -17429,6 +17429,8 @@ INSERT INTO `mob_droplist` VALUES (3222,0,0,1000,15140,10);
 INSERT INTO `mob_droplist` VALUES (3222,0,0,1000,15142,10);
 INSERT INTO `mob_droplist` VALUES (3222,0,0,1000,16360,10);
 INSERT INTO `mob_droplist` VALUES (3223,0,0,1000,16017,150);
+INSERT INTO `mob_droplist` VALUES (3224,0,0,1000,1604,100); -- Mannequin Legs
+INSERT INTO `mob_droplist` VALUES (3224,0,0,1000,1605,100); -- Mannequin Feet
 
 
 /*!40000 ALTER TABLE `mob_droplist` ENABLE KEYS */;
