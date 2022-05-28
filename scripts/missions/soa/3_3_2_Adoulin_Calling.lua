719a7cadaa4dab84a38f96ab79bc9eaaa8168abe
claywar <claywar@beerlab.dev>
2022-05-28 07:40:46 -0400

    Interaction - Mission: Adoulin Calling

---
+++ scripts/missions/soa/3_3_2_Adoulin_Calling.lua
@@ -0,0 +1,46 @@
+-----------------------------------
+-- Adoulin Calling
+-- Seekers of Adoulin M3-3-2
+-----------------------------------
+-- !addmission 12 47
+-- Levil : !pos -87.204 3.350 12.655 256
+-----------------------------------
+require('scripts/globals/missions')
+require('scripts/globals/interaction/mission')
+require('scripts/globals/zone')
+-----------------------------------
+
+local mission = Mission:new(xi.mission.log_id.SOA, xi.mission.id.soa.ADOULIN_CALLING)
+
+mission.reward =
+{
+    nextMission = { xi.mission.log_id.SOA, xi.mission.id.soa.THE_DISAPPEARANCE_OF_NYLINE },
+}
+
+mission.sections =
+{
+    {
+        check = function(player, currentMission, missionStatus, vars)
+            return currentMission == mission.missionId
+        end,
+
+        [xi.zone.WESTERN_ADOULIN] =
+        {
+            ['Levil'] = mission:event(141),
+        },
+
+        [xi.zone.EASTERN_ADOULIN] =
+        {
+            ['Door_Boarding_House'] = mission:progressEvent(1511),
+
+            onEventFinish =
+            {
+                [1511] = function(player, csid, option, npc)
+                    mission:complete(player)
+                end,
+            },
+        },
+    },
+}
+
+return mission
