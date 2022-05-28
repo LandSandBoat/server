82e62dbfdd536fbff8aac6e6ed815643f74cb801
Zach Toogood <zrtoogood@gmail.com>
2022-05-28 19:04:33 +0300

    [ci] Add general check for tabs

--- tools/ci/general.sh
+++ tools/ci/general.sh
@@ -11,5 +11,7 @@ python3 << EOF
 with open('${target}') as f:
     data = f.read()
     if not data.endswith('\n'):
-        print("No newline at end of file: ${target}")
+        print("No newline at end of file: ${target}, please add one.")
+    if "\t" in data:
+        print("Found tab character(s) in file: ${target}, please replace these with 4x spaces.")
 EOF
