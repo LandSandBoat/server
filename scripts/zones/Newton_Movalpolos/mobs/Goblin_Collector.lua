710890f271def7354523d69778641660897fb752
Critical <lyardeen@gmail.com>
2022-05-28 11:19:19 -0600

    Goblin Collector adjustments

--- scripts/zones/Newton_Movalpolos/mobs/Goblin_Collector.lua
+++ scripts/zones/Newton_Movalpolos/mobs/Goblin_Collector.lua
@@ -9,6 +9,7 @@ require("scripts/globals/status")
 local entity = {}
 
 entity.onMobSpawn = function(mob)
+    -- TODO: Needs to do a jumping animation on spawn
     mob:setMod(xi.mod.MATT, 150)
     mob:setMod(xi.mod.BINDRES, 40)
     mob:setMod(xi.mod.GRAVITYRES, 40)
@@ -17,8 +18,8 @@ end
 
 entity.onMobFight = function(mob, target)
     -- Resets threat on every auto attack
-    mob:addListener("ATTACK","COLLECTOR_ATTACK", function(mob)
-        mob:resetEnmity(target)
+    mob:addListener("ATTACK","COLLECTOR_ATTACK", function(goblin)
+        goblin:resetEnmity(target)
     end)
 end
 
