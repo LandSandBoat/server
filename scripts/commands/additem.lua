fcc082e04186f82285b9f3f2e5328a5598f50b77
Damarus <leblanc.jlj@gmail.com>
2022-05-28 22:09:22 -0400

    Modified additem.lua to accept either id value or name of item

--- scripts/commands/additem.lua
+++ scripts/commands/additem.lua
@@ -6,7 +6,7 @@
 cmdprops =
 {
     permission = 1,
-    parameters = "iiiiiiiiiii"
+    parameters = "siiiiiiiiii"
 }
 
 function error(player, msg)
@@ -14,27 +14,47 @@ function error(player, msg)
     player:PrintToPlayer("!additem <itemId> {quantity} {aug1} {v1} {aug2} {v2} {aug3} {v3} {aug4} {v4} {trial}")
 end
 
-function onTrigger(player, itemId, quantity, aug0, aug0val, aug1, aug1val, aug2, aug2val, aug3, aug3val, trialId)
+function onTrigger(player, item, quantity, aug0, aug0val, aug1, aug1val, aug2, aug2val, aug3, aug3val, trialId)
     -- Load needed text ids for players current zone..
     local ID = zones[player:getZoneID()]
+    local itemToGet = 0
 
-    -- validate itemId
-    if (itemId == nil or tonumber(itemId) == nil or tonumber(itemId) == 0) then
-        error(player, "Invalid itemId.")
+    -- validate item
+    if item == nil then
+        -- No Item Provided
+        error(player, "No Item ID given.")
         return
-    end
-
-    -- Ensure the GM has room to obtain the item
-    if (player:getFreeSlotsCount() == 0) then
-        player:messageSpecial( ID.text.ITEM_CANNOT_BE_OBTAINED, itemId )
-        return
-    end
-
-    -- Give the GM the item
-    player:addItem( itemId, quantity, aug0, aug0val, aug1, aug1val, aug2, aug2val, aug3, aug3val, trialId )
-    if quantity and quantity > 1 then
-        player:messageSpecial( ID.text.ITEM_OBTAINED + 9, itemId , quantity )
+    elseif tonumber(item) == nil and item ~= nil then
+        -- Item was provided, but was not a number.  Try text lookup.
+        local retItem = GetItemIDByName(tostring(item))
+        if retItem > 0 then
+            itemToGet = retItem
+        else
+            error(player, string.format("Item %s not found in database.", item))
+            return
+        end
     else
-        player:messageSpecial( ID.text.ITEM_OBTAINED, itemId )
+        -- Number was provided, so just use it
+        itemToGet = tonumber(item)
+    end
+
+    -- At this point, if there's no item found, exit out of the function
+    if itemToGet == 0 then
+        error(player, "Item not found.")
+        return
+    end
+
+    -- Ensure the GM has room to obtain the item...
+    if player:getFreeSlotsCount() == 0 then
+        player:messageSpecial(ID.text.ITEM_CANNOT_BE_OBTAINED, item)
+        return
+    end
+
+    -- Give the GM the item...
+    player:addItem(itemToGet, quantity, aug0, aug0val, aug1, aug1val, aug2, aug2val, aug3, aug3val, trialId)
+    if quantity and quantity > 1 then
+        player:messageSpecial(ID.text.ITEM_OBTAINED + 9, itemToGet, quantity)
+    else
+        player:messageSpecial(ID.text.ITEM_OBTAINED, itemToGet)
     end
 end
