6749d821de086e72babb4464a41cac8fccbbae5e
carver <beneliezer@gmail.com>
2022-05-29 15:35:39 +0000

    split mandau and mjollnir npcs

---
+++ scripts/zones/The_Sanctuary_of_ZiTah/npcs/relic_mandau.lua
@@ -0,0 +1,35 @@
+-----------------------------------
+-- Area: The Sanctuary of Zi'Tah
+--  NPC: <this space intentionally left blank>
+-- !pos 646 -2 -165 121
+-----------------------------------
+local ID = require("scripts/zones/The_Sanctuary_of_ZiTah/IDs")
+require("scripts/globals/items")
+require("scripts/globals/npc_util")
+-----------------------------------
+local entity = {}
+
+entity.onTrade = function(player, npc, trade)
+    local currentRelic = player:getCharVar("RELIC_IN_PROGRESS")
+
+    -- Mandau
+    if currentRelic == xi.items.BATARDEAU and npcUtil.tradeHas(trade, {xi.items.TEN_THOUSAND_BYNE_BILL, xi.items.ORNATE_FRAGMENT, xi.items.SHARD_OF_NECROPSYCHE, xi.items.BATARDEAU}) then -- currency, shard, necropsyche, stage 4
+        player:startEvent(207, xi.items.MANDAU)
+    end
+end
+
+entity.onTrigger = function(player, npc)
+    player:messageSpecial(ID.text.NOTHING_OUT_OF_ORDINARY)
+end
+
+entity.onEventUpdate = function(player, csid, option)
+end
+
+entity.onEventFinish = function(player, csid, option)
+    if csid == 207 and npcUtil.giveItem(player, {xi.items.MANDAU, {xi.items.ONE_HUNDRED_BYNE_BILL, 30}}) then
+        player:confirmTrade()
+        player:setCharVar("RELIC_IN_PROGRESS", 0)
+    end
+end
+
+return entity
