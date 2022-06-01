f8e4ff1caaa1a0187693628785681d92a747521a
NeuromancerXI <neuromancerxi@users.noreply.github.com>
2022-06-01 14:38:38 -0700

    style!

--- scripts/globals/items/behemoth_suit_+1.lua
+++ scripts/globals/items/behemoth_suit_+1.lua
@@ -27,27 +27,27 @@ item_object.onItemCheck = function(target)
 end
 
 item_object.onItemUse = function(target)
-    local meatList = 
-            {
-                { 9, xi.items.SLICE_OF_BUFFALO_MEAT}, -- 5152
-                { 4, xi.items.SLICE_OF_CERBERUS_MEAT}, -- 5565
-                {17, xi.items.SLICE_OF_COCKATRICE_MEAT}, -- 4435
-                {13, xi.items.SLICE_OF_COEURL_MEAT}, -- 4377
-                {20, xi.items.DHALMEL_MEAT}, -- 4359
-                { 5, xi.items.SLICE_OF_DRAGON_MEAT}, -- 4272
-                {17, xi.items.SLICE_OF_GIANT_SHEEP_MEAT}, -- 4372
-                {22, xi.items.SLICE_OF_HARE_MEAT}, -- 4358
-                { 5, xi.items.CHUNK_OF_HYDRA_MEAT}, -- 5564
-                { 4, xi.items.SLICE_OF_KARAKUL_MEAT}, -- 5571
-                { 9, xi.items.LAND_CRAB_MEAT}, -- 4400
-                { 7, xi.items.SLICE_OF_LUCEREWE_MEAT}, -- 5531
-                { 5, xi.items.LYNX_MEAT}, --5667
-                { 9, xi.items.CHUNK_OF_OROBON_MEAT}, -- 5563
-                { 9, xi.items.SLAB_OF_RUSZOR_MEAT}, -- 5755
-                { 7, xi.items.SLICE_OF_ZIZ_MEAT}, -- 5581
-                { 2, xi.items.BEASTLY_SHANK}, -- 3341
-                -- { ?, xi.items.SAVORY_SHANK}, -- 3342
-            }
+    local meatList =
+    {
+        { 9, xi.items.SLICE_OF_BUFFALO_MEAT}, -- 5152
+        { 4, xi.items.SLICE_OF_CERBERUS_MEAT}, -- 5565
+        {17, xi.items.SLICE_OF_COCKATRICE_MEAT}, -- 4435
+        {13, xi.items.SLICE_OF_COEURL_MEAT}, -- 4377
+        {20, xi.items.DHALMEL_MEAT}, -- 4359
+        { 5, xi.items.SLICE_OF_DRAGON_MEAT}, -- 4272
+        {17, xi.items.SLICE_OF_GIANT_SHEEP_MEAT}, -- 4372
+        {22, xi.items.SLICE_OF_HARE_MEAT}, -- 4358
+        { 5, xi.items.CHUNK_OF_HYDRA_MEAT}, -- 5564
+        { 4, xi.items.SLICE_OF_KARAKUL_MEAT}, -- 5571
+        { 9, xi.items.LAND_CRAB_MEAT}, -- 4400
+        { 7, xi.items.SLICE_OF_LUCEREWE_MEAT}, -- 5531
+        { 5, xi.items.LYNX_MEAT}, --5667
+        { 9, xi.items.CHUNK_OF_OROBON_MEAT}, -- 5563
+        { 9, xi.items.SLAB_OF_RUSZOR_MEAT}, -- 5755
+        { 7, xi.items.SLICE_OF_ZIZ_MEAT}, -- 5581
+        { 2, xi.items.BEASTLY_SHANK}, -- 3341
+        -- { ?, xi.items.SAVORY_SHANK}, -- 3342
+    }
     npcUtil.giveItem(target, { { xi.item_utils.pickItemRandom(target, meatList), 1 } })
 end
 
