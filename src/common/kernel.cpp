94bf332242cb5251d3782a0536fd33efded8f919
claywar <claywar@beerlab.dev>
2022-06-01 18:17:01 -0400

    Disable Quick Edit Mode by default on Windows Consoles
    Tabs to spaces
    
    
    More tabs

--- src/common/kernel.cpp
+++ src/common/kernel.cpp
@@ -150,9 +150,9 @@ static void dump_backtrace()
 }
 
 /************************************************************************
- *																		*
- *  CORE : Signal Sub Function											*
- *																		*
+ *                                                                      *
+ *  CORE : Signal Sub Function                                          *
+ *                                                                      *
  ************************************************************************/
 
 static void sig_proc(int sn)
@@ -187,20 +187,14 @@ static void sig_proc(int sn)
         case SIGXFSZ:
             // ignore and allow it to set errno to EFBIG
             ShowWarning("Max file size reached!");
-            // run_flag = 0;	// should we quit?
+            // run_flag = 0; // should we quit?
             break;
         case SIGPIPE:
-            // ShowInfo ("Broken pipe found... closing socket");	// set to eof in socket.c
+            // ShowInfo ("Broken pipe found... closing socket"); // set to eof in socket.c
             break; // does nothing here
 #endif
     }
 }
-
-/************************************************************************
- *																		*
- *																		*
- *																		*
- ************************************************************************/
 
 void signals_init()
 {
@@ -221,9 +215,9 @@ void signals_init()
 }
 
 /************************************************************************
- *																		*
- *  Warning if logged in as superuser (root)								*
- *																		*
+ *                                                                      *
+ *  Warning if logged in as superuser (root)                            *
+ *                                                                      *
  ************************************************************************/
 
 void usercheck()
@@ -239,14 +233,24 @@ void usercheck()
 }
 
 /************************************************************************
- *																		*
- *  CORE : MAINROUTINE													*
- *																		*
+ *                                                                      *
+ *  CORE : MAINROUTINE                                                  *
+ *                                                                      *
  ************************************************************************/
 #ifndef DEFINE_OWN_MAIN
 int main(int argc, char** argv)
 {
     debug::init();
+
+#ifdef _WIN32
+    // Disable Quick Edit Mode (Mark) in Windows Console to prevent users from accidentially
+    // causing the server to freeze.
+    HANDLE hInput;
+    DWORD  prev_mode;
+    hInput = GetStdHandle(STD_INPUT_HANDLE);
+    GetConsoleMode(hInput, &prev_mode);
+    SetConsoleMode(hInput, ENABLE_EXTENDED_FLAGS | (prev_mode & ~ENABLE_QUICK_EDIT_MODE));
+#endif // _WIN32
 
     { // initialize program arguments
         char* p1 = SERVER_NAME = argv[0];
@@ -280,13 +284,17 @@ int main(int argc, char** argv)
         }
     }
 
+#ifdef _WIN32
+    // Re-enable Quick Edit Mode upon Exiting if it is still disabled
+    SetConsoleMode(hInput, prev_mode);
+#endif // _WIN32
+
     gConsoleService = nullptr;
 
     do_final(EXIT_SUCCESS);
-#ifdef _WIN32
-#ifdef _DEBUG
+
+#if defined(_WIN32) && defined(_DEBUG)
     return 0;
-#endif // _WIN32
-#endif // _DEBUG
+#endif
 }
 #endif // DEFINE_OWN_MAIN
