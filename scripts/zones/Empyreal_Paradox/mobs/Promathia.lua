4adf6d9c0e75e619aa0262888cba9b61c8c20e9b
Aerec <ezschneider1@gmail.com>
2022-06-05 21:35:53 -0400

    Add accidentally deleted mod back

--- scripts/zones/Empyreal_Paradox/mobs/Promathia.lua
+++ scripts/zones/Empyreal_Paradox/mobs/Promathia.lua
@@ -11,6 +11,7 @@ local entity = {}
 
 entity.onMobInitialize = function(mob)
     mob:addMod(xi.mod.REGAIN, 75)
+    mob:addMod(xi.mod.UFASTCAST, 50)
     mob:setMobMod(xi.mobMod.MAGIC_COOL, 15)
 end
 
