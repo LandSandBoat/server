d67c81387715d340dcb9b0e4feef4bc324034fdc
claywar <claywar@beerlab.dev>
2022-06-06 16:05:10 -0400

    Fix RoV Progression for Inescapable Binds
    Syntax
    
    
    More syntax

--- scripts/quests/jeuno/The_Road_to_Aht_Urhgan.lua
+++ scripts/quests/jeuno/The_Road_to_Aht_Urhgan.lua
@@ -97,7 +97,7 @@ local function handleSelectionEventFinish(player, csid, option, npc)
     then
         if quest:complete(player) then
             npcUtil.giveKeyItem(player, xi.ki.BOARDING_PERMIT)
-            npcUtil.completeMission(player, xi.mission.log_id.ROV, xi.mission.id.rov.INESCAPABLE_BINDS, { nextMission = xi.mission.id.rov.DESERT_WINDS, })
+            npcUtil.completeMission(player, xi.mission.log_id.ROV, xi.mission.id.rov.INESCAPABLE_BINDS, { nextMission = { xi.mission.log_id.ROV, xi.mission.id.rov.DESERT_WINDS } })
         end
     -- Let me think about it.
     elseif option == 2 then
