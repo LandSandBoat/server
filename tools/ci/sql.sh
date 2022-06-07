784b26e383e755f3ca7646b81054273bcacda5cc
Zach Toogood <zrtoogood@gmail.com>
2022-06-07 16:51:42 +0300

    [ci] Add checks for comments in various sql files

--- tools/ci/sql.sh
+++ tools/ci/sql.sh
@@ -13,11 +13,28 @@ else
     file_list+=(${target})
 fi
 
+check_mod_comments() {
+python3 << EOF
+filename = '$1'
+
+with open(filename) as f:
+    lines = f.readlines()
+    for line in lines:
+        line = line.strip()
+        if not line.startswith("-- ") and not "--" in line:
+            print(line)
+EOF
+}
+
 for f in "${file_list[@]}"
 do
     BOGUS_COMMENTS=`grep -En '(--\w)' $f`
     if [[ -n $BOGUS_COMMENTS ]]; then
         printf "Bogus comments: $f:\n"
         printf "%s\n" "${BOGUS_COMMENTS[@]}"
+    fi
+
+    if [ "$f" = "sql/item_mods.sql" ]; then
+        check_mod_comments $f
     fi
 done
