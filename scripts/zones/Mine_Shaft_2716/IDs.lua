4c6feb5c8e9a9893a6cb51ea49c8cff631afcf24
Zach Toogood <zrtoogood@gmail.com>
2022-06-10 14:58:22 +0300

    [client] Text ID shifts

--- scripts/zones/Mine_Shaft_2716/IDs.lua
+++ scripts/zones/Mine_Shaft_2716/IDs.lua
@@ -17,8 +17,8 @@ zones[xi.zone.MINE_SHAFT_2716] =
         CARRIED_OVER_POINTS       = 7001, -- You have carried over <number> login point[/s].
         LOGIN_CAMPAIGN_UNDERWAY   = 7002, -- The [/January/February/March/April/May/June/July/August/September/October/November/December] <number> Login Campaign is currently underway!<space>
         LOGIN_NUMBER              = 7003, -- In celebration of your most recent login (login no. <number>), we have provided you with <number> points! You currently have a total of <number> points.
-        PARTY_MEMBERS_HAVE_FALLEN = 7403, -- All party members have fallen in battle. Now leaving the battlefield.
-        THE_PARTY_WILL_BE_REMOVED = 7410, -- If all party members' HP are still zero after # minute[/s], the party will be removed from the battlefield.
+        PARTY_MEMBERS_HAVE_FALLEN = 7407, -- All party members have fallen in battle. Now leaving the battlefield.
+        THE_PARTY_WILL_BE_REMOVED = 7414, -- If all party members' HP are still zero after # minute[/s], the party will be removed from the battlefield.
         CONQUEST_BASE             = 7429, -- Tallying conquest results...
     },
     mob =
