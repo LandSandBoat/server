974b3eb4df2b6dcfef443fe3e6e169de428f65a4
Critical <lyardeen@gmail.com>
2022-06-10 12:33:02 -0600

    Pashhow NM audit

--- scripts/zones/Pashhow_Marshlands/mobs/NiZho_Bladebender.lua
+++ scripts/zones/Pashhow_Marshlands/mobs/NiZho_Bladebender.lua
@@ -7,6 +7,13 @@ require("scripts/globals/hunts")
 -----------------------------------
 local entity = {}
 
+entity.onMobWeaponSkillPrepare = function(mob, target)
+    -- Spams headbutt below 30% HP
+    if mob:getHPP() <= 30 then
+        return 612
+    end
+end
+
 entity.onMobDeath = function(mob, player, isKiller)
     xi.regime.checkRegime(player, mob, 60, 1, xi.regime.type.FIELDS)
     xi.hunts.checkHunt(mob, player, 214)
