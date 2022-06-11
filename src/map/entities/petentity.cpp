da0eb72d4181bed62ee6b1082f79c624a3d06d29
WinterSolstice8 <60417494+WinterSolstice8@users.noreply.github.com>
2022-06-11 14:41:22 -0700

    [core] Fix performance warning generated by CI's build

--- src/map/entities/petentity.cpp
+++ src/map/entities/petentity.cpp
@@ -39,12 +39,12 @@
 
 CPetEntity::CPetEntity(PET_TYPE petType)
 : CMobEntity()
+, m_PetType(petType)
 {
     objtype        = TYPE_PET;
     m_EcoSystem    = ECOSYSTEM::UNCLASSIFIED;
     allegiance     = ALLEGIANCE_TYPE::PLAYER;
     m_MobSkillList = 0;
-    m_PetType      = petType;
     m_PetID        = 0;
 
     PAI            = std::make_unique<CAIContainer>(this, std::make_unique<CPathFind>(this), std::make_unique<CPetController>(this), std::make_unique<CTargetFind>(this));
