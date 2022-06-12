236dfe0bfb327f94bc2a20994143e914f23e27b7
claywar <claywar@beerlab.dev>
2022-06-12 11:36:48 -0400

    Interaction - Mission: Lighting the Way
    Fix comment

--- scripts/zones/Kamihr_Drifts/IDs.lua
+++ scripts/zones/Kamihr_Drifts/IDs.lua
@@ -30,6 +30,7 @@ zones[xi.zone.KAMIHR_DRIFTS] =
         ARTIFACT_TERMINAL_VOLUME   = 7710, -- Your artifact has been charged to its terminal volume of kinetic units.
         SURPLUS_LOST_TO_AETHER     = 7711, -- A surplus of <number> kinetic unit[/s] has been lost to the aether.
         HOMEPOINT_SET              = 7963, -- Home point set!
+        PATH_SEEMS_TO_LEAD         = 8039, -- This path seems to lead toward the summit of Mount Kamihr.
     },
     mob =
     {
