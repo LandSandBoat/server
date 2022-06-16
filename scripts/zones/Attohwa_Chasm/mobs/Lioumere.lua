71b4d891b0d8200e758519c185efa6518698a94f
Frankie-hz <105882754+Frankie-hz@users.noreply.github.com>
2022-06-15 22:14:53 -0400

    CoP 3-3 Balance Fixes & Antlion Ambush

--- scripts/zones/Attohwa_Chasm/mobs/Lioumere.lua
+++ scripts/zones/Attohwa_Chasm/mobs/Lioumere.lua
@@ -8,6 +8,30 @@ require("scripts/globals/missions")
 -----------------------------------
 local entity = {}
 
+entity.onMobWeaponSkill = function(player, mob, skill)
+    --Sets entire party enmity CE/VE to 0.
+    local alliance = player:getAlliance() -- Gets member pointers for alliance
+    for _, member in pairs(alliance) do
+        mob:resetEnmity(member) -- Resets player enmity on the mob
+    end
+    mob:pathTo(478.8, 20, 41.7) -- Start walking home
+end
+
+entity.onMobFight = function(mob, player, target)
+    if mob:atPoint(478.8, 20, 41.7) then
+        mob:setHP(mob:getMaxHP())
+    end
+
+    local playerCE = mob:getCE(player)
+    local playerBE = mob:getVE(player)
+    if
+        playerCE >= 180 or
+        playerVE >= 600
+    then
+        mob:clearPath()
+    end
+end
+
 entity.onMobDeath = function(mob, player, isKiller)
 end
 
--- scripts/zones/Carpenters_Landing/mobs/Overgrown_Ivy.lua
+++ scripts/zones/Carpenters_Landing/mobs/Overgrown_Ivy.lua
@@ -4,6 +4,23 @@
 -----------------------------------
 local entity = {}
 
+entity.onMobInitialize = function(mob)
+    mob:addMod(xi.mod.DOUBLE_ATTACK, 10)
+    mob:addMod(xi.mod.REGAIN, 50)
+end
+
+entity.onMobFight = function(mob, target)
+    if mob:getHPP() <= 15 then
+        mob:setMod(xi.mod.REGAIN, 200)
+    end
+end
+
+entity.onMobWeaponSkillPrepare = function(mob, target)
+    if mob:getHPP() <= 15 then
+        return 319
+    end
+end
+
 entity.onMobDeath = function(mob, player, isKiller)
 end
 
