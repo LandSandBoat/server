09181b10a91c53c1ce1e7fac0890a5795ca3fccf
dallano <atwood.dallan@hotmail.com>
2022-06-17 01:21:45 -0600

    King V bug fixes
    King V corrected to despawn when a party wipes or when weather changes while he's roaming. A regen function was written to change between day / night cycles. Also added in the draw in ability.

--- scripts/zones/Western_Altepa_Desert/mobs/King_Vinegarroon.lua
+++ scripts/zones/Western_Altepa_Desert/mobs/King_Vinegarroon.lua
@@ -10,7 +10,10 @@ local entity = {}
 
 entity.onMobInitialize = function(mob)
     mob:setMobMod(xi.mobMod.ADD_EFFECT, 1)
-    mob:setMobMod(xi.mobMod.DRAW_IN, 2)
+    mob:setMobMod(xi.mobMod.DRAW_IN, 1)
+    mob:setMobMod(xi.mobMod.DRAW_IN_INCLUDE_PARTY, 1)
+    mob:setMobMod(xi.mobMod.DRAW_IN_CUSTOM_RANGE, 10)
+
 end
 
 entity.onMobDrawIn = function(mob, target)
