117f8c55ff210e5da99acba0320c6648a530af87
Abdiah <62350957+dallano@users.noreply.github.com>
2022-06-17 16:00:15 -0600

    Update Aiatar.lua

--- scripts/zones/Riverne-Site_A01/mobs/Aiatar.lua
+++ scripts/zones/Riverne-Site_A01/mobs/Aiatar.lua
@@ -13,9 +13,12 @@ entity.onMobDeath = function(mob, player, isKiller)
 end
 
 return entity
+<<<<<<< HEAD
 =======
 end
 >>>>>>> 6d39d7b186 (Aiatar)
 =======
 end
 >>>>>>> 86c674b976 (Update Aiatar.lua)
+=======
+>>>>>>> 806ca478e9 (Update Aiatar.lua)
