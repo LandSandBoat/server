a2d86449bdb2a52b2041f06ed2f759ee2c24ae3a
Aerec <ezschneider1@gmail.com>
2022-06-19 14:52:10 -0400

    AA GK Meikyo light Skillchains

--- scripts/zones/LaLoff_Amphitheater/mobs/Ark_Angel_GK.lua
+++ scripts/zones/LaLoff_Amphitheater/mobs/Ark_Angel_GK.lua
@@ -8,6 +8,7 @@ require("scripts/globals/status")
 local entity = {}
 
 -- TODO: Allegedly has a 12 hp/sec regen.  Determine if true, and add to onMobInitialize if so.
+-- TODO: Create listener for SCing with other AAs during DM
 
 entity.onMobSpawn = function(mob)
     xi.mix.jobSpecial.config(mob, {
@@ -31,7 +32,20 @@ entity.onMobEngaged = function(mob, target)
 end
 
 entity.onMobFight = function(mob, target)
-    -- TODO: AA GK actively seeks to skillchain to Light off of his own WSs under MS, or other AA's WSs.
+    if mob:hasStatusEffect(xi.effect.MEIKYO_SHISUI) then
+        mob:setTP(0)
+        if mob:getLocalVar("order") == 0 then
+            mob:useMobAbility(946)
+            mob:setLocalVar("order", 1)
+        elseif mob:getLocalVar("order") == 1 then
+            mob:useMobAbility(947)
+            mob:setLocalVar("order", 2)
+        else
+            mob:useMobAbility(948)
+            mob:setLocalVar("order", 0)
+            mob:delStatusEffect(xi.effect.MEIKYO_SHISUI)
+        end
+    end
 end
 
 entity.onMobDeath = function(mob, player, isKiller)
