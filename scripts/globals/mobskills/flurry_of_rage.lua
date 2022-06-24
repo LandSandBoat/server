613fe5eb9c761c5788b56d25fe368e18cc0f5557
Critical <lyardeen@gmail.com>
2022-06-24 14:46:18 -0600

    Rolanberry NM audit

---
+++ scripts/globals/mobskills/flurry_of_rage.lua
@@ -0,0 +1,27 @@
+-----------------------------------
+-- Flurry of Rage
+-- Deals multiple hits to a single target.
+-- Only used by Eldritch Edge, Malefic Fencer, and Nightmare Weapon.
+-----------------------------------
+require("scripts/settings/main")
+require("scripts/globals/status")
+require("scripts/globals/mobskills")
+-----------------------------------
+local mobskill_object = {}
+
+mobskill_object.onMobSkillCheck = function(target, mob, skill)
+    return 0
+end
+
+mobskill_object.onMobWeaponSkill = function(target, mob, skill)
+    local numhits = 3
+    local accmod = 1
+    local dmgmod = 1
+    local info = xi.mobskills.mobPhysicalMove(mob, target, skill, numhits, accmod, dmgmod, xi.mobskills.physicalTpBonus.ACC_VARIES, 1, 2, 3)
+    local dmg = xi.mobskills.mobFinalAdjustments(info.dmg, mob, skill, target, xi.attackType.PHYSICAL, xi.damageType.SLASHING, info.hitslanded)
+    target:takeDamage(dmg, mob, xi.attackType.PHYSICAL, xi.damageType.SLASHING)
+
+    return dmg
+end
+
+return mobskill_object
--- sql/mob_skill_lists.sql
+++ sql/mob_skill_lists.sql
@@ -3872,6 +3872,7 @@ INSERT INTO `mob_skill_lists` VALUES ('Jailer_of_Hope', 1202, 1355);
 INSERT INTO `mob_skill_lists` VALUES ('Jailer_of_Hope', 1202, 1356);
 INSERT INTO `mob_skill_lists` VALUES ('Jailer_of_Hope', 1202, 1358);
 INSERT INTO `mob_skill_lists` VALUES ('Zipacna',1203,678);
+INSERT INTO `mob_skill_lists` VALUES ('EldritchEdge',1204,397);
 
 -- Next available ID: 1204
 
