5654c48ff5b3921f34abd4c88338c4fcd9ebe094
claywar <claywar@beerlab.dev>
2022-06-25 12:41:05 -0400

    Interaction - Mission: A Portent Most Ominous

--- scripts/zones/Leafallia/DefaultActions.lua
+++ scripts/zones/Leafallia/DefaultActions.lua
@@ -1,6 +1,6 @@
 local ID = require('scripts/zones/Leafallia/IDs')
 
 return {
-    ['Aged_Stump']       = { messageSpecial = ID.text. RINGS_TOO_NUMEROUS },
+    ['Aged_Stump']       = { messageSpecial = ID.text.RINGS_TOO_NUMEROUS },
     ['Drifting_Feather'] = { messageSpecial = ID.text.NOTHING_OUT_OF_ORDINARY },
 }
