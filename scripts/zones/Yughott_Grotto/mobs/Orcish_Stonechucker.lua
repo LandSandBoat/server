92e43c845a5e75546cdeea776c1c3723f5e55a4f
Critical <lyardeen@gmail.com>
2022-06-27 16:50:49 -0600

    Yughott Zones NM Audit

--- scripts/zones/Yughott_Grotto/mobs/Orcish_Stonechucker.lua
+++ scripts/zones/Yughott_Grotto/mobs/Orcish_Stonechucker.lua
@@ -12,7 +12,7 @@ entity.onMobDeath = function(mob, player, isKiller)
 end
 
 entity.onMobDespawn = function(mob)
-    xi.mob.phOnDespawn(mob, ID.mob.ASHMAKER_GOTBLUT_PH, 5, math.random(7200, 10800)) -- 2 to 3 hours
+    xi.mob.phOnDespawn(mob, ID.mob.ASHMAKER_GOTBLUT_PH, 5, 3600) -- 1 hour minimum
 end
 
 return entity
