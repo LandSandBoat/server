760952c485a5163770e2d7b6ffee92a82b5765e7
dallano <atwood.dallan@hotmail.com>
2022-06-30 13:10:07 -0600

    Update Shen.lua
    Quick fix.

--- scripts/zones/Bibiki_Bay/mobs/Shen.lua
+++ scripts/zones/Bibiki_Bay/mobs/Shen.lua
@@ -24,16 +24,6 @@ entity.onMobFight = function(mob, target)
     spawnFiltrate(mob, target)
 end
 
-entity.spawnFiltrate = function(mob, target)
-    if mob:getAnimationSub() == 1 and mob:getHP() > 0 then
-        for i = 1, 2 do
-            if not GetMobByID(mob:getID()+i):isSpawned() then
-                SpawnMob(mob:getID()+i):updateEnmity(target)
-            end
-        end
-    end
-end
-
 entity.onMobDeath = function(mob, player, isKiller)
 end
 
