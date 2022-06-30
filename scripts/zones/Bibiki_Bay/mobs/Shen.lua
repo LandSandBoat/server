65b88d40d9cad7ead7ee78c3049636939f24efdd
dallano <atwood.dallan@hotmail.com>
2022-06-30 13:06:18 -0600

    Fixes
    Fixed spawning of the filtrate's to be handled locally
    Added spell lists for Shen and filtrates.

--- scripts/zones/Bibiki_Bay/mobs/Shen.lua
+++ scripts/zones/Bibiki_Bay/mobs/Shen.lua
@@ -11,7 +11,17 @@ entity.onMobSpawn = function(mob)
 end
 
 entity.onMobFight = function(mob, target)
-    entity.spawnFiltrate(mob, target)
+    local spawnFiltrate = function(mob, target)
+        if mob:getAnimationSub() == 1 and mob:getHP() > 0 then
+            for i = 1, 2 do
+                if not GetMobByID(mob:getID()+i):isSpawned() then
+                    SpawnMob(mob:getID()+i):updateEnmity(target)
+                end
+            end
+        end
+    end
+
+    spawnFiltrate(mob, target)
 end
 
 entity.spawnFiltrate = function(mob, target)
