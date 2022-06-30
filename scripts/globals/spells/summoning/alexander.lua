1ac4257d4e9a3f8bb2fd7e95d9fd9c4a71559873
Ophelia <84217630+OpheliaXI@users.noreply.github.com>
2022-06-29 21:24:01 -0500

    Revert "Revert "Merge branch 'LandSandBoat:base' into staging""
    This reverts commit 57044776504cc2878347a1d19c932673c1b79ae5.

--- scripts/globals/spells/summoning/alexander.lua
+++ scripts/globals/spells/summoning/alexander.lua
@@ -11,21 +11,20 @@ require("scripts/globals/status")
 local spell_object = {}
 
 spell_object.onMagicCastingCheck = function(caster, target, spell)
-    if (not caster:canUseMisc(xi.zoneMisc.PET)) then
+    if not caster:canUseMisc(xi.zoneMisc.PET) then
         return xi.msg.basic.CANT_BE_USED_IN_AREA
-    elseif (not caster:hasStatusEffect(xi.effect.ASTRAL_FLOW)) then
+    elseif not caster:hasStatusEffect(xi.effect.ASTRAL_FLOW) then
         return xi.msg.basic.MAGIC_MUST_ASTRAL_FLOW
-    elseif (caster:hasPet()) then
+    elseif caster:hasPet() then
         return xi.msg.basic.ALREADY_HAS_A_PET
-    elseif (caster:getObjType() == xi.objType.PC) then
-        return avatarMiniFightCheck(caster)
+    elseif caster:getObjType() == xi.objType.PC then
+        return xi.summon.avatarMiniFightCheck(caster)
     end
     return 0
 end
 
 spell_object.onSpellCast = function(caster, target, spell)
     caster:spawnPet(xi.pet.id.ALEXANDER)
-
     return 0
 end
 
