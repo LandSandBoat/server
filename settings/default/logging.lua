19ebf8decdb0e58d06560de11bc6c3dcdc6caf61
OpheliaXI <heliashelium@gmail.com>
2022-07-01 14:04:14 -0500

    Fix More Issues

---
+++ settings/default/logging.lua
@@ -0,0 +1,16 @@
+-----------------------------------
+-- LOGGING SETTINGS
+-----------------------------------
+-- All settings are attached to the `xi.settings` object. This is published globally, and be accessed from C++ and any script.
+--
+-- This file is concerned mainly with configuring the logging across all server executables.
+-----------------------------------
+
+xi = xi or {}
+xi.settings = xi.settings or {}
+
+xi.settings.logging =
+{
+    TRACE_NAVMESH = true,
+    TRACE_PACKETS = true,
+}
