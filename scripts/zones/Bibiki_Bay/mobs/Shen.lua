057886b1fe43d989a976c2ddca027631bd2ef896
Abdiah <62350957+dallano@users.noreply.github.com>
2022-07-03 09:45:16 -0600

    Update Shen.lua
    Making function local to remove redundancy

--- scripts/zones/Bibiki_Bay/mobs/Shen.lua
+++ scripts/zones/Bibiki_Bay/mobs/Shen.lua
@@ -11,17 +11,13 @@ entity.onMobSpawn = function(mob)
 end
 
 entity.onMobFight = function(mob, target)
-    local spawnFiltrate = function()
-        if mob:getAnimationSub() == 1 and mob:getHP() > 0 then
-            for i = 1, 2 do
-                if not GetMobByID(mob:getID()+i):isSpawned() then
-                    SpawnMob(mob:getID()+i):updateEnmity(target)
-                end
+    if mob:getAnimationSub() == 1 and mob:getHP() > 0 then
+        for i = 1, 2 do
+            if not GetMobByID(mob:getID()+i):isSpawned() then
+                SpawnMob(mob:getID()+i):updateEnmity(target)
             end
         end
     end
-
-    spawnFiltrate()
 end
 
 entity.onMobDeath = function(mob, player, isKiller)
