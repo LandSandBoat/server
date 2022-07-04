1f57a557c9afc85b279fb4f9953a0bd8f351cd30
Abdiah <62350957+dallano@users.noreply.github.com>
2022-07-04 08:11:36 -0700

    Shen rework + removal of Hakutaku's triple attack mod + remove drop pools from mob bst's pets (#2158)
    * Rebasing NM_Mob_Fixes
    
    Created new branch to organize NM_Mob_Fixes into a single commit as well as shaved off irrelevant changes.
    
    Changes included:
    Shen rework
    Removal of Hakutaku's triple attack mod upon request.
    Removal of mob beastmaster pet's ability to drop items.
    
    * Fixes
    
    Fixed spawning of the filtrate's to be handled locally
    Added spell lists for Shen and filtrates.
    
    * Update Shen.lua
    
    Quick fix.
    
    * Update Shen.lua
    
    Another embarrassing fix in the changing of a local function.
    
    * Update mob_spell_lists.sql
    
    SQL duplicate name fix.
    
    * Update mob_spell_lists.sql
    
    Missing fix
    
    * Update Shen.lua
    
    * Update Shen.lua
    
    Making function local to remove redundancy

--- sql/mob_spell_lists.sql
+++ sql/mob_spell_lists.sql
@@ -4925,7 +4925,18 @@ INSERT INTO `mob_spell_lists` VALUES ('Hakutaku',486,204,1,255); -- flare (1~255
 INSERT INTO `mob_spell_lists` VALUES ('Hakutaku',486,235,1,255); -- burn (1~255)
 INSERT INTO `mob_spell_lists` VALUES ('Hakutaku',486,249,1,255); -- blaze_spikes (1~255)
 
--- Next Available: 487
+-- Shen (487)
+INSERT INTO `mob_spell_lists` VALUES ('Shen',487,172,1,255); -- water_iv (1~255)
+INSERT INTO `mob_spell_lists` VALUES ('Shen',487,201,1,255); -- waterga_iii (1~255)
+INSERT INTO `mob_spell_lists` VALUES ('Shen',487,214,1,255); -- flood (1~255)
+INSERT INTO `mob_spell_lists` VALUES ('Shen',487,240,1,255); -- drown (1-255)
+
+-- Shen's Filtrate (488)
+INSERT INTO `mob_spell_lists` VALUES ('Shens_Filtrate',488,172,1,255); -- water_iv (1~255)
+INSERT INTO `mob_spell_lists` VALUES ('Shens_Filtrate',488,201,1,255); -- waterga_iii (1~255)
+INSERT INTO `mob_spell_lists` VALUES ('Shens_Filtrate',488,214,1,255); -- flood (1~255)
+
+-- Next Available: 489
 
 /*!40000 ALTER TABLE `mob_spell_lists` ENABLE KEYS */;
 UNLOCK TABLES;
