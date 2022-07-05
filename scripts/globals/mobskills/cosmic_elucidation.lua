c61d00482de9a1dcd4d87b607011b60aeeca5b77
WinterSolstice8 <60417494+WinterSolstice8@users.noreply.github.com>
2022-07-05 15:12:22 -0700

    [lua] Adjust Tenzen's Cosmic Elucidation to have "non elemental" special damage
    * Clamp damage of Cosmic Elucidation to 0, as there is no absorb message for it

--- scripts/globals/mobskills/cosmic_elucidation.lua
+++ scripts/globals/mobskills/cosmic_elucidation.lua
@@ -21,7 +21,10 @@ mobskill_object.onMobWeaponSkill = function(target, mob, skill)
     local dmgmod = 2
     local info = xi.mobskills.mobMagicalMove(mob, target, skill, mob:getWeaponDmg()*21, xi.magic.ele.LIGHT, dmgmod, xi.mobskills.magicalTpBonus.DMG_BONUS, 1)
     local dmg = xi.mobskills.mobFinalAdjustments(info.dmg, mob, skill, target, xi.attackType.MAGICAL, xi.damageType.LIGHT, 0)
-    target:takeDamage(dmg, mob, xi.attackType.MAGICAL, xi.damageType.LIGHT)
+
+    dmg = math.min(0, dmg) -- Cosmic Elucidation does not have an absorb message
+
+    target:takeDamage(dmg, mob, xi.attackType.SPECIAL, xi.damageType.ELEMENTA)
     skill:setMsg(302)
 
     return dmg
