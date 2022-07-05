48d044ac855e276847a1e344e2d0ce006e1dad95
Tom-Neverwinter <Tom_Neverwinter@yahoo.com>
2022-07-05 14:13:19 -0500

    Update Zone.lua
     --temp zone in testing --teleporters need scripting --zone in doors not scripted add globals

--- scripts/zones/Chocobo_Circuit/Zone.lua
+++ scripts/zones/Chocobo_Circuit/Zone.lua
@@ -4,6 +4,7 @@
 --
 -----------------------------------
 local ID = require("scripts/zones/Chocobo_Circuit/IDs")
+require("scripts/globals/zone")
 -----------------------------------
 local zone_object = {}
 
@@ -12,6 +13,10 @@ end
 
 zone_object.onZoneIn = function(player, prevZone)
     local cs = -1
+
+    if player:getXPos() == 0 and player:getYPos() == 0 and player:getZPos() == 0 then
+        player:setPos(-319.816, -0.2895, -475.1285) --temp zone in testing --teleporters need scripting --zone in doors not scripted
+    end
 
     return cs
 end
@@ -26,3 +31,4 @@ zone_object.onEventFinish = function(player, csid, option)
 end
 
 return zone_object
+
