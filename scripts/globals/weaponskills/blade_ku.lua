53a2e81074ef7a16369355c75598fce0957d53bd
Schaedelschaden <sitfixalot@gmail.com>
2022-07-06 09:18:07 -0600

    [WS] Add multihit FTP to Katana, Polearm, Scythe, and Sword weaponskills (#2148)
    * [WS] Add multihit FTP to Katana, Polearm, Scythe, and Sword weaponskills
    
    * Adjust Jin, Shun, Chant, Req files
    
    * Update Chant du Cygne crit parameters

--- scripts/globals/weaponskills/blade_ku.lua
+++ scripts/globals/weaponskills/blade_ku.lua
@@ -23,27 +23,29 @@ require("scripts/globals/weaponskills")
 local weaponskill_object = {}
 
 weaponskill_object.onUseWeaponSkill = function(player, target, wsID, tp, primary, action, taChar)
-
     local params = {}
     params.numHits = 5
-    params.ftp100 = 1 params.ftp200 = 1 params.ftp300 = 1
-    params.str_wsc = 0.1 params.dex_wsc = 0.1 params.vit_wsc = 0.0 params.agi_wsc = 0.0 params.int_wsc = 0.0 params.mnd_wsc = 0.0 params.chr_wsc = 0.0
+    params.ftp100 = 1.0 params.ftp200 = 1.0 params.ftp300 = 1.0
+    params.str_wsc = 0.1 params.dex_wsc = 0.1 params.vit_wsc = 0.0
+    params.agi_wsc = 0.0 params.int_wsc = 0.0 params.mnd_wsc = 0.0
+    params.chr_wsc = 0.0
     params.crit100 = 0.0 params.crit200 = 0.0 params.crit300 = 0.0
     params.canCrit = false
-    params.acc100 = 0.9 params.acc200= 1 params.acc300= 1.1
-    --  the TP bonus for TP 1,000 and above has been raised, which has substantially raised the hit rate http://wiki.ffo.jp/html/732.html
-    -- data on ws accuracy are difficult to come by, would need to decide on sane value for the accuracy boost stated in JPWiki for now I'm only adding 0.1 per tier
-    params.atk100 = 1; params.atk200 = 1; params.atk300 = 1
+    -- Sufficient data for ACC bonus/penalty does not exist; assuming no penalty and 10% increase per 1,000 TP
+    -- http://wiki.ffo.jp/html/732.html does not list ACC Bonus
+    -- https://www.bg-wiki.com/ffxi/Blade:_Ku does not list ACC Bonus
+    params.acc100 = 1.0 params.acc200 = 1.1 params.acc300 = 1.2
+    params.atk100 = 1.0 params.atk200 = 1.0 params.atk300 = 1.0
 
     if xi.settings.main.USE_ADOULIN_WEAPON_SKILL_CHANGES then
-        params.multiHitfTP = true -- http://wiki.ffo.jp/html/732.html
         params.ftp100 = 1.25 params.ftp200 = 1.25 params.ftp300 = 1.25
         params.str_wsc = 0.3 params.dex_wsc = 0.3
+        params.multiHitfTP = true
     end
 
     local damage, criticalHit, tpHits, extraHits = doPhysicalWeaponskill(player, target, wsID, params, tp, action, primary, taChar)
-    return tpHits, extraHits, criticalHit, damage
 
+    return tpHits, extraHits, criticalHit, damage
 end
 
 return weaponskill_object
--- scripts/globals/weaponskills/stardiver.lua
+++ scripts/globals/weaponskills/stardiver.lua
@@ -16,18 +16,20 @@ require("scripts/globals/weaponskills")
 local weaponskill_object = {}
 
 weaponskill_object.onUseWeaponSkill = function(player, target, wsID, tp, primary, action, taChar)
-
     local params = {}
     params.numHits = 4
     params.ftp100 = 0.75 params.ftp200 = 1.25 params.ftp300 = 1.75
-    params.str_wsc = 0.0 + (player:getMerit(xi.merit.STARDIVER) * 0.17) params.dex_wsc = 0.0 params.vit_wsc = 0.0 params.agi_wsc = 0.0 params.int_wsc = 0.0 params.mnd_wsc = 0.0 params.chr_wsc = 0.0
+    params.str_wsc = player:getMerit(xi.merit.STARDIVER) * 0.17 params.dex_wsc = 0.0 params.vit_wsc = 0.0
+    params.agi_wsc = 0.0 params.int_wsc = 0.0 params.mnd_wsc = 0.0
+    params.chr_wsc = 0.0
     params.crit100 = 0.0 params.crit200 = 0.0 params.crit300 = 0.0
     params.canCrit = false
-    params.acc100 = 0.0 params.acc200= 0.0 params.acc300= 0.0
-    params.atk100 = 1; params.atk200 = 1; params.atk300 = 1
+    params.acc100 = 0.0 params.acc200 = 0.0 params.acc300 = 0.0
+    params.atk100 = 1.0 params.atk200 = 1.0 params.atk300 = 1.0
 
-    if (xi.settings.main.USE_ADOULIN_WEAPON_SKILL_CHANGES == true) then
+    if xi.settings.main.USE_ADOULIN_WEAPON_SKILL_CHANGES then
         params.str_wsc = 0.7 + (player:getMerit(xi.merit.STARDIVER) * 0.03)
+        params.multiHitfTP = true
     end
 
     local damage, criticalHit, tpHits, extraHits = doPhysicalWeaponskill(player, target, wsID, params, tp, action, primary, taChar)
@@ -35,8 +37,8 @@ weaponskill_object.onUseWeaponSkill = function(player, target, wsID, tp, primary
     if (damage > 0 and target:hasStatusEffect(xi.effect.CRIT_HIT_EVASION_DOWN) == false) then
         target:addStatusEffect(xi.effect.CRIT_HIT_EVASION_DOWN, 5, 0, 60)
     end
-    return tpHits, extraHits, criticalHit, damage
 
+    return tpHits, extraHits, criticalHit, damage
 end
 
 return weaponskill_object
