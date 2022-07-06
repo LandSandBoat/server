06d1a28a8e2a08289abfc11dd032a7883d1f2270
beneliezer <beneliezer@gmail.com>
2022-07-06 09:06:07 -0400

    add placeholder for proc effect

--- scripts/globals/geomantic_reservoir.lua
+++ scripts/globals/geomantic_reservoir.lua
@@ -47,13 +47,11 @@ local geoSpellTable =
 xi.geomanticReservoir.onTrigger = function(player, npc, geoSpell)
     -- TODO: According to BG-Wiki there is a sequence here that a player can proc '!!' and achieve a Geomancy skill-up.
     -- https://www.bg-wiki.com/ffxi/Geomantic_Reservoir
-    if player:getMainJob() == xi.job.GEO then
-        if not player:hasSpell(geoSpell) then
-            player:startEvent(15000, 277)
-        else
-            player:messageSpecial(zones[player:getZoneID()].text.UNCANNY_SENSATION)
-        end
-    else -- Default text when player is not on GEO
+    local procEffectTime = math.random(230, 300)
+    if player:getMainJob() == xi.job.GEO and not player:hasSpell(geoSpell) then
+        player:startEvent(15000,  procEffectTime)
+        -- TODO add skillup logic if player clicks at the time the proc happens
+    else
         player:messageSpecial(zones[player:getZoneID()].text.NOTHING_OUT_OF_ORDINARY)
     end
 end
