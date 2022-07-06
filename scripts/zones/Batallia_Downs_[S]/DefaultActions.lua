198447ae20f827fdd33d5b92d6bffea0a24661c3
claywar <claywar@beerlab.dev>
2022-07-06 19:50:17 -0400

    [qc] Add additional dialogue and audit WotG missions through A Nation on the Brink
    Daughter of a Knight
    
    
    A Spoonful of Sugar
    
    
    Affairs of State
    
    
    Bourne by the Wind
    
    
    Add campaign allegiance to parameters
    
    
    Nation on the Brink
    
    
    Syntax

--- scripts/zones/Batallia_Downs_[S]/DefaultActions.lua
+++ scripts/zones/Batallia_Downs_[S]/DefaultActions.lua
@@ -1,6 +1,8 @@
 local ID = require("scripts/zones/Batallia_Downs_[S]/IDs")
 
 return {
-    ['Eberhard'] = { event = 113 },
-    ['Romualdo'] = { event = 102 },
+    ['_qm5']            = { messageSpecial = ID.text.NOTHING_OUT_OF_ORDINARY },
+    ['Eberhard']        = { event = 113 },
+    ['Romualdo']        = { event = 102 },
+    ['Underpass_Hatch'] = { messageSpecial = ID.text.NO_RESPONSE },
 }
