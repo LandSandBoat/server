695a0fddcf1e5f0b73fd01984c3d172d058a2735
Tal Ben-Eliezer <beneliezer@gmail.com>
2022-07-08 01:01:55 -0400

    GEO/RUN stuff Update ZNMs

--- modules/catseyexi/sql/CatsEyeXI_Add_RUN_GEO_to_Gearsets.sql
+++ modules/catseyexi/sql/CatsEyeXI_Add_RUN_GEO_to_Gearsets.sql
@@ -60,6 +60,7 @@ DELETE from item_mods WHERE itemId = "27194";
 DELETE from item_mods WHERE itemId = "27370";
 
 # Geomancy Galero
+UPDATE item_equipment SET level = "75", ilevel = "0" WHERE itemId = "27786";
 INSERT into item_mods VALUES (27786,1,22); -- DEF: 22
 INSERT into item_mods VALUES (27786,5,25); -- MP+25
 INSERT into item_mods VALUES (27786,12,5); -- INT+5
@@ -67,6 +68,7 @@ INSERT into item_mods VALUES (27786,115,15); -- Elemental Magic Skill +15
 -- INSERT into item_mods VALUES (27786,959,22); -- Enhances "Cardinal Chant" Effect
 
 # Geomancy Tunic
+UPDATE item_equipment SET level = "75", ilevel = "0" WHERE itemId = "27926";
 INSERT into item_mods VALUES (27926,1,40); -- DEF: 40
 INSERT into item_mods VALUES (27926,5,35); -- MP+35
 INSERT into item_mods VALUES (27926,116,12); -- Dark magic skill +12
@@ -74,6 +76,7 @@ INSERT into item_mods VALUES (27926,1029,10); -- Enhances "Life Cycle" effect
 INSERT into item_mods VALUES (27926,71,5); -- MP recovered while healing +5
 
 # Geomancy Mitaines
+UPDATE item_equipment SET level = "75", ilevel = "0" WHERE itemId = "28066";
 INSERT into item_mods VALUES (28066,1,15); -- DEF: 15
 INSERT into item_mods VALUES (28066,5,16); -- MP+16
 INSERT into item_mods VALUES (28066,12,3); -- INT+3
@@ -82,12 +85,14 @@ INSERT into item_mods VALUES (28066,123,15); -- Geomancy skill +15
 INSERT into item_mods VALUES (28066,990,22); -- Luopan: (Physical) Damage taken -6% -- No specific mod currently exists for this.
 
 # Geomancy Pants
+UPDATE item_equipment SET level = "75", ilevel = "0" WHERE itemId = "28206";
 INSERT into item_mods VALUES (28206,1,30); -- DEF: 30
 INSERT into item_mods VALUES (28206,5,20); -- MP+20
 INSERT into item_mods VALUES (28206,170,10); -- Enhances "Fast Cast" effect +10%
 INSERT into item_mods VALUES (28206,168,15); -- Spell interruption rate down 15%
 
 # Geomancy Sandals
+UPDATE item_equipment SET level = "75", ilevel = "0" WHERE itemId = "28346";
 INSERT into item_mods VALUES (28346,1,12); -- DEF: 12
 INSERT into item_mods VALUES (28346,5,15); -- MP+15
 INSERT into item_mods VALUES (28346,11,5); -- AGI+5
@@ -95,6 +100,7 @@ INSERT into item_mods VALUES (28346,14,5); -- CHR+5
 INSERT into item_mods VALUES (28346,169,12); -- Movement speed +12%
 
 # Bagua Galero
+UPDATE item_equipment SET level = "75", ilevel = "0" WHERE itemId = "26664";
 INSERT into item_mods VALUES (26664,1,25); -- DEF: 25
 INSERT into item_mods VALUES (26664,5,25); -- MP+25
 INSERT into item_mods VALUES (26664,12,3); -- INT+3
@@ -102,6 +108,7 @@ INSERT into item_mods VALUES (26664,13,3); -- MND+3
 INSERT into item_mods VALUES (26664,315,10); -- “Drain” and “Aspir” potency +10
 
 # Bagua Tunic
+UPDATE item_equipment SET level = "75", ilevel = "0" WHERE itemId = "26840";
 INSERT into item_mods VALUES (26840,1,43); -- DEF: 43
 INSERT into item_mods VALUES (26840,2,15); -- HP+15
 INSERT into item_mods VALUES (26840,5,15); -- MP+15
@@ -109,12 +116,14 @@ INSERT into item_mods VALUES (26840,28,5); -- “Magic Attack Bonus” +5
 INSERT into item_mods VALUES (26840,123,10); -- Geomancy skill +10
 
 # Bagua Mitaines
+UPDATE item_equipment SET level = "75", ilevel = "0" WHERE itemId = "27016";
 INSERT into item_mods VALUES (27016,1,17); -- DEF: 17
 INSERT into item_mods VALUES (27016,5,21); -- MP+21
 INSERT into item_mods VALUES (27016,369,1); -- “Refresh + 1”
 INSERT into item_mods VALUES (27016,901,6); -- Elemental Magic casting time -6%
 
 # Bagua Pants
+UPDATE item_equipment SET level = "75", ilevel = "0" WHERE itemId = "27192";
 INSERT into item_mods VALUES (27192,1,31); -- DEF: 31
 INSERT into item_mods VALUES (27192,5,20); -- MP+20
 INSERT into item_mods VALUES (27192,12,5); -- INT+5
@@ -122,6 +131,7 @@ INSERT into item_mods VALUES (27192,13,5); -- MND+5
 INSERT into item_mods VALUES (27192,960,12); -- “Indicolure” spell duration + 12
 
 # Bagua Sandals
+UPDATE item_equipment SET level = "75", ilevel = "0" WHERE itemId = "27368";
 INSERT into item_mods VALUES (27368,1,16); -- DEF: 16
 INSERT into item_mods VALUES (27368,5,18); -- MP+18
 INSERT into item_mods VALUES (27368,13,3); -- MND+3
@@ -129,6 +139,7 @@ INSERT into item_mods VALUES (27368,114,12); -- Enfeebling magic skill +12
 -- INSERT into item_mods VALUES (27368,1,22); -- Luopan: “Regen”+2 (This doesn't exist)
 
 # Runeist Bandeau
+UPDATE item_equipment SET level = "75", ilevel = "0" WHERE itemId = "27787";
 INSERT into item_mods VALUES (27787,1,27); -- DEF: 27
 INSERT into item_mods VALUES (27787,2,21); -- HP+21
 INSERT into item_mods VALUES (27787,9,5); -- DEX+5
@@ -136,15 +147,24 @@ INSERT into item_mods VALUES (27787,370,3); -- "Regen" +3
 INSERT into item_mods VALUES (27787,161,3); -- Physical damage taken -3%
 
 # Runeist Coat
+UPDATE item_equipment SET level = "75", ilevel = "0" WHERE itemId = "27927";
 INSERT into item_mods VALUES (27927,1,50); -- DEF: 50
 INSERT into item_mods VALUES (27927,2,20); -- HP+20
 INSERT into item_mods VALUES (27927,5,20); -- MP+20
 INSERT into item_mods VALUES (27927,8,5); -- STR+5
 INSERT into item_mods VALUES (27927,10,5); -- VIT+5
-INSERT into item_mods VALUES (27927,802,10); -- Resist all elements +10
+INSERT INTO item_mods VALUES (27927,15,20);  -- FIRE_RES: 20
+INSERT INTO item_mods VALUES (27927,16,20);  -- ICE_RES: 20
+INSERT INTO item_mods VALUES (27927,17,20);  -- WIND_RES: 20
+INSERT INTO item_mods VALUES (27927,18,20);  -- EARTH_RES: 20
+INSERT INTO item_mods VALUES (27927,19,20);  -- THUNDER_RES: 20
+INSERT INTO item_mods VALUES (27927,20,20);  -- WATER_RES: 20
+INSERT INTO item_mods VALUES (27927,21,20);  -- LIGHT_RES: 20
+INSERT INTO item_mods VALUES (27927,22,20);  -- DARK_RES: 20
 INSERT into item_mods VALUES (27927,1010,10); -- Enhances "Valiance" and "Vallation" effects
 
 # Runeist Mitons
+UPDATE item_equipment SET level = "75", ilevel = "0" WHERE itemId = "28067";
 INSERT into item_mods VALUES (28067,1,21); -- DEF: 21
 INSERT into item_mods VALUES (28067,5,22); -- MP+22
 INSERT into item_mods VALUES (28067,10,3); -- VIT+3
@@ -153,6 +173,7 @@ INSERT into item_mods VALUES (28067,369,1); -- "Refresh" +1
 INSERT into item_mods VALUES (28067,1018,10); -- Ennhances "Gambit" Effect
 
 # Runeist Trousers
+UPDATE item_equipment SET level = "75", ilevel = "0" WHERE itemId = "28207";
 INSERT into item_mods VALUES (28207,1,40); -- DEF: 40
 INSERT into item_mods VALUES (28207,2,15); -- HP+15
 INSERT into item_mods VALUES (28207,5,15); -- MP+15
@@ -161,6 +182,7 @@ INSERT into item_mods VALUES (28207,27,5); -- Enmity+5
 INSERT into item_mods VALUES (28207,160,3); -- Damage taken -3%
 
 # Runeist Bottes
+UPDATE item_equipment SET level = "75", ilevel = "0" WHERE itemId = "28347";
 INSERT into item_mods VALUES (28347,1,18); -- DEF: 18
 INSERT into item_mods VALUES (28347,2,18); -- HP+18
 INSERT into item_mods VALUES (28347,10,6); -- VIT+6
@@ -168,6 +190,7 @@ INSERT into item_mods VALUES (28347,29,2); -- Magic def. bonus +2
 INSERT into item_mods VALUES (28347,1011,10); -- Enhances "Pflug" effect
 
 # Futhark Bandeau
+UPDATE item_equipment SET level = "75", ilevel = "0" WHERE itemId = "26666";
 INSERT into item_mods VALUES (26666,1,28); -- DEF: 28
 INSERT into item_mods VALUES (26666,2,11); -- HP+11
 INSERT into item_mods VALUES (26666,5,11); -- MP+11
@@ -176,6 +199,7 @@ INSERT into item_mods VALUES (26666,384,3); -- Haste +3%
 INSERT into item_mods VALUES (26666,1004,10); -- Enhances "Battuta" effect
 
 # Futhark Coat
+UPDATE item_equipment SET level = "75", ilevel = "0" WHERE itemId = "26842";
 INSERT into item_mods VALUES (26842,1,52); -- DEF: 52
 INSERT into item_mods VALUES (26842,2,15); -- HP+15
 INSERT into item_mods VALUES (26842,5,15); -- MP+15
@@ -185,6 +209,7 @@ INSERT into item_mods VALUES (26842,31,9); -- Magic Evasion +9
 INSERT into item_mods VALUES (26842,516,5); -- Converts 5% of physical damage taken to MP
 
 # Futhark Mitons
+UPDATE item_equipment SET level = "75", ilevel = "0" WHERE itemId = "27018";
 INSERT into item_mods VALUES (27018,1,20); -- DEF: 20
 INSERT into item_mods VALUES (27018,2,17); -- HP+17
 INSERT into item_mods VALUES (27018,9,7); -- DEX+7
@@ -192,6 +217,7 @@ INSERT into item_mods VALUES (27018,1008,3); -- "Swordplay" +3
 INSERT into item_mods VALUES (27018,1006,10); -- Enhances "Sleight of Sword" effect
 
 # Futhark Trousers
+UPDATE item_equipment SET level = "75", ilevel = "0" WHERE itemId = "27194";
 INSERT into item_mods VALUES (27194,1,41); -- DEF: 41
 INSERT into item_mods VALUES (27194,2,20); -- HP+20
 INSERT into item_mods VALUES (27194,5,20); -- MP+20
@@ -201,6 +227,7 @@ INSERT into item_mods VALUES (27194,890,10); -- Enhancing magic duration +10
 INSERT into item_mods VALUES (27194,1007,10); -- Enhances "Inspiration" effect
 
 # Futhark Bottes
+UPDATE item_equipment SET level = "75", ilevel = "0" WHERE itemId = "27370";
 INSERT into item_mods VALUES (27370,1,19); -- DEF: 19
 INSERT into item_mods VALUES (27370,5,21); -- MP+21
 INSERT into item_mods VALUES (27370,8,5); -- STR+5
