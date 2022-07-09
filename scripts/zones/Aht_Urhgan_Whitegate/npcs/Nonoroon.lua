083d766b747cf4ac7629b6e9a46189b6ba683ff4
Tal Ben-Eliezer <beneliezer@gmail.com>
2022-07-09 19:35:51 -0400

    fix mythic weapon exchange quest

--- scripts/zones/Aht_Urhgan_Whitegate/npcs/Nonoroon.lua
+++ scripts/zones/Aht_Urhgan_Whitegate/npcs/Nonoroon.lua
@@ -19,7 +19,7 @@ entity.onTrigger = function(player, npc)
     local pZeni  = player:getCurrency("zeni_point")
     local pLevel = player:getMainLvl()
             
-    if pZeni >= 6000 and pLevel > 74 then -- Zeni Points Check and Player level check.
+    if pZeni >= 6000 and pLevel > 74 and player:getCharVar("MythicWeaponExchange") == 1 then -- Zeni Points Check and Player level check.
         player:PrintToPlayer("Nonoroon: I see you have at least 6000 Zeni Points and are able to equip this weapon", 0xd)
         player:PrintToPlayer("Nonoroon: If you can help me out with this myth then you can have the final reward!", 0xd)
         player:PrintToPlayer("Nonoroon: Please make sure you are on the Job you wish to continue this path!", 0xd)
@@ -33,11 +33,12 @@ entity.onTrade = function(player, npc, trade)
     local pZeni  = player:getCurrency("zeni_point")
     local pLevel = player:getMainLvl()
 
-    if pZeni >= 6000 and pLevel > 74 and npcUtil.tradeHasExactly(trade, {{2187, 5}}) then
+    if pZeni >= 6000 and pLevel > 74 and player:getCharVar("MythicWeaponExchange") == 1 and npcUtil.tradeHasExactly(trade, {{2187, 5}}) then
         player:tradeComplete()
         player:delCurrency("zeni_point", 6000)
-        player:PrintToPlayer( "Nonoroon: Thank you for the persistent intrest in this Myth here is your reward.", 0xd)
+        player:PrintToPlayer( "Nonoroon: Yooo man, here's your new base weapon. See the homie Paparoon to cash in that alexandrite!", 0xd)
         npcUtil.giveItem(player, 18970 + player:getMainJob())
+		player:setCharVar("MythicWeaponExchange", 2)
     else
         player:PrintToPlayer( "Nonoroon: You need more Zeni Points, Incorrect level or wrong items to proceed .", 0xd)
     end
