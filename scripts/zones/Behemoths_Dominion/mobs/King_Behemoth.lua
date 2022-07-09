b4d5c32d7233a18e032f7c1d10c26bc08ca101c3
Critical <lyardeen@gmail.com>
2022-07-08 20:57:31 -0600

    Adds special draw in logic to ToAU Kings and Behemoth. Removes Draw In mods from mob_spawn_mods

--- scripts/zones/Behemoths_Dominion/mobs/King_Behemoth.lua
+++ scripts/zones/Behemoths_Dominion/mobs/King_Behemoth.lua
@@ -25,6 +25,22 @@ entity.onMobSpawn = function(mob)
     mob:setLocalVar("[rage]timer", 3600) -- 60 minutes
 end
 
+entity.onMobFight = function(mob, target)
+    local drawInWait = mob:getLocalVar("DrawInWait")
+
+    if (target:getXPos() > -180 and target:getZPos() > 53) and os.time() > drawInWait then -- North Tunnel Draw In
+        local rot = target:getRotPos()
+        target:setPos(-182.19,-19.83,58.34,rot)
+        mob:messageBasic(232, 0, 0, target)
+        mob:setLocalVar("DrawInWait", os.time() + 2)
+    elseif (target:getXPos() > -230 and target:getZPos() < 5) and os.time() > drawInWait then  -- South Tunnel Draw In
+        local rot = target:getRotPos()
+        target:setPos(-235.35,-20.01,-4.47,rot)
+        mob:messageBasic(232, 0, 0, target)
+        mob:setLocalVar("DrawInWait", os.time() + 2)
+    end
+end
+
 entity.onAdditionalEffect = function(mob, target, damage)
     return xi.mob.onAddEffect(mob, target, damage, xi.mob.ae.STUN, {chance = 20, duration = math.random(5, 10)})
 end
