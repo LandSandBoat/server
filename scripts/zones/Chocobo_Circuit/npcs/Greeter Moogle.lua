b5da84cfbd14d5cb79a5404a2a95128fe5b74ac6
Tom-Neverwinter <Tom_Neverwinter@yahoo.com>
2022-07-09 19:41:07 -0500

    Copy greeter moogle
    This is a copy of Port San dOria greeter moogle with csid changed.

--- scripts/zones/Chocobo_Circuit/npcs/Greeter Moogle.lua
+++ scripts/zones/Chocobo_Circuit/npcs/Greeter Moogle.lua
@@ -4,16 +4,20 @@
 -- Novice San d'Oria CRA Branch
 -- pos -323.9516 0.0000 -480.2049
 -----------------------------------
+require("scripts/globals/events/login_campaign")
+-----------------------------------
 local entity = {}
 
 entity.onTrade = function(player, npc, trade)
 end
 
 entity.onTrigger = function(player, npc)
-    player:startEvent(500)
+    local csid = 500
+    xi.events.loginCampaign.onTrigger(player, csid)
 end
 
 entity.onEventUpdate = function(player, csid, option)
+    xi.events.loginCampaign.onEventUpdate(player, csid, option)
 end
 
 entity.onEventFinish = function(player, csid, option)
