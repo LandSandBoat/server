648b225f20847a648cd8c7ac6e0d5a182061d94a
OpheliaXI <heliashelium@gmail.com>
2022-07-10 20:59:39 -0500

    Add Eva Down to Weight Effect

--- scripts/globals/effects/weight.lua
+++ scripts/globals/effects/weight.lua
@@ -7,6 +7,7 @@ local effect_object = {}
 
 effect_object.onEffectGain = function(target, effect)
     target:addMod(xi.mod.MOVE, -effect:getPower())
+    target:addMod(xi.mod.EVA, -10)
 end
 
 effect_object.onEffectTick = function(target, effect)
@@ -14,6 +15,7 @@ end
 
 effect_object.onEffectLose = function(target, effect)
     target:delMod(xi.mod.MOVE, -effect:getPower())
+    target:addMod(xi.mod.EVA, 10)
 end
 
 return effect_object
