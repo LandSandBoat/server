9efe54e615dbdd6c0aa0c9d286b701ed1183c4b9
NeuromancerXI <neuromancerxi@users.noreply.github.com>
2022-07-12 14:31:55 -0700

    Add logic for out of order Mount ID, KI, Item to Mapitoto

--- scripts/zones/Upper_Jeuno/npcs/Mapitoto.lua
+++ scripts/zones/Upper_Jeuno/npcs/Mapitoto.lua
@@ -25,6 +25,12 @@ entity.onTrade = function(player,npc,trade)
         if item == xi.items.CHOCOBO_WHISTLE then
             player:startEvent(10227, xi.items.CHOCOBO_WHISTLE, xi.ki.TRAINERS_WHISTLE, xi.mount.CHOCOBO)
             player:setLocalVar("FullSpeedAheadReward", xi.ki.CHOCOBO_COMPANION)
+        elseif item == xi.items.RED_RAPTOR_NOTEBOOK then -- Key Items, Items, and Mount IDs don't line up for 4 mounts starting with Red Raptor
+            player:setLocalVar("FullSpeedAheadReward", xi.ki.TIGER_COMPANION + mount + 3)
+            player:startEvent(10227, item, xi.ki.TRAINERS_WHISTLE, xi.mount.TIGER + mount + 2)
+        elseif item >= xi.items.GOLDEN_BOMB_NOTEBOOK and item <= xi.items.WIVRE_NOTEBOOK then -- These are all offset by one due to Red Raptor
+            player:setLocalVar("FullSpeedAheadReward", xi.ki.TIGER_COMPANION + mount - 1)
+            player:startEvent(10227, item, xi.ki.TRAINERS_WHISTLE, xi.mount.TIGER + mount - 2)
         elseif mount >= xi.mount.CHOCOBO and mount <= xi.mount.MOUNT_MAX then
             player:setLocalVar("FullSpeedAheadReward", xi.ki.TIGER_COMPANION + mount)
             player:startEvent(10227, item, xi.ki.TRAINERS_WHISTLE, xi.mount.TIGER + mount - 1)
