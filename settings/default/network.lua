1d87a476b475b17c61856e27b0ff56fde9a9aed7
Jason McDaniel <jaymac83@gmail.com>
2022-07-12 09:38:49 -0500

    Moving all packet configuration into the network.lua file  + to differentiate between the two sets I've added either UDP_ or TCP_ to the key name  + all items have been provided along with the comments specified  + for IP rules, the TCP_ALLOW, TCP_DENY keys have been updated to contain lists    + default is empty string (no-op) in the code during setup    + each entry must be separated with a 'comma' character   - removed old configuration files (no longer referenced)   + all settings now consistent across project

--- settings/default/network.lua
+++ settings/default/network.lua
@@ -31,4 +31,76 @@ xi.settings.network =
     -- Central message server settings (ensure these are the same on both all map servers and the central (lobby) server
     ZMQ_IP   = "127.0.0.1",
     ZMQ_PORT = 54003,
+
+    -- ===========================
+    -- UDP Sockets Configuration
+    -- ===========================
+
+    -- Display debug reports (When something goes wrong during the report, the report is saved.)
+    UDP_DEBUG = false,
+
+    -- ===========================
+    -- TCP Sockets Configuration
+    -- ===========================
+
+    -- Display debug reports (When something goes wrong during the report, the report is saved.)
+    TCP_DEBUG = true,
+
+    -- How long can a socket stall before closing the connection (in seconds)
+    TCP_STALL_TIME = 60,
+
+    -- ===========================
+    -- IP Rules Settings
+    -- ===========================
+
+    -- If IP's are checked when connecting. This also enables DDoS protection.
+    TCP_ENABLE_IP_RULES = true,
+
+    -- Order of the checks
+    --   deny,allow     : Checks deny rules, then allow rules. Allows if no rules match.
+    --   allow,deny     : Checks allow rules, then deny rules. Allows if no rules match.
+    --   mutual-failure : Allows only if an allow rule matches and no deny rules match.
+    -- (default is deny,allow)
+    TCP_ORDER = "deny,allow",
+
+    -- ===========================
+    -- IP rules
+    -- ===========================
+    --   allow : Accepts connections from the ip range (even if flagged as DDoS)
+    --   deny  : Rejects connections from the ip range
+    -- The rules are processed in order, the first matching rule of each list
+    -- (allow and deny) is used
+
+    TCP_ALLOW = "",
+    -- TCP_ALLOW = "127.0.0.1,192.168.0.0/16",
+    -- TCP_ALLOW = "127.0.0.1"
+    -- TCP_ALLOW = "192.168.0.0/16"
+    -- TCP_ALLOW = "10.0.0.0/255.0.0.0"
+    -- TCP_ALLOW = "all"
+
+    TCP_DENY = "",
+    -- TCP_DENY = "127.0.0.1",
+    -- TCP_DENY = "10.0.0.0/255.0.0.0",
+
+    -- ===========================
+    -- Connection Limit Settings
+    -- ===========================
+
+    -- If connect_count connection request are made within connect_interval
+    -- msec, it blocks it
+
+    -- Consecutive attempts interval (msec)
+    -- (default is 3000 msecs, 3 seconds)
+    TCP_CONNECT_INTERVAL = 3000,
+
+    -- Consecutive attempts trigger
+    -- (default is 10 attempts)
+    TCP_CONNECT_COUNT = 10,
+
+    -- The time interval after which the connection lockout is lifted. (msec)
+    -- After this amount of time, the connection restrictions are lifted.
+    -- (default is 600000 msecs, 10 minutes)
+    TCP_CONNECT_LOCKOUT = 600000
 }
+
+-- EOF
