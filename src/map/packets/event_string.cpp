4351874a91835a1e3e2ad6d1fe8aaa64a0c26549
WinterSolstice8 <60417494+WinterSolstice8@users.noreply.github.com>
2022-07-11 19:06:53 -0700

    [core] [packets] fixup CEventPacket and CEventStringPacket to match retail behavior

--- src/map/packets/event_string.cpp
+++ src/map/packets/event_string.cpp
@@ -31,8 +31,25 @@ CEventStringPacket::CEventStringPacket(CCharEntity* PChar, EventInfo* eventInfo)
     this->setType(0x33);
     this->setSize(0x70);
 
-    ref<uint32>(0x04) = PChar->id;
-    ref<uint16>(0x08) = PChar->m_TargID;
+    uint32       npcServerID = 0;
+    uint32       npcLocalID  = 0;
+    CBaseEntity* PNpc        = eventInfo->targetEntity;
+
+    if (PNpc)
+    {
+        npcServerID = PNpc->id;
+        npcLocalID  = PNpc->targid;
+    }
+    else
+    {
+        // Fallback to our own CharID because giving a value
+        // of zero makes the game hang.
+        npcServerID = PChar->id;
+        npcLocalID  = PChar->targid;
+    }
+
+    ref<uint32>(0x04) = npcServerID;
+    ref<uint16>(0x08) = npcLocalID;
     ref<uint16>(0x0A) = PChar->getZone();
     ref<uint16>(0x0C) = eventInfo->eventId;
     ref<uint8>(0x0E)  = 8; // camera "jumps" behind the character if < 8 params
