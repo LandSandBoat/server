6e302e440f1fb02423917b5409a6d97550322a35
dallano <atwood.dallan@hotmail.com>
2022-07-13 13:57:11 -0600

    BCNM Steam Sprouts Fix
    Basis for the changes were received from the following:
    https://ffxiclopedia.fandom.com/wiki/Steamed_Sprouts
    
    Fixes:
    Made the Domovoi charmable.
    Fixed spawn points as everything spawned slightly askew to the side.

---
+++ scripts/zones/Balgas_Dais/mobs/Dvorovoi.lua
@@ -0,0 +1,16 @@
+-----------------------------------
+-- Area: Balga's Dais
+--  Mob: Dvorovoi
+-- BCNM: Steamed Sprouts
+-----------------------------------
+require("scripts/globals/status")
+-----------------------------------
+local entity = {}
+
+entity.onMobSpawn = function(mob)
+end
+
+entity.onMobDeath = function(mob, player, isKiller)
+end
+
+return entity
