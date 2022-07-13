6e8550d407d00aa16d044ca6239987cfd342363a
dallano <atwood.dallan@hotmail.com>
2022-07-13 14:13:59 -0600

    BCNM Tails of Woe Fixes
    Basis for the following changes were received from the following:
    https://ffxi.allakhazam.com/db/bestiary.html?fmob=114
    https://ffxi.allakhazam.com/db/quests.html?fquest=337
    https://ffxiclopedia.fandom.com/wiki/Tails_of_Woe
    
    Couldn't find any support or evidence backing the claim of a resistance to silence.
    Removed silence resistance mod
    Gave sleep immunity to Helltail Harry

--- scripts/zones/Horlais_Peak/mobs/Helltail_Harry.lua
+++ scripts/zones/Horlais_Peak/mobs/Helltail_Harry.lua
@@ -9,8 +9,7 @@ local entity = {}
 
 entity.onMobSpawn = function(mob)
     mob:setMod(xi.mod.SLEEPRES, 1000)
-    mob:setMod(xi.mod.SILENCERES, 900)
-    mob:setMod(xi.mod.LULLABYRES, 700)
+    mob:setMod(xi.mod.LULLABYRES, 1000)
 end
 
 entity.onMobDeath = function(mob, player, isKiller)
