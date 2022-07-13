a87b4933e26a0bcaee7fcb1aedf0e4fdc2d721f9
dallano <atwood.dallan@hotmail.com>
2022-07-13 12:46:59 -0600

    BCNM Toadal Recall Fixes
    Basis and evidence of changes were gathered from the following:
    https://ffxiclopedia.fandom.com/wiki/Toadal_Recall
    
    Fixes:
    Adjusted spawn points of toads. Previously were spawning clustered around a tree which prevented them from properly agroing players.
    Fixed the toads from roaming
    Increased agro range to be similar to that of battlefield mobs
    Gave all toads sleep and lullaby immunity

--- scripts/zones/Ghelsba_Outpost/mobs/Toadsquab.lua
+++ scripts/zones/Ghelsba_Outpost/mobs/Toadsquab.lua
@@ -3,7 +3,20 @@
 --  Mob: Toadsquab
 -- BCNM: Toadal Recall
 -----------------------------------
+require("scripts/globals/status")
+-----------------------------------
 local entity = {}
+
+entity.onMobSpawn = function(mob)
+    mob:setMod(xi.mod.SLEEPRES, 1000)
+    mob:setMod(xi.mod.LULLABYRES, 1000)
+    mob:setMobMod(xi.mobMod.NO_MOVE, 1) -- lock from moving
+    mob:setMobMod(xi.mobMod.SOUND_RANGE, 15)
+end
+
+entity.onMobEngaged = function(mob, target)
+    mob:setMobMod(xi.mobMod.NO_MOVE, 0) -- unlock from moving
+end
 
 entity.onMobDeath = function(mob, player, isKiller)
 end
