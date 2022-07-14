62f459054ad6f0f9c769a61ac3a79e57899e2b72
Tal Ben-Eliezer <beneliezer@gmail.com>
2022-07-13 22:49:21 -0400

    Document Drop ID's.

---
+++ modules/catseyexi/sql/Drop_ID_Documentation.sql
@@ -0,0 +1,11 @@
+#########################################
+## CatsEyeXI Custom Drop ID Tracker    ##
+## Please document in descending order ##
+#########################################
+##
+##  4000 - ToAU Beastmen (Alexandrites and sacks)
+##  3999 - Nostokulshedra (Angel Skins)
+##  
+##  3901 - Domain Invasion (Bahamut)
+##  3900 - Domain Invasion (Amphisbaena, Tortuga, and Battosai)
+##
