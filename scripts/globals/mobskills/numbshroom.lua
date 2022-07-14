Wings-XI/LegacyWingsXI

Implement Toadal Recall and Some Funguar Fixes

scripts/globals/mobskills/numbshroom.lua
@@ -13,15 +13,14 @@ require("scripts/globals/monstertpmoves")
 ---------------------------------------------------
 
 function onMobSkillCheck(target, mob, skill)
-    if (mob:getMobMod(tpz.mobMod.VAR) == 1) then
+    if (mob:AnimationSub() == 1) then
         return 0
     end
     return 1
 end
 
 function onMobWeaponSkill(target, mob, skill)
-
-    mob:setMobMod(tpz.mobMod.VAR, 2)
+    mob:AnimationSub(2) -- Removes a cap from it's head
     local numhits = 1
     local accmod = 1
     local dmgmod = 2

scripts/globals/mobskills/queasyshroom.lua
@@ -16,14 +16,14 @@ require("scripts/globals/monstertpmoves")
 ---------------------------------------------------
 
 function onMobSkillCheck(target, mob, skill)
-    if (mob:getMobMod(tpz.mobMod.VAR) == 0) then
+    if (mob:AnimationSub() == 0) or (mob:AnimationSub() == 4)  then
         return 0
     end
     return 1
 end
 
 function onMobWeaponSkill(target, mob, skill)
-    mob:setMobMod(tpz.mobMod.VAR, 1)
+    mob:AnimationSub(1) -- Removes a cap from it's head
     local numhits = 1
     local accmod = 1
     local dmgmod = 2

scripts/globals/mobskills/shakeshroom.lua
@@ -13,15 +13,14 @@ require("scripts/globals/monstertpmoves")
 ---------------------------------------------------
 
 function onMobSkillCheck(target, mob, skill)
-    if (mob:getMobMod(tpz.mobMod.VAR) == 2) then
+    if (mob:AnimationSub() == 2) then
         return 0
     end
     return 1
 end
 
 function onMobWeaponSkill(target, mob, skill)
-
-    mob:setMobMod(tpz.mobMod.VAR, 3)
+    mob:AnimationSub(3) -- Removes a cap from it's head
     local numhits = 1
     local accmod = 1
     local dmgmod = 2

scripts/zones/Ghelsba_Outpost/mobs/Toadbolster.lua
@@ -4,5 +4,63 @@
 -- BCNM: Toadal Recall
 -----------------------------------
 
+function onMobInitialize(mob)
+    mob:setMobMod(tpz.mobMod.SUPERLINK, 1)
+    mob:setMod(tpz.mod.SLEEPRES, 100)
+    mob:setMod(tpz.mod.BINDRES, 50)
+    mob:setMod(tpz.mod.LULLABYRES, 100)
+    mob:setMobMod(tpz.mobMod.NO_MOVE, 1)
+    mob:setMobMod(tpz.mobMod.SOUND_RANGE, 15)
+end
+
+function onMobSpawn(mob)
+    mob:setLocalVar("tpskill", math.random(15, 30))
+    mob:setLocalVar("capreset", 120)
+end
+
+function onMobEngaged(mob)
+    mob:setMobMod(tpz.mobMod.NO_MOVE, 0)
+end
+
+function onMobFight(mob, target)
+    local tpskill = mob:getLocalVar("tpskill")
+    local capreset = mob:getLocalVar("capreset")
+    local wait = mob:getLocalVar("wait")
+    local caps = mob:getLocalVar("caps")
+    local reset = mob:getLocalVar("reset")
+
+    if wait >= tpskill and mob:checkDistance(mob:getTarget()) <= 8 then
+        if mob:AnimationSub() == 0 or mob:AnimationSub() == 4 and caps == 0 then
+            mob:useMobAbility(310)
+            mob:setLocalVar("tpskill", math.random(15, 30))
+            wait = 0
+            mob:setLocalVar("caps", 1) 
+        elseif mob:AnimationSub() == 1 then
+            mob:useMobAbility(311)
+            mob:setLocalVar("tpskill", math.random(15, 30))
+            wait = 0
+            mob:setLocalVar("caps", 2) 
+        elseif mob:AnimationSub() == 2 then
+            mob:useMobAbility(312)
+            mob:setLocalVar("tpskill", 100)
+            wait = 0
+            mob:setLocalVar("caps", 3) 
+        end
+    else
+        mob:setLocalVar("wait", wait+1)
+    end
+
+    if mob:getLocalVar("caps") == 3 and wait >= capreset and reset == 0 then
+        mob:setLocalVar("caps", 0) 
+        mob:timer(1000 , function(mob) mob:AnimationSub(4) end)
+        mob:useMobAbility(626)
+        mob:setLocalVar("reset", 1)
+        mob:setLocalVar("tpskill", math.random(15, 30))
+        mob:setLocalVar("wait", 0)
+    else
+        mob:setLocalVar("wait", wait+1)
+    end
+end
+
 function onMobDeath(mob, player, isKiller)
 end

scripts/zones/Ghelsba_Outpost/mobs/Toadpillow.lua
@@ -4,5 +4,62 @@
 -- BCNM: Toadal Recall
 -----------------------------------
 
+function onMobInitialize(mob)
+    mob:setMobMod(tpz.mobMod.SUPERLINK, 1)
+    mob:setMod(tpz.mod.SLEEPRES, 100)
+    mob:setMod(tpz.mod.BINDRES, 50)
+    mob:setMod(tpz.mod.LULLABYRES, 100)
+    mob:setMobMod(tpz.mobMod.NO_MOVE, 1)
+    mob:setMobMod(tpz.mobMod.SOUND_RANGE, 15)
+end
+
+function onMobSpawn(mob)
+    mob:setLocalVar("tpskill", math.random(15, 30))
+    mob:setLocalVar("capreset", 120)
+end
+
+function onMobEngaged(mob)
+    mob:setMobMod(tpz.mobMod.NO_MOVE, 0)
+end
+
+function onMobFight(mob, target)
+    local tpskill = mob:getLocalVar("tpskill")
+    local capreset = mob:getLocalVar("capreset")
+    local wait = mob:getLocalVar("wait")
+    local caps = mob:getLocalVar("caps")
+    local reset = mob:getLocalVar("reset")
+
+    if wait >= tpskill and mob:checkDistance(mob:getTarget()) <= 8 then
+        if mob:AnimationSub() == 0 or mob:AnimationSub() == 4 and caps == 0 then
+            mob:useMobAbility(310)
+            mob:setLocalVar("tpskill", math.random(15, 30))
+            wait = 0
+            mob:setLocalVar("caps", 1) 
+        elseif mob:AnimationSub() == 1 then
+            mob:useMobAbility(311)
+            mob:setLocalVar("tpskill", math.random(15, 30))
+            wait = 0
+            mob:setLocalVar("caps", 2) 
+        elseif mob:AnimationSub() == 2 then
+            mob:useMobAbility(312)
+            mob:setLocalVar("tpskill", 100)
+            wait = 0
+            mob:setLocalVar("caps", 3) 
+        end
+    else
+        mob:setLocalVar("wait", wait+1)
+    end
+
+    if mob:getLocalVar("caps") == 3 and wait >= capreset and reset == 0 then
+        mob:setLocalVar("caps", 0) 
+        mob:timer(1000 , function(mob) mob:AnimationSub(4) end)
+        mob:useMobAbility(626)
+        mob:setLocalVar("reset", 1)
+        mob:setLocalVar("tpskill", math.random(15, 30))
+        mob:setLocalVar("wait", 0)
+    else
+        mob:setLocalVar("wait", wait+1)
+    end
+end
 function onMobDeath(mob, player, isKiller)
 end

scripts/zones/Ghelsba_Outpost/mobs/Toadsquab.lua
@@ -4,5 +4,63 @@
 -- BCNM: Toadal Recall
 -----------------------------------
 
+function onMobInitialize(mob)
+    mob:setMobMod(tpz.mobMod.SUPERLINK, 1)
+    mob:setMod(tpz.mod.SLEEPRES, 100)
+    mob:setMod(tpz.mod.BINDRES, 50)
+    mob:setMod(tpz.mod.LULLABYRES, 100)
+    mob:setMobMod(tpz.mobMod.NO_MOVE, 1)
+    mob:setMobMod(tpz.mobMod.SOUND_RANGE, 15)
+end
+
+function onMobSpawn(mob)
+    mob:setLocalVar("tpskill", math.random(15, 30))
+    mob:setLocalVar("capreset", 120)
+end
+
+function onMobEngaged(mob)
+    mob:setMobMod(tpz.mobMod.NO_MOVE, 0)
+end
+
+function onMobFight(mob, target)
+    local tpskill = mob:getLocalVar("tpskill")
+    local capreset = mob:getLocalVar("capreset")
+    local wait = mob:getLocalVar("wait")
+    local caps = mob:getLocalVar("caps")
+    local reset = mob:getLocalVar("reset")
+
+    if wait >= tpskill and mob:checkDistance(mob:getTarget()) <= 8 then
+        if mob:AnimationSub() == 0 or mob:AnimationSub() == 4 and caps == 0 then
+            mob:useMobAbility(310)
+            mob:setLocalVar("tpskill", math.random(15, 30))
+            wait = 0
+            mob:setLocalVar("caps", 1) 
+        elseif mob:AnimationSub() == 1 then
+            mob:useMobAbility(311)
+            mob:setLocalVar("tpskill", math.random(15, 30))
+            wait = 0
+            mob:setLocalVar("caps", 2) 
+        elseif mob:AnimationSub() == 2 then
+            mob:useMobAbility(312)
+            mob:setLocalVar("tpskill", 100)
+            wait = 0
+            mob:setLocalVar("caps", 3) 
+        end
+    else
+        mob:setLocalVar("wait", wait+1)
+    end
+
+    if mob:getLocalVar("caps") == 3 and wait >= capreset and reset == 0 then
+        mob:setLocalVar("caps", 0) 
+        mob:timer(1000 , function(mob) mob:AnimationSub(4) end)
+        mob:useMobAbility(626)
+        mob:setLocalVar("reset", 1)
+        mob:setLocalVar("tpskill", math.random(15, 30))
+        mob:setLocalVar("wait", 0)
+    else
+        mob:setLocalVar("wait", wait+1)
+    end
+end
+
 function onMobDeath(mob, player, isKiller)
 end

LandSandBoat/server

scripts/globals/mobskills/numbshroom.lua
@@ -10,17 +10,17 @@ require("scripts/globals/status")
 require("scripts/globals/mobskills")
 -----------------------------------
 local mobskill_object = {}
+---------------------------------------------------
 
 mobskill_object.onMobSkillCheck = function(target, mob, skill)
-    if (mob:getMobMod(xi.mobMod.VAR) == 1) then
+    if (mob:getAnimationSub() == 1) then
         return 0
     end
     return 1
 end
 
 mobskill_object.onMobWeaponSkill = function(target, mob, skill)
-
-    mob:setMobMod(xi.mobMod.VAR, 2)
+    mob:setAnimationSub(2) -- Removes a cap from it's head
     local numhits = 1
     local accmod = 1
     local dmgmod = 2

scripts/globals/mobskills/queasyshroom.lua
@@ -14,15 +14,17 @@ require("scripts/globals/mobskills")
 -----------------------------------
 local mobskill_object = {}
 
+---------------------------------------------------
+
 mobskill_object.onMobSkillCheck = function(target, mob, skill)
-    if (mob:getMobMod(xi.mobMod.VAR) == 0) then
+    if (mob:getAnimationSub() == 0) or (mob:getAnimationSub() == 4)  then
         return 0
     end
     return 1
 end
 
 mobskill_object.onMobWeaponSkill = function(target, mob, skill)
-    mob:setMobMod(xi.mobMod.VAR, 1)
+    mob:setAnimationSub(1) -- Removes a cap from it's head
     local numhits = 1
     local accmod = 1
     local dmgmod = 2

scripts/globals/mobskills/shakeshroom.lua
@@ -10,17 +10,16 @@ require("scripts/globals/status")
 require("scripts/globals/mobskills")
 -----------------------------------
 local mobskill_object = {}
-
+---------------------------------------------------
 mobskill_object.onMobSkillCheck = function(target, mob, skill)
-    if (mob:getMobMod(xi.mobMod.VAR) == 2) then
+    if (mob:getAnimationSub() == 2) then
         return 0
     end
     return 1
 end
 
 mobskill_object.onMobWeaponSkill = function(target, mob, skill)
-
-    mob:setMobMod(xi.mobMod.VAR, 3)
+    mob:setAnimationSub(3) -- Removes a cap from it's head
     local numhits = 1
     local accmod = 1
     local dmgmod = 2

scripts/zones/Ghelsba_Outpost/mobs/Toadbolster.lua
@@ -5,15 +5,62 @@
 -----------------------------------
 local entity = {}
 
-entity.onMobSpawn = function(mob)
-    mob:setMod(xi.mod.SLEEPRES, 1000)
-    mob:setMod(xi.mod.LULLABYRES, 1000)
-    mob:setMobMod(xi.mobMod.NO_MOVE, 1) -- lock from moving
+entity.onMobInitialize = function(mob)
+    mob:setMobMod(xi.mobMod.SUPERLINK, 1)
+    mob:setMod(xi.mod.SLEEPRES, 100)
+    mob:setMod(xi.mod.BINDRES, 50)
+    mob:setMod(xi.mod.LULLABYRES, 100)
     mob:setMobMod(xi.mobMod.SOUND_RANGE, 15)
 end
 
-entity.onMobEngaged = function(mob, target)
-    mob:setMobMod(xi.mobMod.NO_MOVE, 0) -- unlock from moving
+entity.onMobSpawn = function(mob)
+    mob:setMobMod(xi.mobMod.NO_MOVE, 1)
+    mob:setLocalVar("tpskill", math.random(15, 30))
+    mob:setLocalVar("capreset", 120)
+end
+
+entity.onMobEngaged = function(mob)
+    mob:setMobMod(xi.mobMod.NO_MOVE, 0)
+end
+
+entity.onMobFight = function(mob, target)
+    local tpskill = mob:getLocalVar("tpskill")
+    local capreset = mob:getLocalVar("capreset")
+    local wait = mob:getLocalVar("wait")
+    local caps = mob:getLocalVar("caps")
+    local reset = mob:getLocalVar("reset")
+
+    if wait >= tpskill and mob:checkDistance(mob:getTarget()) <= 8 then
+        if mob:getAnimationSub() == 0 or mob:getAnimationSub() == 4 and caps == 0 then
+            mob:useMobAbility(310)
+            mob:setLocalVar("tpskill", math.random(15, 30))
+            wait = 0
+            mob:setLocalVar("caps", 1)
+        elseif mob:getAnimationSub() == 1 then
+            mob:useMobAbility(311)
+            mob:setLocalVar("tpskill", math.random(15, 30))
+            wait = 0
+            mob:setLocalVar("caps", 2)
+        elseif mob:getAnimationSub() == 2 then
+            mob:useMobAbility(312)
+            mob:setLocalVar("tpskill", 100)
+            wait = 0
+            mob:setLocalVar("caps", 3)
+        end
+    else
+        mob:setLocalVar("wait", wait+1)
+    end
+
+    if mob:getLocalVar("caps") == 3 and wait >= capreset and reset == 0 then
+        mob:setLocalVar("caps", 0)
+        mob:timer(1000 , function(mobArg) mobArg:setAnimationSub(4) end)
+        mob:useMobAbility(626)
+        mob:setLocalVar("reset", 1)
+        mob:setLocalVar("tpskill", math.random(15, 30))
+        mob:setLocalVar("wait", 0)
+    else
+        mob:setLocalVar("wait", wait+1)
+    end
 end
 
 entity.onMobDeath = function(mob, player, isKiller)

scripts/zones/Ghelsba_Outpost/mobs/Toadpillow.lua
@@ -5,17 +5,63 @@
 -----------------------------------
 local entity = {}
 
-entity.onMobSpawn = function(mob)
-    mob:setMod(xi.mod.SLEEPRES, 1000)
-    mob:setMod(xi.mod.LULLABYRES, 1000)
-    mob:setMobMod(xi.mobMod.NO_MOVE, 1) -- lock from moving
+entity.onMobInitialize = function(mob)
+    mob:setMobMod(xi.mobMod.SUPERLINK, 1)
+    mob:setMod(xi.mod.SLEEPRES, 100)
+    mob:setMod(xi.mod.BINDRES, 50)
+    mob:setMod(xi.mod.LULLABYRES, 100)
     mob:setMobMod(xi.mobMod.SOUND_RANGE, 15)
 end
 
-entity.onMobEngaged = function(mob, target)
-    mob:setMobMod(xi.mobMod.NO_MOVE, 0) -- unlock from moving
+entity.onMobSpawn = function(mob)
+    mob:setMobMod(xi.mobMod.NO_MOVE, 1)
+    mob:setLocalVar("tpskill", math.random(15, 30))
+    mob:setLocalVar("capreset", 120)
 end
 
+entity.onMobEngaged = function(mob)
+    mob:setMobMod(xi.mobMod.NO_MOVE, 0)
+end
+
+entity.onMobFight = function(mob, target)
+    local tpskill = mob:getLocalVar("tpskill")
+    local capreset = mob:getLocalVar("capreset")
+    local wait = mob:getLocalVar("wait")
+    local caps = mob:getLocalVar("caps")
+    local reset = mob:getLocalVar("reset")
+
+    if wait >= tpskill and mob:checkDistance(mob:getTarget()) <= 8 then
+        if mob:getAnimationSub() == 0 or mob:getAnimationSub() == 4 and caps == 0 then
+            mob:useMobAbility(310)
+            mob:setLocalVar("tpskill", math.random(15, 30))
+            wait = 0
+            mob:setLocalVar("caps", 1)
+        elseif mob:getAnimationSub() == 1 then
+            mob:useMobAbility(311)
+            mob:setLocalVar("tpskill", math.random(15, 30))
+            wait = 0
+            mob:setLocalVar("caps", 2)
+        elseif mob:getAnimationSub() == 2 then
+            mob:useMobAbility(312)
+            mob:setLocalVar("tpskill", 100)
+            wait = 0
+            mob:setLocalVar("caps", 3)
+        end
+    else
+        mob:setLocalVar("wait", wait+1)
+    end
+
+    if mob:getLocalVar("caps") == 3 and wait >= capreset and reset == 0 then
+        mob:setLocalVar("caps", 0)
+        mob:timer(1000 , function(mobArg) mobArg:setAnimationSub(4) end)
+        mob:useMobAbility(626)
+        mob:setLocalVar("reset", 1)
+        mob:setLocalVar("tpskill", math.random(15, 30))
+        mob:setLocalVar("wait", 0)
+    else
+        mob:setLocalVar("wait", wait+1)
+    end
+end
 entity.onMobDeath = function(mob, player, isKiller)
 end
 

scripts/zones/Ghelsba_Outpost/mobs/Toadsquab.lua
@@ -7,15 +7,62 @@ require("scripts/globals/status")
 -----------------------------------
 local entity = {}
 
-entity.onMobSpawn = function(mob)
-    mob:setMod(xi.mod.SLEEPRES, 1000)
-    mob:setMod(xi.mod.LULLABYRES, 1000)
-    mob:setMobMod(xi.mobMod.NO_MOVE, 1) -- lock from moving
+entity.onMobInitialize = function(mob)
+    mob:setMobMod(xi.mobMod.SUPERLINK, 1)
+    mob:setMod(xi.mod.SLEEPRES, 100)
+    mob:setMod(xi.mod.BINDRES, 50)
+    mob:setMod(xi.mod.LULLABYRES, 100)
     mob:setMobMod(xi.mobMod.SOUND_RANGE, 15)
 end
 
-entity.onMobEngaged = function(mob, target)
-    mob:setMobMod(xi.mobMod.NO_MOVE, 0) -- unlock from moving
+entity.onMobSpawn = function(mob)
+    mob:setMobMod(xi.mobMod.NO_MOVE, 1)
+    mob:setLocalVar("tpskill", math.random(15, 30))
+    mob:setLocalVar("capreset", 120)
+end
+
+entity.onMobEngaged = function(mob)
+    mob:setMobMod(xi.mobMod.NO_MOVE, 0)
+end
+
+entity.onMobFight = function(mob, target)
+    local tpskill = mob:getLocalVar("tpskill")
+    local capreset = mob:getLocalVar("capreset")
+    local wait = mob:getLocalVar("wait")
+    local caps = mob:getLocalVar("caps")
+    local reset = mob:getLocalVar("reset")
+
+    if wait >= tpskill and mob:checkDistance(mob:getTarget()) <= 8 then
+        if mob:getAnimationSub() == 0 or mob:getAnimationSub() == 4 and caps == 0 then
+            mob:useMobAbility(310)
+            mob:setLocalVar("tpskill", math.random(15, 30))
+            wait = 0
+            mob:setLocalVar("caps", 1)
+        elseif mob:getAnimationSub() == 1 then
+            mob:useMobAbility(311)
+            mob:setLocalVar("tpskill", math.random(15, 30))
+            wait = 0
+            mob:setLocalVar("caps", 2)
+        elseif mob:getAnimationSub() == 2 then
+            mob:useMobAbility(312)
+            mob:setLocalVar("tpskill", 100)
+            wait = 0
+            mob:setLocalVar("caps", 3)
+        end
+    else
+        mob:setLocalVar("wait", wait+1)
+    end
+
+    if mob:getLocalVar("caps") == 3 and wait >= capreset and reset == 0 then
+        mob:setLocalVar("caps", 0)
+        mob:timer(1000 , function(mobArg) mobArg:setAnimationSub(4) end)
+        mob:useMobAbility(626)
+        mob:setLocalVar("reset", 1)
+        mob:setLocalVar("tpskill", math.random(15, 30))
+        mob:setLocalVar("wait", 0)
+    else
+        mob:setLocalVar("wait", wait+1)
+    end
 end
 
 entity.onMobDeath = function(mob, player, isKiller)