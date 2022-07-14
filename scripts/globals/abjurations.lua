f77d7818e99fce170318e29a78d9674ca00f4c1b
NeuromancerXI <neuromancerxi@users.noreply.github.com>
2022-07-14 05:12:53 -0700

    Add Dryadic Abjurations

--- scripts/globals/abjurations.lua
+++ scripts/globals/abjurations.lua
@@ -27,4 +27,34 @@ xi.abjurations =
     xi.items.BOWL_OF_CURSED_SOUP,
     xi.items.BOWL_OF_AMBROSIA,
     xi.items.BOWL_OF_AMBROSIA,
+
+    xi.items.DRYADIC_ABJURATION_HEAD,
+    xi.items.CURSED_KABUTO,
+    xi.items.CURSED_KABUTO_M1,
+    xi.items.SHURA_ZUNARI_KABUTO,
+    xi.items.SHURA_ZUNARI_KABUTO_P1,
+
+    xi.items.DRYADIC_ABJURATION_BODY,
+    xi.items.CURSED_TOGI,
+    xi.items.CURSED_TOGI_M1,
+    xi.items.SHURA_TOGI,
+    xi.items.SHURA_TOGI_P1,
+
+    xi.items.DRYADIC_ABJURATION_HANDS,
+    xi.items.CURSED_KOTE,
+    xi.items.CURSED_KOTE_M1,
+    xi.items.SHURA_KOTE,
+    xi.items.SHURA_KOTE_P1,
+
+    xi.items.DRYADIC_ABJURATION_LEGS,
+    xi.items.CURSED_HAIDATE,
+    xi.items.CURSED_HAIDATE_M1,
+    xi.items.SHURA_HAIDATE,
+    xi.items.SHURA_HAIDATE_P1,
+
+    xi.items.DRYADIC_ABJURATION_FEET,
+    xi.items.CURSED_SUNE_ATE,
+    xi.items.CURSED_SUNE_ATE_M1,
+    xi.items.SHURA_SUNE_ATE,
+    xi.items.SHURA_SUNE_ATE_P1,
 }
