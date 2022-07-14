098394b9dedc9d8343585170f4e632654523b196
NeuromancerXI <neuromancerxi@users.noreply.github.com>
2022-07-14 05:11:13 -0700

    Migrate Abjuration Magic Numbers to items.lua

---
+++ scripts/globals/abjurations.lua
@@ -0,0 +1,30 @@
+-----------------------------------
+-- Abjurations
+-- Used by Alphollon C Meriard
+-----------------------------------
+require('scripts/globals/items')
+-----------------------------------
+
+xi = xi or {}
+
+xi.abjurations =
+{
+    -- (5 IDs/lines per transaction)
+    --  abjuration,
+    --  item,
+    --  item -1,
+    --  nq reward,
+    --  hq reward
+
+    xi.items.LIBATION_ABJURATION,
+    xi.items.BOTTLE_OF_CURSED_BEVERAGE,
+    xi.items.BOTTLE_OF_CURSED_BEVERAGE,
+    xi.items.BOTTLE_OF_AMRITA,
+    xi.items.BOTTLE_OF_AMRITA,
+
+    xi.items.OBLATION_ABJURATION,
+    xi.items.BOWL_OF_CURSED_SOUP,
+    xi.items.BOWL_OF_CURSED_SOUP,
+    xi.items.BOWL_OF_AMBROSIA,
+    xi.items.BOWL_OF_AMBROSIA,
+}
