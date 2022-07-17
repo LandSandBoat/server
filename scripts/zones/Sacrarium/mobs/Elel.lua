6f1ddc17b3be064e11d939be0355cf19594a6af2
Critical <lyardeen@gmail.com>
2022-07-16 19:50:44 -0600

    Sacrarium NM Audit

---
+++ scripts/zones/Sacrarium/mobs/Elel.lua
@@ -0,0 +1,34 @@
+-----------------------------------
+-- Area: Sacrarium
+--   NM: Balor
+-----------------------------------
+mixins = {require("scripts/mixins/job_special")}
+-----------------------------------
+local entity = {}
+
+-- Get multiple spawn points
+
+entity.onMobDisengage = function(mob)
+    if not (mob:getWeather() == xi.weather.GLOOM or mob:getWeather() == xi.weather.DARKNESS) or (VanadielHour() >= 4 and VanadielHour() < 20) then
+        DespawnMob(mob:getID())
+    end
+end
+
+entity.onMobRoam = function(mob)
+    if not (mob:getWeather() == xi.weather.GLOOM or mob:getWeather() == xi.weather.DARKNESS) or (VanadielHour() >= 4 and VanadielHour() < 20) then
+        DespawnMob(mob:getID())
+    end
+end
+
+entity.onMobDeath = function(mob, player, isKiller)
+end
+
+entity.onMobDespawn = function(mob)
+    UpdateNMSpawnPoint(mob:getID())
+    mob:setRespawnTime(7200)
+    mob:setLocalVar("cooldown", os.time() + 7200)
+    DisallowRespawn(mob:getID(), true) -- prevents accidental 'pop' during no dark weather and immediate despawn
+end
+
+
+return entity
--- scripts/zones/Sacrarium/mobs/Lobais.lua
+++ scripts/zones/Sacrarium/mobs/Lobais.lua
@@ -6,6 +6,8 @@ mixins = {require("scripts/mixins/job_special")}
 -----------------------------------
 local entity = {}
 
+-- TODO: Check if it has an ele pet
+
 entity.onMobDeath = function(mob, player, isKiller)
 end
 
