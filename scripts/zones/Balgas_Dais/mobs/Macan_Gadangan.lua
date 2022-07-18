146cc158fad8336b84746e59343e040b0629f6f5
dallano <atwood.dallan@hotmail.com>
2022-07-18 13:08:58 -0600

    BCNM Wild Wild Whiskers
    BCNM Wild Wild Whiskers Fix
    Basis and evidence received from the following:
    https://ffxiclopedia.fandom.com/wiki/Wild_Wild_Whiskers
    https://www.youtube.com/watch?v=kFYAEeiY078&t=96s&ab_channel=Pepriona
    https://www.youtube.com/watch?v=cBZrxrMznIg&t=37s&ab_channel=upbeatglitch
    https://www.youtube.com/watch?v=BH4IGqAPNmQ&t=127s&ab_channel=James
    Live capture evidence from Aerec  (unrecorded)
    
    Fix:
    Fixed Macan's messaging system when casting spells and added a magic rank system that increased magic attk
    Magic ranks increased after every 3 casts and ends at rank 5
    Fixed spawning of Macan and armoury crate to be centred in the battlefield
    Macan immediately reaches level 5 after being silenced
    Added an immediate use of Petrificative breath after casting a spell at the highest magic rank
    Increased MP of Macan to prevent running out of MP partway into the fight
    
    TODO: Fix the status check MAGIC_INTERRUPT in order for Macan's frenzied rage counter to work.

---
+++ scripts/zones/Balgas_Dais/mobs/Macan_Gadangan.lua
@@ -0,0 +1,76 @@
+-----------------------------------
+-- Area: Balga's Dais
+--  Mob: Macan Gadangan
+-- BCNM: Wild Wild Whiskers
+-----------------------------------
+local ID = require("scripts/zones/Balgas_Dais/IDs")
+-----------------------------------
+local entity = {}
+-- ELEM
+entity.onMobSpawn = function(mob)
+    mob:setLocalVar("magicRank", 1)
+    mob:setLocalVar("casts", 0)
+    mob:setLocalVar("control", 0)
+end
+
+local function magicRankUp(mob)
+    local amount = mob:getLocalVar("casts")
+
+    -- Increase magic rank after every 3 casts
+    if amount % 3 == 0 and mob:getLocalVar("magicRank") ~= 5 then
+        mob:setLocalVar("magicRank", mob:getLocalVar("magicRank") + 1)
+        mob:addMod(xi.mod.MATT, 100)
+    end
+end
+
+entity.onMobMagicPrepare = function(mob, target, spell)
+    mob:setLocalVar("casts", mob:getLocalVar("casts") + 1)
+
+    magicRankUp(mob)
+    local rank = mob:getLocalVar("magicRank")
+
+    if rank == 1 then
+        mob:messageText(target, ID.text.MACAN_WHISKERS_SLIGHTLY, false)
+    elseif rank == 2 then
+        mob:messageText(target, ID.text.MACAN_WHISKERS_RAPIDLY, false)
+    elseif rank == 3 then
+        mob:messageText(target, ID.text.MACAN_WHISKERS_WILDLY, false)
+    elseif rank == 4 then
+        mob:messageText(target, ID.text.MACAN_WHISKERS_VIOLENTLY, false)
+    elseif rank == 5 then
+        mob:messageText(target, ID.text.MACAN_WHISKERS_UNCONTROLLABLY, false)
+        return 212 -- Burst
+    end
+end
+
+entity.onMobFight = function(mob)
+    local rank = mob:getLocalVar("magicRank")
+
+    if mob:getCurrentAction() == xi.act.MAGIC_INTERRUPT then
+        mob:queue(0, function(mobArg)
+            mobArg:useMobAbility(1336)
+        end)
+    end
+
+    if mob:hasStatusEffect(xi.effect.SILENCE) then
+        mob:setLocalVar("magicRank", 5)
+        mob:setMod(xi.mod.MATT, 400)
+    end
+
+    if
+        mob:getCurrentAction() == xi.act.MAGIC_CASTING and
+        mob:getLocalVar("control") == 0 and
+        rank == 5
+    then
+        mob:setLocalVar("control", 1)
+        mob:queue(0, function(mobArg)
+            mobArg:useMobAbility(480)
+            mob:setLocalVar("control", 0)
+        end)
+    end
+end
+
+entity.onMobDeath = function(mob)
+end
+
+return entity
