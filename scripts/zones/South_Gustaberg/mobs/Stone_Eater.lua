05580f72a8e5d0b364a78d0e95bceea7ef09b340
OpheliaXI <heliashelium@gmail.com>
2022-07-17 19:52:07 -0500

    Remove Testing Stuff

--- scripts/zones/South_Gustaberg/mobs/Stone_Eater.lua
+++ scripts/zones/South_Gustaberg/mobs/Stone_Eater.lua
@@ -6,10 +6,6 @@ require("scripts/globals/regimes")
 -----------------------------------
 local entity = {}
 
-entity.onMobEngaged = function(mob, target)
-    mob:setLocalVar("[CastTime]ID_159", 10)
-end
-
 entity.onMobDeath = function(mob, player, isKiller)
     xi.regime.checkRegime(player, mob, 77, 1, xi.regime.type.FIELDS)
 end
