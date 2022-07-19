72050ae6b2a8fa741c8b59d1921f687bf5ecd676
Soapytoast <neosaidin@hotmail.com>
2022-07-19 19:19:54 -0400

    Update gardening_results_era.sql
    deleting the result all together likely causes issues, setting weight to 0 instead.

--- modules/era/sql/gardening_results_era.sql
+++ modules/era/sql/gardening_results_era.sql
@@ -1,27 +1,22 @@
----------------------------------------------------------------
--- Gardening Module to remove Results based on Era by Expansion
----------------------------------------------------------------
-
+UPDATE gardening_results
+SET weight = 0 WHERE resultid IN 
+(
 -- Chains of Promathia
--- DELETE FROM gardening_results WHERE resultId = 34;  -- Eggplant
--- DELETE FROM gardening_results WHERE resultId = 647; -- Sprig Of Misareaux Parsley
--- DELETE FROM gardening_results WHERE resultId = 664; -- Holy Basil
--- DELETE FROM gardening_results WHERE resultId = 667; -- Vanilla
-
--- Treasures of Aht Urghan
--- DELETE FROM gardening_results WHERE resultId = 2423; -- Gregarious Worm
--- DELETE FROM gardening_results WHERE resultId = 2426; -- Clump of Garidav Wildgrass
--- DELETE FROM gardening_results WHERE resultId = 2428; -- Zegham Carrot
--- DELETE FROM gardening_results WHERE resultId = 2429; -- Azouph Greens
--- DELETE FROM gardening_results WHERE resultId = 2434; -- Vomp Carrot
--- DELETE FROM gardening_results WHERE resultId = 2437; -- Sharug Greens
--- DELETE FROM gardening_results WHERE resultId = 2440; -- Clump Of Tokopekko Wildgrass
--- DELETE FROM gardening_results WHERE resultId = 2442; -- Cupid Worm
--- DELETE FROM gardening_results WHERE resultId = 2444; -- Cupid Worm
-
+--34,  -- Eggplant
+--647, -- Sprig Of Misareaux Parsley
+--664, -- Holy Basil
+--667, -- Vanilla 
+--2423, -- Gregarious Worm
+--2426, -- Clump of Garidav Wildgrass
+--2428, -- Zegham Carrot
+--2429, -- Azouph Greens
+--2434, -- Vomp Carrot
+--2437, -- Sharug Greens
+--2440, -- Clump Of Tokopekko Wildgrass
+--2442, -- Cupid Worm
+--2444, -- Cupid Worm
 -- Wings of the Goddess
--- DELETE FROM gardening_results WHERE resultId = 702; -- Burdock
-
+--702, -- Burdock
 -- Out of Era (2012+)
-DELETE FROM gardening_results WHERE resultId = 716; -- Winterflower
-
+716 -- Winterflower
+)
