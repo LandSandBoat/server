5035a33f74b163bca860cabe26ff57dd0c8fd7e7
Xaver-DaRed <xaver.entropia@gmail.com>
2022-07-19 20:30:01 +0200

    Cleanup Abyssea-LaTheine and Abyssea-Konschtat QM's

--- scripts/zones/Abyssea-Konschtat/npcs/qm_kukulkan.lua
+++ scripts/zones/Abyssea-Konschtat/npcs/qm_kukulkan.lua
@@ -6,17 +6,16 @@
 -- !pos -40.000 68.692 575.000 15
 -- !pos -25.000 68.792 560.000 15
 -----------------------------------
+local ID = require('scripts/zones/Abyssea-Konschtat/IDs')
 require('scripts/globals/abyssea')
 require('scripts/globals/keyitems')
 -----------------------------------
 local entity = {}
 
 entity.onTrade = function(player, npc, trade)
-    -- xi.abyssea.qmOnTrade(player, npc, trade)
 end
 
 entity.onTrigger = function(player, npc)
-    local ID = zones[player:getZoneID()]
     xi.abyssea.qmOnTrigger(player, npc, ID.mob.KUKULKAN_1, { xi.ki.TATTERED_HIPPOGRYPH_WING, xi.ki.CRACKED_WIVRE_HORN, xi.ki.MUCID_AHRIMAN_EYEBALL })
 end
 
