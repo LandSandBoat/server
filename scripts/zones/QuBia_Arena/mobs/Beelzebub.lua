ac1b68b27532a7b41bc9e5f4b607ae64465e8f0a
dallano <atwood.dallan@hotmail.com>
2022-07-20 01:24:07 -0600

    Sleep res swap
    Sleep res was incorrectly added to the hell fly instead of Beelzebub

--- scripts/zones/QuBia_Arena/mobs/Beelzebub.lua
+++ scripts/zones/QuBia_Arena/mobs/Beelzebub.lua
@@ -6,6 +6,10 @@
 mixins = { require("scripts/mixins/job_special") }
 local entity = {}
 
+entity.onMobSpawn = function(mob)
+    mob:setMod(xi.mod.SLEEPRES, 250)
+end
+
 entity.onMobDeath = function(mob, player, isKiller)
 end
 
--- scripts/zones/QuBia_Arena/mobs/Hell_Fly.lua
+++ scripts/zones/QuBia_Arena/mobs/Hell_Fly.lua
@@ -7,7 +7,6 @@ local entity = {}
 
 entity.onMobSpawn = function(mob)
     mob:setMobMod(xi.mobMod.CHARMABLE, 1)
-    mob:setMod(xi.mod.SLEEPRES, 250)
 end
 
 entity.onMobDeath = function(mob, player, isKiller)
