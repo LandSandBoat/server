f59d809dab16bda7c0e60e14024fb8936881b220
Grave Taker <bsm267@yahoo.com>
2022-07-23 13:15:24 -0400

    so many typos

--- scripts/zones/Caedarva_Mire/mobs/Caedarva_Toad.lua
+++ scripts/zones/Caedarva_Mire/mobs/Caedarva_Toad.lua
@@ -1,17 +1,17 @@
 -----------------------------------
 -- Area: Caedarva Mire
 --  Mob: Caedarva Toad
--- Involved in Quest: The Wayward Automation
+-- Involved in Quest: The Wayward Automaton
 -----------------------------------
 require("scripts/globals/quests")
 -----------------------------------
 local entity = {}
 
 entity.onMobDeath = function(mob, player, isKiller)
-    local theWaywardAutomation = player:getQuestStatus(xi.quest.log_id.AHT_URHGAN, xi.quest.id.ahtUrhgan.THE_WAYWARD_AUTOMATON)
+    local theWaywardAutomaton = player:getQuestStatus(xi.quest.log_id.AHT_URHGAN, xi.quest.id.ahtUrhgan.THE_WAYWARD_AUTOMATON)
     local theWaywardAutomatonProgress = player:getCharVar("TheWaywardAutomatonProgress")
 
-    if (theWaywardAutomation == QUEST_ACCEPTED and theWaywardAutomatonProgress == 2 and player:getCharVar("TheWaywardAutomatonNM") == 0) then
+    if (theWaywardAutomaton == QUEST_ACCEPTED and theWaywardAutomatonProgress == 2 and player:getCharVar("TheWaywardAutomatonNM") == 0) then
         player:setCharVar("TheWaywardAutomatonNM", 1)
     end
 end
