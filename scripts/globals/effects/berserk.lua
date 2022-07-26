38ff657fa8516addfe2612ec544b7e6672ab4aa7
Schaedelschaden <sitfixalot@gmail.com>
2022-07-25 18:19:34 -0600

    [JAs] Rename BERSERK_EFFECT mod, sort mods, and add notes

--- scripts/globals/effects/berserk.lua
+++ scripts/globals/effects/berserk.lua
@@ -6,6 +6,7 @@ require("scripts/globals/status")
 -----------------------------------
 local effect_object = {}
 
+-- DEFP penalty reduction from Warrior's Calligae NQ/+1/+2 handled by latent effect
 effect_object.onEffectGain = function(target, effect)
     local power = effect:getPower()
     local jpLevel = target:getJobPointLevel(xi.jp.BERSERK_EFFECT)
