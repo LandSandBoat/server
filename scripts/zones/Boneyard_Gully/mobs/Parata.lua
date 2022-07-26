dfddaf520a9bf98e6989abce5861511061209dcf
Critical <lyardeen@gmail.com>
2022-07-26 12:31:37 -0600

    Shell We Dance adjustments

--- scripts/zones/Boneyard_Gully/mobs/Parata.lua
+++ scripts/zones/Boneyard_Gully/mobs/Parata.lua
@@ -4,12 +4,15 @@
 --  ENM: Shell We Dance?
 -----------------------------------
 local ID = require("scripts/zones/Boneyard_Gully/IDs")
+mixins = {require("scripts/mixins/families/uragnite")}
 require("scripts/globals/titles")
 require("scripts/globals/status")
 -----------------------------------
 local entity = {}
 
 entity.onMobSpawn = function(mob)
+    mob:setMod(xi.mod.REGAIN, 100)
+    mob:setMod(xi.mod.MDEF, 50)
 end
 
 entity.onMobFight = function(mob,target)
@@ -21,7 +24,7 @@ entity.onMobFight = function(mob,target)
     -- Pet #1 spawn at 95% hp or less
     if hpp <= 95 and adds == 0 then
         mob:setLocalVar('adds', adds + 1)
-        petID = ID.shellWeDance[bfID].BLADMALL_PET_IDS[1]
+        petID = ID.shellWeDance[bfID].PARATA_PET_IDS[1]
     end
 
     -- Pet #2 spawn at 60% hp or less
@@ -43,6 +46,20 @@ entity.onMobFight = function(mob,target)
 
         local pos = mob:getPos()
         pet:setPos(pos.x, pos.y, pos.z, pos.rot)
+    end
+
+    if mob:getHPP() < 10 and mob:getLocalVar("lastBreath") == 0 then
+        mob:useMobAbility(603)
+        mob:setLocalVar("lastBreath", 1)
+    end
+end
+
+entity.onMobWeaponSkill = function(target, mob, skill)
+    -- Under 10%, Parata will 2hr dust cloud and chain Suctorial Tentacle > Painful Whip
+    if skill:getID() == 603 and mob:getLocalVar("lastBreath") == 0 then
+        mob:queue(10, function(mobArg) mobArg:useMobAbility(508) end)
+    elseif skill:getID() == 508 and mob:getLocalVar("lastBreath") == 0 then
+        mob:queue(10, function(mobArg) mobArg:useMobAbility(507) end)
     end
 end
 
