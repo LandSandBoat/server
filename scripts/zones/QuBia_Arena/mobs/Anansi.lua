1941bd8090b74404fb9d21c31b4775400750988f
dallano <atwood.dallan@hotmail.com>
2022-07-26 21:44:32 -0600

    Come into my Parlor Implementation
    Basis and evidence for changes:
    https://ffxiclopedia.fandom.com/wiki/Come_Into_My_Parlor
    https://www.youtube.com/watch?v=zHy3IJ_mi9k&ab_channel=TatianaFFXI
    https://www.youtube.com/watch?v=XVaG_8QI9Ns&ab_channel=TheJmeat
    
    Changes:
    - Sons of Anansi will now spawn upon death of Anansi in 2 to 6 second intervals
    - Implemented loot tables based off wiki and similar KSNMs with higher sample rates

--- scripts/zones/QuBia_Arena/mobs/Anansi.lua
+++ scripts/zones/QuBia_Arena/mobs/Anansi.lua
@@ -7,33 +7,42 @@ require("scripts/globals/status")
 -----------------------------------
 local entity = {}
 
+local function spawnSpider(mob, attacker, uint)
+    local spider = mob:getID() + uint
+
+    if uint > 8 then
+        return
+    end
+
+    GetMobByID(spider):setSpawn(mob:getXPos(), mob:getYPos(), mob:getZPos(), mob:getRotPos())
+    SpawnMob(spider):updateEnmity(attacker)
+
+    mob:timer(1000 * math.random(2, 6), function(mobArg)
+        spawnSpider(mobArg, attacker, uint + 1)
+    end)
+end
+
 entity.onMobSpawn = function(mob)
     mob:addMod(xi.mod.SLEEPRES, 100)
 
-    mob:addListener("DEATH", "ANANSI_DEATH", function(mobArg, killer)
-        if killer then
-            mobArg:setHP(1)
-            local target = killer
+    mob:addListener("TAKE_DAMAGE", "ANANSI_TAKE_DAMAGE", function(mobArg, amount, attacker, attackType, damageType)
+        if amount > mobArg:getHP() then
+            local target = attacker
             if target:isPet() then
                 target = target:getMaster()
             end
-            mobArg:timer(math.random(2,6)*1000, function(mobArg2)
-            end)
-            for i = 2, 9 do
-                print("entered for loop")
-                local spider = mobArg:getID() + i
 
-                mobArg:timer(1000, function(mobArg2)
-                    SpawnMob(spider):setSpawn(mobArg:getPos())
-                    spider:updateClaim(killer)
-                    spider:updateEnmity(killer)
-                end)
-            end
+            -- Spawn first spider to prevent battlefield ending
+            GetMobByID(mobArg:getID()+2):setSpawn(mobArg:getXPos(), mobArg:getYPos(), mobArg:getZPos(), mobArg:getRotPos())
+            SpawnMob(mobArg:getID()+2):updateEnmity(attacker)
+
+            -- Recursively spawn the rest of the spiders
+            spawnSpider(mob, attacker, 3)
         end
     end)
 end
 
-entity.onMobDeath = function(mob, player, isKiller)
+entity.onMobDeath = function(mob)
 end
 
 return entity
