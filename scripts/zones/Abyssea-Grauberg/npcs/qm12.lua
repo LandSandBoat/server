a06c7de1f8054bbe7364528c15149011d177fdd7
Xaver-DaRed <xaver.entropia@gmail.com>
2022-07-28 13:06:47 +0200

    Enable Grauberg's QM's Triggers and Trades
    Co-authored-by: Hinekri <Hinekri@users.noreply.github.com>

--- scripts/zones/Abyssea-Grauberg/npcs/qm12.lua
+++ scripts/zones/Abyssea-Grauberg/npcs/qm12.lua
@@ -4,16 +4,17 @@
 -- Spawns Alfard
 -- !pos 309 -32 173 254
 -----------------------------------
-require("scripts/globals/abyssea")
+local ID = require('scripts/zones/Abyssea-Grauberg/IDs')
+require('scripts/globals/abyssea')
+require('scripts/globals/keyitems')
 -----------------------------------
 local entity = {}
 
 entity.onTrade = function(player, npc, trade)
-    -- xi.abyssea.qmOnTrade(player, npc, trade)
 end
 
 entity.onTrigger = function(player, npc)
-    -- xi.abyssea.qmOnTrigger(player, npc)
+    xi.abyssea.qmOnTrigger(player, npc, ID.mob.ALFARD_1, { xi.ki.VENOMOUS_HYDRA_FANG })
 end
 
 entity.onEventUpdate = function(player, csid, option)
--- scripts/zones/Abyssea-Grauberg/npcs/qm15.lua
+++ scripts/zones/Abyssea-Grauberg/npcs/qm15.lua
@@ -4,16 +4,17 @@
 -- Spawns Raja
 -- !pos 495 56 679 254
 -----------------------------------
-require("scripts/globals/abyssea")
+local ID = require('scripts/zones/Abyssea-Grauberg/IDs')
+require('scripts/globals/abyssea')
+require('scripts/globals/keyitems')
 -----------------------------------
 local entity = {}
 
 entity.onTrade = function(player, npc, trade)
-    -- xi.abyssea.qmOnTrade(player, npc, trade)
 end
 
 entity.onTrigger = function(player, npc)
-    -- xi.abyssea.qmOnTrigger(player, npc)
+    xi.abyssea.qmOnTrigger(player, npc, ID.mob.RAJA_2, { xi.ki.WARPED_CHARIOT_PLATE, xi.ki.SHATTERED_IRON_GIANT_CHAIN })
 end
 
 entity.onEventUpdate = function(player, csid, option)
