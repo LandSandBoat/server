bccf366ca37be33ee9cf65cf37656582036530a7
HaxPrime <108164867+HaxPrime@users.noreply.github.com>
2022-07-29 12:33:51 -0500

    znm t4 upscale

--- scripts/zones/Wajaom_Woodlands/mobs/Tinnin.lua
+++ scripts/zones/Wajaom_Woodlands/mobs/Tinnin.lua
@@ -1,21 +1,59 @@
 -----------------------------------
 -- Area: Wajaom Woodlands
 --  ZNM: Tinnin
--- !pos 276 0 -694
--- Spawned with Monkey Wine: @additem 2573
--- Wiki: http://ffxiclopedia.wikia.com/wiki/Tinnin
+-- !pos 276 0 -694 51
+-- Spawned with Monkey Wine: !additem 2573
 -----------------------------------
-mixins =
-{
-    require("scripts/mixins/job_special"),
-    require("scripts/mixins/rage")
-}
+mixins ={require("scripts/mixins/job_special"),
+require("scripts/mixins/rage")}
 require("scripts/globals/magic")
 require("scripts/globals/status")
 -----------------------------------
 local entity = {}
 
 entity.onMobInitialize = function(mob)
+    mob:addMod(xi.mod.ATT, 200)
+    mob:addMod(xi.mod.MDEF, 150)
+    mob:addMod(xi.mod.DEF, 100)
+    mob:addMod(xi.mod.MAIN_DMG_RATING, 50)
+    mob:addMod(xi.mod.STR, 50)
+    mob:addMod(xi.mod.VIT, 20)
+    mob:addMod(xi.mod.INT, 50)
+    mob:addMod(xi.mod.MND, 20)
+    mob:addMod(xi.mod.CHR, 20)
+    mob:addMod(xi.mod.AGI, 20)
+    mob:addMod(xi.mod.DEX, 40)
+    mob:setMod(xi.mod.DEFP, 50)
+    mob:addMod(xi.mod.DEFP, 475)
+    mob:setMod(xi.mod.DOUBLE_ATTACK, 10)
+    mob:setMod(xi.mod.EARTH_MEVA, 128)
+    mob:setMod(xi.mod.DARK_MEVA, 250)
+    mob:setMod(xi.mod.LIGHT_MEVA, 128)
+    mob:setMod(xi.mod.FIRE_MEVA, 170)
+    mob:setMod(xi.mod.WATER_MEVA, 250)
+    mob:setMod(xi.mod.THUNDER_MEVA, 128)
+    mob:setMod(xi.mod.ICE_MEVA, 200)
+    mob:setMod(xi.mod.WIND_MEVA, 170)
+    mob:setMod(xi.mod.EARTH_SDT, 128)
+    mob:setMod(xi.mod.DARK_SDT, 250)
+    mob:setMod(xi.mod.LIGHT_SDT, 128)
+    mob:setMod(xi.mod.FIRE_SDT, 170)
+    mob:setMod(xi.mod.WATER_SDT, 250)
+    mob:setMod(xi.mod.THUNDER_SDT, 128)
+    mob:setMod(xi.mod.ICE_SDT, 200)
+    mob:setMod(xi.mod.WIND_SDT, 170)
+    mob:setMod(xi.mod.SILENCERES, 10000)
+    mob:setMod(xi.mod.WATER_ABSORB, 100)
+    mob:setMod(xi.mod.STUNRES, 50)
+    mob:setMod(xi.mod.BINDRES, 10000)
+    mob:setMod(xi.mod.GRAVITYRES, 10000)
+    mob:setMod(xi.mod.SLEEPRES, 10000)
+    mob:setMod(xi.mod.PARALYZERES, 100)
+    mob:setMod(xi.mod.LULLABYRES, 10000)
+    mob:setMod(xi.mod.FASTCAST, 10)
+    mob:addStatusEffect(xi.effect.REGAIN, 10, 3, 0)
+    mob:addStatusEffect(xi.effect.REFRESH, 50, 3, 0)
+    mob:addMod(xi.mod.MOVE, 12) 
     mob:setMobMod(xi.mobMod.GIL_MIN, 12000)
     mob:setMobMod(xi.mobMod.GIL_MAX, 30000)
     mob:setMobMod(xi.mobMod.MUG_GIL, 8000)
@@ -28,8 +66,11 @@ entity.onMobSpawn = function(mob)
     mob:setLocalVar("[rage]timer", 3600) -- 60 minutes
     mob:setHP(mob:getMaxHP()/2)
     mob:setUnkillable(true)
-    mob:setMod(xi.mod.REGEN, 50)
-
+    mob:setMod(xi.mod.REGEN, 50, 3, 0)
+    -- Mighty Strikes
+      xi.mix.jobSpecial.config(mob, {
+        specials ={{id = xi.jsa.MIGHTY_STRIKES, cooldown = 120, hpp = 100},}})
+   
     -- Regen Head every 1.5-4 minutes 90-240
     mob:setLocalVar("headTimer", os.time() + math.random(60, 190))
 
