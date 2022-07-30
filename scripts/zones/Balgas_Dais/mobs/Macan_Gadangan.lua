8186c6a8deb6d8d8a3edb291b5594d21b4b52992
Abdiah <62350957+dallano@users.noreply.github.com>
2022-07-30 17:03:30 -0600

    Wild whiskers ASB (#208)
    * BCNM Wild Wild Whiskers
    
    BCNM Wild Wild Whiskers Fix
    Basis and evidence received from the following:
    https://ffxiclopedia.fandom.com/wiki/Wild_Wild_Whiskers
    https://www.youtube.com/watch?v=kFYAEeiY078&t=96s&ab_channel=Pepriona
    https://www.youtube.com/watch?v=cBZrxrMznIg&t=37s&ab_channel=upbeatglitch
    https://www.youtube.com/watch?v=BH4IGqAPNmQ&t=127s&ab_channel=James
    Live capture evidence from Aerec  (unrecorded)
    
    Fix:
    Fixed Macan's messaging system when casting spells and added a magic rank system that increased magic attk
    Magic ranks increased after every 3 casts and ends at rank 5
    Fixed spawning of Macan and armoury crate to be centred in the battlefield
    Macan immediately reaches level 5 after being silenced
    Added an immediate use of Petrificative breath after casting a spell at the highest magic rank
    Increased MP of Macan to prevent running out of MP partway into the fight
    
    TODO: Fix the status check MAGIC_INTERRUPT in order for Macan's frenzied rage counter to work.
    
    * SQL fix
    
    Fixed a duplicate line error in npc_list.nql
    
    * Update Macan_Gadangan.lua
    
    - Prevented use of frenzied rage while at rank 5
    - Increased magic rank after every 2 casts
    - Now uses blaster at rank 5 after every cast of burst
    - Can only achieve rank 4 unless silenced
    - converted spell IDs to enums from spell_data
    
    * Update Macan_Gadangan.lua
    
    removed .lua from require /spell_data
    
    * MAGIC_INTERRUPTED listener
    
    Added Ophelia's magic interrupted listener from https://github.com/AirSkyBoat/AirSkyBoat/pull/153/files
    
    * Fixes
    
    Styling Fix
    
    * style fixes
    
    Removed old comment
    moved local function to top of file
    
    * Style Fixes
    
    Removed double white space

---
+++ scripts/zones/Balgas_Dais/mobs/Macan_Gadangan.lua
@@ -0,0 +1,78 @@
+-----------------------------------
+-- Area: Balga's Dais
+--  Mob: Macan Gadangan
+-- BCNM: Wild Wild Whiskers
+-----------------------------------
+local ID = require("scripts/zones/Balgas_Dais/IDs")
+require("scripts/globals/spell_data")
+-----------------------------------
+local entity = {}
+
+local function magicRankUp(mob)
+    local amount = mob:getLocalVar("casts")
+
+    -- Increase magic rank after every 3 casts
+    if amount % 3 == 0 and mob:getLocalVar("magicRank") ~= 4 then
+        mob:setLocalVar("magicRank", mob:getLocalVar("magicRank") + 1)
+        mob:addMod(xi.mod.MATT, 100)
+    end
+end
+
+entity.onMobSpawn = function(mob)
+    mob:setLocalVar("magicRank", 1)
+    mob:setLocalVar("casts", 0)
+    mob:setLocalVar("control", 0)
+
+    mob:addListener("MAGIC_INTERRUPTED", "MACAN_MAGIC_INTERRUPTED", function(mobArg, target, spell, action)
+        mobArg:queue(0, function(mobArg1)
+            mobArg1:useMobAbility(1336)
+        end)
+    end)
+end
+
+entity.onMobMagicPrepare = function(mob, target, spell)
+    mob:setLocalVar("casts", mob:getLocalVar("casts") + 1)
+
+    magicRankUp(mob)
+    local rank = mob:getLocalVar("magicRank")
+
+    if rank == 1 then
+        mob:messageText(target, ID.text.MACAN_WHISKERS_SLIGHTLY, false)
+    elseif rank == 2 then
+        mob:messageText(target, ID.text.MACAN_WHISKERS_RAPIDLY, false)
+    elseif rank == 3 then
+        mob:messageText(target, ID.text.MACAN_WHISKERS_WILDLY, false)
+    elseif rank == 4 then
+        mob:messageText(target, ID.text.MACAN_WHISKERS_VIOLENTLY, false)
+        return xi.magic.spell.THUNDAGA_II
+    elseif rank == 5 then
+        mob:messageText(target, ID.text.MACAN_WHISKERS_UNCONTROLLABLY, false)
+        return xi.magic.spell.BURST
+    end
+end
+
+entity.onMobFight = function(mob)
+    local rank = mob:getLocalVar("magicRank")
+
+    if mob:hasStatusEffect(xi.effect.SILENCE) then
+        mob:setLocalVar("magicRank", 5)
+        mob:setMod(xi.mod.MATT, 400)
+    end
+
+    if
+        mob:getCurrentAction() == xi.act.MAGIC_CASTING and
+        mob:getLocalVar("control") == 0 and
+        rank == 5
+    then
+        mob:setLocalVar("control", 1)
+        mob:queue(0, function(mobArg)
+            mobArg:useMobAbility(652)
+            mob:setLocalVar("control", 0)
+        end)
+    end
+end
+
+entity.onMobDeath = function(mob)
+end
+
+return entity
