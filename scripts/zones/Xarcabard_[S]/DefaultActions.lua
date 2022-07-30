df8cfec64a46d7cad69aa37a64a11f7bec9a29e8
claywar <claywar@beerlab.dev>
2022-07-29 20:31:31 -0400

    Interaction - Mission: Storm's Crescendo
    Initial blocking, Key Item recovery
    
    Finish mission

--- scripts/zones/Xarcabard_[S]/DefaultActions.lua
+++ scripts/zones/Xarcabard_[S]/DefaultActions.lua
@@ -2,6 +2,7 @@ local ID = require('scripts/zones/Xarcabard_[S]/IDs')
 
 return {
     ['Animal_Spoor']      = { messageSpecial = ID.text.NOTHING_OUT_OF_ORDINARY },
+    ['Excavated_Snow']    = { messageSpecial = ID.text.NO_RESPONSE },
     ['Forbidden_Portal']  = { messageSpecial = ID.text.NO_RESPONSE },
     ['Spell-worked_Snow'] = { messageSpecial = ID.text.NO_RESPONSE },
     ['Wheel_Rut']         = { messageSpecial = ID.text.NOTHING_OUT_OF_ORDINARY },
