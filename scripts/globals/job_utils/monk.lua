c1fc5007ff13bb3cb2b5a4ea9bc7e02bcc500ce5
Schaedelschaden <sitfixalot@gmail.com>
2022-07-31 23:28:05 -0600

    [LUA] Create job_utils/monk
    Create job_utils/monk.lua and populate necessary ability check and ability use functions.

---
+++ scripts/globals/job_utils/monk.lua
@@ -0,0 +1,61 @@
+-----------------------------------
+-- Monk Job Utilities
+-----------------------------------
+require('scripts/globals/items')
+require("scripts/globals/settings")
+require("scripts/globals/status")
+-----------------------------------
+xi = xi or {}
+xi.job_utils = xi.job_utils or {}
+xi.job_utils.monk = xi.job_utils.monk or {}
+
+-----------------------------------
+-- Ability Check Functions
+-----------------------------------
+xi.job_utils.monk.checkHundredFists = function(player, target, ability)
+end
+
+xi.job_utils.monk.checkInnerStrength = function(player, target, ability)
+end
+
+-----------------------------------
+-- Ability Use Functions
+-----------------------------------
+xi.job_utils.monk.useBoost = function(player, target, ability)
+end
+
+xi.job_utils.monk.useChakra = function(player, target, ability)
+end
+
+xi.job_utils.monk.useChiBlast = function(player, target, ability)
+end
+
+xi.job_utils.monk.useCounterstance = function(player, target, ability)
+end
+
+xi.job_utils.monk.useDodge = function(player, target, ability)
+end
+
+xi.job_utils.monk.useFocus = function(player, target, ability)
+end
+
+xi.job_utils.monk.useFootwork = function(player, target, ability)
+end
+
+xi.job_utils.monk.useFormlessStrikes = function(player, target, ability)
+end
+
+xi.job_utils.monk.useHundredFists = function(player, target, ability)
+end
+
+xi.job_utils.monk.useImpetus = function(player, target, ability)
+end
+
+xi.job_utils.monk.useInnerStrength = function(player, target, ability)
+end
+
+xi.job_utils.monk.useMantra = function(player, target, ability)
+end
+
+xi.job_utils.monk.usePerfectCounter = function(player, target, ability)
+end
