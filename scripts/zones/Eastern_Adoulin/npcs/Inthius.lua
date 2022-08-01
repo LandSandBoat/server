c4d65e6fb672466da4f0c0999edc1f455592fc7e
Tom-Neverwinter <Tom_Neverwinter@yahoo.com>
2022-08-01 12:28:39 -0500

    fix pos

--- scripts/zones/Eastern_Adoulin/npcs/Inthius.lua
+++ scripts/zones/Eastern_Adoulin/npcs/Inthius.lua
@@ -1,22 +1,23 @@
 -----------------------------------
--- Area: Eastern Adoulin
+-- Area: Eastern Adoulin !zone 257
 -- NPC: Inthius
 -- Type: Weather Reporter
--- !pos -97.3680 0.0000 -34.3807
+-- !pos -97.145 0.001 -36.710 257
 -----------------------------------
 local entity = {}
-
+ 
 entity.onTrade = function(player, npc, trade)
 end
-
+ 
 entity.onTrigger = function(player, npc)
     player:startEvent(4, 0, 0, 0, 0, 0, 0, 0, VanadielTime())
 end
-
+ 
 entity.onEventUpdate = function(player, csid, option)
 end
-
+ 
 entity.onEventFinish = function(player, csid, option)
 end
-
+ 
 return entity
+ 
