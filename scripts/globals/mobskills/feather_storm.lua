7f083a24204a234a5a59fe0545426a697919a9d1
Zelita <siknoz@yahoo.com>
2022-08-01 18:10:44 -0600

    Fix Feather Storm doing 0 damage

--- scripts/globals/mobskills/feather_storm.lua
+++ scripts/globals/mobskills/feather_storm.lua
@@ -22,8 +22,12 @@ mobskill_object.onMobWeaponSkill = function(target, mob, skill)
     local dmg = xi.mobskills.mobFinalAdjustments(info.dmg, mob, skill, target, xi.attackType.PHYSICAL, xi.damageType.PIERCING, info.hitslanded)
 
     local typeEffect = xi.effect.POISON
+    local power = (mob:getMainLvl()/7)
+    if (power < 1 ) then
+        power = 1
+    end
 
-    xi.mobskills.mobPhysicalStatusEffectMove(mob, target, skill, typeEffect, mob:getMainLvl()/7, 3, 120)
+    xi.mobskills.mobPhysicalStatusEffectMove(mob, target, skill, typeEffect, power, 3, 120)
 
     target:takeDamage(dmg, mob, xi.attackType.PHYSICAL, xi.damageType.PIERCING)
     return dmg
