b5204d2b5a3771f30ee88eaf40a8021fcddc2a43
Xaver-DaRed <xaver.entropia@gmail.com>
2022-08-04 12:14:04 +0200

    Delete non-retail settings and related code

--- scripts/zones/Dragons_Aery/npcs/qm0.lua
+++ scripts/zones/Dragons_Aery/npcs/qm0.lua
@@ -6,22 +6,18 @@
 -----------------------------------
 local ID = require("scripts/zones/Dragons_Aery/IDs")
 require("scripts/globals/npc_util")
-require("scripts/globals/settings")
 require("scripts/globals/status")
 -----------------------------------
 local entity = {}
 
 entity.onSpawn = function(npc)
-    if xi.settings.main.LandKingSystem_NQ < 1 and xi.settings.main.LandKingSystem_HQ < 1 then
-        npc:setStatus(xi.status.DISAPPEAR)
-    end
 end
 
 entity.onTrade = function(player, npc, trade)
     if not GetMobByID(ID.mob.FAFNIR):isSpawned() and not GetMobByID(ID.mob.NIDHOGG):isSpawned() then
-        if xi.settings.main.LandKingSystem_NQ ~= 0 and npcUtil.tradeHas(trade, 3339) and npcUtil.popFromQM(player, npc, ID.mob.FAFNIR) then
+        if npcUtil.tradeHasExactly(trade, 3339) and npcUtil.popFromQM(player, npc, ID.mob.FAFNIR) then
             player:confirmTrade()
-        elseif xi.settings.main.LandKingSystem_HQ ~= 0 and npcUtil.tradeHas(trade, 3340) and npcUtil.popFromQM(player, npc, ID.mob.NIDHOGG) then
+        elseif npcUtil.tradeHasExactly(trade, 3340) and npcUtil.popFromQM(player, npc, ID.mob.NIDHOGG) then
             player:confirmTrade()
         end
     end
