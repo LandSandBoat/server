ca5ffc95cea969fd10846b4dfc9331884de3aafb
claywar <claywar@beerlab.dev>
2022-08-05 15:40:10 -0400

    Interaction - Mission: By the Fading Light

---
+++ scripts/missions/wotg/39_By_the_Fading_Light.lua
@@ -0,0 +1,42 @@
+-----------------------------------
+-- By the Fading Light
+-- Wings of the Goddess Mission 39
+-----------------------------------
+-- !addmission 5 38
+-- Rally Point: Red : !pos -106.071 -25.5 -52.841 137
+-----------------------------------
+require('scripts/globals/missions')
+require('scripts/globals/interaction/mission')
+require('scripts/globals/zone')
+require('scripts/missions/wotg/helpers')
+-----------------------------------
+
+local mission = Mission:new(xi.mission.log_id.WOTG, xi.mission.id.wotg.BY_THE_FADING_LIGHT)
+
+mission.reward =
+{
+    nextMission = { xi.mission.log_id.WOTG, xi.mission.id.wotg.EDGE_OF_EXISTENCE },
+}
+
+mission.sections =
+{
+    {
+        check = function(player, currentMission, missionStatus, vars)
+            return currentMission == mission.missionId
+        end,
+
+        [xi.zone.XARCABARD_S] =
+        {
+            ['Rally_Point_Red'] = mission:progressEvent(41, 137, 0, 0, 7),
+
+            onEventFinish =
+            {
+                [41] = function(player, csid, option, npc)
+                    mission:complete(player)
+                end,
+            },
+        },
+    },
+}
+
+return mission
