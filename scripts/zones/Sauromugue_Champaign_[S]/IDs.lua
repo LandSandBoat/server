95382e1a56a6a001d0e7183bc66ce16d43d3598f
claywar <claywar@beerlab.dev>
2022-08-06 11:42:12 -0400

    Interaction - Quest: Her Memories: Operation Cupid

--- scripts/zones/Sauromugue_Champaign_[S]/IDs.lua
+++ scripts/zones/Sauromugue_Champaign_[S]/IDs.lua
@@ -17,6 +17,7 @@ zones[xi.zone.SAUROMUGUE_CHAMPAIGN_S] =
         CARRIED_OVER_POINTS      = 7001,  -- You have carried over <number> login point[/s].
         LOGIN_CAMPAIGN_UNDERWAY  = 7002,  -- The [/January/February/March/April/May/June/July/August/September/October/November/December] <number> Login Campaign is currently underway!<space>
         LOGIN_NUMBER             = 7003,  -- In celebration of your most recent login (login no. <number>), we have provided you with <number> points! You currently have a total of <number> points.
+        DOOR_FIRMLY_SEALED       = 7723,  -- The door is firmly sealed.
         SURRENDER_CEREMONY_HASTE = 8471,  -- The surrender ceremony is about to commence underground. Make haste before all is lost!
         VOIDWALKER_DESPAWN       = 8483,  -- The monster fades before your eyes, a look of disappointment on its face.
         VOIDWALKER_NO_MOB        = 8670,  -- The <keyitem> quivers ever so slightly, but emits no light. There seem to be no monsters in the area.
