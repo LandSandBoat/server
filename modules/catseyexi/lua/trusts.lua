590945d1f380e301418d24dc9889f0c8bd3a6f26
Mi <djdeffkid@gmail.com>
2022-08-07 22:26:51 -0400

    Add AAHM to our madness
    Very good. Much Nice. So WoW
    
    -- Adds AAHM Logic

--- modules/catseyexi/lua/trusts.lua
+++ modules/catseyexi/lua/trusts.lua
@@ -515,6 +515,40 @@ m:addOverride("xi.globals.spells.trust.kupofried.onSpellCast", function(caster, 
     trust:SetAutoAttackEnabled(false)
 end)
 
+m:addOverride("xi.globals.spells.trust.aahm.onSpellCast", function(caster, target, spell)
+
+    local trust = caster:spawnTrust(spell:getID())
+
+    local power = trust:getMainLvl() / 3
+
+    trust:addMod(xi.mod.DEF, power)
+    trust:addMod(xi.mod.ATT, power * 3)
+    trust:addMod(xi.mod.ACC, power * 5)
+    trust:addMod(xi.mod.UTSUSEMI_BONUS, 1)
+
+--    trust:addSimpleGambit(ai.t.SELF, ai.c.NOT_STATUS, xi.effect.MIGAWARI, ai.r.MA, ai.s.SPECIFIC, xi.magic.spell.MIGAWARI)
+
+    -- Shadows are represented by xi.effect.COPY_IMAGE, but with different icons depending on the tier
+    trust:addSimpleGambit(ai.t.SELF, ai.c.NOT_STATUS, xi.effect.COPY_IMAGE, ai.r.MA, ai.s.HIGHEST, xi.magic.spellFamily.UTSUSEMI)
+
+    trust:addSimpleGambit(ai.t.TARGET, ai.c.NOT_STATUS, xi.effect.BLINDNESS, ai.r.MA, ai.s.HIGHEST, xi.magic.spellFamily.KURAYAMI, 60)
+    trust:addSimpleGambit(ai.t.TARGET, ai.c.NOT_STATUS, xi.effect.SLOW, ai.r.MA, ai.s.HIGHEST, xi.magic.spellFamily.HOJO, 60)
+
+    -- DD Mode
+    trust:addSimpleGambit(ai.t.SELF, ai.c.PT_HAS_TANK, 0, ai.r.JA, ai.s.SPECIFIC, xi.ja.BERSERK)
+    trust:addSimpleGambit(ai.t.SELF, ai.c.PT_HAS_TANK, 0, ai.r.JA, ai.s.SPECIFIC, xi.ja.INNIN)
+    trust:addSimpleGambit(ai.t.TANK, ai.c.HPP_LT, 50, ai.r.JA, ai.s.SPECIFIC, xi.ja.PROVOKE)
+
+    -- Tank Mode
+    trust:addSimpleGambit(ai.t.TARGET, ai.c.NOT_PT_HAS_TANK, 0, ai.r.JA, ai.s.SPECIFIC, xi.ja.PROVOKE)
+    trust:addSimpleGambit(ai.t.SELF, ai.c.NOT_PT_HAS_TANK, 0, ai.r.JA, ai.s.SPECIFIC, xi.ja.WARCRY)
+    trust:addSimpleGambit(ai.t.SELF, ai.c.NOT_PT_HAS_TANK, 0, ai.r.JA, ai.s.SPECIFIC, xi.ja.YONIN)
+
+    trust:addSimpleGambit(ai.t.MASTER, ai.c.HPP_LT, 50, ai.r.JA, ai.s.SPECIFIC, xi.ja.PROVOKE)
+
+    trust:setTrustTPSkillSettings(ai.tp.ASAP, ai.s.RANDOM)
+end)
+
 local trustToReplaceName = "moogle"
 
 m:addOverride(string.format("xi.globals.spells.trust.%s.onSpellCast", trustToReplaceName), function(caster, target, spell)
@@ -568,7 +602,6 @@ m:addOverride(string.format("xi.globals.spells.trust.%s.onMobSpawn", trustToRepl
 end)
 
 return m 
-
 
 
 
