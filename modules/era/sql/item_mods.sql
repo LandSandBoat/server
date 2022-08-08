0e9c82326b13b3692fe08d0f9be926e5711a4cd3
Critical <lyardeen@gmail.com>
2022-08-08 00:42:55 -0600

    Add new item_mods sql module; MDT II removed from Aegis

---
+++ modules/era/sql/item_mods.sql
@@ -0,0 +1,11 @@
+-- --------------------------------------------------------
+-- AirSkyBoat Database Conversion File
+-- --------------------------------------------------------
+-- Table Structure Definition
+--
+-- 
+-- 
+-- --------------------------------------------------------
+
+REPLACE INTO `item_mods` (`itemid`, `modid`, `value`) VALUES
+    (15070,163,-2500); -- Aegis DMGMAGIC: -2500
