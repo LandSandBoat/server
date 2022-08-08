Wings-XI/LegacyWingsXI

Various Item Fixes

scripts/globals/items/yellow_curry_bun.lua
@@ -32,31 +32,61 @@ function onItemUse(target)
 end
 
 function onEffectGain(target, effect)
-    target:addMod(tpz.mod.HP, 20)
-    target:addMod(tpz.mod.STR, 5)
-    target:addMod(tpz.mod.AGI, 2)
-    target:addMod(tpz.mod.INT, -4)
-    target:addMod(tpz.mod.FOOD_ATTP, 20)
-    target:addMod(tpz.mod.FOOD_ATT_CAP, 75)
-    target:addMod(tpz.mod.FOOD_RATTP, 20)
-    target:addMod(tpz.mod.FOOD_RATT_CAP, 75)
-    target:addMod(tpz.mod.SLEEPRESTRAIT, 3)
-    target:addMod(tpz.mod.STUNRESTRAIT, 4)
-    target:addMod(tpz.mod.HPHEAL, 2)
-    target:addMod(tpz.mod.MPHEAL, 1)
+    if target:getPartySize() > 3 then
+        target:addMod(tpz.mod.HP, 20)
+        target:addMod(tpz.mod.STR, 5)
+        target:addMod(tpz.mod.AGI, 2)
+        target:addMod(tpz.mod.INT, -4)
+        target:addMod(tpz.mod.FOOD_ATTP, 20)
+        target:addMod(tpz.mod.FOOD_ATT_CAP, 85)
+        target:addMod(tpz.mod.FOOD_RATTP, 20)
+        target:addMod(tpz.mod.FOOD_RATT_CAP, 85)
+        target:addMod(tpz.mod.SLEEPRESTRAIT, 3)
+        target:addMod(tpz.mod.STUNRESTRAIT, 4)
+        target:addMod(tpz.mod.HPHEAL, 2)
+        target:addMod(tpz.mod.MPHEAL, 1)
+    else
+        target:addMod(tpz.mod.HP, 20)
+        target:addMod(tpz.mod.STR, 5)
+        target:addMod(tpz.mod.AGI, 2)
+        target:addMod(tpz.mod.INT, -4)
+        target:addMod(tpz.mod.FOOD_ATTP, 20)
+        target:addMod(tpz.mod.FOOD_ATT_CAP, 75)
+        target:addMod(tpz.mod.FOOD_RATTP, 20)
+        target:addMod(tpz.mod.FOOD_RATT_CAP, 75)
+        target:addMod(tpz.mod.SLEEPRESTRAIT, 3)
+        target:addMod(tpz.mod.STUNRESTRAIT, 4)
+        target:addMod(tpz.mod.HPHEAL, 2)
+        target:addMod(tpz.mod.MPHEAL, 1)
+    end
 end
 
 function onEffectLose(target, effect)
-    target:delMod(tpz.mod.HP, 20)
-    target:delMod(tpz.mod.STR, 5)
-    target:delMod(tpz.mod.AGI, 2)
-    target:delMod(tpz.mod.INT, -4)
-    target:delMod(tpz.mod.FOOD_ATTP, 20)
-    target:delMod(tpz.mod.FOOD_ATT_CAP, 75)
-    target:delMod(tpz.mod.FOOD_RATTP, 20)
-    target:delMod(tpz.mod.FOOD_RATT_CAP, 75)
-    target:delMod(tpz.mod.SLEEPRESTRAIT, 3)
-    target:delMod(tpz.mod.STUNRESTRAIT, 4)
-    target:delMod(tpz.mod.HPHEAL, 2)
-    target:delMod(tpz.mod.MPHEAL, 1)
+    if target:getMod(tpz.mod.FOOD_ATT_CAP) == 85 then 
+        target:delMod(tpz.mod.HP, 20)
+        target:delMod(tpz.mod.STR, 5)
+        target:delMod(tpz.mod.AGI, 2)
+        target:delMod(tpz.mod.INT, -4)
+        target:delMod(tpz.mod.FOOD_ATTP, 20)
+        target:delMod(tpz.mod.FOOD_ATT_CAP, 85)
+        target:delMod(tpz.mod.FOOD_RATTP, 20)
+        target:delMod(tpz.mod.FOOD_RATT_CAP, 85)
+        target:delMod(tpz.mod.SLEEPRESTRAIT, 3)
+        target:delMod(tpz.mod.STUNRESTRAIT, 4)
+        target:delMod(tpz.mod.HPHEAL, 2)
+        target:delMod(tpz.mod.MPHEAL, 1)
+    else
+        target:delMod(tpz.mod.HP, 20)
+        target:delMod(tpz.mod.STR, 5)
+        target:delMod(tpz.mod.AGI, 2)
+        target:delMod(tpz.mod.INT, -4)
+        target:delMod(tpz.mod.FOOD_ATTP, 20)
+        target:delMod(tpz.mod.FOOD_ATT_CAP, 75)
+        target:delMod(tpz.mod.FOOD_RATTP, 20)
+        target:delMod(tpz.mod.FOOD_RATT_CAP, 75)
+        target:delMod(tpz.mod.SLEEPRESTRAIT, 3)
+        target:delMod(tpz.mod.STUNRESTRAIT, 4)
+        target:delMod(tpz.mod.HPHEAL, 2)
+        target:delMod(tpz.mod.MPHEAL, 1)
+    end
 end

LandSandBoat/server

scripts/globals/items/yellow_curry_bun.lua
@@ -33,33 +33,63 @@ item_object.onItemUse = function(target)
 end
 
 item_object.onEffectGain = function(target, effect)
-    target:addMod(xi.mod.HP, 20)
-    target:addMod(xi.mod.STR, 5)
-    target:addMod(xi.mod.AGI, 2)
-    target:addMod(xi.mod.INT, -4)
-    target:addMod(xi.mod.FOOD_ATTP, 20)
-    target:addMod(xi.mod.FOOD_ATT_CAP, 75)
-    target:addMod(xi.mod.FOOD_RATTP, 20)
-    target:addMod(xi.mod.FOOD_RATT_CAP, 75)
-    target:addMod(xi.mod.SLEEPRES, 3)
-    target:addMod(xi.mod.STUNRES, 4)
-    target:addMod(xi.mod.HPHEAL, 2)
-    target:addMod(xi.mod.MPHEAL, 1)
+    if target:getPartySize() > 3 then
+        target:addMod(xi.mod.HP, 20)
+        target:addMod(xi.mod.STR, 5)
+        target:addMod(xi.mod.AGI, 2)
+        target:addMod(xi.mod.INT, -4)
+        target:addMod(xi.mod.FOOD_ATTP, 20)
+        target:addMod(xi.mod.FOOD_ATT_CAP, 85)
+        target:addMod(xi.mod.FOOD_RATTP, 20)
+        target:addMod(xi.mod.FOOD_RATT_CAP, 85)
+        target:addMod(xi.mod.SLEEPRESTRAIT, 3)
+        target:addMod(xi.mod.STUNRESTRAIT, 4)
+        target:addMod(xi.mod.HPHEAL, 2)
+        target:addMod(xi.mod.MPHEAL, 1)
+    else
+        target:addMod(xi.mod.HP, 20)
+        target:addMod(xi.mod.STR, 5)
+        target:addMod(xi.mod.AGI, 2)
+        target:addMod(xi.mod.INT, -4)
+        target:addMod(xi.mod.FOOD_ATTP, 20)
+        target:addMod(xi.mod.FOOD_ATT_CAP, 75)
+        target:addMod(xi.mod.FOOD_RATTP, 20)
+        target:addMod(xi.mod.FOOD_RATT_CAP, 75)
+        target:addMod(xi.mod.SLEEPRESTRAIT, 3)
+        target:addMod(xi.mod.STUNRESTRAIT, 4)
+        target:addMod(xi.mod.HPHEAL, 2)
+        target:addMod(xi.mod.MPHEAL, 1)
+    end
 end
 
 item_object.onEffectLose = function(target, effect)
-    target:delMod(xi.mod.HP, 20)
-    target:delMod(xi.mod.STR, 5)
-    target:delMod(xi.mod.AGI, 2)
-    target:delMod(xi.mod.INT, -4)
-    target:delMod(xi.mod.FOOD_ATTP, 20)
-    target:delMod(xi.mod.FOOD_ATT_CAP, 75)
-    target:delMod(xi.mod.FOOD_RATTP, 20)
-    target:delMod(xi.mod.FOOD_RATT_CAP, 75)
-    target:delMod(xi.mod.SLEEPRES, 3)
-    target:delMod(xi.mod.STUNRES, 4)
-    target:delMod(xi.mod.HPHEAL, 2)
-    target:delMod(xi.mod.MPHEAL, 1)
+    if target:getMod(xi.mod.FOOD_ATT_CAP) == 85 then
+        target:delMod(xi.mod.HP, 20)
+        target:delMod(xi.mod.STR, 5)
+        target:delMod(xi.mod.AGI, 2)
+        target:delMod(xi.mod.INT, -4)
+        target:delMod(xi.mod.FOOD_ATTP, 20)
+        target:delMod(xi.mod.FOOD_ATT_CAP, 85)
+        target:delMod(xi.mod.FOOD_RATTP, 20)
+        target:delMod(xi.mod.FOOD_RATT_CAP, 85)
+        target:delMod(xi.mod.SLEEPRESTRAIT, 3)
+        target:delMod(xi.mod.STUNRESTRAIT, 4)
+        target:delMod(xi.mod.HPHEAL, 2)
+        target:delMod(xi.mod.MPHEAL, 1)
+    else
+        target:delMod(xi.mod.HP, 20)
+        target:delMod(xi.mod.STR, 5)
+        target:delMod(xi.mod.AGI, 2)
+        target:delMod(xi.mod.INT, -4)
+        target:delMod(xi.mod.FOOD_ATTP, 20)
+        target:delMod(xi.mod.FOOD_ATT_CAP, 75)
+        target:delMod(xi.mod.FOOD_RATTP, 20)
+        target:delMod(xi.mod.FOOD_RATT_CAP, 75)
+        target:delMod(xi.mod.SLEEPRESTRAIT, 3)
+        target:delMod(xi.mod.STUNRESTRAIT, 4)
+        target:delMod(xi.mod.HPHEAL, 2)
+        target:delMod(xi.mod.MPHEAL, 1)
+    end
 end
 
 return item_object