d5848dc0e6a35e6f8e23dba6309564d012994908
Tom-Neverwinter <Tom_Neverwinter@yahoo.com>
2022-08-08 00:05:26 -0500

    Synergy_Engineer and Synergy_Enthusiast
    Copy pasta change event ID and pos data

---
+++ scripts/zones/Chocobo_Circuit/npcs/Synergy_Engineer.lua
@@ -0,0 +1,23 @@
+-----------------------------------
+-- Area: Chocobo Circuit
+-- NPC: Synergy Engineer
+-- Type: Standard NPC
+-- !pos -325.376,0.000,-524.698
+-- !pos -304.635,0.000,-516.724
+-----------------------------------
+local entity = {}
+
+entity.onTrade = function(player, npc, trade)
+end
+
+entity.onTrigger = function(player, npc)
+    player:startEvent(11001)
+end
+
+entity.onEventUpdate = function(player, csid, option)
+end
+
+entity.onEventFinish = function(player, csid, option)
+end
+
+return entity
