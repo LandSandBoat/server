Wings-XI/LegacyWingsXI

Fix Aern Depuration Skill Message

scripts/globals/mobskills/depuration.lua
@@ -24,8 +24,10 @@ function onMobSkillCheck(target, mob, skill)
 end
 
 function onMobWeaponSkill(target, mob, skill)
-
-    mob:eraseAllStatusEffect()
-
-    return 0
+    local effectcount = target:dispelAllStatusEffect()
+    local finalCount = effectcount + target:eraseAllStatusEffect()
+    target:eraseAllStatusEffect()
+    
+    skill:setMsg(tpz.msg.basic.DISAPPEAR_NUM)
+    return finalCount
 end

LandSandBoat/server

scripts/globals/mobskills/depuration.lua
@@ -25,10 +25,12 @@ mobskill_object.onMobSkillCheck = function(target, mob, skill)
 end
 
 mobskill_object.onMobWeaponSkill = function(target, mob, skill)
+    local effectcount = target:dispelAllStatusEffect()
+    local finalCount = effectcount + target:eraseAllStatusEffect()
+    target:eraseAllStatusEffect()
 
-    mob:eraseAllStatusEffect()
-
-    return 0
+    skill:setMsg(xi.msg.basic.DISAPPEAR_NUM)
+    return finalCount
 end
 
 return mobskill_object