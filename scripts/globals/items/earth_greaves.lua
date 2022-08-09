3f61ff4308d58497a1a01beff5e54e630d593959
Tiberon <TiberonKalkaz@gmail.com>
2022-08-08 19:42:38 -0600

    Adds functionality to Earth Greaves

---
+++ scripts/globals/items/earth_greaves.lua
@@ -0,0 +1,31 @@
+-----------------------------------------
+-- ID: 15708
+-- Item: earth_greaves Earth Greaves
+-- Item Effect: Grants Wyvren Stoneskin (200)
+-- Duration: Not stated on wiki
+-----------------------------------------
+require("scripts/globals/status")
+-----------------------------------------
+local item_object = {}
+
+item_object.onItemCheck = function(target)
+    if not target:getPet() then
+        return xi.msg.basic.REQUIRES_A_PET, 0
+    else
+        if target:getPetID() ~= xi.pet.id.WYVERN then
+            return xi.msg.basic.ITEM_UNABLE_TO_USE
+        else
+            return 0, 0
+        end
+    end
+end
+
+item_object.onItemUse = function(target)
+    local wyvern = target:getPet()
+    if wyvern then
+        if wyvern:getStatusEffect(xi.effect.STONESKIN) then
+            wyvern:delStatusEffectSilent(xi.effect.STONESKIN)
+        end
+        wyvern:addStatusEffect(xi.effect.STONESKIN, 200, 0, 180)
+    end
+end
