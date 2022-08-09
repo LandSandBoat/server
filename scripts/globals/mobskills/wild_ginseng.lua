c4be2da885490bc475c849f3939263634b320b87
dallano <atwood.dallan@hotmail.com>
2022-08-09 01:06:32 -0600

    Update wild_ginseng.lua
    Increased potency for regen when used by Bearclaw Rabbit

--- scripts/globals/mobskills/wild_ginseng.lua
+++ scripts/globals/mobskills/wild_ginseng.lua
@@ -17,12 +17,18 @@ end
 
 mobskill_object.onMobWeaponSkill = function(target, mob, skill)
     local duration = math.random(210, 270)
+    local regenPower = 30
+
+    -- Bearclaw Rabbit ENM: Follow the White Rabbit
+    if mob:getPool() == 384 then
+        regenPower = 50
+    end
 
     mob:addStatusEffect(xi.effect.BLINK, 3, 0, duration)
     mob:addStatusEffect(xi.effect.PROTECT, 60, 0, duration)
     mob:addStatusEffect(xi.effect.SHELL, 60, 0, duration)
     mob:addStatusEffect(xi.effect.HASTE, 2000, 0, duration) -- 20% haste
-    mob:addStatusEffect(xi.effect.REGEN, 30, 0, duration)
+    mob:addStatusEffect(xi.effect.REGEN, regenPower, 0, duration)
 
     mob:messageBasic(xi.msg.basic.GAINS_EFFECT_OF_STATUS, xi.effect.BLINK)
 end
