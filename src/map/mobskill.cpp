59a644786ebaf8ba38e13f42b0215291ca0499bf
Damarus <leblanc.jlj@gmail.com>
2022-08-09 21:19:46 -0600

    Fixed pet avatar animation mapping

--- src/map/mobskill.cpp
+++ src/map/mobskill.cpp
@@ -234,6 +234,14 @@ uint16 CMobSkill::getPetAnimationID() const
         return 144;
     if (m_AnimID == 918) // Diabolos Nightmare
         return 146;
+    if (m_AnimID == 1125) // Diabolos Ruinous Omen
+        return 149;
+    if (m_AnimID == 1126) // Diabolos Somnolence
+        return 142;
+    if (m_AnimID == 1127) // Diabolos Dream Shroud
+        return 145;
+    if (m_AnimID == 1129) // Diabolos Nether Blast
+        return 148;
     //  return 147; pet animationID 147 is an unused Diabolos aoe move encircling him in red rings/script
 
     return m_AnimID;
