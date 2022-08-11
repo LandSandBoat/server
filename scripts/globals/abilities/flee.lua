bf54ef70281b94dd9a26885ef6bbbeb6b3787196
Mi <djdeffkid@gmail.com>
2022-08-10 20:16:32 -0400

    [LUA] Update Flee movement speed

--- scripts/globals/abilities/flee.lua
+++ scripts/globals/abilities/flee.lua
@@ -15,7 +15,7 @@ end
 
 ability_object.onUseAbility = function(player, target, ability)
     local duration = 30 + player:getMod(xi.mod.FLEE_DURATION)
-    player:addStatusEffect(xi.effect.FLEE, 100, 0, duration)
+    player:addStatusEffect(xi.effect.FLEE, 60, 0, duration)
 end
 
 return ability_object
