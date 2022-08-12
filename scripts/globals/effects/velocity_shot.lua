Wings-XI/LegacyWingsXI

Update Velocity Shot to correct Values

scripts/globals/effects/velocity_shot.lua
@@ -8,7 +8,7 @@ function onEffectGain(target, effect)
     target:addMod(tpz.mod.ATTP, -15)
     target:addMod(tpz.mod.HASTE_ABILITY, -1500)
     target:addMod(tpz.mod.RATTP, 15)
-    target:addMod(tpz.mod.RANGED_DELAYP, -10)
+    target:addMod(tpz.mod.RANGED_DELAYP, -15)
 end
 
 function onEffectTick(target, effect)
@@ -18,5 +18,5 @@ function onEffectLose(target, effect)
     target:delMod(tpz.mod.ATTP, -15)
     target:delMod(tpz.mod.HASTE_ABILITY, -1500)
     target:delMod(tpz.mod.RATTP, 15)
-    target:delMod(tpz.mod.RANGED_DELAYP, -10)
+    target:delMod(tpz.mod.RANGED_DELAYP, -15)
 end

LandSandBoat/server

scripts/globals/effects/velocity_shot.lua
@@ -13,7 +13,7 @@ effect_object.onEffectGain = function(target, effect)
     target:addMod(xi.mod.ATTP, -15)
     target:addMod(xi.mod.HASTE_ABILITY, -1500)
     target:addMod(xi.mod.RATTP, 15)
-    target:addMod(xi.mod.RANGED_DELAYP, -10)
+    target:addMod(xi.mod.RANGED_DELAYP, -15)
 end
 
 effect_object.onEffectTick = function(target, effect)
@@ -26,7 +26,7 @@ effect_object.onEffectLose = function(target, effect)
     target:delMod(xi.mod.ATTP, -15)
     target:delMod(xi.mod.HASTE_ABILITY, -1500)
     target:delMod(xi.mod.RATTP, 15)
-    target:delMod(xi.mod.RANGED_DELAYP, -10)
+    target:delMod(xi.mod.RANGED_DELAYP, -15)
 end
 
 return effect_object