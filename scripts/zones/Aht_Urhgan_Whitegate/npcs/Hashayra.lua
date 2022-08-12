1063cb8cb0a8c970a8c95d68669ee94066b23c91
Kain <26943220+kaincenteno@users.noreply.github.com>
2022-08-12 09:38:45 -0700

    npc pathing and coordinates
    adds logic for npc pathing along with coordinates and requirement.

--- scripts/zones/Aht_Urhgan_Whitegate/npcs/Hashayra.lua
+++ scripts/zones/Aht_Urhgan_Whitegate/npcs/Hashayra.lua
@@ -3,7 +3,24 @@
 --  NPC: Hashayra
 -- Standard Info NPC
 -----------------------------------
+require("scripts/globals/pathfind")
+-----------------------------------
 local entity = {}
+
+local path =
+{
+    96.832, -6.000, -110.566, -- TODO: stays at point for 1 second then rotates to R: 146 and waits 1 second.
+    86.308, -6.000, -105.927, -- TODO: stays at point for 1 second then rotates to R: 14 and waits 1 second.
+}
+
+entity.onSpawn = function(npc)
+    npc:initNpcAi()
+    npc:setPos(xi.path.first(path))
+end
+
+entity.onPath = function(npc)
+    xi.path.patrol(npc, path)
+end
 
 entity.onTrade = function(player, npc, trade)
 end
