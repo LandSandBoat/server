d8f10e6e1e91615d7944847e73b7da4b45d55032
claywar <claywar@beerlab.dev>
2022-08-20 08:17:35 -0400

    [chore] Zone.lua formatting for Zones A~B

--- scripts/zones/Boneyard_Gully/Zone.lua
+++ scripts/zones/Boneyard_Gully/Zone.lua
@@ -1,10 +1,8 @@
 -----------------------------------
---
 -- Zone: Boneyard_Gully (8)
---
 -----------------------------------
-local ID = require("scripts/zones/Boneyard_Gully/IDs")
-require("scripts/globals/conquest")
+local ID = require('scripts/zones/Boneyard_Gully/IDs')
+require('scripts/globals/conquest')
 -----------------------------------
 local zone_object = {}
 
@@ -13,9 +11,11 @@ end
 
 zone_object.onZoneIn = function(player, prevZone)
     local cs = -1
-    if (player:getXPos() == 0 and player:getYPos() == 0 and player:getZPos() == 0) then
+
+    if player:getXPos() == 0 and player:getYPos() == 0 and player:getZPos() == 0 then
         player:setPos(-709.508, 18.066, 456.241, 24)
     end
+
     return cs
 end
 
