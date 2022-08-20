ff98f0c101184931a5f7cb38b88b0a8e4302b408
claywar <claywar@beerlab.dev>
2022-08-20 09:53:16 -0400

    [chore] Zone.lua formatting for Zones L~N

--- scripts/zones/Monarch_Linn/Zone.lua
+++ scripts/zones/Monarch_Linn/Zone.lua
@@ -1,10 +1,8 @@
 -----------------------------------
---
 -- Zone: Monarch_Linn
---
 -----------------------------------
-local ID = require("scripts/zones/Monarch_Linn/IDs")
-require("scripts/globals/conquest")
+local ID = require('scripts/zones/Monarch_Linn/IDs')
+require('scripts/globals/conquest')
 -----------------------------------
 local zone_object = {}
 
@@ -16,11 +14,11 @@ zone_object.onConquestUpdate = function(zone, updatetype)
 end
 
 zone_object.onZoneIn = function(player, prevZone)
-    if (player:getXPos() == 0 and player:getYPos() == 0 and player:getZPos() == 0) then
+    local cs = -1
+
+    if player:getXPos() == 0 and player:getYPos() == 0 and player:getZPos() == 0 then
         player:setPos(12.527, 0.345, -539.602, 127)
     end
-
-    local cs = -1
 
     return cs
 end
