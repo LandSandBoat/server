cfca0435c28d030c66651bb8a08dbace77082324
Tal Ben-Eliezer <beneliezer@gmail.com>
2022-08-21 22:00:31 -0400

    really fix sparks cap

--- scripts/globals/weaponskills/realmrazer.lua
+++ scripts/globals/weaponskills/realmrazer.lua
@@ -18,6 +18,13 @@ require("scripts/globals/weaponskills")
 local weaponskill_object = {}
 
 weaponskill_object.onUseWeaponSkill = function(player, target, wsID, tp, primary, action, taChar)
+    -- check to see if player has the unlock charvar for this, if not, do no damage.
+    local hasMeritWsUnlock = 1
+	
+    if player:isPC() then
+	    hasMeritWsUnlock = player:getCharVar("hasRealmrazerUnlock")
+	end
+
     if hasMeritWsUnlock ~= 1 then
         player:PrintToPlayer("You don't have this WS unlocked.")
         return
