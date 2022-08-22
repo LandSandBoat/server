874f2746f7ab035f5bf95cca357d4c938e3a3449
dallano <atwood.dallan@hotmail.com>
2022-08-22 10:07:21 -0600

    Shots in the Dark
    Evidence:
    Personal Captures
    https://ffxiclopedia.fandom.com/wiki/Shots_in_the_Dark
    
    Changes:
    - Added 5 points around the battlefield that the onager will flee to when players get too close
    - Before fleeing, the onager will cast bindga on the player that triggered his flee
    - Gave onager a dynamic regain that allows him to TP at a constant rate throughout the fight
    - Fixed the loot tables to not have such high odds of not dropping anything (as is the behavior with every single BCNM)
    - Gave onager silence and bind immunity
    - Gave onager the appropriate mob skill list
    - Added standback behavior to onager so that it doesn't attempt to close in on the player it has hate on when low HP
    - Added a cooldown of 20 seconds to casting bind as this behavior was seen in capture.
    - Added control variables to the dyanamic regain to prevent the computation from happening every tick

---
+++ scripts/zones/Horlais_Peak/mobs/Orcish_Onager.lua
@@ -0,0 +1,69 @@
+-----------------------------------
+-- Area: Horlais Peak
+-- Mob: Orcish Onager
+-- BCNM Fight: Dismemberment Brigade
+-----------------------------------
+require("scripts/globals/pathfind")
+require("scripts/globals/status")
+-----------------------------------
+local entity = {}
+
+local points =
+{
+    {
+        {-396, 94, -65},
+        {-390, 94, -76},
+        {-409, 84, -70},
+        {-383, 94, -59},
+        {-401, 94, -51},
+    },
+    {
+        {-156, -25, 115.5},
+        {-150, -26, 103.3},
+        {-168, -25, 108.8},
+        {-143, -25, 120.5},
+        {-161, -26, 128.6},
+    },
+    {
+        {83.8, -145, 295.5},
+        {89.4, -146, 283.0},
+        {71.6, -146, 288.5},
+        {96.7, -146, 300.6},
+        {78.4, -146, 308.1},
+    },
+}
+
+entity.onMobSpawn = function(mob)
+    mob:setBehaviour(bit.bor(mob:getBehaviour(), xi.behavior.STANDBACK))
+    mob:setMod(xi.mod.REGAIN, 1000)
+    mob:setSpeed(90)
+end
+
+entity.onMobFight = function(mob)
+    local party = mob:getBattlefield():getPlayers()
+    local bfNum = mob:getBattlefield():getArea()
+
+    for _, member in pairs(party) do
+        if mob:checkDistance(member) < 7.0 and mob:getLocalVar("fleeTimer") < os.time() then
+            local dest = math.random(1,5)
+
+            mob:castSpell(362, member)
+            mob:pathTo(points[bfNum][dest][1], points[bfNum][dest][2], points[bfNum][dest][3], xi.path.flag.SCRIPT)
+            mob:setLocalVar("fleeTimer", os.time() + 20)
+        end
+    end
+
+    if mob:getHPP() <= 50 and mob:getLocalVar("hpControl2") == 0 then
+        mob:setLocalVar("hpControl2", 1)
+        mob:setMod(xi.mod.REGAIN, 667)
+
+    elseif mob:getHPP() <= 25 and mob:getLocalVar("hpControl3") == 0 then
+        mob:setLocalVar("hpControl3", 1)
+        mob:setMod(xi.mod.REGAIN, 334)
+    end
+end
+
+entity.onMobDeath = function(mob, player, isKiller)
+end
+
+return entity
