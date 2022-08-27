356260113ec48c0b471342f517b8ad62729755f2
Zach Toogood <zrtoogood@gmail.com>
2022-08-27 19:15:19 +0300

    Remove <space> text ID token

--- scripts/zones/Promyvion-Dem/IDs.lua
+++ scripts/zones/Promyvion-Dem/IDs.lua
@@ -17,7 +17,7 @@ zones[xi.zone.PROMYVION_DEM] =
         KEYITEM_OBTAINED        = 6393, -- Obtained key item: <keyitem>.
         NOTHING_OUT_OF_ORDINARY = 6404, -- There is nothing out of the ordinary here.
         CARRIED_OVER_POINTS     = 7001, -- You have carried over <number> login point[/s].
-        LOGIN_CAMPAIGN_UNDERWAY = 7002, -- The [/January/February/March/April/May/June/July/August/September/October/November/December] <number> Login Campaign is currently underway!<space>
+        LOGIN_CAMPAIGN_UNDERWAY = 7002, -- The [/January/February/March/April/May/June/July/August/September/October/November/December] <number> Login Campaign is currently underway!
         LOGIN_NUMBER            = 7003, -- In celebration of your most recent login (login no. <number>), we have provided you with <number> points! You currently have a total of <number> points.
         BARRIER_WOVEN           = 7219, -- It appears to be a barrier woven from the energy of overflowing memories...
     },
