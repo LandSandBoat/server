3a50d9a4f2ce43d1c5f847c805ea333d2ee7a214
DiscipleOfEris <discipleoferis1@gmail.com>
2022-08-26 22:53:13 -0700

    Clean redundant function call

--- src/map/utils/mobutils.cpp
+++ src/map/utils/mobutils.cpp
@@ -873,7 +873,6 @@ namespace mobutils
         PMob->m_maxRoamDistance = 0.5f; // always go back to spawn
 
         PMob->setMobMod(MOBMOD_NO_DESPAWN, 1);
-        PMob->setMobMod(MOBMOD_ROAM_RESET_FACING, 1);
     }
 
     void SetupNMMob(CMobEntity* PMob)
