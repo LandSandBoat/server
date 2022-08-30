9889de0a1a6cbd67105303827a5cf163d0f4c17f
Daywhite83 <110785272+Daywhite83@users.noreply.github.com>
2022-08-30 10:27:44 -0400

    Create capacity_ring.lua

---
+++ scripts/globals/items/capacity_ring.lua
@@ -0,0 +1,26 @@
+-----------------------------------
+-- ID: 28546
+-- Item: capacity ring
+-- Capacity point bonus
+-----------------------------------
+-- Bonus: +50%
+-- Duration: 720 min
+-- Max bonus: 30000 cp
+-----------------------------------
+require("scripts/globals/status")
+-----------------------------------
+local item_object = {}
+
+item_object.onItemCheck = function(target)
+    local result = 0
+    if target:hasStatusEffect(xi.effect.COMMITMENT) then
+        result = 56
+    end
+    return result
+end
+
+item_object.onItemUse = function(target)
+   target:addStatusEffect(xi.effect.COMMITMENT, 50, 0, 43200, 0, 30000)
+end
+
+return item_object
