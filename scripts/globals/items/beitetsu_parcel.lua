3a55b863f824e7d63a5efa3c12f7bde8b9dc8711
aliron <42409277+apatrickm@users.noreply.github.com>
2022-09-08 15:17:35 -0400

    Update beitetsu_parcel.lua
    Changed item ID referance to xi.items.BEITETSU

--- scripts/globals/items/beitetsu_parcel.lua
+++ scripts/globals/items/beitetsu_parcel.lua
@@ -8,7 +8,7 @@ require("scripts/globals/msg")
 local item_object = {}
 
 item_object.onItemCheck = function(target)
-    local result = xi.items.BEITETSU
+    local result = 0
     if target:getFreeSlotsCount() == 0 then
         result = xi.msg.basic.ITEM_NO_USE_INVENTORY
     end
@@ -16,7 +16,7 @@ item_object.onItemCheck = function(target)
 end
 
 item_object.onItemUse = function(target)
-    target:addItem(4060, math.random(3, 15))
+    target:addItem(xi.items.BEITETSU, math.random(3, 15))
 end
 
 return item_object
