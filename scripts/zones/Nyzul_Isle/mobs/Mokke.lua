b32f8006d3b234fc31fa6ab36b9c28308f4eb59e
claywar <claywar@beerlab.dev>
2022-09-08 12:08:28 -0400

    [nyzul] Add mob scripts M~P

---
+++ scripts/zones/Nyzul_Isle/mobs/Mokke.lua
@@ -0,0 +1,34 @@
+-----------------------------------
+--  MOB: Mokke
+-- Area: Nyzul Isle
+-- Info: Enemy Leader, Only uses Abrasive Tantara
+-----------------------------------
+require('scripts/globals/status')
+require('scripts/globals/nyzul')
+-----------------------------------
+local entity = {}
+
+entity.onMobSpawn = function(mob)
+    mob:addListener("CRITICAL_TAKE", "IMP_CRITICAL_TAKE", function(impMob)
+        if math.random(100) <= 20 and impMob:getAnimationSub() == 4 then
+            impMob:setAnimationSub(5)
+            -- Reacquire horn after 5 to 60 seconds
+            impMob:timer(math.random(5000, 60000), function(hornLessMob)
+                if hornLessMob:isAlive() then
+                    hornLessMob:setAnimationSub(4)
+                end
+            end)
+        end
+    end)
+    mob:setMod(xi.mod.MOVE, 64)
+    mob:setAnimationSub(4)
+end
+
+entity.onMobDeath = function(mob, player, isKiller, noKiller)
+    if isKiller or noKiller then
+        xi.nyzul.spawnChest(mob, player)
+        xi.nyzul.enemyLeaderKill(mob)
+    end
+end
+
+return entity
---
+++ scripts/zones/Nyzul_Isle/mobs/Pelican.lua
@@ -0,0 +1,27 @@
+-----------------------------------
+--  MOB: Pelican
+-- Area: Nyzul Isle
+-- Info: NM
+-----------------------------------
+require('scripts/globals/nyzul')
+require('scripts/globals/additional_effects')
+require('scripts/globals/status')
+-----------------------------------
+local entity = {}
+
+entity.onMobInitialize = function(mob)
+    mob:setMobMod(xi.mobMod.ADD_EFFECT, 1)
+ end
+
+entity.onAdditionalEffect = function(mob, target, damage)
+    return xi.mob.onAddEffect(mob, target, 0, xi.mob.ae.PETRIFY)
+end
+
+entity.onMobDeath = function(mob, player, isKiller, noKiller)
+    if isKiller or noKiller then
+        xi.nyzul.spawnChest(mob, player)
+        xi.nyzul.eliminateAllKill(mob)
+    end
+end
+
+return entity
