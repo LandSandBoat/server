8c5497f266dee0867172cbfa2e8bfde9670b97c9
dallano <atwood.dallan@hotmail.com>
2022-09-10 20:39:55 -0600

    Finishing up a question of faith

--- scripts/quests/bastok/A_Question_of_Faith.lua
+++ scripts/quests/bastok/A_Question_of_Faith.lua
@@ -14,6 +14,8 @@ require('scripts/globals/titles')
 require('scripts/globals/zone')
 require('scripts/globals/interaction/quest')
 -----------------------------------
+ID = require('scripts/zones/Bastok_Markets/IDs')
+-----------------------------------
 local quest = Quest:new(xi.quest.log_id.BASTOK, xi.quest.id.bastok.A_QUESTION_OF_FAITH)
 
 quest.reward =
@@ -27,7 +29,7 @@ quest.sections =
         check = function(player, status, vars)
             return status == QUEST_AVAILABLE and
             player:getFameLevel(xi.quest.fame_area.BASTOK) >= 4 and
-            player:hasCompletedQuest(xi.quest.id.bastok.OUT_OF_THE_DEPTHS)
+            player:hasCompletedQuest(xi.quest.log_id.BASTOK, xi.quest.id.bastok.OUT_OF_THE_DEPTHS)
         end,
 
         [xi.zone.METALWORKS] =
@@ -71,10 +73,8 @@ quest.sections =
                 end,
 
                 [241] = function(player, csid, option, npc)
-                    if option == 0 then
                         player:addFame(xi.quest.fame_area.BASTOK, 50)
                         quest:complete(player)
-                    end
                 end,
             },
         },
@@ -84,12 +84,14 @@ quest.sections =
             ['Rakorok'] =
             {
                 onTrigger = function(player, npc)
-                    if player:hasKeyItem(xi.ki.DAWN_TALISMAN) and quest.getVar(player, 'Prog') == 0 then
+                    if player:hasKeyItem(xi.ki.DAWN_TALISMAN) and quest:getVar(player, 'Prog') == 0 then
                         npc:messageText(npc, 7733)
                         npc:messageText(npc, 7746)
                         SpawnMob(16822456):updateClaim(player)
-                    elseif not GetMobByID(16822456):isAlive() then
-                        return quest:progressEvent(6, 1)
+                        quest:setVar(player, 'Prog', 1)
+
+                    elseif quest:getVar(player, 'Prog') == 1 and player:hasKeyItem(xi.ki.DAWN_TALISMAN) then
+                        return quest:progressEvent(6)
                     end
                 end,
             },
@@ -97,10 +99,8 @@ quest.sections =
             onEventFinish =
             {
                 [6] = function(player, csid, option, npc)
-                    if option == 1 then
-                        player:delKeyItem(xi.ki.DAWN_TALISMAN)
-                        quest:setVar(player, 'Prog', 1)
-                    end
+                    player:delKeyItem(xi.ki.DAWN_TALISMAN)
+                    player:messageSpecial(ID.text.KEYITEM_LOST, xi.dawn.DAWN_TALISMAN)
                 end,
             },
         },
