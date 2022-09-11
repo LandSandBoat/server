3031072be5847a981abeaa3c8c87223e731d55f2
dallano <atwood.dallan@hotmail.com>
2022-09-11 16:34:07 -0600

    Let sleeping dogs die implementation
    Implemented Let Sleeping Dogs Die
    Captures:
    https://youtu.be/QqKrxYEZuoc
    https://www.dropbox.com/s/9oyev0caypyg4gr/BCNM%2830%29%20Let%20Sleeping%20Dogs%20Die%20%5BL%5D.log?dl=0
    
    Changes:
    - Dogs respawn in 35 seconds if any other dog is still alive
    - Dogs all despawn when all are dead
    - Dogs remain spawned in after dying until respawned

--- scripts/zones/QuBia_Arena/mobs/Guytrash.lua
+++ scripts/zones/QuBia_Arena/mobs/Guytrash.lua
@@ -3,7 +3,36 @@
 --  Mob: Guytrash
 -- BCNM: Let Sleeping Dogs Die
 -----------------------------------
+local ID = require("scripts/zones/QuBia_Arena/IDs")
+-----------------------------------
 local entity = {}
+
+local checkHounds = function(mob)
+    for i = 0, 4 do
+        if GetMobByID(ID.hounds[mob:getBattlefield():getArea()]+i):isAlive() then
+            return true
+        end
+    end
+    return false
+end
+
+entity.onMobSpawn = function(houndMob)
+    houndMob:setBehaviour(bit.bor(houndMob:getBehaviour(), xi.behavior.NO_DESPAWN))
+
+    houndMob:addListener("DEATH", "GUYTRASH_DEATH", function(mob, killer)
+        if checkHounds(mob) then
+            mob:timer(35000, function(mobArg)
+                mobArg:setHP(mobArg:getMaxHP())
+                mobArg:resetAI()
+                mobArg:updateEnmity(killer)
+            end)
+        else
+            for i = 0, 4 do
+                GetMobByID(ID.hounds[mob:getBattlefield():getArea()]+i):setBehaviour(0)
+            end
+        end
+    end)
+end
 
 entity.onMobDeath = function(mob, player, isKiller)
 end
