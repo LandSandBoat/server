d7b1e30a8bdfaff24be993b35d6611d6915278e0
Daywhite83 <110785272+Daywhite83@users.noreply.github.com>
2022-09-11 20:24:24 -0400

    Create Auction_Counter.lua
    Adds auction function to Eastern Adoulin

---
+++ scripts/zones/Eastern_Adoulin/npcs/Auction_Counter.lua
@@ -0,0 +1,23 @@
+-----------------------------------
+-- Area: Eastern Adoulin
+--  NPC: Auction Counter
+-----------------------------------
+require("scripts/quests/tutorial")
+-----------------------------------
+local entity = {}
+
+entity.onTrade = function(player, npc, trade)
+end
+
+entity.onTrigger = function(player, npc)
+    xi.tutorial.onAuctionTrigger(player)
+    player:sendMenu(3)
+end
+
+entity.onEventUpdate = function(player, csid, option)
+end
+
+entity.onEventFinish = function(player, csid, option)
+end
+
+return entity
