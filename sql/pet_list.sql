e084e1172e16e6f0ad4c773d35fc3a89cd2a9ec5
WinterSolstice8 <60417494+WinterSolstice8@users.noreply.github.com>
2022-09-19 23:19:10 -0700

    [sql] Correct pet Siren mob pool

--- sql/pet_list.sql
+++ sql/pet_list.sql
@@ -101,4 +101,4 @@ INSERT INTO `pet_list` VALUES (72, 'StormwakerFrame', 5127, 1, 99, 0, 0);
 -- INSERT INTO `pet_list` VALUES (73, 'AdventuringFellow', 0, 1, 99, 0, 0);
 -- 74 is Chocobo in the enum..
 INSERT INTO `pet_list` VALUES (75, 'Luopan', 6040, 1, 99, 0, 0);
-INSERT INTO `pet_list` VALUES (76, 'Siren', 7041, 1, 99, 0, 0);
+INSERT INTO `pet_list` VALUES (76, 'Siren', 7047, 1, 99, 0, 0);
