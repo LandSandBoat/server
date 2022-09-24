f675a029f95783b0873d937ccf7eabad7598fac6
aliron <42409277+apatrickm@users.noreply.github.com>
2022-09-23 22:28:06 -0400

    Update scroll_of_inundation.lua
    corrected magic number

--- scroll_of_inundation.lua
+++ scroll_of_inundation.lua
@@ -6,11 +6,11 @@
 local item_object = {}
 
 item_object.onItemCheck = function(target)
-    return target:canLearnSpell(879)
+    return target:canLearnSpell(xi.magic.spell.INUNDATION)
 end
 
 item_object.onItemUse = function(target)
-    target:addSpell(879)
+    target:addSpell(xi.magic.spell.INUNDATION)
 end
 
 return item_object
