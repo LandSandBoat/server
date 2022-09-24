9f4b33624ada7349b0f83d19bab94029385a80f2
Abdiah <62350957+dallano@users.noreply.github.com>
2022-09-24 15:45:33 -0600

    Mood for Love Implementation (#413)
    * Mood for Love Implementation
    
    Implements the quest: "In the Mood for Love"
    
    * Update In_the_Mood_for_Love.lua
    
    * Update In_the_Mood_for_Love.lua

--- scripts/globals/items.lua
+++ scripts/globals/items.lua
@@ -587,6 +587,7 @@ xi.items =
     CORAL_CREST_KEY                 = 1659,
     BRONZE_KEY                      = 1660,
     CATHEDRAL_TAPESTRY              = 1662,
+    CHAMELEON_DIAMOND               = 1666,
     CLEAN_SNAP_ROD                  = 1668,
     INGOT_OF_ROYAL_TREASURY_GOLD    = 1682,
     PIECE_OF_ATTOHWA_GINSENG        = 1683,
