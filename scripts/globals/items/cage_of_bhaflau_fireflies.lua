f37bd89e49d30d368317779e2c8b3c16003c1917
David Norris <Knowone134@gmail.com>
2022-09-25 19:50:39 -0500

    replenish dropped fireflies

--- scripts/globals/items/cage_of_bhaflau_fireflies.lua
+++ scripts/globals/items/cage_of_bhaflau_fireflies.lua
@@ -3,8 +3,10 @@
 -- Bhaflau Fireflies
 -- Transports the user to Mamool Ja Staging Point
 -----------------------------------
-require("scripts/globals/teleports")
+require("scripts/globals/items")
+require("scripts/globals/msg")
 require("scripts/globals/status")
+require("scripts/globals/teleports")
 require("scripts/globals/zone")
 -----------------------------------
 local item_object = {}
@@ -13,11 +15,15 @@ item_object.onItemCheck = function(target)
     if target:getZoneID() == xi.zone.MAMOOL_JA_TRAINING_GROUNDS then
         return 0
     end
-    return 56
+    return xi.msg.basic.ITEM_UNABLE_TO_USE_2
 end
 
 item_object.onItemUse = function(target)
     target:addStatusEffectEx(xi.effect.TELEPORT, 0, xi.teleport.id.BHAFLAU, 0, 1)
+end
+
+item_object.onItemDrop = function(target, item)
+   target:addTempItem(xi.items.CAGE_OF_BHAFLAU_FIREFLIES)
 end
 
 return item_object
--- scripts/globals/items/cage_of_zhayolm_fireflies.lua
+++ scripts/globals/items/cage_of_zhayolm_fireflies.lua
@@ -2,6 +2,8 @@
 -- ID: 5345
 -- Item: Zhayolm Fireflies
 -----------------------------------
+require("scripts/globals/items")
+require("scripts/globals/msg")
 require("scripts/globals/status")
 require("scripts/globals/teleports")
 require("scripts/globals/zone")
@@ -12,11 +14,15 @@ item_object.onItemCheck = function(target)
     if target:getZoneID() == xi.zone.LEBROS_CAVERN then
         return 0
     end
-    return 56
+    return xi.msg.basic.ITEM_UNABLE_TO_USE_2
 end
 
 item_object.onItemUse = function(target)
     target:addStatusEffectEx(xi.effect.TELEPORT, 0, xi.teleport.id.ZHAYOLM, 0, 1)
+end
+
+item_object.onItemDrop = function(target, item)
+   target:addTempItem(xi.items.CAGE_OF_ZHAYOLM_FIREFLIES)
 end
 
 return item_object
