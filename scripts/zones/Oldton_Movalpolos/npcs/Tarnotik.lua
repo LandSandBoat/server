1e0f1a7e7f94108b00d69d617144af3456b665c3
Abdiah <62350957+dallano@users.noreply.github.com>
2022-09-28 11:18:11 -0600

    Tarnotik fixes
    Added the ability for players to trade ahriman tears to tarnotik to warp to mineshaft after completing the quest: Return to the Depths

--- scripts/zones/Oldton_Movalpolos/npcs/Tarnotik.lua
+++ scripts/zones/Oldton_Movalpolos/npcs/Tarnotik.lua
@@ -10,7 +10,7 @@ require("scripts/globals/npc_util")
 local entity = {}
 
 entity.onTrade = function(player, npc, trade)
-    if player:getCurrentMission(xi.mission.log_id.COP) >= xi.mission.id.cop.THREE_PATHS and npcUtil.tradeHas(trade, 1725) then
+    if player:getCurrentMission(xi.mission.log_id.COP) >= xi.mission.id.cop.THREE_PATHS and npcUtil.tradeHas(trade, xi.items.SNOW_LILY) then
         if player:hasKeyItem(xi.ki.SHAFT_2716_OPERATING_LEVER) then
             player:startEvent(54)
         else
@@ -35,7 +35,7 @@ entity.onEventFinish = function(player, csid, option)
         player:confirmTrade()
         xi.teleport.to(player, xi.teleport.id.MINESHAFT)
     elseif csid == 54 and option == 1 then
-        player:delKeyItem(xi.item.SHAFT_2716_OPERATING_LEVER)
+        player:delKeyItem(xi.ki.SHAFT_2716_OPERATING_LEVER)
     end
 end
 
