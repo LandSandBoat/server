5e577393af178e05c7a52a6c1bff7b8825f8f504
Zach Toogood <zrtoogood@gmail.com>
2022-09-30 19:33:16 +0300

    Add xi.itemFlag enum

--- scripts/globals/status.lua
+++ scripts/globals/status.lua
@@ -3047,3 +3047,25 @@ xi.pathflag =
     SCRIPT   = 0x08, -- don't overwrite this path before completion (except via another script)
     SLIDE    = 0x10,  -- Slide to end point if close enough (so no over shoot)
 };
+
+-- Check Lua item with:
+-- local isEx = bit.band(item:getFlag(), xi.itemFlag.EX) ~= 0
+xi.itemFlag =
+{
+    WALLHANGING  = 0x0001,
+    -- 01          = 0x0002,
+    MYSTERY_BOX  = 0x0004, -- Can be gained from Gobbie Mystery Box
+    MOG_GARDEN   = 0x0008, -- Can use in Mog Garden
+    MAIL2ACCOUNT = 0x0010, -- CanSendPOL Polutils Value
+    INSCRIBABLE  = 0x0020,
+    NOAUCTION    = 0x0040,
+    SCROLL       = 0x0080,
+    LINKSHELL    = 0x0100, -- Linkshell Polutils Value
+    CANUSE       = 0x0200,
+    CANTRADENPC  = 0x0400,
+    CANEQUIP     = 0x0800,
+    NOSALE       = 0x1000,
+    NODELIVERY   = 0x2000,
+    EX           = 0x4000, -- NoTradePC Polutils Value
+    RARE         = 0x8000,
+}
