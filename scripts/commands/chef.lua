fe1b2e9e1090429314f9cfd931e6135af8ee996e
Tal Ben-Eliezer <beneliezer@gmail.com>
2022-09-30 22:02:38 -0400

    disable tells to hidden gms command to despawn non-persistent nms re-doing chef menu fix homepoint refill hp/mp remove cosmo-cleanse cooldown.

--- scripts/commands/chef.lua
+++ scripts/commands/chef.lua
@@ -6,21 +6,14 @@
 cmdprops =
 {
     permission = 0,
-    parameters = "iiii"
+    parameters = "i"
 };
 
 function onTrigger(player,npc)
-player:PrintToPlayer("Welcome to the one-stop Chef Shop!")
-stock =
-{
- 4421, 2400, -- Melon Pie
- 4488, 3000, -- Jaco
- 4558, 4000, -- Yagudo
- 5149, 6000, -- SoleSushi
- 4381, 6000, -- MeatKabob
- 5174, 5000, -- Tav-Taco
- 6464, 5000, -- BeheSteak
- 4153, 8000, -- Antacid
-} 
-xi.shop.general(player, stock)
+    player:PrintToPlayer("Welcome to the one-stop Chef Shop!")
+    stock =
+    {
+        4153, 8000, -- Antacid
+    } 
+    xi.shop.general(player, stock)
 end
