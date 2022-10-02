7adff5077ca371c2e9bbcb01cbfae2c562e530e4
claywar <claywar@beerlab.dev>
2022-10-02 08:56:07 -0400

    Interaction - Quest: The First Meeting
    Fix CI warnings (trailing whitespace)
    
    
    Missing require
    
    
    Fix syntax

--- scripts/zones/Port_Bastok/npcs/Oggbi.lua
+++ scripts/zones/Port_Bastok/npcs/Oggbi.lua
@@ -1,14 +1,7 @@
 -----------------------------------
 -- Area: Port Bastok (236)
 --  NPC: Oggbi
--- Starts and Finishes: Ghosts of the Past, The First Meeting, The Walls of Your Mind
 -- !pos -159 -7 5 236
------------------------------------
-local ID = require("scripts/zones/Port_Bastok/IDs")
-require("scripts/globals/keyitems")
-require("scripts/globals/settings")
-require("scripts/globals/quests")
-require("scripts/globals/status")
 -----------------------------------
 local entity = {}
 
@@ -16,33 +9,9 @@ entity.onTrade = function(player, npc, trade)
 end
 
 entity.onTrigger = function(player, npc)
-    local ghostsOfThePast = player:getQuestStatus(xi.quest.log_id.BASTOK, xi.quest.id.bastok.GHOSTS_OF_THE_PAST)
-    local theFirstMeeting = player:getQuestStatus(xi.quest.log_id.BASTOK, xi.quest.id.bastok.THE_FIRST_MEETING)
-    local mLvl = player:getMainLvl()
-    local mJob = player:getMainJob()
-
-    if (ghostsOfThePast == QUEST_COMPLETED and player:needToZone() == false and theFirstMeeting == QUEST_AVAILABLE and mJob == xi.job.MNK and mLvl >= 50) then
-        player:startEvent(233) -- Start Quest "The First Meeting"
-    elseif (player:hasKeyItem(xi.ki.LETTER_FROM_DALZAKK) and player:hasKeyItem(xi.ki.SAN_DORIAN_MARTIAL_ARTS_SCROLL)) then
-        player:startEvent(234) -- Finish Quest "The First Meeting"
-    end
 end
 
 entity.onEventFinish = function(player, csid, option)
-    if (csid == 233) then
-        player:addQuest(xi.quest.log_id.BASTOK, xi.quest.id.bastok.THE_FIRST_MEETING)
-    elseif (csid == 234) then
-        if (player:getFreeSlotsCount() == 0) then
-            player:messageSpecial(ID.text.ITEM_CANNOT_BE_OBTAINED, 14090) -- Temple Gaiters
-        else
-            player:delKeyItem(xi.ki.LETTER_FROM_DALZAKK)
-            player:delKeyItem(xi.ki.SAN_DORIAN_MARTIAL_ARTS_SCROLL)
-            player:addItem(14090)
-            player:messageSpecial(ID.text.ITEM_OBTAINED, 14090) -- Temple Gaiters
-            player:addFame(xi.quest.fame_area.BASTOK, 40)
-            player:completeQuest(xi.quest.log_id.BASTOK, xi.quest.id.bastok.THE_FIRST_MEETING)
-        end
-    end
 end
 
 return entity
