26783a5ea5504bb694a337ea81dadcbba6160628
Zach Toogood <zrtoogood@gmail.com>
2022-10-03 11:53:37 +0300

    [packets] Add setting main.EQUIP_FROM_OTHER_CONTAINERS
    Flattens the logic inside SmallPacket0x050
    
    Adds a setting to enable/disable to behaviour defined in:
    https://git.ashitaxi.com/Plugins/AshitaCast/commit/8fcdc3c8aa6ca09eb34b04d7002bb6bae950467f

--- settings/default/main.lua
+++ settings/default/main.lua
@@ -214,18 +214,19 @@ xi.settings.main =
     GARRISON_RANK                = 2,     -- Set to minumum Nation Rank to start Garrison (default: 2).
 
     -- MISC
-    RIVERNE_PORTERS              = 120,  -- Time in seconds that Unstable Displacements in Cape Riverne stay open after trading a scale.
-    LANTERNS_STAY_LIT            = 1200, -- time in seconds that lanterns in the Den of Rancor stay lit.
-    ENABLE_COP_ZONE_CAP          = 0,    -- Enable or disable lvl cap
-    ALLOW_MULTIPLE_EXP_RINGS     = 0,    -- Set to 1 to remove ownership restrictions on the Chariot/Empress/Emperor Band trio.
-    BYPASS_EXP_RING_ONE_PER_WEEK = 0,    -- Set to 1 to bypass the limit of one ring per Conquest Tally Week.
-    NUMBER_OF_DM_EARRINGS        = 1,    -- Number of earrings players can simultaneously own from Divine Might before scripts start blocking them (Default: 1)
-    HOMEPOINT_TELEPORT           = 1,    -- Enables the homepoint teleport system
-    DIG_ABUNDANCE_BONUS          = 0,    -- Increase chance of digging up an item (450  = item digup chance +45)
-    DIG_FATIGUE                  = 1,    -- Set to 0 to disable Dig Fatigue
-    DIG_GRANT_BURROW             = 0,    -- Set to 1 to grant burrow ability
-    DIG_GRANT_BORE               = 0,    -- Set to 1 to grant bore ability
-    ENM_COOLDOWN                 = 120,  -- Number of hours before a player can obtain same KI for ENMs (default: 5 days)
-    FORCE_SPAWN_QM_RESET_TIME    = 300,  -- Number of seconds the ??? remains hidden for after the despawning of the mob it force spawns.
-    GOBBIE_BOX_MIN_AGE           = 45,   -- Minimum character age in days before a character can sign up for Gobbie Mystery Box
+    RIVERNE_PORTERS              = 120,   -- Time in seconds that Unstable Displacements in Cape Riverne stay open after trading a scale.
+    LANTERNS_STAY_LIT            = 1200,  -- time in seconds that lanterns in the Den of Rancor stay lit.
+    ENABLE_COP_ZONE_CAP          = 0,     -- Enable or disable lvl cap
+    ALLOW_MULTIPLE_EXP_RINGS     = 0,     -- Set to 1 to remove ownership restrictions on the Chariot/Empress/Emperor Band trio.
+    BYPASS_EXP_RING_ONE_PER_WEEK = 0,     -- Set to 1 to bypass the limit of one ring per Conquest Tally Week.
+    NUMBER_OF_DM_EARRINGS        = 1,     -- Number of earrings players can simultaneously own from Divine Might before scripts start blocking them (Default: 1)
+    HOMEPOINT_TELEPORT           = 1,     -- Enables the homepoint teleport system
+    DIG_ABUNDANCE_BONUS          = 0,     -- Increase chance of digging up an item (450  = item digup chance +45)
+    DIG_FATIGUE                  = 1,     -- Set to 0 to disable Dig Fatigue
+    DIG_GRANT_BURROW             = 0,     -- Set to 1 to grant burrow ability
+    DIG_GRANT_BORE               = 0,     -- Set to 1 to grant bore ability
+    ENM_COOLDOWN                 = 120,   -- Number of hours before a player can obtain same KI for ENMs (default: 5 days)
+    FORCE_SPAWN_QM_RESET_TIME    = 300,   -- Number of seconds the ??? remains hidden for after the despawning of the mob it force spawns.
+    GOBBIE_BOX_MIN_AGE           = 45,    -- Minimum character age in days before a character can sign up for Gobbie Mystery Box
+    EQUIP_FROM_OTHER_CONTAINERS  = false, -- true/false. Allows equipping items from Mog Satchel, Sack, and Case. Only possible with the use of client addons.
 }
