70ae423c37a4d4936639e3d3f6a6fcb84e0d4074
Tal Ben-Eliezer <beneliezer@gmail.com>
2022-10-19 12:01:05 -0400

    Apply AVATAR_DMG_MOD to merit BP's (more info coming players)

--- scripts/globals/abilities/pets/geocrush.lua
+++ scripts/globals/abilities/pets/geocrush.lua
@@ -33,7 +33,8 @@ abilityObject.onPetAbility = function(target, pet, skill)
     damage = xi.mobskills.mobMagicalMove(pet, target, skill, damage, xi.magic.ele.EARTH, 1, xi.mobskills.magicalTpBonus.NO_EFFECT, 0)
     damage = xi.mobskills.mobAddBonuses(pet, target, damage.dmg, xi.magic.ele.EARTH)
     damage = xi.summon.avatarFinalAdjustments(damage, pet, skill, target, xi.attackType.MAGICAL, xi.damageType.EARTH, 1)
-
+    damage = damage * xi.settings.main.AVATAR_DAMAGE_MOD
+	
     target:takeDamage(damage, pet, xi.attackType.MAGICAL, xi.damageType.EARTH)
     target:updateEnmityFromDamage(pet, damage)
 
