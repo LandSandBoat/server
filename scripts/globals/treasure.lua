08b1ad9f70484f490ade54100d472c6fd83d06e4
Hatberg <c.hatberg@gmail.com>
2022-10-27 22:00:23 +0200

    Add quest enum and additional formatting
    Add quest enum and additional formatting

--- scripts/globals/treasure.lua
+++ scripts/globals/treasure.lua
@@ -624,9 +624,10 @@ xi.treasure.treasureInfo =
                 {
                     {
                         test = function(player)
-                            return xi.quest.getVar(player, xi.quest.log_id.SANDORIA, xi.quest.id.sandoria.ENVELOPED_IN_DARKNESS, 'Prog') >= 2
-                            and player:hasKeyItem(xi.ki.OLD_BOOTS)
-                            and not player:hasKeyItem(xi.ki.CRAWLER_BLOOD)
+                            return player:getQuestStatus(xi.quest.log_id.SANDORIA, xi.quest.id.sandoria.ENVELOPED_IN_DARKNESS) == QUEST_ACCEPTED and
+                                xi.quest.getVar(player, xi.quest.log_id.SANDORIA, xi.quest.id.sandoria.ENVELOPED_IN_DARKNESS, 'Prog') >= 2 and
+                                player:hasKeyItem(xi.ki.OLD_BOOTS) and
+                                not player:hasKeyItem(xi.ki.CRAWLER_BLOOD)
                         end,
                         code = function(player) npcUtil.giveKeyItem(player, xi.ki.CRAWLER_BLOOD) end,
                     },
