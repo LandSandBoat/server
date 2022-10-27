064571acc3f654565b16e39b5ad5d74a900a874f
Zach Toogood <zrtoogood@gmail.com>
2022-10-27 13:42:15 +0100

    [login] Begin login rewrite using asio

---
+++ src/login/main.cpp
@@ -0,0 +1,37 @@
+﻿/*
+===========================================================================
+
+Copyright (c) 2022 LandSandBoat Dev Teams
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see http://www.gnu.org/licenses/
+
+===========================================================================
+*/
+#include "connect_server.h"
+
+// TODO: Standardize our running arguments for shutdown and thread signals
+std::atomic<bool> gRunFlag = true;
+
+int main(int argc, char** argv)
+{
+    auto pConnectServer = std::make_unique<ConnectServer>(argc, argv);
+
+    // TODO: We don't need this
+    while (pConnectServer->IsRunning())
+    {
+        pConnectServer->Tick();
+    }
+
+    return 0;
+}
