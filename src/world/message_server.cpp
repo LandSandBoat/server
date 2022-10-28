1b7a06698d0d06620da7cb76a7c05c182bb8a806
Zach Toogood <zrtoogood@gmail.com>
2022-10-28 21:29:21 +0100

    start session tracking

--- src/world/message_server.cpp
+++ src/world/message_server.cpp
@@ -252,6 +252,8 @@ void message_server_init(const bool& requestExit)
 {
     TracySetThreadName("Message Server (ZMQ)");
 
+    ShowInfo("Starting ZMQ");
+
     zmqSql = std::make_unique<SqlConnection>();
 
     zContext = zmq::context_t(1);
@@ -280,6 +282,8 @@ void message_server_init(const bool& requestExit)
 
 void message_server_close()
 {
+    ShowInfo("Closing ZMQ");
+
     if (zSocket)
     {
         zSocket->close();
