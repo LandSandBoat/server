e6dfc015020ccca2d283abd6eb0f7cf63f06b50f
hooksta4 <lance.hooker@gmail.com>
2022-11-04 11:27:56 -0400

    Removes functions for Promy Holla and Mea bosses to use specific WS at HPP
    Based on the following:
    https://www.youtube.com/watch?v=XF3uhxQoVVQ
    https://www.youtube.com/watch?v=XF3uhxQoVVQ
    the NM's use the abilities above 25/35% respectively.

--- scripts/zones/Spire_of_Holla/mobs/Wreaker.lua
+++ scripts/zones/Spire_of_Holla/mobs/Wreaker.lua
@@ -12,17 +12,6 @@ entity.onMobInitialize = function(mob)
     mob:setMod(xi.mod.STORETP, 100)
 end
 
-entity.onMobWeaponSkillPrepare = function(mob, target)
-    local roll = math.random()
-    if mob:getHPP() <= 25 then
-        if roll <= 0.25 then
-            return 1252 -- Shadow_spread
-        else
-            return 1248 -- Trinary_absorption
-        end
-    end
-end
-
 entity.onMobFight = function(mob, target)
     if mob:getHPP() <= 35 then
         mob:setMod(xi.mod.STORETP, 250)
--- scripts/zones/Spire_of_Mea/mobs/Delver.lua
+++ scripts/zones/Spire_of_Mea/mobs/Delver.lua
@@ -12,17 +12,6 @@ entity.onMobInitialize = function(mob)
     mob:setMod(xi.mod.STORETP, 100)
 end
 
-entity.onMobWeaponSkillPrepare = function(mob, target)
-    local roll = math.random()
-    if mob:getHPP() <= 35 then
-        if roll <= 0.7 then
-            return 1234 -- Carousel
-        else
-            return 1274 -- Impalement
-        end
-    end
-end
-
 entity.onMobFight = function(mob, target)
     if mob:getHPP() <= 35 then
         mob:setMod(xi.mod.STORETP, 250)
