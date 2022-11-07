f2ecba830225850f66cb858f0d22df9c1b7edcff
Frankie-hz <105882754+Frankie-hz@users.noreply.github.com>
2022-11-07 01:38:58 -0500

    Mob Stat & Damage Fixes | Mob and Ranged Pdif Fixes | Jug Stat Fixes (#627)
    * Pdif and stat fixes
    
    - Fixes ranged attacks being calculated
    - Fixes mob attack being calculated incorrectly
    - Ranged attacks will now correctly apply the right pdif
    - pdif for mobs are now capped at 4.0 and changed to correct values
    - wratio has been changed for mobs
    - Mob attack/accuracy/evasion/defense have been fixed to match era values
    - Jug pets attack/accuracy/evasion/defense/hp have been changed to match mob stats
    
    * Pdif Stat Fixes Clang
    
    * Pdif Stat Fixes Clang

--- src/map/entities/charentity.cpp
+++ src/map/entities/charentity.cpp
@@ -1671,7 +1671,7 @@ void CCharEntity::OnRangedAttack(CRangeState& state, action_t& action)
             else
             {
                 bool  isCritical = xirand::GetRandomNumber(100) < battleutils::GetRangedCritHitRate(this, PTarget);
-                float pdif       = battleutils::GetRangedDamageRatio(this, PTarget, isCritical, 0);
+                float pdif       = battleutils::GetDamageRatio(this, PTarget, isCritical, 0, slot, 0, false);
 
                 if (isCritical)
                 {
