87da461b09d206d658508d963a6533f91efdbf34
Zach Toogood <zrtoogood@gmail.com>
2022-11-10 13:54:11 +0000

    [lua] Remove hardcoded ids from Bibiki Bay
    After a recent shift, most hardcoded mob (and npc) ids in Bibiki bay
    would have been made useless. This moves them all over to a
    pattern where we ask the zone for the entities by name and their
    relative index in the zone compared to other entities of the same name
    in that zone. These lookups protect these PH's, qm's, etc. from breaking
    during future shifts.

--- scripts/zones/Bibiki_Bay/mobs/Tartarus_Eft.lua
+++ scripts/zones/Bibiki_Bay/mobs/Tartarus_Eft.lua
@@ -12,7 +12,14 @@ entity.onMobDeath = function(mob, player, optParams)
 end
 
 entity.onMobDespawn = function(mob)
-    xi.mob.phOnDespawn(mob, ID.mob.SPLACKNUCK_PH, 10, 3600) -- 1 hour
+    -- PH is the 2nd Tartarus Eft entry in the zone
+    local eftId        = mob:getZone():queryEntitiesByName('Tartarus_Eft')[2]:getID()
+    local splacknuckId = mob:getZone():queryEntitiesByName('Splacknuck')[1]:getID()
+    local splacknuckPh =
+    {
+        [eftId] = splacknuckId, -- -348 0.001 -904
+    }
+    xi.mob.phOnDespawn(mob, splacknuckPh, 10, 3600) -- 1 hour
 end
 
 return entity
