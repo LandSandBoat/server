8959a56beece5963c4807cf99940800d74d5cdd9
WinterSolstice8 <60417494+WinterSolstice8@users.noreply.github.com>
2022-11-13 20:47:06 -0800

    [lua] [DRG] Wyvern related DRG audit
    * Consolidate all damage breaths into dragoon.lua
    * Consolidate all healing breaths into dragoon.lua
    * Implement Smiting Breath
    * Implement Restoring Breath
    * Implement Steady Wing
    * Add "unable to perform" message for Smiting/Restoring breath if wyvern cannot act
    * Fix Spirit Link not returning HP restored to wyvern in message
    * Respect the wyvern by calling it "wyvern" instead of "pet" in dragoon.lua
    * Add comments and sources all over dragoon.lua
    * Adjust spirit surge level calculation to take the wyvern's level into account for STR bonus, not the dragoons
    * Overhaul status effect removal of Spirit Link to wyvern, with new sources
    * Add wyvern levelup bonus from Empathy during spirit link
    * Only remove HP from the wyvern's master on spirit link if the wyvern didn't level up during spirit link via empathy
    * Silently delete and recopy empathy buffs, less logspam is good
    * Fix Dragon Breaker power
    * Adjust Damage Breath to use retail calculations
    * Adjust Healing Breath to use retail calculations
    * Use master level to determine Healing Breath ability based on retail observations
    * Update Strafe merits & aug behavior to match retail
    * Move Strafe bonus to a macc check on wvyern damage breaths using damage_magic functions
    * Add Magic burst & absorb functionality to wyvern damage breaths (MB does not do bonus damage)
    * Adjust wyvern damage breath picking to match retail (elemental meva only, no random factor)
    * Update remove breath logic to match retail
    * Add subtle blow to wyvern
    * Add job point support for base attack increase to master on wyvern levelup
    * Add job point gift support for extra double attack to master on wyvern levelup
    * Add all hit WSD to wyvern levelups to match retail

--- scripts/globals/abilities/pets/remove_curse.lua
+++ scripts/globals/abilities/pets/remove_curse.lua
@@ -19,6 +19,9 @@ abilityObject.onUseAbility = function(pet, target, skill, action)
     elseif target:delStatusEffect(xi.effect.DOOM) then
         skill:setMsg(xi.msg.basic.JA_REMOVE_EFFECT)
         effect = xi.effect.DOOM
+    elseif target:delStatusEffect(xi.effect.BANE) then
+        skill:setMsg(xi.msg.basic.JA_REMOVE_EFFECT)
+        effect = xi.effect.BANE
     else
         skill:setMsg(xi.msg.basic.JA_NO_EFFECT)
     end
