751aac5d680024b6ac5aebe210fe3fff8e3821d3
Frankie-hz <105882754+Frankie-hz@users.noreply.github.com>
2022-11-15 20:49:12 -0500

    Fixes vendor items on Zhikkom

--- modules/era/lua/era_vendors.lua
+++ modules/era/lua/era_vendors.lua
@@ -40,20 +40,19 @@ xi.eraShops.Charging_Chocobo =
 
 xi.eraShops.Zhikkom =
 {
+    16473,  5713, 1, -- Kukri
     16537, 31648, 1, -- Mythril Sword
     16545, 21535, 1, -- Broadsword
     16513, 11845, 1, -- Tuck
     16558, 62560, 1, -- Falchion
-    16473,  5713, 1, -- Kukri
     16536,  7286, 2, -- Iron Sword
     16552,  4163, 2, -- Scimitar
     16466,  2231, 2, -- Knife
     16465,   150, 3, -- Bronze Knife
-    16517,  9406, 3, -- Degens
-    16551,   713, 3, -- Saparas
-    16530,   618, 3, -- Xiphos
-    16565,  1711, 3, -- Spatha
-    16512,  3215, 3, -- Bilbo
+    16405,   106, 3, -- Cat Baghnakhs
+    16535,   246, 3, -- Bronze Sword
+    16517,  9406, 3, -- Degen
+    16551,   713, 3, -- Sapara
 }
 
 xi.eraShops.Peritrage =
