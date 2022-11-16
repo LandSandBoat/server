ca97432d8763ab9e8905c442603e79a0493f210a
dallano <atwood.dallan@hotmail.com>
2022-11-16 12:20:22 -0700

    Update IDs.lua

--- scripts/zones/South_Gustaberg/IDs.lua
+++ scripts/zones/South_Gustaberg/IDs.lua
@@ -62,7 +62,7 @@ zones[xi.zone.SOUTH_GUSTABERG] =
             [17215887] = 17215888, -- -322.871 30.052 -401.184
         },
         BUBBLY_BERNIE    = 17215494,
-        TOCOCO           = 17093006,
+        TOCOCO           = 17215778,
     },
     npc =
     {
