14420716985c5d35d1451260d97e7afd93bd8bee
Zach Toogood <zrtoogood@gmail.com>
2022-11-16 18:38:09 +0000

    [dbtool] Ensure iterated dirs are sorted

--- tools/dbtool.py
+++ tools/dbtool.py
@@ -293,7 +293,7 @@ def fetch_module_files():
             if not line.startswith("#") and line.strip() and not line in ["\n", "\r\n"]:
                 line = from_server_path("modules/" + line.strip())
                 if pathlib.Path(line).is_dir():
-                    for filename in pathlib.Path(line).glob("**/*.sql"):
+                    for filename in sorted(pathlib.Path(line).glob("**/*.sql")):
                         import_files.append(str(filename).replace("\\", "/"))
                 else:
                     if line.endswith(".sql"):
@@ -352,13 +352,13 @@ def fetch_files(express=False):
             print_red("Error checking diffs.\nCheck that hash is valid in config.yaml.")
     else:
         for (_, _, filenames) in os.walk(from_server_path("sql/")):
-            for filename in filenames:
+            for filename in sorted(filenames):
                 import_files.append(from_server_path("sql/" + filename))
             break
     check_protected()
     backups.clear()
     for (_, _, filenames) in os.walk(from_server_path("sql/backups/")):
-        for file in filenames:
+        for file in sorted(filenames):
             if not ".gitignore" in file:
                 backups.append(from_server_path("sql/backups/" + file))
         break
