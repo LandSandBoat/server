be68ff545d1fc9c744a222ed67aba13d1e2a6384
WinterSolstice8 <60417494+WinterSolstice8@users.noreply.github.com>
2022-11-17 14:48:49 -0800

    [login] Better detection logic for first time characters intro CS
    * This fixes first time CS if you disconnected/crashed before actually logging in,
    which could prevent extraneous waiting for your character to logout.

--- src/login/login_session.h
+++ src/login/login_session.h
@@ -40,6 +40,8 @@ struct login_session_data_t
     int32 login_lobbydata_fd;
     int32 login_lobbyview_fd;
     int32 login_lobbyconf_fd;
+
+    bool justCreatedNewChar = false;
 };
 
 typedef std::list<login_session_data_t*> login_sd_list_t;
