4222dd56d7add213fb4535137442984c5121c2ee
Damarus <leblanc.jlj@gmail.com>
2022-11-18 17:25:39 -0500

    Update !zone to prevent zoning to disabled zones (port = 0)

--- scripts/commands/reset.lua
+++ scripts/commands/reset.lua
@@ -54,9 +54,6 @@ function onTrigger(player, target)
         xi.effect.CHARM_II,
         xi.effect.POISON,
         xi.effect.PETRIFICATION,
-        xi.effect.DOOM,
-        xi.effect.CURSE_I,
-        xi.effect.CURSE_II,
     }
 
     for _, v in pairs(effects) do
--- src/map/utils/zoneutils.cpp
+++ src/map/utils/zoneutils.cpp
@@ -1212,12 +1212,15 @@ namespace zoneutils
     {
         if (auto* PZone = GetZone(zoneId))
         {
-            if (PZone->GetIP() == 0 || PZone->GetPort() == 0)
+            if (PZone->GetPort() == 0)
             {
                 return false;
             }
+
+            return true;
         }
-        return true;
+
+        return false;
     }
 
     void AfterZoneIn(CBaseEntity* PEntity)
