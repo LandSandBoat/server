2ae918bc2600c439614939072ff3f580df4f19b6
Kore <d.fernsboi@gmail.com>
2022-11-17 17:00:13 -0800

    Garrison: Add watchdog and gm command polish
    - Adds a timer to poll for inconsistent garrison state and clean up
    - Change stopgarrison to !garrison stop and !garrison start commands

---
+++ scripts/commands/garrison.lua
@@ -0,0 +1,57 @@
+---------------------------------------------------------------------------------------------------
+-- func: garrison <command> (player)
+-- commands:
+-- !garrison start (player) starts the garrison for the given player (or targetted one). This bypasses requirements like lockout
+-- !garrison stop  (player) stops the garrison (if any) currently running in the player's zone
+---------------------------------------------------------------------------------------------------
+
+require("scripts/globals/status")
+require("scripts/globals/common")
+
+cmdprops =
+{
+    permission = 1,
+    parameters = "ss"
+}
+
+function error(player, msg)
+    local usage = "Usage: !garrison <command> (player)"
+    player:PrintToPlayer(msg .. "\n" .. usage)
+end
+
+function onTrigger(player, command, target)
+    -- Validate command
+    if command == nil then
+        error(player, "Invalid command")
+        return
+    end
+
+    -- Obtain target
+    local targ = player:getCursorTarget()
+    if target ~= nil then
+        targ = GetPlayerByName(target)
+        if targ == nil then
+            error(player, string.format("Player named '%s' not found", target))
+            return
+        end
+    end
+
+    -- Validate target
+    if targ == nil and target == nil then
+        error(player, "Either provide a valid target name (in same zone) or target the desired player")
+        return
+    end
+
+    local zone = targ:getZone()
+    switch(command): caseof
+    {
+        ["start"] = function()
+            xi.garrison.start(targ, targ)
+            targ:PrintToPlayer(string.format("%s garrison started", zone:getName()))
+        end,
+        ["stop"] = function()
+            xi.garrison.stop(targ:getZone())
+            targ:PrintToPlayer(string.format("%s garrison stopped", zone:getName()))
+        end,
+    }
+end
