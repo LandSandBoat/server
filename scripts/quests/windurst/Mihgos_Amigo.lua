97b371490eb8e432abd3b9ae92bf8ccdb43de306
Damarus <leblanc.jlj@gmail.com>
2022-11-21 23:19:58 -0500

    Implemented Mihgo's Amigo and Rock Racketeer in proper framework

---
+++ scripts/quests/windurst/Mihgos_Amigo.lua
@@ -0,0 +1,134 @@
+-----------------------------------
+-- Mihgo's Amigo
+-----------------------------------
+-- !addquest 2 25
+-- Nanaa Mihgo: !pos 62   -4    240    241
+-- Ardea      : !pos -198 -6   -69     235
+-- Varun      : !pos 7.8  -3.5 -10.064 241
+-----------------------------------
+require('scripts/globals/items')
+require('scripts/globals/quests')
+require('scripts/globals/settings')
+require('scripts/globals/status')
+require('scripts/globals/titles')
+require('scripts/globals/interaction/quest')
+-----------------------------------
+
+local quest = Quest:new(xi.quest.log_id.WINDURST, xi.quest.id.windurst.MIHGO_S_AMIGO)
+
+local gilPerTrade = 200
+
+quest.reward =
+{
+    fame = 4, -- Baesd on 212 yag necklaces needed to reach level 4 tenshodo fame.
+    fameArea = xi.quest.fame_area.NORG,
+    title = xi.title.CAT_BURGLAR_GROUPIE,
+}
+
+quest.sections =
+{
+    {
+        -- QUEST AVAILABLE
+        check = function(player, status, vars)
+            return status == QUEST_AVAILABLE and
+                player:getFameLevel(xi.quest.fame_area.NORG) >= 1 and
+                player:getQuestStatus(xi.quest.log_id.WINDURST, xi.quest.id.windurst.THE_TENSHODO_SHOWDOWN) ~= QUEST_ACCEPTED and
+                player:getQuestStatus(xi.quest.log_id.WINDURST, xi.quest.id.windurst.AS_THICK_AS_THIEVES) ~= QUEST_ACCEPTED and
+                not player:hasItem(xi.items.MARAUDERS_KNIFE)
+        end,
+
+        [xi.zone.WINDURST_WOODS] =
+        {
+            ['Nanaa_Mihgo'] =
+            {
+                onTrigger = function(player, npc)
+                    if player:getQuestStatus(xi.quest.log_id.WINDURST, xi.quest.id.windurst.CRYING_OVER_ONIONS) == QUEST_ACCEPTED then
+                        return quest:progressEvent(81)
+                    else
+                        return quest:progressEvent(80)
+                    end
+                end,
+            },
+
+            onEventFinish =
+            {
+                [80] = function(player, csid, option, npc)
+                    return quest:begin(player)
+                end,
+
+                [81] = function(player, csid, option, npc)
+                    return quest:begin(player)
+                end,
+            },
+        },
+    },
+
+    {
+        -- QUEST ACCEPTED
+        check = function(player, status, vars)
+            return status == QUEST_ACCEPTED
+        end,
+
+        [xi.zone.WINDURST_WOODS] =
+        {
+            ['Nanaa_Mihgo'] =
+            {
+                onTrigger = function(player, npc)
+                    return quest:event(82)
+                end,
+
+                onTrade = function(player, npc, trade)
+                    if npcUtil.tradeHasExactly(trade, { { xi.items.YAGUDO_BEAD_NECKLACE, 4 } }) then
+                        return quest:progressEvent(88, { [0] = gilPerTrade })
+                    end
+                end,
+            },
+
+            onEventFinish =
+            {
+                [88] = function(player, csid, option, npc)
+                    player:confirmTrade()
+                    player:addGil(gilPerTrade)
+                    quest:complete(player)
+                    xi.quest.setMustZone(player, xi.quest.log_id.WINDURST, xi.quest.id.windurst.ROCK_RACKETEER)
+                end,
+            },
+        },
+    },
+
+    -- QUEST COMPLETE
+    {
+        check = function(player, status, vars)
+            return status == QUEST_COMPLETED
+        end,
+
+        [xi.zone.WINDURST_WOODS] =
+        {
+            ['Nanaa_Mihgo'] =
+            {
+                onTrigger = function (player, npc)
+                    if player:getQuestStatus(xi.quest.log_id.WINDURST, xi.quest.id.windurst.ROCK_RACKETEER) < QUEST_ACCEPTED then
+                        return quest:event(89):replaceDefault()
+                    end
+                end,
+
+                onTrade = function(player, npc, trade)
+                    if npcUtil.tradeHasExactly(trade, { { xi.items.YAGUDO_BEAD_NECKLACE, 4 } }) then
+                        return quest:progressEvent(494, { [0] = gilPerTrade })
+                    end
+                end,
+            },
+
+            onEventFinish =
+            {
+                [494] = function(player, csid, option, npc)
+                    player:confirmTrade()
+                    player:addGil(gilPerTrade)
+                    player:addFame(xi.quest.fame_area.NORG, 4)
+                end,
+            },
+        },
+    },
+}
+
+return quest
--- scripts/zones/Windurst_Woods/npcs/Nanaa_Mihgo.lua
+++ scripts/zones/Windurst_Woods/npcs/Nanaa_Mihgo.lua
@@ -56,22 +56,11 @@ local trustMemory = function(player)
 end
 
 entity.onTrade = function(player, npc, trade)
-    if npcUtil.tradeHas(trade, { { 498, 4 } }) then -- Yagudo Necklace x4
-        local mihgosAmigo = player:getQuestStatus(xi.quest.log_id.WINDURST, xi.quest.id.windurst.MIHGO_S_AMIGO)
-
-        if mihgosAmigo == QUEST_ACCEPTED then
-            player:startEvent(88, xi.settings.main.GIL_RATE * 200)
-        elseif mihgosAmigo == QUEST_COMPLETED then
-            player:startEvent(494, xi.settings.main.GIL_RATE * 200)
-        end
-    end
 end
 
 entity.onTrigger = function(player, npc)
     local wildcatWindurst = player:getCharVar("WildcatWindurst")
     local mihgosAmigo = player:getQuestStatus(xi.quest.log_id.WINDURST, xi.quest.id.windurst.MIHGO_S_AMIGO)
-    local rockRacketeer = player:getQuestStatus(xi.quest.log_id.WINDURST, xi.quest.id.windurst.ROCK_RACKETEER)
-    local rockRacketeerCS = player:getCharVar("rockracketeer_sold")
 
     -- LURE OF THE WILDCAT (WINDURST 2-1)
     -- Simply checks this NPC as talked to for the PC, should be highest priority
@@ -92,42 +81,6 @@ entity.onTrigger = function(player, npc)
         player:setLocalVar("TrustDialogue", 1)
 
         player:startEvent(865, 0, 0, 0, trustMemory(player), 0, 0, 0, trustFlag)
-
-    -- ROCK RACKETEER (Mihgo's Amigo follow-up)
-    elseif
-        mihgosAmigo == QUEST_COMPLETED and
-        rockRacketeer == QUEST_AVAILABLE and
-        player:getFameLevel(xi.quest.fame_area.WINDURST) >= 3
-    then
-        if player:needToZone() then
-            player:startEvent(89) -- complete
-        else
-            player:startEvent(93) -- quest start
-        end
-    elseif rockRacketeer == QUEST_ACCEPTED and rockRacketeerCS == 1 then
-        player:startEvent(98) -- advance quest talk to Varun
-    elseif rockRacketeer == QUEST_ACCEPTED and rockRacketeerCS == 2 then
-        player:startEvent(95) -- not sold reminder
-    elseif rockRacketeer == QUEST_ACCEPTED then
-        player:startEvent(94) -- quest reminder
-
-    -- MIHGO'S AMIGO
-    elseif mihgosAmigo == QUEST_AVAILABLE then
-        if player:getQuestStatus(xi.quest.log_id.WINDURST, xi.quest.id.windurst.CRYING_OVER_ONIONS) == QUEST_AVAILABLE then
-            player:startEvent(81) -- Start Quest "Mihgo's Amigo" with quest "Crying Over Onions" Activated
-        else
-            player:startEvent(80) -- Start Quest "Mihgo's Amigo"
-        end
-    elseif mihgosAmigo == QUEST_ACCEPTED then
-        player:startEvent(82)
-
-    -- STANDARD DIALOG
-    elseif rockRacketeer == QUEST_COMPLETED then
-        player:startEvent(99) -- new dialog after Rock Racketeer
-    elseif mihgosAmigo == QUEST_COMPLETED then
-        player:startEvent(89) -- new dialog after Mihgo's Amigos
-    else
-        player:startEvent(76) -- standard dialog
     end
 end
 
@@ -138,33 +91,6 @@ entity.onEventFinish = function(player, csid, option)
     -- LURE OF THE WILDCAT (WINDURST)
     if csid == 732 then
         player:setCharVar("WildcatWindurst", utils.mask.setBit(player:getCharVar("WildcatWindurst"), 4, true))
-
-    -- ROCK RACKETEER
-    elseif csid == 93 then
-        player:addQuest(xi.quest.log_id.WINDURST, xi.quest.id.windurst.ROCK_RACKETEER)
-        npcUtil.giveKeyItem(player, xi.ki.SHARP_GRAY_STONE)
-    elseif csid == 98 then
-        player:delGil(10 * xi.settings.main.GIL_RATE)
-        player:setCharVar("rockracketeer_sold", 3)
-
-    -- MIHGO'S AMIGO
-    elseif csid == 80 or csid == 81 then
-        player:addQuest(xi.quest.log_id.WINDURST, xi.quest.id.windurst.MIHGO_S_AMIGO)
-    elseif csid == 88 then
-        player:confirmTrade()
-        player:completeQuest(xi.quest.log_id.WINDURST, xi.quest.id.windurst.MIHGO_S_AMIGO)
-        player:addTitle(xi.title.CAT_BURGLAR_GROUPIE)
-        player:addGil(xi.settings.main.GIL_RATE * 200)
-        player:addFame(xi.quest.fame_area.NORG, 60)
-        player:needToZone(true)
-    elseif csid == 494 then
-        player:confirmTrade()
-        player:addTitle(xi.title.CAT_BURGLAR_GROUPIE)
-        player:addGil(xi.settings.main.GIL_RATE * 200)
-        player:addFame(xi.quest.fame_area.NORG, 30)
-    elseif csid == 865 and option == 2 then
-        player:addSpell(901, true, true)
-        player:messageSpecial(ID.text.YOU_LEARNED_TRUST, 0, 901)
     end
 end
 
