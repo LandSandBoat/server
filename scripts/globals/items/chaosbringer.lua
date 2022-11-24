01193ac88fb99bd69d013d547bb08b9521b7dcb4
dallano <atwood.dallan@hotmail.com>
2022-11-23 20:43:18 -0700

    Chaosbringer fixes

--- scripts/globals/items/chaosbringer.lua
+++ scripts/globals/items/chaosbringer.lua
@@ -4,26 +4,32 @@
 -----------------------------------
 local itemObject = {}
 
-itemObject.onItemDrop = function(target, item)
-    target:setCharVar('ChaosbringerKills', 0)
+itemObject.onItemDrop = function(player, item)
+    player:setCharVar('ChaosbringerKills', 0)
 end
 
-itemObject.onItemEquip = function(target, item)
-    target:addListener('DEFEATED_MOB', 'CHAOSBRINGER_KILLS', function(mob, player, optParams)
-        if
-            (player:getQuestStatus(xi.quest.log_id.BASTOK, xi.quest.id.bastok.BLADE_OF_DARKNESS) == QUEST_ACCEPTED or
-            player:getQuestStatus(xi.quest.log_id.BASTOK, xi.quest.id.bastok.BLADE_OF_DEATH) == QUEST_ACCEPTED) and
-            target:getCharVar('ChaosbringerKills') < 200 and
-            optParams.isKiller and
-            not optParams.isWeaponSkillKill
-        then
-            player:incrementCharVar("ChaosbringerKills", 1)
+itemObject.onItemEquip = function(player, item)
+    player:addListener("ENGAGE", "CHAOSBRINGER_KILLS", function(playerArg, mob)
+        if mob:getLocalVar("listenerApplied") == 0 then
+            mob:addListener("TAKE_DAMAGE", "VALID_KILL", function(mobArg, amount, attacker, attackType, damageType)
+                mob:setLocalVar("listenerApplied", 1)
+                if
+                    (attacker:getQuestStatus(xi.quest.log_id.BASTOK, xi.quest.id.bastok.BLADE_OF_DARKNESS) == QUEST_ACCEPTED or
+                    attacker:getQuestStatus(xi.quest.log_id.BASTOK, xi.quest.id.bastok.BLADE_OF_DEATH) == QUEST_ACCEPTED) and
+                    attacker:getEquipID(xi.slot.MAIN) == xi.items.CHAOSBRINGER and
+                    attackType == xi.attackType.PHYSICAL and
+                    amount > mobArg:getHP()
+                then
+                    playerArg:incrementCharVar("ChaosbringerKills", 1)
+                    print(player:getCharVar("ChaosbringerKills"))
+                end
+            end)
         end
     end)
 end
 
-itemObject.onItemUnequip = function(target, item)
-    target:removeListener('CHAOSBRINGER_KILLS')
+itemObject.onItemUnequip = function(player, item)
+    player:removeListener('CHAOSBRINGER_KILLS')
 end
 
 return itemObject
--- src/map/entities/charentity.cpp
+++ src/map/entities/charentity.cpp
@@ -905,6 +905,7 @@ void CCharEntity::OnEngage(CAttackState& state)
     TracyZoneScoped;
     CBattleEntity::OnEngage(state);
     PLatentEffectContainer->CheckLatentsTargetChange();
+    PAI->EventHandler.triggerListener("ENGAGE", CLuaBaseEntity(this), CLuaBaseEntity(state.GetTarget()));
     this->m_charHistory.battlesFought++;
 }
 
