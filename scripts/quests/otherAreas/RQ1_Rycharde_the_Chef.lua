6189c47164eb63cef1658ea4c175c4aa181d9f7f
hooksta4 <lance.hooker@gmail.com>
2022-11-25 14:02:36 -0500

    Fixes The Quadav Curst and Rycharde the chef.
    Fixes horizon issues # 428 and # 427.

--- scripts/quests/otherAreas/RQ1_Rycharde_the_Chef.lua
+++ scripts/quests/otherAreas/RQ1_Rycharde_the_Chef.lua
@@ -37,9 +37,9 @@ quest.sections =
             {
                 onTrigger = function(player, npc)
                     if quest:getVar(player, 'Prog') == 2 then
-                        return quest:progressEvent(70, xi.items.DHALMEL_MEAT)
+                        return quest:progressEvent(70, xi.items.SLICE_OF_DHALMEL_MEAT)
                     elseif quest:getVar(player, 'Prog') > 2 then
-                        return quest:progressEvent(71, xi.items.DHALMEL_MEAT)
+                        return quest:progressEvent(71, xi.items.SLICE_OF_DHALMEL_MEAT)
                     end
                 end,
             },
@@ -99,9 +99,9 @@ quest.sections =
                 end,
 
                 onTrade = function(player, npc, trade)
-                    if npcUtil.tradeHasExactly(trade, { { xi.items.DHALMEL_MEAT, 2 } }) then
+                    if npcUtil.tradeHasExactly(trade, { { xi.items.SLICE_OF_DHALMEL_MEAT, 2 } }) then
                         return quest:progressEvent(74) -- Quest completed dialog.
-                    elseif npcUtil.tradeHasExactly(trade, { { xi.items.DHALMEL_MEAT, 1 } }) then
+                    elseif npcUtil.tradeHasExactly(trade, { { xi.items.SLICE_OF_DHALMEL_MEAT, 1 } }) then
                         return quest:event(73) -- "That's not enough!" dialog.
                     end
                 end,
