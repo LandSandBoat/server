43641fbb4ac8b1050597e2d65e2caeb513955831
Frankie-hz <105882754+Frankie-hz@users.noreply.github.com>
2022-11-26 02:26:50 -0500

    Missing s

--- scripts/globals/abilities/pets/hastega.lua
+++ scripts/globals/abilities/pets/hastega.lua
@@ -15,7 +15,7 @@ end
 
 abilityObject.onPetAbility = function(target, pet, skill, summoner)
     local bonusTime = utils.clamp(xi.summon.getSummoningSkillOverCap(pet) * 3, 0, 90)-- 3 seconds / skill | Duration is capped at 180 total
-    local duration = 90 + bonusTimes
+    local duration = 90 + bonusTime
     -- Garuda's Hastega is a weird exception and uses 153/1024 instead of 150/1024 like Haste spells
     -- That's why it overwrites some things regular haste won't.
     target:addStatusEffect(xi.effect.HASTE, 1494, 0, duration) -- 153/1024 ~14.94%
