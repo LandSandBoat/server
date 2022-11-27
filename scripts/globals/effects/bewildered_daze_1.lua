173602befec474cd0e9b037eea7a07001cf30372
claywar <claywar@beerlab.dev>
2022-11-27 07:37:22 -0500

    [dancer] refactor and move waltz abilities to global
    Refactor ability check, correct spelling for waltz potency
    
    
    Update documentation formatting
    
    
    Fix additional issues from testing

--- scripts/globals/effects/bewildered_daze_1.lua
+++ scripts/globals/effects/bewildered_daze_1.lua
@@ -5,12 +5,12 @@ require("scripts/globals/status")
 -----------------------------------
 local effectObject = {}
 
--- TODO: CRIT_HIT_EVASION_DOWN is currently not part of hit calculations,
--- and needs to be implemented (but does exist in the enum)
+-- TODO: This should be crit evasion down, but is currently increasing
+-- enemy crit rate (Is this the same thing?)
 effectObject.onEffectGain = function(target, effect)
     local effectPower = effect:getPower()
 
-    target:addMod(xi.mod.CRIT_HIT_EVASION_DOWN, effectPower * -1)
+    target:addMod(xi.mod.ENEMYCRITRATE, effectPower)
 end
 
 effectObject.onEffectTick = function(target, effect)
@@ -19,7 +19,7 @@ end
 effectObject.onEffectLose = function(target, effect)
     local effectPower = effect:getPower()
 
-    target:delMod(xi.mod.CRIT_HIT_EVASION_DOWN, effectPower * -1)
+    target:delMod(xi.mod.ENEMYCRITRATE, effectPower)
 end
 
 return effectObject
