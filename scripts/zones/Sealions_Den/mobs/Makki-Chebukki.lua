2c65222b6459bd1269492b1263623813c0410e4f
dallano <atwood.dallan@hotmail.com>
2022-11-27 18:46:17 -0700

    DMG fixes for tarus in 7-5
    Fixes: https://github.com/AirSkyBoat/AirSkyBoat/issues/1851

--- scripts/zones/Sealions_Den/mobs/Makki-Chebukki.lua
+++ scripts/zones/Sealions_Den/mobs/Makki-Chebukki.lua
@@ -11,10 +11,7 @@ entity.onMobSpawn = function(mob)
     mob:setMobMod(xi.mobMod.SPECIAL_COOL, 30) -- Makki Chebukki uses ranged attack every 30 seconds
     mob:setMobMod(xi.mobMod.NO_AGGRO, 1)
     mob:setMobMod(xi.mobMod.NO_LINK, 1)
-    mob:addMod(xi.mod.UDMGPHYS, -100)
-    mob:addMod(xi.mod.UDMGMAGIC, -100)
-    mob:addMod(xi.mod.UDMGRANGE, -100)
-    mob:addMod(xi.mod.UDMGBREATH, -100)
+    mob:setMod(xi.mod.DMG, -10000)
     mob:setMobMod(xi.mobMod.NO_MOVE, 1)
     mob:setLocalVar("makki", 1)
 end
