52d8cbe2920e9cb23a11089250e6d43cbc6dbfa3
wayne <waynemortenson@gmail.com>
2022-11-27 22:51:35 -0600

    Removed xp reward

--- scripts/quests/windurst/Glyph_Hanger.lua
+++ scripts/quests/windurst/Glyph_Hanger.lua
@@ -16,6 +16,7 @@ local quest = Quest:new(xi.quest.log_id.WINDURST, xi.quest.id.windurst.GLYPH_HAN
 
 quest.reward =
 {
+    xp = 2000,
     fame = 120,
     fameArea = xi.quest.fame_area.WINDURST,
     keyItem = xi.ki.MAP_OF_THE_HORUTOTO_RUINS,
