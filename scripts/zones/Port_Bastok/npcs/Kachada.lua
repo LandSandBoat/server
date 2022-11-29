0047f92aa7694e3c1db2c64bb804cf0ac943cb70
Panic Stevenson <panic.stevenson@gmail.com>
2022-11-28 18:40:56 -0800

    Give World Pass Merchants Custom Text (#1945)

--- scripts/zones/Port_Bastok/npcs/Kachada.lua
+++ scripts/zones/Port_Bastok/npcs/Kachada.lua
@@ -8,7 +8,7 @@ entity.onTrade = function(player, npc, trade)
 end
 
 entity.onTrigger = function(player, npc)
-    player:startEvent(96)--, 0, 0, 0, 0, 0, -1, 2)
+    player:PrintToPlayer("The Vana'diel Adventurer Recruitment Program isn't running currently. World Passes aren't available for purchase at this time.", xi.msg.channel.SAY, npc:getPacketName())
 end
 
 entity.onEventUpdate = function(player, csid, option)
--- scripts/zones/Port_San_dOria/npcs/Ambleon.lua
+++ scripts/zones/Port_San_dOria/npcs/Ambleon.lua
@@ -10,7 +10,7 @@ entity.onTrade = function(player, npc, trade)
 end
 
 entity.onTrigger = function(player, npc)
-    player:startEvent(710)
+    player:PrintToPlayer("The Vana'diel Adventurer Recruitment Program isn't running currently. World Passes aren't available for purchase at this time.", xi.msg.channel.SAY, npc:getPacketName())
 end
 
 entity.onEventUpdate = function(player, csid, option)
