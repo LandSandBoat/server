fae23fd2d54abaa625b264d333b10f4a7383a543
WinterSolstice8 <60417494+WinterSolstice8@users.noreply.github.com>
2022-11-29 21:31:04 -0800

    [lua] Overhaul intro character cutscene handling
    * Add Intro CS as a hidden quest
    * Setup flag to see intro CS in lobby.cpp ahead of login, if intro cs is enabled
    * Update SoA intro CS to depend on seeing the intro CS if intro CS is enabled
    * Move adding adventurer's coupon to intro cutscene finishing
    * If intro CS is disabled, add adventurer's coupon by default in Player.lua
    * Add missing messages to various IDs.lua, use them in intro CS
    * Fix Bastok Markets intro CS to play the second cutscene
    * Document existence of the event update for Windurst Waters intro CS

--- scripts/zones/Port_Windurst/IDs.lua
+++ scripts/zones/Port_Windurst/IDs.lua
@@ -50,6 +50,7 @@ zones[xi.zone.PORT_WINDURST] =
         HOMEPOINT_SET                   = 11073, -- Home point set!
         YOU_ACCEPT_THE_MISSION          = 11166, -- You have accepted the mission.
         KHEL_PAHLHAMA_SHOP_DIALOG       = 11212, -- These magic shells are full of mysteries...
+        MAP_MARKER_TUTORIAL             = 11405, -- The map will open when you select Map from the main menu. Choose Markers and scroll to the right to check the location.
         MOG_LOCKER_OFFSET               = 11490, -- Your Mog Locker lease is valid until <timestamp>, kupo.
         FISHING_MESSAGE_OFFSET          = 11588, -- You can't fish here.
         FISHING_SUPPORT                 = 11692, -- Your [fishing/woodworking/smithing/goldsmithing/clothcraft/leatherworking/bonecraft/alchemy/cooking] skills went up [a little/ever so slightly/ever so slightly].
--- scripts/zones/Windurst_Waters/Zone.lua
+++ scripts/zones/Windurst_Waters/Zone.lua
@@ -18,17 +18,6 @@ zoneObject.onInitialize = function(zone)
 end
 
 zoneObject.onZoneIn = function(player, prevZone)
-    local cs = { -1 }
-
-    -- FIRST LOGIN (START CS)
-    if player:getPlaytime(false) == 0 then
-        if xi.settings.main.NEW_CHARACTER_CUTSCENE == 1 then
-            cs = { 531, -1, xi.cutscenes.params.NO_OTHER_ENTITY }
-        end
-        player:setPos(-40, -5, 80, 64)
-        player:setHomePoint()
-    end
-
     -- MOG HOUSE EXIT
     if
         player:getXPos() == 0 and
@@ -38,8 +27,6 @@ zoneObject.onZoneIn = function(player, prevZone)
         local position = math.random(1, 5) + 157
         player:setPos(position, -5, -62, 192)
     end
-
-    return cs
 end
 
 zoneObject.onConquestUpdate = function(zone, updatetype)
@@ -53,9 +40,6 @@ zoneObject.onEventUpdate = function(player, csid, option)
 end
 
 zoneObject.onEventFinish = function(player, csid, option)
-    if csid == 531 then
-        player:messageSpecial(ID.text.ITEM_OBTAINED, 536)
-    end
 end
 
 return zoneObject
