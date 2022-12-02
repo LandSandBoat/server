f0c9e9685d6542fb37491b183347a15776437cf8
Andrew Morrison <andy.wmorrison@gmail.com>
2022-12-01 20:22:42 -0800

    add latent that checks for HPP without +% or convert gear

--- src/map/latent_effect.h
+++ src/map/latent_effect.h
@@ -73,7 +73,7 @@ enum class LATENT : uint16
     // 41 free to use
     // 42 free to use
     WEAPON_DRAWN_HP_UNDER = 43, // PARAM: HP PERCENT
-    // 44 free to use
+    HP_BASEU_TP_UNDER_100 = 44, // Base HP (no convert or +% taken into account) <= %, TP < 100
     MP_UNDER_VISIBLE_GEAR = 45, // mp less than or equal to %, calculated using MP bonuses from visible gear only
     HP_OVER_VISIBLE_GEAR  = 46, // hp more than or equal to %, calculated using HP bonuses from visible gear only
     WEAPON_BROKEN         = 47, //
--- src/map/latent_effect_container.cpp
+++ src/map/latent_effect_container.cpp
@@ -107,6 +107,7 @@ void CLatentEffectContainer::CheckLatentsHP()
             case LATENT::HP_UNDER_PERCENT:
             case LATENT::HP_OVER_PERCENT:
             case LATENT::HP_UNDER_TP_UNDER_100:
+            case LATENT::HP_BASEU_TP_UNDER_100:
             case LATENT::HP_OVER_TP_UNDER_100:
             case LATENT::SANCTION_REGEN_BONUS:
             case LATENT::SIGIL_REGEN_BONUS:
@@ -134,6 +135,7 @@ void CLatentEffectContainer::CheckLatentsTP()
             case LATENT::TP_UNDER:
             case LATENT::TP_OVER:
             case LATENT::HP_UNDER_TP_UNDER_100:
+            case LATENT::HP_BASEU_TP_UNDER_100:
             case LATENT::HP_OVER_TP_UNDER_100:
             case LATENT::SANCTION_REFRESH_BONUS:
             case LATENT::SIGIL_REFRESH_BONUS:
@@ -688,6 +690,9 @@ bool CLatentEffectContainer::ProcessLatentEffect(CLatentEffect& latentEffect)
             break;
         case LATENT::HP_UNDER_TP_UNDER_100:
             expression = m_POwner->GetHPP() <= latentEffect.GetConditionsValue() && m_POwner->health.tp < 1000;
+            break;
+        case LATENT::HP_BASEU_TP_UNDER_100:
+            expression = m_POwner->GetHPPNoPercentOrConvert() <= latentEffect.GetConditionsValue() && m_POwner->health.tp < 1000;
             break;
         case LATENT::HP_OVER_TP_UNDER_100:
             expression = m_POwner->GetHPP() >= latentEffect.GetConditionsValue() && m_POwner->health.tp < 1000;
