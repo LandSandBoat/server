a4b4f4e8f8d6292d691e5717c22eb2000cf9f4bb
dallano <atwood.dallan@hotmail.com>
2022-12-01 22:53:20 -0700

    Sacrarium Door time fix
    Fixes Sacrarium door to properly give longer than 5 seconds to open the second lock

--- scripts/zones/Sacrarium/npcs/Small_Keyhole.lua
+++ scripts/zones/Sacrarium/npcs/Small_Keyhole.lua
@@ -2,6 +2,9 @@
 --  NPC: Small Keyhole
 -- Area: Sacrarium
 -- !pos 99.772 -1.614 51.545 28
+-- NOTE:
+--   Old event call 100 ran too fast,
+--   used messages instead
 -----------------------------------
 local ID = require("scripts/zones/Sacrarium/IDs")
 require("scripts/globals/keyitems")
@@ -22,8 +25,24 @@ entity.onTrade = function(player, npc, trade)
         if npc:getLocalVar("canTradeSecondKey") == 0 then
             npc:setLocalVar("canTradeSecondKey", 1)
             -- Opens lock visually to indicate to other players when to trade next key
-            GetNPCByID(ID.npc.SMALL_KEYHOLE - 2):openDoor(9)
-            player:startEvent(100)
+            GetNPCByID(ID.npc.SMALL_KEYHOLE - 2):openDoor(18)
+            local speed = player:getSpeed()
+            player:setSpeed(0)
+            --player:startEvent(100)
+
+            player:timer(2000, function(playerArg)
+                playerArg:messageSpecial(ID.text.HOLDING_THE_LOCK)
+                playerArg:timer(10000, function(playerArg1)
+                    playerArg1:messageSpecial(ID.text.HAND_GROWN_NUMB)
+
+                    playerArg1:timer(5000, function(playerArg2)
+                        playerArg2:messageSpecial(ID.text.CORAL_KEY_BREAKS, 0, xi.items.CORAL_CREST_KEY)
+                        npc:setLocalVar("canTradeSecondKey", 0)
+                        player:setSpeed(speed)
+                        playerArg2:confirmTrade()
+                    end)
+                end)
+            end)
         else
             player:messageSpecial(ID.text.CANNOT_TRADE_NOW)
         end
@@ -34,13 +53,6 @@ entity.onEventUpdate = function(player, csid, option)
 end
 
 entity.onEventFinish = function(player, csid, option)
-    if csid == 100 then
-        player:timer(5000, function(playerArg)
-            GetNPCByID(ID.npc.SMALL_KEYHOLE):setLocalVar("canTradeSecondKey", 0)
-            playerArg:messageSpecial(ID.text.CORAL_KEY_BREAKS, 0, xi.items.CORAL_CREST_KEY)
-            playerArg:confirmTrade()
-        end)
-    end
 end
 
 return entity
