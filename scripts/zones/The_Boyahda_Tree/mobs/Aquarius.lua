4b10c85a70721c85cff4f3bd1808e4b8220d4316
Panic Stevenson <panic.stevenson@gmail.com>
2022-12-02 14:46:56 -0800

    Give Aquarius Mob Modifiers

--- scripts/zones/The_Boyahda_Tree/mobs/Aquarius.lua
+++ scripts/zones/The_Boyahda_Tree/mobs/Aquarius.lua
@@ -11,6 +11,12 @@ entity.onMobInitialize = function(mob)
     mob:setMobMod(xi.mobMod.ADD_EFFECT, 1)
 end
 
+entity.onMobSpawn = function(mob)
+    mob:addMod(xi.mod.DEFP, 50)
+    mob:addMod(xi.mod.ACC, -50)
+    mob:addMod(xi.mod.ATTP, 50)
+end
+
 entity.onAdditionalEffect = function(mob, target, damage)
     return xi.mob.onAddEffect(mob, target, damage, xi.mob.ae.ENWATER)
 end
