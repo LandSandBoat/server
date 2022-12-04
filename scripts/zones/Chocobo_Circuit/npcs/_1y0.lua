57ce4c80bf69460bfe8978ce1a9f1e9df7c50a0b
Tom-Neverwinter <Tom_Neverwinter@yahoo.com>
2022-12-04 02:13:53 -0600

    0123456789 not descriptive enough so heres a gate

---
+++ scripts/zones/Chocobo_Circuit/npcs/_1y0.lua
@@ -0,0 +1,20 @@
+-----------------------------------
+-- Area: Chocobo Circuit
+-- NPC: gate
+-- !pos -390.000 -5.500 -490.000
+-----------------------------------
+local entity = {}
+
+entity.onTrade = function(player, npc, trade)
+end
+
+entity.onTrigger = function(player, npc)
+end
+
+entity.onEventUpdate = function(player, csid, option)
+end
+
+entity.onEventFinish = function(player, csid, option)
+end
+
+return entity
