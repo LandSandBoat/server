28cdf4df49c6fc968d3910533db8e122a8b0d775
Damarus <leblanc.jlj@gmail.com>
2022-12-05 00:40:00 -0500

    Fix issues with Promyvion MR respawns

--- scripts/globals/promyvion.lua
+++ scripts/globals/promyvion.lua
@@ -12,30 +12,6 @@ xi.promyvion = xi.promyvion or {}
 -- LOCAL FUNCTIONS
 -----------------------------------
 
-local function maxFloor(ID)
-    local m = 0
-
-    for _, v in pairs(ID.mob.MEMORY_RECEPTACLES) do
-        m = math.max(m, v[1])
-    end
-
-    return m
-end
-
-local function randomizeFloorExit(ID, floor)
-    local streams = {}
-
-    for _, v in pairs(ID.mob.MEMORY_RECEPTACLES) do
-        if v[1] == floor then
-            GetNPCByID(v[3]):setLocalVar("[promy]floorExit", 0)
-            table.insert(streams, v[3])
-        end
-    end
-
-    local exitStreamId = streams[math.random(#streams)]
-    GetNPCByID(exitStreamId):setLocalVar("[promy]floorExit", 1)
-end
-
 local function findMother(mob)
     local ID = zones[mob:getZoneID()]
     local mobId = mob:getID()
@@ -49,30 +25,50 @@ local function findMother(mob)
     return mother
 end
 
+local function receptaclesAliveOnFloor(mob)
+    local zoneReceptacles = zones[mob:getZoneID()].mob.MEMORY_RECEPTACLES
+    local group           = zoneReceptacles[mob:getID()].group
+    local mobId           = mob:getID()
+
+    local receptaclesAlive = {}
+    for id, receptacle in pairs(zoneReceptacles) do
+        if group == receptacle.group then
+            if
+                id ~= mobId and -- Don't added itself to the list
+                GetMobByID(id):isAlive()
+            then
+                table.insert(receptaclesAlive, id)
+            end
+        end
+    end
+
+    return receptaclesAlive
+end
+
+local function receptaclesInGroup(mob)
+    local zoneReceptacles = zones[mob:getZoneID()].mob.MEMORY_RECEPTACLES
+    local group           = zoneReceptacles[mob:getID()].group
+
+    local receptaclesGroup = {}
+    for id, receptacle in pairs(zoneReceptacles) do
+        if group == receptacle.group then
+            table.insert(receptaclesGroup, id)
+        end
+    end
+
+    return receptaclesGroup
+end
+
 -----------------------------------
 -- PUBLIC FUNCTIONS
 -----------------------------------
 
 xi.promyvion.initZone = function(zone)
-    local ID = zones[zone:getID()]
+    local streams = zones[zone:getID()].npc.MEMORY_STREAMS
 
     -- register teleporter trigger areas
-    for k, v in pairs(ID.npc.MEMORY_STREAMS) do
-        zone:registerTriggerArea(k, v[1], v[2], v[3], v[4], v[5], v[6])
-    end
-
-    -- randomize floor exits
-    for i = 1, maxFloor(ID) do
-        randomizeFloorExit(ID, i)
-    end
-end
-
-xi.promyvion.zoneGameDay = function(zone)
-    local ID = zones[zone:getID()]
-
-    -- randomize floor exits
-    for i = 1, maxFloor(ID) do
-        randomizeFloorExit(ID, i)
+    for id, stream in pairs(streams) do
+        zone:registerTriggerArea(id, unpack(stream.triggerArea))
     end
 end
 
@@ -94,7 +90,7 @@ xi.promyvion.receptacleOnFight = function(mob, target)
     if os.time() > mob:getLocalVar("[promy]nextStray") then
         local ID = zones[mob:getZoneID()]
         local mobId = mob:getID()
-        local numStrays = ID.mob.MEMORY_RECEPTACLES[mobId][2]
+        local numStrays = ID.mob.MEMORY_RECEPTACLES[mobId].strays
         local count = 0
 
         for i = mobId + 1, mobId + numStrays do
@@ -124,9 +120,8 @@ end
 
 xi.promyvion.receptacleIdle = function(mob)
     if os.time() > mob:getLocalVar("[promy]nextStray") then
-        local ID = zones[mob:getZoneID()]
         local mobId = mob:getID()
-        local numStrays = ID.mob.MEMORY_RECEPTACLES[mobId][2]
+        local numStrays = zones[mob:getZoneID()].mob.MEMORY_RECEPTACLES[mobId].strays
         local count = 0
 
         for i = mobId + 1, mobId + numStrays do
@@ -140,7 +135,10 @@ xi.promyvion.receptacleIdle = function(mob)
             mob:setLocalVar("[promy]nextStray", os.time() + 300)
             for i = mobId + 1, mobId + numStrays do
                 local stray = GetMobByID(i)
-                if not stray:isSpawned() then
+                if
+                    stray ~= nil and
+                    not stray:isSpawned()
+                then
                     count = count + 1
                     SpawnMob(stray:getID()):setPos(mob:getXPos(), mob:getYPos(), mob:getZPos())
                     break
@@ -156,38 +154,48 @@ end
 
 xi.promyvion.receptacleOnDeath = function(mob, optParams)
     if optParams.isKiller then
-        local zone = mob:getZone()
-        local ID = zones[mob:getZoneID()]
-        local mobId = mob:getID()
-        local floor = ID.mob.MEMORY_RECEPTACLES[mobId][1]
-        local streamId = ID.mob.MEMORY_RECEPTACLES[mobId][3]
-        local stream = GetNPCByID(streamId)
-        local receptDead = zone:getLocalVar(string.format("receptDead[%i][%i]", mob:getZone():getID(), floor))
+        local mobId             = mob:getID()
+        local zoneReceptacles   = zones[mob:getZoneID()].mob.MEMORY_RECEPTACLES
+        local floor             = zoneReceptacles[mobId].group
+        local streamId          = zoneReceptacles[mobId].stream
+        local receptacleStreams = zones[mob:getZoneID()].npc.MEMORY_STREAMS[streamId]
+        local stream            = GetNPCByID(streamId)
+        local alive             = receptaclesAliveOnFloor(mob)
 
         -- Memory receptacles do not respawn until all on current floor are killed
-        DisallowRespawn(mob:getID(), true)
-        receptDead = receptDead + 1
-        zone:setLocalVar(string.format("receptDead[%i][%i]", mob:getZone():getID(), floor), receptDead)
-        mob:setAnimationSub(0)
+        DisallowRespawn(mobId, true)
+        mob:setAnimationSub(2)
 
-        -- open floor exit portal either random choice or all receptacles dead
-        for _, v in pairs(ID.mob.MEMORY_RECEPTACLES) do
-            if (v[1] == floor and receptDead == v[4]) or stream:getLocalVar("[promy]floorExit") == 1 then
-                randomizeFloorExit(ID, floor)
-                local events = ID.npc.MEMORY_STREAMS[streamId][7]
-                local event = events[math.random(#events)]
+        -- Random chance to open a portal, unless there are none left
+        local portalChance = 1 / (#alive + 1)
+
+        if math.random() < portalChance then
+            local zone   = mob:getZone()
+            if zone:getLocalVar(string.format("[MR][%s]", floor)) ~= 1 then
+                -- Only open the portal if no other portals in this group are open
+                local events = receptacleStreams.destinations
+                local event  = events[math.random(#events)]
+                zone:setLocalVar(string.format("[MR][%s]", floor), 1)
+
                 stream:setLocalVar("[promy]destination", event)
+                stream:setLocalVar("zoneportal", floor)
                 stream:openDoor(180)
-                break
+                stream:timer(179000, function(s)
+                    local mobFloor = s:getLocalVar("zoneportal")
+                    s:getZone():setLocalVar(string.format("[MR][%s]", mobFloor), 0)
+                end)
             end
         end
 
         -- Allow respawn if all receptacles on floor are dead
-        for _, v in pairs(ID.mob.MEMORY_RECEPTACLES) do
-            if v[1] == floor and receptDead == v[4] then
-                zone:setLocalVar(string.format("receptDead[%i][%i]", mob:getZone():getID(), floor), 0)
-                DisallowRespawn(mob:getID(), false)
-                mob:setRespawnTime(300)
+        if #alive == 0 then
+            local floorReceptacles = receptaclesInGroup(mob)
+            for _, receptacleId in pairs(floorReceptacles) do
+                DisallowRespawn(receptacleId, false)
+                local receptacle = GetMobByID(receptacleId)
+                if receptacle ~= nil then
+                    receptacle:setRespawnTime(300)
+                end
             end
         end
     end
@@ -195,12 +203,12 @@ end
 
 xi.promyvion.onTriggerAreaEnter = function(player, triggerArea)
     if player:getAnimation() == 0 then
-        local ID = zones[player:getZoneID()]
+        local streams = zones[player:getZoneID()].npc.MEMORY_STREAMS
         local triggerAreaID = triggerArea:GetTriggerAreaID()
         local event = nil
 
         if triggerAreaID < 100 then
-            event = ID.npc.MEMORY_STREAMS[triggerAreaID][7][1]
+            event = streams[triggerAreaID].destinations[1]
         else
             local stream = GetNPCByID(triggerAreaID)
             if stream ~= nil and stream:getAnimation() == xi.anim.OPEN_DOOR then
--- scripts/zones/Promyvion-Vahzl/IDs.lua
+++ scripts/zones/Promyvion-Vahzl/IDs.lua
@@ -30,46 +30,46 @@ zones[xi.zone.PROMYVION_VAHZL] =
     {
         MEMORY_RECEPTACLES =
         {
-            [16867387] = { 1, 3, 16867719, 2 },
-            [16867392] = { 1, 3, 16867720, 2 },
-            [16867439] = { 2, 5, 16867717, 2 },
-            [16867446] = { 2, 5, 16867718, 2 },
-            [16867504] = { 3, 5, 16867722, 4 },
-            [16867511] = { 3, 5, 16867723, 4 },
-            [16867518] = { 3, 5, 16867724, 4 },
-            [16867525] = { 3, 5, 16867725, 4 },
-            [16867601] = { 4, 7, 16867721, 3 },
-            [16867610] = { 4, 7, 16867726, 3 },
-            [16867619] = { 4, 7, 16867727, 3 },
+            [16867387] = { group = 1, strays = 3, stream = 16867719 },
+            [16867392] = { group = 1, strays = 3, stream = 16867720 },
+            [16867439] = { group = 2, strays = 5, stream = 16867717 },
+            [16867446] = { group = 2, strays = 5, stream = 16867718 },
+            [16867504] = { group = 3, strays = 5, stream = 16867722 },
+            [16867511] = { group = 3, strays = 5, stream = 16867723 },
+            [16867518] = { group = 3, strays = 5, stream = 16867724 },
+            [16867525] = { group = 3, strays = 5, stream = 16867725 },
+            [16867601] = { group = 4, strays = 7, stream = 16867721 },
+            [16867610] = { group = 4, strays = 7, stream = 16867726 },
+            [16867619] = { group = 4, strays = 7, stream = 16867727 },
         },
 
-        PONDERER                 = 16867329,
-        PROPAGATOR               = 16867330,
-        SOLICITOR                = 16867333,
-        DEVIATOR                 = 16867465,
-        WAILER                   = 16867554,
-        PROVOKER                 = 16867657,
+        PONDERER   = 16867329,
+        PROPAGATOR = 16867330,
+        SOLICITOR  = 16867333,
+        DEVIATOR   = 16867465,
+        WAILER     = 16867554,
+        PROVOKER   = 16867657,
     },
     npc =
     {
         MEMORY_STREAMS =
         {
-            [11]        = {   -2, -2, -122,    2, 2, -117, { 45 } }, -- floor 1 return
-            [21]        = {  -40, -2,  197,  -37, 2,  202, { 41 } }, -- floor 2 return
-            [31]        = {  317, -2, -282,  322, 2, -277, { 42 } }, -- floor 3 return
-            [41]        = {  277, -2,   38,  282, 2,   42, { 43 } }, -- floor 4 return
-            [51]        = {  -42, -2,   -2,  -36, 2,    2, { 44 } }, -- floor 5 return
-            [16867719]  = {  -43, -2, -362,  -36, 2, -356, { 33 } }, -- floor 1 MR1
-            [16867720]  = {   76, -2,  -43,   82, 2,  -37, { 32 } }, -- floor 1 MR2
-            [16867717]  = { -163, -2,  197, -156, 2,  203, { 30 } }, -- floor 2 MR1
-            [16867718]  = { -162, -2,  117, -156, 2,  123, { 31 } }, -- floor 2 MR2
-            [16867722]  = {  155, -2, -163,  163, 2, -156, { 37 } }, -- floor 3 MR1
-            [16867723]  = {  236, -2,  -43,  243, 2,  -36, { 35 } }, -- floor 3 MR2
-            [16867724]  = {  236, -2, -243,  243, 2, -236, { 38 } }, -- floor 3 MR3
-            [16867725]  = {  356, -2,  -82,  362, 2,  -76, { 36 } }, -- floor 3 MR4
-            [16867721]  = {  116, -2,   37,  122, 2,   42, { 39 } }, -- floor 4 MR1
-            [16867726]  = {  435, -2,   38,  443, 2,   41, { 40 } }, -- floor 4 MR2
-            [16867727]  = {  436, -2,  276,  443, 2,  283, { 34 } }, -- floor 4 MR3
+            [11]        = { triggerArea = {   -2, -2, -122,    2, 2, -117 }, destinations = { 45 } }, -- floor 1 return
+            [21]        = { triggerArea = {  -40, -2,  197,  -37, 2,  202 }, destinations = { 41 } }, -- floor 2 return
+            [31]        = { triggerArea = {  317, -2, -282,  322, 2, -277 }, destinations = { 42 } }, -- floor 3 return
+            [41]        = { triggerArea = {  277, -2,   38,  282, 2,   42 }, destinations = { 43 } }, -- floor 4 return
+            [51]        = { triggerArea = {  -42, -2,   -2,  -36, 2,    2 }, destinations = { 44 } }, -- floor 5 return
+            [16867719]  = { triggerArea = {  -43, -2, -362,  -36, 2, -356 }, destinations = { 33 } }, -- floor 1 MR1
+            [16867720]  = { triggerArea = {   76, -2,  -43,   82, 2,  -37 }, destinations = { 32 } }, -- floor 1 MR2
+            [16867717]  = { triggerArea = { -163, -2,  197, -156, 2,  203 }, destinations = { 30 } }, -- floor 2 MR1
+            [16867718]  = { triggerArea = { -162, -2,  117, -156, 2,  123 }, destinations = { 31 } }, -- floor 2 MR2
+            [16867722]  = { triggerArea = {  155, -2, -163,  163, 2, -156 }, destinations = { 37 } }, -- floor 3 MR1
+            [16867723]  = { triggerArea = {  236, -2,  -43,  243, 2,  -36 }, destinations = { 35 } }, -- floor 3 MR2
+            [16867724]  = { triggerArea = {  236, -2, -243,  243, 2, -236 }, destinations = { 38 } }, -- floor 3 MR3
+            [16867725]  = { triggerArea = {  356, -2,  -82,  362, 2,  -76 }, destinations = { 36 } }, -- floor 3 MR4
+            [16867721]  = { triggerArea = {  116, -2,   37,  122, 2,   42 }, destinations = { 39 } }, -- floor 4 MR1
+            [16867726]  = { triggerArea = {  435, -2,   38,  443, 2,   41 }, destinations = { 40 } }, -- floor 4 MR2
+            [16867727]  = { triggerArea = {  436, -2,  276,  443, 2,  283 }, destinations = { 34 } }, -- floor 4 MR3
         },
     },
 }
