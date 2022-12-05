54ff69ffa6a4ca964f3dffc384e23ce02825a342
OpheliaXI <heliashelium@gmail.com>
2022-12-04 19:10:08 -0600

    Lock Out OOE GP Rewards

--- scripts/zones/Northern_San_dOria/npcs/Andreas.lua
+++ scripts/zones/Northern_San_dOria/npcs/Andreas.lua
@@ -10,120 +10,23 @@ local ID = require("scripts/zones/Northern_San_dOria/IDs")
 -----------------------------------
 local entity = {}
 
-local keyitems =
-{
-    [0] =
-    {
-        id = xi.ki.WOOD_PURIFICATION,
-        rank = 3,
-        cost = 40000
-    },
-
-    [1] =
-    {
-        id = xi.ki.WOOD_ENSORCELLMENT,
-        rank = 3,
-        cost = 40000
-    },
-
-    [2] =
-    {
-        id = xi.ki.LUMBERJACK,
-        rank = 3,
-        cost = 10000
-    },
-
-    [3] =
-    {
-        id = xi.ki.BOLTMAKER,
-        rank = 3,
-        cost = 10000
-    },
-
-    [4] =
-    {
-        id = xi.ki.WAY_OF_THE_CARPENTER,
-        rank = 9,
-        cost = 20000
-    }
-}
-
-local items =
-{
-    [0] =
-    {
-        id = 15444, -- Carpenter's Belt
-        rank = 1,
-        cost = 10000
-    },
-
-    [1] =
-    {
-        id = 14830, -- Carpenter's Gloves
-        rank = 5,
-        cost = 70000
-    },
-
-    [2] =
-    {
-        id = 14392, -- Carpenter's Apron
-        rank = 7,
-        cost = 100000
-    },
-
-    [3] =
-    {
-        id = 28, -- Drawing Desk
-        rank = 9,
-        cost = 150000
-    },
-
-    [4] =
-    {
-        id = 341, -- Carpenter's Signboard
-        rank = 9,
-        cost = 200000
-    },
-
-    [5] =
-    {
-        id = 15819, -- Carpenter's Ring
-        rank = 6,
-        cost = 80000
-    },
-
-    [6] =
-    {
-        id = 3672, -- Carpenter's Kit
-        rank = 8,
-        cost = 50000
-    },
-
-    [7] =
-    {
-        id = 3331, -- Carpenter's Emblem
-        rank = 9,
-        cost = 15000
-    }
-}
-
 entity.onTrade = function(player, npc, trade)
     xi.crafting.unionRepresentativeTrade(player, npc, trade, 732, 1)
 end
 
 entity.onTrigger = function(player, npc)
-    xi.crafting.unionRepresentativeTrigger(player, 1, 731, "guild_woodworking", keyitems)
+    xi.crafting.unionRepresentativeTrigger(player, 1, 731, "guild_woodworking")
 end
 
 entity.onEventUpdate = function(player, csid, option, target)
     if csid == 731 then
-        xi.crafting.unionRepresentativeTriggerFinish(player, option, target, 1, "guild_woodworking", keyitems, items)
+        xi.crafting.unionRepresentativeTriggerFinish(player, option, target, 1, "guild_woodworking")
     end
 end
 
 entity.onEventFinish = function(player, csid, option, target)
     if csid == 731 then
-        xi.crafting.unionRepresentativeTriggerFinish(player, option, target, 1, "guild_woodworking", keyitems, items)
+        xi.crafting.unionRepresentativeTriggerFinish(player, option, target, 1, "guild_woodworking")
     elseif csid == 732 then
         player:messageSpecial(ID.text.GP_OBTAINED, option)
     end
--- scripts/zones/Port_Windurst/npcs/Fennella.lua
+++ scripts/zones/Port_Windurst/npcs/Fennella.lua
@@ -10,113 +10,23 @@ local ID = require("scripts/zones/Port_Windurst/IDs")
 -----------------------------------
 local entity = {}
 
-local keyitems =
-{
-    [0] =
-    {
-        id = xi.ki.FROG_FISHING,
-        rank = 3,
-        cost = 30000
-    },
-
-    [1] =
-    {
-        id = xi.ki.SERPENT_RUMORS,
-        rank = 8,
-        cost = 95000
-    },
-
-    [2] =
-    {
-        id = xi.ki.MOOCHING,
-        rank = 9,
-        cost = 115000
-    },
-
-    [3] =
-    {
-        id = xi.ki.ANGLERS_ALMANAC,
-        rank = 9,
-        cost = 20000
-    }
-}
-
-local items =
-{
-    [0] =
-    {
-        id = 17002, -- Robber's Rig
-        rank = 3,
-        cost = 1500
-    },
-
-    [1] =
-    {
-        id = 15452, -- Fisherman's Belt
-        rank = 4,
-        cost = 10000
-    },
-
-    [2] =
-    {
-        id = 14195, -- Pair of Waders
-        rank = 5,
-        cost = 70000
-    },
-
-    [3] =
-    {
-        id = 14400, -- Fisherman's Apron
-        rank = 7,
-        cost = 100000
-    },
-
-    [4] =
-    {
-        id = 191, -- Fishing hole map
-        rank = 9,
-        cost = 150000
-    },
-
-    [5] =
-    {
-        id = 340, -- Fisherman's Signboard
-        rank = 9,
-        cost = 200000
-    },
-
-    [6] =
-    {
-        id = 3670, -- Fish and Lure
-        rank = 7,
-        cost = 50000
-    },
-
-    [7] =
-    {
-        id = 3330, -- Fishermen's Emblem
-        rank = 9,
-        cost = 15000
-    }
-}
-
 entity.onTrade = function(player, npc, trade)
     xi.crafting.unionRepresentativeTrade(player, npc, trade, 10021, 0)
 end
 
 entity.onTrigger = function(player, npc)
-    xi.crafting.unionRepresentativeTrigger(player, 0, 10020, "guild_fishing", keyitems)
+    xi.crafting.unionRepresentativeTrigger(player, 0, 10020, "guild_fishing")
 end
 
 entity.onEventUpdate = function(player, csid, option, target)
     if csid == 10020 then
-        xi.crafting.unionRepresentativeTriggerFinish(player, option, target, 0, "guild_Fishing", keyitems, items)
+        xi.crafting.unionRepresentativeTriggerFinish(player, option, target, 0, "guild_Fishing")
     end
 end
 
 entity.onEventFinish = function(player, csid, option, target)
     if csid == 10020 then
-        xi.crafting.unionRepresentativeTriggerFinish(player, option, target, 0, "guild_Fishing", keyitems, items)
+        xi.crafting.unionRepresentativeTriggerFinish(player, option, target, 0, "guild_Fishing")
     elseif csid == 10021 then
         player:messageSpecial(ID.text.GP_OBTAINED, option)
     end
