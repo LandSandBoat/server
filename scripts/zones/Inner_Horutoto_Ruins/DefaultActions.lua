fc6823a1047babec11f52413b75000e05c035dc2
dallano <atwood.dallan@hotmail.com>
2022-12-07 15:28:03 -0700

    Update DefaultActions.lua
    Fixes players requiring to click the door twice to complete Making Headlines quest

--- scripts/zones/Inner_Horutoto_Ruins/DefaultActions.lua
+++ scripts/zones/Inner_Horutoto_Ruins/DefaultActions.lua
@@ -1,7 +1,7 @@
 local ID = require("scripts/zones/Inner_Horutoto_Ruins/IDs")
 
 return {
-    ['_5ca'] = { event = 44 },
+    ['_5ca'] = { messageSpecial = ID.text.DOOR_FIRMLY_CLOSED },
     ['_5cb'] = { messageSpecial = ID.text.DOOR_FIRMLY_CLOSED },
     ['_5ci'] = { messageSpecial = ID.text.DOOR_FIRMLY_CLOSED },
     ['_5c5'] = { messageSpecial = ID.text.DOOR_FIRMLY_CLOSED },
