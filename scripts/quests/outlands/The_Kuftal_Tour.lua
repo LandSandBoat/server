14763a5add0de4df8f14952b2a64c261d77d296c
Abdiah <62350957+dallano@users.noreply.github.com>
2022-12-10 14:46:45 -0700

    "The Kuftal Tour" Quest Implementation (#2113)
    * "The Kuftal Tour" Quest Implementation
    
    Implements the quest "The Kuftal Tour"
    
    * Update The_Kuftal_Tour.lua
    
    * Update The_Kuftal_Tour.lua
    
    * Update The_Kuftal_Tour.lua
    
    * Update The_Kuftal_Tour.lua

---
+++ scripts/quests/outlands/The_Kuftal_Tour.lua
@@ -0,0 +1,110 @@
+-----------------------------------
+-- The Kuftal Tour
+-----------------------------------
+-- Log ID: 5, Quest ID: 195
+--  Datta: !pos -6 -25 5
+-----------------------------------
+require('scripts/globals/items')
+require('scripts/globals/npc_util')
+require('scripts/globals/quests')
+require('scripts/globals/titles')
+require('scripts/globals/zone')
+require('scripts/globals/interaction/quest')
+-----------------------------------
+
+local quest = Quest:new(xi.quest.log_id.OUTLANDS, xi.quest.id.outlands.THE_KUFTAL_TOUR)
+
+quest.reward =
+{
+    fame     = 30,
+    fameArea = xi.quest.fame_area.SELBINA_RABAO,
+    gil      = 8000,
+    title    = xi.title.KUFTAL_TOURIST,
+}
+
+quest.sections =
+{
+    {
+        check = function(player, status, vars)
+            return status == QUEST_AVAILABLE and
+            player:getFameLevel(xi.quest.fame_area.OUTLANDS) >= 3
+        end,
+
+        [xi.zone.RABAO] =
+        {
+            ['Datta'] = quest:progressEvent(74),
+
+            onEventFinish =
+            {
+                [74] = function(player, csid, option, npc)
+                    if option == 0 then
+                        quest:begin(player)
+                    end
+                end,
+            },
+        },
+    },
+
+    {
+        check = function(player, status, vars)
+            return status == QUEST_ACCEPTED
+        end,
+
+        [xi.zone.RABAO] =
+        {
+            ['Datta'] =
+            {
+                onTrigger = function(player, npc)
+                    if quest:getVar(player, 'Prog') == 1 then
+                        return quest:progressEvent(75)
+                    end
+                end,
+            },
+
+            onEventFinish =
+            {
+                [75] = function(player, csid, option, npc)
+                    quest:complete(player)
+                end,
+            },
+        },
+
+        [xi.zone.KUFTAL_TUNNEL] =
+        {
+            ['Hawk_Nose'] =
+            {
+                onTrigger = function(player, npc)
+                    local flag = true
+
+                    for _, member in pairs(player:getAlliance()) do
+                        if
+                            member:getMainLvl() > 40 or
+                            member:checkDistance(player) > 15
+                        then
+                            flag = false
+                        end
+                    end
+
+                    if
+                        flag and
+                        quest:getVar(player, 'Prog') == 0 and
+                        #player:getParty() > 1
+                    then
+                        return quest:progressEvent(14)
+                    end
+                end,
+            },
+
+            onEventFinish =
+            {
+                [14] = function(player, csid, option, npc)
+                    for _, member in pairs(player:getAlliance()) do
+                        quest:setVar(member, 'Prog', 1)
+                    end
+                end,
+            },
+        },
+    },
+}
+
+return quest
