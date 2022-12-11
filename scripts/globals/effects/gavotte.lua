d92c49dce09aebbab87644dfc7ba2789339736ef
Xaver-DaRed <xaver.entropia@gmail.com>
2022-12-11 16:37:47 +0100

    Use STATUSRES in Bard Song Effects

--- scripts/globals/effects/gavotte.lua
+++ scripts/globals/effects/gavotte.lua
@@ -4,7 +4,7 @@
 local effectObject = {}
 
 effectObject.onEffectGain = function(target, effect)
-    target:addMod(xi.mod.BIND_MEVA, effect:getPower())
+    target:addMod(xi.mod.BINDRES, effect:getPower())
     target:addMod(xi.mod.STR, effect:getSubPower()) -- Apply Stat Buff from AUGMENT_SONG_STAT
 end
 
@@ -12,7 +12,7 @@ effectObject.onEffectTick = function(target, effect)
 end
 
 effectObject.onEffectLose = function(target, effect)
-    target:delMod(xi.mod.BIND_MEVA, effect:getPower())
+    target:delMod(xi.mod.BINDRES, effect:getPower())
     target:delMod(xi.mod.STR, effect:getSubPower()) -- Remove Stat Buff from AUGMENT_SONG_STAT
 end
 
--- scripts/globals/effects/round.lua
+++ scripts/globals/effects/round.lua
@@ -4,7 +4,7 @@
 local effectObject = {}
 
 effectObject.onEffectGain = function(target, effect)
-    target:addMod(xi.mod.CURSE_MEVA, effect:getPower())
+    target:addMod(xi.mod.CURSERES, effect:getPower())
     target:addMod(xi.mod.CHR, effect:getSubPower()) -- Apply Stat Buff from AUGMENT_SONG_STAT
 end
 
@@ -12,7 +12,7 @@ effectObject.onEffectTick = function(target, effect)
 end
 
 effectObject.onEffectLose = function(target, effect)
-    target:delMod(xi.mod.CURSE_MEVA, effect:getPower())
+    target:delMod(xi.mod.CURSERES, effect:getPower())
     target:delMod(xi.mod.CHR, effect:getSubPower()) -- Remove Stat Buff from AUGMENT_SONG_STAT
 end
 
