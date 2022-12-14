d006b74f9a0c9e3811e85d7c3deb2b7228f44f52
Panic Stevenson <panic.stevenson@gmail.com>
2022-12-14 14:57:49 -0800

    Fix Familiar

--- scripts/globals/abilities/familiar.lua
+++ scripts/globals/abilities/familiar.lua
@@ -16,7 +16,7 @@ abilityObject.onAbilityCheck = function(player, target, ability)
     if not pet then
         return xi.msg.basic.REQUIRES_A_PET, 0
     elseif
-        (not player:isJugPet() and pet:getObjType() ~= xi.objType.MOB) or
+        (not pet:isJugPet() and pet:getObjType() ~= xi.objType.MOB) or
         pet:getLocalVar("ReceivedFamiliar") == 1
     then
         return xi.msg.basic.NO_EFFECT_ON_PET, 0
@@ -24,7 +24,6 @@ abilityObject.onAbilityCheck = function(player, target, ability)
         return xi.msg.basic.NO_EFFECT_ON_PET, 0
     end
 
-    pet:setLocalVar("ReceivedFamiliar", 1)
     ability:setRecast(ability:getRecast() - player:getMod(xi.mod.ONE_HOUR_RECAST))
 
     return 0, 0
@@ -33,7 +32,7 @@ end
 abilityObject.onUseAbility = function(player, target, ability)
     player:getPet():setLocalVar("ReceivedFamiliar", 1)
     player:familiar()
-    target:setLocalVar("ReceivedFamiliar", 1)
+
     -- pets powers increase!
     ability:setMsg(xi.msg.basic.FAMILIAR_PC)
 
