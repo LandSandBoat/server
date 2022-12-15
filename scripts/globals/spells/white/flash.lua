a831fe5b044bf7fe2ca3d935ab056725a19e21cf
Xaver-DaRed <xaver.entropia@gmail.com>
2022-12-15 18:11:14 +0100

    Convert break family, curse, flash and inundation

--- scripts/globals/spells/white/flash.lua
+++ scripts/globals/spells/white/flash.lua
@@ -1,10 +1,7 @@
 -----------------------------------
 -- Spell: Flash
--- Temporarily blinds an enemy, greatly lowering its accuracy.
 -----------------------------------
-require("scripts/globals/status")
-require("scripts/globals/magic")
-require("scripts/globals/msg")
+require("scripts/globals/spells/enfeebling_spell")
 -----------------------------------
 local spellObject = {}
 
@@ -13,30 +10,7 @@ spellObject.onMagicCastingCheck = function(caster, target, spell)
 end
 
 spellObject.onSpellCast = function(caster, target, spell)
-    -- Pull base stats.
-    -- local dINT = (caster:getStat(xi.mod.MND) - target:getStat(xi.mod.MND))
-
-    local params = {}
-    params.diff = nil
-    params.attribute = xi.mod.INT
-    params.skillType = xi.skill.DIVINE_MAGIC
-    params.bonus =  150
-    params.effect = nil
-
-    local resist = applyResistance(caster, target, spell, params)
-    local duration = 12 * resist
-
-    if resist > 0.0625 then
-        if target:addStatusEffect(xi.effect.FLASH, 200, 0, duration) then
-            spell:setMsg(xi.msg.basic.MAGIC_ENFEEB_IS)
-        else
-            spell:setMsg(xi.msg.basic.MAGIC_NO_EFFECT)
-        end
-    else
-        spell:setMsg(xi.msg.basic.MAGIC_RESIST)
-    end
-
-    return xi.effect.FLASH
+    return xi.spells.enfeebling.useEnfeeblingSpell(caster, target, spell)
 end
 
 return spellObject
