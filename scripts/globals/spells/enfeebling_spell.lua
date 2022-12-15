9278a0b07c640c12e5a8ce7e2c26ad758fb493ca
Xaver-DaRed <xaver.entropia@gmail.com>
2022-12-15 15:13:42 +0100

    Convert Slow and Paralyze spell families

--- scripts/globals/spells/enfeebling_spell.lua
+++ scripts/globals/spells/enfeebling_spell.lua
@@ -21,29 +21,34 @@ local e = xi.effect
 local m = xi.mod
 
 local pTable =
-{   --                   1           2          3             4               5      6    7         8       9           10         11
-    --    [Spell ID] = { Effect,     Stat-Used, Resist-Mod,   MEVA-Mod,       pBase, DoT, Duration, Resist, Bonus-MACC, pSaboteur, pResist },
-    [s.BIND        ] = { e.BIND,     m.INT,     m.BINDRES,    m.BIND_MEVA,        0,   0,       60,      2,          0, false,     false   },
-    [s.BINDGA      ] = { e.BIND,     m.INT,     m.BINDRES,    m.BIND_MEVA,        0,   0,       60,      2,          0, false,     false   },
-    [s.BLIND       ] = { e.BLIND,    m.INT,     m.BLINDRES,   m.BLIND_MEVA,       0,   0,      180,      2,          0, true,      false   },
-    [s.BLIND_II    ] = { e.BLIND,    m.INT,     m.BLINDRES,   m.BLIND_MEVA,       0,   0,      180,      2,          0, true,      false   },
-    [s.BLINDGA     ] = { e.BLIND,    m.INT,     m.BLINDRES,   m.BLIND_MEVA,       0,   0,      180,      2,          0, true,      false   },
-    [s.GRAVITY     ] = { e.WEIGHT,   m.INT,     m.GRAVITYRES, m.GRAVITY_MEVA,    26,   0,      120,      2,          0, true,      false   },
-    [s.GRAVITY_II  ] = { e.WEIGHT,   m.INT,     m.GRAVITYRES, m.GRAVITY_MEVA,    32,   0,      180,      2,          0, true,      false   },
-    [s.GRAVIGA     ] = { e.WEIGHT,   m.INT,     m.GRAVITYRES, m.GRAVITY_MEVA,    50,   0,      120,      2,          0, true,      false   },
-    [s.POISON      ] = { e.POISON,   m.INT,     m.POISONRES,  m.POISON_MEVA,      0,   3,       90,      2,          0, true,      false   },
-    [s.POISON_II   ] = { e.POISON,   m.INT,     m.POISONRES,  m.POISON_MEVA,      0,   3,      120,      2,          0, true,      false   },
-    [s.POISON_III  ] = { e.POISON,   m.INT,     m.POISONRES,  m.POISON_MEVA,      0,   3,      150,      2,          0, true,      false   },
-    [s.POISONGA    ] = { e.POISON,   m.INT,     m.POISONRES,  m.POISON_MEVA,      0,   3,       90,      2,          0, true,      false   },
-    [s.POISONGA_II ] = { e.POISON,   m.INT,     m.POISONRES,  m.POISON_MEVA,      0,   3,      120,      2,          0, true,      false   },
-    [s.POISONGA_III] = { e.POISON,   m.INT,     m.POISONRES,  m.POISON_MEVA,      0,   3,      150,      2,          0, true,      false   },
-    [s.SILENCE     ] = { e.SILENCE,  m.MND,     m.SILENCERES, m.SILENCE_MEVA,     1,   0,      120,      2,          0, false,     false   },
-    [s.SILENCEGA   ] = { e.SILENCE,  m.MND,     m.SILENCERES, m.SILENCE_MEVA,     1,   0,      120,      2,          0, false,     false   },
-    [s.SLEEP       ] = { e.SLEEP_I,  m.INT,     m.SLEEPERES,  m.SLEEP_MEVA,       1,   0,       60,      2,          0, false,     false   },
-    [s.SLEEP_II    ] = { e.SLEEP_I,  m.INT,     m.SLEEPERES,  m.SLEEP_MEVA,       2,   0,       60,      2,          0, false,     false   },
-    [s.SLEEPGA     ] = { e.SLEEP_II, m.INT,     m.SLEEPERES,  m.SLEEP_MEVA,       1,   0,       90,      2,          0, false,     false   },
-    [s.SLEEPGA_II  ] = { e.SLEEP_II, m.INT,     m.SLEEPERES,  m.SLEEP_MEVA,       2,   0,       90,      2,          0, false,     false   },
-    [s.SLOW        ] = { e.SLOW,     m.MND,     m.SLOWRES,    m.SLOW_MEVA,        0,   0,      180,      2,          0, true,      true    },
+{   --                   1            2          3              4               5      6    7         8       9           10         11
+    --    [Spell ID] = { Effect,      Stat-Used, Resist-Mod,    MEVA-Mod,       pBase, DoT, Duration, Resist, Bonus-MACC, pSaboteur, pResist },
+    [s.BIND        ] = { e.BIND,      m.INT,     m.BINDRES,     m.BIND_MEVA,        0,   0,       60,      2,          0, false,     false   },
+    [s.BINDGA      ] = { e.BIND,      m.INT,     m.BINDRES,     m.BIND_MEVA,        0,   0,       60,      2,          0, false,     false   },
+    [s.BLIND       ] = { e.BLIND,     m.INT,     m.BLINDRES,    m.BLIND_MEVA,       0,   0,      180,      2,          0, true,      false   },
+    [s.BLIND_II    ] = { e.BLIND,     m.INT,     m.BLINDRES,    m.BLIND_MEVA,       0,   0,      180,      2,          0, true,      false   },
+    [s.BLINDGA     ] = { e.BLIND,     m.INT,     m.BLINDRES,    m.BLIND_MEVA,       0,   0,      180,      2,          0, true,      false   },
+    [s.GRAVITY     ] = { e.WEIGHT,    m.INT,     m.GRAVITYRES,  m.GRAVITY_MEVA,    26,   0,      120,      2,          0, true,      false   },
+    [s.GRAVITY_II  ] = { e.WEIGHT,    m.INT,     m.GRAVITYRES,  m.GRAVITY_MEVA,    32,   0,      180,      2,          0, true,      false   },
+    [s.GRAVIGA     ] = { e.WEIGHT,    m.INT,     m.GRAVITYRES,  m.GRAVITY_MEVA,    50,   0,      120,      2,          0, true,      false   },
+    [s.PARALYZE    ] = { e.PARALYSIS, m.MND,     m.PARALYZERES, m.PARALYZE_MEVA,    0,   0,      120,      2,          0, true,      true    },
+    [s.PARALYZE_II ] = { e.PARALYSIS, m.MND,     m.PARALYZERES, m.PARALYZE_MEVA,    0,   0,      120,      2,          0, true,      true    },
+    [s.PARALYGA    ] = { e.PARALYSIS, m.MND,     m.PARALYZERES, m.PARALYZE_MEVA,    0,   0,      120,      2,          0, true,      true    },
+    [s.POISON      ] = { e.POISON,    m.INT,     m.POISONRES,   m.POISON_MEVA,      0,   3,       90,      2,          0, true,      false   },
+    [s.POISON_II   ] = { e.POISON,    m.INT,     m.POISONRES,   m.POISON_MEVA,      0,   3,      120,      2,          0, true,      false   },
+    [s.POISON_III  ] = { e.POISON,    m.INT,     m.POISONRES,   m.POISON_MEVA,      0,   3,      150,      2,          0, true,      false   },
+    [s.POISONGA    ] = { e.POISON,    m.INT,     m.POISONRES,   m.POISON_MEVA,      0,   3,       90,      2,          0, true,      false   },
+    [s.POISONGA_II ] = { e.POISON,    m.INT,     m.POISONRES,   m.POISON_MEVA,      0,   3,      120,      2,          0, true,      false   },
+    [s.POISONGA_III] = { e.POISON,    m.INT,     m.POISONRES,   m.POISON_MEVA,      0,   3,      150,      2,          0, true,      false   },
+    [s.SILENCE     ] = { e.SILENCE,   m.MND,     m.SILENCERES,  m.SILENCE_MEVA,     1,   0,      120,      2,          0, false,     false   },
+    [s.SILENCEGA   ] = { e.SILENCE,   m.MND,     m.SILENCERES,  m.SILENCE_MEVA,     1,   0,      120,      2,          0, false,     false   },
+    [s.SLEEP       ] = { e.SLEEP_I,   m.INT,     m.SLEEPERES,   m.SLEEP_MEVA,       1,   0,       60,      2,          0, false,     false   },
+    [s.SLEEP_II    ] = { e.SLEEP_II,  m.INT,     m.SLEEPERES,   m.SLEEP_MEVA,       2,   0,       90,      2,          0, false,     false   },
+    [s.SLEEPGA     ] = { e.SLEEP_I,   m.INT,     m.SLEEPERES,   m.SLEEP_MEVA,       1,   0,       60,      2,          0, false,     false   },
+    [s.SLEEPGA_II  ] = { e.SLEEP_II,  m.INT,     m.SLEEPERES,   m.SLEEP_MEVA,       2,   0,       90,      2,          0, false,     false   },
+    [s.SLOW        ] = { e.SLOW,      m.MND,     m.SLOWRES,     m.SLOW_MEVA,        0,   0,      180,      2,          0, true,      true    },
+    [s.SLOW_II     ] = { e.SLOW,      m.MND,     m.SLOWRES,     m.SLOW_MEVA,        0,   0,      180,      2,          0, true,      true    },
+    [s.SLOWGA      ] = { e.SLOW,      m.MND,     m.SLOWRES,     m.SLOW_MEVA,        0,   0,      180,      2,          0, true,      true    },
 }
 
 -- Determine if target mob is immune to a status effect.
@@ -51,11 +56,12 @@ xi.spells.enfeebling.checkInnateImmunity = function(target, spellEffect)
     local immunityTable =
     {
         [xi.effect.SLEEP      ] = {     1 },
+        [xi.effect.SLEEP_II   ] = {     1 },
         [xi.effect.GRAVITY    ] = {     2 },
         [xi.effect.BIND       ] = {     4 },
         [xi.effect.STUN       ] = {     8 },
         [xi.effect.SILENCE    ] = {    16 },
-        [xi.effect.PARALYZE   ] = {    32 },
+        [xi.effect.PARALYSIS  ] = {    32 },
         [xi.effect.BLIND      ] = {    64 },
         [xi.effect.SLOW       ] = {   128 },
         [xi.effect.POISON     ] = {   256 },
@@ -98,49 +104,61 @@ xi.spells.enfeebling.checkTraitTrigger = function(caster, target, spellId)
 end
 
 xi.spells.enfeebling.calculatePotency = function(caster, target, spellId, spellEffect, skillType, statUsed)
-    local potency  = pTable[spellId][5]
-    local statDiff = caster:getStat(statUsed) - target:getStat(statUsed)
+    local potency    = pTable[spellId][5]
+    local statDiff   = caster:getStat(statUsed) - target:getStat(statUsed)
+    local skillLevel = caster:getSkillLevel(skillType)
 
     -- Calculate base potency for spells.
+    switch (spellEffect) : caseof
+    {
+        [xi.effect.BLIND] = function()
+            statDiff = caster:getStat(statUsed) - target:getStat(xi.mod.MND)
 
-    -- Blind Family
-    if spellEffect == xi.effect.BLIND then
-        statDiff = caster:getStat(statUsed) - target:getStat(xi.mod.MND)
-
-        if spellId == xi.magic.spell.BLIND_II then
-            potency = utils.clamp(math.floor(statDiff * 0.375 + 49), 15, 90) -- Values from JP wiki: http://wiki.ffo.jp/html/3449.html
-        else
-            potency = utils.clamp(math.floor(statDiff * 0.225 + 23), 5, 50)  -- Values from JP wiki: http://wiki.ffo.jp/html/834.html
-        end
-
-    -- Poison Family
-    elseif spellEffect == xi.effect.POISON then
-        local skillLevel = caster:getSkillLevel(xi.skill.ENFEEBLING_MAGIC)
-
-        if
-            spellId == xi.magic.spell.POISON or
-            spellId == xi.magic.spell.POISONGA
-        then
-            potency = math.max(skillLevel / 25, 1)
-            if skillLevel > 400 then
-                potency = math.min((skillLevel - 225) / 5, 55) -- Cap is 55 hp/tick.
+            if spellId == xi.magic.spell.BLIND_II then
+                potency = utils.clamp(math.floor(statDiff * 0.375 + 49), 15, 90) -- Values from JP wiki: http://wiki.ffo.jp/html/3449.html
+            else
+                potency = utils.clamp(math.floor(statDiff * 0.225 + 23), 5, 50)  -- Values from JP wiki: http://wiki.ffo.jp/html/834.html
             end
-        elseif
-            spellId == xi.magic.spell.POISON_II or
-            spellId == xi.magic.spell.POISONGA_II
-        then
-            potency = math.max(skillLevel / 20, 4)
-            if skillLevel > 400 then
-                potency = math.floor(skillLevel * 49 / 183 - 55) -- No cap can be reached yet
-            end
-        else
-            potency = skillLevel / 10 + 1
-        end
+        end,
 
-    -- Slow Family
-    elseif spellEffect == xi.effect.SLOW then
-        potency = potency * statDiff
-    end
+        [xi.effect.PARALYSIS] = function()
+            if spellId == xi.magic.spell.PARALYZE_II then
+                potency = utils.clamp(statDiff / 4 + 20, 10, 30)
+            else
+                potency = utils.clamp(statDiff / 4 + 15, 5, 25)
+            end
+        end,
+
+        [xi.effect.POISON] = function()
+            if
+                spellId == xi.magic.spell.POISON or
+                spellId == xi.magic.spell.POISONGA
+            then
+                potency = math.max(skillLevel / 25, 1)
+                if skillLevel > 400 then
+                    potency = math.min((skillLevel - 225) / 5, 55) -- Cap is 55 hp/tick.
+                end
+            elseif
+                spellId == xi.magic.spell.POISON_II or
+                spellId == xi.magic.spell.POISONGA_II
+            then
+                potency = math.max(skillLevel / 20, 4)
+                if skillLevel > 400 then
+                    potency = skillLevel * 49 / 183 - 55 -- No cap can be reached yet
+                end
+            else
+                potency = skillLevel / 10 + 1
+            end
+        end,
+
+        [xi.effect.SLOW] = function()
+            if spellId == xi.magic.spell.SLOW_II then
+                potency = utils.clamp(statDiff * 226 / 15 + 2380, 1250, 3510)
+            else
+                potency = utils.clamp(statDiff * 73 / 5 + 1825, 730, 2920)
+            end
+        end,
+    }
 
     -- Apply Saboteur Effect when aplicable.
     if
@@ -207,7 +225,8 @@ xi.spells.enfeebling.useEnfeeblingSpell = function(caster, target, spell)
     local traitTrigger = xi.spells.enfeebling.calculateTraitTrigger(caster, target, spellId)
 
     if traitTrigger then
-        spell:setMsg(xi.msg.basic.MAGIC_RESIST_2) -- TODO: Confirm correct message.
+        -- TODO: Change action modifier so Resist! appears.
+        spell:setMsg(xi.msg.basic.MAGIC_RESIST)
 
         return spellEffect
     end
