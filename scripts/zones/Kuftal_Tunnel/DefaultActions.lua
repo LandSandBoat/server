806bf4b4dbc14d9e946ad8237a9e34d17dc6c3cc
Abdiah <62350957+dallano@users.noreply.github.com>
2022-12-14 21:05:19 -0700

    Update DefaultActions.lua

--- scripts/zones/Kuftal_Tunnel/DefaultActions.lua
+++ scripts/zones/Kuftal_Tunnel/DefaultActions.lua
@@ -2,6 +2,6 @@ local ID = require("scripts/zones/Kuftal_Tunnel/IDs")
 
 return {
     ['qm3']         = { messageSpecial = ID.text.NOTHING_OUT_OF_ORDINARY },
-    ['Kuftal_Tour'] = { messageSpecial = ID.text.DO_NOT_SEE_ANYTHING     },
+    ['qm5'] = { messageSpecial = ID.text.DO_NOT_SEE_ANYTHING     },
     ['qm6']         = { messageSpecial = ID.text.DO_NOT_SEE_ANYTHING     },
 }
