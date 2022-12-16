831e30d60c179a8a84c3d145083469a4fab88f3b
Frankie-hz <105882754+Frankie-hz@users.noreply.github.com>
2022-12-16 01:11:22 -0500

    Diremite Mobskills (#2210)
    * Diremite Mobskills
    
    * Update scripts/globals/mobskills/filamented_hold.lua
    
    Co-authored-by: Panic Stevenson <panic.stevenson@gmail.com>
    
    Co-authored-by: Panic Stevenson <panic.stevenson@gmail.com>

--- scripts/globals/mobskills/double_claw.lua
+++ scripts/globals/mobskills/double_claw.lua
@@ -14,9 +14,7 @@ end
 
 mobskillObject.onMobWeaponSkill = function(target, mob, skill)
     local numhits = 1
-    local accmod = 1
-    local attmod = 1.5
-    local info = xi.mobskills.mobPhysicalMove(mob, target, skill, numhits, accmod, 1, xi.mobskills.physicalTpBonus.DMG_VARIES, 1.5, 2, 2.5, 0, attmod)
+    local info = xi.mobskills.mobPhysicalMove(mob, target, skill, numhits, 1, 1, xi.mobskills.physicalTpBonus.DMG_VARIES, 1.5, 2, 2.5)
     local dmg = xi.mobskills.mobFinalAdjustments(info.dmg, mob, skill, target, xi.attackType.PHYSICAL, xi.damageType.SLASHING, info.hitslanded)
     target:takeDamage(dmg, mob, xi.attackType.PHYSICAL, xi.damageType.SLASHING)
     return dmg
