553d1a9ee908099c29992e220e14bebba0d2fa0f
TracentEden <92269743+TracentEden@users.noreply.github.com>
2022-12-18 22:50:09 +0200

    Remove duplicate out of era qm1 for quest and set hide to 1 second

--- scripts/zones/South_Gustaberg/npcs/qm1.lua
+++ scripts/zones/South_Gustaberg/npcs/qm1.lua
@@ -14,7 +14,7 @@ entity.onTrade = function(player, npc, trade)
         (npcUtil.tradeHas(trade, 4514) or npcUtil.tradeHas(trade, 5793)) and
         player:getQuestStatus(xi.quest.log_id.BASTOK, xi.quest.id.bastok.THE_COLD_LIGHT_OF_DAY) >= QUEST_ACCEPTED
     then
-        npcUtil.popFromQM(player, npc, ID.mob.BUBBLY_BERNIE, { hide = 0 })
+        npcUtil.popFromQM(player, npc, ID.mob.BUBBLY_BERNIE, { hide = 1 })
         player:confirmTrade()
     else
         player:messageSpecial(ID.text.NOTHING_OUT_OF_ORDINARY)
