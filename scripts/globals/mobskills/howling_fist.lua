9b65684eca2cb90c3610f06a4c5f5fbde91fb89e
Frankie-hz <105882754+Frankie-hz@users.noreply.github.com>
2022-12-21 02:53:42 -0500

    Missing variable

--- scripts/globals/mobskills/howling_fist.lua
+++ scripts/globals/mobskills/howling_fist.lua
@@ -16,7 +16,7 @@ end
 mobskillObject.onMobWeaponSkill = function(target, mob, skill)
     local numhits = 2
     local attmod = 1.5
-    local info = xi.mobskills.mobPhysicalMove(mob, target, skill, numhits, accmod, dmgmod, xi.mobskills.physicalTpBonus.DMG_VARIES, 2.5, 2.5, 2.5, 0, attmod)
+    local info = xi.mobskills.mobPhysicalMove(mob, target, skill, numhits, 1, 1, xi.mobskills.physicalTpBonus.DMG_VARIES, 2.5, 2.5, 2.5, 0, attmod)
     local dmg = xi.mobskills.mobFinalAdjustments(info.dmg, mob, skill, target, xi.attackType.PHYSICAL, xi.damageType.HTH, info.hitslanded)
 
     target:takeDamage(dmg, mob, xi.attackType.PHYSICAL, xi.damageType.HTH)
