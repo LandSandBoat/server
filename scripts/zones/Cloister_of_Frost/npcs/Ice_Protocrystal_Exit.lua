03e396834dd18d1a030d89ff801140803d9100e3
dallano <atwood.dallan@hotmail.com>
2022-12-24 20:05:03 -0700

    BCNM "Run Away" Fix
    - Allows players to exit battlefield from within if they've lost they're battlefield status effect

---
+++ scripts/zones/Cloister_of_Frost/npcs/Ice_Protocrystal_Exit.lua
@@ -0,0 +1,26 @@
+-----------------------------------
+-- Area: Cloister of Frost
+--  NPC: Ice Protocrystal
+-- Involved in Quests: Trial by Ice, Trial Size Trial by Ice
+-----------------------------------
+require("scripts/globals/bcnm")
+-----------------------------------
+local entity = {}
+
+entity.onTrade = function(player, npc, trade)
+    xi.bcnm.onTrade(player, npc, trade)
+end
+
+entity.onTrigger = function(player, npc)
+    player:startOptionalCutscene(32003)
+end
+
+entity.onEventUpdate = function(player, csid, option, extras)
+    xi.bcnm.onEventUpdate(player, csid, option, extras)
+end
+
+entity.onEventFinish = function(player, csid, option)
+    xi.bcnm.onEventFinish(player, csid, option)
+end
+
+return entity
---
+++ scripts/zones/QuBia_Arena/npcs/Burning_Circle_Exit.lua
@@ -0,0 +1,27 @@
+-----------------------------------
+-- Area: Qu'Bia Arena
+--  NPC: Burning Circle Exit
+-----------------------------------
+require("scripts/globals/bcnm")
+require("scripts/globals/missions")
+require("scripts/globals/keyitems")
+-----------------------------------
+local entity = {}
+
+entity.onTrade = function(player, npc, trade)
+    xi.bcnm.onTrade(player, npc, trade)
+end
+
+entity.onTrigger = function(player, npc)
+    player:startOptionalCutscene(32003)
+end
+
+entity.onEventUpdate = function(player, csid, option, extras)
+    xi.bcnm.onEventUpdate(player, csid, option, extras)
+end
+
+entity.onEventFinish = function(player, csid, option)
+    xi.bcnm.onEventFinish(player, csid, option)
+end
+
+return entity
