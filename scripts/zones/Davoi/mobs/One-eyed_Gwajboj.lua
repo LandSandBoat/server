d39efa738b862e4bb47182cbefd2071d9660909c
Panic Stevenson <panic.stevenson@gmail.com>
2022-12-27 12:36:11 -0800

    Add 2-Hours for PLD AF3 NMs

---
+++ scripts/zones/Davoi/mobs/One-eyed_Gwajboj.lua
@@ -0,0 +1,12 @@
+-----------------------------------
+-- Area: Davoi
+--  Mob: One-eyed Gwajboj
+-----------------------------------
+mixins = { require("scripts/mixins/job_special") }
+-----------------------------------
+local entity = {}
+
+entity.onMobDeath = function(mob, player, optParams)
+end
+
+return entity
