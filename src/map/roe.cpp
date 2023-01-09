36315577ee75ca9d817820cb003b1d322ec1301f
Xaver-DaRed <xaver.entropia@gmail.com>
2023-01-09 20:38:18 +0100

    Disable Unity Checks for Non-ROE Servers
    Co-authored-by: panicstevenson <panicstevenson@users.noreply.github.com>

--- src/map/roe.cpp
+++ src/map/roe.cpp
@@ -1,7 +1,4 @@
 ﻿/*
- * roe.cpp
- *      Author: Kreidos | github.com/kreidos
- *
 ===========================================================================
 
   Copyright (c) 2020 Topaz Dev Teams
@@ -687,6 +684,12 @@ namespace roeutils
     void CycleUnityRankings()
     {
         TracyZoneScoped;
+
+        if (!settings::get<bool>("main.ENABLE_ROE"))
+        {
+            return;
+        }
+
         const char* rankingQuery = "UPDATE unity_system SET members_prev = members_current, points_prev = points_current, members_current = 0, points_current = 0;";
         sql->Query(rankingQuery);
 
@@ -696,6 +699,12 @@ namespace roeutils
     void UpdateUnityRankings()
     {
         TracyZoneScoped;
+
+        if (!settings::get<bool>("main.ENABLE_ROE"))
+        {
+            return;
+        }
+
         const char* memberQuery = "UPDATE unity_system JOIN (SELECT unity_leader, COUNT(*) AS members FROM char_profile GROUP BY unity_leader) TMP ON unity_system.leader = unity_leader SET unity_system.members_current = members;";
         sql->Query(memberQuery);
 
