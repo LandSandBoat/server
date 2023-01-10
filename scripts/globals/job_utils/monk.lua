aee70c92faccd8651412fced1193d8806eb1799f
Panic Stevenson <panic.stevenson@gmail.com>
2023-01-09 19:00:54 -0800

    Set Counterstance to 45% Additional Counter Rate

--- scripts/globals/job_utils/monk.lua
+++ scripts/globals/job_utils/monk.lua
@@ -89,7 +89,7 @@ xi.job_utils.monk.useChiBlast = function(player, target, ability)
 end
 
 xi.job_utils.monk.useCounterstance = function(player, target, ability)
-    local power = 3 + player:getMod(xi.mod.COUNTERSTANCE_EFFECT)
+    local power = 45 + player:getMod(xi.mod.COUNTERSTANCE_EFFECT)
 
     target:delStatusEffect(xi.effect.COUNTERSTANCE) --if not found this will do nothing
     target:addStatusEffect(xi.effect.COUNTERSTANCE, power, 0, 300)
