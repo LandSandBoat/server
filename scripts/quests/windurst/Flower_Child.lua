9ecf5cb63445d32a2cd4f4e73f580fcc5f4a5cee
Zach Toogood <zrtoogood@gmail.com>
2023-01-11 16:05:52 +0000

    [lua] Support MH2F remodelling

--- scripts/quests/windurst/Flower_Child.lua
+++ scripts/quests/windurst/Flower_Child.lua
@@ -93,7 +93,8 @@ quest.sections =
                     if option == 3002 then
                         if quest:complete(player) then
                             player:confirmTrade()
-                            player:setMoghouseFlag(4)
+                            local mhflag = player:getMoghouseFlag()
+                            player:setMoghouseFlag(bit.band(mhflag, 0x0004))
                             player:messageSpecial(windurstWallsID.text.MOGHOUSE_EXIT)
                         end
 
