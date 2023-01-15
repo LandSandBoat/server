0491354e9173f4c6f911fa9ef2bb586579f7e48a
Zach Toogood <zrtoogood@gmail.com>
2023-01-15 14:02:56 +0000

    [tools] Make changelog script take arguments

--- tools/generate_changelog.py
+++ tools/generate_changelog.py
@@ -1,4 +1,11 @@
 # pip3 install requests
+#
+# Usage:
+# .\tools\generate_changelog.py <days to generate, or "ci"> <repo owner name> <repo name> <changelog title>
+#
+# Examples:
+# .\tools\generate_changelog.py ci LandSandBoat server LandSandBoat
+# .\tools\generate_changelog.py 7 LandSandBoat server LandSandBoat
 
 import sys
 from datetime import date
@@ -27,13 +34,25 @@ if len(sys.argv) >= 2:
     else:
         length_days = int(sys.argv[1])
 
+user = ""
+if len(sys.argv) >= 3:
+    user = sys.argv[2]
+
+repo = ""
+if len(sys.argv) >= 4:
+    repo = sys.argv[3]
+
+title = ""
+if len(sys.argv) >= 5:
+    title = sys.argv[4]
+
 print(f"Calculating changes for {length_days} days...")
 
 today = date.today()
 last_week = today - timedelta(days=length_days)
 
 params = {
-    "q": f"user:LandSandBoat repo:server state:closed is:pr merged:>={str(last_week)}"
+    "q": f"user:{user} repo:{repo} state:closed is:pr merged:>={str(last_week)}"
 }
 query_string = urllib.parse.urlencode(params)
 request = f"https://api.github.com/search/issues?page=1&per_page=100&{query_string}"
@@ -42,7 +61,7 @@ data = json.loads(response.text)
 
 print(f"Writing to: changelog-{today}.md...")
 with open(f"changelog-{today}.md", "w") as file:
-    file.write(f"## LandSandBoat Changelog ({today})\n")
+    file.write(f"## {title} Changelog ({today})\n")
     for raw_entry in enumerate(data["items"]):
         entry = raw_entry[1]
         title = entry["title"]
