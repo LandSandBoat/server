bbd268db3b6c452265963c52392354f449585a66
Tom-Neverwinter <Tom_Neverwinter@yahoo.com>
2023-01-15 22:52:27 -0600

    another test
    I want to know if this changed anything

--- scripts/zones/Chocobo_Circuit/npcs/Azainnie.lua
+++ scripts/zones/Chocobo_Circuit/npcs/Azainnie.lua
@@ -9,16 +9,13 @@ entity.onTrade = function(player, npc, trade)
 end
 
 entity.onTrigger = function(player, npc)
-    player:startEvent(328)
+    player:startEvent(328, 1) --
 end
 
 entity.onEventUpdate = function(player, csid, option)
 end
 
 entity.onEventFinsih = function(player, csid, option)
-    if csid == 328 and option == 1 then
-        player:setPos(-330.2306, -5.000, -413.3920) -- exits to illsoire
-    end
 end
 
 return entity
