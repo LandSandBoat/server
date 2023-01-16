c7c2ac04345e668b410f1e652699682b69488674
Tom-Neverwinter <Tom_Neverwinter@yahoo.com>
2023-01-15 21:53:05 -0600

    Update npc see if I figured out better formatting
    formatting fix

--- scripts/zones/Chocobo_Circuit/npcs/Azainnie.lua
+++ scripts/zones/Chocobo_Circuit/npcs/Azainnie.lua
@@ -9,13 +9,17 @@ entity.onTrade = function(player, npc, trade)
 end
 
 entity.onTrigger = function(player, npc)
-    player:startEvent(328, 1) --Ilsoire
+    player:startEvent(328)
 end
 
 entity.onEventUpdate = function(player, csid, option)
 end
 
 entity.onEventFinsih = function(player, csid, option)
+    if csid == 328 and option == 1 then
+        player:setpos(-330.2306,-5.000,-413.3920) -- exits to illsoire
+    end;
+
 end
 
 return entity
