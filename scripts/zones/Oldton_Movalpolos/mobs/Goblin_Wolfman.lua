f18c1695ca2c76f0521e9f58042fb773fa9be991
Tracent <92269743+TracentEden@users.noreply.github.com>
2023-01-24 06:48:03 +0200

    Fix goblin wolfman delay issue (#2625)

--- scripts/zones/Oldton_Movalpolos/mobs/Goblin_Wolfman.lua
+++ scripts/zones/Oldton_Movalpolos/mobs/Goblin_Wolfman.lua
@@ -11,18 +11,21 @@ local entity = {}
 entity.onMobInitialize = function(mob)
     mob:setMobMod(xi.mobMod.IDLE_DESPAWN, 180)
     mob:addMod(xi.mod.ACC, 70) -- Very accurate
-    mob:setLocalVar("weaponOn", 0)
+end
+
+entity.onMobWeaponSkill = function(target, mob, skill)
+    if skill:getID() == xi.jsa.BLOOD_WEAPON then
+        mob:addMod(xi.mod.DELAY, 1500)
+        mob:addMod(xi.mod.ATTP, 160)
+        mob:setLocalVar("removeMods", 1)
+    end
 end
 
 entity.onMobFight = function(mob, target)
-    local weaponOn = mob:getLocalVar("weaponOn")
-    if mob:hasStatusEffect(xi.effect.BLOOD_WEAPON) and weaponOn == 0 then
-        mob:addMod(xi.mod.DELAY, 1500)
-        mob:addMod(xi.mod.ATTP, 160)
-        mob:setLocalVar("weaponOn", 1)
-    elseif not mob:hasStatusEffect(xi.effect.BLOOD_WEAPON) and weaponOn == 1 then
+    if not mob:hasStatusEffect(xi.effect.BLOOD_WEAPON) and mob:getLocalVar("removeMods") == 1 then
         mob:delMod(xi.mod.DELAY, 1500)
         mob:delMod(xi.mod.ATTP, 160)
+        mob:setLocalVar("removeMods", 0)
     end
 end
 
