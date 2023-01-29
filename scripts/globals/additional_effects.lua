c5029035cfa9b1af60ed68cc730dead8b0b4b035
austinclprojects <clooneyraustin@gmail.com>
2023-01-29 02:14:16 -0500

    Prevent additional effect if player synced below item level.

--- scripts/globals/additional_effects.lua
+++ scripts/globals/additional_effects.lua
@@ -179,6 +179,11 @@ xi.additionalEffect.attack = function(attacker, defender, baseAttackDamage, item
         end
     end
 
+    -- If player is level synced below the level of the item, do no proc
+    if item:getReqLvl() > attacker:getMainLvl() then
+        return 0, 0, 0
+    end
+
     -- If we're not going to proc, lets not execute all those checks!
     if math.random(1, 100) > chance then
         return 0, 0, 0
