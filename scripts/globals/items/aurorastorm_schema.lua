fc9374a439d1322dca5dc2a8c2b647f71d52f187
Mi <djdeffkid@gmail.com>
2023-02-06 16:02:42 -0500

    Update Item Global "Magic Numbers": A
    This is the first of A-Z structuring using ID lookups for all magic number items found in golbals\items
    
    I am also adding new items to the globals\items.lua for any future needs
    
    Converting all magic numbers found in scrolls to our spell_data global

--- scripts/globals/items/aurorastorm_schema.lua
+++ scripts/globals/items/aurorastorm_schema.lua
@@ -3,14 +3,16 @@
 -- Item: Aurorastorm Schema
 -- Teaches the white magic Aurorastorm
 -----------------------------------
+require("scripts/globals/spell_data")
+-----------------------------------
 local itemObject = {}
 
 itemObject.onItemCheck = function(target)
-    return target:canLearnSpell(119)
+    return target:canLearnSpell(xi.magic.spell.AURORASTORM)
 end
 
 itemObject.onItemUse = function(target)
-    target:addSpell(119)
+    target:addSpell(xi.magic.spell.AURORASTORM)
 end
 
 return itemObject
