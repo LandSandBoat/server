59f9a0bb446f07b474fd69c8f0f1e4296d1928b0
Zach Toogood <zrtoogood@gmail.com>
2023-03-04 09:58:27 +0000

    [core] Move CheckTriggerAreas to its own task

--- src/map/zone_entities.cpp
+++ src/map/zone_entities.cpp
@@ -1407,7 +1407,7 @@ void CZoneEntities::WideScan(CCharEntity* PChar, uint16 radius)
     PChar->pushPacket(new CWideScanPacket(WIDESCAN_END));
 }
 
-void CZoneEntities::ZoneServer(time_point tick, bool check_trigger_areas)
+void CZoneEntities::ZoneServer(time_point tick)
 {
     TracyZoneScoped;
     TracyZoneString(m_zone->GetName());
@@ -1641,10 +1641,6 @@ void CZoneEntities::ZoneServer(time_point tick, bool check_trigger_areas)
             }
             PChar->PAI->Tick(tick);
             PChar->PTreasurePool->CheckItems(tick);
-            if (check_trigger_areas)
-            {
-                m_zone->CheckTriggerAreas(PChar);
-            }
         }
     }
 
