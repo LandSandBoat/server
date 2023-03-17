d7cb3a51870d95a46159c2e92985995484e3d077
TonzoffunCE <tonzoffunce@gmail.com>
2023-03-17 10:29:02 -0400

    Bringing in Dirty Sanchez

--- scripts/globals/pets.lua
+++ scripts/globals/pets.lua
@@ -83,6 +83,7 @@ xi.pet.id =
     PANZER_GALAHAD        = 45,
     CHOPSUEY_CHUCKY       = 46,
     AMIGO_SABOTENDER      = 47,
+    DIRTY_SANCHEZ         = 100,
 
     --  Dragoon
     WYVERN                = 48,
