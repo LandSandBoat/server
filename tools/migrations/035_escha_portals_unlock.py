432e072c0dff5374c0f3550fe58326a26e8df5e0
Corey Sotiropoulos <coreysotiropoulos@protonmail.com>
2023-03-18 01:37:58 -0400

    Replace mysql python module with mariadb

--- tools/migrations/035_escha_portals_unlock.py
+++ tools/migrations/035_escha_portals_unlock.py
@@ -1,5 +1,5 @@
 import array
-import mysql.connector
+import mariadb
 
 def migration_name():
     return "Adding eschan_portals column to char_unlocks table"
@@ -23,5 +23,5 @@ def migrate(cur, db):
             ADD COLUMN `eschan_portals` BLOB DEFAULT NULL AFTER `waypoints`;"
         )
         db.commit()
-    except mysql.connector.Error as err:
+    except mariadb.Error as err:
         print("Something went wrong: {}".format(err))
