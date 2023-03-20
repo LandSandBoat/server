a34e86a74cc1254bdcc266cf0169baeb08a63545
hookstar <lance.hooker@gmail.com>
2023-03-20 10:30:07 -0400

    Update to The Rumor Quest
    Converts The Rumor quest to IF. Adds cell door CS
    
    Converts The Rumor quest to IF. Adds cell door CS

---
+++ scripts/zones/Bostaunieux_Oubliette/npcs/_4ne.lua
@@ -0,0 +1,22 @@
+-----------------------------------
+-- Area: Bostaunieux Obliette
+--  NPC: Cell Door
+-- !pos -19.000 -17.899 20.000 167
+-----------------------------------
+local ID = require("scripts/zones/Bostaunieux_Oubliette/IDs")
+-----------------------------------
+local entity = {}
+
+entity.onTrade = function(player, npc, trade)
+end
+
+entity.onTrigger = function(player, npc)
+end
+
+entity.onEventUpdate = function(player, csid, option)
+end
+
+entity.onEventFinish = function(player, csid, option, npc)
+end
+
+return entity
