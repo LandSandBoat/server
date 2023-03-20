43f22f22359bfefacb8195ff8f8d4ff4ab848df0
Tiberon <TiberonKalkaz@gmail.com>
2023-03-19 23:47:22 -0700

    Fixes inconsistent parrying tracking during an attack

--- src/map/attack.h
+++ src/map/attack.h
@@ -119,12 +119,13 @@ private:
     bool                      m_isCountered{ false };
     bool                      m_isCovered{ false }; // Flag: Is someone covering the victim?
     bool                      m_anticipated{ false };
-    bool                      m_isFirstSwing{ false };  // Flag: Is this attack the first swing?
-    float                     m_damageRatio{ false };   // The damage ratio.
-    int32                     m_damage{ 0 };            // The damage for this attack.
-    int32                     m_trickAttackDamage{ 0 }; // The damage from trick attack.
-    int32                     m_naturalH2hDamage{ 0 };  // The damage from natural H2H.
-    int32                     m_baseDamage{ 0 };        // The base damage.
+    bool                      m_isFirstSwing{ false };     // Flag: Is this attack the first swing?
+    float                     m_damageRatio{ false };      // The damage ratio.
+    int32                     m_damage{ 0 };               // The damage for this attack.
+    int32                     m_trickAttackDamage{ 0 };    // The damage from trick attack.
+    int32                     m_naturalH2hDamage{ 0 };     // The damage from natural H2H.
+    int32                     m_baseDamage{ 0 };           // The base damage.
+    std::optional<bool>       m_isParried{ std::nullopt }; // Is this attack parried by the victim?
 };
 
 #endif
