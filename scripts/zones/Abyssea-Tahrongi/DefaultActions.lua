95e91bcd049e36ed6c82d56c2143f83b2101e156
Kain Zhou <kain.centeno@catsangel.com>
2023-03-26 18:48:16 -0700

    fixes space

--- scripts/zones/Abyssea-Tahrongi/DefaultActions.lua
+++ scripts/zones/Abyssea-Tahrongi/DefaultActions.lua
@@ -3,7 +3,7 @@ return {
     ['Fuepepe'] = { event = 413 },
     ['Furakku-Norakku'] = { event = 414 },
     ['Hakkuru-Rinkuru'] = { event = 403 },
-    ['Janshura-Rashura'] = { event = 400},
+    ['Janshura-Rashura'] = { event = 400 },
     ['Kuroido-Moido'] = { event = 402 },
     ['Ohruru'] = { event = 401 },
     ['Rhy_Epocan'] = { event = 412 },
