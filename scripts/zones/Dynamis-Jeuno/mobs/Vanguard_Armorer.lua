be5c5bb38dd9783ef2057559a6652751978c4a62
EpicTaru <epicone@gmail.com>
2023-03-30 18:35:33 -0700

    [lua] Implement Dynamis-Jeuno Lottery NMs (#3709)
    Implement Dynamis-Jeuno Lottery NMs

--- scripts/zones/Dynamis-Jeuno/mobs/Vanguard_Armorer.lua
+++ scripts/zones/Dynamis-Jeuno/mobs/Vanguard_Armorer.lua
@@ -7,10 +7,19 @@ mixins =
     require("scripts/mixins/dynamis_beastmen"),
     require("scripts/mixins/job_special")
 }
+local ID = require("scripts/zones/Dynamis-Jeuno/IDs")
+require("scripts/globals/mobs")
 -----------------------------------
 local entity = {}
 
 entity.onMobDeath = function(mob, player, optParams)
+end
+
+entity.onMobDespawn = function(mob)
+    xi.mob.phOnDespawn(mob, ID.mob.GABBLOX_MAGPIETONGUE_PH, 10, 1200) -- 10% lottery chance and 20 minute cooldown values ASSUMED same as Dynamis-Beaucedine/Xarcabard, needs final verification
+    xi.mob.phOnDespawn(mob, ID.mob.TUFFLIX_LOGLIMBS_PH, 10, 1200) -- 10% lottery chance and 20 minute cooldown values ASSUMED same as Dynamis-Beaucedine/Xarcabard, needs final verification
+    xi.mob.phOnDespawn(mob, ID.mob.CLOKTIX_LONGNAIL_PH, 10, 1200) -- 10% lottery chance and 20 minute cooldown values ASSUMED same as Dynamis-Beaucedine/Xarcabard, needs final verification
+    xi.mob.phOnDespawn(mob, ID.mob.SCRUFFIX_SHAGGYCHEST_PH, 10, 1200) -- 10% lottery chance and 20 minute cooldown values ASSUMED same as Dynamis-Beaucedine/Xarcabard, needs final verification
 end
 
 return entity
