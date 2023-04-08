cba3863cc95c1e27f2979d10454b9fe6ff991c2b
WinterSolstice8 <60417494+WinterSolstice8@users.noreply.github.com>
2023-04-07 22:12:44 -0700

    [lua] Update Palborough Mines elevator levers to function properly

--- scripts/zones/Palborough_Mines/npcs/_3z7.lua
+++ scripts/zones/Palborough_Mines/npcs/_3z7.lua
@@ -6,17 +6,48 @@ require("scripts/globals/status")
 -----------------------------------
 local entity = {}
 
+entity.isBusy = false
+
 entity.onTrade = function(player, npc, trade)
 end
 
 entity.onTrigger = function(player, npc)
-    RunElevator(xi.elevator.PALBOROUGH_MINES_LIFT)
+    local state = GetElevatorState(xi.elevator.PALBOROUGH_MINES_LIFT)
+
+    if
+        not entity.isBusy and
+        (state == xi.elevatorState.BOTTOM or
+        state == xi.elevatorState.TOP)
+    then
+        player:startEvent(10)
+    end
 end
 
 entity.onEventUpdate = function(player, csid, option)
 end
 
-entity.onEventFinish = function(player, csid, option)
+entity.onEventFinish = function(player, csid, option, npc)
+    if
+        not entity.isBusy and
+        csid == 10 and
+        option == 1
+    then
+        local state = GetElevatorState(xi.elevator.PALBOROUGH_MINES_LIFT)
+
+        if state == xi.elevatorState.BOTTOM or state == xi.elevatorState.TOP then
+            if npc:getAnimation() == xi.animation.OPEN_DOOR then
+                npc:setAnimation(xi.animation.CLOSE_DOOR)
+            else
+                npc:setAnimation(xi.animation.OPEN_DOOR)
+            end
+
+            entity.isBusy = true
+            npc:timer(3000, function()
+                RunElevator(xi.elevator.PALBOROUGH_MINES_LIFT)
+                entity.isBusy = false
+            end)
+        end
+    end
 end
 
 return entity
