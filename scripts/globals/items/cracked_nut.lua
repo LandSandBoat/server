845d27b9a7f516b52420a73965d86ad420dca549
Mi <djdeffkid@gmail.com>
2023-04-15 18:17:53 -0400

    Adds new cosmetic item lua files

---
+++ scripts/globals/items/cracked_nut.lua
@@ -0,0 +1,23 @@
+-----------------------------------
+-- ID: 5113
+-- Cracked Nut
+-- Enchantment: 60Min, Costume - Colibri
+-----------------------------------
+require("scripts/globals/status")
+require("scripts/globals/msg")
+-----------------------------------
+local itemObject = {}
+
+itemObject.onItemCheck = function(target)
+    if not target:canUseMisc(xi.zoneMisc.COSTUME) then
+        return xi.msg.basic.CANT_BE_USED_IN_AREA
+    end
+
+    return 0
+end
+
+itemObject.onItemUse = function(target)
+    target:addStatusEffect(xi.effect.COSTUME, 1720, 0, 3600)
+end
+
+return itemObject
---
+++ scripts/globals/items/worm_masque_+1.lua
@@ -0,0 +1,23 @@
+-----------------------------------
+-- ID: 27718
+-- Worm Masque +1
+-- Enchantment: 60Min, Costume - Big Worm
+-----------------------------------
+require("scripts/globals/status")
+require("scripts/globals/msg")
+-----------------------------------
+local itemObject = {}
+
+itemObject.onItemCheck = function(target)
+    if not target:canUseMisc(xi.zoneMisc.COSTUME) then
+        return xi.msg.basic.CANT_BE_USED_IN_AREA
+    end
+
+    return 0
+end
+
+itemObject.onItemUse = function(target)
+    target:addStatusEffect(xi.effect.COSTUME, 2956, 0, 3600)
+end
+
+return itemObject
