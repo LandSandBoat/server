63f1884f0c86679ea2bd7982dd37c8b1490d067e
WinterSolstice8 <60417494+WinterSolstice8@users.noreply.github.com>
2023-04-21 16:44:13 -0700

    [core] Move out RNG impl into specialized files
    * Remove random_device from RNG seeding
    * Add PCG32 & PCG64 RNG that can be uncommented to be used instead of Mersenne Twister.

--- src/common/xirand.h
+++ src/common/xirand.h
@@ -23,119 +23,118 @@
 #ifndef _XIRAND_H_
 #define _XIRAND_H_
 
-#include <array>
-#include <random>
+// You can choose an RNG by commenting/uncommenting this line. The default is Mersenne Twister in 32 bit.
+#include "rng/mersennetwister.h"
+// #include "rng/pcg.h"
+// #include "rng/pcg64.h"
 
-class xirand
+// RNG template is as follows:
+// The RNG must comply with the CPP standard UniformRandomBitGenerator ( see https://en.cppreference.com/w/cpp/named_req/UniformRandomBitGenerator )
+//
+// The RNG must return a static thread local instance of the RNG with "rng()" as the accessor, such as:
+// static std::mt19937& rng()
+//
+// The RNG must have a seed function declared as follows:
+//
+// static void seed()
+//
+// The RNG must have the following boilerplate templated function declarations
+/*
+    template <typename T>
+    static inline typename std::enable_if<std::is_integral<T>::value, T>::type GetRandomNumber(T min, T max);
+
+    template <typename T>
+    static inline typename std::enable_if<std::is_floating_point<T>::value, T>::type GetRandomNumber(T min, T max);
+
+    template <typename T>
+    static inline T GetRandomNumber(T max);
+
+    template <typename T>
+    static inline typename T::value_type GetRandomElement(T* container);
+
+    template <typename T>
+    static inline typename T::value_type GetRandomElement(T& container);
+
+    template <typename T>
+    static inline T GetRandomElement(std::initializer_list<T> list);
+*/
+// See the latest revision of src/common/RNG/mersennetwister.h for an example.
+
+//
+// The following functions are not meant to be edited by the end user. Change at your own risk -- you will receive no support.
+//
+
+// Generates a random number in the half-open interval [min, max)
+// @param min
+// @param max
+// @returns result
+//
+// Do note that max is subtracted by one as per an inconsistency in the standard, see
+// https://bugs.llvm.org/show_bug.cgi?id=18767#c1
+// this change results in both real and integer templates having the same min/max range
+template <typename T>
+inline typename std::enable_if<std::is_integral<T>::value, T>::type xirand::GetRandomNumber(T min, T max)
 {
-public:
-    static std::mt19937& mt()
+    if (min == max - 1 || max == min || min > max)
     {
-        static thread_local std::mt19937 e{};
-        return e;
+        return min;
     }
+    std::uniform_int_distribution<T> dist(min, max - 1);
+    return dist(rng());
+}
 
-    static void seed(void)
+template <typename T>
+inline typename std::enable_if<std::is_floating_point<T>::value, T>::type xirand::GetRandomNumber(T min, T max)
+{
+    if (min >= max)
     {
-        std::array<uint32_t, std::mt19937::state_size> seed_data;
-        std::random_device                             rd;
-
-        // Certain systems were noted to have bad seeding via only std::random_device,
-        // the following indicated how we could mix in std::random_device with other seed sources
-        // https://stackoverflow.com/a/68382489
-        for (auto it = seed_data.begin(); it != seed_data.end(); ++it)
-        {
-            // read from std::random_device
-            *it = rd();
-
-            // mix with a C++ equivalent of time(NULL) - UNIX time in seconds
-            *it ^= std::chrono::duration_cast<std::chrono::seconds>(
-                       std::chrono::system_clock::now().time_since_epoch())
-                       .count();
-
-            // mix with a high precision time in microseconds
-            *it ^= std::chrono::duration_cast<std::chrono::microseconds>(
-                       std::chrono::high_resolution_clock::now().time_since_epoch())
-                       .count();
-
-            //*it ^= more_external_random_stuff;
-        }
-        std::seed_seq seq(seed_data.cbegin(), seed_data.cend());
-        mt().seed(seq);
+        return min;
     }
+    std::uniform_real_distribution<T> dist(min, max);
+    return dist(rng());
+}
 
-    // Generates a random number in the half-open interval [min, max)
-    // @param min
-    // @param max
-    // @returns result
-    //
-    // Do note that max is subtracted by one as per an inconsistency in the standard, see
-    // https://bugs.llvm.org/show_bug.cgi?id=18767#c1
-    // this change results in both real and integer templates having the same min/max range
-    template <typename T>
-    static inline typename std::enable_if<std::is_integral<T>::value, T>::type GetRandomNumber(T min, T max)
-    {
-        if (min == max - 1 || max == min || min > max)
-        {
-            return min;
-        }
-        std::uniform_int_distribution<T> dist(min, max - 1);
-        return dist(mt());
-    }
+// Generates a random number in the half-open interval [0, max)
+// @param min
+// @param max
+// @returns result
+//
+// Do note that max is subtracted by one as per an inconsistency in the standard, see
+// https://bugs.llvm.org/show_bug.cgi?id=18767#c1
+// this change results in both real and integer templates having the same min/max range
+template <typename T>
+inline T xirand::GetRandomNumber(T max)
+{
+    return GetRandomNumber<T>(0, max);
+}
 
-    template <typename T>
-    static inline typename std::enable_if<std::is_floating_point<T>::value, T>::type GetRandomNumber(T min, T max)
-    {
-        if (min >= max)
-        {
-            return min;
-        }
-        std::uniform_real_distribution<T> dist(min, max);
-        return dist(mt());
-    }
+// Gets a random element from the given stl-like container (container must have members: at() and size()).
+// @param container
+// @returns result
+template <typename T>
+inline typename T::value_type xirand::GetRandomElement(T* container)
+{
+    // NOTE: the specialisation for integral types uses: dist(min, max - 1), so no need to offset container->size()
+    return container->at(GetRandomNumber<std::size_t>(0U, container->size()));
+}
 
-    // Generates a random number in the half-open interval [0, max)
-    // @param min
-    // @param max
-    // @returns result
-    //
-    // Do note that max is subtracted by one as per an inconsistency in the standard, see
-    // https://bugs.llvm.org/show_bug.cgi?id=18767#c1
-    // this change results in both real and integer templates having the same min/max range
-    template <typename T>
-    static inline T GetRandomNumber(T max)
-    {
-        return GetRandomNumber<T>(0, max);
-    }
+// Gets a random element from the given stl-like container (container must have members: at() and size()).
+// @param container
+// @returns result
+template <typename T>
+inline typename T::value_type xirand::GetRandomElement(T& container)
+{
+    return GetRandomElement(&container);
+}
 
-    // Gets a random element from the given stl-like container (container must have members: at() and size()).
-    // @param container
-    // @returns result
-    template <typename T>
-    static inline typename T::value_type GetRandomElement(T* container)
-    {
-        // NOTE: the specialisation for integral types uses: dist(min, max - 1), so no need to offset container->size()
-        return container->at(GetRandomNumber<std::size_t>(0U, container->size()));
-    }
-
-    // Gets a random element from the given stl-like container (container must have members: at() and size()).
-    // @param container
-    // @returns result
-    template <typename T>
-    static inline typename T::value_type GetRandomElement(T& container)
-    {
-        return GetRandomElement(&container);
-    }
-
-    // Gets a random element from the given initializer_list.
-    // @param initializer_list
-    // @returns result
-    template <typename T>
-    static inline T GetRandomElement(std::initializer_list<T> list)
-    {
-        std::vector<T> container(list);
-        return GetRandomElement(container);
-    }
-};
+// Gets a random element from the given initializer_list.
+// @param initializer_list
+// @returns result
+template <typename T>
+inline T xirand::GetRandomElement(std::initializer_list<T> list)
+{
+    std::vector<T> container(list);
+    return GetRandomElement(container);
+}
 
 #endif // _XIRAND_H_
