265fd0a7c8f034683a98e3d0413ed9871050e6a4
N3ckb3ard <djdeffkid@gmail.com>
2023-04-22 22:06:35 -0400

    Update Item Global "Magic Numbers": H

--- scripts/globals/items/holy_bolt_quiver.lua
+++ scripts/globals/items/holy_bolt_quiver.lua
@@ -4,6 +4,7 @@
 -- When used, you will obtain one stack of Holy Bolts
 -----------------------------------
 require("scripts/globals/msg")
+require("scripts/globals/items")
 -----------------------------------
 local itemObject = {}
 
@@ -17,7 +18,7 @@ itemObject.onItemCheck = function(target)
 end
 
 itemObject.onItemUse = function(target)
-    target:addItem(18153, 99)
+    target:addItem(xi.items.HOLY_BOLT, 99)
 end
 
 return itemObject
--- scripts/globals/items/hydrohelix_schema.lua
+++ scripts/globals/items/hydrohelix_schema.lua
@@ -3,14 +3,16 @@
 -- Hydrohelix Schema
 -- Teaches the black magic Hydrohelix
 -----------------------------------
+require("scripts/globals/spell_data")
+-----------------------------------
 local itemObject = {}
 
 itemObject.onItemCheck = function(target)
-    return target:canLearnSpell(279)
+    return target:canLearnSpell(xi.magic.spell.HYDROHELIX)
 end
 
 itemObject.onItemUse = function(target)
-    target:addSpell(279)
+    target:addSpell(xi.magic.spell.HYDROHELIX)
 end
 
 return itemObject
