97e89dac21110ea7db8e432832965c8255103bc5
N3ckb3ard <djdeffkid@gmail.com>
2023-04-24 18:55:41 -0400

    Update Item Global "Magic Numbers": M

--- scripts/globals/items/mythril_bolt_quiver.lua
+++ scripts/globals/items/mythril_bolt_quiver.lua
@@ -4,6 +4,7 @@
 -- When used, you will obtain one stack of Mythril Bolts
 -----------------------------------
 require("scripts/globals/msg")
+require("scripts/globals/items")
 -----------------------------------
 local itemObject = {}
 
@@ -17,7 +18,7 @@ itemObject.onItemCheck = function(target)
 end
 
 itemObject.onItemUse = function(target)
-    target:addItem(17337, 99)
+    target:addItem(xi.items.MYTHRIL_BOLT, 99)
 end
 
 return itemObject
