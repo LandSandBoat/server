0cd02391cacb66fda07cdb732c3bbbc7caf6a1fc
dallano <atwood.dallan@hotmail.com>
2023-04-25 13:00:19 -0600

    When Angels Fall BCNM Fix
    Fixed players being unable to run away from the battlefield in the CoP Mission 8-3 "When Angels Fall"

--- scripts/zones/The_Garden_of_RuHmet/npcs/_0zs.lua
+++ scripts/zones/The_Garden_of_RuHmet/npcs/_0zs.lua
@@ -2,20 +2,27 @@
 -- Area: The_Garden_of_RuHmet
 --  NPC: _0zs
 -----------------------------------
-require("scripts/globals/settings")
------------------------------------
 local entity = {}
 
+require("scripts/globals/settings")
+require("scripts/globals/missions")
+require("scripts/globals/keyitems")
+require("scripts/globals/bcnm")
+
 entity.onTrade = function(player, npc, trade)
+    xi.bcnm.onTrade(player, npc, trade)
 end
 
 entity.onTrigger = function(player, npc)
+    player:startEvent(32003)
 end
 
-entity.onEventUpdate = function(player, csid, option)
+entity.onEventUpdate = function(player, csid, option, extras)
+    xi.bcnm.onEventUpdate(player, csid, option, extras)
 end
 
 entity.onEventFinish = function(player, csid, option)
+    xi.bcnm.onEventFinish(player, csid, option)
 end
 
 return entity
