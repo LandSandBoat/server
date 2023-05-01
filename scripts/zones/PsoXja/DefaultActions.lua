43eeb1260348726d87fc315c971ec8aaae8766d2
Tal Ben-Eliezer <beneliezer@gmail.com>
2023-04-30 22:09:16 -0400

    Port ASB Fishing Quests and A reputation in ruins

---
+++ scripts/zones/PsoXja/DefaultActions.lua
@@ -0,0 +1,7 @@
+local ID = require("scripts/zones/PsoXja/IDs")
+
+return {
+    ['qm_blue_bracelet'] = { messageSpecial = ID.text.NOTHING_OUT_OF_ORDINARY },
+    ['qm_green_bracelet'] = { messageSpecial = ID.text.NOTHING_OUT_OF_ORDINARY },
+    ['_i95'] = { messageSpecial = ID.text.SHUT_TIGHTLY },
+}
