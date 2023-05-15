6c58d17110921e68820bbc9a787a81181318490e
claywar <claywar@beerlab.dev>
2023-05-15 13:14:25 -0400

    [codeql] Remove trivial switch statements from CLatentEffectContainer
    Several functions in CLatentEffectContainer contained switch statements with only one case.
    
    Changed these to conditionals until such time there is more than one value being compared.

--- src/map/latent_effect_container.cpp
+++ src/map/latent_effect_container.cpp
@@ -332,14 +332,11 @@ void CLatentEffectContainer::CheckLatentsRollSong()
 void CLatentEffectContainer::CheckLatentsDay()
 {
     ProcessLatentEffects([this](CLatentEffect& latentEffect) {
-        switch (latentEffect.GetConditionsID())
+        if (latentEffect.GetConditionsID() == LATENT::TIME_OF_DAY)
         {
-            case LATENT::TIME_OF_DAY:
-                return ProcessLatentEffect(latentEffect);
-                break;
-            default:
-                break;
+            return ProcessLatentEffect(latentEffect);
         }
+
         return false;
     });
 }
@@ -353,14 +350,11 @@ void CLatentEffectContainer::CheckLatentsMoonPhase()
 {
     TracyZoneScoped;
     ProcessLatentEffects([this](CLatentEffect& latentEffect) {
-        switch (latentEffect.GetConditionsID())
+        if (latentEffect.GetConditionsID() == LATENT::MOON_PHASE)
         {
-            case LATENT::MOON_PHASE:
-                return ProcessLatentEffect(latentEffect);
-                break;
-            default:
-                break;
+            return ProcessLatentEffect(latentEffect);
         }
+
         return false;
     });
 }
@@ -403,14 +397,11 @@ void CLatentEffectContainer::CheckLatentsHours()
 {
     TracyZoneScoped;
     ProcessLatentEffects([this](CLatentEffect& latentEffect) {
-        switch (latentEffect.GetConditionsID())
+        if (latentEffect.GetConditionsID() == LATENT::HOUR_OF_DAY)
         {
-            case LATENT::HOUR_OF_DAY:
-                return ProcessLatentEffect(latentEffect);
-                break;
-            default:
-                break;
+            return ProcessLatentEffect(latentEffect);
         }
+
         return false;
     });
 }
@@ -472,14 +463,11 @@ void CLatentEffectContainer::CheckLatentsPartyMembers(size_t members)
 void CLatentEffectContainer::CheckLatentsPartyJobs()
 {
     ProcessLatentEffects([this](CLatentEffect& latentEffect) {
-        switch (latentEffect.GetConditionsID())
+        if (latentEffect.GetConditionsID() == LATENT::JOB_IN_PARTY)
         {
-            case LATENT::JOB_IN_PARTY:
-                return ProcessLatentEffect(latentEffect);
-                break;
-            default:
-                break;
+            return ProcessLatentEffect(latentEffect);
         }
+
         return false;
     });
 }
@@ -493,14 +481,11 @@ void CLatentEffectContainer::CheckLatentsPartyJobs()
 void CLatentEffectContainer::CheckLatentsPartyAvatar()
 {
     ProcessLatentEffects([this](CLatentEffect& latentEffect) {
-        switch (latentEffect.GetConditionsID())
+        if (latentEffect.GetConditionsID() == LATENT::AVATAR_IN_PARTY)
         {
-            case LATENT::AVATAR_IN_PARTY:
-                return ProcessLatentEffect(latentEffect);
-                break;
-            default:
-                break;
+            return ProcessLatentEffect(latentEffect);
         }
+
         return false;
     });
 }
@@ -536,14 +521,11 @@ void CLatentEffectContainer::CheckLatentsJobLevel()
 void CLatentEffectContainer::CheckLatentsPetType()
 {
     ProcessLatentEffects([this](CLatentEffect& latentEffect) {
-        switch (latentEffect.GetConditionsID())
+        if (latentEffect.GetConditionsID() == LATENT::PET_ID)
         {
-            case LATENT::PET_ID:
-                return ProcessLatentEffect(latentEffect);
-                break;
-            default:
-                break;
+            return ProcessLatentEffect(latentEffect);
         }
+
         return false;
     });
 }
