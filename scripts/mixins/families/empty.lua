1acab1c42dcce6e8ad7675a851d367d540411d12
Tiberon <TiberonKalkaz@gmail.com>
2023-05-16 19:20:36 -0700

    Set EEM via script for mobs with elemental variance on spawn.

--- scripts/mixins/families/empty.lua
+++ scripts/mixins/families/empty.lua
@@ -9,7 +9,10 @@ g_mixins = g_mixins or {}
 
 local setModel = function(mob, skin)
     -- Strays in Vahzl can also spawn as Weeper and Seether models
-    if mob:getZone():getID() == xi.zone.PROMYVION_VAHZL and mob:getName() == "Stray" then
+    if
+        mob:getZone():getID() == xi.zone.PROMYVION_VAHZL and
+        mob:getName() == "Stray"
+    then
         local chance = math.random(1, 10)
         if chance == 1 then -- Weeper
             mob:setLocalVar("strayType", 1)
@@ -36,22 +39,37 @@ local setModel = function(mob, skin)
     end
 
     -- Set model depending on mob family
-    if mob:getFamily() == 255 or (mob:getFamily() == 499 and mob:getLocalVar("strayType") == 3) then -- Wanderer/Stray
+    if
+        mob:getFamily() == 255 or
+        (mob:getFamily() == 499 and
+        mob:getLocalVar("strayType") == 3)
+    then -- Wanderer/Stray
         if model == 4 then
             model = model + 1
         end
+
         mob:setModelId(1105 + model)
-    elseif mob:getFamily() == 256 or (mob:getFamily() == 499 and mob:getLocalVar("strayType") == 1) then -- Weeper / Stray
+    elseif
+        mob:getFamily() == 256 or
+        (mob:getFamily() == 499 and
+        mob:getLocalVar("strayType") == 1)
+    then -- Weeper / Stray
         mob:setModelId(1111 + model)
-    elseif mob:getFamily() == 220 or (mob:getFamily() == 499 and mob:getLocalVar("strayType") == 2) then -- Seether / Stray
+    elseif
+        mob:getFamily() == 220 or
+        (mob:getFamily() == 499 and
+        mob:getLocalVar("strayType") == 2)
+    then -- Seether / Stray
         if model > 1 then
             model = model + 1
         end
+
         mob:setModelId(1116 + model)
     elseif mob:getFamily() == 241 then -- Thinker
         if model > 2 then
             model = model + 1
         end
+
         mob:setModelId(1122 + model)
     elseif mob:getFamily() == 137 or mob:getFamily() == 138 then -- Gorger
         mob:setModelId(1128 + model)
@@ -59,6 +77,7 @@ local setModel = function(mob, skin)
         if model > 2 then
             model = model + 1
         end
+
         mob:setModelId(1133 + model)
     end
 end
@@ -130,61 +149,58 @@ g_mixins.families.empty = function(emptyMob)
             mob:setAnimationSub(1)
         end
 
+        -- TODO: Merge with Ghrah mixin
         -- Temporary solution until mods on spawn issue is corrected
         local buffed = mob:getLocalVar("buffed")
+        if buffed == 0 then
+            -- return EEM to neutral, overriding DB
+            mob:setMod(xi.mod.FIRE_EEM, 100)
+            mob:setMod(xi.mod.ICE_EEM, 100)
+            mob:setMod(xi.mod.WIND_EEM, 100)
+            mob:setMod(xi.mod.EARTH_EEM, 100)
+            mob:setMod(xi.mod.THUNDER_EEM, 100)
+            mob:setMod(xi.mod.WATER_EEM, 100)
+            mob:setMod(xi.mod.LIGHT_EEM, 100)
+            mob:setMod(xi.mod.DARK_EEM, 100)
+        end
+
         if skin == 1 and buffed == 0 then -- Dark
-            mob:setMod(xi.mod.DARK_MEVA, 99)
-            mob:setMod(xi.mod.SLEEPRES, 90)
-            mob:setMod(xi.mod.LIGHT_MEVA, -27)
+            mob:setMod(xi.mod.DARK_EEM, 5)
+            mob:setMod(xi.mod.LIGHT_EEM, 150)
             mob:setLocalVar("buffed", 1)
         elseif skin == 2 and buffed == 0 then -- Water
-            mob:setMod(xi.mod.FIRE_MEVA, 80)
-            mob:setMod(xi.mod.WATER_MEVA, 99)
-            mob:setMod(xi.mod.POISONRES, 90)
-            mob:setMod(xi.mod.THUNDER_MEVA, -27)
+            mob:setMod(xi.mod.FIRE_EEM, 5)
+            mob:setMod(xi.mod.WATER_EEM, 5)
+            mob:setMod(xi.mod.THUNDER_EEM, 150)
             mob:setLocalVar("buffed", 1)
         elseif skin == 3 and buffed == 0 then -- Lightning
-            mob:setMod(xi.mod.WATER_MEVA, 80)
-            mob:setMod(xi.mod.POISONRES, 90)
-            mob:setMod(xi.mod.THUNDER_MEVA, 99)
-            mob:setMod(xi.mod.STUNRES, 90)
-            mob:setMod(xi.mod.EARTH_MEVA, -27)
+            mob:setMod(xi.mod.WATER_EEM, 5)
+            mob:setMod(xi.mod.THUNDER_EEM, 5)
+            mob:setMod(xi.mod.EARTH_EEM, 150)
             mob:setLocalVar("buffed", 1)
         elseif skin == 4 and buffed == 0 then -- Earth
-            mob:setMod(xi.mod.THUNDER_MEVA, 80)
-            mob:setMod(xi.mod.STUNRES, 90)
-            mob:setMod(xi.mod.EARTH_MEVA, 99)
-            mob:setMod(xi.mod.SLOWRES, 90)
-            mob:setMod(xi.mod.WIND_MEVA, -27)
+            mob:setMod(xi.mod.THUNDER_EEM, 5)
+            mob:setMod(xi.mod.EARTH_EEM, 5)
+            mob:setMod(xi.mod.WIND_EEM, 150)
             mob:setLocalVar("buffed", 1)
         elseif skin == 5 and buffed == 0 then -- Light
-            mob:setMod(xi.mod.LIGHT_MEVA, 99)
-            mob:setMod(xi.mod.LULLABYRES, 90)
-            mob:setMod(xi.mod.DARK_MEVA, -27)
+            mob:setMod(xi.mod.LIGHT_EEM, 5)
+            mob:setMod(xi.mod.DARK_EEM, 150)
             mob:setLocalVar("buffed", 1)
         elseif skin == 6 and buffed == 0 then -- Fire
-            mob:setMod(xi.mod.ICE_MEVA, 80)
-            mob:setMod(xi.mod.PARALYZERES, 90)
-            mob:setMod(xi.mod.BINDRES, 90)
-            mob:setMod(xi.mod.FIRE_MEVA, 99)
-            mob:setMod(xi.mod.WATER_MEVA, -27)
+            mob:setMod(xi.mod.ICE_EEM, 5)
+            mob:setMod(xi.mod.FIRE_EEM, 5)
+            mob:setMod(xi.mod.WATER_EEM, 150)
             mob:setLocalVar("buffed", 1)
         elseif skin == 7 and buffed == 0 then -- Ice
-            mob:setMod(xi.mod.WIND_MEVA, 80)
-            mob:setMod(xi.mod.GRAVITYRES, 90)
-            mob:setMod(xi.mod.SILENCERES, 90)
-            mob:setMod(xi.mod.ICE_MEVA, 99)
-            mob:setMod(xi.mod.PARALYZERES, 90)
-            mob:setMod(xi.mod.BINDRES, 90)
-            mob:setMod(xi.mod.FIRE_MEVA, -27)
+            mob:setMod(xi.mod.WIND_EEM, 5)
+            mob:setMod(xi.mod.ICE_EEM, 5)
+            mob:setMod(xi.mod.FIRE_EEM, 150)
             mob:setLocalVar("buffed", 1)
         elseif buffed == 0 then -- Wind
-            mob:setMod(xi.mod.EARTH_MEVA, 80)
-            mob:setMod(xi.mod.SLOWRES, 90)
-            mob:setMod(xi.mod.WIND_MEVA, 99)
-            mob:setMod(xi.mod.GRAVITYRES, 90)
-            mob:setMod(xi.mod.SILENCERES, 90)
-            mob:setMod(xi.mod.ICE_MEVA, -27)
+            mob:setMod(xi.mod.EARTH_EEM, 5)
+            mob:setMod(xi.mod.WIND_EEM, 5)
+            mob:setMod(xi.mod.ICE_EEM, 150)
             mob:setLocalVar("buffed", 1)
         end
     end)
