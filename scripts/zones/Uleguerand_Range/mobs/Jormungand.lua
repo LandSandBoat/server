4a10510f4c57685d35fc785b28507202e9245f66
claywar <claywar@beerlab.dev>
2023-06-03 16:31:34 -0400

    [ci] Add check for deprecated or invalid requires

--- scripts/zones/Uleguerand_Range/mobs/Jormungand.lua
+++ scripts/zones/Uleguerand_Range/mobs/Jormungand.lua
@@ -50,7 +50,10 @@ entity.onMobFight = function(mob, target)
         elseif
             animation == 1 and
             battleTime - changeTime > 30 and
-            not hasSleepEffects(mob) and mob:checkDistance(target) <= 6 -- This 2 checks are a hack until we can handle skills targeting a position and not an entity.
+            not target:hasStatusEffect(xi.effect.SLEEP_I) and
+            not target:hasStatusEffect(xi.effect.SLEEP_II) and
+            not target:hasStatusEffect(xi.effect.LULLABY) and
+            mob:checkDistance(target) <= 6 -- This 2 checks are a hack until we can handle skills targeting a position and not an entity.
         then
             mob:useMobAbility(1292) -- This ability also handles animation change to 2.
 
