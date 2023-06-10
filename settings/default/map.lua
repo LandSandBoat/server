af57be1c53dea1a1560519ea3b93a0996d19a54c
Mi <djdeffkid@gmail.com>
2023-06-09 21:45:44 -0400

    remove dupe setting

--- settings/default/map.lua
+++ settings/default/map.lua
@@ -228,9 +228,6 @@ xi.settings.map =
     -- Prevent players from performing WS while unengaged using packet injection.
     PREVENT_UNENGAGED_WS = true,
 
-    -- Prevent players from performing WS while unengaged using packet injection.
-    PREVENT_UNENGAGED_WS = true,
-
     -- Command Audit [logging] commands with lower permission than this will not be logged.
     -- Zero for no logging at all. Commands given to non GMs are not logged.
     AUDIT_GM_CMD = 1,
