bf79bbbeece413e31eecbbd1b1bd689cdfd8cc70
Tal Ben-Eliezer <beneliezer@gmail.com>
2023-06-15 22:34:25 -0400

    Move notoriously dangerous mobs away from outposts

---
+++ modules/custom/sql/safe_outposts.sql
@@ -0,0 +1,16 @@
+-- Relocates dangerous monsters away from outposts
+UPDATE mob_spawn_points SET pos_x =  210.481, pos_y =  -6.946, pos_z =  101.274 WHERE mobid = 17199305;  -- Valkurm Dunes, Goblin Leecher
+UPDATE mob_spawn_points SET pos_x = -396.957, pos_y = -32.261, pos_z =   44.893 WHERE mobid = 17260561;  -- Buburimu Peninsula, Goblin Ambusher
+UPDATE mob_spawn_points SET pos_x = -411.430, pos_y = -31.429, pos_z =    3.221 WHERE mobid = 17260563;  -- Buburimu Peninsula, Goblin Butcher
+UPDATE mob_spawn_points SET pos_x =  -75.189, pos_y =  -1.413, pos_z = -130.057 WHERE mobid = 17240091;  -- Cape Terrigan, Goblin Shepard
+UPDATE mob_spawn_points SET pos_x =   -7.247, pos_y =   0.161, pos_z =    0.709 WHERE mobid = 17240093;  -- Cape Terrigan, Goblin Bandit
+UPDATE mob_spawn_points SET pos_x =  -61.728, pos_y =  -1.335, pos_z =  -99.354 WHERE mobid = 17240113;  -- Cape Terrigan, Goblin Mercenary
+UPDATE mob_spawn_points SET pos_x =  -37.084, pos_y =   0.000, pos_z =  -37.121 WHERE mobid = 17240124;  -- Cape Terrigan, Goblin Mercenary
+UPDATE mob_spawn_points SET pos_x =  -24.580, pos_y =   0.737, pos_z =  -82.615 WHERE mobid = 17240125;  -- Cape Terrigan, Goblin Bandit
+UPDATE mob_spawn_points SET pos_x = -325.870, pos_y =   7.453, pos_z = -196.602 WHERE mobid = 17244488;  -- Eastern Altepa Desert, Goblin Robber
+UPDATE mob_spawn_points SET pos_x = -146.846, pos_y =   8.510, pos_z = -239.809 WHERE mobid = 17244509;  -- Eastern Altepa Desert, Goblin Robber
+UPDATE mob_spawn_points SET pos_x = -190.317, pos_y =  -0.048, pos_z = -340.674 WHERE mobid = 17281245;  -- Yuhtunga Jungle, Goblin Furrier
+UPDATE mob_spawn_points SET pos_x = -291.329, pos_y =  -0.013, pos_z = -351.107 WHERE mobid = 17281251;  -- Yuhtunga Jungle, Goblin Furrier
+UPDATE mob_spawn_points SET pos_x =  244.705, pos_y =   0.000, pos_z = -162.007 WHERE mobid = 17285493;  -- Yhoator Jungle, Tonberry Hexer
+UPDATE mob_spawn_points SET pos_x =  230.252, pos_y =   0.600, pos_z = -190.586 WHERE mobid = 17285494;  -- Yhoator Jungle, Tonberry Creeper
+UPDATE mob_spawn_points SET pos_x =  207.793, pos_y =   0.594, pos_z = -174.136 WHERE mobid = 17285495;  -- Yhoator Jungle, Tonberry Harasser
