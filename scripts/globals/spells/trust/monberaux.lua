169a60fb1bbcbcef13c70cf2012b882b663dd08c
thecursedcometh2 <136541954+thecursedcometh2@users.noreply.github.com>
2023-06-18 11:22:38 -0500

    Added todo's and adjusted effects
    Added Paralyze resistance (retail has paralyze immunity) and adjusted MP% -80 to -90 (was wrong value).  Added list of todo's for downstream.

--- scripts/globals/spells/trust/monberaux.lua
+++ scripts/globals/spells/trust/monberaux.lua
@@ -16,13 +16,21 @@ end
 spellObject.onMobSpawn = function(mob)
     -- TODO: Summon (1 dose of Final Elixir ready): I received a vial of a most valuable medicine. It should prove useful in times of emergency.
     -- TODO: Summon (2 doses of Final Elixir ready): I received two vials of a most valuable medicine. They should prove useful in times of emergency.
+    -- TODO: Find right animation for Mix: Insomniant.
+    -- TODO: Add PLD/RUN traits like Resist Sleep and Tenacity.
+    -- TODO: Add Vaccine skill
+    -- TODO: Add Gold Needle skill
+    -- TODO: Add Cover ability with proper conditions (stand behind Monberaux when you have top enmity)
+    -- TODO: Add mechanic for turning status removal skills into AoE -- https://www.bg-wiki.com/ffxi/BGWiki:Trusts#Monberaux
+    
     xi.trust.message(mob, xi.trust.message_offset.SPAWN)
 
     local healingMoveCooldown = 3 -- Retail values from BGWiki
 
     -- Tends to be particular about which potions to use. Seems to favor healing for just the
     -- right amount of HP instead of defaulting to the highest-rank potion.
-    mob:addMod(xi.mod.MPP, -80)
+    mob:addMod(xi.mod.MPP, -90)
+    mob:addMod(xi.mod.PARALYZERES, 100)
     mob:addSimpleGambit(ai.t.CASTER, ai.c.MPP_LT, 25, ai.r.MS, ai.s.SPECIFIC, 4254, healingMoveCooldown) -- Mix: Dry Ether Concoction
     mob:addSimpleGambit(ai.t.PARTY, ai.c.HPP_LT, 90, ai.r.MS, ai.s.SPECIFIC, 4236, healingMoveCooldown) -- Max Potion (500 HP)
     mob:addSimpleGambit(ai.t.PARTY, ai.c.HPP_LT, 75, ai.r.MS, ai.s.SPECIFIC, 4237, healingMoveCooldown) -- Mix: Max Potion (700 HP)
@@ -35,7 +43,7 @@ spellObject.onMobSpawn = function(mob)
     mob:addSimpleGambit(ai.t.PARTY, ai.c.NOT_STATUS, xi.effect.MAGIC_DEF_BOOST, ai.r.MS, ai.s.SPECIFIC, 4259, healingMoveCooldown) -- Mix: Dragon Shield
     mob:addSimpleGambit(ai.t.PARTY, ai.c.NOT_STATUS, xi.effect.MAGIC_ATK_BOOST, ai.r.MS, ai.s.SPECIFIC, 4258, healingMoveCooldown) -- Mix: Elemental Power
     mob:addSimpleGambit(ai.t.PARTY, ai.c.NOT_STATUS, xi.effect.PROTECT, ai.r.MS, ai.s.SPECIFIC, 4255, healingMoveCooldown) -- Mix: Guard Drink
-    -- mob:addSimpleGambit(ai.t.PARTY, ai.c.NOT_STATUS, xi.effect.NEGATE_SLEEP, ai.r.MS, ai.s.SPECIFIC, 4256, healingMoveCooldown) -- Insomniant
+    -- mob:addSimpleGambit(ai.t.PARTY, ai.c.NOT_STATUS, xi.effect.NEGATE_SLEEP, ai.r.MS, ai.s.SPECIFIC, 4256, healingMoveCooldown) -- Insomniant. Disabled because animation when used is completely wrong.
     mob:addSimpleGambit(ai.t.TARGET, ai.c.HP_MISSING, 99, ai.r.MS, ai.s.SPECIFIC, 4260, healingMoveCooldown) -- Dark Potion (666 Dark Damage)
 
     mob:setAutoAttackEnabled(false)
@@ -45,6 +53,7 @@ spellObject.onMobSpawn = function(mob)
         mobArg:setTP(0)
     end)
 
+    -- This listener is needed for Monberaux to display the correct skill name in the combat log.
     mob:addListener("WEAPONSKILL_USE", "MONBERAUX_WS", function(mobArg, targetArg, skillid, spentTP, action)
         action:setCategory(xi.action.MOBABILITY_FINISH)
     end)
