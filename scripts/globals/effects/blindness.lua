c703d44e4da75680c1a3ec30c67dd6c7e1eae2e2
Xaver-DaRed <xaver.entropia@gmail.com>
2023-06-21 18:01:04 +0200

    Add Immunobreak reset to onEffectGain

--- scripts/globals/effects/blindness.lua
+++ scripts/globals/effects/blindness.lua
@@ -5,6 +5,10 @@ local effectObject = {}
 
 effectObject.onEffectGain = function(target, effect)
     target:addMod(xi.mod.ACC, -effect:getPower())
+    target:addMod(xi.mod.RACC, -effect:getPower())
+
+    -- Immunobreak reset.
+    target:setMod(xi.mod.BLIND_IMMUNOBREAK, 0)
 end
 
 effectObject.onEffectTick = function(target, effect)
@@ -12,6 +16,7 @@ end
 
 effectObject.onEffectLose = function(target, effect)
     target:delMod(xi.mod.ACC, -effect:getPower())
+    target:delMod(xi.mod.RACC, -effect:getPower())
 end
 
 return effectObject
