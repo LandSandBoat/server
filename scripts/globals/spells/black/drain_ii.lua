4cd5b810878b6fafbb52e2ba6dae962ff32fdb47
CatsEyeXI <beneliezer@gmail.com>
2023-07-06 01:19:05 -0400

    update duration on drain ii effect

--- scripts/globals/spells/black/drain_ii.lua
+++ scripts/globals/spells/black/drain_ii.lua
@@ -53,7 +53,7 @@ spellObject.onSpellCast = function(caster, target, spell)
     local leftOver = (caster:getHP() + dmg) - caster:getMaxHP()
 
     if leftOver > 0 then
-        caster:addStatusEffect(xi.effect.MAX_HP_BOOST, (leftOver / caster:getMaxHP()) * 100, 0, 60)
+        caster:addStatusEffect(xi.effect.MAX_HP_BOOST, (leftOver / caster:getMaxHP()) * 100, 0, 180)
     end
 
     caster:addHP(dmg)
