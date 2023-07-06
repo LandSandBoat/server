dbf733590009ee9ea784324076381a44f36c3a89
Gweivyth Quartet <admin@mageringnetwork.com>
2023-07-06 16:07:48 -0400

    Commit additional SCNM sealed container exchangers.

--- scripts/zones/Southern_San_dOria_[S]/npcs/Lurouillat.lua
+++ scripts/zones/Southern_San_dOria_[S]/npcs/Lurouillat.lua
@@ -4,12 +4,37 @@
 -- !pos 44 2 -35 80
 -- Sealed Container NPC
 -----------------------------------
+local ID = require("scripts/zones/Southern_San_dOria_[S]/IDs")
+require("scripts/globals/keyitems")
+require("scripts/globals/settings")
+-----------------------------------
 local entity = {}
 
 entity.onTrade = function(player, npc, trade)
+    local itemID = trade:getItemId()
+local itemKeyItemMapping = {
+    [2678] = xi.ki.IMPERIAL_LINEAGE_CHAPTER_I,
+    [2679] = xi.ki.IMPERIAL_LINEAGE_CHAPTER_II,
+    [2680] = xi.ki.IMPERIAL_LINEAGE_CHAPTER_III,
+    [2681] = xi.ki.IMPERIAL_LINEAGE_CHAPTER_IV,
+    [2682] = xi.ki.IMPERIAL_LINEAGE_CHAPTER_V,
+    [2683] = xi.ki.IMPERIAL_LINEAGE_CHAPTER_VI,
+    [2684] = xi.ki.IMPERIAL_LINEAGE_CHAPTER_VII,
+    [2685] = xi.ki.IMPERIAL_LINEAGE_CHAPTER_VIII
+}
+    -- Make sure we're only trading 1 Basket at a time.
+    if trade:hasItemQty(itemID, 1) and trade:getItemCount() == 1 and itemKeyItemMapping[itemID] then
+        player:tradeComplete()
+        local keyItem = itemKeyItemMapping[itemID]
+        npc:showText(npc, 13640)
+        player:addKeyItem(keyItem)
+        player:messageSpecial(ID.text.KEYITEM_OBTAINED, keyItem)
+
+    end
 end
 
 entity.onTrigger = function(player, npc)
+    player:startEvent(350)
 end
 
 entity.onEventUpdate = function(player, csid, option)
--- scripts/zones/Windurst_Waters_[S]/npcs/Pogigi.lua
+++ scripts/zones/Windurst_Waters_[S]/npcs/Pogigi.lua
@@ -1,12 +1,36 @@
 -----------------------------------
 -- Area: Windurst Waters (S)
 --  NPC: Pogigi
--- Type: Sealed Container
 -- !pos -29.787 -4.499 42.603 94
+-- Sealed Container NPC
+-----------------------------------
+local ID = require("scripts/zones/Windurst_Waters_[S]/IDs")
+require("scripts/globals/keyitems")
+require("scripts/globals/settings")
 -----------------------------------
 local entity = {}
 
 entity.onTrade = function(player, npc, trade)
+    local itemID = trade:getItemId()
+    local itemKeyItemMapping = {
+        [2694] = xi.ki.HABALOS_ECLOGUE_VERSE_I,
+        [2695] = xi.ki.HABALOS_ECLOGUE_VERSE_II,
+        [2696] = xi.ki.HABALOS_ECLOGUE_VERSE_III,
+        [2697] = xi.ki.HABALOS_ECLOGUE_VERSE_IV,
+        [2698] = xi.ki.HABALOS_ECLOGUE_VERSE_V,
+        [2699] = xi.ki.HABALOS_ECLOGUE_VERSE_VI,
+        [2700] = xi.ki.HABALOS_ECLOGUE_VERSE_VII,
+        [2701] = xi.ki.HABALOS_ECLOGUE_VERSE_VIII
+    }
+    -- Make sure we're only trading 1 Basket at a time.
+    if trade:hasItemQty(itemID, 1) and trade:getItemCount() == 1 and itemKeyItemMapping[itemID] then
+        player:tradeComplete()
+        local keyItem = itemKeyItemMapping[itemID]
+        npc:showText(npc, 13398) --Need to confirm these texts, used KO captures but might be outdated.
+        player:addKeyItem(keyItem)
+        player:messageSpecial(ID.text.KEYITEM_OBTAINED, keyItem)
+
+    end
 end
 
 entity.onTrigger = function(player, npc)
