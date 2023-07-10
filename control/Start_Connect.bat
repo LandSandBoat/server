28ecb9fb739138bfe260ca1527e896b45c0c1aed
Tal Ben-Eliezer <beneliezer@gmail.com>
2023-07-10 07:28:13 -0400

    Remove auto-restarter from connect server

--- control/Start_Connect.bat
+++ control/Start_Connect.bat
@@ -5,7 +5,5 @@ cd ..
 echo [%date% %time%] Restarting Lobby/Connection Server...
 timeout /t 5 >null
 start xi_connect.exe
-timeout /t 3600 >null
-taskkill /f /im xi_connect.exe >null
 echo ...
 GOTO onCrash
