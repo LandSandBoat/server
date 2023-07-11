ede3c053198b2631411dba547fdee4b2993b5d0e
Loxley <119976133+LoxleyXI@users.noreply.github.com>
2023-07-11 01:34:59 +0100

    Updates and fixes for Egg Hunt Egg-stravaganza

--- scripts/globals/items/hatchling_shield.lua
+++ scripts/globals/items/hatchling_shield.lua
@@ -27,6 +27,13 @@ local eggTable =
     { xi.items.SAIRUI_RAN,      1, 10, },
 }
 
+if xi.settings.main.ENABLE_TOAU == 1 then
+    table.insert(eggTable, { xi.items.APKALLU_EGG,        1, 11, })
+    table.insert(eggTable, { xi.items.CHUNK_OF_FLAN_MEAT, 1,  6, })
+    table.insert(eggTable, { xi.items.PUK_EGG,            1,  7, })
+    table.insert(eggTable, { xi.items.SALMON_EGGS,        3,  7, })
+end
+
 itemObject.onItemUse = function(target)
     local egg = eggTable[math.random(1, #eggTable)]
     target:addItem(egg[1], math.random(egg[2], egg[3]))
