fe729d7efbc45784c9254c5760a58addb4abf2fc
Zach Toogood <zrtoogood@gmail.com>
2023-07-12 17:39:05 +0100

    Add note about payload copying in IMessageHandler

--- src/world/time_server.h
+++ src/world/time_server.h
@@ -1,7 +1,7 @@
 /*
 ===========================================================================
 
-Copyright (c) 2022 LandSandBoat Dev Teams
+Copyright (c) 2023 LandSandBoat Dev Teams
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
