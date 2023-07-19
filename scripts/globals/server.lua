b27206720e2695cc2e0e4b8802f090e7200d8285
Zach Toogood <zrtoogood@gmail.com>
2023-07-19 19:53:24 +0100

    [core] Move SmallPacket0x04B server message logic into Lua
    Also removed deprecated/unused SetDropRate & UpdateServerMessage

--- scripts/globals/server.lua
+++ scripts/globals/server.lua
@@ -16,4 +16,23 @@ end
 xi.server.onTimeServerTick = function()
 end
 
+-- Message for use with SmallPacket0x04B
+xi.server.getServerMessage = function(language)
+    local serverMessage = ""
+
+    if language == xi.language.ENGLISH then
+        serverMessage = xi.settings.main.SERVER_MESSAGE
+
+        if xi.settings.main.ENABLE_TRUST_ALTER_EGO_EXTRAVAGANZA_ANNOUNCE == 1 then
+            serverMessage = serverMessage .. xi.settings.main.TRUST_ALTER_EGO_EXTRAVAGANZA_MESSAGE
+        end
+
+        if xi.settings.main.ENABLE_TRUST_ALTER_EGO_EXPO_ANNOUNCE == 1 then
+            serverMessage = serverMessage .. xi.settings.main.TRUST_ALTER_EGO_EXPO_MESSAGE
+        end
+    end
+
+    return serverMessage
+end
+
 return xi.server
