f75e68da85478d37f624a3b87b01d6233968e6e5
Zach Toogood <zrtoogood@gmail.com>
2023-07-21 08:28:02 +0100

    [squash me] Add review items for Buffalostalker Dodzbraz

--- scripts/zones/Uleguerand_Range/npcs/Buffalostalker_Dodzbraz.lua
+++ scripts/zones/Uleguerand_Range/npcs/Buffalostalker_Dodzbraz.lua
@@ -10,23 +10,22 @@
 local entity = {}
 
 entity.onTrade = function(player, npc, trade)
-    if npcUtil.tradeHas(trade, { { xi.items.CLUSTER_CORE, 2 } }) then -- Cluster Core x2
-        player:startEvent(8, 1667)
+    if npcUtil.tradeHas(trade, { { xi.items.CLUSTER_CORE, 2 } }) then
+        player:startEvent(8, xi.items.CLUSTER_CORE)
     else
-        player:startEvent(7)
+        player:startEvent(7) -- TODO: Does this need the xi.items.CLUSTER_CORE argument?
     end
 end
 
 entity.onTrigger = function(player, npc)
-    player:startEvent(6, 1667)
+    player:startEvent(6, xi.items.CLUSTER_CORE)
 end
 
 entity.onEventUpdate = function(player, csid, option)
 end
 
 entity.onEventFinish = function(player, csid, option)
-    if csid == 8 then
-        npcUtil.giveItem(player, xi.items.CHUNK_OF_SHUMEYO_SALT) -- Chunk of Shumeyo Salt
+    if csid == 8 and npcUtil.giveItem(player, xi.items.CHUNK_OF_SHUMEYO_SALT) then
         player:confirmTrade()
     end
 end
