32051e4ba1b8e5ac1a95bbee1c1f7c42acc4261c
hookstar <lance.hooker@gmail.com>
2023-07-22 19:20:26 -0400

    Implements era quest timers module.
    [Quest] Converts COR AF2 to IF
    
    Navigating the unfriendly seas.

--- scripts/zones/Wajaom_Woodlands/DefaultActions.lua
+++ scripts/zones/Wajaom_Woodlands/DefaultActions.lua
@@ -2,5 +2,6 @@ local ID = require("scripts/zones/Wajaom_Woodlands/IDs")
 
 return {
     ['Giwahb_Watchtower']      = { special = ID.text.GIWAHB_WATCHTOWER_LOCKED },
+    ['Leypoint']               = { special = ID.text.LEYPOINT },
     ['Mythralline_Wellspring'] = { special = ID.text.NOTHING_HAPPENS          },
 }
