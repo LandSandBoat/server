70766c8e7a8658718f02bdd933186d12476a84a1
Xaver-DaRed <xaver.entropia@gmail.com>
2023-07-23 18:26:45 +0200

    Implement "Bombs Away!" quest

--- scripts/zones/Uleguerand_Range/npcs/Buffalostalker_Dodzbraz.lua
+++ scripts/zones/Uleguerand_Range/npcs/Buffalostalker_Dodzbraz.lua
@@ -1,7 +1,6 @@
 -----------------------------------
 -- Area: Uleguerand Range
 --  NPC: Buffalostalker Dodzbraz
--- Type: Quest NPC
 -- !pos -380.171 -24.89 -180.797 5
 -----------------------------------
 local entity = {}
@@ -10,7 +9,6 @@ entity.onTrade = function(player, npc, trade)
 end
 
 entity.onTrigger = function(player, npc)
-    player:startEvent(6)
 end
 
 entity.onEventUpdate = function(player, csid, option, npc)
