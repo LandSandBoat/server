9a214ad8ec93576324a4ce7ddfefed931b341758
Skerxan <sbeyazkilic@gmx.de>
2023-07-25 19:00:45 +0200

    [Shops] Audited Royal Armoury
    Update

--- scripts/zones/Northern_San_dOria/npcs/Tavourine.lua
+++ scripts/zones/Northern_San_dOria/npcs/Tavourine.lua
@@ -14,20 +14,20 @@ end
 entity.onTrigger = function(player, npc)
     local stock =
     {
-        16465,   164, 3,    -- Bronze Knife
-		16466,  2425, 3,    -- Knife
-		17059,   100, 3,    -- Bronze Rod
-		17081,   690, 3,    -- Brass Rod
+        16465,   170, 3,    -- Bronze Knife
+        16466,  2522, 3,    -- Knife
+        17059,   104, 3,    -- Bronze Rod
+        17081,   717, 3,    -- Brass Rod
         17060,  2652, 1,    -- Rod
-		17034,   188, 3,    -- Bronze Mace
+        17034,   195, 3,    -- Bronze Mace
         17035,  4848, 2,    -- Mace
-        16640,   316, 3,    -- Bronze Axe
-        16583,  2720, 3,    -- Claymore
-		16584, 42000, 1,    -- Mythril Claymore
-		16833,   880, 3,    -- Bronze Spear
-		16834,  5200, 3,    -- Brass Spear
-		16835, 17640, 3,    -- Spear
-		16845, 18420, 3,    -- Lance
+        16640,   328, 3,    -- Bronze Axe
+        16583,  2828, 3,    -- Claymore
+        16584, 42000, 1,    -- Mythril Claymore
+        16833,   915, 3,    -- Bronze Spear
+        16834,  5408, 3,    -- Brass Spear
+        16835, 18345, 3,    -- Spear
+        16845, 19156, 3,    -- Lance
     }
 
     player:showText(npc, ID.text.TAVOURINE_SHOP_DIALOG)
