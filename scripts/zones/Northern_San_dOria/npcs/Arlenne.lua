5659655da2b816a4aa6d13a13cfd5f87e0368151
Skerxan <sbeyazkilic@gmx.de>
2023-07-25 19:00:45 +0200

    [Shops] Audited Royal Armoury
    Update

--- scripts/zones/Northern_San_dOria/npcs/Arlenne.lua
+++ scripts/zones/Northern_San_dOria/npcs/Arlenne.lua
@@ -14,21 +14,21 @@ end
 entity.onTrigger = function(player, npc)
     local stock =
     {
-        17024,    72, 3,    -- Ash Club
-		17049,    52, 3,    -- Maple Wand
-		17050,   370, 3,    -- Willow Wand
-		17051,  1566, 1,    -- Yew Wand
-		17088,    64, 3,    -- Ash Staff
-		17089,   635, 3,    -- Holly Staff
+        17024,    74, 3,    -- Ash Club
+        17049,    54, 3,    -- Maple Wand
+        17050,   384, 3,    -- Willow Wand
+        17051,  1566, 1,    -- Yew Wand
+        17088,    66, 3,    -- Ash Staff
+        17089,   660, 3,    -- Holly Staff
         17090,  3606, 1,    -- Elm Staff
-		17095,   420, 3,    -- Ash Pole
-		17096,  5076, 2,    -- Holly Pole
+        17095,   436, 3,    -- Ash Pole
+        17096,  5076, 2,    -- Holly Pole
         17097, 18240, 1,    -- Elm Pole
-		16385,   144, 3,    -- Cesti
-		16391,   900, 3,    -- Brass Knuckles
-		16407,  1690, 3,    -- Brass Baghnakhs
-		16768,   344, 3,    -- Bronze Zaghnal
-		16769,  2825, 3,    -- Brass Zaghnal
+        16385,   149, 3,    -- Cesti
+        16391,   936, 3,    -- Brass Knuckles
+        16407,  1757, 3,    -- Brass Baghnakhs
+        16768,   357, 3,    -- Bronze Zaghnal
+        16769,  2938, 3,    -- Brass Zaghnal
         16770, 12540, 1,    -- Zaghnal
     }
 
