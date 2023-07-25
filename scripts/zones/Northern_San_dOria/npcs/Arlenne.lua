6f290390b239abd431bd4428e44757b9d2dcd4f5
Skerxan <sbeyazkilic@gmx.de>
2023-07-25 02:03:47 +0200

    [Shops] Audited Royal Armoury
    Audited the 2 shop NPCs in Northern Sandy. Arlenne & Tavourine
    
    - Fixed prices
    - Fixed nation placement requirements
    - Fixed order of items
    - Added missing weapons (Bronze Spear, Brass Spear, Spear)
    
    I compared BGwiki to JPwiki and noticed that JPwiki has the updated list of the July 2011 patch. (They even list the changes on their shop page.)
    Also the JP list has a note that their prices are the base prices untouched by fame etc. (The numbers also make alot more sense.)
    I also ordered the items according to the JPwiki because they usually use the retail list instead of the LSB ordering by conquest/ID.)
    Not gonna touch the formating otherwise.
    
    Sources:
    - https://www.bg-wiki.com/ffxi/Tavourine
    - https://wiki-ffo-jp.translate.goog/html/9245.html?_x_tr_sl=auto&_x_tr_tl=en&_x_tr_hl=en-US
    - https://www.bg-wiki.com/ffxi/Version_Update_(07/11/2011)

--- scripts/zones/Northern_San_dOria/npcs/Arlenne.lua
+++ scripts/zones/Northern_San_dOria/npcs/Arlenne.lua
@@ -14,22 +14,22 @@ end
 entity.onTrigger = function(player, npc)
     local stock =
     {
-        17051,  1409, 1,    -- Yew Wand
-        17090,  3245, 1,    -- Elm Staff
-        17097, 16416, 1,    -- Elm Pole
-        16770, 11286, 1,    -- Zaghnal
-        17096,  4568, 2,    -- Holly Pole
-        17024,    66, 3,    -- Ash Club
-        17049,    46, 3,    -- Maple Wand
-        17050,   333, 3,    -- Willow Wand
-        17088,    57, 3,    -- Ash Staff
-        17089,   571, 3,    -- Holly Staff
-        17095,   386, 3,    -- Ash Pole
-        16385,   132, 3,    -- Cesti
-        16391,   828, 3,    -- Brass Knuckles
-        16407,  1554, 3,    -- Brass Baghnakhs
-        16768,   309, 3,    -- Bronze Zaghnal
-        16769,  2542, 2,    -- Brass Zaghnal
+        17024,    72, 3,    -- Ash Club
+		17049,    52, 3,    -- Maple Wand
+		17050,   370, 3,    -- Willow Wand
+		17051,  1566, 1,    -- Yew Wand
+		17088,    64, 3,    -- Ash Staff
+		17089,   635, 3,    -- Holly Staff
+        17090,  3606, 1,    -- Elm Staff
+		17095,   420, 3,    -- Ash Pole
+		17096,  5076, 2,    -- Holly Pole
+        17097, 18240, 1,    -- Elm Pole
+		16385,   144, 3,    -- Cesti
+		16391,   900, 3,    -- Brass Knuckles
+		16407,  1690, 3,    -- Brass Baghnakhs
+		16768,   344, 3,    -- Bronze Zaghnal
+		16769,  2825, 3,    -- Brass Zaghnal
+        16770, 12540, 1,    -- Zaghnal
     }
 
     player:showText(npc, ID.text.ARLENNE_SHOP_DIALOG)
