af2d8054a736181d50794c496d29df2fd2292fa1
hookstar <lance.hooker@gmail.com>
2023-08-02 17:49:57 -0400

    Update to random Ring

--- scripts/globals/items/random_ring.lua
+++ scripts/globals/items/random_ring.lua
@@ -7,32 +7,20 @@
 local itemObject = {}
 
 itemObject.onItemCheck = function(target)
-    if target:getStatusEffect(xi.effect.ENCHANTMENT, nil, xi.items.RANDOM_RING) ~= nil then
-        target:delStatusEffect(xi.effect.ENCHANTMENT, nil, xi.items.RANDOM_RING)
+    local effect = target:getStatusEffect(xi.effect.DEX_BOOST)
+    if effect~= nil and effect:getItemSourceID() == xi.items.RANDOM_RING then
+        target:delStatusEffect(xi.effect.DEX_BOOST)
     end
 
     return 0
 end
 
 itemObject.onItemUse = function(target)
+    local power = math.random(1, 8)
+
     if target:hasEquipped(xi.items.RANDOM_RING) then
-
-        if target:addStatusEffect(xi.effect.ENCHANTMENT, 0, 0, 1800, 0, 0, 0, xi.items.RANDOM_RING) then
-            local effect = target:getStatusEffect(xi.effect.ENCHANTMENT)
-            if effect then
-                local power = math.random(1, 8)
-
-                target:addMod(xi.mod.DEX, power)
-                effect:addMod(xi.mod.DEX, power)
-            end
-        end
+        target:addStatusEffect(xi.effect.DEX_BOOST, power, 0, 300, 0, 0, 0, xi.items.RANDOM_RING)
     end
-end
-
-itemObject.onEffectGain = function(target, effect)
-end
-
-itemObject.onEffectLose = function(target, effect)
 end
 
 return itemObject
