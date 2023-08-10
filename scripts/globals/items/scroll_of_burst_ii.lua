LandSandBoat/server

commit ea9df8c67896efbb37ce35929f01bb0d717447d0

[chore] Fix magic numbers used in addSpell and canLearnSpell
m

scripts/globals/items/scroll_of_burst_ii.lua
@@ -6,11 +6,11 @@
 local itemObject = {}
 
 itemObject.onItemCheck = function(target)
-    return target:canLearnSpell(213)
+    return target:canLearnSpell(xi.magic.spell.xi.magic.spell.BURST_II)
 end
 
 itemObject.onItemUse = function(target)
-    target:addSpell(213)
+    target:addSpell(xi.magic.spell.xi.magic.spell.BURST_II)
 end
 
 return itemObject

commit aa6079cfc65bb91c65b3c46020200ac45f3e1fd5

Fix Scroll of Burst II being unusable
This was broken in ea9df8c67896efbb37ce35929f01bb0d717447d0.

scripts/globals/items/scroll_of_burst_ii.lua
@@ -6,11 +6,11 @@
 local itemObject = {}
 
 itemObject.onItemCheck = function(target)
-    return target:canLearnSpell(xi.magic.spell.xi.magic.spell.BURST_II)
+    return target:canLearnSpell(xi.magic.spell.BURST_II)
 end
 
 itemObject.onItemUse = function(target)
-    target:addSpell(xi.magic.spell.xi.magic.spell.BURST_II)
+    target:addSpell(xi.magic.spell.BURST_II)
 end
 
 return itemObject