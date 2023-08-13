1894fce31e2eb3effec744429f683311abe02c67
Corey Sotiropoulos <coreysotiropoulos@protonmail.com>
2023-08-13 15:42:23 -0400

    Fix OMIT_NO_HISTORY AH setting

--- src/search/data_loader.cpp
+++ src/search/data_loader.cpp
@@ -87,18 +87,26 @@ std::vector<ahItem*> CDataLoader::GetAHItemsToCategory(uint8 AHCategoryID, int8*
     ShowDebug("try find category %u", AHCategoryID);
 
     std::vector<ahItem*> ItemList;
+    const char*          selectFrom = "item_basic";
+    if (settings::get<bool>("search.OMIT_NO_HISTORY"))
+    {
+        // Get all items that have sold before or are currently listed
+        selectFrom = "(SELECT item_basic.aH, item_basic.itemId, item_basic.stackSize "
+                     "FROM item_basic "
+                     "INNER JOIN auction_house ON item_basic.itemId = auction_house.itemid GROUP BY auction_house.itemid"
+                     ") AS item_basic";
+    }
 
     const char* fmtQuery = "SELECT item_basic.itemid, item_basic.stackSize, COUNT(*)-SUM(stack), SUM(stack) "
-                           "FROM item_basic "
+                           "FROM %s "
                            "LEFT JOIN auction_house ON item_basic.itemId = auction_house.itemid AND auction_house.buyer_name IS NULL "
                            "LEFT JOIN item_equipment ON item_basic.itemid = item_equipment.itemid "
                            "LEFT JOIN item_weapon ON item_basic.itemid = item_weapon.itemid "
-                           "WHERE aH = %u AND auction_house.itemid IS NOT NULL "
+                           "WHERE aH = %u "
                            "GROUP BY item_basic.itemid "
                            "%s";
 
-    int32 ret = sql->Query(fmtQuery, AHCategoryID, OrderByString);
-
+    int32 ret = sql->Query(fmtQuery, selectFrom, AHCategoryID, OrderByString);
     if (ret != SQL_ERROR && sql->NumRows() != 0)
     {
         while (sql->NextRow() == SQL_SUCCESS)
@@ -117,49 +125,6 @@ std::vector<ahItem*> CDataLoader::GetAHItemsToCategory(uint8 AHCategoryID, int8*
             }
 
             ItemList.emplace_back(PAHItem);
-        }
-    }
-
-    if (settings::get<bool>("search.OMIT_NO_HISTORY"))
-    {
-        const char* noQtyQuery = "SELECT item_basic.itemid, item_basic.stackSize "
-                                 "FROM item_basic "
-                                 "LEFT JOIN auction_house ON item_basic.itemId = auction_house.itemid "
-                                 "LEFT JOIN item_equipment ON item_basic.itemid = item_equipment.itemid "
-                                 "LEFT JOIN item_weapon ON item_basic.itemid = item_weapon.itemid "
-                                 "WHERE aH = %u AND auction_house.itemid IS NOT NULL "
-                                 "GROUP BY item_basic.itemid "
-                                 "%s";
-
-        int32 noQtyRet = sql->Query(noQtyQuery, AHCategoryID, OrderByString);
-        if (noQtyRet != SQL_ERROR && sql->NumRows() != 0)
-        {
-            while (sql->NextRow() == SQL_SUCCESS)
-            {
-                uint16 CurrItemID = sql->GetUIntData(0);
-                bool   itemFound  = false;
-                for (ahItem* PAHItem : ItemList)
-                {
-                    if (PAHItem->ItemID == CurrItemID)
-                    {
-                        itemFound = true;
-                        break;
-                    }
-                }
-
-                if (!itemFound)
-                {
-                    ahItem* PAHItem       = new ahItem;
-                    PAHItem->ItemID       = CurrItemID;
-                    PAHItem->SingleAmount = 0;
-                    PAHItem->StackAmount  = 0;
-                    if (sql->GetIntData(1) == 1)
-                    {
-                        PAHItem->StackAmount = -1;
-                    }
-                    ItemList.emplace_back(PAHItem);
-                }
-            }
         }
     }
 
