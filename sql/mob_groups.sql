cc6af61952308061d639b3d1ea627741fe28829b
Loxley <119976133+LoxleyXI@users.noreply.github.com>
2023-08-24 13:42:16 +0100

    Adds Garrison NPC mob_pools

--- sql/mob_groups.sql
+++ sql/mob_groups.sql
@@ -13282,11 +13282,11 @@ INSERT INTO `mob_groups` VALUES (7,6954,209,'Titan_Prime_HTBF',0,128,0,0,0,99,99
 
 -- Garrison NPCs (1 per level cap). These are inserted dynamically. Only min/max level matters.
 -- Consider adding a dynamic entity spawn param for min / max level so we only need 1 base mob group.
-INSERT INTO `mob_groups` VALUES (1,20001,210,'Garrison_20',0,129,0,0,0,15,20,1);
-INSERT INTO `mob_groups` VALUES (2,20001,210,'Garrison_30',0,129,0,0,0,25,30,1);
-INSERT INTO `mob_groups` VALUES (3,20001,210,'Garrison_40',0,129,0,0,0,35,40,1);
-INSERT INTO `mob_groups` VALUES (4,20001,210,'Garrison_50',0,129,0,0,0,45,50,1);
-INSERT INTO `mob_groups` VALUES (5,20001,210,'Garrison_75',0,129,0,0,0,70,75,1);
+INSERT INTO `mob_groups` VALUES (1,7071,210,'Garrison_20',0,129,0,0,0,15,20,1);
+INSERT INTO `mob_groups` VALUES (2,7071,210,'Garrison_30',0,129,0,0,0,25,30,1);
+INSERT INTO `mob_groups` VALUES (3,7071,210,'Garrison_40',0,129,0,0,0,35,40,1);
+INSERT INTO `mob_groups` VALUES (4,7071,210,'Garrison_50',0,129,0,0,0,45,50,1);
+INSERT INTO `mob_groups` VALUES (5,7071,210,'Garrison_75',0,129,0,0,0,70,75,1);
 
 -- ------------------------------------------------------------
 -- Cloister_of_Tides (Zone 211)
