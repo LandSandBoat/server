0b1a43415982914b1289fe7fb5601ed7f0c9fdd8
TracentEden <92269743+TracentEden@users.noreply.github.com>
2023-08-31 03:00:50 +0300

    Fix noble mold spawning

--- scripts/zones/The_Sanctuary_of_ZiTah/mobs/Myxomycete.lua
+++ scripts/zones/The_Sanctuary_of_ZiTah/mobs/Myxomycete.lua
@@ -9,15 +9,31 @@ require("scripts/globals/mobs")
 -----------------------------------
 local entity = {}
 
-entity.onMobRoam = function(mob)
-    local weather = mob:getWeather()
+entity.onMobSpawn = function(mob)
+    if mob:getID() == ID.mob.NOBLE_MOLD_PH then
+        mob:setLocalVar("timeToMorph", os.time() + math.random(43200, 57600)) -- 12:00:00 to 16:00:00
+    end
+end
 
-    if weather == xi.weather.RAIN or weather == xi.weather.SQUALL then
-        if xi.mob.phOnDespawn(mob, ID.mob.NOBLE_MOLD_PH, 100, math.random(43200, 57600), true) then -- 12 to 16 hours
-            local p = mob:getPos()
-            GetMobByID(ID.mob.NOBLE_MOLD):setSpawn(p.x, p.y, p.z, p.rot)
-            DespawnMob(mob:getID())
-        end
+entity.onMobDisengage = function(mob)
+    if mob:getID() == ID.mob.NOBLE_MOLD_PH then
+        mob:setLocalVar("timeToMorph", os.time() + math.random(43200, 57600)) -- 12:00:00 to 16:00:00
+    end
+end
+
+entity.onMobRoam = function(mob)
+    -- Noble Mold PH has been left alone for 12-16 hours and it is rainy
+    if
+        mob:getID() == ID.mob.NOBLE_MOLD_PH and
+        os.time() > mob:getLocalVar("timeToMorph") and
+        (mob:getWeather() == xi.weather.RAIN or
+        mob:getWeather() == xi.weather.SQUALL)
+    then
+        DisallowRespawn(ID.mob.NOBLE_MOLD_PH, true)
+        DespawnMob(ID.mob.NOBLE_MOLD_PH)
+        DisallowRespawn(ID.mob.NOBLE_MOLD, false)
+        local pos = mob:getPos()
+        SpawnMob(ID.mob.NOBLE_MOLD):setPos(pos.x, pos.y, pos.z, pos.rot)
     end
 end
 
