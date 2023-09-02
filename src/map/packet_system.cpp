6028a9c7320f8019e98c01a95fd65e13861d7f4b
WinterSolstice8 <60417494+wintersolstice8@users.noreply.github.com>
2023-09-02 10:32:53 -0600

    [core] Audit Party/Alliance messaging
    This should fix up little issues with invalid messages being sent that would never parse, such as your party/alliance never updating until you zone

--- src/map/packet_system.cpp
+++ src/map/packet_system.cpp
@@ -4440,9 +4440,8 @@ void SmallPacket0x071(map_session_data_t* const PSession, CCharEntity* const PCh
                             sql->AffectedRows())
                         {
                             ShowDebug("%s has removed %s from party", PChar->GetName(), data[0x0C]);
-                            uint8 removeData[8]{};
+                            uint8 removeData[4]{};
                             ref<uint32>(removeData, 0) = PChar->PParty->GetPartyID();
-                            ref<uint32>(removeData, 4) = id;
                             message::send(MSG_PT_RELOAD, removeData, sizeof removeData, nullptr);
                         }
                     }
@@ -4529,9 +4528,8 @@ void SmallPacket0x071(map_session_data_t* const PSession, CCharEntity* const PCh
                                 sql->AffectedRows())
                             {
                                 ShowDebug("%s has removed %s party from alliance", PChar->GetName(), data[0x0C]);
-                                uint8 removeData[8]{};
+                                uint8 removeData[4]{};
                                 ref<uint32>(removeData, 0) = PChar->PParty->GetPartyID();
-                                ref<uint32>(removeData, 4) = id;
                                 message::send(MSG_PT_RELOAD, removeData, sizeof removeData, nullptr);
                             }
                         }
@@ -4738,8 +4736,10 @@ void SmallPacket0x077(map_session_data_t* const PSession, CCharEntity* const PCh
             {
                 ShowDebug("(Alliance)Changing leader to %s", data[0x04]);
                 PChar->PParty->m_PAlliance->assignAllianceLeader((const char*)data[0x04]);
+
+                // MSG_PT_RELOAD also reloads all the alliances parties if available
                 uint8 leaderData[4]{};
-                ref<uint32>(leaderData, 0) = PChar->PParty->m_PAlliance->m_AllianceID;
+                ref<uint32>(leaderData, 0) = PChar->PParty->GetPartyID();
                 message::send(MSG_PT_RELOAD, leaderData, sizeof leaderData, nullptr);
             }
         }
