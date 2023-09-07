9dad29d48b7fc5522d41ce912ea37dbf335ca043
claywar <claywar@beerlab.dev>
2023-09-07 09:26:08 -0400

    Update module scripts for newer CI rules, fix xi.item references

--- modules/custom/lua/new_player_linkshell.lua
+++ modules/custom/lua/new_player_linkshell.lua
@@ -1,13 +1,13 @@
 -----------------------------------
 -- Set if you want new players to get a linkshell
 -----------------------------------
-require("modules/module_utils")
-require("scripts/globals/player")
+require('modules/module_utils')
+require('scripts/globals/player')
 -----------------------------------
-local m = Module:new("new_player_linkshell")
+local m = Module:new('new_player_linkshell')
 
-m:addOverride("xi.player.charCreate", function(player)
-    local lsName = "InsertLsNameHere" -- Name of linkshell
+m:addOverride('xi.player.charCreate', function(player)
+    local lsName = 'InsertLsNameHere' -- Name of linkshell
     player:addLinkpearl(lsName, true)
     super(player)
 end)
