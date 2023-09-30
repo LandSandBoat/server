e4ba5dd853573f0af2a85fe81e7cd1176cac2b34
Zach Toogood <zrtoogood@gmail.com>
2023-09-30 14:34:18 +0100

    Core-based level and instinct unlocks

--- sql/monstrosity_species.sql
+++ sql/monstrosity_species.sql
@@ -1,9 +1,7 @@
-
 DROP TABLE IF EXISTS `monstrosity_species`;
-
 CREATE TABLE `monstrosity_species` (
     `monstrosity_id` smallint(30) DEFAULT NULL, -- Used as the shift offset into the monstrosity.levels array
-    `name` -- Human-readable name. Only used for this file and logging.
+    `name` varchar(30) DEFAULT NULL, -- Human-readable name. Only used for this file and logging.
   -- `species_id` smallint(5) unsigned NOT NULL, -- Species ID sent from the client
   -- `name_id` smallint(3) unsigned NOT NULL,    -- Name ID sent from the client (re-used for variants)
   -- `look` smallint(3) unsigned NOT NULL,       -- Look data sent from the client
