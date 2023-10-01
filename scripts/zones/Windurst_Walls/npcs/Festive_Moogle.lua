6be70d2aaa7b9dd50a7d34edc9eebdbf0e286615
claywar <claywar@beerlab.dev>
2023-10-01 13:21:10 -0400

    Move Festive Moogle to global, add pell trades
    * Moves existing granted items from Festive Moogles to festive_moogle global
    * Slightly refactors existing code to use a single table mapping itemId and charvar
    * Adjusts some names in item enum to use their full length for future padding adjustment
    * Moves local table in crafting global `guildTable` to being attached to xi.crafting
    * Adds all pell trade items to festive moogle, along with appropriate item enum entries
    * Adds luautils binding `GetItemByID()` to support lookup for non-existent items
    * Adds utils.EVENT_CANCELLED_OPTION constant (1 << 30), 1073741824
    * Removes fulfilled comments from Mog Garden IDs.lua
    
    Update references to new xi.crafting.guildTable

--- scripts/zones/Windurst_Walls/npcs/Festive_Moogle.lua
+++ scripts/zones/Windurst_Walls/npcs/Festive_Moogle.lua
@@ -6,69 +6,20 @@
 -----------------------------------
 local entity = {}
 
-local stock =
-{
-    16119,  -- Nomad Cap
-    16118,  -- Moogle Cap
-    18401,  -- Moogle Rod
-    320,    -- Harpsichord
-    264,    -- Stuffed Chocobo
-    11290,  -- Tidal Talisman
-    11811,  -- Destrier Beret
-    10293,  -- Chocobo Shirt
-}
-
-local festiveItemVars =
-{
-    [1] = 'festiveMoogleNomadCap',
-    [2] = 'festiveMoogleMoogleCap',
-    [3] = 'festiveMoogleMoogleRod',
-    [4] = 'festiveMoogleHarpsichord',
-    [5] = 'festiveMooglestuffedChocobo',
-    [6] = 'festiveMoogleTidalTalisman',
-    [7] = 'festiveMoogleDestrierBeret',
-    [8] = 'festiveMoogleChocoboShirt',
-}
-
-local function getFestiveItems(player)
-    local festiveItemsAvailable = {}
-    for i = 1, #festiveItemVars, 1 do
-        if player:getCharVar(festiveItemVars[i]) == 1 then
-            table.insert(festiveItemsAvailable, stock[i])
-        end
-    end
-
-    return festiveItemsAvailable
-end
-
 entity.onTrade = function(player, npc, trade)
-    --TODO: trade of pells for prize
+    xi.festiveMoogle.onTrade(player, npc, trade)
 end
 
 entity.onTrigger = function(player, npc)
-    local festiveItems = getFestiveItems(player)
-    if festiveItems[1] then
-        player:startEvent(503, unpack(festiveItems))
-    else
-        player:startEvent(504)
-    end
+    xi.festiveMoogle.onTrigger(player, npc)
 end
 
 entity.onEventUpdate = function(player, csid, option, npc)
+    xi.festiveMoogle.onEventUpdate(player, csid, option, npc)
 end
 
 entity.onEventFinish = function(player, csid, option, npc)
-    local festiveItems = getFestiveItems(player)
-    if csid == 503 then
-        if npcUtil.giveItem(player, festiveItems[option]) then
-            for i = 1, #stock do
-                if stock[i] == festiveItems[option] then
-                    player:setCharVar(festiveItemVars[i], 0)
-                    break
-                end
-            end
-        end
-    end
+    xi.festiveMoogle.onEventFinish(player, csid, option, npc)
 end
 
 return entity
