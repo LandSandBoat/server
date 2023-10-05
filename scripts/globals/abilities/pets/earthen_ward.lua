a7f23f75d80967466fa9c7cdab2c8240a81e8401
TracentEden <92269743+TracentEden@users.noreply.github.com>
2023-10-05 05:20:47 +0300

    Fix dyna mob drop wrong currency and wyvern breath

--- scripts/globals/abilities/pets/earthen_ward.lua
+++ scripts/globals/abilities/pets/earthen_ward.lua
@@ -11,7 +11,19 @@ end
 
 abilityObject.onPetAbility = function(target, pet, skill)
     local amount = pet:getMainLvl() * 2 + 50
-    if target:addStatusEffect(xi.effect.STONESKIN, amount, 0, 900, 0, 0, 3) then
+
+    -- if current stoneskin is from Earthen Ward then overwrite
+    -- need this logic here because Earthern Ward SS overwrites itself
+    -- while normal spell-based SS does not overwrite itself
+    local earthenWardTier = 3
+    if target:hasStatusEffect(xi.effect.STONESKIN) then
+        local status = target:getStatusEffect(xi.effect.STONESKIN)
+        if status:getTier() == earthenWardTier then
+            target:delStatusEffectSilent(xi.effect.STONESKIN)
+        end
+    end
+
+    if target:addStatusEffect(xi.effect.STONESKIN, amount, 0, 900, 0, 0, earthenWardTier) then
         skill:setMsg(xi.msg.basic.SKILL_GAIN_EFFECT)
     else
         skill:setMsg(xi.msg.basic.SKILL_NO_EFFECT)
