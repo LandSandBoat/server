f31878fd24094a33021e0c8df11ff54d0f0c789c
Zach Toogood <zrtoogood@gmail.com>
2023-10-06 16:35:45 +0100

    Instinct mods for bee

--- scripts/commands/addallmonstrosity.lua
+++ scripts/commands/addallmonstrosity.lua
@@ -36,7 +36,7 @@ commandObj.onTrigger = function(player, target)
             [xi.monstrosity.species.MANDRAGORA] = 1,
             [xi.monstrosity.species.LIZARD]     = 1,
         },
-        instincts =
+        instincts = -- std::array<uint8, 64> instincts
         {
             [00] = 0xFF,
             [01] = 0xFF,
@@ -51,8 +51,20 @@ commandObj.onTrigger = function(player, target)
             [10] = 0xFF,
             [11] = 0xFF,
             [12] = 0xFF,
+            [13] = 0xFF,
+            [14] = 0xFF,
+            [15] = 0xFF,
+            [16] = 0xFF,
+            [17] = 0xFF,
+            [18] = 0xFF,
 
-            [20] = 0xFF, -- Unlock all first tier player race instincts
+            [20] = 0xFF,
+            [21] = 0xFF,
+            [22] = 0xFF,
+            [23] = 0xFF,
+
+            [62] = 0xFF,
+            [63] = 0xFF,
         },
         variants =
         {
