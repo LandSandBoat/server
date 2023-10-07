c306fa725c53378f0b9d305247232a6f5a05b770
Zach Toogood <zrtoogood@gmail.com>
2023-10-07 10:00:17 +0100

    Hook up sql helpers: ObjectToBlobString, GetBlobData

--- src/common/sql.h
+++ src/common/sql.h
@@ -186,13 +186,24 @@ public:
     std::string GetStringData(size_t col);
 
     template <typename T>
-    void GetBlobData(size_t col, T& destination)
+    void GetBlobData(size_t col, T* destination)
     {
-        // TTODO: Fix me
         size_t length = 0;
         char*  buffer = nullptr;
         GetData(col, &buffer, &length);
-        // std::memcpy((void*)destination, buffer, (length > sizeof(T) ? sizeof(T) : length));
+        std::memcpy(destination, buffer, (length > sizeof(T) ? sizeof(T) : length));
+    }
+
+    template <typename T>
+    std::string ObjectToBlobString(T* destination)
+    {
+        char buffer[sizeof(T) * 2 + 1];
+        {
+            char dataBlob[sizeof(T)];
+            std::memcpy(dataBlob, destination, sizeof(dataBlob));
+            EscapeStringLen(buffer, dataBlob, sizeof(dataBlob));
+        }
+        return std::string(buffer);
     }
 
     /// Frees the result of the query.
