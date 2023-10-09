fb7c7a264267257cd519c8953fca693e8db606cd
claywar <claywar@beerlab.dev>
2023-10-09 15:41:12 -0400

    Interaction - Hidden Quest: Monstrosity Bee Unlock

--- scripts/zones/Feretory/npcs/Suibhne.lua
+++ scripts/zones/Feretory/npcs/Suibhne.lua
@@ -1,26 +1,20 @@
 -----------------------------------
 -- Area: Feretory
---  NPC: Suibhne
--- !pos TODO
------------------------------------
-require('scripts/globals/monstrosity')
+-- NPC: Suibhne
+-- !pos -366 -3.612 -466 285
 -----------------------------------
 local entity = {}
 
 entity.onTrade = function(player, npc, trade)
-    xi.monstrosity.suibhneOnTrade(player, npc, trade)
 end
 
 entity.onTrigger = function(player, npc)
-    xi.monstrosity.suibhneOnTrigger(player, npc)
 end
 
 entity.onEventUpdate = function(player, csid, option, npc)
-    xi.monstrosity.suibhneOnEventUpdate(player, csid, option, npc)
 end
 
 entity.onEventFinish = function(player, csid, option, npc)
-    xi.monstrosity.suibhneOnEventFinish(player, csid, option, npc)
 end
 
 return entity
