cbc00a67acf737ec17479b3f2478c80c2f1e79ce
claywar <claywar@beerlab.dev>
2023-11-05 10:18:38 -0500

    Move Sigil NPC onEventUpdate, onEventFinish to global
    Checkin mapping for sigil optionType
    
    Add sigil bonus cost table
    
    Move sigil functions to global event finish
    
    Fix event finish logic
    
    Additional changes from testing
    
    Add more detailed comment to logic in event update
    
    Additional updates from testing
    
    Remove bonuses mask from onTrigger which have no impact
    
    Utilize unused variable as placeholder

--- scripts/globals/campaign.lua
+++ scripts/globals/campaign.lua
@@ -218,11 +218,32 @@ local noteRewardItems =
     },
 }
 
-local sigilNpcEvent =
+local sigilNpcInfo =
 {
-    [xi.zone.BASTOK_MARKETS_S    ] = 13,
-    [xi.zone.SOUTHERN_SAN_DORIA_S] = 110,
-    [xi.zone.WINDURST_WATERS_S   ] = 13,
+    [xi.zone.BASTOK_MARKETS_S    ] = {  13, 2 }, -- !pos -248.5 0 81.2 87
+    [xi.zone.SOUTHERN_SAN_DORIA_S] = { 110, 1 }, -- !pos 107 1 -31 80
+    [xi.zone.WINDURST_WATERS_S   ] = {  13, 3 }, -- !pos -31.869 -6.009 226.793 94
+}
+
+-- There appears to be no mathematical correlation between combination of effects and their
+-- cost.  Each index in the table represents the bitmask value, and corresponding cost.
+local bonusEffectCosts =
+{
+    [ 1] = 50,
+    [ 2] = 50,
+    [ 3] = 100,
+    [ 4] = 50,
+    [ 5] = 100,
+    [ 6] = 100,
+    [ 7] = 150,
+    [ 8] = 100,
+    [ 9] = 150,
+    [10] = 150,
+    [11] = 150,
+    [12] = 100,
+    [13] = 150,
+    [14] = 150,
+    [15] = 200,
 }
 
 -- Returns the Vanadiel time in which Sigil will expire or 0
@@ -257,27 +278,21 @@ local function getSigilRankMask(player)
 end
 
 local function getSigilMenuOptions(player)
-    local optionMask = 0
-
     -- Bit Values:
     -- 0: Medal Expired (1 = Expired)
     -- 1: Sigil state (1 = No Sigil Active)
     -- 2: Valaineral Available (1 = Available)
     -- 3: Adelheid Available (1 = Available)
 
-    -- TODO: Implement logic for the above
-    return 3 + xi.extravaganza.campaignActive() * 4
-end
+    local optionMask = xi.extravaganza.campaignActive() * 4
 
-local function getAvailableBonuses(player)
-    -- Bit Values (1 = Available):
-    -- 0: Regen
-    -- 1: Refresh
-    -- 2: Meal Duration
-    -- 3: EXP Loss Reduction
+    if not player:hasStatusEffect(xi.effect.SIGIL) then
+        optionMask = utils.mask.setBit(optionMask, 1, true)
+    end
 
-    -- TODO: Implement logic for the above
-    return 0
+    -- TODO: Hangle Campaign Medal Active/Expired when implemented
+
+    return optionMask
 end
 
 -- TODO: Is this deprecated by the mask function?
@@ -297,10 +312,13 @@ end
 
 -- Sigil NPC
 xi.campaign.sigilOnTrigger = function(player, npc)
-    local baseEvent     = sigilNpcEvent[player:getZoneID()]
-    local freelanceMask = 0 -- TODO: Update on implementation
+    local baseEvent     = sigilNpcInfo[player:getZoneID()][1]
+    local freelanceMask = 0
 
-    if medalRank == 0 then
+    -- TODO: Update freelanceMask on implementation.  Bit 0 is required
+    -- to be true to allow for Reduced XP Loss
+
+    if xi.campaign.getMedalRank(player) == 0 then
         player:startEvent(baseEvent + 1)
     else
         player:startEvent(baseEvent,
@@ -309,7 +327,7 @@ xi.campaign.sigilOnTrigger = function(player, npc)
             freelanceMask,
             getSigilMenuOptions(player),
             getSigilRankMask(player),
-            getAvailableBonuses(player),
+            0,
             getSigilTimeStamp(player),
             0
         )
@@ -317,9 +335,85 @@ xi.campaign.sigilOnTrigger = function(player, npc)
 end
 
 xi.campaign.sigilOnEventUpdate = function(player, csid, option, npc)
+    local optionType   = bit.band(option, 0xF)
 
+    if
+        csid == 13 and
+        optionType == 2
+    then
+        local itemPage     = bit.band(bit.rshift(option, 4), 0xF)
+        local selectedItem = bit.rshift(option, 8)
+        local itemInfo     = noteRewardItems[player:getZoneID()][itemPage][selectedItem]
+        local canEquip     = 2
+
+        -- canEquip Values:
+        -- 0: Incorrect Job
+        -- 1: Incorrect Level
+        -- 2: Everything is in order
+        -- 3: (Or Greater) exits the menu
+
+        -- NOTE: There are some items that are not equipment, and checking reqLvl will
+        -- return 0 for non-equippable items.  There are two canEquipItem calls to simplify
+        -- needing to check types.  The first checks job requirement only, followed by
+        -- job requirement _and_ level so that the appropriate message is displayed.
+
+        if GetItemByID(itemInfo[1]):getReqLvl() > 0 then
+            if not player:canEquipItem(itemInfo[1]) then
+                canEquip = 0
+            elseif not player:canEquipItem(itemInfo[1], true) then
+                canEquip = 1
+            end
+        end
+
+        player:updateEvent(0, 0, 0, 0, 0, 0, 0, canEquip)
+    end
 end
 
 xi.campaign.sigilOnEventFinish = function(player, csid, option, npc)
+    local zoneId = player:getZoneID()
 
+    if
+        option ~= utils.EVENT_CANCELLED_OPTION and
+        csid == sigilNpcInfo[zoneId][1]
+    then
+        local optionType = bit.band(option, 0xF)
+
+        if optionType == 1 then
+            local selectedEffects = bit.rshift(option, 16)
+            local bonusCost       = bonusEffectCosts[selectedEffects] and bonusEffectCosts[selectedEffects] or 0
+            local duration        = 10800 + ((15 * xi.campaign.getMedalRank(player)) * 60) -- 3hrs +15 min per medal (minimum 3hr 15 min with 1st medal)
+            local subPower        = 35 -- Sets % trigger for regen/refresh. Static at minimum value (35%) for now.
+            -- Selected Effect Mask:
+            -- 0: Regen
+            -- 1: Refresh
+            -- 2: Meal Duration
+            -- 3: EXP Loss Reduction
+
+            player:delStatusEffectsByFlag(xi.effectFlag.INFLUENCE, true)
+            player:addStatusEffect(xi.effect.SIGIL, selectedEffects, 0, duration, 0, subPower, 0)
+            player:messageSpecial(zones[zoneId].text.ALLIED_SIGIL)
+
+            if bonusCost > 0 then
+                player:delCurrency('allied_notes', bonusCost)
+            end
+
+        elseif optionType == 2 then
+            local itemPage     = bit.band(bit.rshift(option, 4), 0xF)
+            local selectedItem = bit.rshift(option, 8)
+            local itemInfo     = noteRewardItems[zoneId][itemPage][selectedItem]
+
+            if npcUtil.giveItem(player, itemInfo[1]) then
+                local itemPrice = itemInfo[2]
+
+                if
+                    itemInfo[3] and
+                    player:getCampaignAllegiance() ~= sigilNpcInfo[zoneId][2]
+                then
+                    itemPrice = itemPrice * 1.5
+                end
+
+                player:delCurrency('allied_notes', itemPrice)
+            end
+        end
+    end
 end
