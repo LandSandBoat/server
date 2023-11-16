c0d433faf42db1c98d414c97655f66e10c03139c
Xaver-DaRed <xaver.entropia@gmail.com>
2023-11-16 02:19:51 +0100

    Add new "weight" modifier to status effects

--- scripts/effects/curse.lua
+++ scripts/effects/curse.lua
@@ -7,7 +7,7 @@ effectObject.onEffectGain = function(target, effect)
     --NOTE: The power amount dictates the amount to REDUCE MAX VALUES BY. E.g. Power=75 means 'reduce max hp/mp by 75%'
     target:addMod(xi.mod.HPP, -effect:getPower())
     target:addMod(xi.mod.MPP, -effect:getPower())
-    target:addMod(xi.mod.MOVE, -effect:getPower())
+    target:addMod(xi.mod.MOVE_SPEED_WEIGHT_PENALTY, effect:getPower())
 end
 
 effectObject.onEffectTick = function(target, effect)
@@ -17,7 +17,7 @@ effectObject.onEffectLose = function(target, effect)
     --restore HP and MP to its former state. Remove 100% slow
     target:delMod(xi.mod.HPP, -effect:getPower())
     target:delMod(xi.mod.MPP, -effect:getPower())
-    target:delMod(xi.mod.MOVE, -effect:getPower())
+    target:delMod(xi.mod.MOVE_SPEED_WEIGHT_PENALTY, effect:getPower())
 end
 
 return effectObject
--- scripts/effects/overload.lua
+++ scripts/effects/overload.lua
@@ -8,7 +8,7 @@ effectObject.onEffectGain = function(target, effect)
     if pet then
         pet:setLocalVar('overload', 1)
         pet:addMod(xi.mod.HASTE_MAGIC, -5000)
-        pet:addMod(xi.mod.MOVE, -50)
+        pet:addMod(xi.mod.MOVE_SPEED_WEIGHT_PENALTY, 50)
         pet:addMod(xi.mod.EVA, -10)
     end
 end
@@ -21,7 +21,7 @@ effectObject.onEffectLose = function(target, effect)
     if pet and pet:getLocalVar('overload') ~= 0 then
         pet:setLocalVar('overload', 0)
         pet:delMod(xi.mod.HASTE_MAGIC, -5000)
-        pet:delMod(xi.mod.MOVE, -50)
+        pet:delMod(xi.mod.MOVE_SPEED_WEIGHT_PENALTY, 50)
         pet:delMod(xi.mod.EVA, -10)
     end
 end
