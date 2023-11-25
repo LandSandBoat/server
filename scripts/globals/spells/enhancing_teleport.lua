1ef3c6179a738b6082f79e203fb412b6ec019173
TeoTwawki <arawn.annwn@yandex.com>
2023-11-25 02:16:31 -0500

    correct timing of warp, retrace, teleport, and recall spells
    - float values are invalid
    - this is not an unknown, it's the duration of the animation and status effect

--- scripts/globals/spells/enhancing_teleport.lua
+++ scripts/globals/spells/enhancing_teleport.lua
@@ -10,20 +10,20 @@ xi.spells.enhancing = xi.spells.enhancing or {}
 -- Table variables.
 local pTable =
 {
--- Structure:       [spellId] = { Teleport, Key_Item, unknown_parameter, campaign },
-    [xi.magic.spell.ESCAPE        ] = { xi.teleport.id.ESCAPE,  0,                                4, false },
-    [xi.magic.spell.RECALL_JUGNER ] = { xi.teleport.id.JUGNER,  xi.ki.JUGNER_GATE_CRYSTAL,      4.7, false },
-    [xi.magic.spell.RECALL_MERIPH ] = { xi.teleport.id.MERIPH,  xi.ki.MERIPHATAUD_GATE_CRYSTAL, 4.7, false },
-    [xi.magic.spell.RECALL_PASHH  ] = { xi.teleport.id.PASHH,   xi.ki.PASHHOW_GATE_CRYSTAL,     4.7, false },
-    [xi.magic.spell.RETRACE       ] = { xi.teleport.id.RETRACE, 0,                                4, true  },
-    [xi.magic.spell.TELEPORT_ALTEP] = { xi.teleport.id.ALTEP,   xi.ki.ALTEPA_GATE_CRYSTAL,      4.7, false },
-    [xi.magic.spell.TELEPORT_DEM  ] = { xi.teleport.id.DEM,     xi.ki.DEM_GATE_CRYSTAL,         4.7, false },
-    [xi.magic.spell.TELEPORT_HOLLA] = { xi.teleport.id.HOLLA,   xi.ki.HOLLA_GATE_CRYSTAL,       4.7, false },
-    [xi.magic.spell.TELEPORT_MEA  ] = { xi.teleport.id.MEA,     xi.ki.MEA_GATE_CRYSTAL,         4.7, false },
-    [xi.magic.spell.TELEPORT_VAHZL] = { xi.teleport.id.VAHZL,   xi.ki.VAHZL_GATE_CRYSTAL,       4.7, false },
-    [xi.magic.spell.TELEPORT_YHOAT] = { xi.teleport.id.YHOAT,   xi.ki.YHOATOR_GATE_CRYSTAL,     4.7, false },
-    [xi.magic.spell.WARP          ] = { xi.teleport.id.WARP,    0,                                4, false },
-    [xi.magic.spell.WARP_II       ] = { xi.teleport.id.WARP,    0,                              3.4, false },
+-- Structure:       [spellId] = { Teleport, Key_Item, duration, campaign },
+    [xi.magic.spell.ESCAPE        ] = { xi.teleport.id.ESCAPE,  0,                              4, false },
+    [xi.magic.spell.RECALL_JUGNER ] = { xi.teleport.id.JUGNER,  xi.ki.JUGNER_GATE_CRYSTAL,      4, false },
+    [xi.magic.spell.RECALL_MERIPH ] = { xi.teleport.id.MERIPH,  xi.ki.MERIPHATAUD_GATE_CRYSTAL, 4, false },
+    [xi.magic.spell.RECALL_PASHH  ] = { xi.teleport.id.PASHH,   xi.ki.PASHHOW_GATE_CRYSTAL,     4, false },
+    [xi.magic.spell.RETRACE       ] = { xi.teleport.id.RETRACE, 0,                              3, true  },
+    [xi.magic.spell.TELEPORT_ALTEP] = { xi.teleport.id.ALTEP,   xi.ki.ALTEPA_GATE_CRYSTAL,      4, false },
+    [xi.magic.spell.TELEPORT_DEM  ] = { xi.teleport.id.DEM,     xi.ki.DEM_GATE_CRYSTAL,         4, false },
+    [xi.magic.spell.TELEPORT_HOLLA] = { xi.teleport.id.HOLLA,   xi.ki.HOLLA_GATE_CRYSTAL,       4, false },
+    [xi.magic.spell.TELEPORT_MEA  ] = { xi.teleport.id.MEA,     xi.ki.MEA_GATE_CRYSTAL,         4, false },
+    [xi.magic.spell.TELEPORT_VAHZL] = { xi.teleport.id.VAHZL,   xi.ki.VAHZL_GATE_CRYSTAL,       4, false },
+    [xi.magic.spell.TELEPORT_YHOAT] = { xi.teleport.id.YHOAT,   xi.ki.YHOATOR_GATE_CRYSTAL,     4, false },
+    [xi.magic.spell.WARP          ] = { xi.teleport.id.WARP,    0,                              3, false },
+    [xi.magic.spell.WARP_II       ] = { xi.teleport.id.WARP,    0,                              3, false },
 }
 
 -- Check for "Retrace" Spell.
@@ -40,7 +40,7 @@ xi.spells.enhancing.useTeleportSpell = function(caster, target, spell)
     local spellId    = spell:getID()
     local teleportId = pTable[spellId][1]
     local keyItem    = pTable[spellId][2]
-    local paramFive  = pTable[spellId][3]
+    local duration   = pTable[spellId][3]
     local campaign   = pTable[spellId][4]
 
     if
@@ -48,7 +48,7 @@ xi.spells.enhancing.useTeleportSpell = function(caster, target, spell)
         (keyItem == 0 or (keyItem > 0 and target:hasKeyItem(keyItem))) and
         (not campaign or (campaign and target:getCampaignAllegiance() > 0))
     then
-        target:addStatusEffectEx(xi.effect.TELEPORT, 0, teleportId, 0, paramFive)
+        target:addStatusEffectEx(xi.effect.TELEPORT, 0, teleportId, 0, duration)
         spell:setMsg(xi.msg.basic.MAGIC_TELEPORT)
     else
         spell:setMsg(xi.msg.basic.NONE)
