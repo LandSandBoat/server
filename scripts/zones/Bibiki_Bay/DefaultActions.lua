62ed44c059d2a1dda1dbcd2fb94ecac983334d06
hookstar <lance.hooker@gmail.com>
2023-11-30 17:12:59 -0500

    [Quest] One Good Dead sql and reference corrections.
    [Quest] One Good Dead sql and reference corrections.

--- scripts/zones/Bibiki_Bay/DefaultActions.lua
+++ scripts/zones/Bibiki_Bay/DefaultActions.lua
@@ -2,5 +2,6 @@ local ID = require('scripts/zones/Bibiki_Bay/IDs')
 
 return {
     ['qm_dalham']  = { messageSpecial = ID.text.NOTHING_LEFT_INTEREST },
+    ['qm_deed']    = { messageSpecial = ID.text.NOTHING_LEFT_INTEREST },
     ['Warmachine'] = { text = ID.text.CLUNK_CLUNK_WHIRL_WHIZZ },
 }
