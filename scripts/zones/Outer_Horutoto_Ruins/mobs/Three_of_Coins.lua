c2f0514b95af5c9061108dd5f43b77e7bb9eb3c8
Flibe <augshelakh@gmail.com>
2023-12-03 20:48:03 -0500

    AMK6: Added mechanic to drop KI orbs for cardians

--- scripts/zones/Outer_Horutoto_Ruins/mobs/Three_of_Coins.lua
+++ scripts/zones/Outer_Horutoto_Ruins/mobs/Three_of_Coins.lua
@@ -1,12 +1,11 @@
 -----------------------------------
 -- Area: Outer Horutoto Ruins
---  Mob: Five of Coins
+--  Mob: Three of Coins
 -----------------------------------
 local entity = {}
 
 entity.onMobDeath = function(mob, player, optParams)
     xi.amk.helpers.cardianOrbDrop(mob, player, xi.ki.ORB_OF_COINS)
-    xi.regime.checkRegime(player, mob, 664, 4, xi.regime.type.GROUNDS)
 end
 
 return entity
