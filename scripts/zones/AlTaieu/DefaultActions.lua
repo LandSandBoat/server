048a69818dddf423212e752ea6500d8a47053b5a
HeavensSword <heavenssword88@aol.com>
2023-12-19 17:14:02 -0500

    Implemented Al'Taieu Map Mini-quest in Interaction Framework
    - Fixed Lua sanity check format errors

--- scripts/zones/AlTaieu/DefaultActions.lua
+++ scripts/zones/AlTaieu/DefaultActions.lua
@@ -2,5 +2,4 @@ local ID = require("scripts/zones/AlTaieu/IDs")
 
 return {
     ['_0x0']       = { messageSpecial = ID.text.IMPERVIOUS_FIELD_BLOCKS },
-    ['Quasilumin'] = { text = ID.text.QUASILUMIN_01 },
 }
