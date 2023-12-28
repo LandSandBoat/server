d9c1e486619a6ba9bbd1c202ea26680e835f682c
Zach Toogood <zrtoogood@gmail.com>
2023-12-28 17:39:14 +0000

    [core] Fix minor issue with Tracy build

--- src/common/tracy.h
+++ src/common/tracy.h
@@ -24,7 +24,8 @@
 // clang-format off
 #ifdef TRACY_ENABLE
 #include "tracy/Tracy.hpp"
-#include "cbasetypes.h"
+#include <fmt/format.h>
+#include <string>
 
 #define TracyFrameMark          FrameMark
 #define TracyZoneScoped         ZoneScoped
@@ -35,14 +36,14 @@
 #define TracyZoneString(str)    ZoneText(str.c_str(), str.size())
 #define TracyZoneCString(cstr)  ZoneText(cstr, std::strlen(cstr))
 #define TracyMessageStr(str)    TracyMessage(str.c_str(), str.size())
-#define TracySetThreadName(str) tracy::SetThreadName(str);
+#define TracySetThreadName(str) tracy::SetThreadName(str)
 
-inline std::string Hex8ToString(uint8 hex)
+inline std::string Hex8ToString(std::uint8_t hex)
 {
     return fmt::format("0x{:02X}", hex);
 }
 
-inline std::string Hex16ToString(uint16 hex)
+inline std::string Hex16ToString(std::uint16_t hex)
 {
     return fmt::format("0x{:04X}", hex);
 }
@@ -71,23 +72,23 @@ inline std::string Hex16ToString(uint16 hex)
     TracyReportGraphBytes("Lua Memory Usage", static_cast<double>(lua_gc(L, LUA_GCCOUNT, 0)) * 1024.0); \
 
 #else // Empty stubs for regular builds
-#define TracyFrameMark                     std::ignore = 0;
-#define TracyZoneScoped                    std::ignore = 0;
-#define TracyZoneScopedN(n)                std::ignore = n;
-#define TracyZoneNamed(var)                std::ignore = #var;
-#define TracyZoneText(n, l)                std::ignore = n; std::ignore = l;
-#define TracyZoneScopedC(c)                std::ignore = c;
-#define TracyZoneString(str)               std::ignore = str;
-#define TracyZoneCString(cstr)             std::ignore = cstr;
-#define TracyZoneIString(istr)             std::ignore = istr;
-#define TracyZoneHex8(num)                 std::ignore = num;
-#define TracyZoneHex16(num)                std::ignore = num;
-#define TracyReportGraphNumber(name, num)  std::ignore = name; std::ignore = num;
-#define TracyReportGraphBytes(name, num)   std::ignore = name; std::ignore = num;
-#define TracyReportGraphPercent(name, num) std::ignore = name; std::ignore = num;
-#define TracyReportLuaMemory(L)            std::ignore = L;
-#define TracyMessageStr(str)               std::ignore = str;
-#define TracySetThreadName(str)            std::ignore = str;
+#define TracyFrameMark                     std::ignore = 0
+#define TracyZoneScoped                    std::ignore = 0
+#define TracyZoneScopedN(n)                std::ignore = n
+#define TracyZoneNamed(var)                std::ignore = #var
+#define TracyZoneText(n, l)                std::ignore = n; std::ignore = l
+#define TracyZoneScopedC(c)                std::ignore = c
+#define TracyZoneString(str)               std::ignore = str
+#define TracyZoneCString(cstr)             std::ignore = cstr
+#define TracyZoneIString(istr)             std::ignore = istr
+#define TracyZoneHex8(num)                 std::ignore = num
+#define TracyZoneHex16(num)                std::ignore = num
+#define TracyReportGraphNumber(name, num)  std::ignore = name; std::ignore = num
+#define TracyReportGraphBytes(name, num)   std::ignore = name; std::ignore = num
+#define TracyReportGraphPercent(name, num) std::ignore = name; std::ignore = num
+#define TracyReportLuaMemory(L)            std::ignore = L
+#define TracyMessageStr(str)               std::ignore = str
+#define TracySetThreadName(str)            std::ignore = str
 #endif
 // clang-format on
 
