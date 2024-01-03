45ad07bff1b04d0fb7a4f8ac3dac526727706fbb
Flibe <85970456+Flibe-XI@users.noreply.github.com>
2024-01-03 09:33:26 -0500

    [AMK][Lua] Cardian orb drop mechanic (#4800)
    * AMK6: Added mechanic to drop KI orbs for cardians
    
    * Fixed null player check

--- scripts/zones/Outer_Horutoto_Ruins/mobs/Four_of_Coins.lua
+++ scripts/zones/Outer_Horutoto_Ruins/mobs/Four_of_Coins.lua
@@ -5,6 +5,7 @@
 local entity = {}
 
 entity.onMobDeath = function(mob, player, optParams)
+    xi.amk.helpers.cardianOrbDrop(mob, player, xi.ki.ORB_OF_COINS)
     xi.regime.checkRegime(player, mob, 663, 4, xi.regime.type.GROUNDS)
 end
 
