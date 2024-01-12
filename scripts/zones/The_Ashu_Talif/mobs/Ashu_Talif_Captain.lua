391f76d795c22b31c9f888105a85b872bf26cb36
Matt Magdon <mmagdon1027@gmail.com>
2024-01-12 14:24:16 -0500

    Move Ashu Talif Captain logic into helper function, call in onMobEngaged

--- scripts/zones/The_Ashu_Talif/mobs/Ashu_Talif_Captain.lua
+++ scripts/zones/The_Ashu_Talif/mobs/Ashu_Talif_Captain.lua
@@ -6,8 +6,39 @@ local ID = zones[xi.zone.THE_ASHU_TALIF]
 -----------------------------------
 local entity = {}
 
+local captainEngageSequence = function(mob)
+    local jumped = mob:getLocalVar('jump')
+    local ready = mob:getLocalVar('ready')
+
+    -- Becomes ready when the Crew is engaged. Jump down!
+    if ready == 1 and jumped == 0 then
+        mob:setLocalVar('jump', 1)
+        mob:showText(mob, ID.text.OVERPOWERED_CREW)
+        mob:hideName(true)
+        mob:entityAnimationPacket('jmp0')
+        mob:timer(2000, function(m)
+            m:setPos(0, -22, 13, 192)
+            m:entityAnimationPacket('jmp1')
+            m:showText(mob, ID.text.TEST_YOUR_BLADES)
+            m:timer(2000, function(mAnimation)
+                mAnimation:hideName(false)
+                mAnimation:setUntargetable(false)
+            end)
+        end)
+    end
+end
+
 entity.onMobSpawn = function(mob)
     mob:setUnkillable(true)
+end
+
+entity.onMobEngaged = function(mob, target)
+    captainEngageSequence(mob)
+end
+
+entity.onMobRoam = function(mob)
+    -- failsafe in case of party wipe/reraise
+    captainEngageSequence(mob)
 end
 
 entity.onMobFight = function(mob, target)
@@ -29,29 +60,6 @@ entity.onMobFight = function(mob, target)
             m:showText(m, ID.text.FOR_THE_BLACK_COFFIN)
         end
     end)
-end
-
-entity.onMobRoam = function(mob)
-    local jumped = mob:getLocalVar('jump')
-    local ready = mob:getLocalVar('ready')
-
-    -- Becomes ready when the Crew is engaged. Jump down!
-    if ready == 1 and jumped == 0 then
-        mob:showText(mob, ID.text.OVERPOWERED_CREW)
-        mob:hideName(true)
-        mob:entityAnimationPacket('jmp0')
-        mob:timer(2000, function(m)
-            m:setPos(0, -22, 13, 192)
-            m:entityAnimationPacket('jmp1')
-            m:showText(mob, ID.text.TEST_YOUR_BLADES)
-            m:timer(2000, function(mAnimation)
-                mAnimation:hideName(false)
-                mAnimation:setUntargetable(false)
-            end)
-        end)
-
-        mob:setLocalVar('jump', 1)
-    end
 end
 
 entity.onMobDisengage = function(mob, target)
