694d7babf5d87114f959254d567b7306cdfbd321
Flibe <augshelakh@gmail.com>
2024-02-01 23:53:21 -0500

    puzzle 3 triva challenge implementation
    Almost all logic for determining cutscenes is handled by event arguments
    I've done my best to make all the arguments self-explanatory via their names

---
+++ scripts/zones/Xarcabard/npcs/Option_Two.lua
@@ -0,0 +1,21 @@
+-----------------------------------
+-- Area: Xarcabard
+-- NPC: Option Two
+-- Involved in Mission: A Moogle Kupo d'Etat mission 13 - A Challenge! You Could Be a Winner
+-- !pos 66 -24 -191 112
+-----------------------------------
+local entity = {}
+
+entity.onTrade = function(player, npc, trade)
+end
+
+entity.onTrigger = function(player, npc)
+end
+
+entity.onEventUpdate = function(player, csid, option, npc)
+end
+
+entity.onEventFinish = function(player, csid, option, npc)
+end
+
+return entity
