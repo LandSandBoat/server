73fbcdc4ea7ca5b277cbc1bb7255808fd83391bf
Flibe <augshelakh@gmail.com>
2024-02-17 00:26:30 -0500

    puzzle 4 implemented
    Various bug fixes, and repeatability added to lonely evergreen, whereas
    shadowy pillar is not needed to be talked to again after losing battle

---
+++ scripts/zones/Castle_Zvahl_Baileys/DefaultActions.lua
@@ -0,0 +1,6 @@
+local ID = zones[xi.zone.CASTLE_ZVAHL_BAILEYS]
+
+return {
+    ['Flame_of_Fate']  = { messageSpecial = ID.text.NOTHING_OUT_OF_ORDINARY },
+    ['Shadowy_Pillar'] = { messageSpecial = ID.text.NOTHING_OUT_OF_ORDINARY },
+}
