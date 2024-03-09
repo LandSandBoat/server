1868a50a5cd414e50b3cd87480acd67e8ec4371f
Tal Ben-Eliezer <beneliezer@gmail.com>
2024-03-09 10:22:04 -0500

    fix script for smite_of_rage

--- scripts/actions/mobskills/smite_of_rage.lua
+++ scripts/actions/mobskills/smite_of_rage.lua
@@ -10,11 +10,14 @@ end
 
 mobskillObject.onMobWeaponSkill = function(target, mob, skill)
     local numhits = 1
-    local accmod = 1
-    local dmgmod = 2.7
+    local accmod  = 1
+    local dmgmod  = mob:getWeaponDmg() * 0.03
+
     local info = xi.mobskills.mobPhysicalMove(mob, target, skill, numhits, accmod, dmgmod, xi.mobskills.magicalTpBonus.NO_EFFECT)
-    local dmg = xi.mobskills.mobFinalAdjustments(info.dmg, mob, skill, target, xi.attackType.PHYSICAL, xi.damageType.SLASHING, info.hitslanded * math.random(2, 3))
+    local dmg  = xi.mobskills.mobFinalAdjustments(info.dmg, mob, skill, target, xi.attackType.PHYSICAL, xi.damageType.SLASHING, xi.mobskills.shadowBehavior.NUMSHADOWS_1)
+
     target:takeDamage(dmg, mob, xi.attackType.PHYSICAL, xi.damageType.SLASHING)
+
     return dmg
 end
 
