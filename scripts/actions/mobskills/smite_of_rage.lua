5c29f94a4d234a67100c6a5e33d2da8274124004
Tal Ben-Eliezer <beneliezer@gmail.com>
2024-03-11 13:00:31 -0400

    update parameters per review suggestion

--- scripts/actions/mobskills/smite_of_rage.lua
+++ scripts/actions/mobskills/smite_of_rage.lua
@@ -11,10 +11,10 @@ end
 mobskillObject.onMobWeaponSkill = function(target, mob, skill)
     local numhits = 1
     local accmod  = 1
-    local dmgmod  = mob:getWeaponDmg() * 0.03
+    local dmgmod  = 3
 
     local info = xi.mobskills.mobPhysicalMove(mob, target, skill, numhits, accmod, dmgmod, xi.mobskills.magicalTpBonus.NO_EFFECT)
-    local dmg  = xi.mobskills.mobFinalAdjustments(info.dmg, mob, skill, target, xi.attackType.PHYSICAL, xi.damageType.SLASHING, xi.mobskills.shadowBehavior.NUMSHADOWS_1)
+    local dmg  = xi.mobskills.mobFinalAdjustments(info.dmg, mob, skill, target, xi.attackType.PHYSICAL, xi.damageType.SLASHING, info.hitslanded)
 
     target:takeDamage(dmg, mob, xi.attackType.PHYSICAL, xi.damageType.SLASHING)
 
