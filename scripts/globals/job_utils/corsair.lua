5b29257f1a0691124a55cc35851738ae4fe1101b
MowFord <131182600+MowFord@users.noreply.github.com>
2024-03-11 18:38:09 -0500

    Chance to automatic 11 when previous roll value is 5+

--- scripts/globals/job_utils/corsair.lua
+++ scripts/globals/job_utils/corsair.lua
@@ -254,7 +254,7 @@ xi.job_utils.corsair.useDoubleUp = function(caster, target, ability, action)
         caster:setLocalVar('corsairActiveRoll', duEffect:getSubType())
         local snakeEye = caster:getStatusEffect(xi.effect.SNAKE_EYE)
         if snakeEye then
-            if prevRoll:getPower() >= 5 and math.random(100) < snakeEye:getPower() then
+            if roll >= 5 and math.random(100) < snakeEye:getPower() then
                 roll = 11
             else
                 roll = roll + 1
