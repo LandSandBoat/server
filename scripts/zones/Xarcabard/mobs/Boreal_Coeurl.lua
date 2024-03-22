8156455853546e6ec4fc0c932ebfafd023b9fad7
Nemisis1987 <90221325+Nemisis1987@users.noreply.github.com>
2024-03-22 10:37:21 -0500

    Added pathing for LB2 NMs

--- scripts/zones/Xarcabard/mobs/Boreal_Coeurl.lua
+++ scripts/zones/Xarcabard/mobs/Boreal_Coeurl.lua
@@ -8,10 +8,43 @@ local ID = zones[xi.zone.XARCABARD]
 -----------------------------------
 local entity = {}
 
+local pathNodes = 
+{
+    { x = 580.23, y = -9.68,  z = 290.94, }, -- 1 (Origin)
+    { x = 578.95, y = -10.00, z = 285.33, }, -- 2
+    { x = 581.13, y = -10.00, z = 274.86, }, -- 3
+    { x = 578.93, y = -9.52,  z = 268.83, }, -- 4
+    { x = 580.00, y = -10.00, z = 260.17, }, -- 5
+    { x = 578.93, y = -9.52,  z = 268.83, }, -- 4
+    { x = 581.13, y = -10.00, z = 274.86, }, -- 3
+    { x = 578.95, y = -10.00, z = 285.33, }, -- 2
+    { x = 580.23, y = -9.68,  z = 290.94, }, -- 1 (Origin)
+}
+
 entity.onMobSpawn = function(mob)
     -- Failsafe to make sure NPC is down when NM is up
     if xi.settings.main.OLDSCHOOL_G2 then
         GetNPCByID(ID.npc.BOREAL_COEURL_QM):showNPC(0)
+    end
+end
+
+entity.onMobRoam = function(mob)
+    local PathingIndex = mob:getLocalVar('PathingIndex')
+    local ResumePathingIndex = mob:getLocalVar('ResumePathingIndex')
+    
+    if mob:isFollowingPath() then
+        mob:setLocalVar('ResumePathingIndex', os.time() + 4) -- Make sure mob is waiting 4 seconds to path after stopping
+    elseif (os.time() > ResumePathingIndex) then
+        if (math.random() > .25) then
+            PathingIndex = PathingIndex + math.random(1,3)
+        else
+            PathingIndex = PathingIndex - math.random(1,3)
+        end
+            
+        PathingIndex = utils.clamp(PathingIndex % #pathNodes, 1, #pathNodes) -- Keep PathingIndex between the valid range
+        mob:setLocalVar('PathingIndex', PathingIndex)
+        mob:setLocalVar('ResumePathingIndex', os.time() + 6) -- Make sure mob is waiting at least 6 seconds to path after it's last stop
+        mob:pathTo(pathNodes[PathingIndex].x, pathNodes[PathingIndex].y, pathNodes[PathingIndex].z, xi.path.flag.RUN)
     end
 end
 
