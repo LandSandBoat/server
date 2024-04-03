75d4a6870e6a28b47771b54fa9969a92a9d624c5
MowFord <131182600+MowFord@users.noreply.github.com>
2024-04-03 16:54:55 -0500

    Aura steal should be possible even on mob with steal items

--- scripts/globals/job_utils/thief.lua
+++ scripts/globals/job_utils/thief.lua
@@ -411,7 +411,7 @@ xi.job_utils.thief.useSteal = function(player, target, ability, action)
 
     -- Attempt Aura steal
     -- local effect = xi.effect.NONE
-    if stolen == 0 and player:hasTrait(75) then
+    if player:hasTrait(75) then
         local resist = applyResistanceAbility(player, target, xi.element.NONE, 0, 0)
         -- local effectStealSuccess = false
         if resist > 0.0625 then
