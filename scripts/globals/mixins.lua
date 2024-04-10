83cafcd421e55dae05889c52b4b0c265ad464951
Zach Toogood <zrtoogood@gmail.com>
2024-04-10 12:43:32 +0100

    Lua: Convert Briareus to new mixins

--- scripts/globals/mixins.lua
+++ scripts/globals/mixins.lua
@@ -3,13 +3,27 @@
 --  Contains helper functions for
 --  applying mixins
 -----------------------------------
+xi = xi or {}
 
-function applyMixins(entity, mixins, mixinOptions)
+xi.applyMixins = function(entity, mixins)
+    -- TODO: This could probably be made nicely with recursion
+    if type(mixins) == 'string' then
+        mixins = { mixins }
+    elseif type(mixins) == 'function' then
+        mixins = { mixins }
+    end
+
     for i, v in pairs(mixins) do
         if type(v) == 'table' then
-            applyMixins(entity, v, mixinOptions)
-        else
-            v(entity, mixinOptions)
+            if type(v[1]) == 'string' then
+                xi.mixins[v[1]](entity, v[2])
+            elseif type(v[1]) == 'function' then
+                v[1](entity, v[2])
+            end
+        elseif type(v) == 'string' then
+            xi.mixins[v](entity)
+        elseif type(v) == 'function' then
+            v(entity)
         end
     end
 end
