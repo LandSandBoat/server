19d4dc685b4bfa91cc0247d1818a5581ccc5d22f
Corey Sotiropoulos <coco@solos.dev>
2024-04-12 22:15:19 -0400

    Fix extractFromBlob overwriting initialized values

--- src/common/database.h
+++ src/common/database.h
@@ -270,14 +270,13 @@ namespace db
 
         TracyZoneScoped;
 
-        std::memset(&destination, 0x00, sizeof(T));
-
         // If we use getString on a null blob we will get back garbage data.
         // This will introduce difficult to track down crashes.
         if (!rset->isNull(blobKey.c_str()))
         {
             auto blobStr = rset->getString(blobKey.c_str());
-            std::memcpy(&destination, blobStr.c_str(), sizeof(T));
+            std::memset(&destination, 0x00, sizeof(T));
+            std::memcpy(&destination, blobStr.c_str(), std::min(sizeof(T), blobStr.length()));
         }
     }
 
