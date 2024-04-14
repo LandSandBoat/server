5fa63096930a41e8adc7f85e0a0234eb219a40d1
grahf <tuckerhoog@tutanota.com>
2024-04-14 00:08:35 -0400

    remove deprecated scripts/globals/zone

--- scripts/quests/otherAreas/An_Explorers_Footsteps.lua
+++ scripts/quests/otherAreas/An_Explorers_Footsteps.lua
@@ -5,7 +5,6 @@
 -----------------------------------
 require('scripts/globals/npc_util')
 require('scripts/globals/quests')
-require('scripts/globals/zone')
 require('scripts/globals/interaction/quest')
 -----------------------------------
 
