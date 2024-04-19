c77a2451c55d93badb5bf61d30ce7ef879572129
MowFord <131182600+MowFord@users.noreply.github.com>
2024-04-19 07:17:21 -0500

    Soften logic for creating instances to allow slower load time

--- scripts/globals/instance.lua
+++ scripts/globals/instance.lua
@@ -395,7 +395,18 @@ xi.instance.onEventUpdate = function(player, csid, option, npc)
         player:setLocalVar('INSTANCE_REQUESTED', 1)
     end
 
-    return player:getInstance() ~= nil
+    if
+        player:getInstance() ~= nil or
+        (player:getLocalVar('INSTANCE_REQUESTED') > 0 and
+        player:getLocalVar('INSTANCE_REQUESTED') < 10)
+    then
+        -- return true so we don't immediately call the cancel event update (since instances don't immediately load), but
+        -- increment variable to eventually return false if instance fails to load and trigger onInstanceCreatedCallback
+        player:setLocalVar('INSTANCE_REQUESTED', player:getLocalVar('INSTANCE_REQUESTED') + 1)
+        return true
+    else
+        return false
+    end
 end
 
 -- 'Default' behaviour. It's up to each instance whether or not they want to use this logic
@@ -440,10 +451,10 @@ xi.instance.onInstanceCreatedCallback = function(player, instance)
 
                 v:timer(35000, function(playerArg)
                     -- failsafe to bring all players into instance
-                    -- taking care not to shadow any existing variables
                     -- if a player doesn't receive the event packet, the whole party will be stuck in blackscreen
                     -- timer is destroyed if onEventFinish works properly and player gets zoned
                     -- a properly-functioning event loop will take 20s to zoning into the instance
+                    -- this _should_ be completely unnecessary due to the looping logic with INSTANCE_REQUESTED, but just in case
                     local instanceArg = playerArg:getInstance()
                     if instanceArg then
                         print(fmt('Player {} failed to cleanly transition into instance event, forcing entry via setPos.', playerArg:getName()))
