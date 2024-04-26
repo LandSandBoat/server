754c37266ee9066a5d26aeded9652ebd08657323
Carver <65316697+CatsEyeXI@users.noreply.github.com>
2024-04-26 09:46:11 -0400

    ISNM NPC: Shajaf (Aht Urhgan Whitegate) (#5520)
    * add logic to Shajaf for purchase of ISNM keyitems.
    
    * update for review suggestions

--- scripts/zones/Aht_Urhgan_Whitegate/npcs/Shajaf.lua
+++ scripts/zones/Aht_Urhgan_Whitegate/npcs/Shajaf.lua
@@ -8,13 +8,49 @@ entity.onTrade = function(player, npc, trade)
 end
 
 entity.onTrigger = function(player, npc)
-    player:startEvent(162)
+    local imperialStanding = player:getCurrency('imperial_standing')
+
+    -- Players can only purchase a KI once every real-life day.
+    -- This var is set onEventFinish
+    if player:getCharVar('[ISNM]Accepted') == 1 then
+        -- 'I don't have any offers for you at the moment'
+        player:startEvent(163)
+    elseif
+        player:hasKeyItem(xi.ki.CONFIDENTIAL_IMPERIAL_ORDER) or
+        player:hasKeyItem(xi.ki.SECRET_IMPERIAL_ORDER)
+    then
+        -- 'What are you doing, running off from a job?'
+        player:startEvent(161)
+    elseif player:hasKeyItem(xi.ki.PSC_WILDCAT_BADGE) then
+        player:startEvent(160, imperialStanding, 1)
+    else
+        player:startEvent(162)
+    end
 end
 
 entity.onEventUpdate = function(player, csid, option, npc)
 end
 
 entity.onEventFinish = function(player, csid, option, npc)
+    local imperialStanding = player:getCurrency('imperial_standing')
+
+    if csid == 160 then
+        if
+            option == 1 and
+            imperialStanding >= 2000 and
+            npcUtil.giveKeyItem(player, xi.ki.CONFIDENTIAL_IMPERIAL_ORDER)
+        then
+            player:setCharVar('[ISNM]Accepted', 1, getMidnight())
+            player:delCurrency('imperial_standing', 2000) -- Lv. 60 fight
+        elseif
+            option == 2 and
+            imperialStanding >= 3000 and
+            npcUtil.giveKeyItem(player, xi.ki.SECRET_IMPERIAL_ORDER)
+        then
+            player:setCharVar('[ISNM]Accepted', 1, getMidnight())
+            player:delCurrency('imperial_standing', 3000) -- Lv. Uncapped fight
+        end
+    end
 end
 
 return entity
