841ab9a07534003d26629978c37f8fdc17054856
Xaver-DaRed <xaver.entropia@gmail.com>
2024-04-26 15:16:27 +0200

    Shift-proof Attohwa Chasm

--- scripts/zones/Attohwa_Chasm/mobs/Trench_Antlion.lua
+++ scripts/zones/Attohwa_Chasm/mobs/Trench_Antlion.lua
@@ -8,11 +8,16 @@ local ID = zones[xi.zone.ATTOHWA_CHASM]
 -----------------------------------
 local entity = {}
 
+local ambusherAntlionPHTable =
+{
+    [ID.mob.AMBUSHER_ANTLION - 78] = ID.mob.AMBUSHER_ANTLION, -- -433.309 -4.3 113.841
+}
+
 entity.onMobDeath = function(mob, player, optParams)
 end
 
 entity.onMobDespawn = function(mob)
-    xi.mob.phOnDespawn(mob, ID.mob.AMBUSHER_ANTLION_PH, 10, 3600) -- 1 hour
+    xi.mob.phOnDespawn(mob, ambusherAntlionPHTable, 10, 3600) -- 1 hour
 end
 
 return entity
