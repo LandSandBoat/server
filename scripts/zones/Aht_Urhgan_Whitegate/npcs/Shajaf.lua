a3fd0b2823a1597ebe09804f35510aba2cac5181
Xaver-DaRed <xaver.entropia@gmail.com>
2024-04-27 18:59:55 +0200

    Minor correction to Shajaf event order

--- scripts/zones/Aht_Urhgan_Whitegate/npcs/Shajaf.lua
+++ scripts/zones/Aht_Urhgan_Whitegate/npcs/Shajaf.lua
@@ -10,19 +10,22 @@ end
 entity.onTrigger = function(player, npc)
     local imperialStanding = player:getCurrency('imperial_standing')
 
-    -- Players can only purchase a KI once every real-life day.
-    -- This var is set onEventFinish
-    if player:getCharVar('[ISNM]Accepted') == 1 then
-        -- 'I don't have any offers for you at the moment'
-        player:startEvent(163)
-    elseif
+    -- ISNM accepted but not completed. TODO: Check if KIs expire aswell.
+    if
         player:hasKeyItem(xi.ki.CONFIDENTIAL_IMPERIAL_ORDER) or
         player:hasKeyItem(xi.ki.SECRET_IMPERIAL_ORDER)
     then
-        -- 'What are you doing, running off from a job?'
         player:startEvent(161)
+
+    -- ISNM completed. 1 day lockout hasn't expired.
+    elseif player:getCharVar('[ISNM]Accepted') == 1 then
+        player:startEvent(163)
+
+    -- Can purchuase KI.
     elseif player:hasKeyItem(xi.ki.PSC_WILDCAT_BADGE) then
         player:startEvent(160, imperialStanding, 1)
+
+    -- Default.
     else
         player:startEvent(162)
     end
@@ -35,20 +38,23 @@ entity.onEventFinish = function(player, csid, option, npc)
     local imperialStanding = player:getCurrency('imperial_standing')
 
     if csid == 160 then
+        -- Lv. 60 fight
         if
             option == 1 and
-            imperialStanding >= 2000 and
-            npcUtil.giveKeyItem(player, xi.ki.CONFIDENTIAL_IMPERIAL_ORDER)
+            imperialStanding >= 2000
         then
             player:setCharVar('[ISNM]Accepted', 1, getMidnight())
-            player:delCurrency('imperial_standing', 2000) -- Lv. 60 fight
+            player:delCurrency('imperial_standing', 2000)
+            npcUtil.giveKeyItem(player, xi.ki.CONFIDENTIAL_IMPERIAL_ORDER)
+
+        -- Lv. 75 fight
         elseif
             option == 2 and
-            imperialStanding >= 3000 and
-            npcUtil.giveKeyItem(player, xi.ki.SECRET_IMPERIAL_ORDER)
+            imperialStanding >= 3000
         then
             player:setCharVar('[ISNM]Accepted', 1, getMidnight())
-            player:delCurrency('imperial_standing', 3000) -- Lv. Uncapped fight
+            player:delCurrency('imperial_standing', 3000)
+            npcUtil.giveKeyItem(player, xi.ki.SECRET_IMPERIAL_ORDER)
         end
     end
 end
