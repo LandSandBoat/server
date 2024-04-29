93395a5efde0b5c62ba2cf29067010e3bd3a0a40
Tucker Hoog <77054686+Grahf0085@users.noreply.github.com>
2024-04-28 22:35:09 -0400

    Update An_Explorers_Footsteps.lua to use new quest status enum

--- scripts/quests/otherAreas/An_Explorers_Footsteps.lua
+++ scripts/quests/otherAreas/An_Explorers_Footsteps.lua
@@ -64,7 +64,7 @@ quest.sections =
 {
     {
         check = function(player, status, vars)
-            return status == QUEST_AVAILABLE and player:getFameLevel(xi.quest.fame_area.SELBINA_RABAO) >= 1
+            return status == xi.questStatus.QUEST_AVAILABLE and player:getFameLevel(xi.quest.fame_area.SELBINA_RABAO) >= 1
         end,
 
         [xi.zone.SELBINA] =
@@ -86,7 +86,7 @@ quest.sections =
 
     {
         check = function(player, status, vars)
-            return status == QUEST_ACCEPTED
+            return status == xi.questStatus.QUEST_ACCEPTED
         end,
 
         [xi.zone.WEST_RONFAURE] =
@@ -336,7 +336,7 @@ quest.sections =
 
     {
         check = function(player, status, vars)
-            return status == QUEST_COMPLETED
+            return status == xi.questStatus.QUEST_COMPLETED
         end,
 
         [xi.zone.SELBINA] =
