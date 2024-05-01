a3ab17883dffefef8ca22a7822775ea251f7b2d3
Frankie-hz <105882754+Frankie-hz@users.noreply.github.com>
2024-05-01 00:30:20 -0400

    Shift-proof West Sarutabaruta and tom tit tat PHs
    Shift-proof West Sarutabaruta
    Removed incorrect PHs from Tom Tit Tat PH list
    Fixes Nunyenunc PHs and one mob position from capture

--- scripts/zones/West_Sarutabaruta/mobs/Mandragora.lua
+++ scripts/zones/West_Sarutabaruta/mobs/Mandragora.lua
@@ -7,12 +7,18 @@ local ID = zones[xi.zone.WEST_SARUTABARUTA]
 -----------------------------------
 local entity = {}
 
+local tomTitTatPHTable =
+{
+    [ID.mob.TOM_TIT_TAT - 2] = ID.mob.TOM_TIT_TAT, -- 77.509 -20.719 434.757
+    [ID.mob.TOM_TIT_TAT - 1] = ID.mob.TOM_TIT_TAT, -- 31.149 -20.045 358.265
+}
+
 entity.onMobDeath = function(mob, player, optParams)
     xi.regime.checkRegime(player, mob, 26, 1, xi.regime.type.FIELDS)
 end
 
 entity.onMobDespawn = function(mob)
-    xi.mob.phOnDespawn(mob, ID.mob.TOM_TIT_TAT_PH, 7, math.random(3600, 7200)) -- 1 to 2 hours
+    xi.mob.phOnDespawn(mob, tomTitTatPHTable, 10, math.random(3600, 7200)) -- 1 to 2 hours
 end
 
 return entity
