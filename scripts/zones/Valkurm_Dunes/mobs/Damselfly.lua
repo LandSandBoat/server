d67e895b0095ae7be5f90d7409aa3cb7f21c91b7
Frankie-hz <105882754+Frankie-hz@users.noreply.github.com>
2024-05-01 20:37:52 -0400

    Shift-proof Valkurm Dunes
    Shift-proof Valkurm Dunes

--- scripts/zones/Valkurm_Dunes/mobs/Damselfly.lua
+++ scripts/zones/Valkurm_Dunes/mobs/Damselfly.lua
@@ -7,13 +7,18 @@ local ID = zones[xi.zone.VALKURM_DUNES]
 -----------------------------------
 local entity = {}
 
+local emperorPHList =
+{
+    [ID.mob.VALKURM_EMPEROR - 4] = ID.mob.VALKURM_EMPEROR, -- -228.957 2.776 -101.226
+}
+
 entity.onMobDeath = function(mob, player, optParams)
     xi.regime.checkRegime(player, mob, 9, 1, xi.regime.type.FIELDS)
     xi.regime.checkRegime(player, mob, 10, 2, xi.regime.type.FIELDS)
 end
 
 entity.onMobDespawn = function(mob)
-    xi.mob.phOnDespawn(mob, ID.mob.VALKURM_EMPEROR_PH, 10, 3600) -- 1 hour
+    xi.mob.phOnDespawn(mob, emperorPHList, 10, 3600) -- 1 hour
 end
 
 return entity
