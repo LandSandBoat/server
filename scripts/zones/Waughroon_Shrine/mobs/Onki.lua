c95df4feb6145c6507a1d2e7446192b9bc8210a1
MowFord <131182600+MowFord@users.noreply.github.com>
2024-05-03 09:08:34 -0500

    fix A_THIEF_IN_NORG entry req and completion

--- scripts/zones/Waughroon_Shrine/mobs/Onki.lua
+++ scripts/zones/Waughroon_Shrine/mobs/Onki.lua
@@ -3,6 +3,8 @@
 --  Mob: Onki
 -- a Thief in Norg BCNM Fight
 -----------------------------------
+mixins = { require('scripts/mixins/job_special') }
+-----------------------------------
 local entity = {}
 
 entity.onMobDeath = function(mob, player, optParams)
