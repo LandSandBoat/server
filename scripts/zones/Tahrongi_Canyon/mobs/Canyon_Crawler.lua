57eb2eae4b5760f02b6b4b40acd7dc22c8dff55d
Frankie-hz <105882754+Frankie-hz@users.noreply.github.com>
2024-05-03 22:23:01 -0400

    Shift-proof Tahrongi Canyon
    Shift-proof Tahrongi Canyon

--- scripts/zones/Tahrongi_Canyon/mobs/Canyon_Crawler.lua
+++ scripts/zones/Tahrongi_Canyon/mobs/Canyon_Crawler.lua
@@ -8,13 +8,18 @@ require('scripts/quests/tutorial')
 -----------------------------------
 local entity = {}
 
+local herbagePHTable =
+{
+    [ID.mob.HERBAGE_HUNTER - 1] = ID.mob.HERBAGE_HUNTER, -- -119.301, 24.087, 448.636
+}
+
 entity.onMobDeath = function(mob, player, optParams)
     xi.regime.checkRegime(player, mob, 96, 1, xi.regime.type.FIELDS)
     xi.tutorial.onMobDeath(player)
 end
 
 entity.onMobDespawn = function(mob)
-    xi.mob.phOnDespawn(mob, ID.mob.HERBAGE_HUNTER_PH, 10, 3600) -- 1 hour minimum
+    xi.mob.phOnDespawn(mob, herbagePHTable, 10, 3600) -- 1 hour minimum
 end
 
 return entity
--- scripts/zones/Tahrongi_Canyon/mobs/Wild_Dhalmel.lua
+++ scripts/zones/Tahrongi_Canyon/mobs/Wild_Dhalmel.lua
@@ -8,13 +8,19 @@ require('scripts/quests/tutorial')
 -----------------------------------
 local entity = {}
 
+local serpopardPHTable =
+{
+    [ID.mob.SERPOPARD_ISHTAR[1] - 3] = ID.mob.SERPOPARD_ISHTAR[1], -- -9.176 -8.191 -64.347 (south)
+    [ID.mob.SERPOPARD_ISHTAR[2] - 4] = ID.mob.SERPOPARD_ISHTAR[2], -- 22.360 23.757 281.584 (north)
+}
+
 entity.onMobDeath = function(mob, player, optParams)
     xi.regime.checkRegime(player, mob, 96, 2, xi.regime.type.FIELDS)
     xi.tutorial.onMobDeath(player)
 end
 
 entity.onMobDespawn = function(mob)
-    xi.mob.phOnDespawn(mob, ID.mob.SERPOPARD_ISHTAR_PH, 10, 3600) -- 1 hour
+    xi.mob.phOnDespawn(mob, serpopardPHTable, 10, 3600) -- 1 hour
 end
 
 return entity
