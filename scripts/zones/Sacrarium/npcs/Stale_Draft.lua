dac7961fdefb0078fdbc18cf425aa99f45a7dfcc
Frankie-hz <105882754+Frankie-hz@users.noreply.github.com>
2024-05-04 01:40:12 -0400

    Shift-proof Sacrarium

--- scripts/zones/Sacrarium/npcs/Stale_Draft.lua
+++ scripts/zones/Sacrarium/npcs/Stale_Draft.lua
@@ -7,9 +7,17 @@ local ID = zones[xi.zone.SACRARIUM]
 -----------------------------------
 local entity = {}
 
+local swiftBeltNMs =
+{
+    { ID.mob.SWIFT_BELT_NM_OFFSET,     { [xi.race.HUME_M]   = true, [xi.race.HUME_F]   = true } }, -- Balor (hume)
+    { ID.mob.SWIFT_BELT_NM_OFFSET + 1, { [xi.race.ELVAAN_M] = true, [xi.race.ELVAAN_F] = true } }, -- Luaith (elvaan)
+    { ID.mob.SWIFT_BELT_NM_OFFSET + 2, { [xi.race.TARU_M]   = true, [xi.race.TARU_F]   = true } }, -- Lobais (tarutaru)
+    { ID.mob.SWIFT_BELT_NM_OFFSET + 5, { [xi.race.MITHRA]   = true } },                            -- Caithleann (mithra)
+    { ID.mob.SWIFT_BELT_NM_OFFSET + 6, { [xi.race.GALKA]    = true } },                            -- Indich (galka)
+}
+
 entity.onTrigger = function(player, npc)
     local hate = player:getCharVar('FOMOR_HATE')
-
     if hate < 8 then --hate lvl 1
         player:messageSpecial(ID.text.NOTHING_OUT_OF_ORDINARY)
     elseif hate < 12 then
@@ -25,8 +33,8 @@ entity.onTrade = function(player, npc, trade)
     -- fomor codex
     if trade:hasItemQty(xi.item.FOMOR_CODEX, 1) and trade:getItemCount() == 1 then
         local draftOffset = npc:getID() - ID.npc.STALE_DRAFT_OFFSET
-        local nmId = ID.mob.SWIFT_BELT_NMS[draftOffset + 1][1]
-        local races = ID.mob.SWIFT_BELT_NMS[draftOffset + 1][2]
+        local nmId = swiftBeltNMs[draftOffset + 1][1]
+        local races = swiftBeltNMs[draftOffset + 1][2]
         local nm = GetMobByID(nmId)
         local race = player:getRace()
         local hate = player:getCharVar('FOMOR_HATE')
