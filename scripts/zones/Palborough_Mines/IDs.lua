b2fca8c20b2239ceb3e805b7352a8e6e1d706f02
Frankie-hz <105882754+Frankie-hz@users.noreply.github.com>
2024-05-07 00:35:37 -0400

    Shift-proof Palborough Mines

--- scripts/zones/Palborough_Mines/IDs.lua
+++ scripts/zones/Palborough_Mines/IDs.lua
@@ -30,23 +30,10 @@ zones[xi.zone.PALBOROUGH_MINES] =
     },
     mob =
     {
-        BU_GHI_HOWLBLADE_PH =
-        {
-            [17363177] = 17363181, -- 170.000 -15.000 179.000
-            [17363178] = 17363181, -- 170.000 -16.000 165.000
-            [17363179] = 17363181, -- 166.000 -16.000 135.000
-            [17363180] = 17363181, -- 167.207 -18.027 159.374
-        },
-        ZI_GHI_BONEEATER_PH =
-        {
-            [17363205] = 17363208, -- 130.386 -32.313 73.967
-        },
-        BEHYA_HUNDREDWALL_PH =
-        {
-            [17363256] = 17363258,
-            [17363257] = 17363258,
-        },
-        NI_GHU_NESTFENDER   = 17363318,
+        BU_GHI_HOWLBLADE  = GetFirstID('BuGhi_Howlblade'),
+        ZI_GHI_BONEEATER  = GetFirstID('ZiGhi_Boneeater'),
+        BEHYA_HUNDREDWALL = GetFirstID('BeHya_Hundredwall'),
+        NI_GHU_NESTFENDER = GetFirstID('NiGhu_Nestfender'),
     },
     npc =
     {
