ad4b90f62c70a48daabfdbdc686e3a30790df63e
Frankie-hz <105882754+Frankie-hz@users.noreply.github.com>
2024-05-06 22:29:34 -0400

    Shift-proof Rolanberry Fields and [S]

--- scripts/zones/Rolanberry_Fields/mobs/Ochu.lua
+++ scripts/zones/Rolanberry_Fields/mobs/Ochu.lua
@@ -7,12 +7,17 @@ local ID = zones[xi.zone.ROLANBERRY_FIELDS]
 -----------------------------------
 local entity = {}
 
+local droolingPHTable =
+{
+    [ID.mob.DROOLING_DAISY - 1] = ID.mob.DROOLING_DAISY, -- -691.786 -34.802 -335.763
+}
+
 entity.onMobDeath = function(mob, player, optParams)
     xi.regime.checkRegime(player, mob, 88, 1, xi.regime.type.FIELDS)
 end
 
 entity.onMobDespawn = function(mob)
-    xi.mob.phOnDespawn(mob, ID.mob.DROOLING_DAISY_PH, 10, 3600) -- 1 hour
+    xi.mob.phOnDespawn(mob, droolingPHTable, 10, 3600) -- 1 hour
 end
 
 return entity
