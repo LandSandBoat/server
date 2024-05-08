9392a041e423fdbc838ac808b31328c9610b67a1
Frankie-hz <105882754+Frankie-hz@users.noreply.github.com>
2024-05-08 00:10:36 -0400

    Shift-proof Oldton Movalpolos

--- scripts/zones/Oldton_Movalpolos/IDs.lua
+++ scripts/zones/Oldton_Movalpolos/IDs.lua
@@ -25,16 +25,12 @@ zones[xi.zone.OLDTON_MOVALPOLOS] =
     },
     mob =
     {
-        BUGBEAR_STRONGMAN_PH  =
-        {
-            [16822421] = 16822423, -- -81.31 31.493 210.675 (west)
-            [16822426] = 16822427, -- 58.013, 15.5, -121.928 (east)
-        },
-        GOBLIN_WOLFMAN        = 16822459,
+        BUGBEAR_STRONGMAN = GetTableOfIDs('Bugbear_Strongman'),
+        GOBLIN_WOLFMAN    = GetFirstID('Goblin_Wolfman'),
     },
     npc =
     {
-        SCRAWLED_WRITING = 16822469,
+        SCRAWLED_WRITING = GetFirstID('Scrawled_Writing'),
         OVERSEER_BASE    = GetFirstID('Conquest_Banner'),
         TREASURE_CHEST   = GetFirstID('Treasure_Chest'),
         MINING           = GetTableOfIDs('Mining_Point'),
--- scripts/zones/Oldton_Movalpolos/mobs/Bugbear_Bondman.lua
+++ scripts/zones/Oldton_Movalpolos/mobs/Bugbear_Bondman.lua
@@ -7,11 +7,17 @@ local ID = zones[xi.zone.OLDTON_MOVALPOLOS]
 -----------------------------------
 local entity = {}
 
+local bugbearPHTable =
+{
+    [ID.mob.BUGBEAR_STRONGMAN[1] - 2] = ID.mob.BUGBEAR_STRONGMAN[1], -- -81.31 31.493 210.675 (west)
+    [ID.mob.BUGBEAR_STRONGMAN[2] - 1] = ID.mob.BUGBEAR_STRONGMAN[2], -- 58.013, 15.5, -121.928 (east)
+}
+
 entity.onMobDeath = function(mob, player, optParams)
 end
 
 entity.onMobDespawn = function(mob)
-    xi.mob.phOnDespawn(mob, ID.mob.BUGBEAR_STRONGMAN_PH, 10, 1) -- no cooldown
+    xi.mob.phOnDespawn(mob, bugbearPHTable, 10, 1) -- no cooldown
 end
 
 return entity
