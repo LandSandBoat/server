f3893de1b2b1fc81342552d46529f720558bba74
Xaver-DaRed <xaver.entropia@gmail.com>
2024-05-09 22:30:17 +0200

    Promyvion-Dem: Move trigger area logic.

--- scripts/zones/Promyvion-Dem/IDs.lua
+++ scripts/zones/Promyvion-Dem/IDs.lua
@@ -42,29 +42,7 @@ zones[xi.zone.PROMYVION_DEM] =
     },
     npc =
     {
-        MEMORY_STREAMS =
-        {
-            [11]       = {  160, 3,  -80, 0, 0, 0, { 46 } }, -- Floor 1 return
-            [21]       = { -280, 3,    0, 0, 0, 0, { 41 } }, -- Floor 2 return
-            [31]       = { -160, 3,  440, 0, 0, 0, { 43 } }, -- Floor 3 (North) return
-            [32]       = {    0, 3, -320, 0, 0, 0, { 42 } }, -- Floor 3 (South) return
-            [41]       = {  360, 3,  240, 0, 0, 0, { 44 } }, -- Floor 4 return
-            -- TODO: Cleanup prmyvions. It knows where you came from and will only return the apropiate event acordingly.
-            -- Event 44 -> Return to floor 3 South
-            -- Event 45 -> Return to floor 3 North
-
-            [16851278] = {  120, 3, -280, 0, 0, 0, { 30 } }, -- Floor 1 MR
-            [16851282] = {  -80, 3,  -80, 0, 0, 0, { 36 } }, -- Floor 2 MR SE - Destination: North
-            [16851283] = {  -80, 3,   80, 0, 0, 0, { 37 } }, -- Floor 2 MR NE - Destination: South
-            [16851284] = { -280, 3, -200, 0, 0, 0, { 34 } }, -- Floor 2 MR SW - Destination: South
-            [16851285] = { -360, 3,   40, 0, 0, 0, { 35 } }, -- floor 2 MR NW - Destination: North
-            [16851279] = {   40, 3, -200, 0, 0, 0, { 31 } }, -- Floor 3 (South) MR NE
-            [16851280] = { -120, 3, -240, 0, 0, 0, { 32 } }, -- Floor 3 (South) MR NW
-            [16851281] = { -120, 3, -400, 0, 0, 0, { 33 } }, -- Floor 3 (South) MR SW
-            [16851286] = { -320, 3,  160, 0, 0, 0, { 38 } }, -- Floor 3 (North) MR SW
-            [16851287] = {  -40, 3,  320, 0, 0, 0, { 39 } }, -- Floor 3 (North) MR NE
-            [16851288] = { -120, 3,  160, 0, 0, 0, { 40 } }, -- Floor 3 (North) MR SE
-        },
+        MEMORY_STREAM_OFFSET = GetFirstID('_0i1'),
     },
 }
 
