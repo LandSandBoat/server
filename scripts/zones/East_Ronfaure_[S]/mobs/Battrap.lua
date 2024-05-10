8b9645971dff6aa3b149ea0ef7d1439caaeba7cb
Frankie-hz <105882754+Frankie-hz@users.noreply.github.com>
2024-05-10 01:24:00 -0400

    Shift-proof East Ronfaure &  [S]

--- scripts/zones/East_Ronfaure_[S]/mobs/Battrap.lua
+++ scripts/zones/East_Ronfaure_[S]/mobs/Battrap.lua
@@ -7,11 +7,16 @@ local ID = zones[xi.zone.EAST_RONFAURE_S]
 -----------------------------------
 local entity = {}
 
+local goblintrapPHTable =
+{
+    [ID.mob.GOBLINTRAP - 1] = ID.mob.GOBLINTRAP, -- 156 0 -438
+}
+
 entity.onMobDeath = function(mob, player, optParams)
 end
 
 entity.onMobDespawn = function(mob)
-    xi.mob.phOnDespawn(mob, ID.mob.GOBLINTRAP_PH, 5, 3600) -- 1 hour
+    xi.mob.phOnDespawn(mob, goblintrapPHTable, 5, 3600) -- 1 hour
 end
 
 return entity
