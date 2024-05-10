6358bf29f6be517852b02f23c98dba3bfbff9046
Frankie-hz <105882754+Frankie-hz@users.noreply.github.com>
2024-05-10 01:18:58 -0400

    Shift-proof Gustav Tunnel

--- scripts/zones/Gustav_Tunnel/mobs/Antares.lua
+++ scripts/zones/Gustav_Tunnel/mobs/Antares.lua
@@ -7,12 +7,19 @@ local ID = zones[xi.zone.GUSTAV_TUNNEL]
 -----------------------------------
 local entity = {}
 
+local amikiriPHTable =
+{
+    [ID.mob.AMIKIRI - 11] = ID.mob.AMIKIRI, -- -245.000 -0.045 146.000
+    [ID.mob.AMIKIRI - 6]  = ID.mob.AMIKIRI, -- -228.872 -0.264 144.689
+    [ID.mob.AMIKIRI - 2]  = ID.mob.AMIKIRI, -- -209.552 -0.257 161.728
+}
+
 entity.onMobDeath = function(mob, player, optParams)
     xi.regime.checkRegime(player, mob, 768, 2, xi.regime.type.GROUNDS)
 end
 
 entity.onMobDespawn = function(mob)
-    xi.mob.phOnDespawn(mob, ID.mob.AMIKIRI_PH, 5, math.random(25200, 32400)) -- 7 to 9 hours
+    xi.mob.phOnDespawn(mob, amikiriPHTable, 5, math.random(25200, 32400)) -- 7 to 9 hours
 end
 
 return entity
