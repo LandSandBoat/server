2e8818b5be0c6d23389ec29e61254972fc85bc68
Frankie-hz <105882754+Frankie-hz@users.noreply.github.com>
2024-05-10 01:22:58 -0400

    Shift-proof Fort Karugo Narugo [s]

--- scripts/zones/Fort_Karugo-Narugo_[S]/mobs/Vorpal_Bunny.lua
+++ scripts/zones/Fort_Karugo-Narugo_[S]/mobs/Vorpal_Bunny.lua
@@ -7,11 +7,16 @@ local ID = zones[xi.zone.FORT_KARUGO_NARUGO_S]
 -----------------------------------
 local entity = {}
 
+local ratatoskrPHTable =
+{
+    [ID.mob.RATATOSKR - 3] = ID.mob.RATATOSKR,
+}
+
 entity.onMobDeath = function(mob, player, optParams)
 end
 
 entity.onMobDespawn = function(mob)
-    xi.mob.phOnDespawn(mob, ID.mob.RATATOSKR_PH, 10, 5400) -- 90 minutes
+    xi.mob.phOnDespawn(mob, ratatoskrPHTable, 10, 5400) -- 90 minutes
 end
 
 return entity
