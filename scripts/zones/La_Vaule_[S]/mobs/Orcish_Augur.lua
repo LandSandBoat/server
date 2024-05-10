c77b19881708cfba568b443b3b562201b14ff268
Frankie-hz <105882754+Frankie-hz@users.noreply.github.com>
2024-05-09 22:37:24 -0400

    Shift-proof La Vaule [S]

--- scripts/zones/La_Vaule_[S]/mobs/Orcish_Augur.lua
+++ scripts/zones/La_Vaule_[S]/mobs/Orcish_Augur.lua
@@ -7,11 +7,16 @@ local ID = zones[xi.zone.LA_VAULE_S]
 -----------------------------------
 local entity = {}
 
+local ashmakerPHTable =
+{
+    [ID.mob.ASHMAKER_GOTBLUT - 2] = ID.mob.ASHMAKER_GOTBLUT, -- 234.481 3.424 -241.751
+}
+
 entity.onMobDeath = function(mob, player, optParams)
 end
 
 entity.onMobDespawn = function(mob)
-    xi.mob.phOnDespawn(mob, ID.mob.ASHMAKER_GOTBLUT_PH, 10, 3600) -- 1 hour
+    xi.mob.phOnDespawn(mob, ashmakerPHTable, 10, 3600) -- 1 hour
 end
 
 return entity
