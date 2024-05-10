949acb11fd6c7acfd00902ac3c600cd867a24558
Frankie-hz <105882754+Frankie-hz@users.noreply.github.com>
2024-05-09 22:48:50 -0400

    Shift-proof Kuftal Tunnel

--- scripts/zones/Kuftal_Tunnel/mobs/Greater_Cockatrice.lua
+++ scripts/zones/Kuftal_Tunnel/mobs/Greater_Cockatrice.lua
@@ -7,12 +7,19 @@ local ID = zones[xi.zone.KUFTAL_TUNNEL]
 -----------------------------------
 local entity = {}
 
+local pelicanPHTable =
+{
+    [ID.mob.PELICAN - 7] = ID.mob.PELICAN, -- 180.000 21.000 -39.000
+    [ID.mob.PELICAN - 4] = ID.mob.PELICAN, -- 178.857 20.256 -44.151
+    [ID.mob.PELICAN - 3] = ID.mob.PELICAN, -- 179.394 20.061 -34.062
+}
+
 entity.onMobDeath = function(mob, player, optParams)
     xi.regime.checkRegime(player, mob, 741, 2, xi.regime.type.GROUNDS)
 end
 
 entity.onMobDespawn = function(mob)
-    xi.mob.phOnDespawn(mob, ID.mob.PELICAN_PH, 5, math.random(10800, 43200)) -- 4 to 12 hours
+    xi.mob.phOnDespawn(mob, pelicanPHTable, 5, math.random(10800, 43200)) -- 4 to 12 hours
 end
 
 return entity
--- scripts/zones/Kuftal_Tunnel/mobs/Sand_Lizard.lua
+++ scripts/zones/Kuftal_Tunnel/mobs/Sand_Lizard.lua
@@ -7,12 +7,29 @@ local ID = zones[xi.zone.KUFTAL_TUNNEL]
 -----------------------------------
 local entity = {}
 
+local amemetPHTable =
+{
+    [ID.mob.AMEMET - 84] = ID.mob.AMEMET, -- 74.662 -0.513 3.685
+    [ID.mob.AMEMET - 83] = ID.mob.AMEMET, -- 109.000 -0.010 -48.000
+    [ID.mob.AMEMET - 82] = ID.mob.AMEMET, -- 92.000 -0.396 14.000
+    [ID.mob.AMEMET - 22] = ID.mob.AMEMET, -- 123.046 0.250 18.642
+    [ID.mob.AMEMET - 16] = ID.mob.AMEMET, -- 112.135 -0.278 38.281
+    [ID.mob.AMEMET - 15] = ID.mob.AMEMET, -- 112.008 -0.530 50.994
+    [ID.mob.AMEMET - 14] = ID.mob.AMEMET, -- 67.998 -0.500 12.000
+    [ID.mob.AMEMET - 13] = ID.mob.AMEMET, -- 89.590 -0.321 -9.390
+    [ID.mob.AMEMET - 12] = ID.mob.AMEMET, -- 123.186 0.213 -24.716
+    [ID.mob.AMEMET - 11] = ID.mob.AMEMET, -- 96.365 -0.269 -7.619
+    [ID.mob.AMEMET - 8]  = ID.mob.AMEMET, -- 122.654 -0.491 0.840
+    [ID.mob.AMEMET - 7]  = ID.mob.AMEMET, -- 68.454 -0.417 -0.413
+    [ID.mob.AMEMET - 6]  = ID.mob.AMEMET, -- 118.633 -0.470 -43.282
+}
+
 entity.onMobDeath = function(mob, player, optParams)
     xi.regime.checkRegime(player, mob, 735, 2, xi.regime.type.GROUNDS)
 end
 
 entity.onMobDespawn = function(mob)
-    xi.mob.phOnDespawn(mob, ID.mob.AMEMET_PH, 5, math.random(7200, 43200)) -- 2 to 12 hours
+    xi.mob.phOnDespawn(mob, amemetPHTable, 5, math.random(7200, 43200)) -- 2 to 12 hours
 end
 
 return entity
