b7ee2e11e5257335bb5fa76933412f5d5c0e9a2c
Frankie-hz <105882754+Frankie-hz@users.noreply.github.com>
2024-05-09 22:33:46 -0400

    Shift-proof Lower Delkfutts

--- scripts/zones/Lower_Delkfutts_Tower/mobs/Giant_Gatekeeper.lua
+++ scripts/zones/Lower_Delkfutts_Tower/mobs/Giant_Gatekeeper.lua
@@ -7,13 +7,24 @@ local ID = zones[xi.zone.LOWER_DELKFUTTS_TOWER]
 -----------------------------------
 local entity = {}
 
+local hippolytosPHTable =
+{
+    [ID.mob.HIPPOLYTOS + 1] = ID.mob.HIPPOLYTOS, -- 337.079 -16.1 17.386
+}
+
+local epialtesPHTable =
+{
+    [ID.mob.EPIALTES + 1] = ID.mob.EPIALTES, -- 432.952 -0.350 -3.719
+    [ID.mob.EPIALTES + 6] = ID.mob.EPIALTES, -- 484.735 0.046 23.048
+}
+
 entity.onMobDeath = function(mob, player, optParams)
     xi.regime.checkRegime(player, mob, 778, 2, xi.regime.type.GROUNDS)
 end
 
 entity.onMobDespawn = function(mob)
-    xi.mob.phOnDespawn(mob, ID.mob.EPIALTES_PH, 5, 1) -- no cooldown
-    xi.mob.phOnDespawn(mob, ID.mob.HIPPOLYTOS_PH, 5, 1) -- no cooldown
+    xi.mob.phOnDespawn(mob, epialtesPHTable, 5, 1) -- no cooldown
+    xi.mob.phOnDespawn(mob, hippolytosPHTable, 5, 1) -- no cooldown
 end
 
 return entity
