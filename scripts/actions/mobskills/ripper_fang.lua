c30d76e364ad026e71a9327003425e66ee5c5485
Ampitere <ampitere@live.com>
2024-05-11 14:37:31 -0700

    fix: Ripper Fang doesn't match wiki

--- scripts/actions/mobskills/ripper_fang.lua
+++ scripts/actions/mobskills/ripper_fang.lua
@@ -1,7 +1,7 @@
 -----------------------------------
---  Sprout Smack
---  Description: Additional effect: Slow.  Duration of effect varies with TP.
---  Type: Physical (Blunt)
+--  Ripper Fang
+--  Description: Deals physical damage. Damage varies with TP.
+--  Type: Physical (Slashing)
 -----------------------------------
 local mobskillObject = {}
 
@@ -12,12 +12,11 @@ end
 mobskillObject.onMobWeaponSkill = function(target, mob, skill)
     local numhits = 1
     local accmod = 1
-    local dmgmod = 2.5
-    local info = xi.mobskills.mobPhysicalMove(mob, target, skill, numhits, accmod, dmgmod, xi.mobskills.magicalTpBonus.NO_EFFECT)
-    local dmg = xi.mobskills.mobFinalAdjustments(info.dmg, mob, skill, target, xi.attackType.PHYSICAL, xi.damageType.PIERCING, info.hitslanded)
+    local dmgmod = 1.5
+    local info = xi.mobskills.mobPhysicalMove(mob, target, skill, numhits, accmod, dmgmod, xi.mobskills.physicalTpBonus.ATK_VARIES, 1.3, 1.3, 1.3)
+    local dmg = xi.mobskills.mobFinalAdjustments(info.dmg, mob, skill, target, xi.attackType.PHYSICAL, xi.damageType.SLASHING, info.hitslanded)
 
-    xi.mobskills.mobPhysicalStatusEffectMove(mob, target, skill, xi.effect.SLOW, 1250, 0, 120)
-    target:takeDamage(dmg, mob, xi.attackType.PHYSICAL, xi.damageType.PIERCING)
+    target:takeDamage(dmg, mob, xi.attackType.PHYSICAL, xi.damageType.SLASHING)
 
     return dmg
 end
