d467b1e43a298fb0921d66157a009d61004bec3a
MowFord <131182600+MowFord@users.noreply.github.com>
2024-05-13 11:04:06 -0500

    Immanence logic cleanup

--- scripts/actions/abilities/immanence.lua
+++ scripts/actions/abilities/immanence.lua
@@ -7,11 +7,17 @@
 local abilityObject = {}
 
 abilityObject.onAbilityCheck = function(player, target, ability)
+    if player:hasStatusEffect(xi.effect.IMMANENCE) then
+        return xi.msg.basic.EFFECT_ALREADY_ACTIVE, 0
+    end
+
     return 0, 0
 end
 
 abilityObject.onUseAbility = function(player, target, ability)
     player:addStatusEffect(xi.effect.IMMANENCE, 1, 0, 60)
+
+    return xi.effect.IMMANENCE
 end
 
 return abilityObject
