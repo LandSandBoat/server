8805aaad07dcdcd038b38ed0f5daf688a581cd1c
Frankie-hz <105882754+Frankie-hz@users.noreply.github.com>
2024-05-13 19:27:20 -0400

    Shift-proof Caedarva Mire

--- scripts/zones/Caedarva_Mire/IDs.lua
+++ scripts/zones/Caedarva_Mire/IDs.lua
@@ -45,37 +45,29 @@ zones[xi.zone.CAEDARVA_MIRE] =
     },
     mob =
     {
+        AYNU_KAYSEY           = GetFirstID('Aynu-kaysey'),
+        CAEDARVA_TOAD         = GetFirstID('Caedarva_Toad'),
         CHIGOES =
         {
             ['Wild_Karakul'] = GetTableOfIDs('Chigoe', 5),
             ['Mosshorn']     = GetTableOfIDs('Chigoe', 5, 5),
         },
-        PEALLAIDH_PH =
-        {
-            [17100870] = 17101143, -- 333.885 -9.646 -447.557
-            [17100871] = 17101143, -- 309.638 -8.548 -447.557
-            [17100872] = 17101143, -- 307.320 -10.088 -451.786
-            [17100873] = 17101143, -- 295.122 -12.271 -414.418
-            [17100874] = 17101143, -- 287.607 -16.220 -387.671
-            [17100875] = 17101143, -- 315.793 -16.336 -402.407
-            [17100876] = 17101143, -- 321.809 -16.843 -373.780
-        },
-        AYNU_KAYSEY           = 17101099,
-        CAEDARVA_TOAD         = 17101145,
-        JAZARAAT              = 17101146,
-        LAMIA_NO27            = 17101148,
-        MOSHDAHN              = 17101149,
-        KHIMAIRA              = 17101197,
-        VERDELET              = 17101198,
-        TYGER                 = 17101199,
-        MAHJLAEF_THE_PAINTORN = 17101200,
-        EXPERIMENTAL_LAMIA    = 17101201,
+        EXPERIMENTAL_LAMIA    = GetFirstID('Experimental_Lamia'),
+        JAZARAAT              = GetFirstID('Jazaraat'),
+        KHIMAIRA              = GetFirstID('Khimaira'),
+        LAMIA_NO27            = GetFirstID('Lamia_No27'),
+        MAHJLAEF_THE_PAINTORN = GetFirstID('Mahjlaef_the_Paintorn'),
+        MOSHDAHN              = GetFirstID('Moshdahn'),
+        PEALLAIDH             = GetFirstID('Peallaidh'),
+        PEALLAIDH_PH_OFFSET   = GetFirstID('Wild_Karakul'), -- These are 270IDs away. Use offset in case of weird shift.
+        TYGER                 = GetFirstID('Tyger'),
+        VERDELET              = GetFirstID('Verdelet'),
     },
     npc =
     {
         LOGGING             = GetTableOfIDs('Logging_Point'),
-        RUNIC_PORTAL_AZOUPH = 17101319,
-        RUNIC_PORTAL_DVUCCA = 17101322,
+        RUNIC_PORTAL_AZOUPH = GetFirstID('Runic_Portal_Azouph'),
+        RUNIC_PORTAL_DVUCCA = GetFirstID('Runic_Portal_Dvucca'),
     },
 }
 
