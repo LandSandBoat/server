d4248bababb37aa95ff8aa66a84554f9f2453afe
Frankie-hz <105882754+Frankie-hz@users.noreply.github.com>
2024-05-13 19:26:18 -0400

    Shift-proof Castle Oztroja

--- scripts/zones/Castle_Oztroja/mobs/Yagudo_Herald.lua
+++ scripts/zones/Castle_Oztroja/mobs/Yagudo_Herald.lua
@@ -7,11 +7,19 @@ local ID = zones[xi.zone.CASTLE_OZTROJA]
 -----------------------------------
 local entity = {}
 
+local quuDomiPHTable =
+{
+    [ID.mob.QUU_DOMI_THE_GALLANT - 41] = ID.mob.QUU_DOMI_THE_GALLANT, -- 33.832 -0.068 -176.627
+    [ID.mob.QUU_DOMI_THE_GALLANT - 33] = ID.mob.QUU_DOMI_THE_GALLANT, -- 18.545 -0.056 -120.283
+    [ID.mob.QUU_DOMI_THE_GALLANT - 26] = ID.mob.QUU_DOMI_THE_GALLANT, -- 103.948 -1.250 -189.869
+    [ID.mob.QUU_DOMI_THE_GALLANT - 3]  = ID.mob.QUU_DOMI_THE_GALLANT, -- 59.000 -4.000 -131.000
+}
+
 entity.onMobDeath = function(mob, player, optParams)
 end
 
 entity.onMobDespawn = function(mob)
-    xi.mob.phOnDespawn(mob, ID.mob.QUU_DOMI_THE_GALLANT_PH, 5, 3600) -- 1 hour
+    xi.mob.phOnDespawn(mob, quuDomiPHTable, 5, 3600) -- 1 hour
 end
 
 return entity
