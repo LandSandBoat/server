f5e0e7f452cc97a51a8eee1fc289303dd48437e9
Frankie-hz <105882754+Frankie-hz@users.noreply.github.com>
2024-05-13 19:24:03 -0400

    Shift-proof Castle Zvahl Keep

--- scripts/zones/Castle_Zvahl_Keep/mobs/Demon_Wizard.lua
+++ scripts/zones/Castle_Zvahl_Keep/mobs/Demon_Wizard.lua
@@ -7,11 +7,17 @@ local ID = zones[xi.zone.CASTLE_ZVAHL_KEEP]
 -----------------------------------
 local entity = {}
 
+local baronPHTable =
+{
+    [ID.mob.BARON_VAPULA - 3] = ID.mob.BARON_VAPULA, -- -254.000 -52.125 86.000
+    [ID.mob.BARON_VAPULA - 1] = ID.mob.BARON_VAPULA, -- -227.007 -52.125 83.768
+}
+
 entity.onMobDeath = function(mob, player, optParams)
 end
 
 entity.onMobDespawn = function(mob)
-    xi.mob.phOnDespawn(mob, ID.mob.BARON_VAPULA_PH, 10, math.random(3600, 28800)) -- 1 to 8 hours
+    xi.mob.phOnDespawn(mob, baronPHTable, 10, math.random(3600, 28800)) -- 1 to 8 hours
 end
 
 return entity
