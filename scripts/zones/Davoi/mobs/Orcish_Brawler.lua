6c22daec0954170c923c917113a48b8ac02c001a
Frankie-hz <105882754+Frankie-hz@users.noreply.github.com>
2024-05-13 19:21:18 -0400

    Shift-proof Davoi

--- scripts/zones/Davoi/mobs/Orcish_Brawler.lua
+++ scripts/zones/Davoi/mobs/Orcish_Brawler.lua
@@ -7,11 +7,16 @@ local ID = zones[xi.zone.DAVOI]
 -----------------------------------
 local entity = {}
 
+local poisonhandPHTable =
+{
+    [ID.mob.POISONHAND_GNADGAD - 7] = ID.mob.POISONHAND_GNADGAD, -- -59.013 -0.590 14.783
+}
+
 entity.onMobDeath = function(mob, player, optParams)
 end
 
 entity.onMobDespawn = function(mob)
-    xi.mob.phOnDespawn(mob, ID.mob.POISONHAND_GNADGAD_PH, 10, 3600) -- 1 hour
+    xi.mob.phOnDespawn(mob, poisonhandPHTable, 10, 3600) -- 1 hour
 end
 
 return entity
--- scripts/zones/Davoi/mobs/Orcish_Firebelcher.lua
+++ scripts/zones/Davoi/mobs/Orcish_Firebelcher.lua
@@ -7,11 +7,16 @@ local ID = zones[xi.zone.DAVOI]
 -----------------------------------
 local entity = {}
 
+local poisonhandPHTable =
+{
+    [ID.mob.POISONHAND_GNADGAD - 3] = ID.mob.POISONHAND_GNADGAD, -- -60.057 -0.655 29.127
+}
+
 entity.onMobDeath = function(mob, player, optParams)
 end
 
 entity.onMobDespawn = function(mob)
-    xi.mob.phOnDespawn(mob, ID.mob.POISONHAND_GNADGAD_PH, 10, 3600) -- 1 hour
+    xi.mob.phOnDespawn(mob, poisonhandPHTable, 10, 3600) -- 1 hour
 end
 
 return entity
