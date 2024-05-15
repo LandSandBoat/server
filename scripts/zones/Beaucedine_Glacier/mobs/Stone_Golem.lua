23b0c203624a62abd7ef065a10aaef2804d6b6a0
Frankie-hz <105882754+Frankie-hz@users.noreply.github.com>
2024-05-14 22:20:17 -0400

    Shift-proof Beaucedine Glacier & [S]

--- scripts/zones/Beaucedine_Glacier/mobs/Stone_Golem.lua
+++ scripts/zones/Beaucedine_Glacier/mobs/Stone_Golem.lua
@@ -7,11 +7,16 @@ local ID = zones[xi.zone.BEAUCEDINE_GLACIER]
 -----------------------------------
 local entity = {}
 
+local gargantuaPHTable =
+{
+    [ID.mob.GARGANTUA - 1] = ID.mob.GARGANTUA, -- 339 -0.472 -20
+}
+
 entity.onMobDeath = function(mob, player, optParams)
 end
 
 entity.onMobDespawn = function(mob)
-    xi.mob.phOnDespawn(mob, ID.mob.GARGANTUA_PH, 5, math.random(3600, 25200)) -- 1 to 7 hours
+    xi.mob.phOnDespawn(mob, gargantuaPHTable, 5, math.random(3600, 25200)) -- 1 to 7 hours
 end
 
 return entity
