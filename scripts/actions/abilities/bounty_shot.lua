c348438408e8eef071c6752f29418747df6aa2ab
andeluvian <kenneth.forsman@gmail.com>
2024-06-05 18:00:57 +0300

    Lua: Move all ranger job logic to job_utils

--- scripts/actions/abilities/bounty_shot.lua
+++ scripts/actions/abilities/bounty_shot.lua
@@ -7,10 +7,10 @@
 local abilityObject = {}
 
 abilityObject.onAbilityCheck = function(player, target, ability)
-    return 0, 0
+    return xi.job_utils.ranger.checkBountyShot(player, target, ability)
 end
 
-abilityObject.onUseAbility = function(player, target, ability)
+abilityObject.onUseAbility = function(player, target, ability, action)
     -- target:addStatusEffect(xi.effect.BOUNTY_SHOT, 11, 1, 30) -- TODO: implement xi.effect.BOUNTY_SHOT
 end
 
