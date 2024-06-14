d9bc4fbced650c569047829392e759d00a966223
andeluvian <kenneth.forsman@gmail.com>
2024-06-14 13:59:42 +0300

    moved all ninja job abilities to use job_utils
    Refactor Ninja jobabilities to jobutils

--- scripts/actions/abilities/sange.lua
+++ scripts/actions/abilities/sange.lua
@@ -8,13 +8,11 @@
 local abilityObject = {}
 
 abilityObject.onAbilityCheck = function(player, target, ability)
-    return 0, 0
+    xi.job_utils.ninja.checkSange(player, target, ability)
 end
 
-abilityObject.onUseAbility = function(player, target, ability)
-    local potency = player:getMerit(xi.merit.SANGE)-1
-
-    player:addStatusEffect(xi.effect.SANGE, potency * 25, 0, 60)
+abilityObject.onUseAbility = function(player, target, ability, action)
+    xi.job_utils.ninja.useSange(player, target, ability, action)
 end
 
 return abilityObject
