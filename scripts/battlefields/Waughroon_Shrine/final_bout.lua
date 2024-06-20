891c72a9a4500b832a1c8925b0573fdf4231a7cf
Umeboshi <terrandragon@gmail.com>
2024-06-20 11:37:56 -0700

    [lua] BCNM content.loot variable rename Loot tables were using old naming convention. Have been renamed correctly so that the chest will open and players can obtain items.

--- scripts/battlefields/Waughroon_Shrine/final_bout.lua
+++ scripts/battlefields/Waughroon_Shrine/final_bout.lua
@@ -26,43 +26,43 @@ content.loot =
 {
     {
         quantity = 2,
-        { itemid = xi.item.BAG_OF_TREE_CUTTINGS, droprate = 1000 }, -- bag_of_tree_cuttings
+        { item = xi.item.BAG_OF_TREE_CUTTINGS, weight = 1000 }, -- bag_of_tree_cuttings
     },
 
     {
-        { itemid = xi.item.CLUMP_OF_BOYAHDA_MOSS, droprate = 1000 }, -- clump_of_boyahda_moss
+        { item = xi.item.CLUMP_OF_BOYAHDA_MOSS, weight = 1000 }, -- clump_of_boyahda_moss
     },
 
     {
-        { itemid = xi.item.SCROLL_OF_QUAKE,          droprate = 100 }, -- scroll_of_quake
-        { itemid = xi.item.PIECE_OF_WISTERIA_LUMBER, droprate = 100 }, -- piece_of_wisteria_lumber
-        { itemid = xi.item.MAHOGANY_LOG,             droprate = 100 }, -- mahogany_log
-        { itemid = xi.item.EBONY_LOG,                droprate = 100 }, -- ebony_log
-        { itemid = xi.item.SCROLL_OF_FREEZE,         droprate = 100 }, -- scroll_of_freeze
-        { itemid = xi.item.DARKSTEEL_INGOT,          droprate = 100 }, -- darksteel_ingot
-        { itemid = xi.item.SCROLL_OF_RAISE_II,       droprate = 100 }, -- scroll_of_raise_ii
-        { itemid = xi.item.PETRIFIED_LOG,            droprate = 100 }, -- petrified_log
-        { itemid = xi.item.GOLD_INGOT,               droprate = 100 }, -- gold_ingot
-        { itemid = xi.item.CORAL_FRAGMENT,           droprate = 100 }, -- coral_fragment
+        { item = xi.item.SCROLL_OF_QUAKE,          weight = 100 }, -- scroll_of_quake
+        { item = xi.item.PIECE_OF_WISTERIA_LUMBER, weight = 100 }, -- piece_of_wisteria_lumber
+        { item = xi.item.MAHOGANY_LOG,             weight = 100 }, -- mahogany_log
+        { item = xi.item.EBONY_LOG,                weight = 100 }, -- ebony_log
+        { item = xi.item.SCROLL_OF_FREEZE,         weight = 100 }, -- scroll_of_freeze
+        { item = xi.item.DARKSTEEL_INGOT,          weight = 100 }, -- darksteel_ingot
+        { item = xi.item.SCROLL_OF_RAISE_II,       weight = 100 }, -- scroll_of_raise_ii
+        { item = xi.item.PETRIFIED_LOG,            weight = 100 }, -- petrified_log
+        { item = xi.item.GOLD_INGOT,               weight = 100 }, -- gold_ingot
+        { item = xi.item.CORAL_FRAGMENT,           weight = 100 }, -- coral_fragment
     },
 
     {
-        { itemid = xi.item.SHOCK_MASK,              droprate = 62 }, -- shock_mask
-        { itemid = xi.item.SUPER_RIBBON,            droprate = 62 }, -- super_ribbon
-        { itemid = xi.item.RIVAL_RIBBON,            droprate = 62 }, -- rival_ribbon
-        { itemid = xi.item.IVORY_MITTS,             droprate = 62 }, -- ivory_mitts
-        { itemid = xi.item.SPIKED_FINGER_GAUNTLETS, droprate = 70 }, -- spiked_finger_gauntlets
-        { itemid = xi.item.SLY_GAUNTLETS,           droprate = 62 }, -- sly_gauntlets
-        { itemid = xi.item.RUSH_GLOVES,             droprate = 62 }, -- rush_gloves
-        { itemid = xi.item.MANA_CIRCLET,            droprate = 62 }, -- mana_circlet
-        { itemid = xi.item.HATEFUL_COLLAR,          droprate = 62 }, -- hateful_collar
-        { itemid = xi.item.ESOTERIC_MANTLE,         droprate = 62 }, -- esoteric_mantle
-        { itemid = xi.item.TEMPLARS_MANTLE,         droprate = 62 }, -- templars_mantle
-        { itemid = xi.item.HEAVY_MANTLE,            droprate = 62 }, -- heavy_mantle
-        { itemid = xi.item.INTELLECT_TORQUE,        droprate = 62 }, -- intellect_torque
-        { itemid = xi.item.STORM_GORGET,            droprate = 62 }, -- storm_gorget
-        { itemid = xi.item.BENIGN_NECKLACE,         droprate = 62 }, -- benign_necklace
-        { itemid = xi.item.SNIPERS_MANTLE,          droprate = 62 }, -- snipers_mantle
+        { item = xi.item.SHOCK_MASK,              weight = 62 }, -- shock_mask
+        { item = xi.item.SUPER_RIBBON,            weight = 62 }, -- super_ribbon
+        { item = xi.item.RIVAL_RIBBON,            weight = 62 }, -- rival_ribbon
+        { item = xi.item.IVORY_MITTS,             weight = 62 }, -- ivory_mitts
+        { item = xi.item.SPIKED_FINGER_GAUNTLETS, weight = 70 }, -- spiked_finger_gauntlets
+        { item = xi.item.SLY_GAUNTLETS,           weight = 62 }, -- sly_gauntlets
+        { item = xi.item.RUSH_GLOVES,             weight = 62 }, -- rush_gloves
+        { item = xi.item.MANA_CIRCLET,            weight = 62 }, -- mana_circlet
+        { item = xi.item.HATEFUL_COLLAR,          weight = 62 }, -- hateful_collar
+        { item = xi.item.ESOTERIC_MANTLE,         weight = 62 }, -- esoteric_mantle
+        { item = xi.item.TEMPLARS_MANTLE,         weight = 62 }, -- templars_mantle
+        { item = xi.item.HEAVY_MANTLE,            weight = 62 }, -- heavy_mantle
+        { item = xi.item.INTELLECT_TORQUE,        weight = 62 }, -- intellect_torque
+        { item = xi.item.STORM_GORGET,            weight = 62 }, -- storm_gorget
+        { item = xi.item.BENIGN_NECKLACE,         weight = 62 }, -- benign_necklace
+        { item = xi.item.SNIPERS_MANTLE,          weight = 62 }, -- snipers_mantle
     },
 }
 
