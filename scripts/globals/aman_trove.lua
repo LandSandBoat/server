3c6d8e2eaf75167997b6ca194112e3174684cebf
Zach Toogood <zrtoogood@gmail.com>
2024-06-20 10:11:28 +0100

    Add debouncing to chests in aman trove

--- scripts/globals/aman_trove.lua
+++ scripts/globals/aman_trove.lua
@@ -4,6 +4,12 @@
 xi.amanTrove = {}
 
 xi.amanTrove.chestOPlentyOnTrigger = function(player, npc)
+    -- Debouncing (just in case)
+    if npc:getLocalVar('Triggered') == 1 then
+        return
+    end
+    npc:setLocalVar('Triggered', 1)
+
     if npc:getLocalVar('Mimic') == 1 then
         xi.amanTrove.prepareMimic(player, npc)
         return
@@ -11,17 +17,45 @@ xi.amanTrove.chestOPlentyOnTrigger = function(player, npc)
 
     print("You have triggered the Chest O'Plenty.")
 
+
+    npc:setAnimationSub(1)
+    npc:setUntargetable(true)
+
     -- TODO: Thuds etc.
 end
 
 xi.amanTrove.terminalCofferOnTrigger = function(player, npc)
+    -- Debouncing (just in case)
+    if npc:getLocalVar('Triggered') == 1 then
+        return
+    end
+    npc:setLocalVar('Triggered', 1)
+
+    npc:setAnimationSub(1)
+    npc:setUntargetable(true)
+
     print("You have triggered the Terminal Coffer.")
 
     -- TODO: Count up thuds, etc. generate loot
+
+    -- TODO: We're going to predetermine how many thuds etc. there are
+    --     : available at the start of the BC. If somehow we get to this point
+    --     : and there are more than we allocated - then the player has managed
+    --     : to cheat and we should report it and make sure they don't get any
+    --     : loot!
 end
 
 xi.amanTrove.prepareMimic = function(player, npc)
     print("The chest is a mimic!")
 
-    -- TODO: Make all other chests disappear, change model into animated mimic, set enmity, etc.
+    npc:setStatus(xi.status.UPDATE)
+    npc:setModelId(258)
+    npc:setAnimation(1) -- Combat
+    npc:setAnimationSub(1) -- Mouth open
+
+    -- Make things disappear:
+    -- npc:entityAnimationPacket('kesu', npc)
+
+    -- TODO: Make all other closed chests disappear, open chests remain.
+    -- TODO: Change model into animated mimic, make name visible, set enmity, etc.
 end
