e7b2472ac5aa8abb12fcc69eb36f26ccf8045caf
Jasson McMorris <jasson@siegegames.com>
2024-07-02 19:33:27 -0700

    Add additional effects to Chigoes

--- scripts/zones/Abyssea-Attohwa/mobs/Tunga.lua
+++ scripts/zones/Abyssea-Attohwa/mobs/Tunga.lua
@@ -6,6 +6,14 @@ mixins = { require('scripts/mixins/families/chigoe') }
 -----------------------------------
 local entity = {}
 
+entity.onMobInitialize = function(mob)
+    mob:setMobMod(xi.mobMod.ADD_EFFECT, 1)
+end
+
+entity.onAdditionalEffect = function(mob, target, damage)
+    return xi.mob.onAddEffect(mob, target, damage, xi.mob.ae.POISON, { chance = 10, power = 130 })
+end
+
 entity.onMobDeath = function(mob, player, optParams)
 end
 
--- scripts/zones/The_Eldieme_Necropolis_[S]/mobs/Chigoe.lua
+++ scripts/zones/The_Eldieme_Necropolis_[S]/mobs/Chigoe.lua
@@ -4,6 +4,14 @@
 -----------------------------------
 local entity = {}
 
+entity.onMobInitialize = function(mob)
+    mob:setMobMod(xi.mobMod.ADD_EFFECT, 1)
+end
+
+entity.onAdditionalEffect = function(mob, target, damage)
+    return xi.mob.onAddEffect(mob, target, damage, xi.mob.ae.PLAGUE, { chance = 10 })
+end
+
 entity.onMobDeath = function(mob, player, optParams)
 end
 
