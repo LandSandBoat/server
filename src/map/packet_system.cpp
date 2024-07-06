ba43875934f6c0d4fa62524c77ed18e9a07764ec
Tiberon <TiberonKalkaz@gmail.com>
2024-07-05 19:45:34 -0700

    Correct the look of suit syle equipment

--- src/map/packet_system.cpp
+++ src/map/packet_system.cpp
@@ -3683,13 +3683,13 @@ void SmallPacket0x053(map_session_data_t* const PSession, CCharEntity* const PCh
                     break;
             }
         }
-        charutils::UpdateRemovedSlots(PChar);
+        charutils::UpdateRemovedSlotsLookForLockStyle(PChar);
         PChar->RequestPersist(CHAR_PERSIST::EQUIP);
     }
     else if (type == 4)
     {
         charutils::SetStyleLock(PChar, true);
-        charutils::UpdateRemovedSlots(PChar);
+        charutils::UpdateRemovedSlotsLookForLockStyle(PChar);
         PChar->RequestPersist(CHAR_PERSIST::EQUIP);
     }
 
