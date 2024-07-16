a600278bcbb16df2837e9cb1af4d6ad9325dfdfa
hooksta4 <carl.hooker@gmail.com>
2024-07-16 06:11:19 -0400

    Sandy 6-2 Adds table and npcUtil.popFromQM & shift proof popped NM (#5633)
    Co-authored-by: Frank <105882754+Frankie-hz@users.noreply.github.com>
    Co-authored-by: Abdiah <62350957+dallano@users.noreply.github.com>

--- scripts/zones/King_Ranperres_Tomb/DefaultActions.lua
+++ scripts/zones/King_Ranperres_Tomb/DefaultActions.lua
@@ -1,5 +1,6 @@
 local ID = zones[xi.zone.KING_RANPERRES_TOMB]
 
 return {
-    ['_5a0'] = { messageSpecial = ID.text.HEAVY_DOOR },
+    ['_5a0']      = { messageSpecial = ID.text.HEAVY_DOOR },
+    ['Tombstone'] = { messageSpecial = ID.text.A_TOMB },
 }
