e141dd50f90baffc19004d0120e1747bdacb0ea9
Hugin <bradturner43@gmail.com>
2024-07-22 22:59:18 -0500

    Added ranged attack and ranged accuracy to attack and accuracy down.

--- scripts/effects/accuracy_down.lua
+++ scripts/effects/accuracy_down.lua
@@ -5,6 +5,7 @@ local effectObject = {}
 
 effectObject.onEffectGain = function(target, effect)
     target:addMod(xi.mod.ACC, -effect:getPower())
+    target:addMod(xi.mod.RACC, -effect:getPower())
 end
 
 effectObject.onEffectTick = function(target, effect)
@@ -13,6 +14,7 @@ effectObject.onEffectTick = function(target, effect)
     if downACCEffectSize > 0 then
         effect:setPower(downACCEffectSize - 1)
         target:delMod(xi.mod.ACC, -1)
+        target:delMod(xi.mod.RACC, -1)
     end
 end
 
@@ -20,6 +22,7 @@ effectObject.onEffectLose = function(target, effect)
     local downACCEffectSize = effect:getPower()
     if downACCEffectSize > 0 then
         target:delMod(xi.mod.ACC, -effect:getPower())
+        target:delMod(xi.mod.RACC, -effect:getPower())
     end
 end
 
