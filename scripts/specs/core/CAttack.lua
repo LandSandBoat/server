769da2ba137a6115907b865d8ae4319120f8a3e3
claywar <claywar@lsb.dev>
2024-08-21 13:55:19 -0400

    [lls] Define CAttack, CBattlefield functions
    Add placeholder for CBaseEntity

---
+++ scripts/specs/core/CAttack.lua
@@ -0,0 +1,13 @@
+---@meta
+
+---@class CAttack
+local CAttack = {}
+
+---@return boolean
+function CAttack:getCritical()
+end
+
+---@param critical boolean
+---@return nil
+function CAttack:setCritical(critical)
+end
