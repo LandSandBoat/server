2500a0869cbad64b8c3c117eaf15e6f905f888ed
claywar <claywar@lsb.dev>
2024-08-21 18:26:32 -0400

    [lls] Define CTriggerArea, CZone functions

---
+++ scripts/specs/core/CTriggerArea.lua
@@ -0,0 +1,22 @@
+---@meta
+
+---@class CTriggerArea
+local CTriggerArea = {}
+
+---@return integer
+function CTriggerArea:GetTriggerAreaID()
+end
+
+---@return integer
+function CTriggerArea:GetCount()
+end
+
+---@param count integer
+---@return integer
+function CTriggerArea:AddCount(count)
+end
+
+---@param count integer
+---@return integer
+function CTriggerArea:DelCount(count)
+end
