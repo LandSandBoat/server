d7437a307368e58ab31fd04525667c25e2c8356f
claywar <claywar@lsb.dev>
2024-08-21 16:18:53 -0400

    [lls] Define CLootContainer, CMobSkill functions
    Update README notes

---
+++ scripts/specs/core/CLootContainer.lua
@@ -0,0 +1,30 @@
+---@meta
+
+---@class CLootContainer
+local CLootContainer = {}
+
+---@param item integer
+---@param rate integer
+---@param quantity integer?
+---@return nil
+function CLootContainer:addItem(item, rate, quantity)
+end
+
+---@param groupRate integer
+---@param items table
+---@return nil
+function addGroup(groupRate, items)
+end
+
+---@param item integer
+---@param rate integer
+---@param quantity integer?
+---@return nil
+function addItemFixed(item, rate, quantity)
+end
+
+---@param groupRate integer
+---@param items table
+---@return nil
+function addGroupFixed(groupRate, items)
+end
