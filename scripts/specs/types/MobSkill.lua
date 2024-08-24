2211fcfa12306ea875453df8e3bf732f750986d3
claywar <claywar@lsb.dev>
2024-08-24 19:09:36 -0400

    [type] Add MobSkill type

---
+++ scripts/specs/types/MobSkill.lua
@@ -0,0 +1,5 @@
+---@meta
+
+---@class TMobSkill
+---@field onMobSkillCheck? fun(PTarget: CBaseEntity, PMob: CBaseEntity, PMobSkill: CMobSkill): integer?
+---@field onMobWeaponSkill? fun(PTarget: CBaseEntity, PMob: CBaseEntity, PMobSkill: CMobSkill, action: CAction): integer?
