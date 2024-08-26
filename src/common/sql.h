f93e273f3b1cefb3c309d22a60a25a43a362898c
Zach Toogood <zrtoogood@gmail.com>
2024-08-26 10:58:52 +0100

    Chore: Normalise license header format

--- src/common/sql.h
+++ src/common/sql.h
@@ -1,5 +1,23 @@
-﻿// Copyright (c) Athena Dev Teams - Licensed under GNU GPL
-// For more information, see LICENCE in the main folder
+﻿/*
+===========================================================================
+
+  Copyright (c) Athena Dev Teams
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see http://www.gnu.org/licenses/
+
+===========================================================================
+*/
 
 #ifndef _COMMON_SQL_H
 #define _COMMON_SQL_H
