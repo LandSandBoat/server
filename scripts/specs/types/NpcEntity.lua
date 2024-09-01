f2a04a15a2a77c269d51acd93e09f304ebee5908
claywar <claywar@lsb.dev>
2024-09-01 07:03:41 -0400

    [type] Add TNpcEntity type

---
+++ scripts/specs/types/NpcEntity.lua
@@ -0,0 +1,7 @@
+---@meta
+
+---@class TNpcEntity
+---@field onTrade? fun(player: CBaseEntity, npc: CBaseEntity, trade: CTradeContainer)
+---@field onTrigger? fun(player: CBaseEntity, npc: CBaseEntity)
+---@field onEventUpdate? fun(player: CBaseEntity, csid: integer, option: integer, npc: CBaseEntity)
+---@field onEventFinish? fun(player: CBaseEntity, csid: integer, option: integer, npc: CBaseEntity)
