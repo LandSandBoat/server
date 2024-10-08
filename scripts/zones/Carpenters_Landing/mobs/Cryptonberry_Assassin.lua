28d319d26dce1c065c66996781ca2e9d83faa837
TracentEden <92269743+TracentEden@users.noreply.github.com>
2024-10-09 00:44:37 +0300

    Fix issues with cop 7-4 cryptonberry

--- scripts/zones/Carpenters_Landing/mobs/Cryptonberry_Assassin.lua
+++ scripts/zones/Carpenters_Landing/mobs/Cryptonberry_Assassin.lua
@@ -4,36 +4,63 @@
 -- !pos 120.615 -5.457 -390.133 2
 -----------------------------------
 local ID = zones[xi.zone.CARPENTERS_LANDING]
-mixins = { require('scripts/mixins/job_special') }
 -----------------------------------
 ---@type TMobEntity
 local entity = {}
+
+local cryptonberrySpecials =
+{
+    [xi.job.SMN] = xi.jsa.ASTRAL_FLOW,
+    [xi.job.BLM] = xi.jsa.MANAFONT,
+    [xi.job.THF] = xi.jsa.PERFECT_DODGE,
+}
 
 entity.onMobInitialize = function(mob)
     mob:setMobMod(xi.mobMod.IDLE_DESPAWN, 180) -- 3 minutes
 end
 
 entity.onMobSpawn = function(mob)
-    xi.mix.jobSpecial.config(mob, {
-        specials =
-        {
-            {
-                id = xi.jsa.MIJIN_GAKURE,
-                begCode = function(mobArg)
-                    mobArg:messageText(mobArg, ID.text.CRYPTONBERRY_ASSASSIN_2HR)
-                end,
-            },
-        },
-    })
+    mob:addImmunity(xi.immunity.LIGHT_SLEEP)
+    mob:setMod(xi.mod.SLEEP_MEVA, 90)
+    mob:addImmunity(xi.immunity.SILENCE)
+    -- assassins and executor have special 2hr logic thus use local vars to track
+    mob:setLocalVar('twoHourThreshold', math.random(20, 75))
 end
 
-entity.onMobEngage = function(mob, target)
-end
-
-entity.onMobRoam = function(mob)
+entity.onMobFight = function(mob, target)
+    -- mob 2hr was triggered by executor death
+    if
+        mob:getLocalVar('triggerTwoHour') == 1 and
+        mob:getLocalVar('usedTwoHour') == 0
+    then
+        mob:setLocalVar('usedTwoHour', 1)
+        mob:messageText(mob, ID.text.CRYPTONBERRY_ASSASSIN_2HR)
+        mob:useMobAbility(cryptonberrySpecials[mob:getMainJob()])
+    -- mob 2hr was unlocked by executor death but still need to reach random threshold
+    elseif
+        mob:getLocalVar('unlockTwoHour') == 1 and
+        mob:getLocalVar('usedTwoHour') == 0 and
+        mob:getHPP() < mob:getLocalVar('twoHourThreshold')
+    then
+        mob:setLocalVar('usedTwoHour', 1)
+        mob:messageText(mob, ID.text.CRYPTONBERRY_ASSASSIN_2HR)
+        mob:useMobAbility(cryptonberrySpecials[mob:getMainJob()])
+    end
 end
 
 entity.onMobDeath = function(mob, player, optParams)
+    if mob:getLocalVar('firstOnMobDeathCall') == 0 then
+        -- have the executor track how many assassins have been killed
+        -- so executor can use 2hr after first one
+        mob:setLocalVar('firstOnMobDeathCall', 1)
+        local executor = GetMobByID(ID.mob.CRYPTONBERRY_EXECUTOR)
+        if
+            executor and
+            executor:isAlive()
+        then
+            executor:setLocalVar('assassinsKilled', executor:getLocalVar('assassinsKilled') + 1)
+        end
+    end
 end
 
 return entity
--- scripts/zones/Carpenters_Landing/mobs/Cryptonberry_Executor.lua
+++ scripts/zones/Carpenters_Landing/mobs/Cryptonberry_Executor.lua
@@ -4,43 +4,83 @@
 -- !pos 120.615 -5.457 -390.133 2
 -----------------------------------
 local ID = zones[xi.zone.CARPENTERS_LANDING]
-mixins = { require('scripts/mixins/job_special') }
 -----------------------------------
 ---@type TMobEntity
 local entity = {}
+
+local assassins = { ID.mob.CRYPTONBERRY_EXECUTOR + 1, ID.mob.CRYPTONBERRY_EXECUTOR + 2, ID.mob.CRYPTONBERRY_EXECUTOR + 3 }
+
+local function unlockAssassinTwoHours()
+    for _, assassinID in ipairs(assassins) do
+        local assassin = GetMobByID(assassinID)
+        if assassin then
+            assassin:setLocalVar('unlockTwoHour', 1)
+        end
+    end
+end
 
 entity.onMobInitialize = function(mob)
     mob:setMobMod(xi.mobMod.IDLE_DESPAWN, 180) -- 3 minutes
 end
 
 entity.onMobSpawn = function(mob)
-    xi.mix.jobSpecial.config(mob, {
-        delay = 180,
-        specials =
-        {
-            {
-                id = xi.jsa.MIJIN_GAKURE,
-                hpp = 100,
-                begCode = function(mobArg)
-                    mobArg:messageText(mobArg, ID.text.CRYPTONBERRY_EXECUTOR_2HR)
-                end,
-            },
-        },
-    })
+    mob:addImmunity(xi.immunity.LIGHT_SLEEP)
+    mob:addImmunity(xi.immunity.DARK_SLEEP)
+    mob:addImmunity(xi.immunity.SILENCE)
+    mob:addImmunity(xi.immunity.TERROR)
+    mob:setMod(xi.mod.REGAIN, 100)
 end
 
 entity.onMobFight = function(mob, target)
-    -- spawn Assassins when enmity is gained against Executor
-    if mob:getLocalVar('spawnedAssassins') == 0 and mob:getCE(target) > 0 then
+    -- spawn assassins when enmity is gained against Executor
+    if
+        mob:getLocalVar('spawnedAssassins') == 0 and
+        mob:getCE(target) > 0
+    then
         mob:setLocalVar('spawnedAssassins', 1)
 
-        for i = 1, 3 do
-            SpawnMob(ID.mob.CRYPTONBERRY_EXECUTOR + i)
+        -- inject packet for 2hr animation when spawning
+        mob:injectActionPacket(mob:getID(), 11, 439, 0, 0x18, 0, 0, 307)
+        for _, assassinID in ipairs(assassins) do
+            local assassin = SpawnMob(assassinID)
+            if assassin then
+                assassin:updateEnmity(target)
+            end
         end
+    end
+
+    -- if any assassin killed before executor
+    if
+        mob:getLocalVar('spawnedAssassins') == 1 and
+        mob:getLocalVar('assassinsKilled') > 0 and
+        mob:getLocalVar('usedTwoHour') == 0
+    then
+    -- have executor use 2hr and unlock the 2hrs of other assassins
+        mob:setLocalVar('usedTwoHour', 1)
+        mob:messageText(mob, ID.text.CRYPTONBERRY_EXECUTOR_2HR)
+        -- Use Mijin Gakure
+        mob:useMobAbility(731)
+        -- unlock the 2hrs of the assassins
+        unlockAssassinTwoHours()
     end
 end
 
 entity.onMobDeath = function(mob, player, optParams)
+    if mob:getLocalVar('firstOnMobDeathCall') == 0 then
+        mob:setLocalVar('firstOnMobDeathCall', 1)
+        mob:messageText(mob, ID.text.CRYPTONBERRY_EXECUTOR_DIE)
+        -- if executor is the first mob killed
+        if mob:getLocalVar('assassinsKilled') == 0 then
+            -- then force one random assassin to use 2hr
+            local randomAssassin = GetMobByID(assassins[math.random(1, #assassins)])
+            if randomAssassin then
+                randomAssassin:setLocalVar('triggerTwoHour', 1)
+            end
+
+            -- unlock the 2hrs of the other two (to use at random threshold)
+            unlockAssassinTwoHours()
+        end
+    end
 end
 
 return entity
