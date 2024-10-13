2fdcd31e92593f6df14ab1a0e7941570a80926c9
TracentEden <92269743+TracentEden@users.noreply.github.com>
2024-10-13 16:09:17 +0300

    Fix feeler antlion qm respawn time

--- scripts/zones/Attohwa_Chasm/mobs/Executioner_Antlion.lua
+++ scripts/zones/Attohwa_Chasm/mobs/Executioner_Antlion.lua
@@ -2,7 +2,9 @@
 -- Area: Attohwa Chasm
 --  Mob: Executioner Antlion
 -----------------------------------
+local ID = zones[xi.zone.ATTOHWA_CHASM]
 mixins = { require('scripts/mixins/families/antlion_ambush_noaggro') }
+local attohwaChasmGlobal = require('scripts/zones/Attohwa_Chasm/globals')
 -----------------------------------
 ---@type TMobEntity
 local entity = {}
@@ -12,6 +14,12 @@ entity.onMobInitialize = function(mob)
 end
 
 entity.onMobDeath = function(mob, player, optParams)
+end
+
+entity.onMobDespawn = function(mob)
+    if attohwaChasmGlobal.canStartFeelerQMTimer() then
+        GetNPCByID(ID.npc.QM_FEELER_ANTLION):updateNPCHideTime(xi.settings.main.FORCE_SPAWN_QM_RESET_TIME)
+    end
 end
 
 return entity
--- scripts/zones/Attohwa_Chasm/mobs/Feeler_Antlion.lua
+++ scripts/zones/Attohwa_Chasm/mobs/Feeler_Antlion.lua
@@ -2,6 +2,9 @@
 -- Area: Attohwa Chasm
 --  Mob: Feeler Antlion
 -----------------------------------
+local ID = zones[xi.zone.ATTOHWA_CHASM]
+local attohwaChasmGlobal = require('scripts/zones/Attohwa_Chasm/globals')
+-----------------------------------
 ---@type TMobEntity
 local entity = {}
 
@@ -16,6 +19,12 @@ entity.onMobSpawn = function(mob)
 end
 
 entity.onMobDeath = function(mob, player, optParams)
+end
+
+entity.onMobDespawn = function(mob)
+    if attohwaChasmGlobal.canStartFeelerQMTimer() then
+        GetNPCByID(ID.npc.QM_FEELER_ANTLION):updateNPCHideTime(xi.settings.main.FORCE_SPAWN_QM_RESET_TIME)
+    end
 end
 
 return entity
