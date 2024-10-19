d7b0abd8c865a989d7a017e4e3ec623abe27bb8e
jamesbradleym <jamesbradleym@gmail.com>
2024-10-18 23:53:54 -0400

    Implement You Call That a Knife Quest

---
+++ scripts/zones/Temple_of_Uggalepih/mobs/Cook_Minberry.lua
@@ -0,0 +1,13 @@
+-----------------------------------
+-- Area: Temple of Uggalepih
+--   NM: Cook Minberry
+-----------------------------------
+mixins = { require('scripts/mixins/job_special') }
+-----------------------------------
+---@type TMobEntity
+local entity = {}
+
+entity.onMobSpawn = function(mob)
+end
+
+return entity
