eb6bb92b0a62db548a88ecd15838ea097de5a7d1
TracentEden <92269743+TracentEden@users.noreply.github.com>
2024-10-19 19:47:36 +0300

    Fix several den of rancor mob issues
    Co-authored-by: dallano <dallano@users.noreply.github.com>
    Co-authored-by: CriticalXI <CriticalXI@users.noreply.github.com>

--- scripts/zones/Den_of_Rancor/mobs/Hakutaku.lua
+++ scripts/zones/Den_of_Rancor/mobs/Hakutaku.lua
@@ -6,8 +6,17 @@
 local entity = {}
 
 entity.onMobInitialize = function(mob)
-    mob:setMobMod(xi.mobMod.GIL_MIN, 1600)
-    mob:setMobMod(xi.mobMod.GIL_MAX, 3500)
+    mob:setMobMod(xi.mobMod.GIL_MIN, 18000)
+end
+
+entity.onMobSpawn = function(mob)
+    mob:addImmunity(xi.immunity.LIGHT_SLEEP)
+    mob:addImmunity(xi.immunity.DARK_SLEEP)
+    mob:addImmunity(xi.immunity.PETRIFY)
+
+    mob:setMod(xi.mod.SILENCE_MEVA, 200)
+    mob:setMod(xi.mod.TRIPLE_ATTACK, 25)
+    mob:setMod(xi.mod.DOUBLE_ATTACK, 15)
 end
 
 entity.onMobDeath = function(mob, player, optParams)
