2c0ade714bbb3fcf5804cfb7a3d506c08353d772
eqbot <trentkenn8@gmail.com>
2025-02-19 14:21:58 -0800

    Add setbattlefieldtime command to adjust time on battlefield

---
+++ scripts/commands/setbattlefieldtime.lua
@@ -0,0 +1,28 @@
+-----------------------------------
+-- func: setbattlefieldtime
+-- desc: Set time limit for an active battlefield
+-----------------------------------
+---@type TCommand
+local commandObj = {}
+
+commandObj.cmdprops =
+{
+    permission = 4,
+    parameters = 'si'
+}
+
+local function error(player, msg)
+    player:printToPlayer(msg)
+    player:printToPlayer('!setbattlefieldtime <minutes>')
+end
+
+commandObj.onTrigger = function(player, minutes)
+    local battlefield     = player:getBattlefield()
+    if not battlefield then
+        return
+    end
+
+    battlefield:setTimeLimit(minutes * 60)
+end
+
+return commandObj
