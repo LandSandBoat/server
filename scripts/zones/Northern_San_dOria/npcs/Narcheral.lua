4862d89b056acc2b9061f0150e73a0e466b8ad28
KnowOne <Knowone134@gmail.com>
2025-02-26 20:56:54 -0600

    Quest Prelude of Black and White to IF

--- scripts/zones/Northern_San_dOria/npcs/Narcheral.lua
+++ scripts/zones/Northern_San_dOria/npcs/Narcheral.lua
@@ -17,15 +17,6 @@ entity.onTrade = function(player, npc, trade)
         then
             player:startEvent(690) -- Finish quest "Messenger from Beyond"
         end
-    elseif player:getQuestStatus(xi.questLog.SANDORIA, xi.quest.id.sandoria.PRELUDE_OF_BLACK_AND_WHITE) == xi.questStatus.QUEST_ACCEPTED then
-        if
-            trade:hasItemQty(xi.item.CANTEEN_OF_YAGUDO_HOLY_WATER, 1) and
-            trade:hasItemQty(xi.item.MOCCASINS, 1) and
-            trade:getItemCount() == 2
-        then
-            -- Trade Yagudo Holy Water & Moccasins
-            player:startEvent(691) -- Finish quest "Prelude of Black and White"
-        end
     elseif player:getQuestStatus(xi.questLog.SANDORIA, xi.quest.id.sandoria.PIEUJES_DECISION) == xi.questStatus.QUEST_ACCEPTED then
         if
             trade:hasItemQty(xi.item.TAVNAZIAN_MASK, 1) and
@@ -66,16 +57,6 @@ entity.onEventFinish = function(player, csid, option, npc)
             player:tradeComplete()
             player:addFame(xi.fameArea.SANDORIA, 20)
             player:completeQuest(xi.questLog.SANDORIA, xi.quest.id.sandoria.MESSENGER_FROM_BEYOND)
-        end
-    elseif csid == 691 then
-        if player:getFreeSlotsCount() == 0 then
-            player:messageSpecial(ID.text.ITEM_CANNOT_BE_OBTAINED, xi.item.HEALERS_DUCKBILLS) -- Healer's Duckbills
-        else
-            player:addItem(xi.item.HEALERS_DUCKBILLS)
-            player:messageSpecial(ID.text.ITEM_OBTAINED, xi.item.HEALERS_DUCKBILLS) -- Healer's Duckbills
-            player:tradeComplete()
-            player:addFame(xi.fameArea.SANDORIA, 40)
-            player:completeQuest(xi.questLog.SANDORIA, xi.quest.id.sandoria.PRELUDE_OF_BLACK_AND_WHITE)
         end
     elseif csid == 692 then
         if player:getFreeSlotsCount() == 0 then
