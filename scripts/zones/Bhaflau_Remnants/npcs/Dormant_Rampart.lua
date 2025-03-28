c6bc847c0525c624d47a2ea5c29d4f2279a63129
Zach Toogood <zrtoogood@gmail.com>
2025-03-28 15:44:44 +0000

    Salvage: Move pos table lookup to ID

--- scripts/zones/Bhaflau_Remnants/npcs/Dormant_Rampart.lua
+++ scripts/zones/Bhaflau_Remnants/npcs/Dormant_Rampart.lua
@@ -4,112 +4,6 @@
 local ID = zones[xi.zone.BHAFLAU_REMNANTS]
 -----------------------------------
 
-local pos =
-{
-    [1] =
-    {
-        [1] =
-        {
-            enter = { -340, 0, -530, 192 },
-            exit  = { 420, 16, -291,  64 },
-        },
-        [2] =
-        {
-            enter = { -340, 0, -530, 192 },
-            exit  = { 451, 16, -460, 255 },
-        },
-        [3] =
-        {
-            enter = { -340, 0, -530, 192 },
-            exit  = { 260, 16, -291,  64 },
-        },
-        [4] =
-        {
-            enter = { -340, 0, -530, 192 },
-            exit  = { 229, 16, -460, 129 },
-        },
-    },
-    [2] =
-    {
-        [1] =
-        {
-            enter = { -340, 0, -233, 64 },
-            exit  = {  309, -4, 260,  0 },
-        },
-        [2] =
-        {
-            enter = { -340, 0, -233, 64 },
-            exit  = { 340, -4, 229, 197 },
-        },
-        [3] =
-        {
-            enter = { -340, 0, -233, 64 },
-            exit  = { 371, -4, 260, 126 },
-        },
-        [4] =
-        {
-            enter = { -340, 0, -233, 64 },
-            exit  = { 340, -4,  291, 63 },
-        },
-    },
-    [3] =
-    {
-        [1] =
-        {
-            enter = { 260, 0.5, 114 , 192 },
-            exit  = { -300, -4, -420,   0 },
-        },
-        [2] =
-        {
-            enter = { 260, 0.5,  114, 192 },
-            exit  = { -380, -4, -420, 128 },
-        },
-    },
-    [4] =
-    {
-        [1] =
-        {
-            enter = { 420,  0, 114, 192 },
-            exit  = { -300, 0, -75, 192 },
-        },
-        [2] =
-        {
-            enter = { 420, 0, 114, 192 },
-            exit  = { -315, -4, 20,  0 },
-        },
-        [3] =
-        {
-            enter = {  420,  0, 114, 192 },
-            exit  = { -220, -4, 125, 192 },
-        },
-        [4] =
-        {
-            enter = { 420,  0, 114, 192 },
-            exit  = { -300, 0, 195,  64 },
-        },
-        [5] =
-        {
-            enter = {  420, 0, 114, 192 },
-            exit  = { -380, 0, -75, 192 },
-        },
-        [6] =
-        {
-            enter = {  420, 0, 114, 192 },
-            exit  = { -365, -4, 20, 128 },
-        },
-        [7] =
-        {
-            enter = {  420,  0, 114, 192 },
-            exit  = { -460, -4, 125, 192 },
-        },
-        [8] =
-        {
-            enter = {  420, 0, 114, 192 },
-            exit  = { -380, 0, 195,  64 },
-        },
-    },
-}
-
 ---@type TNpcEntity
 local entity = {}
 
@@ -130,10 +24,9 @@ entity.onEventUpdate = function(player, csid, option, npc)
                 end
             end
         elseif csid == 5 then
-            local stage = instance:getStage()
+            local stage   = instance:getStage()
             local section = instance:getLocalVar('dormantArea')
-            local sendTo = pos[stage][section].enter
-
+            local sendTo  = ID.pos[stage][section].enter
             if sendTo then
                 player:setPos(unpack(sendTo))
             end
