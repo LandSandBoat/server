620bcedaae92ccfee8fa5b2a69f0a79b5ebf3eb5
Ganiman <13344227+GanimanSwift@users.noreply.github.com>
2025-04-01 18:00:35 -0400

    Make Royal Savior better

--- scripts/actions/mobskills/royal_savior.lua
+++ scripts/actions/mobskills/royal_savior.lua
@@ -1,6 +1,6 @@
 -----------------------------------
 -- Royal Savior
--- Grants effect of Protect
+-- Grants effects of Sentinel and Stoneskin
 -----------------------------------
 ---@type TMobSkill
 local mobskillObject = {}
@@ -10,9 +10,18 @@ mobskillObject.onMobSkillCheck = function(target, mob, skill)
 end
 
 mobskillObject.onMobWeaponSkill = function(target, mob, skill)
-    skill:setMsg(xi.mobskills.mobBuffMove(mob, xi.effect.PROTECT, 175, 0, 300))
+    -- Unsure if this is the correct value for enmity gain. The Wikis say it is stronger than Sentinel.
+    -- Sentinel is 900, Provoke is 1800
+    local power = 1300
 
-    return xi.effect.PROTECT
+    -- No data for how strong the stoneskin effect is from Royal Savior. Stoneskin cap is 350 without bonuses. This will make it 300 for level 99.
+    local amount = mob:getMainLvl() * 3 + 3
+
+    target:addEnmity(mob, 1, power)
+    mob:addStatusEffect(xi.effect.STONESKIN, amount, 0, 300)
+    skill:setMsg(xi.msg.basic.SKILL_GAIN_EFFECT)
+
+    return xi.effect.SENTINEL
 end
 
 return mobskillObject
