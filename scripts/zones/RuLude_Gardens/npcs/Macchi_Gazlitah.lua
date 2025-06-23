4229987028dac0c8496c7e505bf20fae0053070c
mikeyuzu <agnet.tea@gmail.com>
2025-06-23 12:41:44 +0900

    Macchi Gazlitahのランクを常時4にしておく

--- scripts/zones/RuLude_Gardens/npcs/Macchi_Gazlitah.lua
+++ scripts/zones/RuLude_Gardens/npcs/Macchi_Gazlitah.lua
@@ -14,6 +14,18 @@ entity.onTrigger = function(player, npc)
         { xi.item.JUG_OF_ULEGUERAND_MILK, 300 },
         { xi.item.WEDGE_OF_CHALAIMBILLE,  600 },
         { xi.item.JUG_OF_WORMY_BROTH,     100 },
+        { 5686,    800, }, -- Cheese Sandwich
+        { 5729,   3360, }, -- Bavarois
+        { 5718,   1300, }, -- Cream Puff
+        { 461,    5000, }, -- Buffalo Milk Case
+        { 5152,   1280, }, -- Buffalo Meat
+        { 4722,  31878, }, -- Enfire II
+        { 4723,  30492, }, -- Enblizzard II
+        { 4724,  27968, }, -- Enaero II
+        { 4725,  26112, }, -- Enstone II
+        { 4726,  25600, }, -- Enthunder II
+        { 4727,  33000, }, -- Enwater II
+        { 4850, 150000, }, -- Refresh II
     }
 
     -- TODO: Use a Server Var to track shop sales.
@@ -33,7 +45,7 @@ entity.onTrigger = function(player, npc)
     -- { 4727,  33000, }, -- Enwater II
     -- { 4850, 150000, }, -- Refresh II
 
-    player:showText(npc, ID.text.MACCHI_GAZLITAH_SHOP_DIALOG1)
+    player:showText(npc, ID.text.MACCHI_GAZLITAH_SHOP_DIALOG4)
     xi.shop.general(player, stock, xi.fameArea.JEUNO)
 end
 
