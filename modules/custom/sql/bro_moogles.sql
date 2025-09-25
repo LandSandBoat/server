2db9ab482cc614598085890d9dde6808a69985b3
unknown <zlatirl1@gmail.com>
2025-09-25 17:19:08 +0100

    move bro moogles into modules folder

---
+++ modules/custom/sql/bro_moogles.sql
@@ -0,0 +1,24 @@
+-- Places Bro Moogles in major cities
+-- Aht Urghan Whitegate
+UPDATE npc_list SET name = 'Bro_Moogle', pos_rot = 192, pos_x = 6.222, pos_y = 0.000, pos_z = 10.583, status = 0, look = _binary 0x00003E0900000000000000000000000000000000, widescan = 1 WHERE npcid = 16982024;
+
+-- Southern San d'Oria
+UPDATE npc_list SET name = 'Bro_Moogle', pos_rot = 194, pos_x = 0.000, pos_y = 1.999, pos_z = -13.0, status = 0, look = _binary 0x00003E0900000000000000000000000000000000, widescan = 1 WHERE npcid = 17719478;
+
+-- Bastok Markets
+UPDATE npc_list SET name = 'Bro_Moogle', pos_rot = 96, pos_x = -350.393, pos_y = -10.005, pos_z = -162.349, status = 0, look = _binary 0x00003E0900000000000000000000000000000000, widescan = 1 WHERE npcid = 17739880;
+
+-- Windurst Woods
+UPDATE npc_list SET name = 'Bro_Moogle', pos_rot = 94, pos_x = 109.329, pos_y = -5.0, pos_z = -35.728, status = 0, look = _binary 0x00003E0900000000000000000000000000000000, widescan = 1 WHERE npcid = 17764563;
+
+-- Ru'Lude Gardens
+UPDATE npc_list SET name = 'Bro_Moogle', pos_rot = 0, pos_x = -70.8, pos_y = 5.591, pos_z = -7.947, status = 0, look = _binary 0x00003E0900000000000000000000000000000000, widescan = 1 WHERE npcid = 17772673;
+
+-- Upper Jeuno
+UPDATE npc_list SET name = 'Bro_Moogle', pos_rot = 145, pos_x = -50.577, pos_y = 1.0, pos_z = 22.556, status = 0, look = _binary 0x00003E0900000000000000000000000000000000, widescan = 1 WHERE npcid = 17776751;
+
+-- Lower Jeuno
+UPDATE npc_list SET name = 'Bro_Moogle', pos_rot = 22, pos_x = -19.1, pos_y = 0.000, pos_z = -1.2, status = 0, look = _binary 0x00003E0900000000000000000000000000000000, widescan = 1 WHERE npcid = 17780901;
+
+-- Port Jeuno
+UPDATE npc_list SET name = 'Bro_Moogle', pos_rot = 197, pos_x = 28, pos_y = 0.000, pos_z = -9, status = 0, look = _binary 0x00003E0900000000000000000000000000000000, widescan = 1 WHERE npcid = 17784938;
