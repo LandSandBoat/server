d2afd4e581c142537ab07f3dd1e8ab2134e9da6f
unknown <zlatirl1@gmail.com>
2025-11-17 04:51:24 +0000

    repopulate enum/item.lua

--- modules/custom/sql/ffera_custom_npcs.sql
+++ modules/custom/sql/ffera_custom_npcs.sql
@@ -1,6 +1,37 @@
 -- Moves around NPCs that ERA uses
 
+-- ------------------------------------------------------------
+-- Southern San d'Oria
+-- ------------------------------------------------------------
+
+-- B.R.O. Moogle
+UPDATE npc_list SET name = 'Bro_Moogle', pos_rot = 194, pos_x = 0.000, pos_y = 1.999, pos_z = -13.0, status = 0, look = 0x00003E0900000000000000000000000000000000, widescan = 1 WHERE npcid = 17719478;
+
+-- ------------------------------------------------------------
+-- Bastok Markets
+-- ------------------------------------------------------------
+
+-- B.R.O. Moogle
+UPDATE npc_list SET name = 'Bro_Moogle', pos_rot = 96, pos_x = -350.393, pos_y = -10.005, pos_z = -162.349, status = 0, look = 0x00003E0900000000000000000000000000000000, widescan = 1 WHERE npcid = 17739880;
+
+-- ------------------------------------------------------------
+-- Windurst Woods
+-- ------------------------------------------------------------
+
+-- B.R.O. Moogle
+UPDATE npc_list SET name = 'Bro_Moogle', pos_rot = 94, pos_x = 109.329, pos_y = -5.0, pos_z = -35.728, status = 0, look = 0x00003E0900000000000000000000000000000000, widescan = 1 WHERE npcid = 17764563;
+
+-- ------------------------------------------------------------
+-- Port Jeuno
+-- ------------------------------------------------------------
+
+-- B.R.O. Moogle
+UPDATE npc_list SET name = 'Bro_Moogle', pos_rot = 197, pos_x = 28, pos_y = 0.000, pos_z = -9, status = 0, look = 0x00003E0900000000000000000000000000000000, widescan = 1 WHERE npcid = 17784938;
+
+-- ------------------------------------------------------------
 -- Lower Jeuno
+-- ------------------------------------------------------------
+
 -- Runga-Kuponga
 UPDATE npc_list SET pos_rot = 91, pos_x = 13.000, pos_y = 0.000, pos_z = 7.000, status = 0, look = 0x0000580800000000000000000000000000000000 WHERE npcid = 17780772;
 
@@ -19,11 +50,76 @@ UPDATE npc_list SET pos_rot = 22, pos_x = -17.100, pos_y = 0.000, pos_z = 1.500,
 -- Homepoint #2
 UPDATE npc_list SET pos_x = -6.681, pos_y = 0.000, pos_z = -11.259 WHERE npcid = 17780874;
 
+-- B.R.O. Moogle
+UPDATE npc_list SET name = 'Bro_Moogle', pos_rot = 22, pos_x = -19.1, pos_y = 0.000, pos_z = -1.2, status = 0, look = 0x00003E0900000000000000000000000000000000, widescan = 1 WHERE npcid = 17780901;
+
+-- ------------------------------------------------------------
+-- Upper Jeuno
+-- ------------------------------------------------------------
+
+-- B.R.O. Moogle
+UPDATE npc_list SET name = 'Bro_Moogle', pos_rot = 145, pos_x = -50.577, pos_y = 1.0, pos_z = 22.556, status = 0, look = 0x00003E0900000000000000000000000000000000, widescan = 1 WHERE npcid = 17776751;
+
+-- ------------------------------------------------------------
 -- Ru'Lude Gardens
+-- ------------------------------------------------------------
+
 -- Explorer Moogle NPC for Era's NM Hunt
 UPDATE npc_list SET pos_rot = 0, pos_x = -61.000, pos_y = 6.000, pos_z = -6.000, flag = 0, speed = 40, speedsub = 40, status = 0, look = 0x00003A0800000000000000000000000000000000 WHERE npcid = 17772772;
 
+-- Achieve Master for F.A.I.L. Badge Custom Quest
+UPDATE npc_list SET pos_rot = 235, pos_x = -14.000, pos_y = 1.000, pos_z = 131.000, status = 0, look = 0x0000790900000000000000000000000000000000, widescan = 0 WHERE npcid = 17772850;
+
+-- B.R.O. Moogle
+UPDATE npc_list SET name = 'Bro_Moogle', pos_rot = 0, pos_x = -70.8, pos_y = 5.591, pos_z = -7.947, status = 0, look = 0x00003E0900000000000000000000000000000000, widescan = 1 WHERE npcid = 17772673;
+
+-- ------------------------------------------------------------
+-- Aht Urghan Whitegate
+-- ------------------------------------------------------------
+
+-- B.R.O. Moogle
+UPDATE npc_list SET name = 'Bro_Moogle', pos_rot = 192, pos_x = 6.222, pos_y = 0.000, pos_z = 10.583, status = 0, look = 0x00003E0900000000000000000000000000000000, widescan = 1 WHERE npcid = 16982024;
+
+-- ------------------------------------------------------------
+-- Western Altepa Desert
+-- ------------------------------------------------------------
+
+-- Achieve Master for F.A.I.L. Badge Custom Quest
+UPDATE npc_list SET pos_rot = 90, pos_x = -211.614, pos_y = -15.270, pos_z = -14.192, status = 0, look = 0x0000790900000000000000000000000000000000, widescan = 0 WHERE npcid = 17289740;
+
+-- ------------------------------------------------------------
+-- Uleguerand Range
+-- ------------------------------------------------------------
+
+-- Achieve Master for F.A.I.L. Badge Custom Quest
+UPDATE npc_list SET pos_rot = 95, pos_x = 541.376, pos_y = -43.997, pos_z = 300.202, status = 0, look = 0x0000790900000000000000000000000000000000, widescan = 0 WHERE npcid = 16798101;
+
+-- ------------------------------------------------------------
+-- The Sanctuary of Zi'Tah
+-- ------------------------------------------------------------
+
+-- Achieve Master for F.A.I.L. Badge Custom Quest
+UPDATE npc_list SET pos_rot = 20, pos_x = 102.299, pos_y = -0.005, pos_z = -405.258, status = 0, look = 0x0000790900000000000000000000000000000000, widescan = 0 WHERE npcid = 17273355;
+
+-- ------------------------------------------------------------
+-- Ifrit's Cauldron
+-- ------------------------------------------------------------
+
+-- Achieve Master for F.A.I.L. Badge Custom Quest
+UPDATE npc_list SET pos_rot = 174, pos_x = 31.608, pos_y = 19.759, pos_z = -22.542, status = 0, look = 0x0000790900000000000000000000000000000000, widescan = 0 WHERE npcid = 17617198;
+
+-- ------------------------------------------------------------
+-- Castle Zvahl Keep
+-- ------------------------------------------------------------
+
+-- Achieve Master for F.A.I.L. Badge Custom Quest
+UPDATE npc_list SET pos_rot = 127, pos_x = -492.860, pos_y = -68.000, pos_z = 59.888, status = 0, look = 0x0000790900000000000000000000000000000000, widescan = 0 WHERE npcid = 17441012;
+
+-- ------------------------------------------------------------
 -- Provenance
+-- ------------------------------------------------------------
+
+-- Achieve Master for F.A.I.L. Badge Custom Quest
+UPDATE npc_list SET pos_rot = 48, pos_x = -631.087, pos_y = -20.425, pos_z = -462.653, status = 0, look = 0x0000790900000000000000000000000000000000, widescan = 0 WHERE npcid = 17686578;
+
 -- Need to add Provenance Crystal/Protocrystal as well as Pil in here but there are different IDs...?
-
-
