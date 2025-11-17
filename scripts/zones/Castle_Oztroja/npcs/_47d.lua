a4fa9895704742eed67cd45f404f90052f705454
slashtangent <steve50013@gmail.com>
2025-11-16 18:03:21 -0600

    Rework logic for obtaining the old ring in Castle Oztroja

--- scripts/zones/Castle_Oztroja/npcs/_47d.lua
+++ scripts/zones/Castle_Oztroja/npcs/_47d.lua
@@ -7,12 +7,19 @@
 local entity = {}
 
 entity.onTrigger = function(player, npc)
-    if not player:hasKeyItem(xi.ki.OLD_RING) then
-        npcUtil.giveKeyItem(player, xi.ki.OLD_RING)
-    end
-
-    if npc:getAnimation() == xi.anim.CLOSE_DOOR then
+    if
+        not player:hasCompletedQuest(xi.questLog.WINDURST, xi.quest.id.windurst.ONION_RINGS) and
+        not player:hasKeyItem(xi.ki.OLD_RING)
+    then
+        player:startCutscene(44, 0, xi.ki.OLD_RING)
+    elseif npc:getAnimation() == xi.anim.CLOSE_DOOR then
         npc:openDoor()
+    end
+end
+
+entity.onEventFinish = function(player, csid, option, npc)
+    if csid == 44 then
+        npcUtil.giveKeyItem(player, xi.ki.OLD_RING)
     end
 end
 
