8fd235d3cdacf28d4aaab4f5a4f956f25c552a4e
unknown <zlatirl1@gmail.com>
2025-12-05 11:55:57 +0000

    remove whitespace

--- settings/default/main.lua
+++ settings/default/main.lua
@@ -316,7 +316,7 @@ xi.settings.main =
     -- SYSTEM
     DISABLE_INACTIVITY_WATCHDOG = false, -- true/false. If this is enabled, the watchdog which detects if the main loop isn't being ticked will no longer be able to kill the process.
     INACTIVITY_WATCHDOG_PERIOD  = 2000,  -- Time in milliseconds which the inactivity watchdog will wait between ticks of the main loop before potentially killing the target process.
-    
+
     -- CUSTOM ERA EVENTS / CAMPAIGNS, 0 for Off, 1 for On
     -- OR Set os.time value to set "Event End Date" if ("TIMER") Implemented
     CHRISTMAS = 0, -- Jeuno Guide stone treats,
