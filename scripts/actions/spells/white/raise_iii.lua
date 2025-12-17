3c307a94a5064b961e53720b9c35a475e1727dfb
AlphieZ <Alphiez@users.noreply.github.com>
2025-12-16 23:18:19 -0500

    fix(spells): allow Raise spells to target raisable NPC allies

--- scripts/actions/spells/white/raise_iii.lua
+++ scripts/actions/spells/white/raise_iii.lua
@@ -1,15 +1,27 @@
 -----------------------------------
--- Spell: Raise
+-- Spell: Raise III
 -----------------------------------
 ---@type TSpell
 local spellObject = {}
+local BEHAVIOR_RAISABLE = 0x004
 
 spellObject.onMagicCastingCheck = function(caster, target, spell)
-    if
-        target:isAlive() or          -- Can't cast on alive targets.
-        target:hasRaiseTractorMenu() -- Raise and tractor menus both block the casting.
-    then
+    -- Can't cast on living targets
+    if target:isAlive() then
         return xi.msg.basic.MAGIC_CANNOT_BE_CAST
+    end
+
+    -- Only PCs should ever be blocked by the Raise/Tractor menu state
+    if target:isPC() and target:hasRaiseTractorMenu() then
+        return xi.msg.basic.MAGIC_CANNOT_BE_CAST
+    end
+
+    -- Non-PC targets must be explicitly raisable (behavior bit)
+    if not target:isPC() then
+        local behavior = target:getBehavior()
+        if bit.band(behavior, BEHAVIOR_RAISABLE) == 0 then
+            return xi.msg.basic.MAGIC_CANNOT_BE_CAST
+        end
     end
 
     return 0
@@ -19,17 +31,13 @@ spellObject.onSpellCast = function(caster, target, spell)
     if target:isPC() then
         target:sendRaise(3)
     else
-        if target:getName() == 'Prishe' then
-            -- CoP 8-4 Prishe
-            target:setLocalVar('Raise', 1)
-            target:entityAnimationPacket(xi.animationString.SPECIAL_00)
-            target:addHP(target:getMaxHP())
-            target:addMP(target:getMaxMP())
-        end
+        -- NPC ally "raise" behavior (instant revive style)
+        target:addHP(target:getMaxHP())
+        target:addMP(target:getMaxMP())
+        target:entityAnimationPacket(xi.animationString.SPECIAL_00)
     end
 
     spell:setMsg(xi.msg.basic.MAGIC_CASTS_ON)
-
     return 3
 end
 
