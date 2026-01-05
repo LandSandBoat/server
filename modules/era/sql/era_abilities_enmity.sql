0196c9464ba913ac35e2aaeba8c47ab6e4fcc4d5
ZenDiime <h4yw4rd@gmail.com>
2026-01-04 18:47:59 -0800

    Era: adjust CE/VE for era abilities

---
+++ modules/era/sql/era_abilities_enmity.sql
@@ -0,0 +1,15 @@
+-- ----------------------------------
+-- Era SpellAbilities Enmity Overrides
+-- CE = minimal (1), VE = era-accurate spike
+-- ----------------------------------
+
+UPDATE `abilities` SET CE = 1, VE = 900  WHERE `name` = 'Shield Bash';
+UPDATE `abilities` SET CE = 1, VE = 1800 WHERE `name` = 'Sentinel';
+UPDATE `abilities` SET CE = 1, VE = 80   WHERE `name` = 'Divine Seal';
+UPDATE `abilities` SET CE = 1, VE = 80   WHERE `name` = 'Elemental Seal';
+UPDATE `abilities` SET CE = 1, VE = 0    WHERE `name` = 'Cover';
+UPDATE `abilities` SET CE = 1, VE = 300  WHERE `name` = 'Rampart';
+UPDATE `abilities` SET CE = 1, VE = 300  WHERE `name` = 'Modus Veritas';
+UPDATE `abilities` SET CE = 1, VE = 80   WHERE `name` = 'Pianissimo';
+UPDATE `abilities` SET CE = 1, VE = 300  WHERE `name` = 'Divine Emblem';
+UPDATE `abilities` SET CE = 1, VE = 300  WHERE `name` = 'Nether Void';
