c6f4a96dae57ef60e9d23521adbbc18aee309477
Skold177 <113406182+Skold177@users.noreply.github.com>
2026-01-28 22:39:46 -0500

    [lua] Mandragora Skill Audit
    Updates Mandragora TP Moves with information from Jimmys sheet

--- scripts/actions/mobskills/leaf_dagger.lua
+++ scripts/actions/mobskills/leaf_dagger.lua
@@ -2,10 +2,9 @@
 --  Leaf Dagger
 --
 --  Description: Deals piercing damage to a single target. Additional effect: Poison
---  Type: Physical
---  Utsusemi/Blink absorb: 2 shadows
---  Range: Melee?
+--  Type: Physical (Ranged)
 --  Notes: On higher level Madragoras (Korrigans for example) the poision is 5hp/tick for about 5-6 ticks, damaging a total of 25-30 HP.
+--  TODO: Should be subject to ranged penalties.
 -----------------------------------
 ---@type TMobSkill
 local mobskillObject = {}
@@ -15,16 +14,17 @@ mobskillObject.onMobSkillCheck = function(target, mob, skill)
 end
 
 mobskillObject.onMobWeaponSkill = function(target, mob, skill)
-    local numhits = 2
-    local accmod = 1
-    local ftp    = 1.4
-    local info = xi.mobskills.mobPhysicalMove(mob, target, skill, numhits, accmod, ftp, xi.mobskills.physicalTpBonus.NO_EFFECT)
-    local dmg = xi.mobskills.mobFinalAdjustments(info.dmg, mob, skill, target, xi.attackType.PHYSICAL, xi.damageType.PIERCING, info.hitslanded)
-    local power = math.max(1, mob:getMainLvl() / 10)
-
-    xi.mobskills.mobPhysicalStatusEffectMove(mob, target, skill, xi.effect.POISON, power, 3, 18)
+    local numhits = 1
+    local accmod  = 1
+    local ftp     = 2
+    local info    = xi.mobskills.mobPhysicalMove(mob, target, skill, numhits, accmod, ftp, xi.mobskills.physicalTpBonus.NO_EFFECT)
+    local dmg     = xi.mobskills.mobFinalAdjustments(info.dmg, mob, skill, target, xi.attackType.PHYSICAL, xi.damageType.PIERCING, info.hitslanded)
+    local power   = math.max(1, mob:getMainLvl() / 10)
 
     target:takeDamage(dmg, mob, xi.attackType.PHYSICAL, xi.damageType.PIERCING)
+
+    xi.mobskills.mobPhysicalStatusEffectMove(mob, target, skill, xi.effect.POISON, power, 3, 90)
+
     return dmg
 end
 
