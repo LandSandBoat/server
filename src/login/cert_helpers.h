3bd89aaba72eaecc00bbfce916ac7c6938a2f87e
sruon <sruon@users.noreply.github.com>
2026-02-07 10:52:27 -0700

    Protect fclose for login.key

--- src/login/cert_helpers.h
+++ src/login/cert_helpers.h
@@ -41,8 +41,9 @@ inline void generateSelfSignedCert()
             {
                 ShowInfo(fmt::format("Found existing login.key"));
             }
+
+            fclose(fileHandle);
         }
-        fclose(fileHandle);
     }
 
     if (std::filesystem::exists("login.cert"))
