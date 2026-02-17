cc7a803e774e8f8e3d3ef3e1babfbaba076ace70
toshi <ku.small.soldier@gmail.com>
2026-02-17 23:32:22 +0900

    map設定: POST_ENABLED_ANYWHEREの既定値をtrueへ変更

--- settings/default/map.lua
+++ settings/default/map.lua
@@ -61,7 +61,7 @@ xi.settings.map =
 
     -- Allow use of delivery/post system in normally disallowed zones.
     -- Requires modules/custom/cpp/post_anywhere.cpp to be loaded.
-    POST_ENABLED_ANYWHERE = false,
+    POST_ENABLED_ANYWHERE = true,
 
     -- The total enmity cap for a given entity on the enmity table.
     -- 30,000 is believed to be approximately current retail cap.
