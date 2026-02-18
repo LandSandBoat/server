96b69b911b50446a1b27fa3dfe66bb5027a1520b
jsxxi <112220682+jsxxi@users.noreply.github.com>
2026-02-17 20:54:38 -0500

    Updated Indentation

--- scripts/commands/addallhp.lua
+++ scripts/commands/addallhp.lua
@@ -7,33 +7,33 @@ local commandObj = {}
 
 commandObj.cmdprops =
 {
-  permission = 1,
-  parameters = 's'
+    permission = 1,
+    parameters = 's'
 }
 
 local function error(player, msg)
-  player:printToPlayer(msg)
-  player:printToPlayer('!addallhp (player)')
+    player:printToPlayer(msg)
+    player:printToPlayer('!addallhp (player)')
 end
 
 commandObj.onTrigger = function(player, target)
 -- validate target
 local targ
 if target == nil then
-  targ = player
+    targ = player
 else
-  targ = GetPlayerByName(target)
-  if targ == nil then
-    error(player, string.format('Player named "%s" not found!', target))
-    return
-  end
+    targ = GetPlayerByName(target)
+    if targ == nil then
+        error(player, string.format('Player named "%s" not found!', target))
+        return
+    end
 end
 
 -- adds all Home Points
 for i = 0, 121, 1 do
-  local hpBit = i % 32
-  local hpSet = math.floor(i / 32)
-  targ:addTeleport(xi.teleport.type.HOMEPOINT, hpBit, hpSet)
+    local hpBit = i % 32
+    local hpSet = math.floor(i / 32)
+    targ:addTeleport(xi.teleport.type.HOMEPOINT, hpBit, hpSet)
 end
 
 player:printToPlayer(string.format('%s now has all Home Points.', targ:getName()))
--- scripts/commands/addallmounts.lua
+++ scripts/commands/addallmounts.lua
@@ -7,31 +7,31 @@ local commandObj = {}
 
 commandObj.cmdprops =
 {
-  permission = 1,
-  parameters = 's'
+    permission = 1,
+    parameters = 's'
 }
 
 local function error(player, msg)
-  player:printToPlayer(msg)
-  player:printToPlayer('!addallmounts (player)')
+    player:printToPlayer(msg)
+    player:printToPlayer('!addallmounts (player)')
 end
 
 commandObj.onTrigger = function(player, target)
 -- validate target
 local targ
 if target == nil then
-  targ = player
+    targ = player
 else
-  targ = GetPlayerByName(target)
-  if targ == nil then
-    error(player, string.format('Player named "%s" not found!', target))
-    return
-  end
+    targ = GetPlayerByName(target)
+    if targ == nil then
+        error(player, string.format('Player named "%s" not found!', target))
+        return
+    end
 end
 
 -- add all mount key items
 for i = 3072, 3108, 1 do
-  targ:addKeyItem(i)
+    targ:addKeyItem(i)
 end
 
 player:printToPlayer(string.format('%s now has all mounts.', targ:getName()))
