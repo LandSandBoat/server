790b85fca94558233ea13ee1d88fdfaef5a01853
jsxxi <112220682+jsxxi@users.noreply.github.com>
2026-02-17 20:46:29 -0500

    Updated formatting and spacing

--- scripts/commands/addallhp.lua
+++ scripts/commands/addallhp.lua
@@ -7,37 +7,36 @@ local commandObj = {}
 
 commandObj.cmdprops =
 {
-    permission = 1,
-    parameters = 's'
+  permission = 1,
+  parameters = 's'
 }
 
 local function error(player, msg)
-    player:printToPlayer(msg)
-    player:printToPlayer('!addallhp (player)')
+  player:printToPlayer(msg)
+  player:printToPlayer('!addallhp (player)')
 end
 
 commandObj.onTrigger = function(player, target)
-    -- validate target
-    local targ
-    if target == nil then
-        targ = player
-    else
-        targ = GetPlayerByName(target)
-        if targ == nil then
-            error(player, string.format('Player named "%s" not found!', target))
-            return
-        end
-    end
+-- validate target
+local targ
+if target == nil then
+  targ = player
+else
+  targ = GetPlayerByName(target)
+  if targ == nil then
+    error(player, string.format('Player named "%s" not found!', target))
+    return
+  end
+end
 
-    -- adds all Home Points
-    for i = 0, 121, 1 do
-        local hpBit = i % 32
-        local hpSet = math.floor(i / 32)
-        targ:addTeleport(xi.teleport.type.HOMEPOINT, hpBit, hpSet)
+-- adds all Home Points
+for i = 0, 121, 1 do
+  local hpBit = i % 32
+  local hpSet = math.floor(i / 32)
+  targ:addTeleport(xi.teleport.type.HOMEPOINT, hpBit, hpSet)
+end
 
-	end
-
-    player:printToPlayer(string.format('%s now has all Home Points.', targ:getName()))
+player:printToPlayer(string.format('%s now has all Home Points.', targ:getName()))
 end
 
 return commandObj
--- scripts/commands/addallsg.lua
+++ scripts/commands/addallsg.lua
@@ -4,49 +4,44 @@
 -----------------------------------
 ---@type TCommand
 require('scripts/globals/teleports')
-local survival = require('scripts/globals/teleports/survival_guide_map')
 xi = xi or {}
 xi.survivalGuide = xi.survivalGuide or {}
 local commandObj = {}
 
 commandObj.cmdprops =
 {
-    permission = 1,
-    parameters = 's'
+  permission = 1,
+  parameters = 's'
 }
 
 local function error(player, msg)
-    player:printToPlayer(msg)
-    player:printToPlayer('!addallsg (player)')
+  player:printToPlayer(msg)
+  player:printToPlayer('!addallsg (player)')
 end
 
 commandObj.onTrigger = function(player, target, zoneId, text, guide)
-    -- validate target
-    local targ
-    if target == nil then
-        targ = player
-    else
-        targ = GetPlayerByName(target)
-        if targ == nil then
-            error(player, string.format('Player named "%s" not found!', target))
-            return
-        end
-    end
+-- validate target
+local targ
+if target == nil then
+  targ = player
+else
+  targ = GetPlayerByName(target)
+  if targ == nil then
+    error(player, string.format('Player named "%s" not found!', target))
+    return
+  end
+end
 
-    -- adds all Survival Guides
-    for i = 1, 32, 1 do
-        
-		local groupIndex = i
-		local group = 1
-		
-		while( group ~= 4 ) do
-		targ:addTeleport(xi.teleport.type.SURVIVAL, groupIndex - 1, group - 1)
-		group = group+1
-		end
-		
-	end
-
-    player:printToPlayer(string.format('%s now has all Survival Guides.', targ:getName()))
+-- adds all Survival Guides
+for i = 1, 32, 1 do
+  local groupIndex = i
+  local group = 1
+  while( group ~= 4 ) do
+    targ:addTeleport(xi.teleport.type.SURVIVAL, groupIndex - 1, group - 1)
+    group = group+1
+  end
+end
+player:printToPlayer(string.format('%s now has all Survival Guides.', targ:getName()))
 end
 
 return commandObj
