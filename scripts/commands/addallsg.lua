6197aa4537f7663f6df918cfe2da394f36a00aa5
jsxxi <112220682+jsxxi@users.noreply.github.com>
2026-02-18 12:28:20 -0500

    whitespace and newline

--- scripts/commands/addallsg.lua
+++ scripts/commands/addallsg.lua
@@ -39,7 +39,6 @@ commandObj.onTrigger = function(player, target, zoneId, text, guide)
         while(group ~= 4) do
             targ:addTeleport(xi.teleport.type.SURVIVAL, groupIndex - 1, group - 1)
             group = group + 1
-        
 		end
     end
 
