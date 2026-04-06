bca3a53991f2d6a1762eb0c1658d93a63cec69a9
Frank <105882754+Frankie-hz@users.noreply.github.com>
2026-04-05 21:56:39 -0400

    [lua] Changed Leaping Lizzy Phs

--- scripts/zones/South_Gustaberg/mobs/Leaping_Lizzy.lua
+++ scripts/zones/South_Gustaberg/mobs/Leaping_Lizzy.lua
@@ -77,8 +77,8 @@ entity.spawnPoints =
 
 entity.phList =
 {
-    [ID.mob.LEAPING_LIZZY[1] - 1] = ID.mob.LEAPING_LIZZY[1], -- -275.441 20.451 -347.294
-    [ID.mob.LEAPING_LIZZY[2] - 1] = ID.mob.LEAPING_LIZZY[2], -- -322.871 30.052 -401.184
+    [ID.mob.LEAPING_LIZZY[1] - 1] = ID.mob.LEAPING_LIZZY[1], -- Confirmed on retail
+    [ID.mob.LEAPING_LIZZY[1] - 1] = ID.mob.LEAPING_LIZZY[2], -- Confirmed on retail
 }
 
 entity.onMobDeath = function(mob, player, optParams)
