2ff0766c49e8014bce51632973111e46a453dbb7
Crazyalextrebek <cwalke39@gmail.com>
2026-05-25 23:13:44 -0600

    [IDs] Updates Zone Message IDs for Zones A through J

--- scripts/zones/Crawlers_Nest_[S]/IDs.lua
+++ scripts/zones/Crawlers_Nest_[S]/IDs.lua
@@ -9,15 +9,15 @@ zones[xi.zone.CRAWLERS_NEST_S] =
     {
         CAMPAIGN_RESULTS_TALLIED      = 437,  -- Campaign results tallied.
         ITEM_CANNOT_BE_OBTAINED       = 6907, -- You cannot obtain the <item>. Come back after sorting your inventory.
-        ITEM_OBTAINED                 = 6915, -- Obtained: <item>.
-        GIL_OBTAINED                  = 6916, -- Obtained <number> gil.
-        KEYITEM_OBTAINED              = 6918, -- Obtained key item: <keyitem>.
-        CARRIED_OVER_POINTS           = 7526, -- You have carried over <number> login point[/s].
-        LOGIN_CAMPAIGN_UNDERWAY       = 7527, -- The [/January/February/March/April/May/June/July/August/September/October/November/December] <number> Login Campaign is currently underway!
-        LOGIN_NUMBER                  = 7528, -- In celebration of your most recent login (login no. <number>), we have provided you with <number> points! You currently have a total of <number> points.
-        MEMBERS_LEVELS_ARE_RESTRICTED = 7548, -- Your party is unable to participate because certain members' levels are restricted.
-        ITEM_DELIVERY_DIALOG          = 7699, -- Hello! Any packages to sendy-wend?
-        COMMON_SENSE_SURVIVAL         = 8664, -- It appears that you have arrived at a new survival guide provided by the Servicemen's Mutual Aid Network. Common sense dictates that you should now be able to teleport here from similar tomes throughout the world.
+        ITEM_OBTAINED                 = 6913, -- Obtained: <item>.
+        GIL_OBTAINED                  = 6914, -- Obtained <number> gil.
+        KEYITEM_OBTAINED              = 6916, -- Obtained key item: <keyitem>.
+        CARRIED_OVER_POINTS           = 7524, -- You have carried over <number> login point[/s].
+        LOGIN_CAMPAIGN_UNDERWAY       = 7525, -- The [/January/February/March/April/May/June/July/August/September/October/November/December] <number> Login Campaign is currently underway!
+        LOGIN_NUMBER                  = 7526, -- In celebration of your most recent login (login no. <number>), we have provided you with <number> points! You currently have a total of <number> points.
+        MEMBERS_LEVELS_ARE_RESTRICTED = 7546, -- Your party is unable to participate because certain members' levels are restricted.
+        ITEM_DELIVERY_DIALOG          = 7697, -- Hello! Any packages to sendy-wend?
+        COMMON_SENSE_SURVIVAL         = 8662, -- It appears that you have arrived at a new survival guide provided by the Servicemen's Mutual Aid Network. Common sense dictates that you should now be able to teleport here from similar tomes throughout the world.
     },
     mob =
     {
--- scripts/zones/Dynamis-San_dOria_[D]/IDs.lua
+++ scripts/zones/Dynamis-San_dOria_[D]/IDs.lua
@@ -8,16 +8,16 @@ zones[xi.zone.DYNAMIS_SAN_DORIA_D] =
     text =
     {
         ITEM_CANNOT_BE_OBTAINED       = 6385, -- You cannot obtain the <item>. Come back after sorting your inventory.
-        ITEM_OBTAINED                 = 6393, -- Obtained: <item>.
-        GIL_OBTAINED                  = 6394, -- Obtained <number> gil.
-        KEYITEM_OBTAINED              = 6396, -- Obtained key item: <keyitem>.
-        ITEMS_OBTAINED                = 6402, -- You obtain <number> <item>!
-        NOTHING_OUT_OF_ORDINARY       = 6407, -- There is nothing out of the ordinary here.
-        CARRIED_OVER_POINTS           = 7004, -- You have carried over <number> login point[/s].
-        LOGIN_CAMPAIGN_UNDERWAY       = 7005, -- The [/January/February/March/April/May/June/July/August/September/October/November/December] <number> Login Campaign is currently underway!
-        LOGIN_NUMBER                  = 7006, -- In celebration of your most recent login (login no. <number>), we have provided you with <number> points! You currently have a total of <number> points.
-        MEMBERS_LEVELS_ARE_RESTRICTED = 7026, -- Your party is unable to participate because certain members' levels are restricted.
-        CONQUEST_BASE                 = 7067, -- Tallying conquest results...
+        ITEM_OBTAINED                 = 6391, -- Obtained: <item>.
+        GIL_OBTAINED                  = 6392, -- Obtained <number> gil.
+        KEYITEM_OBTAINED              = 6394, -- Obtained key item: <keyitem>.
+        ITEMS_OBTAINED                = 6400, -- You obtain <number> <item>!
+        NOTHING_OUT_OF_ORDINARY       = 6405, -- There is nothing out of the ordinary here.
+        CARRIED_OVER_POINTS           = 7002, -- You have carried over <number> login point[/s].
+        LOGIN_CAMPAIGN_UNDERWAY       = 7003, -- The [/January/February/March/April/May/June/July/August/September/October/November/December] <number> Login Campaign is currently underway!
+        LOGIN_NUMBER                  = 7004, -- In celebration of your most recent login (login no. <number>), we have provided you with <number> points! You currently have a total of <number> points.
+        MEMBERS_LEVELS_ARE_RESTRICTED = 7024, -- Your party is unable to participate because certain members' levels are restricted.
+        CONQUEST_BASE                 = 7065, -- Tallying conquest results...
     },
     mob =
     {
