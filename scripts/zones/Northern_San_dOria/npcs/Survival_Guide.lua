d318811d9fe7c3d66d8a4b1fbf1bf495cebc9a08
greentchristian-LSB <green.tchristian@gmail.com>
2026-06-30 10:33:58 -0600

    Updated survival guide for updated retail tutorial
    Updated tutorial asks player to interact with a nation guide and an outpost guide. Added logic to advance tutorial when interacting with this guide.

--- scripts/zones/Northern_San_dOria/npcs/Survival_Guide.lua
+++ scripts/zones/Northern_San_dOria/npcs/Survival_Guide.lua
@@ -2,10 +2,13 @@
 -- Area: Northern San d'Oria
 --  NPC: Survival Guide
 -----------------------------------
+require('scripts/quests/tutorial')
+-----------------------------------
 ---@type TNpcEntity
 local entity = {}
 
 entity.onTrigger = function(player, targetNpc)
+    xi.tutorial.onGuideTriggerFirst(player)
     xi.survivalGuide.onTrigger(player)
 end
 
