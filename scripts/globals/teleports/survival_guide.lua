05f196899d8a09101c9ecaf37c1d451bcbf9baf3
greentchristian-LSB <green.tchristian@gmail.com>
2026-07-01 13:19:22 -0600

    Corrected formatting
    Replaced tab characters with 4x spaces, added newline to end of file.

--- scripts/globals/teleports/survival_guide.lua
+++ scripts/globals/teleports/survival_guide.lua
@@ -164,18 +164,18 @@ xi.survivalGuide.onEventFinish = function(player, eventId, option, npc)
                 local teleportCostGil  = 1000
                 local teleportCostTabs = 50
                 local canTeleport      = false
-				local tutorialStage    = player:getCharVar('TutorialProgress')
+			    local tutorialStage    = player:getCharVar('TutorialProgress')
 
                 -- When all mog tablets are found, survival guides are free.
                 local allMogTabletsFound = false -- TODO: Implement mog tablets and fetch if they are all found here.
                 if allMogTabletsFound then
                     teleportCostGil  = 0
                     teleportCostTabs = 0
-				
-				-- Free teleport during tutorial survival guide objective.
-				elseif tutorialStage == 13 then
-					teleportCostGil  = 0
-					teleportCostTabs = 0
+
+                -- Free teleport during tutorial survival guide objective.
+                elseif tutorialStage == 13 then
+                    teleportCostGil  = 0
+                    teleportCostTabs = 0
 
                 -- If the player has the "Rhapsody in White" KI, the cost is 10% of original gil or 20% of original tabs.
                 elseif player:hasKeyItem(xi.ki.RHAPSODY_IN_WHITE) then
@@ -199,9 +199,9 @@ xi.survivalGuide.onEventFinish = function(player, eventId, option, npc)
 
                 if canTeleport then
                     player:setPos(guide.posX, guide.posY, guide.posZ, guide.posRot, guide.zoneId)
-					if tutorialStage == 13 then
-						player:setCharVar('TutorialProgress', 14)	-- Prevents infinite free teleports by not finishing tutorial.
-					end
+                    if tutorialStage == 13 then
+                        player:setCharVar('TutorialProgress', 14)    -- Prevents infinite free teleports by not finishing tutorial.
+                    end
                 end
             end
         end
