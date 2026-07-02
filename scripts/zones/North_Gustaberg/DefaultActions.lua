69f51e3ea461e5cad8d21a76f178d0444979d8de
WinterSolstice8 <60417494+wintersolstice8@users.noreply.github.com>
2026-07-02 17:24:18 -0600

    [lua] Set qm2 default action in North Gustaberg

--- scripts/zones/North_Gustaberg/DefaultActions.lua
+++ scripts/zones/North_Gustaberg/DefaultActions.lua
@@ -4,5 +4,6 @@ return {
     ['Field_Parchment'] = { event = 2001 },
     ['Hunting_Bear']    = { event = 20 },
     ['Stone_Monument']  = { event = 900 },
+    ['qm2']             = { messageSpecial = ID.text.NOTHING_OUT_OF_ORDINARY },
     ['Waterfall_Base']  = { messageSpecial = ID.text.REACH_WATER_FROM_HERE },
 }
