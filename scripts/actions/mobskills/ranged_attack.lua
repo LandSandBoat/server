782a25b5ab0b8d4a4014637784ee0d22308776ff
Skold <113406182+Skold177@users.noreply.github.com>
2026-07-05 22:48:59 -0400

    Reduce Ranged Attack Damage
    Reduces the fTP of Ranged Attack to 1.0 from 1.5

--- scripts/actions/mobskills/ranged_attack.lua
+++ scripts/actions/mobskills/ranged_attack.lua
@@ -18,7 +18,7 @@ mobskillObject.onMobWeaponSkill = function(mob, target, skill, action)
 
     params.baseDamage     = mob:getWeaponDmg()
     params.numHits        = 1
-    params.fTP            = { 1.5, 1.5, 1.5 } -- TODO: Mobs get more base damage on their ranged weapon slot already. Do we need the 1.5 fTP?
+    params.fTP            = { 1.0, 1.0, 1.0 }
     params.attackType     = xi.attackType.RANGED
     params.damageType     = xi.damageType.PIERCING
     params.shadowBehavior = xi.mobskills.shadowBehavior.NUMSHADOWS_1
