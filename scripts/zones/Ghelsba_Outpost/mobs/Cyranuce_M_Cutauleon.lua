18bd8fb86018d5918c178f6e8e94218a2f3b29f7
Kipling <kiplingffxi@gmail.com>
2026-07-06 22:19:52 -0700

    DRG Job Quest NM Audits

--- scripts/zones/Ghelsba_Outpost/mobs/Cyranuce_M_Cutauleon.lua
+++ scripts/zones/Ghelsba_Outpost/mobs/Cyranuce_M_Cutauleon.lua
@@ -9,6 +9,8 @@ local entity = {}
 entity.onMobInitialize = function(mob)
     mob:addImmunity(xi.immunity.LIGHT_SLEEP)
     mob:addImmunity(xi.immunity.DARK_SLEEP)
+    mob:addImmunity(xi.immunity.SILENCE)
+    mob:addImmunity(xi.immunity.TERROR)
 end
 
 entity.onMobMobskillChoose = function(mob, target, skillId)
