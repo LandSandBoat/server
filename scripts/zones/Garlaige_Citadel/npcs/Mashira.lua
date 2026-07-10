2ff4e39008035dd712cecfad3364909e9c7832cd
Skold <113406182+Skold177@users.noreply.github.com>
2026-07-09 20:25:05 -0400

    Convert Rubbish Day to IF / Fix Compatability Events
    Converts Rubbish Day to IF / Fix Compatibility Events

--- scripts/zones/Garlaige_Citadel/npcs/Mashira.lua
+++ scripts/zones/Garlaige_Citadel/npcs/Mashira.lua
@@ -8,12 +8,7 @@
 local entity = {}
 
 entity.onTrigger = function(player, npc)
-    if
-        player:getQuestStatus(xi.questLog.JEUNO, xi.quest.id.jeuno.RUBBISH_DAY) == xi.questStatus.QUEST_ACCEPTED and
-        player:getCharVar('RubbishDayVar') == 0
-    then
-        player:startEvent(11, 1) -- For the quest "Rubbish day"
-    elseif player:getQuestStatus(xi.questLog.WINDURST, xi.quest.id.windurst.MAKING_AMENS) == xi.questStatus.QUEST_ACCEPTED then
+    if player:getQuestStatus(xi.questLog.WINDURST, xi.quest.id.windurst.MAKING_AMENS) == xi.questStatus.QUEST_ACCEPTED then
         if player:hasKeyItem(xi.ki.BROKEN_WAND) then
             player:startEvent(11, 3)
         else player:startEvent(11, 0) -- Making Amens dialogue
@@ -24,19 +19,10 @@ entity.onTrigger = function(player, npc)
 end
 
 entity.onEventFinish = function(player, csid, option, npc)
-    local rubbishDay = player:getQuestStatus(xi.questLog.JEUNO, xi.quest.id.jeuno.RUBBISH_DAY)
-    local makingAmens = player:getQuestStatus(xi.questLog.WINDURST, xi.quest.id.windurst.MAKING_AMENS)
     if
         csid == 11 and
-        option == 1 and
-        rubbishDay == xi.questStatus.QUEST_ACCEPTED
-    then
-        player:delKeyItem(xi.ki.MAGIC_TRASH)
-        player:setCharVar('RubbishDayVar', 1)
-    elseif
-        csid == 11 and
         option == 0 and
-        makingAmens == xi.questStatus.QUEST_ACCEPTED
+        player:getQuestStatus(xi.questLog.WINDURST, xi.quest.id.windurst.MAKING_AMENS) == xi.questStatus.QUEST_ACCEPTED
     then
         npcUtil.giveKeyItem(player, xi.ki.BROKEN_WAND)
         player:tradeComplete()
