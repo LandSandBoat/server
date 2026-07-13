2ead23a55231c29021a278d4751a14db42c4215d
Kipling <kiplingffxi@gmail.com>
2026-07-13 08:06:18 -0700

    Sandy 6-2 Audits

--- scripts/zones/King_Ranperres_Tomb/mobs/Corrupted_Soffeil.lua
+++ scripts/zones/King_Ranperres_Tomb/mobs/Corrupted_Soffeil.lua
@@ -7,12 +7,31 @@ local entity = {}
 
 entity.onMobInitialize = function(mob)
     mob:setMobMod(xi.mobMod.IDLE_DESPAWN, 180)
-    mob:setMod(xi.mod.SLEEP_MEVA, 50)
-    mob:setMod(xi.mod.LULLABY_MEVA, 50)
+    mob:addImmunity(xi.immunity.DARK_SLEEP)
+    mob:addImmunity(xi.immunity.LIGHT_SLEEP)
+    mob:setMobMod(xi.mobMod.NO_STANDBACK, 1)
 end
 
-entity.onMobSpawn = function(mob)
-    DespawnMob(mob:getID(), 180)
+entity.onMobSpellChoose = function(mob, target, spellId)
+    local spellList =
+    {
+        [ 1] = { xi.magic.spell.ICE_SPIKES,  mob,    false, xi.action.type.ENHANCING_FORCE_SELF, xi.effect.ICE_SPIKES, 0, 100 },
+        [ 2] = { xi.magic.spell.DRAIN,       target, false, xi.action.type.DRAIN_HP,             nil,                  0, 100 },
+        [ 3] = { xi.magic.spell.STUN,        target, false, xi.action.type.ENFEEBLING_TARGET,    xi.effect.STUN,       0, 100 },
+        [ 4] = { xi.magic.spell.BIND,        target, false, xi.action.type.ENFEEBLING_TARGET,    xi.effect.BIND,       0, 100 },
+        [ 5] = { xi.magic.spell.GRAVITY,     target, false, xi.action.type.ENFEEBLING_TARGET,    xi.effect.WEIGHT,     0, 100 },
+        [ 6] = { xi.magic.spell.BIO_II,      target, false, xi.action.type.ENFEEBLING_TARGET,    xi.effect.BIO,        4, 100 },
+        [ 7] = { xi.magic.spell.RASP,        target, false, xi.action.type.ENFEEBLING_TARGET,    xi.effect.RASP,       0, 100 },
+        [ 8] = { xi.magic.spell.SLEEP,       target, false, xi.action.type.ENFEEBLING_TARGET,    xi.effect.SLEEP_I,    1,  25 },
+        [ 9] = { xi.magic.spell.SLEEPGA,     target, false, xi.action.type.ENFEEBLING_TARGET,    xi.effect.SLEEP_I,    1,  25 },
+        [10] = { xi.magic.spell.SLEEP_II,    target, false, xi.action.type.ENFEEBLING_TARGET,    xi.effect.SLEEP_I,    2, 100 },
+        [11] = { xi.magic.spell.SLEEPGA_II,  target, false, xi.action.type.ENFEEBLING_TARGET,    xi.effect.SLEEP_I,    2, 100 },
+        [12] = { xi.magic.spell.STONEGA_III, target, false, xi.action.type.DAMAGE_TARGET,        nil,                  0, 100 },
+        [13] = { xi.magic.spell.THUNDAGA_II, target, false, xi.action.type.DAMAGE_TARGET,        nil,                  0, 100 },
+        [14] = { xi.magic.spell.TORNADO,     target, false, xi.action.type.DAMAGE_TARGET,        nil,                  0, 100 },
+    }
+
+    return xi.combat.behavior.chooseAction(mob, target, nil, spellList)
 end
 
 return entity
