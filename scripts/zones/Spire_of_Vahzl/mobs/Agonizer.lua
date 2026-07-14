8d194cb07313a071eb35fa3bafc1919c7fb0d1d6
Skold177 <113406182+Skold177@users.noreply.github.com>
2026-07-14 11:37:57 -0400

    Harden Promy Vahzl Boss Engage
    Adds a check in death to make sure it fires if the target is killed instantly.

--- scripts/zones/Spire_of_Vahzl/mobs/Agonizer.lua
+++ scripts/zones/Spire_of_Vahzl/mobs/Agonizer.lua
@@ -5,6 +5,25 @@
 ---@type TMobEntity
 local entity = {}
 
+local function engageNextMob(mob, target)
+    if not target then
+        return
+    end
+
+    local nextMob = GetMobByID(mob:getID() + 6) -- Cumulator
+
+    if not nextMob then
+        return
+    end
+
+    if
+        nextMob:isAlive() and
+        not nextMob:isEngaged()
+    then
+        nextMob:updateEnmity(target)
+    end
+end
+
 entity.onMobInitialize = function(mob)
     mob:addImmunity(xi.immunity.DARK_SLEEP)
     mob:addImmunity(xi.immunity.LIGHT_SLEEP)
@@ -44,11 +63,14 @@ entity.onMobFight = function(mob, target)
         mob:setMod(xi.mod.REGAIN, 100)
     end
 
-    if mob:getHPP() < 20 then
-        local nextMob = GetMobByID(mob:getID() + 6) --Cumulator aggros at <20%
-        if nextMob and not nextMob:isEngaged() then
-            nextMob:updateEnmity(target)
-        end
+    if mob:getHPP() < 20 then -- Cumulator engages < 20% HP.
+        engageNextMob(mob, target)
+    end
+end
+
+entity.onMobDeath = function(mob, player, optParams)
+    if optParams.isKiller or optParams.noKiller then
+        engageNextMob(mob, player)
     end
 end
 
