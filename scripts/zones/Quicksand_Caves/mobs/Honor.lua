b44c56f9f3edc4e1de1b9568f383a67e6434a96c
Kipling <kiplingffxi@gmail.com>
2026-07-16 02:19:55 -0700

    Sandy 8-1 Audits

--- scripts/zones/Quicksand_Caves/mobs/Honor.lua
+++ scripts/zones/Quicksand_Caves/mobs/Honor.lua
@@ -13,4 +13,19 @@ entity.onMobInitialize = function(mob)
     mob:setMod(xi.mod.SILENCE_RES_RANK, 8)
 end
 
+entity.onMobSpawn = function(mob)
+    mob:setMobMod(xi.mobMod.BASE_DAMAGE_MULTIPLIER, 150)
+    mob:setMod(xi.mod.ACC, 150)
+end
+
+entity.onMobSpellChoose = function(mob, target, spellId)
+    local spellList =
+    {
+        [1] = { xi.magic.spell.PARALYGA,  target, false, xi.action.type.ENFEEBLING_TARGET, xi.effect.PARALYSIS, 0, 100 },
+        [2] = { xi.magic.spell.SILENCEGA, target, false, xi.action.type.ENFEEBLING_TARGET, xi.effect.SILENCE,   0, 100 },
+    }
+
+    return xi.combat.behavior.chooseAction(mob, target, nil, spellList)
+end
+
 return entity
--- scripts/zones/Quicksand_Caves/mobs/Valor.lua
+++ scripts/zones/Quicksand_Caves/mobs/Valor.lua
@@ -15,4 +15,17 @@ entity.onMobInitialize = function(mob)
     mob:setMod(xi.mod.SILENCE_RES_RANK, 8)
 end
 
+entity.onMobSpawn = function(mob)
+    mob:setMobMod(xi.mobMod.BASE_DAMAGE_MULTIPLIER, 150)
+    mob:setMod(xi.mod.ACC, 150)
+
+    -- Valor uses Hundred Fists despite the WAR main job, so force it rather than the job 2hr.
+    xi.mix.jobSpecial.config(mob, {
+        specials =
+        {
+            { id = xi.mobSkill.HUNDRED_FISTS_1 },
+        },
+    })
+end
+
 return entity
