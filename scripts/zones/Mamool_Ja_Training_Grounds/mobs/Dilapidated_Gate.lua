7589f2e3313e6e85fc099b2887f97976c9f1426a
Joshua Hughes <joshua.hughesfl@gmail.com>
2026-07-17 11:17:47 -0400

    Derive Imperial Agent Rescue entity IDs

--- scripts/zones/Mamool_Ja_Training_Grounds/mobs/Dilapidated_Gate.lua
+++ scripts/zones/Mamool_Ja_Training_Grounds/mobs/Dilapidated_Gate.lua
@@ -25,13 +25,14 @@ entity.onMobDeath = function(mob, player, optParams)
     end
 
     local mobId = mob:getID()
+    local gates = ID.mob[xi.assault.mission.IMPERIAL_AGENT_RESCUE].GATES
     local doorId
 
-    if mobId == ID.mob[xi.assault.mission.IMPERIAL_AGENT_RESCUE].MOBS_START.GATE_1 then
+    if mobId == gates[1] then
         doorId = ID.npc.DOOR_1
-    elseif mobId == ID.mob[xi.assault.mission.IMPERIAL_AGENT_RESCUE].MOBS_START.GATE_2 then
+    elseif mobId == gates[2] then
         doorId = ID.npc.DOOR_2
-    elseif mobId == ID.mob[xi.assault.mission.IMPERIAL_AGENT_RESCUE].MOBS_START.GATE_3 then
+    elseif mobId == gates[3] then
         doorId = ID.npc.DOOR_3
     end
 
