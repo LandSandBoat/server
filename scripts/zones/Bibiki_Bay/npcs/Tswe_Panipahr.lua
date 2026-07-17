da420bb0800567821f9bdf69934a1c6f073929a1
Hatberg <46464306+Hatberg@users.noreply.github.com>
2026-07-17 19:02:14 +0200

    [lua] NPC Tswe Panipahr (Manaclipper) cleanup

--- scripts/zones/Bibiki_Bay/npcs/Tswe_Panipahr.lua
+++ scripts/zones/Bibiki_Bay/npcs/Tswe_Panipahr.lua
@@ -10,30 +10,48 @@ local ID = zones[xi.zone.BIBIKI_BAY]
 local entity = {}
 
 entity.onTrigger = function(player, npc)
-    local curentticket = 0
-    if  player:hasKeyItem(xi.ki.MANACLIPPER_TICKET) then
-        curentticket = xi.ki.MANACLIPPER_TICKET
-    elseif player:hasKeyItem(xi.ki.MANACLIPPER_MULTI_TICKET) then
-        curentticket = xi.ki.MANACLIPPER_MULTI_TICKET
-    end
-
-    if curentticket ~= 0 then
-        player:messageSpecial(ID.text.HAVE_BILLET, curentticket)
-    else
-        local gil = player:getGil()
-        player:startEvent(35, xi.ki.MANACLIPPER_TICKET, xi.ki.MANACLIPPER_MULTI_TICKET , 80, gil, 0, 500)
-    end
+    player:startEvent(35, xi.ki.MANACLIPPER_TICKET, xi.ki.MANACLIPPER_MULTI_TICKET, 80, player:getGil(), 0, 500)
 end
 
 entity.onEventFinish = function(player, csid, option, npc)
-    if csid == 35 then
-        if option == 1 then
+    if csid ~= 35 then
+        return
+    end
+
+    local playerGil = player:getGil()
+
+    -- Buy single.
+    if option == 1 then
+        if player:hasKeyItem(xi.ki.MANACLIPPER_TICKET) then
+            player:messageSpecial(ID.text.HAVE_BILLET, xi.ki.MANACLIPPER_TICKET)
+            return
+        end
+
+        if playerGil >= 80 then
             player:delGil(80)
             npcUtil.giveKeyItem(player, xi.ki.MANACLIPPER_TICKET)
-        elseif option == 2 then
-            player:delGil(500)
-            npcUtil.giveKeyItem(player, xi.ki.MANACLIPPER_MULTI_TICKET)
-            player:setCharVar('Manaclipper_Ticket', 10)
+        end
+
+    -- Buy multi.
+    elseif option == 2 then
+        if player:hasKeyItem(xi.ki.MANACLIPPER_MULTI_TICKET) then
+            local manaclipperTicketAmount = player:getCharVar('Manaclipper_Ticket')
+            if manaclipperTicketAmount == 10 then
+                player:messageSpecial(ID.text.NO_NEED_TO_PURCHASE)
+                return
+            end
+
+            if playerGil >= 500 then
+                player:delGil(500)
+                player:messageSpecial(ID.text.NUM_TICKETS_ADDED_TO_MULTI, xi.ki.MANACLIPPER_MULTI_TICKET, 10 - manaclipperTicketAmount)
+                player:setCharVar('Manaclipper_Ticket', 10)
+            end
+        else
+            if playerGil >= 500 then
+                player:delGil(500)
+                npcUtil.giveKeyItem(player, xi.ki.MANACLIPPER_MULTI_TICKET)
+                player:setCharVar('Manaclipper_Ticket', 10)
+            end
         end
     end
 end
