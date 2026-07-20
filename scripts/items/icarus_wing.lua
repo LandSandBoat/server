498009c347e114cb0234ba8449dbb52db78dbb7f
Skold <113406182+Skold177@users.noreply.github.com>
2026-07-20 19:05:46 -0400

    Give Icarus Wing Store TP scaling
    Found on the JP Wiki

--- scripts/items/icarus_wing.lua
+++ scripts/items/icarus_wing.lua
@@ -2,6 +2,7 @@
 -- ID: 4213
 -- Icarus Wing
 -- Increases TP of the user by 1000
+-- Modified by Store TP. https://wiki.ffo.jp/html/3809.html
 -----------------------------------
 ---@type TItem
 local itemObject = {}
@@ -15,7 +16,9 @@ itemObject.onItemCheck = function(target, item, caster)
 end
 
 itemObject.onItemUse = function(target, user)
-    target:addTP(1000)
+    local storeTPModifier = 1 + (target:getMod(xi.mod.STORETP) + target:getMerit(xi.merit.STORE_TP_EFFECT)) / 100
+
+    target:addTP(math.floor(1000 * storeTPModifier))
     target:addStatusEffect(xi.effect.MEDICINE, { duration = 7200, origin = user })
 end
 
