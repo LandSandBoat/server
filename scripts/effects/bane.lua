5a799eee71e165614d5d3b11799cae81f6449a79
Skold177 <113406182+Skold177@users.noreply.github.com>
2026-07-20 14:45:23 -0400

    Update Bane to use effect:addMod
    Players no longer get double speed down

--- scripts/effects/bane.lua
+++ scripts/effects/bane.lua
@@ -6,17 +6,14 @@ local effectObject = {}
 
 effectObject.onEffectGain = function(target, effect)
     --NOTE: The power amount dictates the amount to REDUCE MAX VALUES BY. E.g. Power=75 means 'reduce max hp/mp by 75%'
-    target:addMod(xi.mod.CURSE_PCT, -effect:getPower())
-    target:addMod(xi.mod.MOVE_SPEED_WEIGHT_PENALTY, effect:getPower())
+    effect:addMod(xi.mod.CURSE_PCT, -effect:getPower())
+    effect:addMod(xi.mod.MOVE_SPEED_WEIGHT_PENALTY, effect:getPower())
 end
 
 effectObject.onEffectTick = function(target, effect)
 end
 
 effectObject.onEffectLose = function(target, effect)
-    --restore HP and MP to its former state.
-    target:delMod(xi.mod.CURSE_PCT, -effect:getPower())
-    target:addMod(xi.mod.MOVE_SPEED_WEIGHT_PENALTY, effect:getPower())
 end
 
 return effectObject
