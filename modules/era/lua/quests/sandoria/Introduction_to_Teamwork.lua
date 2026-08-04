eb64f7a103b710bb20d75326d58694d02a8b91d8
Kipling <kiplingffxi@gmail.com>
2026-08-03 23:46:12 -0700

    Teamwork Quest Modules

---
+++ modules/era/lua/quests/sandoria/Introduction_to_Teamwork.lua
@@ -0,0 +1,58 @@
+-----------------------------------
+-- Introduction to Teamwork
+-- Reverts the required number of same-nation party members to six.
+-- The May 14, 2015 version update reduced the requirement to two.
+-----------------------------------
+-- Source: https://forum.square-enix.com/ffxi/threads/46976
+-----------------------------------
+require('modules/module_utils')
+-----------------------------------
+local moduleName = 'era_quest_introduction_to_teamwork'
+
+if xi.module.isContentEnabled('ROV') then
+    return { name = moduleName }
+end
+
+local m = Module:new(moduleName)
+
+m:addOverride('xi.server.onServerStart', function()
+    super()
+
+    xi.module.modifyInteractionEntry('scripts/quests/sandoria/Introduction_to_Teamwork', function(quest)
+        -- Copy of the base handler with partySizeRequirement raised to 6
+        quest.sections[2][xi.zone.WEST_RONFAURE].onEventUpdate[129] = function(player, csid, option, npc)
+            local partySizeRequirement = 6
+            local party = player:getParty()
+            local partySameNationCount = 0
+
+            if #party >= partySizeRequirement then
+                for key, member in pairs(party) do
+                    if
+                        member:getZoneID() ~= player:getZoneID() or
+                        member:checkDistance(player) > 15
+                    then
+                        player:updateEvent(1)
+                        return
+                    else
+                        if member:getNation() == player:getNation() then
+                            partySameNationCount = partySameNationCount + 1
+                        end
+                    end
+                end
+            else
+                player:updateEvent(1)
+                return
+            end
+
+            if partySameNationCount >= partySizeRequirement then
+                quest:setLocalVar(player, 'Prog', 1)
+                player:updateEvent(15, 1)
+                return
+            else
+                player:updateEvent(3, 1)
+            end
+        end
+    end)
+end)
+
+return m
