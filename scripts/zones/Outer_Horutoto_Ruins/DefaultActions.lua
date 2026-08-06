6ac5b8ec08ca6c5bd282669a7672694d63b3c19e
Nobutadas <nobutadas.one@gmail.com>
2026-08-06 08:10:33 -0500

    [lua][sql] Windurst 8-2 audit

--- scripts/zones/Outer_Horutoto_Ruins/DefaultActions.lua
+++ scripts/zones/Outer_Horutoto_Ruins/DefaultActions.lua
@@ -1,7 +1,8 @@
 local ID = zones[xi.zone.OUTER_HORUTOTO_RUINS]
 
 return {
-    ['_5e9']  = { text = ID.text.DOOR_FIRMLY_SHUT },
+    ['_5e5'] = { messageSpecial = ID.text.HIDDEN_DOOR_FIRMLY_SHUT },
+    ['_5e9'] = { text = ID.text.DOOR_FIRMLY_SHUT },
     ['_5eb'] = { messageSpecial = ID.text.DOOR_FIRMLY_SHUT },
-    ['qm1'] = { messageSpecial = ID.text.NOTHING_OUT_OF_ORDINARY },
+    ['qm1']  = { messageSpecial = ID.text.NOTHING_OUT_OF_ORDINARY },
 }
