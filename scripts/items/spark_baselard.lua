9b1657d69820553b6c9254658221dd84e483e239
WinterSolstice8 <60417494+wintersolstice8@users.noreply.github.com>
2026-08-06 14:52:31 -0600

    [lua] [sql] Implement Spark weapons

---
+++ scripts/items/spark_baselard.lua
@@ -0,0 +1,31 @@
+-----------------------------------
+-- ID: 17986
+-- Item: Spark Baselard
+-- Additional effect: lightning damage depending on ammo
+-----------------------------------
+---@type TItem
+local itemObject = {}
+
+itemObject.onItemAdditionalEffect = function(actor, target, baseAttackDamage, item)
+    local damageRange = xi.additionalEffect.getConsumableAmmoItemDamageRange(actor:getEquipID(xi.slot.AMMO))
+
+    if not damageRange then
+        return 0, 0, 0
+    end
+
+    local pTable =
+    {
+        chance          = 10, -- TODO: get data on Daggers. This rate is from h2h (hydro patas)
+        basePower       = math.randomInt(damageRange[1], damageRange[2]),
+        attackType      = xi.attackType.MAGICAL,
+        magicalElement  = xi.element.THUNDER,
+        canMAB          = false,
+        canResist       = true,
+        lowestResist    = 0.5,
+        useAmmo         = true,
+    }
+
+    return xi.combat.action.executeAddEffectDamage(actor, target, pTable)
+end
+
+return itemObject
---
+++ scripts/items/spark_spear_+1.lua
@@ -0,0 +1,31 @@
+-----------------------------------
+-- ID: 18077
+-- Item: Spark Spear +1
+-- Additional effect: lightning damage depending on ammo
+-----------------------------------
+---@type TItem
+local itemObject = {}
+
+itemObject.onItemAdditionalEffect = function(actor, target, baseAttackDamage, item)
+    local damageRange = xi.additionalEffect.getConsumableAmmoItemDamageRange(actor:getEquipID(xi.slot.AMMO))
+
+    if not damageRange then
+        return 0, 0, 0
+    end
+
+    local pTable =
+    {
+        chance          = 20,
+        basePower       = math.randomInt(damageRange[1], damageRange[2]),
+        attackType      = xi.attackType.MAGICAL,
+        magicalElement  = xi.element.THUNDER,
+        canMAB          = false,
+        canResist       = true,
+        lowestResist    = 0.5,
+        useAmmo         = true,
+    }
+
+    return xi.combat.action.executeAddEffectDamage(actor, target, pTable)
+end
+
+return itemObject
