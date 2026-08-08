c51a0a8e171882239ab2a647bdad8f0c3769f36d
Skold <113406182+Skold177@users.noreply.github.com>
2026-08-08 00:04:10 -0400

    75 Era Silent Oil / Prism Powder Duration Module
    Adds the old randomized times back to these items.

---
+++ modules/era/lua/items/flask_of_deodorizer.lua
@@ -0,0 +1,23 @@
+-----------------------------------
+-- Restores the random Deodorize duration (1 minute 30 seconds to 6 minutes).
+-- Changed December 7th, 2010 : https://www.playonline.com/pcd/verup/ff11/detail/6024/detail.html
+-----------------------------------
+require('modules/module_utils')
+-----------------------------------
+local moduleName = 'era_deodorizer_duration'
+
+if xi.module.isContentEnabled('ABYSSEA') then
+    return { name = moduleName }
+end
+
+local m = Module:new(moduleName)
+
+m:addOverride('xi.items.flask_of_deodorizer.onItemUse', function(target, user)
+    if not target:hasStatusEffect(xi.effect.DEODORIZE) then
+        target:addStatusEffect(xi.effect.DEODORIZE, { power = 1, duration = math.random(90, 360), origin = user, tick = 10 })
+    else
+        target:messageBasic(xi.msg.basic.NO_EFFECT)
+    end
+end)
+
+return m
---
+++ modules/era/lua/items/pinch_of_rainbow_powder.lua
@@ -0,0 +1,23 @@
+-----------------------------------
+-- Restores the fixed 3 minute Invisible duration.
+-- Changed December 7th, 2010 : https://www.playonline.com/pcd/verup/ff11/detail/6024/detail.html
+-----------------------------------
+require('modules/module_utils')
+-----------------------------------
+local moduleName = 'era_rainbow_powder_duration'
+
+if xi.module.isContentEnabled('ABYSSEA') then
+    return { name = moduleName }
+end
+
+local m = Module:new(moduleName)
+
+m:addOverride('xi.items.pinch_of_rainbow_powder.onItemUse', function(target, user)
+    if target:hasStatusEffect(xi.effect.INVISIBLE) then
+        target:delStatusEffect(xi.effect.INVISIBLE)
+    end
+
+    target:addStatusEffect(xi.effect.INVISIBLE, { power = 1, duration = math.floor(180 * xi.settings.main.SNEAK_INVIS_DURATION_MULTIPLIER), origin = user, tick = 10 })
+end)
+
+return m
