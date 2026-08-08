a0a6455897f0023d59513790e2eff44a1ece1fc0
Skold <113406182+Skold177@users.noreply.github.com>
2026-08-08 01:35:56 -0400

    Add Free Alzadaal Entry with Remnants Permit
    Adds Free Alzadaal Entry with Remnants Permit

--- scripts/zones/Caedarva_Mire/npcs/Nasheefa.lua
+++ scripts/zones/Caedarva_Mire/npcs/Nasheefa.lua
@@ -26,6 +26,9 @@ entity.onTrigger = function(player, npc)
         if player:hasKeyItem(xi.ki.CAPTAIN_WILDCAT_BADGE) then
             player:messageSpecial(ID.text.YOU_HAVE_A_BADGE, xi.ki.CAPTAIN_WILDCAT_BADGE)
             player:startEvent(183)
+        elseif player:hasKeyItem(xi.ki.REMNANTS_PERMIT) then
+            player:messageSpecial(ID.text.YOU_HAVE_A_BADGE, xi.ki.REMNANTS_PERMIT)
+            player:startEvent(183)
         else
             player:startEvent(182)
         end
--- scripts/zones/Caedarva_Mire/npcs/Nuimahn.lua
+++ scripts/zones/Caedarva_Mire/npcs/Nuimahn.lua
@@ -26,6 +26,9 @@ entity.onTrigger = function(player, npc)
         if player:hasKeyItem(xi.ki.CAPTAIN_WILDCAT_BADGE) then -- entering
             player:messageSpecial(ID.text.YOU_HAVE_A_BADGE, xi.ki.CAPTAIN_WILDCAT_BADGE)
             player:startEvent(203)
+        elseif player:hasKeyItem(xi.ki.REMNANTS_PERMIT) then
+            player:messageSpecial(ID.text.YOU_HAVE_A_BADGE, xi.ki.REMNANTS_PERMIT)
+            player:startEvent(203)
         else
             player:startEvent(202)
         end
