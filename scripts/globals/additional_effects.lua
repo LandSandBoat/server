d99b32b1a79d0ec3fc1c439d250f68b197e36f54
WinterSolstice8 <60417494+wintersolstice8@users.noreply.github.com>
2026-08-09 18:05:49 -0600

    [lua] Make Hydro/Gust/Spark weapons not proc off wrong ammo type

--- scripts/globals/additional_effects.lua
+++ scripts/globals/additional_effects.lua
@@ -567,25 +567,39 @@ xi.additionalEffect.linearProcRate = function(dStat, dStatCap, startingRate, cap
     return math.max(rate, 1)
 end
 
+---@param element xi.element
 ---@param ammoID xi.item|number
 ---@return table<integer, integer>?
-xi.additionalEffect.getConsumableAmmoItemDamageRange = function(ammoID)
-    local items =
+xi.additionalEffect.getConsumableAmmoItemDamageRange = function(element, ammoID)
+    local elements =
     {
-        [xi.item.BATTERY]      = { 18, 25 }, -- range on these seems to be 18*N+X, 25*N where N = tier (1, 2, 3) and X = tier-1 (0, 1, 2)
-        [xi.item.KILO_BATTERY] = { 37, 50 },
-        [xi.item.MEGA_BATTERY] = { 56, 75 },
+        [xi.element.THUNDER] =
+        {
+            [xi.item.BATTERY]      = { 18, 25 }, -- range on these seems to be 18*N+X, 25*N where N = tier (1, 2, 3) and X = tier-1 (0, 1, 2)
+            [xi.item.KILO_BATTERY] = { 37, 50 },
+            [xi.item.MEGA_BATTERY] = { 56, 75 },
+        },
 
         -- Tiers 2 and 3 of pumps/fans aren't tested very well but seem to have the same range as above.
-        [xi.item.WIND_FAN] = { 18, 25 },
-        [xi.item.KILO_FAN] = { 37, 50 },
-        [xi.item.MEGA_FAN] = { 56, 75 },
+        [xi.element.WIND] =
+        {
+            [xi.item.WIND_FAN] = { 18, 25 },
+            [xi.item.KILO_FAN] = { 37, 50 },
+            [xi.item.MEGA_FAN] = { 56, 75 },
+        },
 
-        [xi.item.HYDRO_PUMP] = { 18, 25 },
-        [xi.item.KILO_PUMP]  = { 37, 50 },
-        [xi.item.MEGA_PUMP]  = { 56, 75 },
+        [xi.element.WATER] =
+        {
+            [xi.item.HYDRO_PUMP] = { 18, 25 },
+            [xi.item.KILO_PUMP]  = { 37, 50 },
+            [xi.item.MEGA_PUMP]  = { 56, 75 },
+        },
     }
 
-    -- returns nil if it's not in the table. This will signal 0% proc rate in the weapon script.
-    return items[ammoID]
+    if elements[element] then
+        -- returns nil if ammoID is not in the table. This will signal 0% proc rate in the weapon script.
+        return elements[element][ammoID]
+    end
+
+    return nil
 end
