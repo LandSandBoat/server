c3a70457e82e09644b1d83fcfe20cf9ec08d4376
Skold <113406182+Skold177@users.noreply.github.com>
2026-08-12 19:39:37 -0400

    Adjust Berserk / Defender in era module
    Sets Berserk & Defender to a static 25%

--- modules/era/lua/globals/job_utils/warrior.lua
+++ modules/era/lua/globals/job_utils/warrior.lua
@@ -1,9 +1,30 @@
 -----------------------------------
 -- Module: Warrior Job Adjustments
 -----------------------------------
+-- https://forum.square-enix.com/ffxi/threads/44592-Oct-7-2014-%28JST%29-Version-Update (Berserk and Defender changes)
+-----------------------------------
 require('modules/module_utils')
 -----------------------------------
 local m = Module:new('era_job_utils_warrior', xi.pre(xi.expansion.ABYSSEA))
+
+-- Remove Level Scaling from Berserk
+m:addOverride('xi.job_utils.warrior.useBerserk', function(player, target, ability)
+    local power    = 25 + player:getMod(xi.mod.BERSERK_POTENCY)
+    local duration = 180 + player:getMod(xi.mod.BERSERK_DURATION)
+
+    player:addStatusEffect(xi.effect.BERSERK, { power = power, duration = duration, origin = player })
+
+    return xi.effect.BERSERK
+end)
+
+-- Remove Level Scaling from Defender
+m:addOverride('xi.job_utils.warrior.useDefender', function(player, target, ability)
+    local duration = 180 + player:getMod(xi.mod.DEFENDER_DURATION)
+
+    player:addStatusEffect(xi.effect.DEFENDER, { power = 25, duration = duration, origin = player })
+
+    return xi.effect.DEFENDER
+end)
 
 -- Warrior's Charge: Remove Triple Attack bonus
 -- TODO: find a patch note or source for this change
