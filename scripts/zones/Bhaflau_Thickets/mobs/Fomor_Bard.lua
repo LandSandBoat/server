dbc1b3eec052fdde53830d5c0f6241d4ab207d04
Hatberg <46464306+Hatberg@users.noreply.github.com>
2026-08-12 18:43:09 +0200

    Fix Fomor detection

--- scripts/zones/Bhaflau_Thickets/mobs/Fomor_Bard.lua
+++ scripts/zones/Bhaflau_Thickets/mobs/Fomor_Bard.lua
@@ -1,12 +1,11 @@
 -----------------------------------
--- Area: Wajaom Woodlands
---  Mob: Fomor Beastmaster
+-- Area: Bhaflau Thickets
+--  Mob: Fomor Bard
 -----------------------------------
 ---@type TMobEntity
 local entity = {}
 
 entity.onMobInitialize = function(mob)
-    xi.pet.setMobPet(mob, 1, 'Fomors_Bats')
     mob:setMobMod(xi.mobMod.DETECTION, bit.bor(xi.detects.HEARING, xi.detects.LOWHP, xi.detects.ABILITY))
 end
 
--- scripts/zones/Bhaflau_Thickets/mobs/Fomor_Beastmaster.lua
+++ scripts/zones/Bhaflau_Thickets/mobs/Fomor_Beastmaster.lua
@@ -7,6 +7,7 @@ local entity = {}
 
 entity.onMobInitialize = function(mob)
     xi.pet.setMobPet(mob, 1, 'Fomors_Bats')
+    mob:setMobMod(xi.mobMod.DETECTION, bit.bor(xi.detects.HEARING, xi.detects.LOWHP, xi.detects.ABILITY))
 end
 
 return entity
