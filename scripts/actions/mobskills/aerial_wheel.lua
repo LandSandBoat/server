e5bd6da763f76a466993facb9d2cc0c2b0909577
ThrisStraizo <45871917+ThrisStraizo@users.noreply.github.com>
2026-08-16 17:37:39 -0700

    Fix Orc TP Skills
    This PR adjusts a few Orc TP skills to more accurately represent caps from retail.

--- scripts/actions/mobskills/aerial_wheel.lua
+++ scripts/actions/mobskills/aerial_wheel.lua
@@ -34,7 +34,7 @@ mobskillObject.onMobWeaponSkill = function(mob, target, skill, action)
     if xi.mobskills.processDamage(mob, target, skill, action, info) then
         target:takeDamage(info.damage, mob, info.attackType, info.damageType)
 
-        xi.mobskills.mobStatusEffectMove(mob, target, xi.effect.STUN, 1, 0, 4) -- TODO: Capture stun duration
+        xi.mobskills.mobStatusEffectMove(mob, target, xi.effect.STUN, 1, 0, math.randomInt(5, 10))
     end
 
     return info.damage
