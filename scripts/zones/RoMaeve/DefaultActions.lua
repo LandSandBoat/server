4952c434d6a107b1b70e2a8bd0dccfac769e609a
Nobutadas <nobutadas.one@gmail.com>
2026-08-20 14:28:08 -0500

    [lua] Windurst 8-1 optional dialog and bug fixes

--- scripts/zones/RoMaeve/DefaultActions.lua
+++ scripts/zones/RoMaeve/DefaultActions.lua
@@ -3,6 +3,5 @@ local ID = zones[xi.zone.ROMAEVE]
 return {
     ['qm1']          = { messageSpecial = ID.text.NOTHING_OUT_OF_ORDINARY },
     ['qm2']          = { messageSpecial = ID.text.PILE_OF_RUBBLE },
-    ['QuHau_Spring'] = { messageSpecial = ID.text.NOTHING_OUT_OF_ORDINARY },
-    ['Signpost']     = { messageSpecial = ID.text.SIGN },
+    ['QuHau_Spring'] = { messageSpecial = ID.text.MYSTERIOUS_ANCIENT_MONUMENT },
 }
