a6fda149106b81830191c5b2e5e3f4cbe3c879e0
Umeboshi <kuroi_neko91@live.com>
2026-08-20 05:49:49 -0700

    [lua] Eruca Adjustments

--- scripts/mixins/families/eruca.lua
+++ scripts/mixins/families/eruca.lua
@@ -6,32 +6,37 @@ g_mixins.families = g_mixins.families or {}
 -----------------------------------
 
 g_mixins.families.eruca = function(erucaMob)
-    erucaMob:addListener('ROAM_TICK', 'ERUCA_ROAM_TICK', function(mob)
-        if
-            VanadielDayElement() == xi.element.FIRE and
-            mob:getMod(xi.mod.REGAIN) == 0
-        then
-            mob:setMod(xi.mod.REGAIN, 30)
-        elseif
-            VanadielDayElement() ~= xi.element.FIRE and
-            mob:getMod(xi.mod.REGAIN) ~= 0
-        then
+    erucaMob:addListener('SPAWN', 'ERUCA_SPAWN', function(mob)
+        if xi.data.element.getWeatherElement(mob:getWeather()) == xi.element.FIRE then
+            mob:setMod(xi.mod.REGAIN, 150)
+        end
+    end)
+
+    erucaMob:addListener('WEATHER_CHANGE', 'ERUCA_WEATHER_CHANGE', function(mob, weather, element)
+        if not mob:isAlive() then
+            return
+        end
+
+        if element == xi.element.FIRE then
+            mob:setMod(xi.mod.REGAIN, 150)
+        else
             mob:setMod(xi.mod.REGAIN, 0)
         end
     end)
 
-    erucaMob:addListener('COMBAT_TICK', 'ERUCA_COMBAT_TICK', function(mob)
-        if
-            VanadielDayElement() == xi.element.FIRE and
-            mob:getMod(xi.mod.REGAIN) == 0
-        then
-            mob:setMod(xi.mod.REGAIN, 30)
-        elseif
-            VanadielDayElement() ~= xi.element.FIRE and
-            mob:getMod(xi.mod.REGAIN) ~= 0
-        then
-            mob:setMod(xi.mod.REGAIN, 0)
+    erucaMob:addListener('ENGAGE', 'ERUCA_ENGAGE', function(mob)
+        if mob:isCharmed() then
+            return
         end
+
+        -- If fire weather is active, Eruca will instantly use a mobskill when they engage.
+        if xi.data.element.getWeatherElement(mob:getWeather()) == xi.element.FIRE then
+            mob:setTP(3000)
+        end
+    end)
+
+    erucaMob:addListener('DESPAWN', 'ERUCA_DESPAWN', function(mob)
+        mob:setMod(xi.mod.REGAIN, 0)
     end)
 end
 
