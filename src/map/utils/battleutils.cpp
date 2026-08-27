73211a5d2dae292b4ea003433d68f4874330f2d5
Skold177 <113406182+Skold177@users.noreply.github.com>
2026-08-27 11:41:11 -0400

    Fix CE Reduction for Utsusemi
    Fixes a bug where the last shadow of Utsusemi was not reducing CE and the first shadow of Utsusemi:San was not reducing CE

--- src/map/utils/battleutils.cpp
+++ src/map/utils/battleutils.cpp
@@ -3109,6 +3109,15 @@ bool IsAbsorbByShadow(CBattleEntity* PDefender, CBattleEntity* PAttacker)
             static_cast<CCharEntity*>(PDefender)->setPersist(CharPersist::Effects);
         }
 
+        // player loses 25 CE every time an attack is absorbed by an utsusemi shadow
+        if (xi::Mod::UTSUSEMI == modShadow && PDefender->objtype == TYPE_PC)
+        {
+            if (auto* PMob = dynamic_cast<CMobEntity*>(PAttacker))
+            {
+                PMob->PEnmityContainer->UpdateEnmity(PDefender, -25, 0);
+            }
+        }
+
         if (Shadow == 0)
         {
             switch (modShadow)
@@ -3140,11 +3149,6 @@ bool IsAbsorbByShadow(CBattleEntity* PDefender, CBattleEntity* PAttacker)
                         case 2:
                             icon = static_cast<uint16>(xi::StatusEffect::CopyImage2);
                             break;
-                    }
-                    // player loses 25 CE if attack absorbed by utsusemi shadow
-                    if (auto* PMob = dynamic_cast<CMobEntity*>(PAttacker))
-                    {
-                        PMob->PEnmityContainer->UpdateEnmity(PDefender, -25, 0);
                     }
                     PStatusEffect->SetIcon(icon);
                     PDefender->StatusEffectContainer->UpdateStatusIcons();
