9070d72f1d48e7d554de4fefd33136d36f291598
Hatberg <46464306+Hatberg@users.noreply.github.com>
2026-09-05 23:00:27 +0200

    migrate normal mobmod detection to yaml, fix assorted detection/links (#11297)
    Co-authored-by: sruon <sruon@users.noreply.github.com>

--- scripts/zones/Bhaflau_Thickets/mobs/Fomor_Thief.lua
+++ scripts/zones/Bhaflau_Thickets/mobs/Fomor_Thief.lua
@@ -5,8 +5,4 @@
 ---@type TMobEntity
 local entity = {}
 
-entity.onMobInitialize = function(mob)
-    mob:setMobMod(xi.mobMod.DETECTION, bit.bor(xi.detects.HEARING, xi.detects.LOWHP, xi.detects.ABILITY))
-end
-
 return entity
