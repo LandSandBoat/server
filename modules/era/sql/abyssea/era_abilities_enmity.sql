4ba3a52e97971ab322ec0b7674a7c50005e95efb
Skold <113406182+Skold177@users.noreply.github.com>
2026-09-07 11:21:13 -0400

    FIx Enmity Modules / Update PDIF wRatio
    Fixes some name mismatches in the enmity module and updates wRatio function with information from Winter.

--- modules/era/sql/abyssea/era_abilities_enmity.sql
+++ modules/era/sql/abyssea/era_abilities_enmity.sql
@@ -5,13 +5,13 @@
 -- Source : https://kanican.livejournal.com/tag/enmity%20table%21/
 ------------------------------------
 
-UPDATE `abilities` SET CE = 1, VE = 900  WHERE `name` = 'Shield Bash';
-UPDATE `abilities` SET CE = 1, VE = 1800 WHERE `name` = 'Sentinel';
-UPDATE `abilities` SET CE = 1, VE = 80   WHERE `name` = 'Divine Seal';
-UPDATE `abilities` SET CE = 1, VE = 80   WHERE `name` = 'Elemental Seal';
-UPDATE `abilities` SET CE = 1, VE = 0    WHERE `name` = 'Cover';
-UPDATE `abilities` SET CE = 1, VE = 300  WHERE `name` = 'Rampart';
-UPDATE `abilities` SET CE = 1, VE = 300  WHERE `name` = 'Modus Veritas';
-UPDATE `abilities` SET CE = 1, VE = 80   WHERE `name` = 'Pianissimo';
-UPDATE `abilities` SET CE = 1, VE = 300  WHERE `name` = 'Divine Emblem';
-UPDATE `abilities` SET CE = 1, VE = 300  WHERE `name` = 'Nether Void';
+UPDATE `abilities` SET CE = 1, VE = 900  WHERE `name` = 'shield_bash';
+UPDATE `abilities` SET CE = 1, VE = 1800 WHERE `name` = 'sentinel';
+UPDATE `abilities` SET CE = 1, VE = 80   WHERE `name` = 'divine_seal';
+UPDATE `abilities` SET CE = 1, VE = 80   WHERE `name` = 'elemental_seal';
+UPDATE `abilities` SET CE = 1, VE = 0    WHERE `name` = 'cover';
+UPDATE `abilities` SET CE = 1, VE = 300  WHERE `name` = 'rampart';
+UPDATE `abilities` SET CE = 1, VE = 300  WHERE `name` = 'modus_veritas';
+UPDATE `abilities` SET CE = 1, VE = 80   WHERE `name` = 'pianissimo';
+UPDATE `abilities` SET CE = 1, VE = 300  WHERE `name` = 'divine_emblem';
+UPDATE `abilities` SET CE = 1, VE = 300  WHERE `name` = 'nether_void';
