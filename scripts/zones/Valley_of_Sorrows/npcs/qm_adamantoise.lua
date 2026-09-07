9f4bf2acfed5f9d81accc40e38d0c7b3940398c3
Skold <113406182+Skold177@users.noreply.github.com>
2026-09-07 12:28:34 -0400

    Fix Adamantoise QM Text
    Adamantoise ??? didn't have its QM text, fixes it

--- scripts/zones/Valley_of_Sorrows/npcs/qm_adamantoise.lua
+++ scripts/zones/Valley_of_Sorrows/npcs/qm_adamantoise.lua
@@ -29,7 +29,7 @@ entity.onTrade = function(player, npc, trade)
 end
 
 entity.onTrigger = function(player, npc)
-    player:messageSpecial(ID.text.NOTHING_OUT_OF_ORDINARY)
+    player:messageSpecial(ID.text.AURA_THREATENS)
 end
 
 return entity
