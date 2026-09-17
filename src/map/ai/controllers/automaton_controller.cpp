c07dc8876c393dd9737200f967face028687aada
caerulius <cae@polywhack.com>
2026-09-17 12:03:26 -0500

    stormwaker head can't enfeeble

--- src/map/ai/controllers/automaton_controller.cpp
+++ src/map/ai/controllers/automaton_controller.cpp
@@ -787,7 +787,7 @@ auto CAutomatonController::TryEnfeeble(const CurrentManeuvers& maneuvers) -> boo
             {
                 castPriority.emplace_back(SpellID::Dispel);
             }
-            break;
+            [[fallthrough]];
         }
         default:
         {
