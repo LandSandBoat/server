d45035e69b81e04a6b98a3e49ca2c1e29985eac0
WinterSolstice8 <60417494+wintersolstice8@users.noreply.github.com>
2026-09-17 09:55:35 -0600

    [core] Azure Lore doesnt consume TP when making skillchains

--- src/map/entities/char_entity.cpp
+++ src/map/entities/char_entity.cpp
@@ -1508,16 +1508,20 @@ void CCharEntity::OnCastFinished(CMagicState& state, action_t& action)
                     actionResult.recordSkillchain(effect, battleutils::TakeSkillchainDamage(this, PTarget, actionResult.param, taChar));
                 }
 
-                if (StatusEffectContainer->HasStatusEffect({ xi::StatusEffect::Sekkanoki, xi::StatusEffect::MeikyoShisui }))
+                // only Chain Affinity reduces TP
+                if (StatusEffectContainer->HasStatusEffect(xi::StatusEffect::ChainAffinity))
                 {
-                    health.tp = (health.tp > 1000 ? health.tp - 1000 : 0);
-                }
-                else
-                {
-                    health.tp = 0;
-                }
+                    if (StatusEffectContainer->HasStatusEffect({ xi::StatusEffect::Sekkanoki, xi::StatusEffect::MeikyoShisui }))
+                    {
+                        health.tp = (health.tp > 1000 ? health.tp - 1000 : 0);
+                    }
+                    else
+                    {
+                        health.tp = 0;
+                    }
 
-                StatusEffectContainer->DelStatusEffectSilent(xi::StatusEffect::ChainAffinity);
+                    StatusEffectContainer->DelStatusEffectSilent(xi::StatusEffect::ChainAffinity);
+                }
             }
 
             // Immanence will create or extend a skillchain for elemental spells
