d9421307be4bca01a199d1702283f8c1ef7dc72a
WinterSolstice8 <60417494+wintersolstice8@users.noreply.github.com>
2026-09-17 10:50:01 -0600

    [lua] Implement Signet's evasion bonus and remove old bonus

--- scripts/effects/signet.lua
+++ scripts/effects/signet.lua
@@ -7,21 +7,18 @@
 --   No TP loss while resting
 --   Bonus experience earned in smaller parties
 --   Increased defense and evasion against attacks from your auto-attack target when even match or lower
+--   DEF and Evasion are implemented in physical utils
 -----------------------------------
 ---@type TEffect
 local effectObject = {}
 
 effectObject.onEffectGain = function(target, effect)
-    target:addLatent(xi.latent.SIGNET_BONUS, 0, xi.mod.DEF, 15)
-    target:addLatent(xi.latent.SIGNET_BONUS, 0, xi.mod.EVA, 15)
 end
 
 effectObject.onEffectTick = function(target, effect)
 end
 
 effectObject.onEffectLose = function(target, effect)
-    target:delLatent(xi.latent.SIGNET_BONUS, 0, xi.mod.DEF, 15)
-    target:delLatent(xi.latent.SIGNET_BONUS, 0, xi.mod.EVA, 15)
 end
 
 return effectObject
