a6dcb8c74c158c0bf6915991e69748e648a2fdd5
Nobutadas <nobutadas.one@gmail.com>
2026-09-21 16:33:02 -0500

    [Assault] Excavation Duty mob buffs

--- scripts/zones/Lebros_Cavern/mobs/Volcanic_Bomb.lua
+++ scripts/zones/Lebros_Cavern/mobs/Volcanic_Bomb.lua
@@ -5,8 +5,13 @@
 ---@type TMobEntity
 local entity = {}
 
+entity.onMobInitialize = function(mob)
+    mob:setMod(xi.mod.HPP, 33)
+end
+
 entity.onMobSpawn = function(mob)
     xi.assault.adjustMobLevel(mob)
+    mob:setMod(xi.mod.DOUBLE_ATTACK, 25)
 end
 
 return entity
