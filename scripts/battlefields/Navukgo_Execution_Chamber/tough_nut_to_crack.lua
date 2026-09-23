b3833d86e5b7d6ed354212bda964130d0cfdd97f
Skold <113406182+Skold177@users.noreply.github.com>
2026-09-23 18:36:04 -0400

    Fix Copy/Pasted ISNM Loot Pool
    Fixes Tough Nut to Crack loot pool

--- scripts/battlefields/Navukgo_Execution_Chamber/tough_nut_to_crack.lua
+++ scripts/battlefields/Navukgo_Execution_Chamber/tough_nut_to_crack.lua
@@ -2,6 +2,7 @@
 -- Tough Nut to Crack
 -- Navukgo Execution Chamber, Confidential Imperial Order
 -- !addkeyitem CONFIDENTIAL_IMPERIAL_ORDER
+-- TODO: Get real drop weights
 -----------------------------------
 local ID = zones[xi.zone.NAVUKGO_EXECUTION_CHAMBER]
 -----------------------------------
@@ -23,55 +24,72 @@ content:addEssentialMobs({ 'Watch_Wamoura' })
 content.loot =
 {
     {
-        { itemId = xi.item.GIL, weight = xi.loot.weight.NORMAL, amount = 8000 },
+        { itemId = xi.item.GIL,                         weight = 10000, amount = 8000 },
     },
 
     {
-        { itemId = xi.item.AGILITY_POTION, weight = xi.loot.weight.NORMAL },
-        { itemId = xi.item.VITALITY_POTION, weight = xi.loot.weight.NORMAL },
-        { itemId = xi.item.STRENGTH_POTION, weight = xi.loot.weight.NORMAL },
-        { itemId = xi.item.DEXTERITY_POTION, weight = xi.loot.weight.NORMAL },
+        { itemId = xi.item.NONE,                        weight =  5000 },
+        { itemId = xi.item.GOBNIUS_RING,                weight =  1250 },
+        { itemId = xi.item.BUSKERS_EARRING,             weight =  1250 },
+        { itemId = xi.item.PIRATES_CAPE,                weight =  1250 },
+        { itemId = xi.item.STRIKE_SUBLIGAR,             weight =  1250 },
     },
 
     {
-        { itemId = xi.item.GLASS_SHEET, weight = xi.loot.weight.NORMAL },
-        { itemId = xi.item.BUNDLE_OF_HOMUNCULUS_NERVES, weight = xi.loot.weight.NORMAL },
-        { itemId = xi.item.SQUARE_OF_POLYFLAN, weight = xi.loot.weight.NORMAL },
+        { itemId = xi.item.AGILITY_POTION,              weight =  2500 },
+        { itemId = xi.item.VITALITY_POTION,             weight =  2500 },
+        { itemId = xi.item.STRENGTH_POTION,             weight =  2500 },
+        { itemId = xi.item.DEXTERITY_POTION,            weight =  2500 },
     },
 
     {
-        { itemId = xi.item.SCROLL_OF_MAIDENS_VIRELAI, weight = xi.loot.weight.HIGH },
-        { itemId = xi.item.SCROLL_OF_CARNAGE_ELEGY, weight = xi.loot.weight.HIGH },
-        { itemId = xi.item.SCROLL_OF_AERO_IV, weight = xi.loot.weight.NORMAL },
-        { itemId = xi.item.SCROLL_OF_FLARE, weight = xi.loot.weight.NORMAL },
-        { itemId = xi.item.SCROLL_OF_FLOOD, weight = xi.loot.weight.NORMAL },
-        { itemId = xi.item.SCROLL_OF_TORNADO, weight = xi.loot.weight.NORMAL },
-        { itemId = xi.item.DARK_SPIRIT_PACT, weight = xi.loot.weight.NORMAL },
-        { itemId = xi.item.LIGHT_SPIRIT_PACT, weight = xi.loot.weight.NORMAL },
+        { itemId = xi.item.CHUNK_OF_PLATINUM_ORE,       weight =  4000 },
+        { itemId = xi.item.CHUNK_OF_DARKSTEEL_ORE,      weight =  4000 },
+        { itemId = xi.item.CHUNK_OF_ADAMAN_ORE,         weight =  1000 },
+        { itemId = xi.item.CHUNK_OF_ORICHALCUM_ORE,     weight =  1000 },
     },
 
     {
-        { itemId = xi.item.CHOCOBO_EGG_A_LITTLE_WARM, weight = xi.loot.weight.NORMAL },
+        { itemId = xi.item.NONE,                        weight =  5000 },
+        { itemId = xi.item.CHUNK_OF_PLATINUM_ORE,       weight =  2000 },
+        { itemId = xi.item.CHUNK_OF_DARKSTEEL_ORE,      weight =  2000 },
+        { itemId = xi.item.CHUNK_OF_ADAMAN_ORE,         weight =   500 },
+        { itemId = xi.item.CHUNK_OF_ORICHALCUM_ORE,     weight =   500 },
     },
 
     {
-        { itemId = xi.item.WAMOURA_COCOON, weight = xi.loot.weight.NORMAL },
+        { itemId = xi.item.NONE,                        weight =  5000 },
+        { itemId = xi.item.ALUMINUM_SHEET,              weight =  1250 },
+        { itemId = xi.item.IYO_SCALE,                   weight =  1250 },
+        { itemId = xi.item.SQUARE_OF_MOBLINWEAVE,       weight =  1250 },
+        { itemId = xi.item.PETRIFIED_LOG,               weight =  1250 },
     },
 
     {
-        { itemId = xi.item.NONE, weight = xi.loot.weight.EXTREMELY_HIGH },
-        { itemId = xi.item.TEMPLAR_SABATONS, weight = xi.loot.weight.LOW },
-        { itemId = xi.item.BUSKERS_CAPE, weight = xi.loot.weight.LOW },
-        { itemId = xi.item.DOMINION_RING, weight = xi.loot.weight.LOW },
-        { itemId = xi.item.IMMORTALS_EARRING, weight = xi.loot.weight.LOW },
+        { itemId = xi.item.SCROLL_OF_AERO_IV,           weight =  1250 },
+        { itemId = xi.item.SCROLL_OF_CARNAGE_ELEGY,     weight =  1250 },
+        { itemId = xi.item.SCROLL_OF_MAIDENS_VIRELAI,   weight =  1250 },
+        { itemId = xi.item.SCROLL_OF_FLARE,             weight =  1250 },
+        { itemId = xi.item.SCROLL_OF_FLOOD,             weight =  1250 },
+        { itemId = xi.item.SCROLL_OF_TORNADO,           weight =  1250 },
+        { itemId = xi.item.DARK_SPIRIT_PACT,            weight =  1250 },
+        { itemId = xi.item.LIGHT_SPIRIT_PACT,           weight =  1250 },
     },
 
     {
-        { itemId = xi.item.NONE, weight = xi.loot.weight.EXTREMELY_HIGH },
-        { itemId = xi.item.CHUNK_OF_PLATINUM_ORE, weight = xi.loot.weight.NORMAL },
-        { itemId = xi.item.CHUNK_OF_DARKSTEEL_ORE, weight = xi.loot.weight.NORMAL },
-        { itemId = xi.item.CHUNK_OF_ADAMAN_ORE, weight = xi.loot.weight.NORMAL },
-        { itemId = xi.item.CHUNK_OF_ORICHALCUM_ORE, weight = xi.loot.weight.NORMAL },
+        { itemId = xi.item.GLASS_SHEET,                 weight =  2000 },
+        { itemId = xi.item.BUNDLE_OF_HOMUNCULUS_NERVES, weight =  2000 },
+        { itemId = xi.item.SQUARE_OF_POLYFLAN,          weight =  2000 },
+        { itemId = xi.item.DEMON_HORN,                  weight =  2000 },
+        { itemId = xi.item.MYTHRIL_GEAR_MACHINE,        weight =  2000 },
+    },
+
+    {
+        { itemId = xi.item.CHOCOBO_EGG_A_LITTLE_WARM,   weight = 10000 },
+    },
+
+    {
+        { itemId = xi.item.WAMOURA_COCOON,              weight = 10000 },
     },
 }
 
