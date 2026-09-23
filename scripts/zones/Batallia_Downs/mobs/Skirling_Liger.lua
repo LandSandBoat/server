41b1b25cf62f1468907e6bdd5b71252cb2073cc6
Frankie-hz <105882754+Frankie-hz@users.noreply.github.com>
2026-09-23 01:58:09 -0400

    Batallia Downs spawn point cleanup

--- scripts/zones/Batallia_Downs/mobs/Skirling_Liger.lua
+++ scripts/zones/Batallia_Downs/mobs/Skirling_Liger.lua
@@ -5,20 +5,7 @@
 ---@type TMobEntity
 local entity = {}
 
-entity.spawnPoints =
-{
-    { x = -380.000, y = -16.000, z = 258.000 },
-    { x = -429.000, y = -15.000, z = 240.000 },
-    { x = -437.000, y = -15.000, z = 195.000 },
-    { x = -401.000, y = -15.000, z = 173.000 },
-    { x = -395.000, y = -15.000, z = 127.000 },
-    { x = -337.000, y = -15.000, z = 115.000 },
-    { x = -321.000, y = -15.000, z = 181.000 },
-    { x = -377.000, y = -15.000, z = 204.000 },
-}
-
 entity.onMobInitialize = function(mob)
-    xi.mob.updateNMSpawnPoint(mob)
     mob:setRespawnTime(3600) -- 60 min
 end
 
@@ -35,7 +22,6 @@ entity.onMobDeath = function(mob, player, optParams)
 end
 
 entity.onMobDespawn = function(mob)
-    xi.mob.updateNMSpawnPoint(mob)
     mob:setRespawnTime(3600) -- 60 min
 end
 
