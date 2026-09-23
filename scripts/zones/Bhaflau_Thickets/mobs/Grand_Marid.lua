9bfe41969152ddf36ac41f7f4c6d51ec8da62346
Frankie-hz <105882754+Frankie-hz@users.noreply.github.com>
2026-09-23 01:58:10 -0400

    Bhaflau Thickets spawn point cleanup

--- scripts/zones/Bhaflau_Thickets/mobs/Grand_Marid.lua
+++ scripts/zones/Bhaflau_Thickets/mobs/Grand_Marid.lua
@@ -7,9 +7,4 @@ mixins = { require('scripts/mixins/families/marid'), require('scripts/mixins/fam
 ---@type TMobEntity
 local entity = {}
 
-entity.spawnPoints =
-{
-    { x =  137.000, y = -18.000, z =  334.000 }
-}
-
 return entity
