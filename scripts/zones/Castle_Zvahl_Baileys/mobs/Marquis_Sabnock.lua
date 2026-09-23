5d01dc93a975ea9c66b34055b6e06d8e98c246ab
Frankie-hz <105882754+Frankie-hz@users.noreply.github.com>
2026-09-23 02:09:07 -0400

    Castle Zvahl Baileys spawn point cleanup

--- scripts/zones/Castle_Zvahl_Baileys/mobs/Marquis_Sabnock.lua
+++ scripts/zones/Castle_Zvahl_Baileys/mobs/Marquis_Sabnock.lua
@@ -9,21 +9,8 @@ local entity = {}
 
 entity.phList =
 {
-    [ID.mob.MARQUIS_SABNOCK + 1] = ID.mob.MARQUIS_SABNOCK,
-    [ID.mob.MARQUIS_SABNOCK - 2] = ID.mob.MARQUIS_SABNOCK,
-}
-
-entity.spawnPoints =
-{
-    { x = 70.800, y = -8.000, z = -119.500 },
-    { x = 63.285, y = -8.000, z = -102.540 },
-    { x = 80.141, y = -8.000, z =  -99.397 },
-    { x = 96.419, y = -8.000, z = -118.788 },
-    { x = 91.070, y = -8.000, z = -138.734 },
-    { x = 74.301, y = -8.000, z = -131.159 },
-    { x = 84.099, y = -8.000, z = -121.188 },
-    { x = 77.088, y = -8.000, z = -113.123 },
-    { x = 68.494, y = -8.000, z = -117.972 },
+    [ID.mob.MARQUIS_SABNOCK - 3] = ID.mob.MARQUIS_SABNOCK, -- Confirmed on retail
+    [ID.mob.MARQUIS_SABNOCK - 2] = ID.mob.MARQUIS_SABNOCK, -- Confirmed on retail
 }
 
 entity.onMobInitialize = function(mob)
