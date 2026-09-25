99be9e11cd8d1a7ba027d3957d2c8c04c8665ef4
Frankie-hz <105882754+Frankie-hz@users.noreply.github.com>
2026-09-25 03:02:56 -0400

    The Sanctuary of Zi'Tah spawn point cleanup

--- scripts/zones/The_Sanctuary_of_ZiTah/mobs/Bastet.lua
+++ scripts/zones/The_Sanctuary_of_ZiTah/mobs/Bastet.lua
@@ -6,24 +6,7 @@
 ---@type TMobEntity
 local entity = {}
 
-entity.spawnPoints =
-{
-    { x = -292.801, y = -0.292, z = 196.281 },
-    { x = -513.329, y =  0.879, z = 222.353 },
-    { x = -481.339, y =  0.000, z = 238.461 },
-    { x = -452.624, y =  0.458, z = 239.152 },
-    { x = -429.475, y =  0.325, z = 282.488 },
-    { x = -400.346, y =  0.000, z = 278.538 },
-    { x = -375.992, y =  1.059, z = 296.278 },
-    { x = -340.516, y =  0.973, z = 321.054 },
-    { x = -288.632, y =  0.343, z = 247.113 },
-    { x = -339.312, y =  2.639, z = 164.708 },
-    { x = -336.081, y =  2.986, z = 101.906 },
-    { x = -406.237, y =  0.185, z =  45.638 },
-}
-
 entity.onMobInitialize = function(mob)
-    xi.mob.updateNMSpawnPoint(mob)
 end
 
 entity.onMobSpawn = function(mob)
@@ -39,7 +22,6 @@ entity.onMobDeath = function(mob, player, optParams)
 end
 
 entity.onMobDespawn = function(mob)
-    xi.mob.updateNMSpawnPoint(mob)
 end
 
 return entity
--- scripts/zones/The_Sanctuary_of_ZiTah/mobs/Keeper_of_Halidom.lua
+++ scripts/zones/The_Sanctuary_of_ZiTah/mobs/Keeper_of_Halidom.lua
@@ -9,21 +9,10 @@ local entity = {}
 
 entity.phList =
 {
-    [ID.mob.KEEPER_OF_HALIDOM - 1] = ID.mob.KEEPER_OF_HALIDOM, -- 319.939 -0.037 187.231
-}
-
-entity.spawnPoints =
-{
-    { x = 312.000, y = -0.500, z = 159.000 },
-    { x = 352.304, y =  0.183, z = 198.403 },
-    { x = 321.489, y =  0.033, z = 195.306 },
-    { x = 303.480, y =  0.847, z = 169.473 },
-    { x = 304.815, y =  0.563, z = 141.717 },
-    { x = 325.957, y =  0.096, z = 157.485 },
+    [ID.mob.KEEPER_OF_HALIDOM - 1] = ID.mob.KEEPER_OF_HALIDOM, -- Confirmed on retail
 }
 
 entity.onMobInitialize = function(mob)
-    xi.mob.updateNMSpawnPoint(mob)
 end
 
 entity.onMobSpawn = function(mob)
@@ -36,7 +25,6 @@ entity.onMobDeath = function(mob, player, optParams)
 end
 
 entity.onMobDespawn = function(mob)
-    xi.mob.updateNMSpawnPoint(mob)
 end
 
 return entity
