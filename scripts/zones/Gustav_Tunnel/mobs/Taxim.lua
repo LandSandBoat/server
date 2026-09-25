54cf4b42b9bfa3fb0ab27e7e3decfe154849d1d9
Frankie-hz <105882754+Frankie-hz@users.noreply.github.com>
2026-09-25 02:19:55 -0400

    Gustav Tunnel spawn point cleanup

--- scripts/zones/Gustav_Tunnel/mobs/Taxim.lua
+++ scripts/zones/Gustav_Tunnel/mobs/Taxim.lua
@@ -9,10 +9,7 @@ local entity = {}
 
 entity.phList =
 {
-    [ID.mob.TAXIM - 11] = ID.mob.TAXIM, -- -172.941 -1.220 55.577
-    [ID.mob.TAXIM - 4]  = ID.mob.TAXIM, -- -137.334 -0.108 48.105
-    [ID.mob.TAXIM - 3]  = ID.mob.TAXIM, -- -118.000 -0.515 79.000
-    [ID.mob.TAXIM + 2]  = ID.mob.TAXIM, -- -125.000 0.635 59.000
+    [ID.mob.TAXIM - 4] = ID.mob.TAXIM, -- Confirmed on retail
 }
 
 entity.onMobInitialize = function(mob)
