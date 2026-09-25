c0532777b2740a99381c5bb8854b0544fb066b6d
Frankie-hz <105882754+Frankie-hz@users.noreply.github.com>
2026-09-25 03:02:53 -0400

    Ranguemont Pass spawn point cleanup

--- scripts/zones/Ranguemont_Pass/mobs/Gloom_Eye.lua
+++ scripts/zones/Ranguemont_Pass/mobs/Gloom_Eye.lua
@@ -7,14 +7,9 @@ local ID = zones[xi.zone.RANGUEMONT_PASS]
 ---@type TMobEntity
 local entity = {}
 
-entity.spawnPoints =
-{
-    { x = -152.136, y = -5.216, z = -89.814 }
-}
-
 entity.phList =
 {
-    [ID.mob.GLOOM_EYE - 4] = ID.mob.GLOOM_EYE,
+    [ID.mob.GLOOM_EYE - 4] = ID.mob.GLOOM_EYE, -- Confirmed on retail
 }
 
 entity.onMobFight = function(mob, target)
