6737933b0af09f17d070679699739f0cf4ec6d73
Frankie-hz <105882754+Frankie-hz@users.noreply.github.com>
2026-09-25 03:02:58 -0400

    West Sarutabaruta spawn point cleanup

--- scripts/zones/West_Sarutabaruta/mobs/Numbing_Norman.lua
+++ scripts/zones/West_Sarutabaruta/mobs/Numbing_Norman.lua
@@ -5,16 +5,7 @@
 ---@type TMobEntity
 local entity = {}
 
-entity.spawnPoints =
-{
-    { x = -347.000, y = -29.000, z = 465.000 },
-    { x = -383.000, y = -29.000, z = 435.000 },
-    { x = -405.000, y = -29.000, z = 381.000 },
-    { x = -433.000, y = -28.000, z = 332.000 },
-}
-
 entity.onMobInitialize = function(mob)
-    xi.mob.updateNMSpawnPoint(mob)
     mob:setRespawnTime(1200) -- When server restarts, reset timer
 
     mob:setMobMod(xi.mobMod.ADD_EFFECT, 1)
@@ -38,7 +29,6 @@ entity.onMobDeath = function(mob, player, optParams)
 end
 
 entity.onMobDespawn = function(mob)
-    xi.mob.updateNMSpawnPoint(mob)
     mob:setRespawnTime(1200) -- 20 minutes
 end
 
