ff73db7ffcde6a31b8128812c7aa52c51ac2920e
Frankie-hz <105882754+Frankie-hz@users.noreply.github.com>
2026-09-25 02:43:59 -0400

    Inner Horutoto Ruins spawn point cleanup

--- scripts/zones/Inner_Horutoto_Ruins/mobs/Nocuous_Weapon.lua
+++ scripts/zones/Inner_Horutoto_Ruins/mobs/Nocuous_Weapon.lua
@@ -7,13 +7,6 @@ local ID = zones[xi.zone.INNER_HORUTOTO_RUINS]
 ---@type TMobEntity
 local entity = {}
 
-entity.spawnPoints =
-{
-    { x = -236.855, y =  0.476, z = -51.263 },
-    { x = -237.426, y =  0.500, z = -23.412 },
-    { x = -230.732, y = -0.025, z = -52.324 }
-}
-
 entity.phList =
 {
     [ID.mob.NOCUOUS_WEAPON - 4] = ID.mob.NOCUOUS_WEAPON, -- Confirmed on retail
--- scripts/zones/Inner_Horutoto_Ruins/mobs/Slendlix_Spindlethumb.lua
+++ scripts/zones/Inner_Horutoto_Ruins/mobs/Slendlix_Spindlethumb.lua
@@ -9,11 +9,6 @@ local ID = zones[xi.zone.INNER_HORUTOTO_RUINS]
 ---@type TMobEntity
 local entity = {}
 
-entity.spawnPoints =
-{
-    { x = -238.000, y = -0.500, z = -167.000 }
-}
-
 entity.phList =
 {
     [ID.mob.SLENDLIX_SPINDLETHUMB - 2] = ID.mob.SLENDLIX_SPINDLETHUMB, -- Confirmed on retail
