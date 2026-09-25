f7f1d0088e612e9511c941cf670f3a1603fc928b
Frankie-hz <105882754+Frankie-hz@users.noreply.github.com>
2026-09-25 03:02:51 -0400

    Pso'Xja spawn point cleanup

--- scripts/zones/PsoXja/mobs/Gyre-Carlin.lua
+++ scripts/zones/PsoXja/mobs/Gyre-Carlin.lua
@@ -9,12 +9,7 @@ local entity = {}
 
 entity.phList =
 {
-    -- TODO: There are 3 IDs for Gyre Carlin but all the PH share the same slot.
-    -- Need to verify which diremite (PH) spawns the correct ID.
-    -- Since only 1 NM can be up at a time use only 1 ID until verified.
-    [ID.mob.GYRE_CARLIN - 1]  = ID.mob.GYRE_CARLIN, -- Confirmed on retail
-    [ID.mob.GYRE_CARLIN + 10] = ID.mob.GYRE_CARLIN, -- Confirmed on retail
-    [ID.mob.GYRE_CARLIN + 23] = ID.mob.GYRE_CARLIN, -- Confirmed on retail
+    [ID.mob.GYRE_CARLIN - 1] = ID.mob.GYRE_CARLIN, -- Confirmed on retail
 }
 
 return entity
