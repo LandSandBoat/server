18df7ce922124703c337bd99ccbf5c38dab42fce
Frankie-hz <105882754+Frankie-hz@users.noreply.github.com>
2026-09-25 03:02:54 -0400

    Sauromugue Champaign spawn point cleanup

--- scripts/zones/Sauromugue_Champaign/mobs/Thunderclaw_Thuban.lua
+++ scripts/zones/Sauromugue_Champaign/mobs/Thunderclaw_Thuban.lua
@@ -5,17 +5,11 @@
 ---@type TMobEntity
 local entity = {}
 
-entity.spawnPoints =
-{
-    { x =  423.313, y =  16.568, z = -110.108 }
-}
-
 entity.onMobDeath = function(mob, player, optParams)
     xi.hunts.checkHunt(mob, player, 274)
 end
 
 entity.onMobDespawn = function(mob)
-    xi.mob.updateNMSpawnPoint(mob)
     mob:setRespawnTime(math.randomInt(5400, 7200)) -- 90 to 120 minutes
 end
 
