59051d0f65ffda6c95dd263b88cc5ed7234771dc
Frankie-hz <105882754+Frankie-hz@users.noreply.github.com>
2026-09-25 03:02:57 -0400

    Upper Delkfutt's Tower spawn point cleanup

--- scripts/zones/Upper_Delkfutts_Tower/mobs/Enkelados.lua
+++ scripts/zones/Upper_Delkfutts_Tower/mobs/Enkelados.lua
@@ -9,11 +9,6 @@ local ID = zones[xi.zone.UPPER_DELKFUTTS_TOWER]
 ---@type TMobEntity
 local entity = {}
 
-entity.spawnPoints =
-{
-    { x = -226.158, y = -144.099, z =  28.522 }
-}
-
 entity.phList =
 {
     [ID.mob.ENKELADOS[1] + 3] = ID.mob.ENKELADOS[1], -- Confirmed on retail
